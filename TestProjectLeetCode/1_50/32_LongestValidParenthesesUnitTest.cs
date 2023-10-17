48b5aced4cc5428666c699eff91a02fecd288beb
hongtao_luo <luohongtao621@hotmail.com>
2023-10-17 18:02:30 +0800

    32

---
+++ TestProjectLeetCode/1_50/32_LongestValidParenthesesUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._1_50;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._1_50
+{
+    [TestClass]
+    public class _32_LongestValidParenthesesUnitTest
+    {
+        LongestValidParenthesesAlg alg = new();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "(()";
+            int num1 = 2;
+            int num2 = alg.LongestValidParentheses(s);
+            Assert.AreEqual(num1, num2);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = ")()())";
+            int num1 = 4;
+            int num2 = alg.LongestValidParentheses(s);
+            Assert.AreEqual(num1, num2);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "";
+            int num1 = 0;
+            int num2 = alg.LongestValidParentheses(s);
+            Assert.AreEqual(num1, num2);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "()(())";
+            int num1 = 6;
+            int num2 = alg.LongestValidParentheses(s);
+            Assert.AreEqual(num1, num2);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            //string s = "()(())";
+            //int num1 = 6;
+            //int num2 = alg.LongestValidParentheses(s);
+            //Assert.AreEqual(num1, num2);
+        }
+    }
+}
---
+++ leetCode/1_50/32_LongestValidParenthesesAlg.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1_50
+{
+    public class LongestValidParenthesesAlg
+    {
+        public int LongestValidParentheses(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+
+            int max = 0;
+            int tempLen = 0;
+            string prefix = "()";
+            string sub = s;
+            while (true)
+            {
+                if (sub.StartsWith(prefix))
+                {
+                    tempLen += prefix.Length;
+                    if (tempLen > max)
+                    {
+                        max = tempLen;
+                    }
+                    sub = sub.Substring(prefix.Length);
+                    if (sub.Length == 0 || sub.Length == 1)
+                    {
+                        break;
+                    }
+
+                }
+                else
+                {
+                    tempLen = 0;
+                    sub = sub.Substring(1);
+                    if (sub.Length == 0 || sub.Length == 1)
+                    {
+                        break;
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
