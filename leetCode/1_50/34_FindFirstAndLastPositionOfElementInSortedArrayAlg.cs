45aa31316cf1f7d5364a8da0fe4b371c9650b7c4
hongtao_luo <luohongtao621@hotmail.com>
2023-10-30 16:44:47 +0800

    34

---
+++ leetCode/1_50/34_FindFirstAndLastPositionOfElementInSortedArrayAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1_50
+{
+    public class FindFirstAndLastPositionOfElementInSortedArrayAlg
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+
+            return null;
+        }
+    }
+}
