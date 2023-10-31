ef82d84ce7dffd26f9191e0ab49e2f1442228bf0
hongtao_luo <luohongtao621@hotmail.com>
2023-10-31 09:38:01 +0800

    34

--- TestProjectLeetCode/1_50/34_FindFirstAndLastPositionOfElementInSortedArrayUnitTest.cs
+++ TestProjectLeetCode/1_50/34_FindFirstAndLastPositionOfElementInSortedArrayUnitTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using leetCode._1_50;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,40 @@ namespace TestProjectLeetCode._1_50
     [TestClass]
     public class _34_FindFirstAndLastPositionOfElementInSortedArrayUnitTest
     {
+        FindFirstAndLastPositionOfElementInSortedArrayAlg alg = new FindFirstAndLastPositionOfElementInSortedArrayAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = { 5, 7, 7, 8, 8, 10 };
+            var index = alg.SearchRange(nums, 8);
+            if (index[0] != 3 || index[1] != 4)
+            {
+                Assert.Fail();
+            }
 
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = { 5, 7, 7, 8, 8, 10 };
+            var index = alg.SearchRange(nums, 6);
+            if (index[0] != -1 || index[1] != -1)
+            {
+                Assert.Fail();
+            }
+
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = { };
+            var index = alg.SearchRange(nums, 6);
+            if (index[0] != -1 || index[1] != -1)
+            {
+                Assert.Fail();
+            }
+
+        }
     }
 }
--- leetCode/1_50/34_FindFirstAndLastPositionOfElementInSortedArrayAlg.cs
+++ leetCode/1_50/34_FindFirstAndLastPositionOfElementInSortedArrayAlg.cs
@@ -10,8 +10,56 @@ namespace leetCode._1_50
     {
         public int[] SearchRange(int[] nums, int target)
         {
+            int[] result = { -1, -1 };
 
-            return null;
+            // 查找目标值的起始位置
+            int left = 0, right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target)
+                {
+                    result[0] = mid;
+                    right = mid - 1; // 继续查找左边的子数组
+                }
+                else if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            // 如果起始位置没找到，说明目标值不存在
+            if (result[0] == -1)
+            {
+                return result;
+            }
+
+            // 查找目标值的结束位置
+            left = result[0];
+            right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target)
+                {
+                    result[1] = mid;
+                    left = mid + 1; // 继续查找右边的子数组
+                }
+                else if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return result;
         }
     }
 }
