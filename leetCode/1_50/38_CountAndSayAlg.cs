028b7a9cb80b56d84be995cb84d4cf628339a705
hongtao_luo <luohongtao621@hotmail.com>
2023-11-01 16:29:24 +0800

    38

---
+++ leetCode/1_50/38_CountAndSayAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1_50
+{
+    public class _38_CountAndSayAlg
+    {
+        public string CountAndSay(int n)
+        {
+            return null;
+        }
+    }
+}
