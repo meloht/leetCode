de32adae7a0c5c9694c2caaf781811a3f83026b2
hongtao_luo <luohongtao621@hotmail.com>
2023-11-06 14:57:32 +0800

    41

---
+++ TestProjectLeetCode/1_50/41_FirstMissingPositiveUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using leetCode._1_50;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._1_50
+{
+    [TestClass]
+    public class _41_FirstMissingPositiveUnitTest
+    {
+        _41_FirstMissingPositiveAlg alg = new _41_FirstMissingPositiveAlg();
+    }
+}
