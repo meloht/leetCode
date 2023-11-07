a75c5693d3a9fafe98e8f57e4ca4ded7b2ee92a8
hongtao_luo <luohongtao621@hotmail.com>
2023-11-07 09:49:13 +0800

    41

--- TestProjectLeetCode/1_50/41_FirstMissingPositiveUnitTest.cs
+++ TestProjectLeetCode/1_50/41_FirstMissingPositiveUnitTest.cs
@@ -11,5 +11,29 @@ namespace TestProjectLeetCode._1_50
     public class _41_FirstMissingPositiveUnitTest
     {
         _41_FirstMissingPositiveAlg alg = new _41_FirstMissingPositiveAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var candidates = new int[] { 1, 2, 0 };
+            var res = alg.FirstMissingPositive(candidates);
+            Assert.AreEqual(3, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var candidates = new int[] { 3, 4, -1, 1 };
+            var res = alg.FirstMissingPositive(candidates);
+            Assert.AreEqual(2, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            var candidates = new int[] { 7, 8, 9, 11, 12 };
+            var res = alg.FirstMissingPositive(candidates);
+            Assert.AreEqual(1, res);
+        }
     }
 }
--- leetCode/1_50/41_FirstMissingPositiveAlg.cs
+++ leetCode/1_50/41_FirstMissingPositiveAlg.cs
@@ -10,7 +10,55 @@ namespace leetCode._1_50
     {
         public int FirstMissingPositive(int[] nums)
         {
-            return 0;
+            int numMin = int.MaxValue;
+            int count = 0;
+            foreach (int i in nums)
+            {
+                if (i > 0)
+                {
+                    count++;
+                    if (i < numMin)
+                    {
+                        numMin = i;
+                    }
+                }
+            }
+            if (numMin > 1)
+            {
+                return 1;
+            }
+            int target = 0;
+            int[] arr = new int[count];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                if (num >= numMin)
+                {
+                    int index = num - numMin;
+                    if (index < arr.Length)
+                    {
+                        arr[index] = num;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int num = arr[i];
+                if (i != arr.Length - 1)
+                {
+                    if (arr[i + 1] == 0)
+                    {
+                        target = num + 1;
+                        break;
+                    }
+                }
+                else
+                {
+                    target = num + 1;
+                }
+            }
+
+            return target;
         }
     }
 }
