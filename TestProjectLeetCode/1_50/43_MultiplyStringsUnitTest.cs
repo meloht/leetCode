2b8ca767f3fe5ed7c09b3c19c24b65799478b231
hongtao_luo <luohongtao621@hotmail.com>
2023-11-09 13:47:16 +0800

    43

---
+++ TestProjectLeetCode/1_50/43_MultiplyStringsUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._1_50;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._1_50
+{
+    [TestClass]
+    public class _43_MultiplyStringsUnitTest
+    {
+        _43_MultiplyStringsAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _43_MultiplyStringsAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var num1 = "2";
+            var num2 = "3";
+            var res = alg.Multiply(num1,num2);
+            Assert.AreEqual(6, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var num1 = "123";
+            var num2 = "456";
+            var res = alg.Multiply(num1, num2);
+            Assert.AreEqual(56088, res);
+        }
+    }
+}
---
+++ leetCode/1_50/43_MultiplyStringsAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1_50
+{
+    public class _43_MultiplyStringsAlg
+    {
+        public string Multiply(string num1, string num2)
+        {
+            return null;
+        }
+    }
+}
