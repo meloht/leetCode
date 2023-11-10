7902dc3885daebe3bf74c0a41aeb32488c4b1a25
hongtao_luo <luohongtao621@hotmail.com>
2023-11-10 17:19:41 +0800

    44

--- TestProjectLeetCode/1_50/44_WildcardMatchingUnitTest.cs
+++ TestProjectLeetCode/1_50/44_WildcardMatchingUnitTest.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,18 +30,143 @@ namespace TestProjectLeetCode._1_50
         public void TestCase02()
         {
             var s = "aa";
+            var p = "aa";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            var s = "aa";
             var p = "*";
             var res = alg.IsMatch(s, p);
             Assert.AreEqual(true, res);
         }
 
         [TestMethod]
-        public void TestCase03()
+        public void TestCase04()
         {
             var s = "cb";
             var p = "?a";
             var res = alg.IsMatch(s, p);
             Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void TestCase05()
+        {
+            var s = "aaaabcd";
+            var p = "a*bcd";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase06()
+        {
+            var s = "aaaabcd";
+            var p = "a*b*";
+      
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase07()
+        {
+            var s = "aaaabcd";
+            var p = "a*bc*";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase08()
+        {
+            var s = "aaaabcd";
+            var p = "a*b*cd";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase09()
+        {
+            var s = "aaaabcad";
+            var p = "a*b*c?d";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase10()
+        {
+            var s = "aaaabcaad";
+            var p = "a*b*ca?d";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase11()
+        {
+            var s = "ab";
+            var p = "?*";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase12()
+        {
+            var s = "abcabczzzde";
+            var p = "*abc???de*";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase13()
+        {
+            var s = "b";
+            var p = "?*?";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(false, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase14()
+        {
+            var s = "ho";
+            var p = "**ho";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase15()
+        {
+            var s = "abcede";
+            var p = "abc*d";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(false, res);
+        }
+
+        [TestMethod]
+        public void TestCase16()
+        {
+            var s = "";
+            var p = "******";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase17()
+        {
+            var s = "mississippi";
+            var p = "m*iss*iss*";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
         }
     }
 }
--- leetCode/1_50/44_WildcardMatchingAlg.cs
+++ leetCode/1_50/44_WildcardMatchingAlg.cs
@@ -10,7 +10,132 @@ namespace leetCode._1_50
     {
         public bool IsMatch(string s, string p)
         {
-            return false;
+            bool blX = p.Contains("*");
+            bool blY = p.Contains("?");
+            if (!blX && !blY)
+                return s == p;
+
+            if (p == "*")
+                return true;
+
+            if (!blX && p.Length != s.Length)
+            {
+                return false;
+            }
+            if (blX)
+            {
+                string item = p.Replace("*", "");
+                if (item.Length > s.Length)
+                    return false;
+
+                if (item.Length == s.Length)
+                {
+                    p = item;
+                }
+                if (p == s)
+                {
+                    return true;
+                }
+            }
+            if (p[p.Length - 1] != s[s.Length - 1] && p[p.Length - 1] != '*' && p[p.Length-1] != '?')
+            {
+                return false;
+            }
+            if (blX)
+            {
+                if (p.Length == s.Length)
+                {
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        if (p[i] != '*' && p[i] != '?' && p[i] != s[i])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                else
+                {
+                    int i = 0, j = 0;
+                    while (i < s.Length && j < p.Length)
+                    {
+                        if (p[j] != '*')
+                        {
+                            if (p[j] != '?' && p[j] != s[i])
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            if (p[j] == '*' && j == p.Length - 1)
+                            {
+                                return true;
+                            }
+
+     
+                            string pp = p.Substring(j + 1);
+                            int nextIndex = j + 1;
+                            int starIndex = pp.IndexOf("*");
+                            string nextItem = pp;
+
+                            if (starIndex != -1)
+                            {
+                                nextItem = pp.Substring(0, starIndex);
+                                j = nextIndex + starIndex;
+                            }
+                            else
+                            {
+                                j = nextIndex + nextItem.Length;
+                            }
+
+                            int questionIndex = nextItem.IndexOf("?");
+                            if (questionIndex != -1)
+                            {
+                                nextItem = nextItem.Substring(0, questionIndex);
+                                j = nextIndex + questionIndex;
+                            }
+
+                            var sIndex = s.Substring(i).IndexOf(nextItem);
+                            if (sIndex == 0)
+                            {
+                                sIndex = s.Substring(i + 1).IndexOf(nextItem);
+                                i = sIndex + nextItem.Length + i + 1;
+                                continue;
+                            }
+                            else if (sIndex > 0)
+                            {
+                                i = sIndex + nextItem.Length + i;
+                                continue;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+
+
+                        }
+                        i++;
+                        j++;
+                    }
+
+                   
+                    return true;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (p[i] != '?' && p[i] != s[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
         }
+
     }
 }
