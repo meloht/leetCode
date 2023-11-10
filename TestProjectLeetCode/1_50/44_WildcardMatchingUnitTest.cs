b084b517ee90f70c5bdee4ec4af0a20d71638f46
hongtao_luo <luohongtao621@hotmail.com>
2023-11-10 10:25:18 +0800

    44

---
+++ TestProjectLeetCode/1_50/44_WildcardMatchingUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._1_50;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._1_50
+{
+    [TestClass]
+    public class _44_WildcardMatchingUnitTest
+    {
+        _44_WildcardMatchingAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _44_WildcardMatchingAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var s = "aa";
+            var p = "a";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            var s = "aa";
+            var p = "*";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            var s = "cb";
+            var p = "?a";
+            var res = alg.IsMatch(s, p);
+            Assert.AreEqual(false, res);
+        }
+    }
+}
---
+++ leetCode/1_50/44_WildcardMatchingAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1_50
+{
+    public class _44_WildcardMatchingAlg
+    {
+        public bool IsMatch(string s, string p)
+        {
+            return false;
+        }
+    }
+}
