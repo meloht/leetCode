0feb0a0e75cf6416c71106a5c308414f0b260aac
hongtao_luo <luohongtao621@hotmail.com>
2023-11-13 12:53:01 +0800

    47

---
+++ TestProjectLeetCode/1_50/47_PermutationsIIUnitTest.cs
@@ -0,0 +1,67 @@
+﻿using leetCode._1_50;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._1_50
+{
+    [TestClass]
+    public class _47_PermutationsIIUnitTest
+    {
+        _47_PermutationsIIAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _47_PermutationsIIAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var nums = new int[] { 1, 1, 2 };
+            var res = alg.PermuteUnique(nums);
+            IList<IList<int>> list = new List<IList<int>>();
+            list.Add(new[] { 1, 1, 2 });
+            list.Add(new[] { 1, 2, 1 });
+            list.Add(new[] { 2, 1, 1 });
+
+
+            bool bl = Utils.IsSameList(list, res);
+            Assert.AreEqual(true, bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var nums = new int[] { 1, 2, 3 };
+            var res = alg.PermuteUnique(nums);
+            IList<IList<int>> list = new List<IList<int>>();
+            list.Add(new[] { 1, 2, 3 });
+            list.Add(new[] { 1, 3, 2 });
+            list.Add(new[] { 2, 1, 3 });
+            list.Add(new[] { 2, 3, 1 });
+            list.Add(new[] { 3, 1, 2 });
+            list.Add(new[] { 3, 2, 1 });
+
+            bool bl = Utils.IsSameList(list, res);
+            Assert.AreEqual(true, bl);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            var nums = new int[] { 3, 3, 0, 3 };
+            var res = alg.PermuteUnique(nums);
+            IList<IList<int>> list = new List<IList<int>>();
+            list.Add(new[] { 0, 3, 3, 3 });
+            list.Add(new[] { 3, 0, 3, 3 });
+            list.Add(new[] { 3, 3, 0, 3 });
+            list.Add(new[] { 3, 3, 3, 0 });
+
+            bool bl = Utils.IsSameList(list, res);
+            Assert.AreEqual(true, bl);
+        }
+    }
+}
--- leetCode/1_50/47_PermutationsIIAlg.cs
+++ leetCode/1_50/47_PermutationsIIAlg.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace leetCode._1_50
 {
-    public class _46_PermutationsAlg
+    public class _47_PermutationsIIAlg
     {
-        public IList<IList<int>> Permute(int[] nums)
+        public IList<IList<int>> PermuteUnique(int[] nums)
         {
             var res = new List<IList<int>>();
             if (nums.Length == 0)
@@ -17,37 +16,42 @@ namespace leetCode._1_50
                 res.Add(new List<int>(nums));
                 return res;
             }
-
+            HashSet<string> set = new HashSet<string>();
             List<int> current = new List<int>();
             bool[] used = new bool[nums.Length];
-            Permute(nums, current, res, used);
+            Permute(nums, set, current, res, used);
             return res;
         }
 
-
-        private static void Permute(int[] candidates, List<int> current, IList<IList<int>> result, bool[] used)
+        private static void Permute(int[] candidates, HashSet<string> set, List<int> current, IList<IList<int>> result, bool[] used)
         {
             if (current.Count == candidates.Length)
             {
-                result.Add(new List<int>(current));
+                var dd = new List<int>(current);
+                string res = string.Join(",", dd);
+                if (!set.Contains(res))
+                {
+                    result.Add(dd);
+                    set.Add(res);
+                }
+                
                 return;
             }
-
             for (int i = 0; i < candidates.Length; i++)
             {
                 if (used[i])
                 {
                     continue; // 跳过重复元素
                 }
-
+          
                 used[i] = true;
                 current.Add(candidates[i]);
-                Permute(candidates, current, result, used);
+                Permute(candidates, set, current, result, used);
                 used[i] = false;
                 current.RemoveAt(current.Count - 1);
+               
+
             }
         }
-
-
     }
 }
