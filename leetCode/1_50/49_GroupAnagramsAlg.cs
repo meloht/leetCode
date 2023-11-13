68d7cf719d65dd92cd7bcea9bb5d9db1ae16450d
hongtao_luo <luohongtao621@hotmail.com>
2023-11-13 17:03:31 +0800

    49

--- leetCode/1_50/49_GroupAnagramsAlg.cs
+++ leetCode/1_50/49_GroupAnagramsAlg.cs
@@ -12,32 +12,40 @@ namespace leetCode._1_50
         {
             List<IList<string>> result = new List<IList<string>>();
             Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
-            HashSet<string> keys = new HashSet<string>(strs);
+            Dictionary<string, int> dictArr = GetArrDict(strs);
 
-            while (keys.Count > 0)
+            while (dictArr.Count > 0)
             {
-                var str = keys.First();
+                var arrData = dictArr.First();
+                string str = arrData.Key;
                 if (!dict.ContainsKey(str))
                 {
                     dict.Add(str, new List<string>());
-                    dict[str].Add(str);
-                    keys.Remove(str);
+                    for (int i = 0; i < arrData.Value; i++)
+                    {
+                        dict[str].Add(str);
+                    }
+                    dictArr.Remove(str);
                 }
 
                 Dictionary<char, int> dict1 = GetWordDict(str);
-                var arr = keys.Where(p => p != str && p.Length == str.Length).ToList();
+                var arr = dictArr.Keys.Where(p => p != str && p.Length == str.Length).ToList();
                 foreach (var item in arr)
                 {
                     Dictionary<char, int> dict2 = GetWordDict(item);
                     if (IsSameWord(dict1, dict2))
                     {
-                        dict[str].Add(item);
-                        keys.Remove(item);
+                        int num = dictArr[item];
+                        for (int i = 0; i < num; i++)
+                        {
+                            dict[str].Add(item);
+                        }
+                        dictArr.Remove(item);
                     }
                 }
             }
 
-            foreach (var key in dict) 
+            foreach (var key in dict)
             {
                 result.Add(key.Value);
             }
@@ -59,6 +67,7 @@ namespace leetCode._1_50
             }
             return dict;
         }
+      
         private bool IsSameWord(Dictionary<char, int> dict1, Dictionary<char, int> dict2)
         {
             foreach (var item in dict1)
@@ -70,6 +79,23 @@ namespace leetCode._1_50
                     return false;
             }
             return true;
+        }
+
+        private Dictionary<string, int> GetArrDict(string[] strs)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (string str in strs)
+            {
+                if (!dict.ContainsKey(str))
+                {
+                    dict.Add(str, 1);
+                }
+                else
+                {
+                    dict[str]++;
+                }
+            }
+            return dict;
         }
 
 
