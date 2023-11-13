e2be5ee0f152a87e264217bd277813d39a2ac134
hongtao_luo <luohongtao621@hotmail.com>
2023-11-13 15:30:36 +0800

    48

--- leetCode/1_50/48_RotateImageAlg.cs
+++ leetCode/1_50/48_RotateImageAlg.cs
@@ -10,6 +10,26 @@ namespace leetCode._1_50
     {
         public void Rotate(int[][] matrix)
         {
+            int len = matrix.Length;
+            List<List<int>> list = new List<List<int>>();
+            for (int i = 0; i < len; i++)
+            {
+                List<int> arr = new List<int>();
+                for (int j = 0; j < len; j++)
+                {
+                    arr.Add(matrix[i][j]);
+                }
+                list.Add(arr);
+            }
+
+            for (int i = 0; i < len ; i++)
+            {
+                for (int j = 0, n = len - 1; j < len && n >= 0; j++, n--)
+                {
+                    matrix[i][j] = list[n][i];
+                }
+
+            }
 
         }
     }
