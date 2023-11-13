a5eabe3f71c8286884d961646c4bc7eec7a3f2be
hongtao_luo <luohongtao621@hotmail.com>
2023-11-13 13:55:10 +0800

    47

--- leetCode/1_50/47_PermutationsIIAlg.cs
+++ leetCode/1_50/47_PermutationsIIAlg.cs
@@ -3,12 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace leetCode._1_50
 {
     public class _47_PermutationsIIAlg
     {
+
         public IList<IList<int>> PermuteUnique(int[] nums)
+        {
+            var ans = new List<IList<int>>();
+            List<int> perm = new List<int>();
+            bool[] vis = new bool[nums.Length];
+            Array.Sort(nums);
+            Backtrack(nums, ans, perm, vis);
+            return ans;
+
+        }
+        public void Backtrack(int[] nums, List<IList<int>> ans, List<int> perm, bool[] vis)
+        {
+            if (perm.Count == nums.Length)
+            {
+                ans.Add(new List<int>(perm));
+                return;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1]))
+                {
+                    continue;
+                }
+                perm.Add(nums[i]);
+                vis[i] = true;
+                Backtrack(nums, ans, perm, vis);
+                vis[i] = false;
+                perm.RemoveAt(perm.Count - 1);
+            }
+        }
+
+        public IList<IList<int>> PermuteUnique1(int[] nums)
         {
             var res = new List<IList<int>>();
             if (nums.Length == 0)
@@ -16,6 +50,7 @@ namespace leetCode._1_50
                 res.Add(new List<int>(nums));
                 return res;
             }
+            Array.Sort(nums);
             HashSet<string> set = new HashSet<string>();
             List<int> current = new List<int>();
             bool[] used = new bool[nums.Length];
@@ -34,22 +69,22 @@ namespace leetCode._1_50
                     result.Add(dd);
                     set.Add(res);
                 }
-                
+
                 return;
             }
             for (int i = 0; i < candidates.Length; i++)
             {
-                if (used[i])
+                if (used[i] || (i > 0 && candidates[i] == candidates[i - 1] && !used[i - 1]))
                 {
-                    continue; // 跳过重复元素
+                    continue;
                 }
-          
+
                 used[i] = true;
                 current.Add(candidates[i]);
                 Permute(candidates, set, current, result, used);
                 used[i] = false;
                 current.RemoveAt(current.Count - 1);
-               
+
 
             }
         }
