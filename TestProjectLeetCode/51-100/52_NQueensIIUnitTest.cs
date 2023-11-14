2422a1885f50fde5fceeb754037c2a5a2b1d4071
hongtao_luo <luohongtao621@hotmail.com>
2023-11-14 14:01:33 +0800

    52

---
+++ TestProjectLeetCode/51-100/52_NQueensIIUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _52_NQueensIIUnitTest
+    {
+        _52_NQueensIIAlg alg;
+
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _52_NQueensIIAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var res = alg.TotalNQueens(4);
+
+            Assert.AreEqual(2, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var res = alg.TotalNQueens(1);
+
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ leetCode/51-100/52_NQueensIIAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _52_NQueensIIAlg
+    {
+        public int TotalNQueens(int n)
+        {
+            return 0;
+        }
+    }
+}
