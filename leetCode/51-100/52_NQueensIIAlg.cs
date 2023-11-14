a956d2a25425884c18f716aebbe584686a0a8c6d
hongtao_luo <luohongtao621@hotmail.com>
2023-11-14 14:15:28 +0800

    52

--- leetCode/51-100/52_NQueensIIAlg.cs
+++ leetCode/51-100/52_NQueensIIAlg.cs
@@ -8,9 +8,42 @@ namespace leetCode._51_100
 {
     public class _52_NQueensIIAlg
     {
+        int count = 0;
         public int TotalNQueens(int n)
         {
-            return 0;
+            int[] arr = new int[n];
+            Check(0, n, arr);
+            return count;
+        }
+
+        private void Check(int n, int max, int[] arr)
+        {
+            if (n == max)
+            {
+                count++;
+                return;
+            }
+            for (int i = 0; i < max; i++)
+            {
+                arr[n] = i;
+                if (Judge(n, arr))
+                {
+                    Check(n + 1, max, arr);
+                }
+            }
+        }
+
+
+        private bool Judge(int n, int[] arr)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == arr[n] || Math.Abs(n - i) == Math.Abs(arr[n] - arr[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
