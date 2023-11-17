b4e16abea456b2d873ede75f24d5bf22c85bafa5
hongtao_luo <luohongtao621@hotmail.com>
2023-11-17 13:48:20 +0800

    53

--- leetCode/51-100/53_MaximumSubarrayAlg.cs
+++ leetCode/51-100/53_MaximumSubarrayAlg.cs
@@ -37,54 +37,9 @@ namespace leetCode._51_100
             {
                 list.RemoveAt(list.Count - 1);
             }
-
-            List<int> listNext = list;
-            if (len > 2)
-            {
-                listNext = GetMergeList(list);
-                int lenNext = listNext.Count;
-                while (lenNext < len)
-                {
-                    len = lenNext;
-                    listNext = GetMergeList(listNext);
-                    lenNext = listNext.Count;
-                }
-
-            }
-            if (listNext.Count == 1)
-            {
-                int maxNum = listNext.Max();
-                return Math.Max(max, maxNum);
-            }
-
-            if (listNext.Count > 0)
-            {
-                len = listNext.Count;
-                int maxNum = listNext.Max();
-
-                List<int> plusList = new List<int>();
-                for (int i = 0; i < len; i++)
-                {
-                    if (listNext[i] > 0)
-                    {
-                        plusList.Add(i);
-                    }
-                }
-
-                foreach (int i in plusList)
-                {
-                    int num = GetMergeListMax(listNext, i);
-                    if (num > maxNum)
-                    {
-                        maxNum = num;
-                    }
-                }
-                if (maxNum > max)
-                {
-                    max = maxNum;
-                }
-            }
-            return max;
+            int numMax1 = GetMergeList(list);
+            return Math.Max(max, numMax1);
+           
         }
         private int GetMaxForList(int[] nums)
         {
@@ -98,118 +53,71 @@ namespace leetCode._51_100
             max = Math.Max(max, sum);
             return max;
         }
-        private int GetMergeListMax(List<int> res, int index)
-        {
-            int num = 0;
-            int left = index - 1;
-            int leftMax = int.MinValue;
-            for (int i = left; i >= 0; i--)
-            {
-                num += res[i];
-                if (num > leftMax)
-                {
-                    leftMax = num;
-                }
-            }
-            num = 0;
-            int right = index + 1;
-            int rightMax = int.MinValue;
-            for (int i = right; i < res.Count; i++)
-            {
-                num += res[i];
-                if (num > rightMax)
-                {
-                    rightMax = num;
-                }
-            }
 
-            int max = res[index];
-            if (leftMax > 0)
+        private int GetMergeList2(int[] nums)
+        {
+            int len = nums.Length;
+            int total = 0;
+            int max = int.MinValue;
+            int i = 0;
+            while (i < len)
             {
-                max += leftMax;
-            }
-            if (rightMax > 0)
-            {
-                max += rightMax;
+                int plusNum = nums[i];
+                total += plusNum;
+                max = Math.Max(total, max);
+
+                int indexNext = i + 1;
+                if (indexNext < len)
+                {
+                    int nextNum = nums[indexNext];
+
+                    int num = total + nextNum;
+                    if (num > 0)
+                    {
+                        total = num;
+                    }
+                    else
+                    {
+                        total = 0;
+                    }
+                }
+                i += 2;
             }
             return max;
         }
 
-        private List<int> GetMergeList2(List<int> res)
+        private int GetMergeList(List<int> res)
         {
-            List<int> list = new List<int>();
             int len = res.Count;
+            int total = 0;
+            int max = int.MinValue;
             int i = 0;
             while (i < len)
             {
+                int plusNum = res[i];
+                total += plusNum;
+                max = Math.Max(total, max);
 
-            }
-            return list;
-        }
-        private List<int> GetMergeList(List<int> res)
-        {
-            List<int> list = new List<int>();
-            int len = res.Count;
-            int indexPlus2 = 0;
-            int indexMinus = 0;
-            int plusNum2 = 0;
-            int minusNum = 0;
-            int plusNum1 = 0;
-            int i = 0;
-            while (i < len)
-            {
-                minusNum = 0;
-                plusNum2 = 0;
+                int indexNext = i + 1;
+                if (indexNext < len)
+                {
+                    int nextNum = res[indexNext];
 
-                plusNum1 = res[i];
-                indexMinus = i + 1;
-                indexPlus2 = i + 2;
-                if (indexMinus < len)
-                {
-                    minusNum = res[indexMinus];
-                }
-                else
-                {
-                    list.Add(plusNum1);
-                    break;
-                }
-                if (indexPlus2 < len)
-                {
-                    plusNum2 = res[indexPlus2];
-                }
-                else
-                {
-                    list.Add(plusNum1);
-                    break;
-                }
-
-                int maxPlus = Math.Max(plusNum1, plusNum2);
-
-                int newNum = Math.Min(plusNum1, plusNum2) + minusNum;
-                if (newNum > 0)
-                {
-                    list.Add(newNum + maxPlus);
-                    int index = i + 3;
-                    if (index < len)
+                    int num = total + nextNum;
+                    if (num > 0)
                     {
-                        list.Add(res[index]);
+                        total = num;
                     }
-                    i += 4;
-                }
-                else
-                {
-
-                    list.Add(plusNum1);
-                    if (minusNum < 0)
+                    else
                     {
-                        list.Add(minusNum);
+                        total = 0;
                     }
-                    i += 2;
                 }
-
+                i += 2;
             }
-            return list;
+            return max;
         }
+      
         private List<int> GetMergePlusMinus(int[] nums)
         {
             int len = nums.Length;
