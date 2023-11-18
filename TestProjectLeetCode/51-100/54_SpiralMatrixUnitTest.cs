8885b7dd61bbbea04ed617ad3222fdf6b2cbb23f
hongtao luo <luohongtao621@163.com>
2023-11-18 19:02:53 +0800

    54

---
+++ TestProjectLeetCode/51-100/54_SpiralMatrixUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _54_SpiralMatrixUnitTest
+    {
+        _54_SpiralMatrixAlg alg;
+
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _54_SpiralMatrixAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+
+            int[][] nums = new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 4, 5, 6 },
+                new int[] { 7, 8, 9 }
+            };
+            List<int> list = new List<int>() { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+            var res = alg.SpiralOrder(nums);
+            Assert.AreEqual(true, res.SequenceEqual(list));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+
+            int[][] nums = new int[][]
+            {
+                new int[] { 1,2,3,4 },
+                new int[] { 5,6,7,8 },
+                new int[] { 9, 10, 11, 12 }
+            };
+            List<int> list = new List<int>() { 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7 };
+            var res = alg.SpiralOrder(nums);
+            Assert.AreEqual(true, res.SequenceEqual(list));
+        }
+    }
+}
---
+++ leetCode/51-100/54_SpiralMatrixAlg.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _54_SpiralMatrixAlg
+    {
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            List<int> list = new List<int>();
+            int yMin = 0;
+            int yMax = matrix.Length - 1;
+            int xMin = 0;
+            int xMax = matrix[0].Length - 1;
+
+            int tempYMin = 0;
+            int tempYMax = matrix.Length - 1;
+
+            int tempXMin = 0;
+            int tempXMax = matrix[0].Length - 1;
+
+            int y = 0;
+            int x = 0;
+            while (y >= yMin && y <= yMax && x >= xMin && x <= xMax)
+            {
+                if (x <= xMax && y == yMin && y <= yMax)
+                {
+                    if (x == xMin)
+                    {
+                        xMin = tempXMin;
+                    }
+                    list.Add(matrix[y][x]);
+                    x++;
+                    if (x == xMax)
+                    {
+                        tempYMin = yMin + 1;
+                        y = yMin;
+                    }
+
+                }
+                else if (x == xMax + 1 && x >= xMin && y <= yMax)
+                {
+                    if (y == yMin)
+                    {
+                        yMin = tempYMin;
+                    }
+                    list.Add(matrix[y][x]);
+                    y++;
+                    if (y == yMax)
+                    {
+                        tempXMax = xMax - 1;
+                        x = xMax;
+                    }
+                }
+                else if (x == xMin && x <= xMax && y >= yMin)
+                {
+                    if (y == yMax)
+                    {
+                        yMax = tempYMax;
+                    }
+                    list.Add(matrix[y][x]);
+                    y--;
+
+                    if (y == yMin)
+                    {
+                        tempXMin = xMin + 1;
+                        x = xMin;
+                    }
+                }
+                else if (x >= xMin && y == yMax + 1 && y >= yMin)
+                {
+                    if (x == xMax)
+                    {
+                        xMax = tempXMax;
+                    }
+                    list.Add(matrix[y][x]);
+                    x--;
+                    if (x == xMin)
+                    {
+                        tempYMax = yMax - 1;
+                        y= yMax;
+
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            return list;
+        }
+    }
+}
