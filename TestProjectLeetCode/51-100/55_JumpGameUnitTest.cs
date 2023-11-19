bac5f63049d3ba5d889740c18a404fbd9848ff90
hongtao luo <luohongtao621@163.com>
2023-11-19 17:36:42 +0800

    55

---
+++ TestProjectLeetCode/51-100/55_JumpGameUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _55_JumpGameUnitTest
+    {
+        _55_JumpGameAlg alg;
+
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _55_JumpGameAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var nums = new int[] { 2, 3, 1, 1, 4 };
+            var res = alg.CanJump(nums);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var nums = new int[] { 3, 2, 1, 0, 4 };
+            var res = alg.CanJump(nums);
+            Assert.AreEqual(false, res);
+        }
+    }
+}
---
+++ leetCode/51-100/55_JumpGameAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _55_JumpGameAlg
+    {
+        public bool CanJump(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return true;
+            }
+            int index = 0;
+            int endIndex = nums.Length - 1;
+            while (index < nums.Length)
+            {
+                int num = nums[index];
+                if (num == 0 && index != endIndex)
+                {
+                    return false;
+                }
+
+                int end = index + num;
+                if (end >= nums.Length - 1)
+                {
+                    return true;
+                }
+                int nextIndex = 0;
+                int max = 0;
+                for (int i = index + 1; i <= end; i++)
+                {
+                    int temp = nums[i] + i;
+                    if (temp > max)
+                    {
+                        max = temp;
+                        nextIndex = i;
+                    }
+                }
+                index = nextIndex;
+            }
+            return false;
+        }
+    }
+}
