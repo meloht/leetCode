acdb53bb9b9b9874cd10b2a6a3b73b70bb936cec
hongtao_luo <luohongtao621@hotmail.com>
2023-11-20 16:44:16 +0800

    60

--- leetCode/51-100/60_PermutationSequenceAlg.cs
+++ leetCode/51-100/60_PermutationSequenceAlg.cs
@@ -8,6 +8,8 @@ namespace leetCode._51_100
 {
     public class _60_PermutationSequenceAlg
     {
+        int count = 0;
+        string res = "";
         public string GetPermutation(int n, int k)
         {
             List<int> ints = new List<int>();
@@ -16,31 +18,35 @@ namespace leetCode._51_100
                 ints.Add(i);
             }
             var nums = ints.ToArray();
-            var res = new List<IList<int>>();
+          
             if (nums.Length == 1)
             {
                 return "1";
             }
-
+            Dictionary<int, string> dict = new Dictionary<int, string>();
             List<int> current = new List<int>();
             bool[] used = new bool[nums.Length];
-            Permute(nums, current, res, used, k);
-            string ss = string.Join("", res[res.Count-1]);
-            return ss;
+            Permute(nums, current, used, k);
+            return res;
         }
 
 
-        private static void Permute(int[] candidates, List<int> current, IList<IList<int>> result, bool[] used, int k)
+        private void Permute(int[] candidates, List<int> current, bool[] used, int k)
         {
-            if (current.Count == candidates.Length && result.Count < k)
+            if (current.Count == candidates.Length)
             {
-                result.Add(new List<int>(current));
+                count++;
+                if (count == k)
+                {
+                    res = string.Join("", current);
+                }
+
                 return;
             }
 
             for (int i = 0; i < candidates.Length; i++)
             {
-                if (result.Count == k)
+                if (count == k)
                     return;
 
                 if (used[i])
@@ -50,7 +56,7 @@ namespace leetCode._51_100
 
                 used[i] = true;
                 current.Add(candidates[i]);
-                Permute(candidates, current, result, used, k);
+                Permute(candidates, current, used, k);
                 used[i] = false;
                 current.RemoveAt(current.Count - 1);
 
