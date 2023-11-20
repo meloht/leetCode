207eaf3f82060cbc809de7538a9f646cfca357cc
hongtao_luo <luohongtao621@hotmail.com>
2023-11-20 16:34:34 +0800

    60

---
+++ TestProjectLeetCode/51-100/60_PermutationSequenceUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _60_PermutationSequenceUnitTest
+    {
+        _60_PermutationSequenceAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _60_PermutationSequenceAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string res = "213";
+            var dd = alg.GetPermutation(3, 3);
+            Assert.AreEqual(res, dd);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string res = "2314";
+            var dd = alg.GetPermutation(4, 9);
+            Assert.AreEqual(res, dd);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string res = "123";
+            var dd = alg.GetPermutation(3, 1);
+            Assert.AreEqual(res, dd);
+        }
+    }
+}
---
+++ leetCode/51-100/60_PermutationSequenceAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _60_PermutationSequenceAlg
+    {
+        public string GetPermutation(int n, int k)
+        {
+            List<int> ints = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                ints.Add(i);
+            }
+            var nums = ints.ToArray();
+            var res = new List<IList<int>>();
+            if (nums.Length == 1)
+            {
+                return "1";
+            }
+
+            List<int> current = new List<int>();
+            bool[] used = new bool[nums.Length];
+            Permute(nums, current, res, used, k);
+            string ss = string.Join("", res[res.Count-1]);
+            return ss;
+        }
+
+
+        private static void Permute(int[] candidates, List<int> current, IList<IList<int>> result, bool[] used, int k)
+        {
+            if (current.Count == candidates.Length && result.Count < k)
+            {
+                result.Add(new List<int>(current));
+                return;
+            }
+
+            for (int i = 0; i < candidates.Length; i++)
+            {
+                if (result.Count == k)
+                    return;
+
+                if (used[i])
+                {
+                    continue;
+                }
+
+                used[i] = true;
+                current.Add(candidates[i]);
+                Permute(candidates, current, result, used, k);
+                used[i] = false;
+                current.RemoveAt(current.Count - 1);
+
+            }
+        }
+    }
+}
