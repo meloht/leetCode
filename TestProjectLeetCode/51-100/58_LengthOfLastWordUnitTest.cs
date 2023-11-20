01b429d74e6294cba9ca67c06b1860d720fd66b5
hongtao_luo <luohongtao621@hotmail.com>
2023-11-20 14:46:14 +0800

    58

---
+++ TestProjectLeetCode/51-100/58_LengthOfLastWordUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _58_LengthOfLastWordUnitTest
+    {
+        _58_LengthOfLastWordAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _58_LengthOfLastWordAlg();
+        }
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "Hello World";
+            int num=alg.LengthOfLastWord(s);
+            Assert.AreEqual(5, num);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "   fly me   to   the moon  ";
+            int num = alg.LengthOfLastWord(s);
+            Assert.AreEqual(4, num);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "luffy is still joyboy";
+            int num = alg.LengthOfLastWord(s);
+            Assert.AreEqual(6, num);
+        }
+    }
+}
---
+++ leetCode/51-100/58_LengthOfLastWordAlg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _58_LengthOfLastWordAlg
+    {
+        public int LengthOfLastWord(string s)
+        {
+            string ss=s.Trim();
+            var arr = ss.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            return arr[arr.Length-1].Trim().Length;
+            
+        }
+
+    }
+}
