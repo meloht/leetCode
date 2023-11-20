6516ed8ee8ce4eceee2f37e55c8dcf1ceb909012
hongtao_luo <luohongtao621@hotmail.com>
2023-11-20 14:48:50 +0800

    58

--- leetCode/51-100/58_LengthOfLastWordAlg.cs
+++ leetCode/51-100/58_LengthOfLastWordAlg.cs
@@ -8,12 +8,30 @@ namespace leetCode._51_100
 {
     public class _58_LengthOfLastWordAlg
     {
+        public int LengthOfLastWord1(string s)
+        {
+            string ss = s.Trim();
+            var arr = ss.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            return arr[arr.Length - 1].Trim().Length;
+
+        }
+
         public int LengthOfLastWord(string s)
         {
-            string ss=s.Trim();
-            var arr = ss.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            return arr[arr.Length-1].Trim().Length;
-            
+            string ss = s.Trim();
+            int num = 0;
+            for (int i = ss.Length - 1; i >= 0; i--)
+            {
+                if (ss[i] != ' ')
+                {
+                    num++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return num;
         }
 
     }
