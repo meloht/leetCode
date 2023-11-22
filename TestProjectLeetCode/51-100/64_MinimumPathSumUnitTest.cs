375b9420725bbdc19bfc4da484c0e9d32a11c373
hongtao_luo <luohongtao621@hotmail.com>
2023-11-22 16:40:20 +0800

    64

---
+++ TestProjectLeetCode/51-100/64_MinimumPathSumUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _64_MinimumPathSumUnitTest
+    {
+        _64_MinimumPathSumAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _64_MinimumPathSumAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var arr = new int[][] { [1, 3, 1], [1, 5, 1], [4, 2, 1] };
+            var res = alg.MinPathSum(arr);
+            Assert.AreEqual(7, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var arr = new int[][] { [1, 2, 3], [4, 5, 6] };
+            var res = alg.MinPathSum(arr);
+            Assert.AreEqual(12, res);
+        }
+
+    }
+}
---
+++ leetCode/51-100/64_MinimumPathSumAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _64_MinimumPathSumAlg
+    {
+        public int MinPathSum(int[][] grid)
+        {
+            return 0;
+        }
+    }
+}
