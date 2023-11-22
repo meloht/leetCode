3be2e2fd75bed8e6f7f9c7a07c3ca2bdf089fd55
hongtao luo <luohongtao621@163.com>
2023-11-22 21:17:19 +0800

    65

--- TestProjectLeetCode/51-100/65_ValidNumberUnitTest.cs
+++ TestProjectLeetCode/51-100/65_ValidNumberUnitTest.cs
@@ -35,5 +35,142 @@ namespace TestProjectLeetCode._51_100
             var res = alg.IsNumber(".");
             Assert.AreEqual(false, res);
         }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            var res = alg.IsNumber("0089");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase05()
+        {
+            var res = alg.IsNumber("2");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase06()
+        {
+            var res = alg.IsNumber("-0.1");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase07()
+        {
+            var res = alg.IsNumber("+3.14");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase08()
+        {
+            var res = alg.IsNumber("4.");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase09()
+        {
+            var res = alg.IsNumber("-.9");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase10()
+        {
+            var res = alg.IsNumber("2e10");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase11()
+        {
+            var res = alg.IsNumber("-90E3");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase12()
+        {
+            var res = alg.IsNumber("3e+7");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase13()
+        {
+            var res = alg.IsNumber("+6e-1");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase14()
+        {
+            var res = alg.IsNumber("53.5e93");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase15()
+        {
+            var res = alg.IsNumber("-123.456e789");
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase16()
+        {
+            var res = alg.IsNumber("abc");
+            Assert.AreEqual(false, res);
+        }
+
+        [TestMethod]
+        public void TestCase17()
+        {
+            var res = alg.IsNumber("1a");
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void TestCase18()
+        {
+            var res = alg.IsNumber("1e");
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void TestCase19()
+        {
+            var res = alg.IsNumber("e3");
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void TestCase20()
+        {
+            var res = alg.IsNumber("99e2.5");
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void TestCase21()
+        {
+            var res = alg.IsNumber("--6");
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void TestCase22()
+        {
+            var res = alg.IsNumber("-+3");
+            Assert.AreEqual(false, res);
+        }
+
+        [TestMethod]
+        public void TestCase23()
+        {
+            var res = alg.IsNumber("95a54e53");
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void TestCase24()
+        {
+            var res = alg.IsNumber("+.");
+            Assert.AreEqual(false, res);
+        }
+
+        [TestMethod]
+        public void TestCase25()
+        {
+            var res = alg.IsNumber("46.e3");
+            Assert.AreEqual(true, res);
+        }
     }
 }
--- leetCode/51-100/65_ValidNumberAlg.cs
+++ leetCode/51-100/65_ValidNumberAlg.cs
@@ -10,7 +10,187 @@ namespace leetCode._51_100
     {
         public bool IsNumber(string s)
         {
-            return false;
+
+            if (s.Length == 1)
+            {
+                return char.IsNumber(s[0]);
+            }
+            bool isDot = false;
+            bool isFirstDot = false;
+            int i = 0;
+            while (i < s.Length)
+            {
+                if (s[i] == '.' && isDot)
+                {
+                    return false;
+                }
+                if (s[i] == '.' && isDot == false)
+                {
+                    isDot = true;
+                    bool bl1 = false;
+                    bool bl2 = false;
+                    if (i > 0)
+                    {
+                        bl1 = char.IsNumber(s[i - 1]);
+                    }
+
+                    i++;
+                    if (i < s.Length)
+                    {
+                        bl2 = char.IsNumber(s[i]);
+                    }
+
+                    if (bl1 == false && bl2 == false)
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+
+                if (i == 0)
+                {
+                    if (s[i] == '+' || s[i] == '-' || s[i] == '.')
+                    {
+
+                    }
+                    else
+                    {
+                        if (!char.IsNumber(s[i]))
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+
+                    if (s[i] == '+' || s[i] == '-')
+                    {
+                        return false;
+                    }
+                    if (s[i - 1] == '.' && !char.IsNumber(s[i]) && !(s[i] == 'e' || s[i] == 'E'))
+                    {
+                        return false;
+                    }
+                    if (char.IsNumber(s[i - 1]) && !char.IsNumber(s[i]) && (s[i] == 'e' || s[i] == 'E'))
+                    {
+                        if ((i + 1) >= s.Length)
+                        {
+                            return false;
+                        }
+                        if (s[i + 1] == '+' || s[i + 1] == '-')
+                        {
+                            if ((i + 2) >= s.Length)
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                if (!char.IsNumber(s[i + 2]))
+                                {
+                                    return false;
+                                }
+                                i = i + 3;
+                            }
+                            i = i + 2;
+                        }
+                        else
+                        {
+                            i++;
+                        }
+                        while (i < s.Length)
+                        {
+                            if (!char.IsNumber(s[i]))
+                            {
+                                return false;
+                            }
+                            i++;
+                        }
+                        break;
+                    }
+
+                    if (!char.IsNumber(s[i]))
+                    {
+                        return false;
+                    }
+
+                }
+                i++;
+            }
+
+            return true;
+        }
+
+
+        public bool IsNumber1(string s)
+        {
+            if (s.Length == 1)
+            {
+                return char.IsNumber(s[0]);
+            }
+
+            char first = s[0];
+            if (!(first == '+' || first == '-' || first == '.'))
+            {
+                if (!char.IsNumber(first))
+                {
+                    return false;
+                }
+            }
+
+            bool isDot = first == '.';
+            int i = 1;
+            while (i < s.Length)
+            {
+                if (first == '.' && s[i] == '.')
+                    return false;
+
+                if (isDot == false && s[i] == '.')
+                {
+                    isDot = true;
+                    int index = i;
+                    if (!CheckDot(s, ref index))
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+
+                if (s[i] == '+' || s[i] == '-')
+                {
+                    return false;
+                }
+                if (s[i - 1] == '.' && !char.IsNumber(s[i]) && !(s[i] == 'e' || s[i]=='E'))
+                {
+                    return false;
+                }
+
+
+                i++;
+            }
+            return true;
+        }
+
+        private bool CheckDot(string s, ref int i)
+        {
+            bool bl1 = false;
+            bool bl2 = false;
+            if (i > 0)
+            {
+                bl1 = char.IsNumber(s[i - 1]);
+            }
+
+            i++;
+            if (i < s.Length)
+            {
+                bl2 = char.IsNumber(s[i]);
+            }
+
+            if (bl1 == false && bl2 == false)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
