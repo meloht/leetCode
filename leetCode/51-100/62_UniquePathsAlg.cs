6311d14c52825306a97edd578ceb1297a0b7e994
hongtao_luo <luohongtao621@hotmail.com>
2023-11-22 17:10:22 +0800

    62 滚动数组

--- leetCode/51-100/62_UniquePathsAlg.cs
+++ leetCode/51-100/62_UniquePathsAlg.cs
@@ -131,6 +131,44 @@ namespace leetCode._51_100
 
         #endregion
 
+        public int UniquePaths22(int m, int n)
+        {
+            int[,] f = new int[m,n];
+            for (int i = 0; i < m; ++i)
+            {
+                f[i, 0] = 1;
+            }
+            for (int j = 0; j < n; ++j)
+            {
+                f[0, j] = 1;
+            }
+            for (int i = 1; i < m; ++i)
+            {
+                for (int j = 1; j < n; ++j)
+                {
+                    f[i, j] = f[i - 1, j] + f[i, j - 1];
+                }
+            }
+            return f[m - 1, n - 1];
+        }
+
+        public int UniquePaths2(int m, int n)
+        {
+            int[] f = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                f[i] = 1;
+            }
+            for (int i = 1; i < m; ++i)
+            {
+                for (int j = 1; j < n; ++j)
+                {
+                    f[j] += f[j - 1];
+                }
+            }
+            return f[n - 1];
+        }
+
     }
 
 
