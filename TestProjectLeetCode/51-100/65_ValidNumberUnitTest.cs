4f8cf254718328c6176b24bbd9ffdea2e66dc136
hongtao_luo <luohongtao621@hotmail.com>
2023-11-22 17:41:42 +0800

    65

---
+++ TestProjectLeetCode/51-100/65_ValidNumberUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _65_ValidNumberUnitTest
+    {
+        _65_ValidNumberAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _65_ValidNumberAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var res = alg.IsNumber("0");
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            var res = alg.IsNumber("e");
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            var res = alg.IsNumber(".");
+            Assert.AreEqual(false, res);
+        }
+    }
+}
---
+++ leetCode/51-100/65_ValidNumberAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _65_ValidNumberAlg
+    {
+        public bool IsNumber(string s)
+        {
+            return false;
+        }
+    }
+}
