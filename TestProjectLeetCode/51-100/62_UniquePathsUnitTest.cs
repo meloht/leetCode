76ebce47f6ba624ca85eda6d1778298a71794066
hongtao_luo <luohongtao621@hotmail.com>
2023-11-22 14:06:24 +0800

    62

--- TestProjectLeetCode/51-100/62_UniquePathsUnitTest.cs
+++ TestProjectLeetCode/51-100/62_UniquePathsUnitTest.cs
@@ -37,17 +37,62 @@ namespace TestProjectLeetCode._51_100
             Assert.AreEqual(2, res);
         }
         [TestMethod]
-        public void TestCase05()
+        public void TestCase04()
         {
             var res = alg.UniquePaths(2, 6);
             Assert.AreEqual(6, res);
         }
 
         [TestMethod]
-        public void TestCase04()
+        public void TestCase05()
+        {
+            var res = alg.UniquePaths(3, 3);
+            Assert.AreEqual(6, res);
+        }
+        [TestMethod]
+        public void TestCase06()
+        {
+            var res = alg.UniquePaths(3, 4);
+            Assert.AreEqual(10, res);
+        }
+        [TestMethod]
+        public void TestCase07()
+        {
+            var res = alg.UniquePaths(3, 5);
+            Assert.AreEqual(15, res);
+        }
+
+        [TestMethod]
+        public void TestCase08()
+        {
+            var res = alg.UniquePaths(3, 6);
+            Assert.AreEqual(21, res);
+        }
+
+        [TestMethod]
+        public void TestCase09()
+        {
+            var res = alg.UniquePaths(3, 1);
+            Assert.AreEqual(1, res);
+        }
+        [TestMethod]
+        public void TestCase10()
+        {
+            var res = alg.UniquePaths(4, 2);
+            Assert.AreEqual(4, res);
+        }
+        [TestMethod]
+        public void TestCase11()
+        {
+            var res = alg.UniquePaths(5, 2);
+            Assert.AreEqual(5, res);
+        }
+
+        [TestMethod]
+        public void TestCase12()
         {
             var res = alg.UniquePaths(23, 12);
-            Assert.AreEqual(3, res);
+           
         }
     }
 }
--- leetCode/51-100/62_UniquePathsAlg.cs
+++ leetCode/51-100/62_UniquePathsAlg.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace leetCode._51_100
         private int mMax = 0;
         private int nMax = 0;
         private int count = 0;
-        public int UniquePaths(int m, int n)
+        public int UniquePaths1(int m, int n)
         {
             int[,] map = new int[m, n];
             mMax = m - 1;
@@ -38,8 +39,99 @@ namespace leetCode._51_100
                     SetWay(map, x, y + 1);
                     map[x, y] = 0;
                 }
-              
+
             }
         }
+
+        #region 动态规划
+
+
+
+        public int UniquePaths(int m, int n)
+        {
+            Dictionary<string, MatrixModel> dict = new Dictionary<string, MatrixModel>();
+
+            if (m < 2 || n < 2)
+            {
+                MatrixModel model = new MatrixModel(m, n);
+
+                int[,] map = new int[m, n];
+                SetPathWay(map, 0, 0, model);
+                return model.Count;
+            }
+            for (int i = 2; i <= m; i++)
+            {
+                for (int j = 2; j <= n; j++)
+                {
+                    ComputeArr(i, j, dict);
+                }
+            }
+            int num = dict[$"{m}-{n}"].Count;
+            return num;
+        }
+
+        private void ComputeArr(int m, int n, Dictionary<string, MatrixModel> dict)
+        {
+            MatrixModel data = new MatrixModel(m, n);
+
+            for (int i = 1; i <= m; i++)
+            {
+                string key = $"{i}-{n - 1}";
+                if (!dict.ContainsKey(key))
+                {
+                    MatrixModel model = new MatrixModel(i, n - 1);
+                    int[,] map = new int[model.M, model.N];
+                    SetPathWay(map, 0, 0, model);
+                    dict.Add(key, model);
+                }
+
+                data.Count += dict[key].Count;
+
+            }
+            dict.Add($"{m}-{n}", data);
+        }
+        private void SetPathWay(int[,] map, int x, int y, MatrixModel model)
+        {
+            if (x == model.EndX && y == model.EndY)
+            {
+                model.Count++;
+                return;
+            }
+            else
+            {
+                if (x > model.EndX || y > model.EndY)
+                    return;
+                if (map[x, y] == 0)
+                {
+                    map[x, y] = 1;
+                    SetPathWay(map, x + 1, y, model);
+                    SetPathWay(map, x, y + 1, model);
+                    map[x, y] = 0;
+                }
+
+            }
+        }
+
+        class MatrixModel
+        {
+            public int M { get; }
+            public int N { get; }
+            public int EndX { get; }
+            public int EndY { get; }
+            public int Count { get; set; }
+            public MatrixModel(int x, int y)
+            {
+                M = x;
+                N = y;
+                EndX = x - 1;
+                EndY = y - 1;
+            }
+
+        }
+
+        #endregion
+
     }
+
+
 }
