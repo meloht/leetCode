d20702d544932615a5e3c42525e3dd43eda2ccf0
hongtao_luo <luohongtao621@hotmail.com>
2023-11-22 16:59:34 +0800

    64

--- leetCode/51-100/64_MinimumPathSumAlg.cs
+++ leetCode/51-100/64_MinimumPathSumAlg.cs
@@ -10,7 +10,44 @@ namespace leetCode._51_100
     {
         public int MinPathSum(int[][] grid)
         {
-            return 0;
+            int m = grid.Length;
+            int n = grid[0].Length;
+            int[,] f = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                if (i == 0)
+                {
+                    f[i, 0] = grid[0][0];
+                }
+                else
+                {
+                    f[i, 0] = grid[i][0] + f[i - 1, 0];
+                }
+
+
+            }
+            for (int j = 0; j < n; j++)
+            {
+                if (j == 0)
+                {
+                    f[0, j] = grid[0][0];
+                }
+                else
+                {
+                    f[0, j] = grid[0][j] + f[0, j - 1];
+                }
+
+            }
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    f[i, j] = grid[i][j] + Math.Min(f[i - 1, j], f[i, j - 1]);
+                }
+            }
+            return f[m - 1, n - 1];
+
         }
     }
 }
