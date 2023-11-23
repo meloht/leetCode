d277762e3532f508ad9f98deaad16d7e7d98b7f4
hongtao_luo <luohongtao621@hotmail.com>
2023-11-23 16:33:41 +0800

    69

---
+++ TestProjectLeetCode/51-100/69_SqrtxUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _69_SqrtxUnitTest
+    {
+        _69_SqrtxAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _69_SqrtxAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int res = alg.MySqrt(4);
+            int exp = 2;
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int res = alg.MySqrt(8);
+            int exp = 2;
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/51-100/69_SqrtxAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _69_SqrtxAlg
+    {
+        public int MySqrt(int x)
+        {
+            return 0;
+        }
+    }
+}
