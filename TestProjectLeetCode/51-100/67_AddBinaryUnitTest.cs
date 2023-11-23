4944467accfe033e6c0947dc171732aa819afefe
hongtao_luo <luohongtao621@hotmail.com>
2023-11-23 13:20:11 +0800

    67

---
+++ TestProjectLeetCode/51-100/67_AddBinaryUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _67_AddBinaryUnitTest
+    {
+        _67_AddBinaryAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _67_AddBinaryAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var res = alg.AddBinary("11","1");
+            Assert.AreEqual("100", res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var res = alg.AddBinary("1010", "1011");
+            Assert.AreEqual("10101", res);
+        }
+    }
+}
---
+++ leetCode/51-100/67_AddBinaryAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _67_AddBinaryAlg
+    {
+        public string AddBinary(string a, string b)
+        {
+            return null;
+        }
+    }
+}
