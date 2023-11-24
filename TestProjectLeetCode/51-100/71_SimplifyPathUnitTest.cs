d1273ae46def2cbf571c13f347ac77314c08db75
hongtao_luo <luohongtao621@hotmail.com>
2023-11-24 11:31:44 +0800

    71

---
+++ TestProjectLeetCode/51-100/71_SimplifyPathUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _71_SimplifyPathUnitTest
+    {
+        _71_SimplifyPathAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _71_SimplifyPathAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var res = alg.SimplifyPath("/home/");
+            var exp = "/home";
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var res = alg.SimplifyPath("/../");
+            var exp = "/";
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            var res = alg.SimplifyPath("/home//foo/");
+            var exp = "/home/foo";
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            var res = alg.SimplifyPath("/a/./b/../../c/");
+            var exp = "/c";
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/51-100/71_SimplifyPathAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _71_SimplifyPathAlg
+    {
+        public string SimplifyPath(string path)
+        {
+            return null;
+        }
+    }
+}
