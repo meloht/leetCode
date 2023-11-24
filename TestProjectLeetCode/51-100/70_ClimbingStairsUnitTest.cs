97277e441779d0559b0bd590569db6746f3ad0b5
hongtao_luo <luohongtao621@hotmail.com>
2023-11-24 11:20:15 +0800

    70

---
+++ TestProjectLeetCode/51-100/70_ClimbingStairsUnitTest.cs
@@ -0,0 +1,66 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _70_ClimbingStairsUnitTest
+    {
+        _70_ClimbingStairsAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _70_ClimbingStairsAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int res = alg.ClimbStairs(2);
+            int exp = 2;
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int res = alg.ClimbStairs(3);
+            int exp = 3;
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int res = alg.ClimbStairs(4);
+            int exp = 5;
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int res = alg.ClimbStairs(5);
+            int exp = 8;
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int res = alg.ClimbStairs(6);
+            int exp = 13;
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase06()
+        {
+            int res = alg.ClimbStairs(45);
+
+        }
+    }
+}
---
+++ leetCode/51-100/70_ClimbingStairsAlg.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _70_ClimbingStairsAlg
+    {
+        int count = 0;
+        public int ClimbStairs1(int n)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            dict.Add(1, 1);
+            dict.Add(2, 2);
+
+            for (int i = 3; i <= n; i++)
+            {
+                if (!dict.ContainsKey(i))
+                {
+                    int num = dict[i - 1] + dict[i - 2];
+                    dict.Add(i, num);
+                }
+            }
+            return dict[n];
+        }
+
+        public int ClimbStairs(int n)
+        {
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 2;
+            int num1 = 1;
+            int num2 = 2;
+            int total = num1 + num2;
+
+            for (int i = 3; i <= n; i++)
+            {
+                total = num1 + num2;
+                num1 = num2;
+                num2 = total;
+
+            }
+            return total;
+        }
+
+        private void RunSteps(int n)
+        {
+            if (n <= 0)
+            {
+                count++;
+                return;
+            }
+            for (int i = 1; i <= 2; i++)
+            {
+                if (n >= i)
+                {
+                    RunSteps(n - i);
+                }
+            }
+        }
+
+
+
+
+    }
+}
