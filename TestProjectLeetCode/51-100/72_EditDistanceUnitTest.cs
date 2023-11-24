fbbcde5a47ee4e555a71279617fb33f23e885cf0
hongtao_luo <luohongtao621@hotmail.com>
2023-11-24 12:34:06 +0800

    72

---
+++ TestProjectLeetCode/51-100/72_EditDistanceUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _72_EditDistanceUnitTest
+    {
+        _72_EditDistanceAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _72_EditDistanceAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var res = alg.MinDistance("horse", "ros");
+            var exp = 3;
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var res = alg.MinDistance("intention", "execution");
+            var exp = 5;
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/51-100/72_EditDistanceAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _72_EditDistanceAlg
+    {
+        public int MinDistance(string word1, string word2)
+        {
+            return 0;
+        }
+    }
+}
