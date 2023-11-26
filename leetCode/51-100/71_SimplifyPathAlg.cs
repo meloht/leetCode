961d4dabef988bfa7a2b7a742d466eb360ecea16
hongtao luo <luohongtao621@163.com>
2023-11-26 13:53:02 +0800

    71

--- leetCode/51-100/71_SimplifyPathAlg.cs
+++ leetCode/51-100/71_SimplifyPathAlg.cs
@@ -10,29 +10,22 @@ namespace leetCode._51_100
     {
         public string SimplifyPath(string path)
         {
-            string sss = path.Replace("/./", "/");
-            while (sss.Contains("/./"))
-            {
-                sss = sss.Replace("/./", "/");
-            }
-            if (sss.EndsWith("/."))
-            {
-                sss = $"{sss}/".Replace("/./", "/");
-            }
+            string sss = path;
             var arr = sss.Split('/', StringSplitOptions.RemoveEmptyEntries).ToList();
             List<string> res = new List<string>();
             for (int i = 0; i < arr.Count; i++)
             {
-                if (arr[i] != "..")
-                {
-                    res.Add(arr[i]);
-                }
-                else
+                if (arr[i] == "..")
                 {
                     if (res.Count > 0)
                     {
                         res.RemoveAt(res.Count - 1);
                     }
+                   
+                }
+                else if(arr[i]!=".")
+                {
+                    res.Add(arr[i]);
                 }
             }
 
