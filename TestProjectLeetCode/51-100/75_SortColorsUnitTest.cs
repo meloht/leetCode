195fd6acda73e4306ba6cecf9cc14ef6692848e4
hongtao_luo <luohongtao621@hotmail.com>
2023-11-27 17:08:49 +0800

    75

---
+++ TestProjectLeetCode/51-100/75_SortColorsUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _75_SortColorsUnitTest
+    {
+        _75_SortColorsAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _75_SortColorsAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var nums = new int[] { 2, 0, 2, 1, 1, 0 };
+            var exp = new int[] { 0, 0, 1, 1, 2, 2 };
+
+            alg.SortColors(nums);
+            Assert.AreEqual(true, nums.SequenceEqual(exp));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var nums = new int[] { 2, 0, 1 };
+            var exp = new int[] { 0, 1, 2 };
+
+            alg.SortColors(nums);
+            Assert.AreEqual(true, nums.SequenceEqual(exp));
+        }
+    }
+}
---
+++ leetCode/51-100/75_SortColorsAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _75_SortColorsAlg
+    {
+        public void SortColors(int[] nums)
+        {
+            int a = 0, b = 0, c = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    b++;
+                }
+                else if (nums[i] == 0)
+                {
+                    a++;
+                }
+                else
+                {
+                    c++;
+                }
+            }
+            for (int i = 0; i < a; i++)
+            {
+                nums[i] = 0;
+            }
+            int len = a + b;
+            for (int i = a; i < len; i++)
+            {
+                nums[i] = 1;
+            }
+            for (int i = len; i < nums.Length; i++)
+            {
+                nums[i] = 2;
+            }
+        }
+    }
+}
