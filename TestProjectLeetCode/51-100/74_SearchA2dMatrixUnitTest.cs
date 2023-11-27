9e4461a873e362b2574fef5e4c1c6a1badcc3c53
hongtao_luo <luohongtao621@hotmail.com>
2023-11-27 16:02:44 +0800

    74

---
+++ TestProjectLeetCode/51-100/74_SearchA2dMatrixUnitTest.cs
@@ -0,0 +1,98 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _74_SearchA2dMatrixUnitTest
+    {
+        _74_SearchA2dMatrixAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _74_SearchA2dMatrixAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var matrix = new int[][] { [1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60] };
+            bool bl = alg.SearchMatrix(matrix, 3);
+            Assert.AreEqual(bl, true);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var matrix = new int[][] { [1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60] };
+            bool bl = alg.SearchMatrix(matrix, 13);
+            Assert.AreEqual(bl, false);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            var matrix = new int[][] { [1] };
+            bool bl = alg.SearchMatrix(matrix, 0);
+            Assert.AreEqual(bl, false);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            var matrix = new int[][] { [1] };
+            bool bl = alg.SearchMatrix(matrix, 1);
+            Assert.AreEqual(bl, true);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            var matrix = new int[][] { [1], [3] };
+            bool bl = alg.SearchMatrix(matrix, 2);
+            Assert.AreEqual(bl, false);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            var matrix = new int[][] { [1], [3] };
+            bool bl = alg.SearchMatrix(matrix, 3);
+            Assert.AreEqual(bl, true);
+        }
+        [TestMethod]
+        public void TestCase07()
+        {
+            var matrix = new int[][] { [1], [3], [5] };
+            bool bl = alg.SearchMatrix(matrix, 4);
+            Assert.AreEqual(bl, false);
+        }
+
+        [TestMethod]
+        public void TestCase08()
+        {
+            var matrix = new int[][] { [1], [3], [5] };
+            bool bl = alg.SearchMatrix(matrix, 5);
+            Assert.AreEqual(bl, true);
+        }
+
+        [TestMethod]
+        public void TestCase09()
+        {
+            var matrix = new int[][] { [1], [3], [5] };
+            bool bl = alg.SearchMatrix(matrix, 6);
+            Assert.AreEqual(bl, false);
+        }
+
+        [TestMethod]
+        public void TestCase10()
+        {
+            var matrix = new int[][] { [-9, -7, -7, -5, -3], [-1, 0, 1, 3, 3], [5, 7, 9, 11, 12], [13, 14, 15, 16, 18], [19, 21, 22, 22, 22] };
+            bool bl = alg.SearchMatrix(matrix, -4);
+            Assert.AreEqual(bl, false);
+        }
+    }
+}
---
+++ leetCode/51-100/74_SearchA2dMatrixAlg.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace leetCode._51_100
+{
+    public class _74_SearchA2dMatrixAlg
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            List<Node> list = new List<Node>(matrix.Length);
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                list.Add(new Node(matrix[i][0], matrix[i][matrix[i].Length - 1], matrix[i]));
+
+            }
+
+            bool bl = GetBinaryIndex(list, 0, list.Count - 1, target); ;
+
+            return bl;
+        }
+        private bool GetBinaryIndex(List<Node> list, int left, int right, int target)
+        {
+            if (left == right)
+            {
+                if (left < list.Count)
+                {
+                    return list[left].Arr.Contains(target);
+                }
+                return false;
+            }
+
+            int mid = (left + right) / 2;
+
+            var node = list[mid];
+
+
+            if (target >= node.Begin && target <= node.End)
+            {
+                return node.Arr.Any(p => p == target);
+            }
+            else if (target > node.End)
+            {
+                int next = mid + 1;
+                if (next < list.Count)
+                {
+                    var nextNode = list[next];
+                    if (target >= nextNode.Begin && target <= nextNode.End)
+                    {
+                        return nextNode.Arr.Any(p => p == target);
+                    }
+                    else if (target < nextNode.Begin)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return GetBinaryIndex(list, mid + 2, right, target);
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (target < node.Begin)
+            {
+                int pre = mid - 1;
+                if (pre >= 0)
+                {
+                    var preNode = list[pre];
+                    if (target >= preNode.Begin && target <= preNode.End)
+                    {
+                        return preNode.Arr.Any(p => p == target);
+                    }
+                    else if (target < preNode.Begin)
+                    {
+                        return GetBinaryIndex(list, left, mid - 2, target); ;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+
+                }
+
+            }
+
+            return false;
+        }
+
+        class Node
+        {
+            public int Begin;
+            public int End;
+            public int[] Arr;
+            public Node(int begin, int end, int[] arr)
+            {
+                this.Begin = begin;
+                this.End = end;
+                Arr = arr;
+            }
+        }
+    }
+}
