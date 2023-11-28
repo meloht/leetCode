2c3a579c38690e2c15d81073c12d416ec46ff52e
hongtao_luo <luohongtao621@hotmail.com>
2023-11-28 12:49:15 +0800

    76

--- leetCode/51-100/76_MinimumWindowSubstringAlg.cs
+++ leetCode/51-100/76_MinimumWindowSubstringAlg.cs
@@ -10,8 +10,68 @@ namespace leetCode._51_100
     {
         public string MinWindow(string s, string t)
         {
+            int n = s.Length;
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            foreach (char c in t)
+            {
+                if (!dict.ContainsKey(c))
+                {
+                    dict.Add(c, 1);
+                }
+                else
+                {
+                    dict[c]++;
+                }
+            }
+            List<int> arr = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (dict.ContainsKey(s[i]))
+                {
+                    arr.Add(i);
+                }
+            }
+            if (arr.Count == 0 || arr.Count < t.Length)
+                return string.Empty;
 
-            return null;
+            string res = "";
+            int min = n + 1;
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in arr)
+            {
+                sb.Clear();
+                Dictionary<char, int> dictTemp = new Dictionary<char, int>(dict);
+                int index = item;
+                int count = 0;
+
+                for (int i = item; i < n; i++)
+                {
+                    sb.Append(s[i]);
+                    if (dictTemp.ContainsKey(s[i]))
+                    {
+                        dictTemp[s[i]]--;
+                        if (dictTemp[s[i]] == 0)
+                        {
+                            dictTemp.Remove(s[i]);
+                            if (dictTemp.Count == 0)
+                            {
+
+                                if ((count + 1) < min)
+                                {
+                                    res = sb.ToString();
+                                    min = count + 1;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    count++;
+                }
+            }
+            return res;
         }
+
+
     }
 }
