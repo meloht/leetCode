b4e5ba8352c271a2d063f2cdffcb075c292f963d
hongtao luo <luohongtao621@163.com>
2023-11-28 22:42:33 +0800

    76

--- leetCode/51-100/76_MinimumWindowSubstringAlg.cs
+++ leetCode/51-100/76_MinimumWindowSubstringAlg.cs
@@ -3,71 +3,129 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace leetCode._51_100
 {
     public class _76_MinimumWindowSubstringAlg
     {
-        public string MinWindow2(string s, string t)
+        public string MinWindow(string s, string t)
         {
             if (s.Length < t.Length)
                 return string.Empty;
 
             int n = s.Length;
-            Dictionary<char, int> dict = new Dictionary<char, int>();
+            Dictionary<char, Node> dict = new Dictionary<char, Node>();
             foreach (char c in t)
             {
                 if (!dict.ContainsKey(c))
                 {
-                    dict.Add(c, 1);
+                    dict.Add(c, new Node() { Total = 1 });
                 }
                 else
                 {
-                    dict[c]++;
+                    dict[c].Total++;
                 }
             }
-            int begin = 0;
+            int index = 0;
+
             for (int i = 0; i < n; i++)
             {
                 if (dict.ContainsKey(s[i]))
                 {
-                    begin = i;
+                    index = i;
                     break;
                 }
             }
+
+            Queue<char> queue = new Queue<char>();
+            HashSet<char> overflowSet = new HashSet<char>(); 
+            int actNum = 0;
             int min = n + 1;
             string res = "";
-            Dictionary<char, int> dictTemp = new Dictionary<char, int>(dict);
-            List<char> list = new List<char>();
-            for (int i = begin; i < n; i++)
+
+
+            while (index < n)
             {
-                char ch = s[i];
-                list.Add(ch);
-                if (dictTemp.ContainsKey(s[i]))
+                char ch = s[index];
+                queue.Enqueue(ch);
+                if (dict.ContainsKey(ch))
                 {
-                    dictTemp[s[i]]--;
-                    if (dictTemp[s[i]] == 0)
+                    var node = dict[ch];
+                    if (node.Count < node.Total)
                     {
-                        dictTemp.Remove(s[i]);
-                        if (dictTemp.Count == 0)
+                        node.Count++;
+                        actNum++;
+                    }
+                    else if (node.Count == node.Total)
+                    {
+                        node.OverFlow++;
+                        overflowSet.Add(ch);
+                    }
+                    if (overflowSet.Count > 0)
+                    {
+                        RemoveQueue(queue, dict, overflowSet);
+                    }
+                    if (actNum == t.Length)
+                    {
+
+                        if (queue.Count < min)
                         {
-                            begin = i + 1;
-                            min = list.Count;
-                            res = string.Join("", list);
-                            break;
+                            res = string.Join("", queue);
+                            min = queue.Count;
                         }
                     }
                 }
+
+                index++;
             }
 
-            for (int i = begin; i < n; i++)
-            {
 
-            }
-
-            return null;
+            return res;
         }
-        public string MinWindow(string s, string t)
+
+        private void RemoveQueue(Queue<char> queue, Dictionary<char, Node> dict, HashSet<char> overflowSet)
+        {
+            while (queue.Count > 0)
+            {
+                var test = queue.Peek();
+                if (dict.ContainsKey(test))
+                {
+                    var node = dict[test];
+                    if (overflowSet.Contains(test) && node.OverFlow > 0)
+                    {
+                        queue.Dequeue();
+                        node.OverFlow--;
+                        if (node.OverFlow == 0)
+                        {
+                            overflowSet.Remove(test);
+                        }
+                    }
+                    else if (!dict.ContainsKey(test))
+                    {
+                        queue.Dequeue();
+                    }
+                    else if (dict.ContainsKey(test) && !overflowSet.Contains(test))
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    queue.Dequeue();
+                }
+               
+            }
+        }
+
+        class Node
+        {
+            public int Total;
+            public int Count;
+            public int OverFlow;
+        }
+
+        public string MinWindow1(string s, string t)
         {
             if (s.Length < t.Length)
                 return string.Empty;
