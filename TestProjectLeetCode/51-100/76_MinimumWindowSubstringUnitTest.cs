d086c1186d9edbd61e414c58f44211b1d946a3d7
hongtao_luo <luohongtao621@hotmail.com>
2023-11-28 10:59:22 +0800

    76

---
+++ TestProjectLeetCode/51-100/76_MinimumWindowSubstringUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _76_MinimumWindowSubstringUnitTest
+    {
+        _76_MinimumWindowSubstringAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _76_MinimumWindowSubstringAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "ADOBECODEBANC";
+            string t = "ABC";
+            string exp = "BANC";
+            var res = alg.MinWindow(s, t);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "a";
+            string t = "a";
+            string exp = "a";
+            var res = alg.MinWindow(s, t);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "a";
+            string t = "aa";
+            string exp = "";
+            var res = alg.MinWindow(s, t);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/51-100/76_MinimumWindowSubstringAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _76_MinimumWindowSubstringAlg
+    {
+        public string MinWindow(string s, string t)
+        {
+
+            return null;
+        }
+    }
+}
