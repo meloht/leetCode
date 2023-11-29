0be6ebea2c4ee0b31b8b389c5ab8d07e1237bf54
hongtao_luo <luohongtao621@hotmail.com>
2023-11-29 16:09:41 +0800

    78

--- leetCode/51-100/78_SubsetsAlg.cs
+++ leetCode/51-100/78_SubsetsAlg.cs
@@ -10,7 +10,32 @@ namespace leetCode._51_100
     {
         public IList<IList<int>> Subsets(int[] nums)
         {
-            return null;
+            List<IList<int>> res = new List<IList<int>>();
+            res.Add(new int[] { });
+
+            List<int> ls = new List<int>();
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                CombineList(res, 0, i, ls.ToArray(), nums);
+            }
+
+            return res;
+        }
+
+        private void CombineList(List<IList<int>> res, int begin, int k, int[] arr, int[] nums)
+        {
+            if (arr.Length == k)
+            {
+                res.Add(arr.ToList());
+                return;
+            }
+            List<int> ls = arr.ToList();
+            for (int i = begin; i < nums.Length; i++)
+            {
+                ls.Add(nums[i]);
+                CombineList(res, i + 1, k, ls.ToArray(), nums);
+                ls.RemoveAt(ls.Count - 1);
+            }
         }
     }
 }
