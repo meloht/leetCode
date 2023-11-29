8f4f81059749d649e12b11b2aef7fc7dd7585ccd
hongtao_luo <luohongtao621@hotmail.com>
2023-11-29 17:05:00 +0800

    79

---
+++ TestProjectLeetCode/51-100/79_WordSearchUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _79_WordSearchUnitTest
+    {
+        _79_WordSearchAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _79_WordSearchAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[][] arr =new char[][] { ['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E'] };
+          
+            var res = alg.Exist(arr, "ABCCED");
+          
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[][] arr = new char[][] { ['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E'] };
+
+            var res = alg.Exist(arr, "SEE");
+
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            char[][] arr = new char[][] { ['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E'] };
+
+            var res = alg.Exist(arr, "ABCB");
+
+            Assert.AreEqual(false, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            char[][] arr = new char[][] { ['a', 'b'], ['c', 'd'] };
+
+            var res = alg.Exist(arr, "bacd");
+
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ leetCode/51-100/79_WordSearchAlg.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _79_WordSearchAlg
+    {
+        public bool Exist(char[][] board, string word)
+        {
+            List<int[]> list = new List<int[]>();
+            char begin = word[0];
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    if (board[i][j] == begin)
+                    {
+                        list.Add(new[] { i, j });
+                    }
+                }
+            }
+
+            bool[,] used = new bool[board.Length, board[0].Length];
+            foreach (var item in list)
+            {
+                bool bl = Dfs(board, item[0], item[1], word, 0, used);
+                if (bl)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool Dfs(char[][] board, int i, int j, string word, int index, bool[,] used)
+        {
+            if (i >= board.Length || i < 0 || j >= board[0].Length || j < 0)
+                return false;
+            if (used[i, j] == true)
+                return false;
+
+           
+            char ch = word[index];
+            if (board[i][j] == ch)
+            {
+
+                if (index == word.Length - 1)
+                {
+                    return true;
+                }
+                index++;
+                used[i, j] = true;
+                bool bl = Dfs(board, i + 1, j, word, index, used);
+                used[i, j] = false;
+                if (bl == false)
+                {
+                    used[i, j] = true;
+                    bl = Dfs(board, i, j + 1, word, index, used);
+                    used[i, j] = false;
+                    if (bl)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        used[i, j] = true;
+                        bl = Dfs(board, i - 1, j, word, index, used);
+                        used[i, j] = false;
+                        if (bl)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            used[i, j] = true;
+                            bl = Dfs(board, i, j - 1, word, index, used);
+                            used[i, j] = false;
+                            return bl;
+                        }
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
