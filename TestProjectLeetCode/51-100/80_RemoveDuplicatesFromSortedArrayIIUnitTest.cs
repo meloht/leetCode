7f1e59a3f8f1f430370580f1db9fd946e25a0193
hongtao_luo <luohongtao621@hotmail.com>
2023-11-29 17:15:12 +0800

    80

---
+++ TestProjectLeetCode/51-100/80_RemoveDuplicatesFromSortedArrayIIUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _80_RemoveDuplicatesFromSortedArrayIIUnitTest
+    {
+        _80_RemoveDuplicatesFromSortedArrayIIAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _80_RemoveDuplicatesFromSortedArrayIIAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = { 1, 1, 1, 2, 2, 3 };
+
+            var res = alg.RemoveDuplicates(arr);
+
+            Assert.AreEqual(5, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+
+            var res = alg.RemoveDuplicates(arr);
+
+            Assert.AreEqual(7, res);
+        }
+    }
+}
---
+++ leetCode/51-100/80_RemoveDuplicatesFromSortedArrayIIAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _80_RemoveDuplicatesFromSortedArrayIIAlg
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
