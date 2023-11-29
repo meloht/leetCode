71a5b8859892739f8be7710bb59b4366985bb0ab
hongtao_luo <luohongtao621@hotmail.com>
2023-11-29 17:58:14 +0800

    80

--- leetCode/51-100/80_RemoveDuplicatesFromSortedArrayIIAlg.cs
+++ leetCode/51-100/80_RemoveDuplicatesFromSortedArrayIIAlg.cs
@@ -10,7 +10,57 @@ namespace leetCode._51_100
     {
         public int RemoveDuplicates(int[] nums)
         {
-            return 0;
+
+            int i = 0;
+            int len = nums.Length;
+            while (i < len)
+            {
+
+                int j = i + 1;
+                int count = 1;
+
+                while (j < len)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+
+                        if (count <= 2)
+                        {
+                            i = j - 1;
+                            break;
+                        }
+                        else
+                        {
+
+                            int begin = j - (count - 2);
+                            Swap(nums, begin, j, len);
+                            len = len - (count - 2);
+                            i = j - 1;
+                            break;
+                        }
+
+                    }
+                    j++;
+                }
+                i++;
+            }
+            return len;
+        }
+
+        private void Swap(int[] nums, int begin, int nextIndex, int len)
+        {
+            int end = nums.Length - 2;
+            for (int i = begin, j = nextIndex; i < end && nextIndex < len; i++, j++)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+            }
         }
     }
 }
