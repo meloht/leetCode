aa6afcca083d1bd64339ddc0d6ee9241f007358b
hongtao_luo <luohongtao621@hotmail.com>
2023-11-29 15:51:33 +0800

    78

---
+++ TestProjectLeetCode/51-100/78_SubsetsUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _78_SubsetsUnitTest
+    {
+        _78_SubsetsAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _78_SubsetsAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = { 1, 2, 3 };
+            IList<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { });
+            exp.Add(new[] { 1 });
+            exp.Add(new[] { 2 });
+            exp.Add(new[] { 1, 2 });
+            exp.Add(new[] { 3 });
+            exp.Add(new[] { 1, 3 });
+            exp.Add(new[] { 2, 3 });
+            exp.Add(new[] { 1, 2, 3 });
+
+            var res = alg.Subsets(arr);
+            bool bl = Utils.IsSame(exp, res);
+            Assert.AreEqual(true, bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = { 0 };
+            IList<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { });
+            exp.Add(new[] { 0 });
+
+            var res = alg.Subsets(arr);
+            bool bl = Utils.IsSame(exp, res);
+            Assert.AreEqual(true, bl);
+        }
+    }
+}
---
+++ leetCode/51-100/78_SubsetsAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _78_SubsetsAlg
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            return null;
+        }
+    }
+}
