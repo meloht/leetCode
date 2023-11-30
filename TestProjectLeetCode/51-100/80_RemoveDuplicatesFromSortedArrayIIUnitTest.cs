e872aab8d6136dc75c8363ff4c60a359ff1e27aa
hongtao_luo <luohongtao621@hotmail.com>
2023-11-30 12:39:46 +0800

    80

--- TestProjectLeetCode/51-100/80_RemoveDuplicatesFromSortedArrayIIUnitTest.cs
+++ TestProjectLeetCode/51-100/80_RemoveDuplicatesFromSortedArrayIIUnitTest.cs
@@ -21,9 +21,9 @@ namespace TestProjectLeetCode._51_100
         public void TestCase01()
         {
             int[] arr = { 1, 1, 1, 2, 2, 3 };
-
+            int[] exp = { 1, 1, 2, 2, 3, 1 };
             var res = alg.RemoveDuplicates(arr);
-
+   
             Assert.AreEqual(5, res);
         }
 
@@ -31,10 +31,31 @@ namespace TestProjectLeetCode._51_100
         public void TestCase02()
         {
             int[] arr = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+            int[] exp = { 0, 0, 1, 1, 2, 3, 3, 1, 1 };
+            var res = alg.RemoveDuplicates(arr);
+           
+            Assert.AreEqual(7, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] arr = { 1, 1, 1 };
 
             var res = alg.RemoveDuplicates(arr);
+            Assert.AreEqual(2, res);
 
-            Assert.AreEqual(7, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] arr = { 1, 1, 1, 2, 2, 2, 3, 3 };
+
+            var res = alg.RemoveDuplicates(arr);
+            Assert.AreEqual(6, res);
+         
+
         }
     }
 }
--- leetCode/51-100/80_RemoveDuplicatesFromSortedArrayIIAlg.cs
+++ leetCode/51-100/80_RemoveDuplicatesFromSortedArrayIIAlg.cs
@@ -8,22 +8,43 @@ namespace leetCode._51_100
 {
     public class _80_RemoveDuplicatesFromSortedArrayIIAlg
     {
+        public int RemoveDuplicates2(int[] nums)
+        {
+            int n = nums.Length;
+            if (n <= 2)
+            {
+                return n;
+            }
+            int slow = 2, fast = 2;
+            while (fast < n)
+            {
+                if (nums[slow - 2] != nums[fast])
+                {
+                    nums[slow] = nums[fast];
+                    ++slow;
+                }
+                ++fast;
+            }
+            return slow;
+        }
+
         public int RemoveDuplicates(int[] nums)
         {
-
             int i = 0;
             int len = nums.Length;
+            int count = 0;
+            int index = 0;
             while (i < len)
             {
-
                 int j = i + 1;
-                int count = 1;
-
+                count = 1;
+                index = j;
                 while (j < len)
                 {
                     if (nums[i] == nums[j])
                     {
                         count++;
+                        index = j;
                     }
                     else
                     {
@@ -34,30 +55,39 @@ namespace leetCode._51_100
                         }
                         else
                         {
-                            int begin = j - (count - 2);
-                            Swap(nums, begin, j, len);
-                            len = len - (count - 2);
-                            i = j - 1;
+                            index = j;
                             break;
                         }
-
                     }
                     j++;
                 }
-                i++;
+
+                if (count > 2)
+                {
+                    int begin = index - (count - 2);
+                    int end = len - (count - 2);
+                    Swap(nums, begin, index, end, len);
+                    len = len - (count - 2);
+                    i = begin;
+                }
+                else
+                {
+                    i++;
+                }
+
+              
             }
+
             return len;
         }
 
-        private void Swap(int[] nums, int begin, int nextIndex, int len)
+        private void Swap(int[] nums, int begin, int nextIndex, int end, int len)
         {
-            int end = nums.Length - 2;
             for (int i = begin, j = nextIndex; i < end && nextIndex < len; i++, j++)
             {
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-
             }
         }
     }
