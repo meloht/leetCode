51dbff0d6249591af7bc44ed0789d140b205af79
hongtao_luo <luohongtao621@hotmail.com>
2023-12-01 10:16:42 +0800

    83

--- leetCode/51-100/83_RemoveDuplicatesFromSortedListAlg.cs
+++ leetCode/51-100/83_RemoveDuplicatesFromSortedListAlg.cs
@@ -10,7 +10,22 @@ namespace leetCode._51_100
     {
         public ListNode DeleteDuplicates(ListNode head)
         {
-            return null;
+            ListNode nodeHead = head;
+            ListNode node1 = nodeHead;
+            while (node1 != null)
+            {
+                ListNode node2 = node1.next;
+                while (node2 != null && node2.val == node1.val)
+                {
+                    node2 = node2.next;
+                }
+                node1.next = node2;
+                node1 = node1.next;
+
+            }
+            return nodeHead;
         }
+
+
     }
 }
