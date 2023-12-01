d0c0ce099cea1213e27d7ce4e4eb6f47ab076a3f
hongtao_luo <luohongtao621@hotmail.com>
2023-12-01 17:29:30 +0800

    87

---
+++ TestProjectLeetCode/51-100/87_ScrambleStringUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode;
+using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    public class _87_ScrambleStringUnitTest
+    {
+        _87_ScrambleStringAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _87_ScrambleStringAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s1 = "great", s2 = "rgeat";
+            var res = alg.IsScramble(s1, s2);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s1 = "abcde", s2 = "caebd";
+            var res = alg.IsScramble(s1, s2);
+            Assert.AreEqual(false, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s1 = "a", s2 = "a";
+            var res = alg.IsScramble(s1, s2);
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ leetCode/51-100/87_ScrambleStringAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _87_ScrambleStringAlg
+    {
+        public bool IsScramble(string s1, string s2)
+        {
+            return false;
+        }
+    }
+}
