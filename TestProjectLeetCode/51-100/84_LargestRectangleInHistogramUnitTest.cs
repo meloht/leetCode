d6d1f6f2daf4dfd5cccba7ee115503eebe29db59
hongtao_luo <luohongtao621@hotmail.com>
2023-12-01 10:42:57 +0800

    84

---
+++ TestProjectLeetCode/51-100/84_LargestRectangleInHistogramUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _84_LargestRectangleInHistogramUnitTest
+    {
+        _84_LargestRectangleInHistogramAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _84_LargestRectangleInHistogramAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = { 2, 1, 5, 6, 2, 3 };
+            int res=alg.LargestRectangleArea(arr);
+            Assert.AreEqual(10, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = { 2, 4 };
+            int res = alg.LargestRectangleArea(arr);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ leetCode/51-100/84_LargestRectangleInHistogramAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _84_LargestRectangleInHistogramAlg
+    {
+        public int LargestRectangleArea(int[] heights)
+        {
+            return 0;
+        }
+    }
+}
