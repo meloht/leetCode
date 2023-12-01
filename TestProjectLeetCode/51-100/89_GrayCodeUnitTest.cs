2fafa603cfde48300ad185e68e20a61e9a3eae19
hongtao_luo <luohongtao621@hotmail.com>
2023-12-01 17:46:28 +0800

    89

---
+++ TestProjectLeetCode/51-100/89_GrayCodeUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _89_GrayCodeUnitTest
+    {
+        _89_GrayCodeAlg alg;
+        [TestInitialize]
+        public void Init()
+        {
+            alg = new _89_GrayCodeAlg();
+        }
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] exp = { 0, 1, 3, 2 };
+            var res = alg.GrayCode(2);
+            bool bl = res.ToArray().SequenceEqual(exp);
+            Assert.IsTrue(bl);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] exp = { 0, 1 };
+            var res = alg.GrayCode(1);
+            bool bl = res.ToArray().SequenceEqual(exp);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/51-100/89_GrayCodeAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _89_GrayCodeAlg
+    {
+        public IList<int> GrayCode(int n)
+        {
+            return null;
+        }
+    }
+}
