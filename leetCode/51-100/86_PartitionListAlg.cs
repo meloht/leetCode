c26d19733e0f8169be303c0b8115bdd0acb7570a
hongtao luo <luohongtao621@163.com>
2023-12-02 12:34:41 +0800

    86

--- leetCode/51-100/86_PartitionListAlg.cs
+++ leetCode/51-100/86_PartitionListAlg.cs
@@ -10,7 +10,32 @@ namespace leetCode._51_100
     {
         public ListNode Partition(ListNode head, int x)
         {
-            return null;
+            if (head == null)
+                return null;
+
+            List<ListNode> lefts = new List<ListNode>();
+            List<ListNode> rights = new List<ListNode>();
+            ListNode node = head;
+            while (node != null)
+            {
+                if (node.val < x)
+                {
+                    lefts.Add(node);
+                }
+                else
+                {
+                    rights.Add(node);
+                }
+                node = node.next;
+            }
+            lefts.AddRange(rights);
+
+            for (int i = 1; i < lefts.Count; i++)
+            {
+                lefts[i - 1].next = lefts[i];
+            }
+            lefts[lefts.Count - 1].next = null;
+            return lefts[0];
         }
     }
 }
