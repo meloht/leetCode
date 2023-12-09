a122c36d47e3e897d49fb810a8c9a06e52c4fa61
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-09 20:05:23 +0800

    91

---
+++ TestProjectLeetCode/51-100/91_DecodeWaysUnitTest.cs
@@ -0,0 +1,86 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _91_DecodeWaysUnitTest
+    {
+        _91_DecodeWaysAlg alg = new _91_DecodeWaysAlg();
+       
+        [TestMethod]
+        public void TestCase01()
+        {
+            string str = "12";
+            var res = alg.NumDecodings(str);
+           
+            Assert.AreEqual(2,res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string str = "226";
+            var res = alg.NumDecodings(str);
+
+            Assert.AreEqual(3, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string str = "06";
+            var res = alg.NumDecodings(str);
+
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string str = "2101";
+            var res = alg.NumDecodings(str);
+
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string str = "1201234";
+            var res = alg.NumDecodings(str);
+
+            Assert.AreEqual(3, res);
+        }
+        [TestMethod]
+        public void TestCase06()
+        {
+            string str = "10011";
+            var res = alg.NumDecodings(str);
+
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestCase07()
+        {
+            string str = "2611055971756562";
+            var res = alg.NumDecodings(str);
+
+            Assert.AreEqual(4, res);
+        }
+
+        [TestMethod]
+        public void TestCase08()
+        {
+            string str = "111111111111111111111111111111111111111111111";
+            var res = alg.NumDecodings(str);
+
+          
+        }
+    }
+}
---
+++ leetCode/51-100/91_DecodeWaysAlg.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _91_DecodeWaysAlg
+    {
+        public int NumDecodings(string s)
+        {
+            if (s.StartsWith("0"))
+                return 0;
+
+            int total = 1;
+            if (s.Split("00").Length > 1)
+                return 0;
+            string[] arr = s.Split('0', StringSplitOptions.RemoveEmptyEntries);
+            if (arr.Length > 0 && s.EndsWith("0"))
+            {
+                string ss = arr[arr.Length - 1];
+                arr[arr.Length - 1] = ss.Substring(0, ss.Length - 1);
+            }
+            int len = arr.Length - 1;
+            for (int i = 0; i < len; i++)
+            {
+                string ss = arr[i];
+                arr[i] = ss.Substring(0, ss.Length - 1);
+            }
+
+            foreach (string str in arr)
+            {
+                int[] nums = new int[] { 0 };
+                NumWord(str, nums);
+                if (nums[0] > 0)
+                {
+                    total *= nums[0];
+                }
+
+            }
+            return total;
+        }
+        private void NumWord(string s, int[] nums)
+        {
+            if (s.Length == 1 || s.Length == 0)
+            {
+                nums[0]++;
+                return;
+            }
+            var curr = s[0];
+            string ss = s.Substring(1);
+
+            var next = s[1];
+            if ((curr <= '2' && next <= '6') || (curr == '1'))
+            {
+                string ss2 = s.Substring(2);
+                NumWord(ss2, nums);
+
+            }
+            NumWord(ss, nums);
+        }
+
+    }
+}
