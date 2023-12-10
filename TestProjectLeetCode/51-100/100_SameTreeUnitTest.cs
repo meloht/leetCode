ec124db8c2153843fc3a8bbdcf0c62eeccaa764b
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-10 18:06:30 +0800

    100

---
+++ TestProjectLeetCode/51-100/100_SameTreeUnitTest.cs
@@ -0,0 +1,81 @@
+﻿using leetCode;
+using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _100_SameTreeUnitTest
+    {
+
+        _100_SameTreeAlg alg = new _100_SameTreeAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] arr = { 1, 2, 3 };
+            int?[] arr2 = { 1, 2, 3 };
+
+            TreeNode root1 = Utils.BuildBinaryTree(arr);
+            TreeNode root2 = Utils.BuildBinaryTree(arr2);
+            bool bl = alg.IsSameTree(root1, root2);
+
+            Assert.IsTrue(bl);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] arr = { 1, 2 };
+            int?[] arr2 = { 1, null, 2 };
+
+            TreeNode root1 = Utils.BuildBinaryTree(arr);
+            TreeNode root2 = Utils.BuildBinaryTree(arr2);
+            bool bl = alg.IsSameTree(root1, root2);
+
+            Assert.AreEqual(bl, false);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] arr = { 1, 2, 1 };
+            int?[] arr2 = { 1, 1, 2 };
+
+            TreeNode root1 = Utils.BuildBinaryTree(arr);
+            TreeNode root2 = Utils.BuildBinaryTree(arr2);
+            bool bl = alg.IsSameTree(root1, root2);
+
+            Assert.AreEqual(bl, false);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int?[] arr = { 1, 1 };
+            int?[] arr2 = { 1, null, 2 };
+
+            TreeNode root1 = Utils.BuildBinaryTree(arr);
+            TreeNode root2 = Utils.BuildBinaryTree(arr2);
+            bool bl = alg.IsSameTree(root1, root2);
+
+            Assert.AreEqual(bl, false);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int?[] arr = { 10, 5, 15 };
+            int?[] arr2 = { 10, 5, null, null, 15 };
+
+            TreeNode root1 = Utils.BuildBinaryTree(arr);
+            TreeNode root2 = Utils.BuildBinaryTree(arr2);
+            bool bl = alg.IsSameTree(root1, root2);
+
+            Assert.AreEqual(bl, false);
+        }
+    }
+}
---
+++ leetCode/51-100/100_SameTreeAlg.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _100_SameTreeAlg
+    {
+        public bool IsSameTree1(TreeNode p, TreeNode q)
+        {
+            var a1 = PreTraversal(p);
+            var a2 = PreTraversal(q);
+            bool bl = a1.SequenceEqual(a2);
+            return bl;
+        }
+
+
+        private IList<int?> PreTraversal(TreeNode root)
+        {
+            List<int?> result = new List<int?>();
+            PreOrder(root, result);
+            return result;
+        }
+        private void PreOrder(TreeNode root, List<int?> result)
+        {
+            if (root != null)
+            {
+                result.Add(root.val);
+                if (root.left != null)
+                {
+                    PreOrder(root.left, result);
+                }
+                else
+                {
+                    result.Add(null);
+                }
+                if (root.right != null)
+                {
+                    PreOrder(root.right, result);
+                }
+                else
+                {
+                    result.Add(null);
+                }
+            }
+        }
+
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            if (IsSame(p, q) == false)
+                return false;
+          
+
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+        private bool IsSame(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            if (p == null || q == null)
+                return false;
+
+            if (q.val == p.val)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
+
