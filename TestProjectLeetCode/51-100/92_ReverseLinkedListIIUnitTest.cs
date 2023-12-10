f304dc06ed0ce0189c19d4b42c731ddb9d5c2377
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-10 13:37:54 +0800

    92

--- TestProjectLeetCode/51-100/92_ReverseLinkedListIIUnitTest.cs
+++ TestProjectLeetCode/51-100/92_ReverseLinkedListIIUnitTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using leetCode;
+using leetCode._51_100;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +11,43 @@ namespace TestProjectLeetCode._51_100
     [TestClass]
     public class _92_ReverseLinkedListIIUnitTest
     {
+        _92_ReverseLinkedListIIAlg alg = new _92_ReverseLinkedListIIAlg();
 
+        [TestMethod]
+        public void TestCase01()
+        {
+            ListNode node1 = Utils.BuildListNode(new int[] { 1, 2, 3, 4, 5 });
+            var res = alg.ReverseBetween(node1, 2, 4);
+            int[] arr = { 1, 4, 3, 2, 5 };
+
+            bool bl = Utils.ListNodeSame(res, arr);
+
+            Assert.AreEqual(true, bl);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            ListNode node1 = Utils.BuildListNode(new int[] { 5 });
+            var res = alg.ReverseBetween(node1, 1, 1);
+            int[] arr = { 5 };
+
+            bool bl = Utils.ListNodeSame(res, arr);
+
+            Assert.AreEqual(true, bl);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            ListNode node1 = Utils.BuildListNode(new int[] { 1, 2, 3 });
+            var res = alg.ReverseBetween(node1, 3, 3);
+            int[] arr = { 1, 2, 3 };
+
+            bool bl = Utils.ListNodeSame(res, arr);
+
+            Assert.AreEqual(true, bl);
+        }
     }
 }
--- leetCode/51-100/92_ReverseLinkedListIIAlg.cs
+++ leetCode/51-100/92_ReverseLinkedListIIAlg.cs
@@ -8,9 +8,110 @@ namespace leetCode._51_100
 {
     public class _92_ReverseLinkedListIIAlg
     {
-        public ListNode ReverseBetween(ListNode head, int left, int right)
+        public ListNode ReverseBetween2(ListNode head, int left, int right)
         {
-            return null;
+            ListNode prev = null;
+            ListNode curr = head;
+            ListNode leftNode = head;
+            ListNode rightNode = null;
+            int index = 1;
+            if (left > 1)
+            {
+                while (curr != null)
+                {
+                    if (index >= left - 1)
+                    {
+                        leftNode = curr;
+                        break;
+                    }
+                    curr = curr.next;
+                    index++;
+                }
+            }
+            else
+            {
+
+            }
+
+          
+            return head;
         }
+        private ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null)
+            {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
+        public ListNode ReverseBetween1(ListNode head, int left, int right)
+        {
+            if (head == null)
+                return null;
+            if (head != null && head.next == null)
+                return head;
+            Stack<ListNode> stack = new Stack<ListNode>();
+
+            ListNode node = head;
+            ListNode leftNode = null;
+            ListNode rightNode = null;
+            int index = 1;
+            while (node != null)
+            {
+                if (index >= left && index <= right)
+                {
+                    stack.Push(node);
+                }
+                if (index < left)
+                {
+                    leftNode = node;
+                }
+                if (index == right)
+                {
+                    rightNode = node.next;
+                    break;
+                }
+                node = node.next;
+                index++;
+            }
+            ListNode header = stack.Pop();
+            node = header;
+            if (stack.Count == 0)
+            {
+                header.next = rightNode;
+            }
+            while (stack.Count > 0)
+            {
+                var temp = stack.Pop();
+                if (temp != null)
+                {
+                    node.next = temp;
+                    node = temp;
+                }
+                if (stack.Count == 0)
+                {
+                    temp.next = rightNode;
+                }
+            }
+
+
+            if (leftNode != null)
+            {
+                leftNode.next = header;
+            }
+            else
+            {
+                head = header;
+            }
+            return head;
+
+        }
+
+       
     }
 }
