56c31a1cfdf4778c56b57b52ed43416125d1ed11
hongtao_luo <luohongtao621@hotmail.com>
2023-12-12 16:18:31 +0800

    98

---
+++ TestProjectLeetCode/51-100/98_ValidateBinarySearchTreeUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _98_ValidateBinarySearchTreeUnitTest
+    {
+        _98_ValidateBinarySearchTreeAlg alg = new _98_ValidateBinarySearchTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] arr = { 2, 1, 3 };
+            var tree = Utils.BuildBinaryTree(arr);
+            var bl= alg.IsValidBST(tree);
+            Assert.IsTrue(bl);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] arr = { 5, 1, 4, null, null, 3, 6 };
+            var tree = Utils.BuildBinaryTree(arr);
+            var bl = alg.IsValidBST(tree);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/51-100/98_ValidateBinarySearchTreeAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _98_ValidateBinarySearchTreeAlg
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return false;
+        }
+    }
+}
