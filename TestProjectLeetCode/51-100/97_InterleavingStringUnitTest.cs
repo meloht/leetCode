081ffbaa0868cc5025d39d3654a84adfe4431d48
hongtao_luo <luohongtao621@hotmail.com>
2023-12-12 11:30:22 +0800

    97

---
+++ TestProjectLeetCode/51-100/97_InterleavingStringUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._51_100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._51_100
+{
+    [TestClass]
+    public class _97_InterleavingStringUnitTest
+    {
+        _97_InterleavingStringAlg alg = new _97_InterleavingStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            bool res = alg.IsInterleave("aabcc", "dbbca", "aadbbcbcac");
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            bool res = alg.IsInterleave("aabcc", "dbbca", "aadbbbaccc");
+            Assert.AreEqual(false, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            bool res = alg.IsInterleave("", "", "");
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ leetCode/51-100/97_InterleavingStringAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._51_100
+{
+    public class _97_InterleavingStringAlg
+    {
+        public bool IsInterleave(string s1, string s2, string s3)
+        {
+            return false;
+        }
+    }
+}
