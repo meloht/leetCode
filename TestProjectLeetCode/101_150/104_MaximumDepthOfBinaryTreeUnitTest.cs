3ea90d567300a6f444eaaceaed721e120b6192ec
hongtao_luo <luohongtao621@hotmail.com>
2023-12-13 16:21:54 +0800

    104

---
+++ TestProjectLeetCode/101_150/104_MaximumDepthOfBinaryTreeUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._101_150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._101_150
+{
+    [TestClass]
+    public class _104_MaximumDepthOfBinaryTreeUnitTest
+    {
+        _104_MaximumDepthOfBinaryTreeAlg alg = new _104_MaximumDepthOfBinaryTreeAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = { 3, 9, 20, null, null, 15, 7 };
+            var tree = Utils.BuildTree(root);
+            var res = alg.MaxDepth(tree);
+            Assert.AreEqual(3, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = { 1, null, 2 };
+            var tree = Utils.BuildTree(root);
+            var res = alg.MaxDepth(tree);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
---
+++ leetCode/101_150/104_MaximumDepthOfBinaryTreeAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._101_150
+{
+    public class _104_MaximumDepthOfBinaryTreeAlg
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            return 0;
+        }
+    }
+}
