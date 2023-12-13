d7af1a5bd0ba4df64fe3819acc98757514de36ce
hongtao_luo <luohongtao621@hotmail.com>
2023-12-13 13:51:46 +0800

    101

--- leetCode/101_150/101_SymmetricTreeAlg.cs
+++ leetCode/101_150/101_SymmetricTreeAlg.cs
@@ -8,9 +8,41 @@ namespace leetCode._101_150
 {
     public class _101_SymmetricTreeAlg
     {
+       
+        private List<int?> left=new List<int?>();
+        private List<int?> right=new List<int?>();    
+
         public bool IsSymmetric(TreeNode root)
         {
+            LeftTree(root.left);
+            RightTree(root.right);
+            if(left.SequenceEqual(right))
+                return true;
             return false;
+        }
+        private void LeftTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                left.Add(null);
+                return;
+            }
+           
+            left.Add(root.val);
+            LeftTree(root.left);
+            LeftTree(root.right);
+        }
+
+        private void RightTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                right.Add(null);
+                return;
+            }
+            right.Add(root.val);
+            RightTree(root.right);
+            RightTree(root.left);
         }
     }
 }
