dd2fba3e5cd5c3916d572885f7430c8448fca023
hongtao_luo <luohongtao621@hotmail.com>
2023-12-13 13:33:38 +0800

    101

---
+++ TestProjectLeetCode/101_150/101_SymmetricTreeUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._101_150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._101_150
+{
+    [TestClass]
+    public class _101_SymmetricTreeUnitTest
+    {
+        _101_SymmetricTreeAlg alg = new _101_SymmetricTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = { 1, 2, 2, 3, 4, 4, 3 };
+            var tree = Utils.BuildTree(root);
+            bool bl = alg.IsSymmetric(tree);
+            Assert.AreEqual(true, bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = { 1, 2, 2, null, 3, null, 3 };
+            var tree = Utils.BuildTree(root);
+            bool bl = alg.IsSymmetric(tree);
+            Assert.AreEqual(false, bl);
+        }
+    }
+}
---
+++ leetCode/101_150/101_SymmetricTreeAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._101_150
+{
+    public class _101_SymmetricTreeAlg
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            return false;
+        }
+    }
+}
