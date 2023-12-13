82bd66e331d4f2569e64476713a3c5f47be8cfa4
hongtao_luo <luohongtao621@hotmail.com>
2023-12-13 15:22:24 +0800

    103

---
+++ TestProjectLeetCode/101_150/103_BinaryTreeZigzagLevelOrderTraversalUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._101_150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._101_150
+{
+    [TestClass]
+    public class _103_BinaryTreeZigzagLevelOrderTraversalUnitTest
+    {
+        _103_BinaryTreeZigzagLevelOrderTraversalAlg alg = new _103_BinaryTreeZigzagLevelOrderTraversalAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = { 3, 9, 20, null, null, 15, 7 };
+            List<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { 3 });
+            exp.Add(new int[] { 20, 9 });
+            exp.Add(new int[] { 15, 7 });
+
+            var tree = Utils.BuildTree(root);
+            var res = alg.ZigzagLevelOrder(tree);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = { 1 };
+            List<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { 1 });
+
+            var tree = Utils.BuildTree(root);
+            var res = alg.ZigzagLevelOrder(tree);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = { };
+            List<IList<int>> exp = new List<IList<int>>();
+
+            var tree = Utils.BuildTree(root);
+            var res = alg.ZigzagLevelOrder(tree);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+    }
+}
---
+++ leetCode/101_150/103_BinaryTreeZigzagLevelOrderTraversalAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._101_150
+{
+    public class _103_BinaryTreeZigzagLevelOrderTraversalAlg
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            return null;
+        }
+    }
+}
