825c7133bb04dafb11518447f8b0c22862687927
hongtao_luo <luohongtao621@hotmail.com>
2023-12-13 14:02:19 +0800

    101

--- leetCode/101_150/101_SymmetricTreeAlg.cs
+++ leetCode/101_150/101_SymmetricTreeAlg.cs
@@ -8,15 +8,15 @@ namespace leetCode._101_150
 {
     public class _101_SymmetricTreeAlg
     {
-       
-        private List<int?> left=new List<int?>();
-        private List<int?> right=new List<int?>();    
 
-        public bool IsSymmetric(TreeNode root)
+        private List<int?> left = new List<int?>();
+        private List<int?> right = new List<int?>();
+
+        public bool IsSymmetric1(TreeNode root)
         {
             LeftTree(root.left);
             RightTree(root.right);
-            if(left.SequenceEqual(right))
+            if (left.SequenceEqual(right))
                 return true;
             return false;
         }
@@ -27,7 +27,7 @@ namespace leetCode._101_150
                 left.Add(null);
                 return;
             }
-           
+
             left.Add(root.val);
             LeftTree(root.left);
             LeftTree(root.right);
@@ -43,6 +43,38 @@ namespace leetCode._101_150
             right.Add(root.val);
             RightTree(root.right);
             RightTree(root.left);
+        }
+
+
+        public bool IsSymmetric(TreeNode root)
+        {
+            return IsSameTree(root.left,root.right);
+        }
+
+        private bool IsSameTree(TreeNode root1, TreeNode root2)
+        {
+            if (IsSameNode(root1, root2))
+            {
+                if (root1 != null && root2 != null)
+                {
+                    return IsSameTree(root1.left, root2.right) && IsSameTree(root1.right, root2.left);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsSameNode(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null && root2 == null)
+            {
+                return true;
+            }
+            if (root1 != null && root2 != null && root1.val == root2.val)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
