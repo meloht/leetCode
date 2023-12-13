f889ffe35c0951c533e051d7f877ed1568477421
hongtao_luo <luohongtao621@hotmail.com>
2023-12-13 14:50:04 +0800

    102

---
+++ TestProjectLeetCode/101_150/102_BinaryTreeLevelOrderTraversalUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using leetCode._101_150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._101_150
+{
+    [TestClass]
+    public class _102_BinaryTreeLevelOrderTraversalUnitTest
+    {
+        _102_BinaryTreeLevelOrderTraversalAlg alg = new _102_BinaryTreeLevelOrderTraversalAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = { 1, 2, 2, 3, 4, 4, 3 };
+            List<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { 3 });
+            exp.Add(new int[] { 9, 20 });
+            exp.Add(new int[] { 15, 7 });
+
+            var tree = Utils.BuildTree(root);
+            var res = alg.LevelOrder(tree);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = { 1 };
+            List<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { 1 });
+
+            var tree = Utils.BuildTree(root);
+            var res = alg.LevelOrder(tree);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = { };
+            List<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { });
+
+            var tree = Utils.BuildTree(root);
+            var res = alg.LevelOrder(tree);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+    }
+}
---
+++ leetCode/101_150/102_BinaryTreeLevelOrderTraversalAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._101_150
+{
+    public class _102_BinaryTreeLevelOrderTraversalAlg
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            return null;
+        }
+    }
+}
