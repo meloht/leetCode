56c45e36e31e4b81a8b2a4896151bd7e5332aba1
hongtao_luo <luohongtao621@hotmail.com>
2023-12-14 15:03:21 +0800

    110

--- leetCode/0101_0150/110_BalancedBinaryTreeAlg.cs
+++ leetCode/0101_0150/110_BalancedBinaryTreeAlg.cs
@@ -10,7 +10,32 @@ namespace leetCode._101_150
     {
         public bool IsBalanced(TreeNode root)
         {
-            return false;
+            var res = GetMax(root, 0);
+            return res.Item2;
+        }
+
+        private Tuple<int, bool> GetMax(TreeNode root, int num)
+        {
+            if (root == null)
+                return new Tuple<int, bool>(num, true);
+            num++;
+            var left = GetMax(root.left, num);
+
+            if (left.Item2 == false)
+            {
+                return left;
+            }
+            var right = GetMax(root.right, num);
+            if (right.Item2 == false)
+            {
+                return right;
+            }
+
+            int mm = Math.Max(left.Item1, right.Item1);
+
+            if (Math.Abs(left.Item1 - right.Item1) > 1)
+                return new Tuple<int, bool>(mm, false);
+            return new Tuple<int, bool>(mm, true);
         }
     }
 }
