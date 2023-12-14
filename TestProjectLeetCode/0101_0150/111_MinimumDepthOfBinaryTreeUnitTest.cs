ba39c07faaa827424a1edf15efaeb256c7890f8c
hongtao_luo <luohongtao621@hotmail.com>
2023-12-14 15:18:38 +0800

    111

---
+++ TestProjectLeetCode/0101_0150/111_MinimumDepthOfBinaryTreeUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._0101_0150
+{
+    [TestClass]
+    public class _111_MinimumDepthOfBinaryTreeUnitTest
+    {
+        _111_MinimumDepthOfBinaryTreeAlg alg = new _111_MinimumDepthOfBinaryTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [3, 9, 20, null, null, 15, 7];
+            var tree = Utils.BuildTree(root);
+            int num = alg.MinDepth(tree);
+            Assert.AreEqual(2, num);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [2, null, 3, null, 4, null, 5, null, 6];
+            var tree = Utils.BuildTree(root);
+            int num = alg.MinDepth(tree);
+            Assert.AreEqual(5, num);
+        }
+    }
+}
