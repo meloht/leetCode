f45f25cf00d5314723613c327ed78b9e892cb672
hongtao_luo <luohongtao621@hotmail.com>
2023-12-14 13:11:52 +0800

    108

---
+++ TestProjectLeetCode/101_150/108_ConvertSortedArrayToBinarySearchTreeUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._101_150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._101_150
+{
+    [TestClass]
+    public class _108_ConvertSortedArrayToBinarySearchTreeUnitTest
+    {
+        _108_ConvertSortedArrayToBinarySearchTreeAlg alg = new _108_ConvertSortedArrayToBinarySearchTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [-10, -3, 0, 5, 9];
+            var tree = alg.SortedArrayToBST(nums);
+            var res = Utils.GetTreeList(tree);
+            int?[] exp1 = [0, -3, 9, -10, null, 5];
+            int?[] exp2 = [0, -10, 5, null, -3, null, 9];
+            bool bl = exp1.SequenceEqual(res) || exp2.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 3];
+            var tree = alg.SortedArrayToBST(nums);
+            var res = Utils.GetTreeList(tree);
+            int?[] exp1 = [3, 1];
+            int?[] exp2 = [1, null, 3];
+            bool bl = exp1.SequenceEqual(res) || exp2.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/101_150/108_ConvertSortedArrayToBinarySearchTreeAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._101_150
+{
+    public class _108_ConvertSortedArrayToBinarySearchTreeAlg
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            return null;
+        }
+    }
+}
