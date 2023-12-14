ff807d74aeafe08fe135443f15e1794d08e0a770
hongtao_luo <luohongtao621@hotmail.com>
2023-12-14 12:59:14 +0800

    107

---
+++ TestProjectLeetCode/101_150/107_BinaryTreeLevelOrderTraversalIIUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using leetCode._101_150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._101_150
+{
+    [TestClass]
+    public class _107_BinaryTreeLevelOrderTraversalIIUnitTest
+    {
+        _107_BinaryTreeLevelOrderTraversalIIAlg alg = new _107_BinaryTreeLevelOrderTraversalIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = { 3, 9, 20, null, null, 15, 7 };
+            List<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { 15, 7 });
+            exp.Add(new int[] { 9, 20 });
+            exp.Add(new int[] { 3 });
+          
+            var tree = Utils.BuildTree(root);
+            var res = alg.LevelOrderBottom(tree);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = { 1 };
+            List<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { 1 });
+
+            var tree = Utils.BuildTree(root);
+            var res = alg.LevelOrderBottom(tree);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = { };
+            List<IList<int>> exp = new List<IList<int>>();
+
+            var tree = Utils.BuildTree(root);
+            var res = alg.LevelOrderBottom(tree);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+    }
+}
---
+++ leetCode/101_150/107_BinaryTreeLevelOrderTraversalIIAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._101_150
+{
+    public class _107_BinaryTreeLevelOrderTraversalIIAlg
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+            if (root == null)
+                return res;
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                List<int> ls = new List<int>();
+                int count = queue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+                    ls.Add(node.val);
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                res.Add(ls);
+            }
+            res.Reverse();
+
+            return res;
+        }
+    }
+}
