666d28bb7eb2fdd8659c00d22a122d7d4e4108bc
hongtao_luo <luohongtao621@hotmail.com>
2023-12-14 12:41:28 +0800

    106

---
+++ TestProjectLeetCode/101_150/106_ConstructBinaryTreeFromInorderAndPostorderTraversalUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using leetCode._101_150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._101_150
+{
+    [TestClass]
+    public class _106_ConstructBinaryTreeFromInorderAndPostorderTraversalUnitTest
+    {
+        _106_ConstructBinaryTreeFromInorderAndPostorderTraversalAlg alg = new _106_ConstructBinaryTreeFromInorderAndPostorderTraversalAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] inorder = [9, 3, 15, 20, 7];
+            int[] postorder = [9, 15, 7, 20, 3];
+
+            int?[] exp = [3, 9, 20, null, null, 15, 7];
+
+            var tree = alg.BuildTree(inorder, postorder);
+
+            var res = Utils.GetTreeList(tree);
+
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] inorder = [-1];
+            int[] postorder = [-1];
+
+            int?[] exp = [-1];
+
+            var tree = alg.BuildTree(inorder, postorder);
+
+            var res = Utils.GetTreeList(tree);
+
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/101_150/106_ConstructBinaryTreeFromInorderAndPostorderTraversalAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._101_150
+{
+    public class _106_ConstructBinaryTreeFromInorderAndPostorderTraversalAlg
+    {
+        private Dictionary<int, int> dict;
+        public TreeNode BuildTree(int[] inorder, int[] postorder)
+        {
+            int n = postorder.Length;
+            dict = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                dict.Add(inorder[i], i);
+            }
+            return BuildTree(postorder, inorder, 0, n - 1, 0, n - 1);
+        }
+
+        private TreeNode BuildTree(int[] postorder, int[] inorder, int postOrderLeft, int postOrderRight, int inorderLeft, int inorderRight)
+        {
+            if (postOrderLeft > postOrderRight)
+            {
+                return null;
+            }
+            int postOrderRoot = postOrderRight;
+            // 在中序遍历中定位根节点
+            int inorderRoot = dict[postorder[postOrderRoot]];
+
+            // 先把根节点建立出来
+            TreeNode root = new TreeNode(postorder[postOrderRoot]);
+
+            // 得到左子树中的节点数目
+            int size_left_subtree = inorderRoot - inorderLeft;
+
+            root.left = BuildTree(postorder, inorder, postOrderLeft, postOrderLeft + size_left_subtree - 1, inorderLeft, inorderRoot - 1);
+
+            root.right = BuildTree(postorder, inorder, postOrderLeft + size_left_subtree, postOrderRight - 1, inorderRoot + 1, inorderRight);
+            return root;
+        }
+    }
+}
