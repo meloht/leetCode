b0b1fd81bf177e7857e237677c3fa1d60fdfc7cd
hongtao_luo <luohongtao621@hotmail.com>
2023-12-14 14:29:24 +0800

    update weekly contest folder

--- TestProjectLeetCode/0101_0150/110_BalancedBinaryTreeUnitTest.cs
+++ TestProjectLeetCode/0101_0150/110_BalancedBinaryTreeUnitTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using leetCode._101_150;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace TestProjectLeetCode._101_150
 {
+    [TestClass]
     public class _110_BalancedBinaryTreeUnitTest
     {
+        _110_BalancedBinaryTreeAlg alg = new _110_BalancedBinaryTreeAlg();
     }
 }
