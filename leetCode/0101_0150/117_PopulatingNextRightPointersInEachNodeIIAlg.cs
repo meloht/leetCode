18d65ae3a0aab9f82e71b7c8e8d81199fb9538e3
hongtao_luo <luohongtao621@hotmail.com>
2023-12-15 18:01:10 +0800

    117

--- leetCode/0101_0150/117_PopulatingNextRightPointersInEachNodeIIAlg.cs
+++ leetCode/0101_0150/117_PopulatingNextRightPointersInEachNodeIIAlg.cs
@@ -11,6 +11,44 @@ namespace leetCode._0101_0150
         public Node Connect(Node root)
         {
             if (root == null)
+                return root;
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+                Node pre = null;
+                for (int i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    if (node.left != null && node.right != null)
+                    {
+                        if (pre != null)
+                        {
+                            pre.next = node.left;
+                        }
+                        node.left.next = node.right;
+                        queue.Enqueue(node.left);
+                        queue.Enqueue(node.right);
+                        pre = node.right;
+                    }
+                    else if (node.left == null && node.right != null)
+                    {
+
+                    }
+                    else if (node.left != null && node.right == null)
+                    {
+                        
+                    }
+                }
+            }
+            return root;
+        }
+        public Node Connect1(Node root)
+        {
+            if (root == null)
             {
                 return root;
             }
