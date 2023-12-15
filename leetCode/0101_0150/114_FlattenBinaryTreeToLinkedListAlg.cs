286e3e51c862e526c5a7e9e2705ab2d808cc9785
hongtao_luo <luohongtao621@hotmail.com>
2023-12-15 11:28:06 +0800

    114

--- leetCode/0101_0150/114_FlattenBinaryTreeToLinkedListAlg.cs
+++ leetCode/0101_0150/114_FlattenBinaryTreeToLinkedListAlg.cs
@@ -8,14 +8,14 @@ namespace leetCode._0101_0150
 {
     public class _114_FlattenBinaryTreeToLinkedListAlg
     {
-        public void Flatten(TreeNode root)
+        public void Flatten1(TreeNode root)
         {
             List<TreeNode> list = new List<TreeNode>();
             Dfs(root, list);
-            for (int i = 1; i < list.Count; i++) 
+            for (int i = 1; i < list.Count; i++)
             {
-                var pre = list[i-1];
-                var next= list[i];
+                var pre = list[i - 1];
+                var next = list[i];
                 pre.right = next;
                 pre.left = null;
                 next.left = null;
@@ -31,6 +31,19 @@ namespace leetCode._0101_0150
             list.Add(node);
             Dfs(node.left, list);
             Dfs(node.right, list);
+        }
+
+        private TreeNode pre = null;
+
+        public void Flatten(TreeNode root)
+        {
+            if (root == null)
+                return;
+            Flatten(root.right);
+            Flatten(root.left);
+            root.right = pre;
+            root.left = null;
+            pre = root;
         }
     }
 }
