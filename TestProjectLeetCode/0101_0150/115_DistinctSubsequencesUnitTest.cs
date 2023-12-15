1e1143152c2aa227a046475e063fddf5682e8400
hongtao_luo <luohongtao621@hotmail.com>
2023-12-15 11:34:21 +0800

    115

---
+++ TestProjectLeetCode/0101_0150/115_DistinctSubsequencesUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._0101_0150
+{
+    [TestClass]
+    public class _115_DistinctSubsequencesUnitTest
+    {
+        _115_DistinctSubsequencesAlg alg = new _115_DistinctSubsequencesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "rabbbit";
+            string t = "rabbit";
+            int num = alg.NumDistinct(s, t);
+
+            Assert.AreEqual(num, 3);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "babgbag";
+            string t = "bag";
+            int num = alg.NumDistinct(s, t);
+
+            Assert.AreEqual(num, 5);
+        }
+    }
+}
---
+++ leetCode/0101_0150/115_DistinctSubsequencesAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _115_DistinctSubsequencesAlg
+    {
+        public int NumDistinct(string s, string t)
+        {
+            return 0;
+        }
+    }
+}
