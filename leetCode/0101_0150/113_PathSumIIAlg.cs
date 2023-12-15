d2593856f027d508d5782ec4e7f75ea414f623e7
hongtao_luo <luohongtao621@hotmail.com>
2023-12-15 10:10:29 +0800

    113

--- leetCode/0101_0150/113_PathSumIIAlg.cs
+++ leetCode/0101_0150/113_PathSumIIAlg.cs
@@ -8,9 +8,41 @@ namespace leetCode._0101_0150
 {
     public class _113_PathSumIIAlg
     {
+        List<IList<int>> res = new List<IList<int>>();
+
         public IList<IList<int>> PathSum(TreeNode root, int targetSum)
         {
-            return new List<IList<int>>();
+            List<int> list = new List<int>();
+            Dfs(root, targetSum, list.ToArray());
+            return res;
+        }
+
+        private void Dfs(TreeNode root, int targetSum, int[] list)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            targetSum -= root.val;
+            var arr = list.ToList();
+            arr.Add(root.val);
+            if (root.right == null && root.left == null)
+            {
+                if (targetSum == 0)
+                {
+                    res.Add(arr.ToArray());
+                }
+                return;
+            }
+            if (root.left != null)
+            {
+                Dfs(root.left, targetSum, arr.ToArray());
+            }
+
+            if (root.right != null)
+            {
+                Dfs(root.right, targetSum, arr.ToArray());
+            }
         }
     }
 }
