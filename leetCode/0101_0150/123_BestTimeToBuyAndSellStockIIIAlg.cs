9a762d2e78f402227b5480ff471db1e3dfc0b539
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-16 21:10:54 +0800

    123

--- leetCode/0101_0150/123_BestTimeToBuyAndSellStockIIIAlg.cs
+++ leetCode/0101_0150/123_BestTimeToBuyAndSellStockIIIAlg.cs
@@ -165,5 +165,21 @@ namespace leetCode._0101_0150
             return dp;
         }
 
+        public int MaxProfit4(int[] prices)
+        {
+            int n = prices.Length;
+            int buy1 = -prices[0], sell1 = 0;
+            int buy2 = -prices[0], sell2 = 0;
+            for (int i = 1; i < n; ++i)
+            {
+                buy1 = Math.Max(buy1, -prices[i]);
+                sell1 = Math.Max(sell1, buy1 + prices[i]);
+                buy2 = Math.Max(buy2, sell1 - prices[i]);
+                sell2 = Math.Max(sell2, buy2 + prices[i]);
+            }
+            return sell2;
+        }
+
+
     }
 }
