853fdb23f16eb350090ad710e206b12db6675c78
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-16 13:16:54 +0800

    120

---
+++ TestProjectLeetCode/0101_0150/120_TriangleUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._0101_0150
+{
+    [TestClass]
+    public class _120_TriangleUnitTest
+    {
+        _120_TriangleAlg alg = new _120_TriangleAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] arr = { [2], [3, 4], [6, 5, 7], [4, 1, 8, 3] };
+            int res = alg.MinimumTotal(arr);
+            Assert.AreEqual(11, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] arr = { [-10] };
+            int res = alg.MinimumTotal(arr);
+            Assert.AreEqual(-10, res);
+        }
+    }
+}
---
+++ leetCode/0101_0150/120_TriangleAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _120_TriangleAlg
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+        
+            int[] dp = new int[triangle.Count];
+            dp[0] = triangle[0][0];
+            int[] dpTem = new int[triangle.Count];
+            IList<int> firstRow = triangle[0];
+            for (int i = 1; i < triangle.Count; i++)
+            {
+                var arr = triangle[i];
+               
+                for (int j = 0; j < dpTem.Length; j++)
+                {
+                    dpTem[j] = int.MaxValue;
+                }
+                for (int n = 0; n < firstRow.Count; n++)
+                {
+                    int end = n + 1;
+                    for (int j = n; j < arr.Count && j <= end; j++)
+                    {
+                        if (dpTem[j] == int.MaxValue)
+                        {
+                            dpTem[j] = arr[j] + dp[n];
+                        }
+                        else
+                        {
+                            dpTem[j] = Math.Min(arr[j] + dp[n], dpTem[j]);
+                        }
+                    }
+                }
+                for (int j = 0; j < dpTem.Length; j++)
+                {
+                    dp[j] = dpTem[j];
+                }
+                firstRow = arr;
+            }
+            int min = int.MaxValue;
+            for (int i = 0; i < dp.Length; i++)
+            {
+                min = Math.Min(dp[i], min);
+            }
+
+            return min;
+        }
+    }
+}
