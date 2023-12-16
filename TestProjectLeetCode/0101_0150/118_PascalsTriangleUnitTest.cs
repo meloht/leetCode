9ca042ee7c3c9f1e5d2b38d748d63b1088320d29
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-16 11:54:31 +0800

    118

---
+++ TestProjectLeetCode/0101_0150/118_PascalsTriangleUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._0101_0150
+{
+    [TestClass]
+    public class _118_PascalsTriangleUnitTest
+    {
+        _118_PascalsTriangleAlg alg = new _118_PascalsTriangleAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            List<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { 1 });
+            exp.Add(new int[] { 1, 1 });
+            exp.Add(new int[] { 1, 2, 1 });
+            exp.Add(new int[] { 1, 3, 3, 1 });
+            exp.Add(new int[] { 1, 4, 6, 4, 1 });
+
+            var res = alg.Generate(5);
+
+            bool bl=Utils.IsSameList(exp,res);
+            Assert.IsTrue(bl);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            List<IList<int>> exp = new List<IList<int>>();
+            exp.Add(new int[] { 1 });
+            var res = alg.Generate(1);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0101_0150/118_PascalsTriangleAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _118_PascalsTriangleAlg
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+            res.Add(new List<int>() { 1 });
+            if (numRows == 1)
+                return res;
+
+            res.Add(new List<int>() { 1, 1 });
+
+            if (numRows == 2)
+                return res;
+
+           
+            for (int i = 3; i <= numRows; i++)
+            {
+                List<int> list = new List<int>();
+                list.Add(1);
+                var arr = res[res.Count - 1];
+                for (int j = 1; j < arr.Count; j++)
+                {
+                    int num= arr[j-1] + arr[j];
+                    list.Add(num);
+                }
+                list.Add(1);
+
+                res.Add(list);
+            }
+
+            return res;
+        }
+    }
+}
