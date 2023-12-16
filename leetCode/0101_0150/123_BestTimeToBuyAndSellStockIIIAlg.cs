298a9d8c86cd1af6ea4a06abc7807ebae660af4c
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-16 21:03:54 +0800

    123

--- leetCode/0101_0150/123_BestTimeToBuyAndSellStockIIIAlg.cs
+++ leetCode/0101_0150/123_BestTimeToBuyAndSellStockIIIAlg.cs
@@ -30,11 +30,11 @@ namespace leetCode._0101_0150
             return max;
         }
 
-        public int MaxProfit(int[] prices)
+        public int MaxProfit2(int[] prices)
         {
             int[] dpLeft = new int[prices.Length];
             int[] dpRight = new int[prices.Length];
-            
+
             for (int i = 0; i < prices.Length; i++)
             {
                 for (int j = i + 1; j < prices.Length; j++)
@@ -52,5 +52,118 @@ namespace leetCode._0101_0150
 
             return max;
         }
+
+        public int MaxProfit(int[] prices)
+        {
+            List<int> lowList = GetMinList(prices);
+            List<int> peekList = GetPeekList(prices, lowList);
+
+            int max = 0;
+
+            var right = GetRightPeek(peekList, prices, lowList);
+            var left = GetLeftPeek(peekList, prices, lowList);
+            for (int i = 0; i < left.Length; i++)
+            {
+                int leftNum = left[i];
+                int rightNum = 0;
+                if (i + 1 < left.Length)
+                {
+                    rightNum = right[i + 1];
+                }
+                max = Math.Max(max, rightNum + leftNum);
+            }
+            return max;
+        }
+
+        private List<int> GetMinList(int[] prices)
+        {
+            List<int> lowList = new List<int>();
+
+            int index = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                int current = prices[i - 1];
+                int next = prices[i];
+                if (next > current)
+                {
+                    if (index != -1 || index == 0)
+                    {
+                        lowList.Add(index);
+
+                        index = -1;
+                    }
+                }
+                else if (next < current)
+                {
+                    index = i;
+                }
+            }
+            return lowList;
+        }
+
+        private List<int> GetPeekList(int[] prices, List<int> lowList)
+        {
+            List<int> peekList = new List<int>();
+            for (int i = 1; i <= lowList.Count; i++)
+            {
+                int begin = lowList[i - 1];
+                int end = 0;
+                if (i == lowList.Count)
+                {
+                    end = prices.Length;
+                }
+                else
+                {
+                    end = lowList[i];
+                }
+                int maxNum = 0;
+                int maxIndex = 0;
+
+                for (int j = begin; j < end; j++)
+                {
+                    if (prices[j] > maxNum)
+                    {
+                        maxNum = prices[j];
+                        maxIndex = j;
+                    }
+                }
+                peekList.Add(maxIndex);
+            }
+            return peekList;
+        }
+        private int[] GetRightPeek(List<int> peekList, int[] prices, List<int> lowList)
+        {
+            int[] dp = new int[lowList.Count];
+            if (lowList.Count == 0)
+                return dp;
+
+            dp[lowList.Count - 1] = prices[peekList[peekList.Count - 1]] - prices[lowList[lowList.Count - 1]];
+            int max = prices[peekList[peekList.Count - 1]];
+            for (int i = lowList.Count - 2; i >= 0; i--)
+            {
+                max = Math.Max(prices[peekList[i]], max);
+                dp[i] = Math.Max(dp[i + 1], max - prices[lowList[i]]);
+            }
+            return dp;
+        }
+
+        private int[] GetLeftPeek(List<int> peekList, int[] prices, List<int> lowList)
+        {
+            int[] dp = new int[lowList.Count];
+            if (lowList.Count == 0)
+                return dp;
+
+            dp[0] = prices[peekList[0]] - prices[lowList[0]];
+
+            int min = prices[lowList[0]];
+            for (int i = 1; i < lowList.Count; i++)
+            {
+                min = Math.Min(min, prices[lowList[i]]);
+                dp[i] = Math.Max(prices[peekList[i]] - min, dp[i - 1]);
+            }
+            return dp;
+        }
+
     }
 }
