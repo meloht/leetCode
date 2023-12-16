dd0ae3c9dbd48969eae22298a8aca2a1a73d5663
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-16 14:44:24 +0800

    122

---
+++ TestLeetCode/0101_0150/122_BestTimeToBuyAndSellStockIIUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _122_BestTimeToBuyAndSellStockIIUnitTest
+    {
+        _122_BestTimeToBuyAndSellStockIIAlg alg = new _122_BestTimeToBuyAndSellStockIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [7, 1, 5, 3, 6, 4];
+            int res = alg.MaxProfit(arr);
+            Assert.AreEqual(7, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [1, 2, 3, 4, 5];
+            int res = alg.MaxProfit(arr);
+            Assert.AreEqual(4, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] arr = [7, 6, 4, 3, 1];
+            int res = alg.MaxProfit(arr);
+            Assert.AreEqual(0, res);
+        }
+    }
+}
---
+++ leetCode/0101_0150/122_BestTimeToBuyAndSellStockIIAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _122_BestTimeToBuyAndSellStockIIAlg
+    {
+        public int MaxProfit(int[] prices)
+        {
+            return 0;
+        }
+    }
+}
