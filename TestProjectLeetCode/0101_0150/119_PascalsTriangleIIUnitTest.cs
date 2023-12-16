ceaa0c11b708e4f0bdd5f796ab28b3015b29199e
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-16 12:11:54 +0800

    119

---
+++ TestProjectLeetCode/0101_0150/119_PascalsTriangleIIUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLeetCode._0101_0150
+{
+    [TestClass]
+    public class _119_PascalsTriangleIIUnitTest
+    {
+        _119_PascalsTriangleIIAlg alg = new _119_PascalsTriangleIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] exp = { 1, 3, 3, 1 };
+            var res = alg.GetRow(3);
+            Assert.AreEqual(true, exp.SequenceEqual(res));
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] exp = { 1 };
+            var res = alg.GetRow(0);
+            Assert.AreEqual(true, exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0101_0150/119_PascalsTriangleIIAlg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _119_PascalsTriangleIIAlg
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            List<int> rowLast = new List<int>();
+           
+            for (int i = 0; i <= rowIndex; i++)
+            {
+                List<int> row = new List<int>();
+                if (i == 0)
+                {
+                    row.Add(1);
+                    rowLast = row;
+                }
+                else if (i == 1)
+                {
+                    row.Add(1);
+                    row.Add(1);
+                    rowLast = row;
+                }
+                else
+                {
+                    row.Add(1);
+                    for (int j = 1; j < rowLast.Count; j++)
+                    {
+                        int num = rowLast[j - 1] + rowLast[j];
+                        row.Add(num);
+                    }
+                    row.Add(1);
+                    rowLast = row;
+                }
+
+            }
+            return rowLast;
+        }
+    }
+}
