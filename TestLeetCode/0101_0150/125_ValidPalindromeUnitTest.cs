5f8296347b118dec7bfe55086e42780e8b727df5
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-17 13:04:54 +0800

    125

---
+++ TestLeetCode/0101_0150/125_ValidPalindromeUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _125_ValidPalindromeUnitTest
+    {
+        _125_ValidPalindromeAlg alg = new _125_ValidPalindromeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "A man, a plan, a canal: Panama";
+            bool bl = alg.IsPalindrome(s);
+            Assert.AreEqual(true, bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "race a car";
+            bool bl = alg.IsPalindrome(s);
+            Assert.AreEqual(false, bl);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = " ";
+            bool bl = alg.IsPalindrome(s);
+            Assert.AreEqual(true, bl);
+        }
+    }
+}
---
+++ leetCode/0101_0150/125_ValidPalindromeAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _125_ValidPalindromeAlg
+    {
+        public bool IsPalindrome(string s)
+        {
+
+            List<char> arr = new List<char>();
+            foreach (char c in s)
+            {
+                if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+                {
+                    arr.Add(c);
+                }
+                else if (c >= 'A' && c <= 'Z')
+                {
+                    arr.Add(Char.ToLower(c));
+                }
+
+            }
+            if (arr.Count <= 1)
+                return true;
+
+            int mid = arr.Count / 2;
+            if (arr.Count % 2 == 1)
+            {
+                mid++;
+            }
+            for (int i = 0, j = arr.Count - 1; i < mid; i++, j--)
+            {
+                if (arr[i] != arr[j])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
