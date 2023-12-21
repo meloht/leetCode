2834c2442ef467387225ffd2d8a85cf110f0048d
hongtao_luo <luohongtao621@hotmail.com>
2023-12-21 14:49:25 +0800

    131

---
+++ TestLeetCode/0101_0150/131_PalindromePartitioningUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _131_PalindromePartitioningUnitTest
+    {
+        _131_PalindromePartitioningAlg alg = new _131_PalindromePartitioningAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "aab";
+            string[][] exp = [["a", "a", "b"], ["aa", "b"]];
+            var res = alg.Partition(s);
+            bool bl = Utils.IsSame(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "a";
+            string[][] exp = [["a"]];
+            var res = alg.Partition(s);
+            bool bl = Utils.IsSame(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0101_0150/131_PalindromePartitioningAlg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _131_PalindromePartitioningAlg
+    {
+        public IList<IList<string>> Partition(string s)
+        {
+            List<IList<string>> res = new List<IList<string>>();
+
+            return res;
+        }
+    }
+}
