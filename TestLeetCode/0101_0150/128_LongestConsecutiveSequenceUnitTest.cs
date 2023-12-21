8b855058ec34bd88bb44e8402dcb7396a3b4323d
hongtao_luo <luohongtao621@hotmail.com>
2023-12-21 10:53:55 +0800

    128

---
+++ TestLeetCode/0101_0150/128_LongestConsecutiveSequenceUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _128_LongestConsecutiveSequenceUnitTest
+    {
+        _128_LongestConsecutiveSequenceAlg alg = new _128_LongestConsecutiveSequenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [100, 4, 200, 1, 3, 2];
+            var res = alg.LongestConsecutive(nums);
+            Assert.AreEqual(4, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
+            var res = alg.LongestConsecutive(nums);
+            Assert.AreEqual(9, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 2, 0, 1];
+            var res = alg.LongestConsecutive(nums);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ leetCode/0101_0150/128_LongestConsecutiveSequenceAlg.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _128_LongestConsecutiveSequenceAlg
+    {
+        public int LongestConsecutive1(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+            if (nums.Length < 2)
+                return 1;
+            Array.Sort(nums);
+            int max = 1;
+            int count = 1;
+            int len = nums.Length - 1;
+            for (int i = 0; i < len; i++)
+            {
+                int curr = nums[i];
+                int next = nums[i + 1];
+                if (curr == next)
+                    continue;
+                if (next - curr == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    max = Math.Max(max, count);
+                    count = 1;
+                }
+            }
+            max = Math.Max(max, count);
+            return max;
+        }
+        public int LongestConsecutive(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+            if (nums.Length < 2)
+                return 1;
+            HashSet<int> arr = new HashSet<int>();
+            int max = 1;
+            int count = 1;
+            int len = nums.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                arr.Add(nums[i]);
+
+            }
+
+            foreach (var item in arr)
+            {
+                if (arr.Contains(item - 1))
+                    continue;
+                count = 1;
+                int next = item + 1;
+                while (arr.Contains(next))
+                {
+                    count++;
+                    next++;
+                }
+                max = Math.Max(max, count);
+
+            }
+
+            return max;
+        }
+    }
+}
