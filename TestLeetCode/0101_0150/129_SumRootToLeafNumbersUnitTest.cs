c2b82325a8227d6fdb19ed076c64aa089a246f34
hongtao_luo <luohongtao621@hotmail.com>
2023-12-21 11:37:57 +0800

    129

---
+++ TestLeetCode/0101_0150/129_SumRootToLeafNumbersUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _129_SumRootToLeafNumbersUnitTest
+    {
+        _129_SumRootToLeafNumbersAlg alg = new _129_SumRootToLeafNumbersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 2, 3];
+            var tree=Utils.BuildTree(root);
+            int res=alg.SumNumbers(tree);
+            Assert.AreEqual(25, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [4, 9, 0, 5, 1];
+            var tree = Utils.BuildTree(root);
+            int res = alg.SumNumbers(tree);
+            Assert.AreEqual(1026, res);
+        }
+    }
+}
---
+++ leetCode/0101_0150/129_SumRootToLeafNumbersAlg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _129_SumRootToLeafNumbersAlg
+    {
+        private int total = 0;
+        public int SumNumbers(TreeNode root)
+        {
+            Reduce(root, 0);
+            return total;
+        }
+
+       
+
+        private void Reduce(TreeNode node, int num)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            if (node.left == null && node.right == null)
+            {
+                int n = num + node.val;
+                total += n;
+
+            }
+            else
+            {
+                int n = (num + node.val) * 10;
+                Reduce(node.left, n);
+                Reduce(node.right, n);
+            }
+        }
+
+        private void Dfs(TreeNode node, List<int> nums)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            nums.Add(node.val);
+            if (node.left == null && node.right == null)
+            {
+                string ss = string.Join("", nums);
+                int nn = int.Parse(ss);
+                total += nn;
+
+            }
+            else
+            {
+                Dfs(node.left, new List<int>(nums));
+                Dfs(node.right, new List<int>(nums));
+            }
+        }
+    }
+}
