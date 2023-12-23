6360a8ff3f38a3ae4b70f26bd46b0437347c9326
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-23 17:58:28 +0800

    136

---
+++ TestLeetCode/0101_0150/136_SingleNumberUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _136_SingleNumberUnitTest
+    {
+        _136_SingleNumberAlg alg = new _136_SingleNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 2, 1];
+            int res = alg.SingleNumber(nums);
+            Assert.AreEqual(1, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [4, 1, 2, 1, 2];
+            int res = alg.SingleNumber(nums);
+            Assert.AreEqual(4, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1];
+            int res = alg.SingleNumber(nums);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ leetCode/0101_0150/136_SingleNumberAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _136_SingleNumberAlg
+    {
+        public int SingleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int len = nums.Length - 1;
+            for (int i = 0; i < len; i += 2)
+            {
+                if (nums[i] != nums[i + 1])
+                {
+                    return nums[i];
+                }
+            }
+            
+            return nums[len];
+        }
+    }
+}
