3fa6fab0818a5b783e2e6b39e7fe01d9444eb1c7
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-23 18:36:48 +0800

    136

--- leetCode/0101_0150/136_SingleNumberAlg.cs
+++ leetCode/0101_0150/136_SingleNumberAlg.cs
@@ -8,7 +8,7 @@ namespace leetCode._0101_0150
 {
     public class _136_SingleNumberAlg
     {
-        public int SingleNumber(int[] nums)
+        public int SingleNumber2(int[] nums)
         {
             Array.Sort(nums);
 
@@ -20,8 +20,38 @@ namespace leetCode._0101_0150
                     return nums[i];
                 }
             }
-            
+
             return nums[len];
         }
+
+        public int SingleNumber1(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!set.Add(nums[i]))
+                {
+                    set.Remove(nums[i]);
+                }
+            }
+            return set.First();
+        }
+
+        public int SingleNumber(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+
+            int n = 0;
+            foreach (int i in nums)
+            {
+                n = n ^ i;
+            }
+
+
+            return n;
+        }
+
+
     }
 }
