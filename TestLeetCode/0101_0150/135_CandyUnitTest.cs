f2d1251418b18dc8005bee06835864e1871b71f8
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-24 11:04:07 +0800

    135

---
+++ TestLeetCode/0101_0150/135_CandyUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _135_CandyUnitTest
+    {
+        _135_CandyAlg alg = new _135_CandyAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] ratings = [1, 0, 2];
+            int res=alg.Candy(ratings);
+            Assert.AreEqual(5, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] ratings = [1, 2, 2];
+            int res = alg.Candy(ratings);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ leetCode/0101_0150/135_CandyAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _135_CandyAlg
+    {
+        public int Candy(int[] ratings)
+        {
+            return 0;
+        }
+    }
+}
