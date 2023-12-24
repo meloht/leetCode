6deb0f99f3f6d7bbb9613831b99d598eea6cf3fc
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-24 19:42:55 +0800

    137

---
+++ TestLeetCode/0101_0150/137_SingleNumberIIUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _137_SingleNumberIIUnitTest
+    {
+        _137_SingleNumberIIAlg alg = new _137_SingleNumberIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 2, 3, 2];
+            int res = alg.SingleNumber(nums);
+            Assert.AreEqual(1, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0, 1, 0, 1, 0, 1, 99];
+            int res = alg.SingleNumber(nums);
+            Assert.AreEqual(99, res);
+        }
+    }
+}
---
+++ leetCode/0101_0150/137_SingleNumberIIAlg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _137_SingleNumberIIAlg
+    {
+        public int SingleNumber(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!set.Add(nums[i]))
+                {
+                    set.Remove(nums[i]);
+                }
+            }
+            return set.First();
+        }
+    }
+}
