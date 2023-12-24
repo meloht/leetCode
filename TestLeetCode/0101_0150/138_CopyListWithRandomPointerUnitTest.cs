210a05bdbaf3dd3bd49439e1228f9cc19b83e2e2
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-24 21:02:38 +0800

    138

---
+++ TestLeetCode/0101_0150/138_CopyListWithRandomPointerUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _138_CopyListWithRandomPointerUnitTest
+    {
+        _138_CopyListWithRandomPointerAlg alg = new _138_CopyListWithRandomPointerAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[][] head = [[7, null], [13, 0], [11, 4], [10, 2], [1, 0]];
+            var node = _138_CopyListWithRandomPointerAlg.Node.Build(head);
+            var ls = _138_CopyListWithRandomPointerAlg.Node.GetList(node);
+            int?[][] exp = [[7, null], [13, 0], [11, 4], [10, 2], [1, 0]];
+
+            bool bl = Utils.IsSameList(exp, ls);
+            Assert.IsTrue(bl);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[][] head = [[3, null], [3, 0], [3, null]];
+            var node = _138_CopyListWithRandomPointerAlg.Node.Build(head);
+            var ls = _138_CopyListWithRandomPointerAlg.Node.GetList(node);
+            int?[][] exp = [[3, null], [3, 0], [3, null]];
+
+            bool bl = Utils.IsSameList(exp, ls);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0101_0150/138_CopyListWithRandomPointerAlg.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._0101_0150
+{
+    public class _138_CopyListWithRandomPointerAlg
+    {
+        public Node CopyRandomList(Node head)
+        {
+            if (head == null)
+                return null;
+            Dictionary<Node, Node> dict = new Dictionary<Node, Node>();
+
+            Node node = head;
+            while (node != null)
+            {
+                Node node1 = new Node(node.val);
+                dict.Add(node, node1);
+                node = node.next;
+            }
+            foreach (var item in dict)
+            {
+                if (item.Key.random != null)
+                {
+                    item.Value.random = dict[item.Key.random];
+                }
+                if (item.Key.next != null)
+                {
+                    item.Value.next= dict[item.Key.next];
+                }
+               
+            }
+
+            return dict[head];
+        }
+        public class Node
+        {
+            public int val;
+            public Node next;
+            public Node random;
+
+            public Node(int _val)
+            {
+                val = _val;
+                next = null;
+                random = null;
+            }
+
+            public static Node Build(int?[][] arr)
+            {
+                if (arr.Length == 0)
+                    return null;
+                Dictionary<int, Node> dict = new Dictionary<int, Node>();
+                Node node = new Node(arr[0][0].Value);
+                dict.Add(0, node);
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    Node next = new Node(arr[i][0].Value);
+                    dict[i - 1].next = next;
+                    dict.Add(i, next);
+                }
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    var rn = arr[i][1];
+                    if (rn != null)
+                    {
+                        dict[i].random = dict[rn.Value];
+                    }
+                }
+                return node;
+
+            }
+
+            public static int?[][] GetList(Node head)
+            {
+                List<int?[]> list = new List<int?[]>();
+                Node node = head;
+                Dictionary<Node, int> dict = new Dictionary<Node, int>();
+                List<Node> listNode = new List<Node>();
+                int index = 0;
+                while (node != null)
+                {
+                    listNode.Add(node);
+                    dict.Add(node, index);
+                    node = node.next;
+                    index++;
+                }
+
+                foreach (var item in listNode)
+                {
+                    List<int?> ls = new List<int?>();
+                    ls.Add(item.val);
+                    if (item.random != null)
+                    {
+                        ls.Add(dict[item.random]);
+                    }
+                    else
+                    {
+                        ls.Add(null);
+                    }
+                    list.Add(ls.ToArray());
+                }
+
+
+                return list.ToArray();
+            }
+        }
+    }
+}
