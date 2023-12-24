6c7498c193fd6cbd592b3cdd2db74027b63cb9de
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-24 10:58:23 +0800

    134

--- leetCode/0101_0150/134_GasStationAlg.cs
+++ leetCode/0101_0150/134_GasStationAlg.cs
@@ -57,26 +57,6 @@ namespace leetCode._0101_0150
 
             return -1;
         }
-        private bool IsPass(int begin, int[] gas, int[] cost)
-        {
-            int index = begin;
-            int total = 0;
-            while (true)
-            {
-                total += gas[index];
-                total = total - cost[index];
-                if (total >= 0)
-                {
-                    index++;
-                    index = index % gas.Length;
-                    if (index == begin)
-                        return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
+      
     }
 }
