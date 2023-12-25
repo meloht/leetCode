3474570f79cf3143651038c288fec80a808a607f
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-25 17:31:43 +0800

    142

---
+++ TestLeetCode/0101_0150/142_LinkedlListCycleIIUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _142_LinkedlListCycleIIUnitTest
+    {
+        _142_LinkedlListCycleIIAlg alg = new _142_LinkedlListCycleIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [3, 2, 0, -4];
+            int pos = 1;
+            var node = Utils.BuildListNode(head, pos);
+            var bl = alg.DetectCycle(node);
+            Assert.AreEqual(2, bl.val);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [1, 2];
+            int pos = 0;
+            var node = Utils.BuildListNode(head, pos);
+            var bl = alg.DetectCycle(node);
+            Assert.AreEqual(1, bl.val);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] head = [1];
+            int pos = -1;
+            var node = Utils.BuildListNode(head, pos);
+            var bl = alg.DetectCycle(node);
+            Assert.AreEqual(null, bl);
+        }
+    }
+}
---
+++ leetCode/0101_0150/142_LinkedlListCycleIIAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _142_LinkedlListCycleIIAlg
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            return null;
+        }
+    }
+}
