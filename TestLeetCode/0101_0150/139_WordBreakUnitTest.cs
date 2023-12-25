f6863e58237762cf379c2fce49f877249897efda
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-25 11:05:42 +0800

    139

---
+++ TestLeetCode/0101_0150/139_WordBreakUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _139_WordBreakUnitTest
+    {
+        _139_WordBreakAlg alg = new _139_WordBreakAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string word = "leetcode";
+            string[] wordDict = ["leet", "code"];
+
+            bool bl = alg.WordBreak(word, wordDict);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string word = "applepenapple";
+            string[] wordDict = ["apple", "pen"];
+
+            bool bl = alg.WordBreak(word, wordDict);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string word = "catsandog";
+            string[] wordDict = ["cats", "dog", "sand", "and", "cat"];
+
+            bool bl = alg.WordBreak(word, wordDict);
+            Assert.IsFalse(bl);
+        }
+    }
+}
---
+++ leetCode/0101_0150/139_WordBreakAlg.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _139_WordBreakAlg
+    {
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            HashSet<string> wordSet = new HashSet<string>(wordDict);
+            int[,] dp = new int[s.Length, s.Length];
+         
+            if (wordSet.Contains(s))
+                return true;
+            bool bl = Dfs(s, 0, dp, wordSet);
+            return bl;
+        }
+        private bool Dfs(string s, int index, int[,] dp, HashSet<string> wordSet)
+        {
+            for (int i = index + 1; i < s.Length; i++)
+            {
+                bool bl1 = IsExist(s, index, i - 1, dp, wordSet);
+                bool bl2 = IsExist(s, i, s.Length - 1, dp, wordSet);
+                if (bl1 && bl2)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (bl1 && bl2 == false)
+                    {
+                        bool bl = Dfs(s, i, dp, wordSet);
+                        if (bl)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsExist(string s, int begin, int end, int[,] dp, HashSet<string> wordSet)
+        {
+            if (dp[begin, end] == 0)
+            {
+                string ss = s.Substring(begin, end - begin + 1);
+                if (wordSet.Contains(ss))
+                {
+                    dp[begin, end] = 1;
+                    return true;
+                }
+                else
+                {
+                    dp[begin, end] = -1;
+                    return false;
+                }
+            }
+            else if (dp[begin, end] == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
