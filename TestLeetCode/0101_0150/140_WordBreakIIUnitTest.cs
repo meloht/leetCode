c841ab69a73873b8c8d372a1a5087e7d67a5aeeb
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-25 15:14:10 +0800

    140

---
+++ TestLeetCode/0101_0150/140_WordBreakIIUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _140_WordBreakIIUnitTest
+    {
+        _140_WordBreakIIAlg alg = new _140_WordBreakIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string word = "catsanddog";
+            string[] wordDict = ["cat", "cats", "and", "sand", "dog"];
+
+            var res = alg.WordBreak(word, wordDict);
+            string[] exp = ["cats and dog", "cat sand dog"];
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string word = "catsandog";
+            string[] wordDict = ["cats", "dog", "sand", "and", "cat"];
+
+            var res = alg.WordBreak(word, wordDict);
+            string[] exp = [];
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0101_0150/140_WordBreakIIAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _140_WordBreakIIAlg
+    {
+        public IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+            return null;
+        }
+    }
+}
