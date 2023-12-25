d2877f68c2ed6261d933d74e2dbeedf0e651a614
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-25 19:05:15 +0800

    146

---
+++ TestLeetCode/0101_0150/146_LRUCacheUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0101_0150._146_LRUCacheAlg;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _146_LRUCacheUnitTest
+    {
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            _146_LRUCacheAlg.LRUCache alg = new LRUCache(2);
+            alg.Put(1, 1);
+            alg.Put(2, 2);
+            Assert.AreEqual(1, alg.Get(1));
+            alg.Put(3, 3);
+            Assert.AreEqual(2, alg.Get(2));
+            alg.Put(4, 4);
+            Assert.AreEqual(-1, alg.Get(1));
+            Assert.AreEqual(3, alg.Get(3));
+            Assert.AreEqual(4, alg.Get(4));
+        }
+    }
+}
---
+++ leetCode/0101_0150/146_LRUCacheAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _146_LRUCacheAlg
+    {
+        public class LRUCache
+        {
+
+            public LRUCache(int capacity)
+            {
+
+            }
+
+            public int Get(int key)
+            {
+                return 0;
+            }
+
+            public void Put(int key, int value)
+            {
+
+            }
+        }
+    }
+}
