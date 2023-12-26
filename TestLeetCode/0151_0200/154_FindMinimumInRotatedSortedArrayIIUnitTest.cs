f85d568e2706bf903bb50344cae4de2b45f28533
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-26 19:15:56 +0800

    154

---
+++ TestLeetCode/0151_0200/154_FindMinimumInRotatedSortedArrayIIUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _154_FindMinimumInRotatedSortedArrayIIUnitTest
+    {
+        _154_FindMinimumInRotatedSortedArrayIIAlg alg = new _154_FindMinimumInRotatedSortedArrayIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 5];
+            int res = alg.FindMin(nums);
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 2, 2, 0, 1];
+            int res = alg.FindMin(nums);
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 3, 3];
+            int res = alg.FindMin(nums);
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [3, 3, 1, 3];
+            int res = alg.FindMin(nums);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ leetCode/0151_0200/154_FindMinimumInRotatedSortedArrayIIAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _154_FindMinimumInRotatedSortedArrayIIAlg
+    {
+        public int FindMin1(int[] nums)
+        {
+            int min = int.MaxValue;
+            foreach (int i in nums)
+            {
+                min = Math.Min(min, i);
+            }
+            return min;
+        }
+
+        public int FindMin(int[] nums)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+            while (low < high)
+            {
+                int pivot = low + (high - low) / 2;
+                if (nums[pivot] < nums[high])
+                {
+                    high = pivot;
+                }
+                else if (nums[pivot] > nums[high])
+                {
+                    low = pivot + 1;
+                }
+                else
+                {
+                    high--;
+                }
+            }
+            return nums[low];
+        }
+    }
+}
