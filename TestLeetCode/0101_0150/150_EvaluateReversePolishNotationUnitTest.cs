95e47f921d09b7a0b7ed3d1f0332a1fa92b71498
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-26 15:32:57 +0800

    150

---
+++ TestLeetCode/0101_0150/150_EvaluateReversePolishNotationUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _150_EvaluateReversePolishNotationUnitTest
+    {
+        _150_EvaluateReversePolishNotationAlg alg = new _150_EvaluateReversePolishNotationAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] tokens = ["2", "1", "+", "3", "*"];
+            int res = alg.EvalRPN(tokens);
+            Assert.AreEqual(9, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] tokens = ["4", "13", "5", "/", "+"];
+            int res = alg.EvalRPN(tokens);
+            Assert.AreEqual(6, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] tokens = ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"];
+            int res = alg.EvalRPN(tokens);
+            Assert.AreEqual(22, res);
+        }
+    }
+}
---
+++ leetCode/0101_0150/150_EvaluateReversePolishNotationAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _150_EvaluateReversePolishNotationAlg
+    {
+        public int EvalRPN(string[] tokens)
+        {
+            return 0;
+        }
+    }
+}
