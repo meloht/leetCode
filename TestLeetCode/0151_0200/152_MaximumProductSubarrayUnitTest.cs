548b7d61daba5ed5339e26d053457f194e4f4798
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-26 16:26:29 +0800

    152

---
+++ TestLeetCode/0151_0200/152_MaximumProductSubarrayUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _152_MaximumProductSubarrayUnitTest
+    {
+        _152_MaximumProductSubarrayAlg alg = new _152_MaximumProductSubarrayAlg();
+        
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 3, -2, 4];
+            int res = alg.MaxProduct(nums);
+            Assert.AreEqual(6, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [-2, 0, -1];
+            int res = alg.MaxProduct(nums);
+            Assert.AreEqual(0, res);
+        }
+    }
+}
---
+++ leetCode/0151_0200/152_MaximumProductSubarrayAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _152_MaximumProductSubarrayAlg
+    {
+        public int MaxProduct(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
