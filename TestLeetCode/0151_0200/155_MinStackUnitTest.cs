2321c644a5904c72e791ab7b79451a2c63d29d98
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-26 19:36:12 +0800

    155

---
+++ TestLeetCode/0151_0200/155_MinStackUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0151_0200._155_MinStackAlg;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _155_MinStackUnitTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            _155_MinStackAlg.MinStack alg = new MinStack();
+            alg.Push(-2);
+            alg.Push(0);
+            alg.Push(-3);
+            Assert.AreEqual(-3, alg.GetMin());
+            alg.Pop();
+            Assert.AreEqual(0, alg.Top());
+            Assert.AreEqual(-2, alg.GetMin());
+        }
+    }
+}
---
+++ leetCode/0151_0200/155_MinStackAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _155_MinStackAlg
+    {
+        public class MinStack
+        {
+            Stack<int> stack;
+            public MinStack()
+            {
+                stack = new Stack<int>();
+            }
+
+            public void Push(int val)
+            {
+                stack.Push(val);
+            }
+
+            public void Pop()
+            {
+                stack.Pop();
+            }
+
+            public int Top()
+            {
+                return stack.Peek();
+            }
+
+            public int GetMin()
+            {
+                return 0;
+            }
+        }
+
+    }
+}
