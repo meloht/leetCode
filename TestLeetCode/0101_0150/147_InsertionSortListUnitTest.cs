08536afa49915fcd2af365e24630a5c9846842f8
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-26 10:37:17 +0800

    147

---
+++ TestLeetCode/0101_0150/147_InsertionSortListUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _147_InsertionSortListUnitTest
+    {
+        _147_InsertionSortListAlg alg = new _147_InsertionSortListAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [4, 2, 1, 3];
+            var list = Utils.BuildListNode(head);
+            int[] res = Utils.GetLinkNodeList(list);
+            int[] exp = [1, 2, 3, 4];
+
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [-1, 5, 3, 4, 0];
+            var list = Utils.BuildListNode(head);
+            int[] res = Utils.GetLinkNodeList(list);
+            int[] exp = [-1, 0, 3, 4, 5];
+
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
