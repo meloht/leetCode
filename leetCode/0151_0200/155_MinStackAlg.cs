1f906cf7e8fbf25620f101589d0b7b53a0926423
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-26 20:02:54 +0800

    155

--- leetCode/0151_0200/155_MinStackAlg.cs
+++ leetCode/0151_0200/155_MinStackAlg.cs
@@ -11,18 +11,28 @@ namespace leetCode._0151_0200
         public class MinStack
         {
             Stack<int> stack;
+            Stack<int> minStack;
             public MinStack()
             {
                 stack = new Stack<int>();
+                minStack = new Stack<int>();
             }
 
             public void Push(int val)
             {
                 stack.Push(val);
+                if (minStack.Count == 0 || minStack.Peek() >= val)
+                {
+                    minStack.Push(val);
+                }
             }
 
             public void Pop()
             {
+                if (minStack.Peek() == stack.Peek())
+                {
+                    minStack.Pop();
+                }
                 stack.Pop();
             }
 
@@ -33,7 +43,7 @@ namespace leetCode._0151_0200
 
             public int GetMin()
             {
-                return 0;
+                return minStack.Peek();
             }
         }
 
