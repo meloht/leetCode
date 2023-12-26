9e425ff26e9a2b5d09c1b77dc4b4728409f029c8
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-26 13:28:53 +0800

    148

---
+++ TestLeetCode/0101_0150/148_SortListUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0101_0150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0101_0150
+{
+    [TestClass]
+    public class _148_SortListUnitTest
+    {
+        _148_SortListAlg alg = new _148_SortListAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [4, 2, 1, 3];
+            var list = Utils.BuildListNode(head);
+            var node = alg.SortList(list);
+            int[] res = Utils.GetLinkNodeList(node);
+            int[] exp = [1, 2, 3, 4];
+
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [-1, 5, 3, 4, 0];
+            var list = Utils.BuildListNode(head);
+            var node = alg.SortList(list);
+            int[] res = Utils.GetLinkNodeList(node);
+            int[] exp = [-1, 0, 3, 4, 5];
+
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0101_0150/148_SortListAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0101_0150
+{
+    public class _148_SortListAlg
+    {
+        public ListNode SortList(ListNode head)
+        {
+            return null;
+        }
+    }
+}
