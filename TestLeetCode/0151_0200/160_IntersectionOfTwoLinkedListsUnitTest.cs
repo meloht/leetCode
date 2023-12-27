a391993f916f51110f9735ecece0844d7e416005
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-27 10:31:04 +0800

    160

---
+++ TestLeetCode/0151_0200/160_IntersectionOfTwoLinkedListsUnitTest.cs
@@ -0,0 +1,107 @@
+﻿using leetCode;
+using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _160_IntersectionOfTwoLinkedListsUnitTest
+    {
+
+        _160_IntersectionOfTwoLinkedListsAlg alg = new _160_IntersectionOfTwoLinkedListsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] a = [4, 1, 8, 4, 5];
+            int[] b = [5, 6, 1];
+
+            var lista = Utils.BuildListNode(a);
+            int intersectVal = 8;
+            ListNode intersectNode = null;
+            ListNode node = lista;
+            while (node != null)
+            {
+                if (node.val == intersectVal)
+                {
+                    intersectNode = node;
+                    break;
+                }
+                node = node.next;
+            }
+
+            var listb = Utils.BuildListNode(b);
+            node = listb;
+            ListNode last = null;
+            while (node != null)
+            {
+                if (node.next == null)
+                {
+                    last = node;
+                    break;
+                }
+                node = node.next;
+            }
+            last.next = intersectNode;
+
+            var res = alg.GetIntersectionNode(lista, listb);
+            Assert.AreEqual(res.val, intersectVal);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] a = [1, 9, 1, 2, 4];
+            int[] b = [3];
+
+            var lista = Utils.BuildListNode(a);
+            int intersectVal = 2;
+            ListNode intersectNode = null;
+            ListNode node = lista;
+            while (node != null)
+            {
+                if (node.val == intersectVal)
+                {
+                    intersectNode = node;
+                    break;
+                }
+                node = node.next;
+            }
+
+            var listb = Utils.BuildListNode(b);
+            node = listb;
+            ListNode last = null;
+            while (node != null)
+            {
+                if (node.next == null)
+                {
+                    last = node;
+                    break;
+                }
+                node = node.next;
+            }
+            last.next = intersectNode;
+
+            var res = alg.GetIntersectionNode(lista, listb);
+            Assert.AreEqual(res.val, intersectVal);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] a = [2, 6, 4];
+            int[] b = [1, 5];
+
+            var lista = Utils.BuildListNode(a);
+           
+            var listb = Utils.BuildListNode(b);
+
+            var res = alg.GetIntersectionNode(lista, listb);
+            Assert.AreEqual(res, null);
+        }
+    }
+}
---
+++ leetCode/0151_0200/160_IntersectionOfTwoLinkedListsAlg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _160_IntersectionOfTwoLinkedListsAlg
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+
+            ListNode nodeA= headA;
+            ListNode nodeB= headB;
+            HashSet<ListNode> listNodes = new HashSet<ListNode>();
+            while (nodeA != null || nodeB != null)
+            {
+                if (nodeA != null)
+                {
+                    if (listNodes.Contains(nodeA))
+                    {
+                        return nodeA;
+                    }
+                    else
+                    {
+                        listNodes.Add(nodeA);
+                    }
+                    nodeA = nodeA.next;
+                }
+                if (nodeB != null)
+                {
+                    if (listNodes.Contains(nodeB))
+                    {
+                        return nodeB;
+                    }
+                    else
+                    {
+                        listNodes.Add(nodeB);
+                    }
+                    nodeB = nodeB.next;
+                }
+            }
+            return null;
+        }
+    }
+}
