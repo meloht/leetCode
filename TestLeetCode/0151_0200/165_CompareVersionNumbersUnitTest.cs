6d6dd5a93adee775432befdeb530e82788b95e19
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-27 14:17:55 +0800

    165

---
+++ TestLeetCode/0151_0200/165_CompareVersionNumbersUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _165_CompareVersionNumbersUnitTest
+    {
+        _165_CompareVersionNumbersAlg alg = new _165_CompareVersionNumbersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string version1 = "1.01", version2 = "1.001";
+            int res = alg.CompareVersion(version1, version2);
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string version1 = "1.0", version2 = "1.0.0";
+            int res = alg.CompareVersion(version1, version2);
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string version1 = "0.1", version2 = "1.1";
+            int res = alg.CompareVersion(version1, version2);
+            Assert.AreEqual(-1, res);
+        }
+    }
+}
---
+++ leetCode/0151_0200/165_CompareVersionNumbersAlg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _165_CompareVersionNumbersAlg
+    {
+        public int CompareVersion(string version1, string version2)
+        {
+            int i = 0;
+
+            string[] arr1 = version1.Split('.', StringSplitOptions.RemoveEmptyEntries);
+            string[] arr2 = version2.Split('.', StringSplitOptions.RemoveEmptyEntries);
+            int len1 = arr1.Length;
+            int len2 = arr2.Length;
+            while (i < len1 || i < len2)
+            {
+                if (i < len1 && i < len2)
+                {
+                    int v1 = GetNum(arr1[i].TrimStart('0'));
+                    int v2 = GetNum(arr2[i].TrimStart('0'));
+                    if (v1 > v2)
+                        return 1;
+                    if (v1 < v2)
+                        return -1;
+                }
+                else if (i < len1)
+                {
+                    int v1 = GetNum(arr1[i].TrimStart('0'));
+                    if (v1 > 0)
+                        return 1;
+                }
+                else if (i < len2)
+                {
+                    int v1 = GetNum(arr2[i].TrimStart('0'));
+                    if (v1 > 0)
+                        return -1;
+                }
+
+                i++;
+            }
+            return 0;
+        }
+        private int GetNum(string s)
+        {
+            var res = s.TrimStart('0');
+            if (res.Length == 0)
+                return 0;
+            return int.Parse(res);
+        }
+    }
+}
