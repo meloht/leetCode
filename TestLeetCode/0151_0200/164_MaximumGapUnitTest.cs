7d090b2c82bc7918651aa0b58363d93f78f5197c
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-27 12:26:37 +0800

    164

---
+++ TestLeetCode/0151_0200/164_MaximumGapUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _164_MaximumGapUnitTest
+    {
+        _164_MaximumGapAlg alg = new _164_MaximumGapAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 6, 9, 1];
+            int res = alg.MaximumGap(nums);
+            Assert.AreEqual(3, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [10];
+            int res = alg.MaximumGap(nums);
+            Assert.AreEqual(0, res);
+        }
+    }
+}
