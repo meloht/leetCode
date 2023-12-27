f6291dddbeaefa8974a3a2a9f24d0a119f392b26
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-27 21:06:09 +0800

    168

---
+++ TestLeetCode/0151_0200/168_ExcelSheetColumnTitleUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _168_ExcelSheetColumnTitleUnitTest
+    {
+        _168_ExcelSheetColumnTitleAlg alg = new _168_ExcelSheetColumnTitleAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int columnNumber = 1;
+            var res=alg.ConvertToTitle(columnNumber);
+
+            Assert.AreEqual("A", res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int columnNumber = 28;
+            var res = alg.ConvertToTitle(columnNumber);
+
+            Assert.AreEqual("AB", res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int columnNumber = 701;
+            var res = alg.ConvertToTitle(columnNumber);
+
+            Assert.AreEqual("ZY", res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int columnNumber = 2147483647;
+            var res = alg.ConvertToTitle(columnNumber);
+
+            Assert.AreEqual("FXSHRXW", res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int columnNumber = 52;
+            var res = alg.ConvertToTitle(columnNumber);
+
+            Assert.AreEqual("AZ", res);
+        }
+    }
+}
---
+++ leetCode/0151_0200/168_ExcelSheetColumnTitleAlg.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _168_ExcelSheetColumnTitleAlg
+    {
+        public string ConvertToTitle(int columnNumber)
+        {
+            if (columnNumber == 26)
+                return "Z";
+            char a = 'A';
+            string[] arr = new string[26];
+            for (int i = 0; i < 26; i++)
+            {
+                char ch = (char)(a + i);
+                if (i == 25)
+                {
+                    arr[0] = ch.ToString();
+                }
+                else
+                {
+                    arr[i + 1] = ch.ToString();
+                }
+
+            }
+            List<string> list = new List<string>();
+
+            if (columnNumber < 26)
+            {
+                return arr[columnNumber];
+            }
+            int rem = 0;
+            int n = columnNumber;
+            while (n >= 26)
+            {
+                rem = n % 26;
+                n = n / 26;
+                if (rem == 0)
+                {
+                    n--;
+                }
+                list.Add(arr[rem]);
+
+            }
+            if (n > 0)
+            {
+                list.Add(arr[n]);
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                stringBuilder.Append(list[i]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
