c1f4e36c2c7f6d8b405edd0a3197e7338b138820
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-27 12:34:52 +0800

    164

--- leetCode/0151_0200/164_MaximumGapAlg.cs
+++ leetCode/0151_0200/164_MaximumGapAlg.cs
@@ -10,7 +10,20 @@ namespace leetCode._0151_0200
     {
         public int MaximumGap(int[] nums)
         {
-            return 0;
+            if (nums.Length < 2)
+                return 0;
+            int max = 0;
+            Array.Sort(nums);
+            int len = nums.Length - 1;
+            for (int i = 0; i < len; i++)
+            {
+                int curr = nums[i];
+                int next = nums[i + 1];
+                max = Math.Max(max, next - curr);
+            }
+
+
+            return max;
         }
     }
 }
