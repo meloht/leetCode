fabcc0e518d796a6bd1eea3f8ddcdd7888ff904e
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-27 11:36:45 +0800

    160

--- leetCode/0151_0200/160_IntersectionOfTwoLinkedListsAlg.cs
+++ leetCode/0151_0200/160_IntersectionOfTwoLinkedListsAlg.cs
@@ -95,5 +95,21 @@ namespace leetCode._0151_0200
             }
             return num;
         }
+
+        public ListNode GetIntersectionNode3(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null)
+            {
+                return null;
+            }
+            ListNode pA = headA, pB = headB;
+            while (pA != pB)
+            {
+                pA = pA == null ? headB : pA.next;
+                pB = pB == null ? headA : pB.next;
+            }
+            return pA;
+        }
+
     }
 }
