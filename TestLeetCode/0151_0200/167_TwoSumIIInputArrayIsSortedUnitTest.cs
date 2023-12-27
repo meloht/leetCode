031008ccaccf05b17f9653023a97f22233617962
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-27 20:11:10 +0800

    167

---
+++ TestLeetCode/0151_0200/167_TwoSumIIInputArrayIsSortedUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _167_TwoSumIIInputArrayIsSortedUnitTest
+    {
+        _167_TwoSumIIInputArrayIsSortedAlg alg = new _167_TwoSumIIInputArrayIsSortedAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] numbers = [2, 7, 11, 15];
+            var res = alg.TwoSum(numbers, 9);
+            int[] exp = [1, 2];
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] numbers = [2, 3, 4];
+            var res = alg.TwoSum(numbers, 6);
+            int[] exp = [1, 3];
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] numbers = [-1, 0];
+            var res = alg.TwoSum(numbers, -1);
+            int[] exp = [1, 2];
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0151_0200/167_TwoSumIIInputArrayIsSortedAlg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _167_TwoSumIIInputArrayIsSortedAlg
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int len = numbers.Length - 1;
+            for (int i = 0; i <= len; i++)
+            {
+                int num1 = target - numbers[i];
+                int index = BinarySearch(i + 1, len, numbers, num1);
+                if (index != -1)
+                {
+                    int[] arr = { i + 1, index + 1 };
+                    return arr;
+                }
+            }
+            return null;
+        }
+
+        private int BinarySearch(int left, int right, int[] numbers, int target)
+        {
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (numbers[mid] == target)
+                {
+                    return mid;
+                }
+                else if (numbers[mid] > target)
+                {
+                    right = mid - 1;
+                   
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
