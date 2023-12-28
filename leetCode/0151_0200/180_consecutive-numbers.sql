0c24a7da66e752bd052dda0a0d8487fbe85ff103
hongtao luo <luohongtao621@163.com>
2023-12-28 17:36:51 +0800

    180

---
+++ leetCode/0151_0200/180_consecutive-numbers.sql
@@ -0,0 +1,27 @@
+﻿
+SELECT num AS ConsecutiveNums
+FROM (
+  SELECT *, 
+         @count := IF(@prev = num, @count + 1, 1) AS count, 
+         @prev := num
+  FROM Logs, (SELECT @count := 0, @prev := NULL) AS vars
+  ORDER BY id
+) AS tmp
+WHERE count >= 3
+GROUP BY num;
+
+
+SELECT
+    DISTINCT t.Num AS ConsecutiveNums
+FROM
+(
+SELECT 
+    CASE @pre WHEN Num THEN 
+        @cnt := @cnt + 1
+    ELSE @cnt := 1 END AS cnt,
+    @pre := Num,
+    Num
+FROM
+    Logs a,
+    (SELECT @cnt := 1, @pre := '') b
+) t WHERE t.cnt >= 3
