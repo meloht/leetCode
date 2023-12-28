49dd1042c78487e319777a4cd09576d9bd5c3874
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-28 11:45:00 +0800

    172

---
+++ TestLeetCode/0151_0200/172_FactorialTrailingZeroesUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _172_FactorialTrailingZeroesUnitTest
+    {
+        _172_FactorialTrailingZeroesAlg alg = new _172_FactorialTrailingZeroesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3;
+            var res = alg.TrailingZeroes(n);
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 5;
+            var res = alg.TrailingZeroes(n);
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 0;
+            var res = alg.TrailingZeroes(n);
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int n = 30;
+            var res = alg.TrailingZeroes(n);
+            Assert.AreEqual(7, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int n = 625;
+            var res = alg.TrailingZeroes(n);
+            Assert.AreEqual(156, res);
+        }
+    }
+}
---
+++ leetCode/0151_0200/172_FactorialTrailingZeroesAlg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _172_FactorialTrailingZeroesAlg
+    {
+        public int TrailingZeroes(int n)
+        {
+            if (n < 5)
+                return 0;
+            int count = 0;
+
+            for (int i = 5; i <= n; i++)
+            {
+
+                if (i % 5 == 0)
+                {
+                    int m = i;
+                    while (m > 0)
+                    {
+                        if (m % 5 == 0)
+                        {
+                            count++;
+                            m = m / 5;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public int TrailingZeroes2(int n)
+        {
+            int ans = 0;
+            while (n != 0)
+            {
+                n /= 5;
+                ans += n;
+            }
+            return ans;
+        }
+
+
+      
+    }
+}
