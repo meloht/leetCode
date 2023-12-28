b247bbdb35ed9145717dfea28e16a67efefa1ace
hongtao luo <luohongtao621@163.com>
2023-12-28 16:27:21 +0800

    176

---
+++ leetCode/0151_0200/176_SecondHighestSalarySql.sql
@@ -0,0 +1,12 @@
+﻿
+ select max(salary) as SecondHighestSalary from employee where salary<(select max(salary) from employee );
+ 
+ select (select distinct salary from Employee order by salary desc limit 1,1) as SecondHighestSalary;
+ SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
+
