9ed14062f041293766f49dab2f2f1c8afc4d0b38
hongtao luo <hongtao_luo.bys@htc.com>
2023-12-28 10:24:07 +0800

    169

---
+++ TestLeetCode/0151_0200/169_MajorityElementUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _169_MajorityElementUnitTest
+    {
+        _169_MajorityElementAlg alg = new _169_MajorityElementAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 2, 3];
+            int res = alg.MajorityElement(nums);
+            Assert.AreEqual(3, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 2, 1, 1, 1, 2, 2];
+            int res = alg.MajorityElement(nums);
+            Assert.AreEqual(2, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1];
+            int res = alg.MajorityElement(nums);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ leetCode/0151_0200/169_MajorityElementAlg.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _169_MajorityElementAlg
+    {
+        public int MajorityElement2(int[] nums)
+        {
+            int n = nums.Length / 2;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            foreach (int i in nums)
+            {
+                if (dict.ContainsKey(i))
+                {
+                    dict[i]++;
+
+                }
+                else
+                {
+                    dict.Add(i, 1);
+                }
+                if (dict[i] > n)
+                    return i;
+            }
+            return 0;
+        }
+
+        public int MajorityElement1(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+            int n = nums.Length / 2;
+            Array.Sort(nums);
+
+          
+            return nums[n];
+        }
+
+        public int MajorityElement(int[] nums)
+        {
+            int temp = 0;
+            int count = 0;
+            foreach (int i in nums) 
+            {
+                if (count == 0)
+                {
+                    temp = i;
+                }
+                if (temp == i)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+            
+            return temp;
+        }
+    }
+}
