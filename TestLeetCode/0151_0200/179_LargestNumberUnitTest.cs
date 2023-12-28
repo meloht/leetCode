9c1b09ed17e66f2d1364e132bcbc5b556867bd85
hongtao luo <luohongtao621@163.com>
2023-12-28 20:43:00 +0800

    179

---
+++ TestLeetCode/0151_0200/179_LargestNumberUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _179_LargestNumberUnitTest
+    {
+        _179_LargestNumberAlg alg = new _179_LargestNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [10, 2];
+            string exp = "210";
+            var res = alg.LargestNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [3, 30, 34, 5, 9];
+            string exp = "9534330";
+            var res = alg.LargestNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [432, 43243];
+            string exp = "43243432";
+            var res = alg.LargestNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [34323, 3432];
+            string exp = "343234323";
+            var res = alg.LargestNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] nums = [0, 0];
+            string exp = "0";
+            var res = alg.LargestNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0151_0200/179_LargestNumberAlg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _179_LargestNumberAlg
+    {
+        public string LargestNumber(int[] nums)
+        {
+            var list = nums.Select(x => x.ToString()).ToList();
+            list.Sort(StrComparison);
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var item in list)
+            {
+                stringBuilder.Append(item);
+            }
+            string ans = stringBuilder.ToString().TrimStart('0');
+            if (ans.Length == 0)
+                return "0";
+            return ans;
+        }
+        private int StrComparison(string a, string b)
+        {
+            int i = 0;
+            while (i < a.Length || i < b.Length)
+            {
+                if (i < a.Length && i < b.Length)
+                {
+                    if (a[i] > b[i])
+                        return -1;
+
+                    if (a[i] < b[i])
+                        return 1;
+                }
+                else if (i < a.Length)
+                {
+                    string aa = a.Substring(i);
+                    return StrComparison(aa, b);
+                }
+                else if (i < b.Length)
+                {
+                    string bb = b.Substring(i);
+                    return StrComparison(a, bb);
+                }
+                i++;
+            }
+            return 0;
+        }
+
+
+    }
+}
