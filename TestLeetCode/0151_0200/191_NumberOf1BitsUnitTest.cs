35fce60178041f7aaad2aa9518751d61e99e968d
hongtao luo <luohongtao621@163.com>
2023-12-29 18:03:44 +0800

    191

---
+++ TestLeetCode/0151_0200/191_NumberOf1BitsUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _191_NumberOf1BitsUnitTest
+    {
+        _191_NumberOf1BitsAlg alg = new _191_NumberOf1BitsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            uint n = Convert.ToUInt32("00000000000000000000000000001011", 2);
+            var res = alg.HammingWeight(n);
+            Assert.AreEqual(3, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            uint n = Convert.ToUInt32("00000000000000000000000010000000", 2);
+            var res = alg.HammingWeight(n);
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            uint n = Convert.ToUInt32("11111111111111111111111111111101", 2);
+            var res = alg.HammingWeight(n);
+            Assert.AreEqual(31, res);
+        }
+    }
+}
---
+++ leetCode/0151_0200/191_NumberOf1BitsAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _191_NumberOf1BitsAlg
+    {
+        public int HammingWeight1(uint n)
+        {
+            int count = 0;
+
+            for (; n > 0;)
+            {
+                if ((n & 1) == 1)
+                {
+                    count++;
+                }
+                n >>= 1;
+            }
+
+            return count;
+        }
+
+        public int HammingWeight(uint n)
+        {
+            int ret = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if ((n & (1 << i)) != 0)
+                {
+                    ret++;
+                }
+            }
+            return ret;
+
+        }
+    }
+}
