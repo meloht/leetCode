af8d51fb2ca9c4b9731bf3da2b49534bca8f75e1
hongtao luo <luohongtao621@163.com>
2023-12-29 18:52:57 +0800

    196

--- leetCode/0151_0200/196_delete-duplicate-emails.sql
+++ leetCode/0151_0200/196_delete-duplicate-emails.sql
@@ -5,7 +5,8 @@ DELETE p1 FROM Person p1,
 WHERE
     p1.Email = p2.Email AND p1.Id > p2.Id
 
-    delete from person where id not in (
+
+delete from person where id not in (
     select id from (
         select min(id) as id from person group by email
     ) as tmp
