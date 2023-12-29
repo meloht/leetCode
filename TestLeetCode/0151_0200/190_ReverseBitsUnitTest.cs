4c7d0480e5368f68158be907af90f1b816abc7d2
hongtao luo <luohongtao621@163.com>
2023-12-29 17:17:20 +0800

    190

---
+++ TestLeetCode/0151_0200/190_ReverseBitsUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _190_ReverseBitsUnitTest
+    {
+        _190_ReverseBitsAlg alg = new _190_ReverseBitsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            uint n = 43261596;
+            uint exp = 43261596;
+            var res = alg.reverseBits(n);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            uint n = 4294967293;
+            uint exp = 3221225471;
+            var res = alg.reverseBits(n);
+            Assert.AreEqual(exp, res);
+
+        }
+
+    }
+}
---
+++ leetCode/0151_0200/190_ReverseBitsAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _190_ReverseBitsAlg
+    {
+        public uint reverseBits(uint n)
+        {
+            return 0;
+        }
+    }
+}
