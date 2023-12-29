5e222eed71b066e269b0267f15c1212dbf17d2c0
hongtao luo <luohongtao621@163.com>
2023-12-29 10:18:44 +0800

    183

--- leetCode/0151_0200/183_customers-who-never-order.sql
+++ leetCode/0151_0200/183_customers-who-never-order.sql
@@ -1,3 +1,10 @@
 ﻿
 
 select  a.name as Customers from customers a where a.id not in(select customerId from orders)
+
+
+SELECT name AS 'Customers'
+FROM Customers
+LEFT JOIN Orders ON Customers.Id = Orders.CustomerId
+WHERE Orders.CustomerId IS NULL
+
