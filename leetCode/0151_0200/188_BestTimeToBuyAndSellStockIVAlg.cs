8f5325729058c25082121ac986dabc5e579dabe0
hongtao luo <luohongtao621@163.com>
2023-12-29 13:49:06 +0800

    188

--- leetCode/0151_0200/188_BestTimeToBuyAndSellStockIVAlg.cs
+++ leetCode/0151_0200/188_BestTimeToBuyAndSellStockIVAlg.cs
@@ -10,7 +10,33 @@ namespace leetCode._0151_0200
     {
         public int MaxProfit(int k, int[] prices)
         {
+            int len = prices.Length;
+            int[,] arr = new int[len, len];
+            Dictionary<int, int> dictMax = new Dictionary<int, int>();
+            for (int i = 0; i < len; i++)
+            {
+                int max = 0;
+                int index = 0;
+                for (int j = 1; j < len; j++)
+                {
+                    int nn = prices[j] - prices[i];
+                    if (nn > 0)
+                    {
+                        max = nn;
+                        index = j;
+                        arr[i, j] = nn;
+                    }
+                }
+                if (index > i)
+                {
+                    dictMax.Add(i, max);
+                }
+                arr[i, index] = max;
+
+            }
             return 0;
         }
+
+
     }
 }
