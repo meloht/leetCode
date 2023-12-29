b608332b4d30c3d3af66042d016a34904b28bb7a
hongtao luo <luohongtao621@163.com>
2023-12-29 12:18:32 +0800

    188

---
+++ TestLeetCode/0151_0200/188_BestTimeToBuyAndSellStockIVUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _188_BestTimeToBuyAndSellStockIVUnitTest
+    {
+        _188_BestTimeToBuyAndSellStockIVAlg alg = new _188_BestTimeToBuyAndSellStockIVAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int k = 2;
+            int[] prices = [2, 4, 1];
+            int exp = 2;
+            int res = alg.MaxProfit(k, prices);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int k = 2;
+            int[] prices = [3, 2, 6, 5, 0, 3];
+            int exp = 7;
+            int res = alg.MaxProfit(k, prices);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0151_0200/188_BestTimeToBuyAndSellStockIVAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _188_BestTimeToBuyAndSellStockIVAlg
+    {
+        public int MaxProfit(int k, int[] prices)
+        {
+            return 0;
+        }
+    }
+}
