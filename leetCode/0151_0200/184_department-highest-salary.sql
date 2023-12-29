fdba84352b6ed51ea66be1c3cdceeda0e588cd75
hongtao luo <luohongtao621@163.com>
2023-12-29 11:20:42 +0800

    184

--- leetCode/0151_0200/184_department-highest-salary.sql
+++ leetCode/0151_0200/184_department-highest-salary.sql
@@ -9,3 +9,27 @@ from
   ) AS 'rank'
 FROM
   Employee a ) aa inner join Department b on aa.departmentId=b.id where aa.rank=1
+
+
+ 
+ SELECT
+    Department.name AS 'Department',
+    Employee.name AS '
+
+Employee',
+    Salary
+FROM
+    Employee
+        JOIN
+    Department ON Employee.DepartmentId = Department.Id
+WHERE
+    (Employee.DepartmentId , Salary) IN
+    (   SELECT
+            DepartmentId, MAX(Salary)
+        FROM
+            Employee
+        GROUP BY DepartmentId
+    )
+;
+
+
