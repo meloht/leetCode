74dda91e2c2a92e0b426cf0b9b38b1ee95f56c8a
hongtao luo <luohongtao621@163.com>
2023-12-29 15:54:15 +0800

    188

--- leetCode/0151_0200/188_BestTimeToBuyAndSellStockIVAlg.cs
+++ leetCode/0151_0200/188_BestTimeToBuyAndSellStockIVAlg.cs
@@ -39,7 +39,7 @@ namespace leetCode._0151_0200
         }
 
 
-        public int MaxProfit(int k, int[] prices)
+        public int MaxProfit3(int k, int[] prices)
         {
             int n = prices.Length;
             int[,] f = new int[k + 2, 2];
@@ -60,6 +60,65 @@ namespace leetCode._0151_0200
             return f[k + 1, 0];
         }
 
+        public int MaxProfit(int k, int[] prices)
+        {
+            if (prices.Length == 0)
+                return 0;
+            int n = prices.Length;
+            int left = 1;
+            int right = prices.Max();
+            int ans = -1;
+            while (left <= right)
+            {
+                int c = (left + right) / 2;
+                int buyCount = 0, sellCount = 0;
+                int buy = -prices[0], sell = 0;
+
+                for (int i = 1; i < n; ++i)
+                {
+                    if (sell - prices[i] >= buy)
+                    {
+                        buy = sell - prices[i];
+                        buyCount = sellCount;
+                    }
+                    if (buy + prices[i] - c >= sell)
+                    {
+                        sell = buy + prices[i] - c;
+                        sellCount = buyCount + 1;
+                    }
+                }
+
+                // 如果交易次数大于等于 k，那么可以更新答案
+                // 这里即使交易次数严格大于 k，更新答案也没有关系，因为总能二分到等于 k 的
+                if (sellCount >= k)
+                {
+                    // 别忘了加上 kc
+                    ans = sell + k * c;
+                    left = c + 1;
+                }
+                else
+                {
+                    right = c - 1;
+                }
+
+
+            }
+
+            // 如果二分查找失败，说明交易次数的限制不是瓶颈
+            // 可以看作交易次数无限，直接使用贪心方法得到答案
+            if (ans == -1)
+            {
+                ans = 0;
+                for (int i = 1; i < n; ++i)
+                {
+                    ans += Math.Max(prices[i] - prices[i - 1], 0);
+                }
+            }
+
+
+
+            return ans;
+        }
 
 
     }
