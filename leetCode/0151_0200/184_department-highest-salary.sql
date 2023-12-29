36e8f8742154ae0b238f84874ea2fb172a9b1006
hongtao luo <luohongtao621@163.com>
2023-12-29 11:12:08 +0800

    184

---
+++ leetCode/0151_0200/184_department-highest-salary.sql
@@ -0,0 +1,11 @@
+﻿select aa.name as Employee, aa.salary, b.name as Department 
+from
+(SELECT
+  a.salary,a.departmentId,a.name,
+  DENSE_RANK() OVER (
+  partition by a.departmentId
+    ORDER BY
+      a.salary DESC
+  ) AS 'rank'
+FROM
+  Employee a ) aa inner join Department b on aa.departmentId=b.id where aa.rank=1
