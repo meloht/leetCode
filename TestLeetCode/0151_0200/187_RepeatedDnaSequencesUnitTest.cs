c0b99b4ab58de997abb540cab58a5dc3233323aa
hongtao luo <luohongtao621@163.com>
2023-12-29 12:07:36 +0800

    187

--- TestLeetCode/0151_0200/187_RepeatedDnaSequencesUnitTest.cs
+++ TestLeetCode/0151_0200/187_RepeatedDnaSequencesUnitTest.cs
@@ -34,5 +34,37 @@ namespace TestLeetCode._0151_0200
             bool bl = exp.SequenceEqual(res);
             Assert.IsTrue(bl);
         }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "AAAAAAAAAAA";
+            string[] exp = ["AAAAAAAAAA"];
+
+            var res = alg.FindRepeatedDnaSequences(s);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "AAAAAAAAAA";
+            string[] exp = [];
+
+            var res = alg.FindRepeatedDnaSequences(s);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+        [TestMethod]
+        public void TestCase05()
+        {
+            string s = "CAAAAAAAAAC";
+            string[] exp = [];
+
+            var res = alg.FindRepeatedDnaSequences(s);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
     }
 }
--- leetCode/0151_0200/187_RepeatedDnaSequencesAlg.cs
+++ leetCode/0151_0200/187_RepeatedDnaSequencesAlg.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,41 @@ namespace leetCode._0151_0200
     {
         public IList<string> FindRepeatedDnaSequences(string s)
         {
-            return null;
+            HashSet<string> res = new HashSet<string>();
+            if (s.Length <= 10)
+                return res.ToList();
+
+            HashSet<string> set = new HashSet<string>();
+           
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i < 10)
+                {
+                    sb.Append(s[i]);
+                    if (i == 9)
+                    {
+                        string ss = sb.ToString();
+                        set.Add(ss);
+                    }
+                }
+                else
+                {
+                    sb.Remove(0, 1);
+                    sb.Append(s[i]);
+                    string ss = sb.ToString();
+                    if (set.Contains(ss))
+                    {
+                        res.Add(ss);
+                    }
+                    else
+                    {
+                        set.Add(ss);
+                    }
+                }
+            }
+
+            return res.ToList();
         }
     }
 }
