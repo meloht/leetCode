13e77d675e8c6e8e0077115fdfed3f8edffb0eb9
hongtao luo <luohongtao621@163.com>
2023-12-29 16:21:49 +0800

    189

--- leetCode/0151_0200/189_RotateArrayAlg.cs
+++ leetCode/0151_0200/189_RotateArrayAlg.cs
@@ -10,7 +10,26 @@ namespace leetCode._0151_0200
     {
         public void Rotate(int[] nums, int k)
         {
+            int len = nums.Length;
+            k = k % len;
+            if (k == 0)
+                return;
+            List<int> list = new List<int>();
 
+            int n = len - k;
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(nums[i]);
+            }
+            for (int i = n, j = 0; i < len; i++, j++)
+            {
+                nums[j] = nums[i];
+            }
+
+            for (int i = k, j = 0; i < len; i++, j++)
+            {
+                nums[i] = list[j];
+            }
         }
     }
 }
