5b5c027fe66976becf0a52e8b6876740d36fc3c5
hongtao luo <luohongtao621@163.com>
2023-12-29 11:38:13 +0800

    187

---
+++ TestLeetCode/0151_0200/187_RepeatedDnaSequencesUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _187_RepeatedDnaSequencesUnitTest
+    {
+        _187_RepeatedDnaSequencesAlg alg = new _187_RepeatedDnaSequencesAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+            string[] exp = ["AAAAACCCCC", "CCCCCAAAAA"];
+
+            var res = alg.FindRepeatedDnaSequences(s);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "AAAAAAAAAAAAA";
+            string[] exp = ["AAAAAAAAAA"];
+
+            var res = alg.FindRepeatedDnaSequences(s);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0151_0200/187_RepeatedDnaSequencesAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _187_RepeatedDnaSequencesAlg
+    {
+        public IList<string> FindRepeatedDnaSequences(string s)
+        {
+            return null;
+        }
+    }
+}
