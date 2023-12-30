27567220fa0f6b9a5f3683dc46c0176512d80131
hongtao luo <luohongtao621@163.com>
2023-12-30 12:14:24 +0800

    198

---
+++ TestLeetCode/0151_0200/198_HouseRobberUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _198_HouseRobberUnitTest
+    {
+        _198_HouseRobberAlg alg = new _198_HouseRobberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 1];
+            int res = alg.Rob(nums);
+            Assert.AreEqual(4, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 7, 9, 3, 1];
+            int res = alg.Rob(nums);
+            Assert.AreEqual(12, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [0];
+            int res = alg.Rob(nums);
+            Assert.AreEqual(0, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [2, 1];
+            int res = alg.Rob(nums);
+            Assert.AreEqual(2, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] nums = [2, 1, 1, 2];
+            int res = alg.Rob(nums);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ leetCode/0151_0200/198_HouseRobberAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _198_HouseRobberAlg
+    {
+        public int Rob(int[] nums)
+        {
+
+            if (nums.Length < 2)
+            {
+                return nums[0];
+            }
+            if (nums.Length == 2)
+            {
+                return Math.Max(nums[0], nums[1]);
+            }
+            int[,] dp = new int[nums.Length, 2];
+            dp[0, 1] = nums[0];
+            dp[1, 1] = nums[1];
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                dp[i, 0] = Math.Max(dp[i - 2, 1], dp[i - 1, 1]);
+                dp[i, 1] = Math.Max(dp[i - 2, 1] + nums[i], dp[i - 1, 0] + nums[i]);
+
+            }
+
+            return Math.Max(dp[nums.Length - 1, 0], dp[nums.Length - 1, 1]);
+        }
+    }
+}
