c4650e4e2f4f4593ce6178b8669ebd7ae1f8224e
hongtao luo <luohongtao621@163.com>
2023-12-30 13:06:02 +0800

    200

---
+++ TestLeetCode/0151_0200/200_NumberOfIslandsUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _200_NumberOfIslandsUnitTest
+    {
+        _200_NumberOfIslandsAlg alg = new _200_NumberOfIslandsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[][] grid = [
+  ['1', '1', '1', '1', '0'],
+                ['1', '1', '0', '1', '0'],
+                ['1', '1', '0', '0', '0'],
+                ['0', '0', '0', '0', '0']
+];
+            int res=alg.NumIslands(grid);
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[][] grid = [
+  ['1', '1', '0', '0', '0'],
+                ['1', '1', '0', '0', '0'],
+                ['0', '0', '1', '0', '0'],
+                ['0', '0', '0', '1', '1']
+];
+            int res = alg.NumIslands(grid);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
