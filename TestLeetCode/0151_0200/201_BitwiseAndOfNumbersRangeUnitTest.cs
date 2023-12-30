a36b90e546a81ce804f8cfe79bd648f01c7b4e90
hongtao luo <luohongtao621@163.com>
2023-12-30 15:46:14 +0800

    201

---
+++ TestLeetCode/0151_0200/201_BitwiseAndOfNumbersRangeUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _201_BitwiseAndOfNumbersRangeUnitTest
+    {
+        _201_BitwiseAndOfNumbersRangeAlg alg = new _201_BitwiseAndOfNumbersRangeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int left = 5, right = 7;
+            int res = alg.RangeBitwiseAnd(left, right);
+            Assert.AreEqual(4, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int left = 0, right = 0;
+            int res = alg.RangeBitwiseAnd(left, right);
+            Assert.AreEqual(0, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int left = 1, right = 2147483647;
+            int res = alg.RangeBitwiseAnd(left, right);
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int left = 2147483646, right = 2147483647;
+            int res = alg.RangeBitwiseAnd(left, right);
+            Assert.AreEqual(2147483646, res);
+        }
+        [TestMethod]
+        public void TestCase05()
+        {
+            int left = 2147483647, right = 2147483647;
+            int res = alg.RangeBitwiseAnd(left, right);
+            Assert.AreEqual(2147483647, res);
+        }
+    }
+}
--- leetCode/0151_0200/201_BitwiseAndOfNumbersRangeAlg.cs
+++ leetCode/0151_0200/201_BitwiseAndOfNumbersRangeAlg.cs
@@ -8,6 +8,21 @@ namespace leetCode._0151_0200
 {
     public class _201_BitwiseAndOfNumbersRangeAlg
     {
+        public int RangeBitwiseAnd(int left, int right)
+        {
+            ulong res = (ulong)left;
+            ulong leftu = res;
+            ulong rightu = (ulong)right;
 
+            for (ulong i = leftu + 1; i <= rightu; i++)
+            {
+               
+                res = res & i;
+                if (res == 0)
+                    return (int)res;
+
+            }
+            return (int)res;
+        }
     }
 }
