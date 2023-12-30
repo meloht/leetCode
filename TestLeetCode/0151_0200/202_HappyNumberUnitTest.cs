fb2c90743b7e7a2c271c9a970e9944215eeb1c48
hongtao luo <luohongtao621@163.com>
2023-12-30 16:05:27 +0800

    202

---
+++ TestLeetCode/0151_0200/202_HappyNumberUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _202_HappyNumberUnitTest
+    {
+        _202_HappyNumberAlg alg = new _202_HappyNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            var res = alg.IsHappy(19);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            var res = alg.IsHappy(2);
+            Assert.AreEqual(false, res);
+        }
+
+    }
+}
---
+++ leetCode/0151_0200/202_HappyNumberAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _202_HappyNumberAlg
+    {
+        public bool IsHappy(int n)
+        {
+            return false;
+        }
+    }
+}
