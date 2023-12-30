7cd95bcadb5eeb3b3962a1aa38a72b6e4380d5c2
hongtao luo <luohongtao621@163.com>
2023-12-30 12:49:55 +0800

    199

---
+++ TestLeetCode/0151_0200/199_BinaryTreeRightSideViewUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using leetCode._0151_0200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0151_0200
+{
+    [TestClass]
+    public class _199_BinaryTreeRightSideViewUnitTest
+    {
+        _199_BinaryTreeRightSideViewAlg alg = new _199_BinaryTreeRightSideViewAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] nums = [1, 2, 3, null, 5, null, 4];
+            var tree = Utils.BuildTree(nums);
+            var res = alg.RightSideView(tree);
+            int[] exp = [1, 3, 4];
+            Assert.AreEqual(exp.SequenceEqual(res), true);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] nums = [1, null, 3];
+            var tree = Utils.BuildTree(nums);
+            var res = alg.RightSideView(tree);
+            int[] exp = [1, 3];
+            Assert.AreEqual(exp.SequenceEqual(res), true);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] nums = [];
+            var tree = Utils.BuildTree(nums);
+            var res = alg.RightSideView(tree);
+            int[] exp = [];
+            Assert.AreEqual(exp.SequenceEqual(res), true);
+        }
+
+
+    }
+}
---
+++ leetCode/0151_0200/199_BinaryTreeRightSideViewAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0151_0200
+{
+    public class _199_BinaryTreeRightSideViewAlg
+    {
+        int dep = 0;
+        public IList<int> RightSideView(TreeNode root)
+        {
+            var res = new List<int>();
+
+            if (root == null)
+                return res;
+
+            Dfs(root, res, 0);
+
+            return res;
+        }
+
+        private void Dfs(TreeNode root, List<int> res, int depth)
+        {
+            if (root == null)
+                return;
+            depth++;
+            if (depth > dep)
+            {
+                res.Add(root.val);
+                dep = depth;
+            }
+            Dfs(root.right, res, depth);
+            Dfs(root.left, res, depth);
+        }
+    }
+}
