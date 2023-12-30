ebf9bfb62b976a7d89fc1d6c17b8d26533264c4c
hongtao luo <luohongtao621@163.com>
2023-12-30 16:29:05 +0800

    202

--- leetCode/0151_0200/202_HappyNumberAlg.cs
+++ leetCode/0151_0200/202_HappyNumberAlg.cs
@@ -59,5 +59,31 @@ namespace leetCode._0151_0200
 
             return false;
         }
+
+        public int getNext(int n)
+        {
+            int totalSum = 0;
+            while (n > 0)
+            {
+                int d = n % 10;
+                n = n / 10;
+                totalSum += d * d;
+            }
+            return totalSum;
+        }
+
+        public bool IsHappy3(int n)
+        {
+            int slowRunner = n;
+            int fastRunner = getNext(n);
+            while (fastRunner != 1 && slowRunner != fastRunner)
+            {
+                slowRunner = getNext(slowRunner);
+                fastRunner = getNext(getNext(fastRunner));
+            }
+            return fastRunner == 1;
+        }
+
+ 
     }
 }
