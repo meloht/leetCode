5ee0ca3d6472dea35a5165a1bd093f37332fd015
hongtao_luo <luohongtao621@hotmail.com>
2024-01-02 17:23:16 +0800

    1137

---
+++ TestLeetCode/1101_1150/1137_NThTribonacciNumberUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._1101_1150
+{
+    [TestClass]
+    public class _1137_NThTribonacciNumberUnitTest
+    {
+        _1137_NThTribonacciNumberAlg alg = new _1137_NThTribonacciNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 4;
+            int res = alg.Tribonacci(n);
+            Assert.AreEqual(4, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 25;
+            int res = alg.Tribonacci(n);
+            Assert.AreEqual(1389537, res);
+        }
+    }
+}
---
+++ leetCode/1101_1150/1137_NThTribonacciNumberAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1137_NThTribonacciNumberAlg
+    {
+        public int Tribonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            if (n == 2)
+                return 1;
+
+            int t0 = 0;
+            int t1 = 0;
+            int t2 = 1;
+            int ans = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                t0 = t1;
+                t1 = t2;
+                t2 = ans;
+
+                ans = t0 + t1 + t2;
+
+            }
+
+            return ans;
+        }
+
+
+
+    }
+}
