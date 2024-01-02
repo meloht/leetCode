348e80b843165ff5bdb8231cfb3d93fb8cb2df45
hongtao_luo <luohongtao621@hotmail.com>
2024-01-02 16:43:02 +0800

    509

---
+++ TestLeetCode/0501_0550/509_FibonacciNumberUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0501_0550
+{
+    [TestClass]
+    public class _509_FibonacciNumberUnitTest
+    {
+        _509_FibonacciNumberAlg alg = new _509_FibonacciNumberAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 2;
+            int res = alg.Fib(n);
+
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 3;
+            int res = alg.Fib(n);
+
+            Assert.AreEqual(2, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 4;
+            int res = alg.Fib(n);
+
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/509_FibonacciNumberAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _509_FibonacciNumberAlg
+    {
+        public int Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            int f1 = 0;
+            int f2 = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                int tem2 = f2;
+                f2 = f1 + f2;
+                f1 = tem2;
+            }
+            return f2;
+        }
+    }
+}
