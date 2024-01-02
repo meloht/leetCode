c7ecf97a5a2ca50f228af2888ab7f4e2ca1eb7af
hongtao_luo <luohongtao621@hotmail.com>
2024-01-02 14:22:46 +0800

    210

---
+++ TestLeetCode/0201_0250/210_CourseScheduleIIUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _210_CourseScheduleIIUnitTest
+    {
+        _210_CourseScheduleIIAlg alg = new _210_CourseScheduleIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int numCourses = 2;
+            int[][] prerequisites = [[1, 0]];
+
+            var res = alg.FindOrder(numCourses, prerequisites);
+            int[] exp = [0, 1];
+            Assert.AreEqual(true, exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int numCourses = 4;
+            int[][] prerequisites = [[1, 0], [2, 0], [3, 1], [3, 2]];
+
+            var res = alg.FindOrder(numCourses, prerequisites);
+            int[] exp = [0, 2, 1, 3];
+            Assert.AreEqual(true, exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int numCourses = 1;
+            int[][] prerequisites = [];
+
+            var res = alg.FindOrder(numCourses, prerequisites);
+            int[] exp = [0];
+            Assert.AreEqual(true, exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0201-0250/210_CourseScheduleIIAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _210_CourseScheduleIIAlg
+    {
+        public int[] FindOrder(int numCourses, int[][] prerequisites)
+        {
+            return null;
+        }
+    }
+}
