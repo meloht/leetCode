6afbd131a743bc037bd36bd53083c206fc75d449
hongtao_luo <luohongtao621@hotmail.com>
2024-01-02 10:49:57 +0800

    205

---
+++ TestLeetCode/0201_0250/205_IsomorphicStringsUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _205_IsomorphicStringsUnitTest
+    {
+        _205_IsomorphicStringsAlg alg = new _205_IsomorphicStringsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "egg", t = "add";
+            var res = alg.IsIsomorphic(s, t);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "foo", t = "bar";
+            var res = alg.IsIsomorphic(s, t);
+            Assert.AreEqual(false, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "paper", t = "title";
+            var res = alg.IsIsomorphic(s, t);
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ leetCode/0201-0250/205_IsomorphicStringsAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _205_IsomorphicStringsAlg
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            return false;
+        }
+    }
+}
