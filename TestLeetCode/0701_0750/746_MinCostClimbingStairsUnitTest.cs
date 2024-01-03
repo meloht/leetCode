60f8bf8aa1a66df3af30be8af8e20ad6f8673b93
hongtao_luo <luohongtao621@hotmail.com>
2024-01-03 14:46:17 +0800

    746

---
+++ TestLeetCode/0701_0750/746_MinCostClimbingStairsUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0701_0750
+{
+    [TestClass]
+    public class _746_MinCostClimbingStairsUnitTest
+    {
+        _746_MinCostClimbingStairsAlg alg = new _746_MinCostClimbingStairsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] cost = [10, 15, 20];
+            int exp = 15;
+            int res = alg.MinCostClimbingStairs(cost);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
+            int exp = 6;
+            int res = alg.MinCostClimbingStairs(cost);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] cost = [0, 0, 1, 1];
+            int exp = 1;
+            int res = alg.MinCostClimbingStairs(cost);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0701_0750/746_MinCostClimbingStairsAlg.cs
@@ -0,0 +1,57 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _746_MinCostClimbingStairsAlg
+    {
+        public int MinCostClimbingStairs1(int[] cost)
+        {
+            int[] dp = new int[cost.Length + 1];
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+            for (int i = 2; i <= cost.Length; i++)
+            {
+                if (i < cost.Length)
+                {
+                    dp[i] = Math.Min(dp[i - 2], dp[i - 1]) + cost[i];
+                }
+                else
+                {
+                    dp[i] = Math.Min(dp[i - 2], dp[i - 1]);
+                }
+
+            }
+            return dp[cost.Length];
+        }
+
+        public int MinCostClimbingStairs2(int[] cost)
+        {
+            int[] dp = new int[cost.Length + 1];
+            dp[0] = 0;
+            dp[1] = 0;
+            for (int i = 2; i <= cost.Length; i++)
+            {
+                dp[i] = Math.Min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+
+            }
+            return dp[cost.Length];
+        }
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            int prev = 0;
+            int curr = 0;
+            for (int i = 2; i <= cost.Length; i++)
+            {
+                int next= Math.Min(prev + cost[i - 2], curr + cost[i - 1]);
+                prev = curr;
+                curr = next;
+
+            }
+            return curr;
+        }
+    }
+}
