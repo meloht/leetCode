a8323edba175f3ccffecb84eb7bbe3c9b8bca9f4
hongtao_luo <luohongtao621@hotmail.com>
2024-01-03 17:24:29 +0800

    740

--- TestLeetCode/0701_0750/740_DeleteAndEarnUnitTest.cs
+++ TestLeetCode/0701_0750/740_DeleteAndEarnUnitTest.cs
@@ -30,5 +30,23 @@ namespace TestLeetCode._0701_0750
             int res = alg.DeleteAndEarn(nums);
             Assert.AreEqual(exp, res);
         }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [8, 3, 4, 7, 6, 6, 9, 2, 5, 8, 2, 4, 9, 5, 9, 1, 5, 7, 1, 4];
+            int exp = 61;
+            int res = alg.DeleteAndEarn(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [3, 7, 10, 5, 2, 4, 8, 9, 9, 4, 9, 2, 6, 4, 6, 5, 4, 7, 6, 10];
+            int exp = 66;
+            int res = alg.DeleteAndEarn(nums);
+            Assert.AreEqual(exp, res);
+        }
     }
 }
--- leetCode/0701_0750/740_DeleteAndEarnAlg.cs
+++ leetCode/0701_0750/740_DeleteAndEarnAlg.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,73 @@ namespace leetCode._0701_0750
     {
         public int DeleteAndEarn(int[] nums)
         {
-            return 0;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            foreach (int item in nums)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item] += item;
+                }
+                else
+                {
+                    dict.Add(item, item);
+                }
+            }
+
+
+            int max = int.MinValue;
+            int maxNum = 0;
+            int num = 0;
+            int total = 0;
+            while (dict.Count > 0)
+            {
+                max = int.MinValue;
+
+                foreach (var item in dict.Keys)
+                {
+                    int tt = item * dict[item];
+                    int temp = tt;
+                    int m1 = item + 1;
+                    int m2 = item - 1;
+
+                    if (dict.ContainsKey(m1))
+                    {
+                        temp -= dict[m1] * m1;
+                    }
+                    if (dict.ContainsKey(m2))
+                    {
+                        temp -= dict[m2] * m2;
+                    }
+                    if (temp > max)
+                    {
+                        max = temp;
+                        maxNum = tt;
+                        num = item;
+                    }
+                }
+                total += maxNum;
+                int mm1 = num - 1;
+                int mm2 = num + 1;
+
+                RemoveNum(mm1, dict);
+                RemoveNum(mm2, dict);
+               
+                dict.Remove(num);
+
+            }
+
+
+            return total;
         }
+        private void RemoveNum(int num, Dictionary<int, int> dict)
+        {
+            if (dict.ContainsKey(num))
+            { 
+                dict.Remove(num);
+                System.Diagnostics.Debug.WriteLine(num);
+            }
+        }
+
     }
 }
