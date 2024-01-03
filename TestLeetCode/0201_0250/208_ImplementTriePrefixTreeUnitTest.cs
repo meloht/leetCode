49f6e4db200bdd5c57c7712f680803363101b037
hongtao_luo <luohongtao621@hotmail.com>
2024-01-03 11:55:40 +0800

    208

---
+++ TestLeetCode/0201_0250/208_ImplementTriePrefixTreeUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _208_ImplementTriePrefixTreeUnitTest
+    {
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            _208_ImplementTriePrefixTreeAlg.Trie trie = new _208_ImplementTriePrefixTreeAlg.Trie();
+            trie.Insert("apple");
+            Assert.IsTrue(trie.Search("apple"));   // 返回 True
+            Assert.IsFalse(trie.Search("app"));     // 返回 False
+            Assert.IsTrue(trie.StartsWith("app")); // 返回 True
+            trie.Insert("app");
+            Assert.IsTrue(trie.Search("app"));     // 返回 True
+        }
+    }
+}
---
+++ leetCode/0201-0250/208_ImplementTriePrefixTreeAlg.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _208_ImplementTriePrefixTreeAlg
+    {
+        public class Trie
+        {
+            TrieNode root;
+            public Trie()
+            {
+                root = new TrieNode();
+            }
+
+            public void Insert(string word)
+            {
+                TrieNode node = root;
+                if (word.Length == 0)
+                    return;
+
+                char[] ch = word.ToCharArray();
+                for (int i = 0; i < ch.Length; i++)
+                {
+                    int index = ch[i] - 'a';
+                    if (node.NextNode[index] == null)
+                    {
+                        node.NextNode[index] = new TrieNode();
+                    }
+                    node = node.NextNode[index];
+                    node.Prefix++;
+                }
+                node.Count++;
+
+            }
+
+            public bool Search(string word)
+            {
+                if (word.Length == 0)
+                    return false;
+                TrieNode node = root;
+                char[] ch = word.ToCharArray();
+                for (int i = 0; i < ch.Length; i++)
+                {
+                    int index = ch[i] - 'a';
+                    if (node.NextNode[index] == null)
+                        return false;
+                    node = node.NextNode[index];
+                }
+
+                if (node.Count > 0)
+                    return true;
+                return false;
+            }
+
+            public bool StartsWith(string prefix)
+            {
+                if (prefix.Length == 0)
+                    return false;
+
+                TrieNode node = root;
+                char[] ch = prefix.ToCharArray();
+                for (int i = 0; i < ch.Length; i++)
+                {
+                    int index = ch[i] - 'a';
+                    if (node.NextNode[index] == null)
+                        return false;
+                    node = node.NextNode[index];
+                }
+
+                return node.Prefix > 0;
+            }
+        }
+
+        public class TrieNode
+        {
+            public int Count;
+            public int Prefix;
+            public TrieNode[] NextNode = new TrieNode[26];
+
+            public override string ToString()
+            {
+                return $"{Count},{Prefix}";
+            }
+        }
+
+    }
+}
