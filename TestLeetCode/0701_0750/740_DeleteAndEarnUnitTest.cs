64a529eeccfe85fcc835d02b8ea3579c5c2205c7
hongtao_luo <luohongtao621@hotmail.com>
2024-01-03 14:52:03 +0800

    740

---
+++ TestLeetCode/0701_0750/740_DeleteAndEarnUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0701_0750
+{
+    [TestClass]
+    public class _740_DeleteAndEarnUnitTest
+    {
+        _740_DeleteAndEarnAlg alg = new _740_DeleteAndEarnAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 4, 2];
+            int exp = 6;
+            int res = alg.DeleteAndEarn(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 2, 3, 3, 3, 4];
+            int exp = 9;
+            int res = alg.DeleteAndEarn(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0701_0750/740_DeleteAndEarnAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _740_DeleteAndEarnAlg
+    {
+        public int DeleteAndEarn(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
