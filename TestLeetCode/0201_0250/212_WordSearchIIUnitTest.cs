86b8b70a0a06be4a20c2af98b9c4005748610de2
hongtao_luo <luohongtao621@hotmail.com>
2024-01-04 17:19:10 +0800

    212

---
+++ TestLeetCode/0201_0250/212_WordSearchIIUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _212_WordSearchIIUnitTest
+    {
+        _212_WordSearchIIAlg alg = new _212_WordSearchIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[][] board = [['o', 'a', 'a', 'n'], ['e', 't', 'a', 'e'], ['i', 'h', 'k', 'r'], ['i', 'f', 'l', 'v']];
+            string[] words = ["oath", "pea", "eat", "rain"];
+            string[] exp = ["eat", "oath"];
+            var res = alg.FindWords(board, words);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[][] board = [['a', 'b'], ['c', 'd']];
+            string[] words = ["abcb"];
+            string[] exp = [];
+            var res = alg.FindWords(board, words);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+    }
+}
---
+++ leetCode/0201-0250/212_WordSearchIIAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _212_WordSearchIIAlg
+    {
+        public IList<string> FindWords(char[][] board, string[] words)
+        {
+            return null;
+        }
+    }
+}
