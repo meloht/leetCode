0f66e0d28acd41e8a662a4e7db160d708b608d62
hongtao_luo <luohongtao621@hotmail.com>
2024-01-05 14:31:29 +0800

    1944

---
+++ TestLeetCode/1901_1950/1944_NumberOfVisiblePeopleInAQueueUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._1901_1950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._1901_1950
+{
+    [TestClass]
+    public class _1944_NumberOfVisiblePeopleInAQueueUnitTest
+    {
+        _1944_NumberOfVisiblePeopleInAQueueAlg alg = new _1944_NumberOfVisiblePeopleInAQueueAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] heights = [10, 6, 8, 5, 11, 9];
+            int[] exp = [3, 1, 2, 1, 1, 0];
+
+            var res = alg.CanSeePersonsCount(heights);
+            Assert.AreEqual(true, exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] heights = [5, 1, 2, 3, 10];
+            int[] exp = [4, 1, 1, 1, 0];
+
+            var res = alg.CanSeePersonsCount(heights);
+            Assert.AreEqual(true, exp.SequenceEqual(res));
+
+        }
+    }
+}
---
+++ leetCode/1901_1950/1944_NumberOfVisiblePeopleInAQueueAlg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1901_1950
+{
+    public class _1944_NumberOfVisiblePeopleInAQueueAlg
+    {
+        public int[] CanSeePersonsCount(int[] heights)
+        {
+            int[] res = new int[heights.Length];
+            for (int i = 0; i < heights.Length; i++)
+            {
+                int count = 0;
+                int max = 0;
+
+                for (int j = i + 1; j < heights.Length; j++)
+                {
+                    if (max == 0)
+                    {
+                        if (heights[j] < heights[i])
+                        {
+                            count++;
+                            max = heights[j];
+                        }
+                        else
+                        {
+                            count++;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (heights[j] > max && heights[j] < heights[i])
+                        {
+                            count++;
+                            max = heights[j];
+                        }
+                        else if (heights[j] > max && heights[j] >= heights[i])
+                        {
+                            count++;
+                            break;
+                        }
+                        else if (heights[j] <= max)
+                        {
+                            continue;
+                        }
+                    }
+
+                }
+                res[i] = count;
+            }
+
+            return res;
+        }
+    }
+}
