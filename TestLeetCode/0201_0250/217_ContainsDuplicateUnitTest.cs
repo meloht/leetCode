ba97b4c11bff7b73630914a57459e0cebaf0420c
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-07 17:15:05 +0800

    217

---
+++ TestLeetCode/0201_0250/217_ContainsDuplicateUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _217_ContainsDuplicateUnitTest
+    {
+        _217_ContainsDuplicateAlg alg = new _217_ContainsDuplicateAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 1];
+            bool res = alg.ContainsDuplicate(nums);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4];
+            bool res = alg.ContainsDuplicate(nums);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+            bool res = alg.ContainsDuplicate(nums);
+            Assert.IsTrue(res);
+        }
+    }
+}
---
+++ leetCode/0201-0250/217-ContainsDuplicateAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _217_ContainsDuplicateAlg
+    {
+        public bool ContainsDuplicate1(int[] nums)
+        {
+            HashSet<int> result = new HashSet<int>();
+            foreach (int num in nums)
+            {
+                if (result.Add(num) == false)
+                    return true; ;
+            }
+
+            return false;
+        }
+
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i - 1] == nums[i])
+                    return true;
+            }
+            return false;
+        }
+    }
+}
