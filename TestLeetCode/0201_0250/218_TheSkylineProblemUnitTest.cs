26a0eb2651e8f5403d7eba5ae82b4f8225799b99
hongtao_luo <luohongtao621@hotmail.com>
2024-01-08 13:36:45 +0800

    218

---
+++ TestLeetCode/0201_0250/218_TheSkylineProblemUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _218_TheSkylineProblemUnitTest
+    {
+        _218_TheSkylineProblemAlg alg = new _218_TheSkylineProblemAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] buildings = [[2, 9, 10], [3, 7, 15], [5, 12, 12], [15, 20, 10], [19, 24, 8]];
+
+            int[][] exp = [[2, 10], [3, 15], [7, 12], [12, 0], [15, 10], [20, 8], [24, 0]];
+
+            var res = alg.GetSkyline(buildings);
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] buildings = [[0, 2, 3], [2, 5, 3]];
+
+            int[][] exp = [[0, 3], [5, 0]];
+
+            var res = alg.GetSkyline(buildings);
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0201-0250/218_TheSkylineProblemAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _218_TheSkylineProblemAlg
+    {
+        public IList<IList<int>> GetSkyline(int[][] buildings)
+        {
+            return null;
+        }
+    }
+}
