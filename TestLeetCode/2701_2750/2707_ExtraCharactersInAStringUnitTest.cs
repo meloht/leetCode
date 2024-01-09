fd7fbb3e141c274a4ddfbb305938cf2db024d2f1
hongtao_luo <luohongtao621@hotmail.com>
2024-01-09 11:58:23 +0800

    2707

---
+++ TestLeetCode/2701_2750/2707_ExtraCharactersInAStringUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2701_2750
+{
+    [TestClass]
+    public class _2707_ExtraCharactersInAStringUnitTest
+    {
+        _2707_ExtraCharactersInAStringAlg alg = new _2707_ExtraCharactersInAStringAlg();
+       
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "leetscode";
+            string[] dictionary = ["leet", "code", "leetcode"];
+            int res = alg.MinExtraChar(s, dictionary);
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "sayhelloworld";
+            string[] dictionary = ["hello", "world"];
+            int res = alg.MinExtraChar(s, dictionary);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ leetCode/2701_2750/2707_ExtraCharactersInAStringAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2707_ExtraCharactersInAStringAlg
+    {
+        public int MinExtraChar(string s, string[] dictionary)
+        {
+            return 0;
+        }
+    }
+}
