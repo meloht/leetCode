54afba28f8d82f6f1bccb6cdaf8286d6b939e4c1
hongtao_luo <luohongtao621@hotmail.com>
2024-01-10 18:04:38 +0800

    1599

---
+++ TestLeetCode/1551_1600/1599_MaximumProfitOfOperatingACentennialWheelUnitTest.cs
@@ -0,0 +1,100 @@
+﻿using leetCode._1551_1600;
+using leetCode.WeeklyContest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._1551_1600
+{
+    [TestClass]
+    public class _1599_MaximumProfitOfOperatingACentennialWheelUnitTest
+    {
+        _1599_MaximumProfitOfOperatingACentennialWheelAlg alg = new _1599_MaximumProfitOfOperatingACentennialWheelAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] customers = [8, 3];
+            int boardingCost = 5;
+            int runningCost = 6;
+
+            int exp = 3;
+            int res = alg.MinOperationsMaxProfit(customers, boardingCost, runningCost);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] customers = [10, 9, 6];
+            int boardingCost = 6;
+            int runningCost = 4;
+
+            int exp = 7;
+            int res = alg.MinOperationsMaxProfit(customers, boardingCost, runningCost);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] customers = [3, 4, 0, 5, 1];
+            int boardingCost = 1;
+            int runningCost = 92;
+
+            int exp = -1;
+            int res = alg.MinOperationsMaxProfit(customers, boardingCost, runningCost);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] customers = [10, 10, 6, 4, 7];
+            int boardingCost = 3;
+            int runningCost = 8;
+
+            int exp = 9;
+            int res = alg.MinOperationsMaxProfit(customers, boardingCost, runningCost);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] customers = [2];
+            int boardingCost = 2;
+            int runningCost = 4;
+
+            int exp = -1;
+            int res = alg.MinOperationsMaxProfit(customers, boardingCost, runningCost);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase06()
+        {
+            int[] customers = [8, 3, 0, 0];
+            int boardingCost = 5;
+            int runningCost = 6;
+
+            int exp = 3;
+            int res = alg.MinOperationsMaxProfit(customers, boardingCost, runningCost);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase07()
+        {
+            int[] customers = [0, 43, 37, 9, 23, 35, 18, 7, 45, 3, 8, 24, 1, 6, 37, 2, 38, 15, 1, 14, 39, 27, 4, 25, 27, 33, 43, 8, 44, 30, 38, 40, 20, 5, 17, 27, 43, 11, 6, 2, 30, 49, 30, 25, 32, 3, 18, 23, 45, 43, 30, 14, 41, 17, 42, 42, 44, 38, 18, 26, 32, 48, 37, 5, 37, 21, 2, 9, 48, 48, 40, 45, 25, 30, 49, 41, 4, 48, 40, 29, 23, 17, 7, 5, 44, 23, 43, 9, 35, 26, 44, 3, 26, 16, 31, 11, 9, 4, 28, 49, 43, 39, 9, 39, 37, 7, 6, 7, 16, 1, 30, 2, 4, 
+                43, 23, 16, 39, 5, 30, 23, 39, 29, 31, 26, 35, 15, 5, 11, 45, 44, 45, 43, 4, 24, 40, 7, 36, 10, 10, 18, 6, 20, 13, 11, 20, 3, 32, 49, 34, 41, 13, 11, 3, 13, 0, 13, 44, 48, 43, 23, 12, 23, 2];
+            int boardingCost = 43;
+            int runningCost = 54;
+
+            int exp = 993;
+            int res = alg.MinOperationsMaxProfit(customers, boardingCost, runningCost);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1551_1600/1599_MaximumProfitOfOperatingACentennialWheelAlg.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1551_1600
+{
+    public class _1599_MaximumProfitOfOperatingACentennialWheelAlg
+    {
+        public int MinOperationsMaxProfit(int[] customers, int boardingCost, int runningCost)
+        {
+            int total = 0;
+            int cnum = 0;
+            int count = 0;
+
+            for (int i = 0; i < customers.Length; i++)
+            {
+                cnum += customers[i];
+
+                if (cnum >= 4)
+                {
+                    cnum -= 4;
+                    total = 4 * boardingCost + total;
+                    count++;
+                    total = total - runningCost;
+                }
+                else if (cnum > 0)
+                {
+                    total = cnum * boardingCost + total;
+                    cnum = 0;
+                    count++;
+                    total = total - runningCost;
+                }
+              
+            }
+            while (cnum > 0)
+            {
+                if (cnum >= 4)
+                {
+                    cnum -= 4;
+                    total = 4 * boardingCost + total;
+                    count++;
+                    total = total - runningCost;
+                }
+                else if (cnum > 0)
+                {
+                    int nn = cnum * boardingCost;
+                    if (nn <= runningCost)
+                    {
+                        break;
+                    }
+                    total = cnum * boardingCost + total;
+                    cnum = 0;
+                    count++;
+                    total = total - runningCost;
+                }
+               
+
+            }
+            if (total <= 0)
+                return -1;
+            return count;
+        }
+    }
+}
