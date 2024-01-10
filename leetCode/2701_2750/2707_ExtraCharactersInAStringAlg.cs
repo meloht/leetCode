ff5b6bb92e5263d0451a483c372d0a97af906b7d
hongtao_luo <luohongtao621@hotmail.com>
2024-01-10 15:54:46 +0800

    2707

--- leetCode/2701_2750/2707_ExtraCharactersInAStringAlg.cs
+++ leetCode/2701_2750/2707_ExtraCharactersInAStringAlg.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,10 @@ namespace leetCode._2701_2750
     public class _2707_ExtraCharactersInAStringAlg
     {
         int min = int.MaxValue;
-        HashSet<string> dict = new HashSet<string>();
-        public int MinExtraChar(string s, string[] dictionary)
+        HashSet<int> dict = new HashSet<int>();
+        public int MinExtraChar1(string s, string[] dictionary)
         {
             HashSet<string> keys = new HashSet<string>(dictionary);
-
 
             Dfs(s.Length, s, 0, keys);
 
@@ -22,14 +22,14 @@ namespace leetCode._2701_2750
 
         private void Dfs(int len, string word, int index, HashSet<string> keys)
         {
-            string key = $"{len},{index}";
-            if (dict.Contains(key))
+            int hashcode = HashCode.Combine(len, index);
+            if (dict.Contains(hashcode))
             {
                 return;
             }
             else
             {
-                dict.Add(key);
+                dict.Add(hashcode);
             }
             if (index >= word.Length)
             {
@@ -51,23 +51,74 @@ namespace leetCode._2701_2750
                     Dfs(len, word, i + 1, keys);
                 }
             }
+
+
         }
 
-        public int MinExtraChar1(string s, string[] dictionary)
+        public int MinExtraChar2(string s, string[] dictionary)
         {
-            WordDictionary dic = new WordDictionary();
-            foreach (var word in dictionary)
+            int n = s.Length;
+
+            int[] dp = new int[n + 1];
+            Array.Fill(dp, int.MaxValue);
+            HashSet<string> dict = new HashSet<string>(dictionary);
+            dp[0] = 0;
+            for (int i = 1; i <= n; i++)
             {
-                dic.Insert(word);
+                dp[i] = dp[i - 1] + 1;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (dict.Contains(s.Substring(j, i - j)))
+                    {
+                        dp[i] = Math.Min(dp[i], dp[j]);
+                    }
+                }
             }
-            return 0;
+            return dp[n];
+        }
+
+        public int MinExtraChar(string s, string[] dictionary)
+        {
+            int n = s.Length;
+
+            int[] dp = new int[n + 1];
+            Array.Fill(dp, int.MaxValue);
+
+            dp[0] = 0;
+            WordDictionary dict = new WordDictionary();
+            foreach (var item in dictionary)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = item.Length - 1; i >= 0; i--)
+                {
+                    sb.Append(item[i]);
+                }
+                dict.Insert(sb.ToString());
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + 1;
+                WordDictionary node = dict;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (node != null)
+                    {
+                        node = node[s[j]];
+                        if (node != null && node.IsEnd)
+                        {
+                            dp[i] = Math.Min(dp[i], dp[j]);
+                        }
+                    }
+
+                }
+            }
+            return dp[n];
         }
 
         private class WordDictionary
         {
             public WordDictionary[] next = new WordDictionary[26];
-            public string word;
-            public bool hashNext = false;
+            public bool IsEnd = false;
             public WordDictionary this[char c]
             {
                 get
@@ -77,7 +128,6 @@ namespace leetCode._2701_2750
                 set
                 {
                     next[c - 'a'] = value;
-                    hashNext = true;
                 }
             }
 
@@ -90,8 +140,9 @@ namespace leetCode._2701_2750
                     {
                         node[c] = new WordDictionary();
                     }
-                    node.word = word;
+                    node = node[c];
                 }
+                node.IsEnd = true;
             }
         }
     }
