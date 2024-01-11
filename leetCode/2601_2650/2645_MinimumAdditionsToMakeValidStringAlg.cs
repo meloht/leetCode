a2f69cba58e53aaf73809084a944ad8edbfbf413
hongtao_luo <luohongtao621@hotmail.com>
2024-01-11 11:42:30 +0800

    2645

--- leetCode/2601_2650/2645_MinimumAdditionsToMakeValidStringAlg.cs
+++ leetCode/2601_2650/2645_MinimumAdditionsToMakeValidStringAlg.cs
@@ -9,7 +9,7 @@ namespace leetCode._2601_2650
 {
     public class _2645_MinimumAdditionsToMakeValidStringAlg
     {
-        public int AddMinimum(string word)
+        public int AddMinimum1(string word)
         {
             if (word.Length == 1)
                 return 2;
@@ -47,5 +47,47 @@ namespace leetCode._2601_2650
             return dp[word.Length];
 
         }
+
+        public int AddMinimum(string word)
+        {
+            int n = word.Length;
+            int[] d = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                d[i] = d[i - 1] + 2;
+                if (i > 1 && word[i - 1] > word[i - 2])
+                {
+                    d[i] = d[i - 1] - 1;
+                }
+            }
+            return d[n];
+        }
+
+        public int AddMinimum2(string word)
+        {
+            int n = word.Length;
+            int res = word[0] - word[n - 1] + 2;
+            for (int i = 1; i < n; i++)
+            {
+                res += (word[i] - word[i - 1] + 2) % 3;
+            }
+            return res;
+        }
+
+        public int AddMinimum3(string word)
+        {
+            int n = word.Length, cnt = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (word[i] <= word[i - 1])
+                {
+                    cnt++;
+                }
+            }
+            return cnt * 3 - n;
+        }
+
+
+
     }
 }
