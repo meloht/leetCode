cbdb2c01f07dbd6b1b2e31a1400adac3c44b8c45
hongtao_luo <luohongtao621@hotmail.com>
2024-01-11 11:31:53 +0800

    2645

---
+++ TestLeetCode/2601_2650/2645_MinimumAdditionsToMakeValidStringUnitTest.cs
@@ -0,0 +1,61 @@
+﻿using leetCode._2601_2650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2601_2650
+{
+    [TestClass]
+    public class _2645_MinimumAdditionsToMakeValidStringUnitTest
+    {
+        _2645_MinimumAdditionsToMakeValidStringAlg alg = new _2645_MinimumAdditionsToMakeValidStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string word = "b";
+            int exp = 2;
+            int res = alg.AddMinimum(word);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string word = "aaa";
+            int exp = 6;
+            int res = alg.AddMinimum(word);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string word = "abc";
+            int exp = 0;
+            int res = alg.AddMinimum(word);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string word = "abcaaaaa";
+            int exp = 10;
+            int res = alg.AddMinimum(word);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string word = "aaaabb";
+            int exp = 9;
+            int res = alg.AddMinimum(word);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/2601_2650/2645_MinimumAdditionsToMakeValidStringAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2601_2650
+{
+    public class _2645_MinimumAdditionsToMakeValidStringAlg
+    {
+        public int AddMinimum(string word)
+        {
+            if (word.Length == 1)
+                return 2;
+
+            int[] dp = new int[word.Length + 1];
+            dp[1] = 2;
+            string a = word.Substring(0, 2);
+            if (a == "ab" || a == "bc" || a == "ac")
+            {
+                dp[2] = 1;
+            }
+            else
+            {
+                dp[2] = 4;
+            }
+
+            for (int i = 3; i <= word.Length; i++)
+            {
+                string s = word.Substring(i - 3, 3);
+                if (s == "abc")
+                {
+                    dp[i] = dp[i - 3];
+                }
+                else if (s.EndsWith("ab") || s.EndsWith("ac") || s.EndsWith("bc"))
+                {
+                    dp[i] = dp[i - 2] + 1;
+                }
+                else
+                {
+                    dp[i] = dp[i - 1] + 2;
+                }
+
+            }
+
+            return dp[word.Length];
+
+        }
+    }
+}
