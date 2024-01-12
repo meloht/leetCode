c1fe777899a44b63e489ce83db2615f705a1c4a1
hongtao_luo <luohongtao621@hotmail.com>
2024-01-12 13:51:26 +0800

    222

--- leetCode/0201-0250/222_CountCompleteTreeNodesAlg.cs
+++ leetCode/0201-0250/222_CountCompleteTreeNodesAlg.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace leetCode._0201_0250
 {
     public class _222_CountCompleteTreeNodesAlg
     {
         int num = 0;
-        public int CountNodes(TreeNode root)
+        public int CountNodes1(TreeNode root)
         {
             Dfs(root);
-            return num; 
+            return num;
         }
         private void Dfs(TreeNode node)
         {
@@ -22,5 +23,54 @@ namespace leetCode._0201_0250
             Dfs(node.left);
             Dfs(node.right);
         }
+
+        public int CountNodes(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            int level = 0;
+            TreeNode node = root;
+            while (node.left != null)
+            {
+                level++;
+                node = node.left;
+            }
+
+            int low = 1 << level;
+            int high = (1 << (level + 1)) - 1;
+            while (low < high)
+            {
+                int mid = (high + low + 1) / 2 ;
+                if (Exists(root, level, mid))
+                {
+                    low = mid;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return low;
+        }
+        public bool Exists(TreeNode root, int level, int k)
+        {
+            int bits = 1 << (level - 1);
+            TreeNode node = root;
+            while (node != null && bits > 0)
+            {
+                if ((bits & k) == 0)
+                {
+                    node = node.left;
+                }
+                else
+                {
+                    node = node.right;
+                }
+                bits >>= 1;
+            }
+            return node != null;
+        }
+
+
     }
 }
