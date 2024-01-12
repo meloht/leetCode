5fb25ffc8dd45faa593ce67d2f0dade87980c03e
hongtao_luo <luohongtao621@hotmail.com>
2024-01-12 10:21:22 +0800

    219

---
+++ TestLeetCode/0201_0250/219_ContainsDuplicateIIUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _219_ContainsDuplicateIIUnitTest
+    {
+        _219_ContainsDuplicateIIAlg alg = new _219_ContainsDuplicateIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 1];
+            int k = 3;
+            bool res = alg.ContainsNearbyDuplicate(nums, k);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 0, 1, 1];
+            int k = 1;
+            bool res = alg.ContainsNearbyDuplicate(nums, k);
+            Assert.AreEqual(true, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 2, 3, 1, 2, 3];
+            int k = 2;
+            bool res = alg.ContainsNearbyDuplicate(nums, k);
+            Assert.AreEqual(false, res);
+        }
+    }
+}
---
+++ leetCode/0201-0250/219_ContainsDuplicateIIAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _219_ContainsDuplicateIIAlg
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i <= k && i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+
+            for (int i = k + 1; i < nums.Length; i++)
+            {
+
+                if (dict.ContainsKey(nums[i]))
+                {
+                    if (i - dict[nums[i]] <= k)
+                    {
+                        return true;
+                    }
+                    dict[nums[i]] = i;
+                }
+                else
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
