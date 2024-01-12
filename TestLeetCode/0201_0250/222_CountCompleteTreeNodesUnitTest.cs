0c065fa961626742f2986b78bd959af459de30bc
hongtao_luo <luohongtao621@hotmail.com>
2024-01-12 13:33:20 +0800

    222

---
+++ TestLeetCode/0201_0250/222_CountCompleteTreeNodesUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _222_CountCompleteTreeNodesUnitTest
+    {
+        _222_CountCompleteTreeNodesAlg alg = new _222_CountCompleteTreeNodesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 2, 3, 4, 5, 6];
+            var tree = Utils.BuildTree(root);
+            int exp = 6;
+            int res = alg.CountNodes(tree);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [];
+            var tree = Utils.BuildTree(root);
+            int exp = 0;
+            int res = alg.CountNodes(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [1];
+            var tree = Utils.BuildTree(root);
+            int exp = 1;
+            int res = alg.CountNodes(tree);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0201-0250/222_CountCompleteTreeNodesAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _222_CountCompleteTreeNodesAlg
+    {
+        int num = 0;
+        public int CountNodes(TreeNode root)
+        {
+            Dfs(root);
+            return num; 
+        }
+        private void Dfs(TreeNode node)
+        {
+            if (node == null)
+                return;
+            num++;
+            Dfs(node.left);
+            Dfs(node.right);
+        }
+    }
+}
