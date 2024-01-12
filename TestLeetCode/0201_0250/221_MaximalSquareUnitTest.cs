7721163dc9aeae2c016dd7ec257ddc8de347cbc0
hongtao_luo <luohongtao621@hotmail.com>
2024-01-12 13:22:43 +0800

    221

---
+++ TestLeetCode/0201_0250/221_MaximalSquareUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _221_MaximalSquareUnitTest
+    {
+        _221_MaximalSquareAlg alg = new _221_MaximalSquareAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[][] matrix = [['1', '0', '1', '0', '0'], ['1', '0', '1', '1', '1'], ['1', '1', '1', '1', '1'], ['1', '0', '0', '1', '0']];
+            int exp = 4;
+            int res = alg.MaximalSquare(matrix);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[][] matrix = [['0', '1'], ['1', '0']];
+            int exp = 1;
+            int res = alg.MaximalSquare(matrix);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            char[][] matrix = [['0']];
+            int exp = 0;
+            int res = alg.MaximalSquare(matrix);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0201-0250/221_MaximalSquareAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _221_MaximalSquareAlg
+    {
+        public int MaximalSquare(char[][] matrix)
+        {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            int[,] dp = new int[m, n];
+            int max = 0;
+            for (int i = 0; i < n; i++)
+            {
+                dp[0, i] = matrix[0][i] - '0';
+                max = Math.Max(max, dp[0, i]);
+            }
+            for (int i = 0; i < m; i++)
+            {
+                dp[i, 0] = matrix[i][0] - '0';
+                max = Math.Max(max, dp[i, 0]);
+            }
+
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    if (matrix[i][j] == '1')
+                    {
+                        int num = Math.Min(dp[i - 1, j], dp[i - 1, j - 1]);
+                        num = Math.Min(num, dp[i, j - 1]);
+                        dp[i, j] = num + 1;
+                        max = Math.Max(max, dp[i, j]);
+                    }
+                }
+            }
+
+            return max * max;
+        }
+    }
+}
