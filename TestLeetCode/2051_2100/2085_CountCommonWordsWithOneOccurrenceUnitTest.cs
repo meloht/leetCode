431fb2cbcf94f9b1b85ea8c2fbaef2678f1414e9
hongtao_luo <luohongtao621@hotmail.com>
2024-01-12 09:54:20 +0800

    2085

---
+++ TestLeetCode/2051_2100/2085_CountCommonWordsWithOneOccurrenceUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._2051_2100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2051_2100
+{
+    [TestClass]
+    public class _2085_CountCommonWordsWithOneOccurrenceUnitTest
+    {
+        _2085_CountCommonWordsWithOneOccurrenceAlg alg = new _2085_CountCommonWordsWithOneOccurrenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words1 = ["leetcode", "is", "amazing", "as", "is"];
+            string[] words2 = ["amazing", "leetcode", "is"];
+            int exp = 2;
+            int res = alg.CountWords(words1, words2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words1 = ["b", "bb", "bbb"];
+            string[] words2 = ["a", "aa", "aaa"];
+            int exp = 0;
+            int res = alg.CountWords(words1, words2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] words1 = ["a", "ab"];
+            string[] words2 = ["a", "a", "a", "ab"];
+            int exp = 1;
+            int res = alg.CountWords(words1, words2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2051_2100/2085_CountCommonWordsWithOneOccurrenceAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2051_2100
+{
+    public class _2085_CountCommonWordsWithOneOccurrenceAlg
+    {
+        public int CountWords(string[] words1, string[] words2)
+        {
+            Dictionary<string, int> dict1 = new Dictionary<string, int>();
+            Dictionary<string, int> dict2 = new Dictionary<string, int>();
+
+            AddDict(dict1, words1);
+            AddDict(dict2, words2);
+
+            int count = 0;
+            foreach (var item in dict1)
+            {
+                if (item.Value == 1 && dict2.ContainsKey(item.Key) && dict2[item.Key]==1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        private void AddDict(Dictionary<string, int> dict, string[] words)
+        {
+            foreach (string word in words) 
+            {
+                if (dict.ContainsKey(word))
+                {
+                    dict[word] += 1;
+                }
+                else
+                {
+                    dict.Add(word, 1);
+                }
+            }
+        }
+    }
+}
+
