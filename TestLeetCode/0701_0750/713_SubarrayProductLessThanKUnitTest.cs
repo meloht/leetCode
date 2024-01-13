9eadb16a1f44d3d3d025a24498f9ac395df88863
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-13 14:44:43 +0800

    713

---
+++ TestLeetCode/0701_0750/713_SubarrayProductLessThanKUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0701_0750
+{
+    [TestClass]
+    public class _713_SubarrayProductLessThanKUnitTest
+    {
+        _713_SubarrayProductLessThanKAlg alg = new _713_SubarrayProductLessThanKAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [10, 5, 2, 6];
+            int k = 100;
+            int exp = 8;
+            int res = alg.NumSubarrayProductLessThanK(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3];
+            int k = 0;
+            int exp = 0;
+            int res = alg.NumSubarrayProductLessThanK(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0701_0750/713_SubarrayProductLessThanKAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _713_SubarrayProductLessThanKAlg
+    {
+        public int NumSubarrayProductLessThanK(int[] nums, int k)
+        {
+            if (k <= 1)
+                return 0;
+            int ans = 0;
+            int res = 1;
+            int left = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res *= nums[i];
+                while (res >= k)
+                {
+                    res /= nums[left];
+                    left++;
+                }
+                ans += i - left + 1;
+            }
+            return ans;
+        }
+    }
+}
