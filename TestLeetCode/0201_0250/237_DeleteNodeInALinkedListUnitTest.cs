a6487ba5786942943ab7bd4d47838fede7881afb
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-13 15:56:45 +0800

    237

---
+++ TestLeetCode/0201_0250/237_DeleteNodeInALinkedListUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _237_DeleteNodeInALinkedListUnitTest
+    {
+        _237_DeleteNodeInALinkedListAlg alg = new _237_DeleteNodeInALinkedListAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [4, 5, 1, 9];
+            var list = Utils.BuildListNode(head);
+            var node = list.next;
+            alg.DeleteNode(node);
+            var res = Utils.GetLinkNodeList(list);
+
+            int[] exp = [4, 1, 9];
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [4, 5, 1, 9];
+            var list = Utils.BuildListNode(head);
+            var node = list.next.next;
+            alg.DeleteNode(node);
+            var res = Utils.GetLinkNodeList(list);
+
+            int[] exp = [4, 5, 9];
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0201-0250/237_DeleteNodeInALinkedListAlg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _237_DeleteNodeInALinkedListAlg
+    {
+        public void DeleteNode(ListNode node)
+        {
+            ListNode temp = node.next;
+            node.val = temp.val;
+            node.next = temp.next;
+
+
+        }
+    }
+}
