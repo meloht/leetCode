e5848cefd29ee564cf9f8a432cf724179911b04b
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-13 17:15:09 +0800

    235

---
+++ TestLeetCode/0201_0250/235_LowestCommonAncestorOfABinarySearchTreeUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _235_LowestCommonAncestorOfABinarySearchTreeUnitTest
+    {
+        _235_LowestCommonAncestorOfABinarySearchTreeAlg alg = new _235_LowestCommonAncestorOfABinarySearchTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [6, 2, 8, 0, 4, 7, 9, null, null, 3, 5];
+            var tree = Utils.BuildTree(root);
+            var p = Utils.GetTreeNodeByVal(2, tree);
+            var q = Utils.GetTreeNodeByVal(8, tree);
+            int exp = 6;
+            var res = alg.LowestCommonAncestor(tree, p, q);
+            Assert.AreEqual(exp, res.val);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [6, 2, 8, 0, 4, 7, 9, null, null, 3, 5];
+            var tree = Utils.BuildTree(root);
+            var p = Utils.GetTreeNodeByVal(2, tree);
+            var q = Utils.GetTreeNodeByVal(4, tree);
+            int exp = 2;
+            var res = alg.LowestCommonAncestor(tree, p, q);
+            Assert.AreEqual(exp, res.val);
+        }
+    }
+}
---
+++ leetCode/0201-0250/235_LowestCommonAncestorOfABinarySearchTreeAlg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _235_LowestCommonAncestorOfABinarySearchTreeAlg
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (p.val < root.val && q.val < root.val)
+                return LowestCommonAncestor(root.left, p, q);
+            if (q.val > root.val && p.val > root.val)
+                return LowestCommonAncestor(root.right, p, q);
+            return root;
+        }
+    }
+}
