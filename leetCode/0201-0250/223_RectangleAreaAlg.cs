aa0a6ab095b46694977905f5779acbafa584a978
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-13 11:48:31 +0800

    223

--- leetCode/0201-0250/223_RectangleAreaAlg.cs
+++ leetCode/0201-0250/223_RectangleAreaAlg.cs
@@ -13,18 +13,21 @@ namespace leetCode._0201_0250
             int area = GetArea(ax1, ax2, ay1, ay2);
             area += GetArea(bx1, bx2, by1, by2);
 
-            if (ax2 <= bx1)
-            {
-                return area;
-            }
-            if (ay1 >= by2)
+            if (ax2 <= bx1 || ay1 >= by2 || ax1 >= bx2 || ay2 <= by1)
             {
                 return area;
             }
 
-        
+            int x1 = Math.Min(bx2, ax2);
+            int x2 = Math.Max(bx1, ax1);
 
-            return 0;
+            int y1 = Math.Min(by2, ay2);
+            int y2 = Math.Max(by1, ay1);
+
+            int a = GetArea(x1, x2, y1, y2);
+            area -= a;
+
+            return area;
         }
 
         private int GetArea(int x1, int x2, int y1, int y2)
