e4e52c82493c17dd8a0c2d964d3b8b7c36f6804b
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-13 15:34:54 +0800

    876

---
+++ TestLeetCode/0851_0900/876_MiddleOfTheLinkedListUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0851_0900
+{
+    [TestClass]
+    public class _876_MiddleOfTheLinkedListUnitTest
+    {
+        _876_MiddleOfTheLinkedListAlg alg = new _876_MiddleOfTheLinkedListAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [1, 2, 3, 4, 5];
+            var node = Utils.BuildListNode(head);
+            var res = alg.MiddleNode(node);
+            int[] rr = Utils.GetLinkNodeList(res);
+            int[] exp = [3, 4, 5];
+            Assert.IsTrue(exp.SequenceEqual(rr));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [1, 2, 3, 4, 5, 6];
+            var node = Utils.BuildListNode(head);
+            var res = alg.MiddleNode(node);
+            int[] rr = Utils.GetLinkNodeList(res);
+            int[] exp = [4, 5, 6];
+            Assert.IsTrue(exp.SequenceEqual(rr));
+        }
+    }
+}
---
+++ leetCode/0851_0900/876_MiddleOfTheLinkedListAlg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _876_MiddleOfTheLinkedListAlg
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+}
