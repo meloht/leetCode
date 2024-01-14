6be3af1c4182013349bb6610350525e5e4a51118
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-14 14:58:23 +0800

    1143

---
+++ TestLeetCode/1101_1150/1143_LongestCommonSubsequenceUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._1101_1150
+{
+    [TestClass]
+    public class _1143_LongestCommonSubsequenceUnitTest
+    {
+        _1143_LongestCommonSubsequenceAlg alg = new _1143_LongestCommonSubsequenceAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string text1 = "abcde", text2 = "ace";
+            int exp = 3;
+
+            int res = alg.LongestCommonSubsequence(text1, text2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string text1 = "abc", text2 = "abc";
+            int exp = 3;
+
+            int res = alg.LongestCommonSubsequence(text1, text2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string text1 = "abc", text2 = "def";
+            int exp = 0;
+
+            int res = alg.LongestCommonSubsequence(text1, text2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1101_1150/1143_LongestCommonSubsequenceAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1143_LongestCommonSubsequenceAlg
+    {
+        public int LongestCommonSubsequence(string text1, string text2)
+        {
+            int n = text1.Length;
+            int m = text2.Length;
+
+            int[,] dp = new int[n + 1, m + 1];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (text1[i] == text2[j])
+                    {
+                        dp[i + 1, j + 1] = dp[i, j] + 1;
+                    }
+                    else
+                    {
+                        dp[i + 1, j + 1] = Math.Max(dp[i, j + 1], dp[i + 1, j]);
+                    }
+                }
+            }
+            return dp[n, m];
+        }
+    }
+}
