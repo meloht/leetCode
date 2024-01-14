7726fa5169bfb578ad788e00debb23062b9c1de3
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-14 15:35:21 +0800

    300

---
+++ TestLeetCode/0251_0300/300_LongestIncreasingSubsequenceUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0251_0300
+{
+    [TestClass]
+    public class _300_LongestIncreasingSubsequenceUnitTest
+    {
+        _300_LongestIncreasingSubsequenceAlg alg = new _300_LongestIncreasingSubsequenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [10, 9, 2, 5, 3, 7, 101, 18];
+            int exp = 4;
+            int res = alg.LengthOfLIS(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0, 1, 0, 3, 2, 3];
+            int exp = 4;
+            int res = alg.LengthOfLIS(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [7, 7, 7, 7, 7, 7, 7];
+            int exp = 1;
+            int res = alg.LengthOfLIS(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0251_0300/300_LongestIncreasingSubsequenceAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _300_LongestIncreasingSubsequenceAlg
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+            int n = nums.Length;
+            int[] dp = new int[n];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j]);
+                    }
+                }
+                dp[i] = dp[i] + 1;
+
+
+            }
+
+            return dp.Max(); 
+        }
+    } 
+}
