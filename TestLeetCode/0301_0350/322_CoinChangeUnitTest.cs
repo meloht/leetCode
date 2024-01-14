17b6b98eee6cf3163949e65a2dfb47d10a08308d
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-14 14:41:56 +0800

    322

---
+++ TestLeetCode/0301_0350/322_CoinChangeUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0301_0350
+{
+    [TestClass]
+    public class _322_CoinChangeUnitTest
+    {
+        _322_CoinChangeAlg alg = new _322_CoinChangeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] coins = [1, 2, 5];
+            int amount = 11;
+            int exp = 3;
+            int res = alg.CoinChange(coins, amount);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] coins = [2];
+            int amount = 3;
+            int exp = -1;
+            int res = alg.CoinChange(coins, amount);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] coins = [1];
+            int amount = 0;
+            int exp = 0;
+            int res = alg.CoinChange(coins, amount);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0301_0350/322_CoinChangeAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _322_CoinChangeAlg
+    {
+        public int CoinChange(int[] coins, int amount)
+        {
+            int n = coins.Length;
+            int nn = n + 1;
+            int mm = amount + 1;
+            int[,] dp = new int[nn, mm];
+            for (int i = 0; i < mm; i++)
+            {
+                dp[0, i] = int.MaxValue / 2;
+            }
+            dp[0, 0] = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= amount; j++)
+                {
+                    if (j < coins[i])
+                    {
+                        dp[i + 1, j] = dp[i, j];
+                    }
+                    else
+                    {
+                        dp[i + 1, j] = Math.Min(dp[i, j], dp[i + 1, j - coins[i]] + 1);
+                    }
+                }
+            }
+            int ans = dp[n, amount];
+            if (ans < int.MaxValue/2)
+                return ans;
+            return -1;
+        }
+    }
+}
