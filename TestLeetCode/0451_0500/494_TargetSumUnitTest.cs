9bf4e2eff1f57bc5fb04a5dd16a5c73632196037
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-14 14:04:30 +0800

    494

---
+++ TestLeetCode/0451_0500/494_TargetSumUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0451_0500
+{
+    [TestClass]
+    public class _494_TargetSumUnitTest
+    {
+        _494_TargetSumAlg alg = new _494_TargetSumAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 1, 1, 1, 1];
+            int target = 3;
+            int res = alg.FindTargetSumWays(nums, target);
+            int exp = 5;
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1];
+            int target = 1;
+            int res = alg.FindTargetSumWays(nums, target);
+            int exp = 1;
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/494_TargetSumAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _494_TargetSumAlg
+    {
+        public int FindTargetSumWays(int[] nums, int target)
+        {
+            target += nums.Sum();
+            if (target < 0 || target % 2 > 0)
+                return 0;
+
+            target /= 2;
+            int n = nums.Length;
+            int[,] dp = new int[n + 1, target + 1];
+            dp[0, 0] = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= target; j++)
+                {
+                    if (j < nums[i])
+                    {
+                        dp[i + 1, j] = dp[i, j];
+                    }
+                    else
+                    {
+                        dp[i + 1, j] = dp[i, j] + dp[i, j - nums[i]];
+                    }
+                }
+            }
+
+            return dp[n, target];
+        }
+    }
+}
