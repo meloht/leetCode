5c4ddf41a0f8208e2e5407fb865741a2f8e9dcd8
hongtao_luo <luohongtao621@hotmail.com>
2024-01-15 15:14:56 +0800

    714

--- leetCode/0701_0750/714_BestTimeToBuyAndSellStockWithTransactionFeeAlg.cs
+++ leetCode/0701_0750/714_BestTimeToBuyAndSellStockWithTransactionFeeAlg.cs
@@ -8,7 +8,7 @@ namespace leetCode._0701_0750
 {
     public class _714_BestTimeToBuyAndSellStockWithTransactionFeeAlg
     {
-        public int MaxProfit(int[] prices, int fee)
+        public int MaxProfit1(int[] prices, int fee)
         {
             int n = prices.Length;
             int[,] dp = new int[n, 2];
@@ -20,6 +20,22 @@ namespace leetCode._0701_0750
                 dp[i, 1] = Math.Max(dp[i - 1, 1], dp[i - 1, 0] - prices[i]);
             }
             return dp[n - 1, 0];
+
+        }
+
+        public int MaxProfit(int[] prices, int fee)
+        {
+            int n = prices.Length;
+            int sell0 = 0;
+            int buy0 = -prices[0];
+
+            for (int i = 1; i < n; ++i)
+            {
+                int sell = Math.Max(sell0, buy0 + prices[i] - fee);
+                buy0 = Math.Max(buy0, sell0 - prices[i]);
+                sell0 = sell;
+            }
+            return sell0;
 
         }
     }
