1f489edc9cb637c2ee504c5534d48d1ecf102a56
hongtao_luo <luohongtao621@hotmail.com>
2024-01-15 15:06:16 +0800

    714

---
+++ TestLeetCode/0701_0750/714_BestTimeToBuyAndSellStockWithTransactionFeeUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0701_0750
+{
+    [TestClass]
+    public class _714_BestTimeToBuyAndSellStockWithTransactionFeeUnitTest
+    {
+        _714_BestTimeToBuyAndSellStockWithTransactionFeeAlg alg = new _714_BestTimeToBuyAndSellStockWithTransactionFeeAlg();
+       
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] prices = [1, 3, 2, 8, 4, 9];
+            int fee = 2;
+            int exp = 8;
+          
+            int res = alg.MaxProfit(prices, fee);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] prices = [1, 3, 7, 5, 10, 3];
+            int fee = 3;
+            int exp = 6;
+
+            int res = alg.MaxProfit(prices, fee);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0701_0750/714_BestTimeToBuyAndSellStockWithTransactionFeeAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _714_BestTimeToBuyAndSellStockWithTransactionFeeAlg
+    {
+        public int MaxProfit(int[] prices, int fee)
+        {
+            int n = prices.Length;
+            int[,] dp = new int[n, 2];
+            dp[0, 0] = 0;
+            dp[0, 1] = -prices[0];
+            for (int i = 1; i < n; ++i)
+            {
+                dp[i, 0] = Math.Max(dp[i - 1, 0], dp[i - 1, 1] + prices[i] - fee);
+                dp[i, 1] = Math.Max(dp[i - 1, 1], dp[i - 1, 0] - prices[i]);
+            }
+            return dp[n - 1, 0];
+
+        }
+    }
+}
