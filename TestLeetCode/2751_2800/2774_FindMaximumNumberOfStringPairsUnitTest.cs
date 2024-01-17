ba96b7968c64c6fc4cc2b4c726f3b58815ada4b8
hongtao_luo <luohongtao621@hotmail.com>
2024-01-17 13:45:44 +0800

    2774

---
+++ TestLeetCode/2751_2800/2774_FindMaximumNumberOfStringPairsUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2751_2800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2751_2800
+{
+    [TestClass]
+    public class _2774_FindMaximumNumberOfStringPairsUnitTest
+    {
+        _2774_FindMaximumNumberOfStringPairsAlg alg = new _2774_FindMaximumNumberOfStringPairsAlg();
+        
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["cd", "ac", "dc", "ca", "zz"];
+            int exp = 2;
+            int res = alg.MaximumNumberOfStringPairs(words);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["ab", "ba", "cc"];
+            int exp = 1;
+            int res = alg.MaximumNumberOfStringPairs(words);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] words = ["aa", "ab"];
+            int exp = 0;
+            int res = alg.MaximumNumberOfStringPairs(words);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2751_2800/2774_FindMaximumNumberOfStringPairsAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2751_2800
+{
+    public class _2774_FindMaximumNumberOfStringPairsAlg
+    {
+        public int MaximumNumberOfStringPairs1(string[] words)
+        {
+            int ans = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                var aa = words[i].ToArray();
+                Array.Reverse(aa);
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    var ss = words[j].ToArray();
+                  
+                    if (ss.SequenceEqual(aa))
+                    {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+        public int MaximumNumberOfStringPairs(string[] words)
+        {
+            int n = words.Length;
+            int ans = 0;
+            ISet<int> seen = new HashSet<int>();
+            for (int i = 0; i < n; ++i)
+            {
+                if (seen.Contains(words[i][1] * 100 + words[i][0]))
+                {
+                    ++ans;
+                }
+                seen.Add(words[i][0] * 100 + words[i][1]);
+            }
+            return ans;
+        }
+
+
+    }
+}
