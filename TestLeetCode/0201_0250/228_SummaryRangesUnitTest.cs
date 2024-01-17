e83a4d78beb97a70daa52f7b578239d1547eb533
hongtao_luo <luohongtao621@hotmail.com>
2024-01-17 13:53:36 +0800

    228

---
+++ TestLeetCode/0201_0250/228_SummaryRangesUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _228_SummaryRangesUnitTest
+    {
+        _228_SummaryRangesAlg alg = new _228_SummaryRangesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [0, 1, 2, 4, 5, 7];
+            string[] exp = ["0->2", "4->5", "7"];
+            var res = alg.SummaryRanges(nums);
+            bool bl = exp.SequenceEqual(res.ToArray());
+            Assert.IsTrue(bl);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0, 2, 3, 4, 6, 8, 9];
+            string[] exp = ["0", "2->4", "6", "8->9"];
+            var res = alg.SummaryRanges(nums);
+            bool bl = exp.SequenceEqual(res.ToArray());
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0201-0250/228_SummaryRangesAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _228_SummaryRangesAlg
+    {
+        public IList<string> SummaryRanges(int[] nums)
+        {
+            return null;
+        }
+    }
+}
