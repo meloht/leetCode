23ffdb6fc64437e07a3bbf75c399ca05cb299e19
hongtao_luo <luohongtao621@hotmail.com>
2024-01-17 14:17:49 +0800

    228

--- leetCode/0201-0250/228_SummaryRangesAlg.cs
+++ leetCode/0201-0250/228_SummaryRangesAlg.cs
@@ -10,7 +10,73 @@ namespace leetCode._0201_0250
     {
         public IList<string> SummaryRanges(int[] nums)
         {
-            return null;
+            List<string> result = new List<string>();
+            if (nums.Length == 0)
+                return result;
+            if (nums.Length == 1)
+            {
+                result.Add(nums[0].ToString());
+                return result;
+            }
+
+            int begin = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int curr = nums[i];
+                if (nums[i - 1] + 1 != curr)
+                {
+                    if (begin == nums[i - 1])
+                    {
+                        result.Add(begin.ToString());
+                    }
+                    else
+                    {
+                        result.Add($"{begin}->{nums[i - 1]}");
+                    }
+                    begin = curr;
+                }
+              
+                if (i == nums.Length - 1)
+                {
+                    if (nums[i - 1] + 1 != curr)
+                    {
+                        result.Add(curr.ToString());
+                    }
+                    else
+                    {
+                        result.Add($"{begin}->{curr}");
+                    }
+                }
+            }
+            return result;
         }
+
+        public IList<string> SummaryRanges2(int[] nums)
+        {
+            List<string> result = new List<string>();
+            int i = 0;
+            int n = nums.Length;
+            while (i < n)
+            {
+                int low = i;
+                i++;
+                while (i < n && nums[i] == nums[i - 1] + 1)
+                {
+                    i++;
+                }
+                int high = i - 1;
+                StringBuilder temp = new StringBuilder();
+                temp.Append(nums[low]);
+                if (low < high)
+                {
+                    temp.Append("->");
+                    temp.Append(nums[high]);
+                }
+                result.Add(temp.ToString());
+            }
+            return result;
+        }
+
+
     }
 }
