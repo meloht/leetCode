fa12bcfe4239496aaa8f0046884f1742e2ee7652
hongtao_luo <luohongtao621@hotmail.com>
2024-01-18 14:20:02 +0800

    233

---
+++ TestLeetCode/0201_0250/_233_NumberOfDigitOneUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _233_NumberOfDigitOneUnitTest
+    {
+        _233_NumberOfDigitOneAlg alg = new _233_NumberOfDigitOneAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int exp = 6;
+            int res = alg.CountDigitOne(13);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int exp = 0;
+            int res = alg.CountDigitOne(0);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int exp = 12;
+            int res = alg.CountDigitOne(20);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int exp = 767944060;
+            int res = alg.CountDigitOne(824883294);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0201-0250/233_NumberOfDigitOneAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _233_NumberOfDigitOneAlg
+    {
+        int Mod = 1_000_000_007;
+        int[,] dp;
+        public int CountDigitOne(int n)
+        {
+            string ss = n.ToString();
+            int nn = ss.Length;
+            dp = new int[nn, nn];
+            for (int i = 0; i < nn; i++)
+            {
+                for (int j = 0; j < nn; j++)
+                {
+                    dp[i, j] = -1;
+                }
+            }
+            int ans = Dfs(0, 0, ss, true);
+            return ans;
+        }
+
+        private int Dfs(int i, int num, string s, bool isLimit)
+        {
+            if (i == s.Length)
+            {
+                return num;
+            }
+            if (!isLimit && dp[i, num] >= 0)
+            {
+                return dp[i, num];
+            }
+            int up = isLimit ? s[i] - '0' : 9;
+            int res = 0;
+            for (int j = 0; j <= up; j++)
+            {
+                int cnt = num;
+                if (j == 1)
+                {
+                    cnt++;
+                }
+                res = res + Dfs(i + 1, cnt, s, isLimit && (j == up));
+
+
+            }
+            if (!isLimit)
+            {
+                dp[i, num] = res;
+            }
+            return res;
+        }
+
+    }
+}
