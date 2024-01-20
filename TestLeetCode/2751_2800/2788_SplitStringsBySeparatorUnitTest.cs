0abbb5df8677cc1edac60c5b4e3c54f5e8d53feb
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-20 12:16:04 +0800

    2788

---
+++ TestLeetCode/2751_2800/2788_SplitStringsBySeparatorUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._2751_2800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2751_2800
+{
+    [TestClass]
+    public class _2788_SplitStringsBySeparatorUnitTest
+    {
+        _2788_SplitStringsBySeparatorAlg alg = new _2788_SplitStringsBySeparatorAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["one.two.three", "four.five", "six"];
+            char separator = '.';
+
+            string[] exp = ["one", "two", "three", "four", "five", "six"];
+            var res = alg.SplitWordsBySeparator(words, separator);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["$easy$", "$problem$"];
+            char separator = '$';
+
+            string[] exp = ["easy", "problem"];
+            var res = alg.SplitWordsBySeparator(words, separator);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] words = ["|||"];
+            char separator = '|';
+
+            string[] exp = [];
+            var res = alg.SplitWordsBySeparator(words, separator);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/2751_2800/2788_SplitStringsBySeparatorAlg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2751_2800
+{
+    public class _2788_SplitStringsBySeparatorAlg
+    {
+        public IList<string> SplitWordsBySeparator(IList<string> words, char separator)
+        {
+            List<string> result = new List<string>();
+            foreach (string word in words)
+            {
+                var ls = word.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                result.AddRange(ls);
+            }
+            return result;
+        }
+    }
+}
