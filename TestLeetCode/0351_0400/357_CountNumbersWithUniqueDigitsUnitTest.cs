572885f82dcdcd4153a5f54e8570783317c5daa2
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-20 19:36:32 +0800

    357

---
+++ TestLeetCode/0351_0400/357_CountNumbersWithUniqueDigitsUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0351_0400
+{
+    [TestClass]
+    public class _357_CountNumbersWithUniqueDigitsUnitTest
+    {
+        _357_CountNumbersWithUniqueDigitsAlg alg = new _357_CountNumbersWithUniqueDigitsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 2;
+            int exp = 91;
+            int res = alg.CountNumbersWithUniqueDigits(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 0;
+            int exp = 1;
+            int res = alg.CountNumbersWithUniqueDigits(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/357_CountNumbersWithUniqueDigitsAlg.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _357_CountNumbersWithUniqueDigitsAlg
+    {
+        int[,] dp;
+        public int CountNumbersWithUniqueDigits(int n)
+        {
+            int num = (int)Math.Pow(10, n) - 1;
+            string s = num.ToString();
+            int m = 1 << 10;
+            dp = new int[s.Length, m];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    dp[i, j] = -1;
+                }
+            }
+            return Dfs(0, 0, s, true, false) + 1;
+        }
+
+        private int Dfs(int i, int mask, string s, bool isLimit, bool isNum)
+        {
+            if (i == s.Length)
+            {
+                return isNum ? 1 : 0;
+            }
+            if (!isLimit && isNum && dp[i, mask] >= 0)
+            {
+                return dp[i, mask];
+            }
+
+            int res = 0;
+            if (!isNum)
+            {
+                res = Dfs(i + 1, mask, s, false, false);
+            }
+            int up = isLimit ? s[i] - '0' : 9;
+
+            for (int j = isNum ? 0 : 1; j <= up; j++)
+            {
+                if (((mask >> j) & 1) == 0)
+                {
+                    res += Dfs(i + 1, mask | (1 << j), s, isLimit && j == up, true);
+                }
+            }
+
+            if (!isLimit && isNum)
+            {
+                dp[i, mask] = res;
+            }
+            return res;
+        }
+
+        public int CountNumbersWithUniqueDigits1(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            if (n == 1)
+            {
+                return 10;
+            }
+            int res = 10, cur = 9;
+            for (int i = 0; i < n - 1; i++)
+            {
+                cur *= 9 - i;
+                res += cur;
+            }
+            return res;
+        }
+
+    }
+}
