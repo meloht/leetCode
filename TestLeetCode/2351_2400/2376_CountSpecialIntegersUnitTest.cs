e3214f519aa7b61337b7a2f25561a10ebc37a88c
hongtao luo <hongtao_luo.bys@htc.com>
2024-01-20 17:46:05 +0800

    2376

---
+++ TestLeetCode/2351_2400/2376_CountSpecialIntegersUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2351_2400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2351_2400
+{
+    [TestClass]
+    public class _2376_CountSpecialIntegersUnitTest
+    {
+        _2376_CountSpecialIntegersAlg alg = new _2376_CountSpecialIntegersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 20;
+            int exp = 19;
+            int res = alg.CountSpecialNumbers(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 5;
+            int exp = 5;
+            int res = alg.CountSpecialNumbers(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 135;
+            int exp = 110;
+            int res = alg.CountSpecialNumbers(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2351_2400/2376_CountSpecialIntegersAlg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2351_2400
+{
+    public class _2376_CountSpecialIntegersAlg
+    {
+        int[,] dp;
+        public int CountSpecialNumbers(int n)
+        {
+            string s = n.ToString();
+            int m = 1 << 10;
+            dp = new int[s.Length, m];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    dp[i, j] = -1;
+                }
+            }
+            return Dfs(0, 0, s, true, false);
+        }
+
+        private int Dfs(int i, int mask, string s, bool isLimit, bool isNum)
+        {
+            if (i == s.Length)
+            {
+                return isNum ? 1 : 0;
+            }
+            if (!isLimit && isNum && dp[i, mask] >= 0)
+            {
+                return dp[i, mask];
+            }
+
+            int res = 0;
+            if (!isNum)
+            {
+                res = Dfs(i + 1, mask, s, false, false);
+            }
+            int up = isLimit ? s[i] - '0' : 9;
+
+            for (int j = isNum ? 0 : 1; j <= up; j++)
+            {
+                if (((mask >> j) & 1) == 0)
+                {
+                    res += Dfs(i + 1, mask | (1 << j), s, isLimit && j == up, true);
+                }
+            }
+
+            if (!isLimit && isNum)
+            {
+                dp[i, mask] = res;
+            }
+            return res;
+        }
+    }
+}
