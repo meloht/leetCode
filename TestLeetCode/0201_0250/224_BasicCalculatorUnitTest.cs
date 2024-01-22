2b34a26b14801e9d39b78ccdec0f3a1aa1d7ca5d
hongtao_luo <luohongtao621@hotmail.com>
2024-01-22 17:29:14 +0800

    224

---
+++ TestLeetCode/0201_0250/224_BasicCalculatorUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _224_BasicCalculatorUnitTest
+    {
+        _224_BasicCalculatorAlg alg = new _224_BasicCalculatorAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "1 + 1";
+            int exp = 2;
+            int res = alg.Calculate(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = " 2-1 + 2 ";
+            int exp = 3;
+            int res = alg.Calculate(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "(1+(4+5+2)-3)+(6+8)";
+            int exp = 23;
+            int res = alg.Calculate(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0201-0250/224_BasicCalculatorAlg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _224_BasicCalculatorAlg
+    {
+        public int Calculate(string s)
+        {
+            s = s.Trim();
+            if (s.Length == 1)
+                return Convert.ToInt32(s);
+
+
+            Stack<string> strings = new Stack<string>();
+
+            return 0;
+        }
+    }
+}
