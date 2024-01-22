d9fb630a2d056cb75b53130a7c3a1ff044d0c30b
hongtao_luo <luohongtao621@hotmail.com>
2024-01-22 12:36:29 +0800

    231

---
+++ TestLeetCode/0201_0250/231_PowerOfTwoUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _231_PowerOfTwoUnitTest
+    {
+        _231_PowerOfTwoAlg alg = new _231_PowerOfTwoAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 1;
+            bool exp = true;
+            var res = alg.IsPowerOfTwo(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 16;
+            bool exp = true;
+            var res = alg.IsPowerOfTwo(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 3;
+            bool exp = false;
+            var res = alg.IsPowerOfTwo(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int n = 4;
+            bool exp = true;
+            var res = alg.IsPowerOfTwo(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int n = 5;
+            bool exp = false;
+            var res = alg.IsPowerOfTwo(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0201-0250/231_PowerOfTwoAlg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _231_PowerOfTwoAlg
+    {
+        public bool IsPowerOfTwo1(int n)
+        {
+            if (n <= 0)
+                return false;
+            if (n == 1)
+                return true;
+            if (n % 2 > 0)
+            {
+                return false;
+            }
+            n = n / 2;
+            return IsPowerOfTwo1(n);
+        }
+
+        public bool IsPowerOfTwo2(int n)
+        {
+            if (n <= 0)
+                return false;
+
+            return (n & (n - 1)) == 0;
+        }
+
+        public bool IsPowerOfTwo3(int n)
+        {
+            return n > 0 && (n & -n) == n;
+        }
+
+        const int BIG = 1 << 30;
+
+        public bool IsPowerOfTwo(int n)
+        {
+            return n > 0 && BIG % n == 0;
+        }
+
+    }
+}
