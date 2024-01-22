05392eeb3620b7685bbd0d0db0c4a6ca9773f43c
hongtao_luo <luohongtao621@hotmail.com>
2024-01-22 13:11:35 +0800

    232

---
+++ TestLeetCode/0201_0250/232_ImplementQueueUsingStacksUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0201_0250._232_ImplementQueueUsingStacksAlg;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _232_ImplementQueueUsingStacksUnitTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            _232_ImplementQueueUsingStacksAlg.MyQueue myQueue = new _232_ImplementQueueUsingStacksAlg.MyQueue();
+
+            myQueue.Push(1); // queue is: [1]
+            myQueue.Push(2); // queue is: [1, 2] (leftmost is front of the queue)
+            Assert.AreEqual(1, myQueue.Peek()); // return 1
+            Assert.AreEqual(1, myQueue.Pop()); // return 1, queue is [2]
+            Assert.AreEqual(false, myQueue.Empty()); // return false
+        }
+    }
+}
---
+++ leetCode/0201-0250/232_ImplementQueueUsingStacksAlg.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _232_ImplementQueueUsingStacksAlg
+    {
+        public class MyQueue
+        {
+            Stack<int> stack = new Stack<int>();
+            Stack<int> queue = new Stack<int>();
+            public MyQueue()
+            {
+
+            }
+
+            public void Push(int x)
+            {
+                while (queue.Count > 0)
+                {
+                    stack.Push(queue.Pop());
+                }
+                stack.Push(x);
+                while (stack.Count > 0)
+                {
+                    queue.Push(stack.Pop());
+                }
+            }
+
+            public int Pop()
+            {
+                return queue.Pop();
+            }
+
+            public int Peek()
+            {
+                return queue.Peek();
+            }
+
+            public bool Empty()
+            {
+                return queue.Count == 0;
+            }
+        }
+
+        public class MyQueue1
+        {
+            Stack<int> inStack;
+            Stack<int> outStack;
+
+            public MyQueue1()
+            {
+                inStack = new Stack<int>();
+                outStack = new Stack<int>();
+            }
+
+            public void Push(int x)
+            {
+                inStack.Push(x);
+            }
+
+            public int Pop()
+            {
+                if (outStack.Count == 0)
+                {
+                    In2Out();
+                }
+                return outStack.Pop();
+            }
+
+            public int Peek()
+            {
+                if (outStack.Count == 0)
+                {
+                    In2Out();
+                }
+                return outStack.Peek();
+            }
+
+            public bool Empty()
+            {
+                return inStack.Count == 0 && outStack.Count == 0;
+            }
+
+            private void In2Out()
+            {
+                while (inStack.Count > 0)
+                {
+                    outStack.Push(inStack.Pop());
+                }
+            }
+        }
+
+    }
+}
