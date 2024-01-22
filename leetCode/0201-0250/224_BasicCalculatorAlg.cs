c78b1e884ed4ae3059242b2435a805c345536472
hongtao_luo <luohongtao621@hotmail.com>
2024-01-22 17:57:23 +0800

    224

--- leetCode/0201-0250/224_BasicCalculatorAlg.cs
+++ leetCode/0201-0250/224_BasicCalculatorAlg.cs
@@ -14,9 +14,36 @@ namespace leetCode._0201_0250
             if (s.Length == 1)
                 return Convert.ToInt32(s);
 
-
-            Stack<string> strings = new Stack<string>();
-
+            Stack<string> stack = new Stack<string>();
+            foreach (char item in s)
+            {
+                if (item == ' ')
+                {
+                    continue;
+                }
+                if (item == ')')
+                {
+                    while (stack.Peek() != "(")
+                    {
+                        var num1 = stack.Pop();
+                        var op = stack.Pop();
+                        var num2 = stack.Pop();
+                        int num = 0;
+                        if (op == "-")
+                        {
+                             num = int.Parse(num2) - int.Parse(num1);
+                        }
+                        else
+                        {
+                             num = int.Parse(num2) + int.Parse(num1);
+                        }
+                       
+                        stack.Push(num.ToString());
+                    }
+                    stack.Pop();
+                }
+               // stack.Push(item);
+            }
             return 0;
         }
     }
