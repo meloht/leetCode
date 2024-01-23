406a75f8930675745a6b905dbe8c571d4c093bfa
hongtao_luo <luohongtao621@hotmail.com>
2024-01-23 15:56:52 +0800

    543

---
+++ TestLeetCode/0501_0550/543_DiameterOfBinaryTreeUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0501_0550
+{
+    [TestClass]
+    public class _543_DiameterOfBinaryTreeUnitTest
+    {
+        _543_DiameterOfBinaryTreeAlg alg = new _543_DiameterOfBinaryTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 2, 3, 4, 5];
+            var tree = Utils.BuildTree(root);
+            int exp = 3;
+            int res = alg.DiameterOfBinaryTree(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1, 2];
+            var tree = Utils.BuildTree(root);
+            int exp = 1;
+            int res = alg.DiameterOfBinaryTree(tree);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/543_DiameterOfBinaryTreeAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _543_DiameterOfBinaryTreeAlg
+    {
+        private int max = 0;
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            Dfs(root, -1);
+            return max;
+        }
+        private int Dfs(TreeNode root, int depth)
+        {
+            if (root == null)
+                return depth;
+            depth++;
+            max = Math.Max(max, depth);
+
+            int left = Dfs(root.left, depth);
+            int right = Dfs(root.right, depth);
+
+            int n = left - depth + right - depth;
+            max = Math.Max(max, n);
+           
+            return Math.Max(left,right);
+
+        }
+    }
+}
