acbca5cc6ceaccb7b9174e83ad2908995a03eab7
hongtao_luo <luohongtao621@hotmail.com>
2024-01-23 12:38:31 +0800

    2765

---
+++ TestLeetCode/2751_2800/2765_LongestAlternatingSubarrayUnitTest.cs
@@ -0,0 +1,68 @@
+﻿using leetCode._2751_2800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2751_2800
+{
+    [TestClass]
+    public class _2765_LongestAlternatingSubarrayUnitTest
+    {
+        _2765_LongestAlternatingSubarrayAlg alg = new _2765_LongestAlternatingSubarrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 3, 4, 3, 4];
+            int exp = 4;
+            int res = alg.AlternatingSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [4, 5, 6];
+            int exp = 2;
+            int res = alg.AlternatingSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 29, 30, 5];
+            int exp = 2;
+            int res = alg.AlternatingSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [14, 30, 29, 49, 3, 23, 44, 21, 26, 52];
+            int exp = -1;
+            int res = alg.AlternatingSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] nums = [8, 4, 3, 2, 6, 4, 3, 5, 2, 8];
+            int exp = -1;
+            int res = alg.AlternatingSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            int[] nums = [8, 4, 3, 2, 6, 4, 3, 5, 2, 8];
+            int exp = -1;
+            int res = alg.AlternatingSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2751_2800/2765_LongestAlternatingSubarrayAlg.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2751_2800
+{
+    public class _2765_LongestAlternatingSubarrayAlg
+    {
+        public int AlternatingSubarray(int[] nums)
+        {
+            int pre = int.MaxValue;
+            int max = -1;
+            int count = -1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int prev = nums[i - 1];
+                int curr = nums[i];
+                int diff = curr - prev;
+                if (i == 1)
+                {
+                    if (diff == 1)
+                    {
+                        count = 2;
+                        max = Math.Max(max, count);
+                    }
+                }
+                else
+                {
+                    if (pre == 1)
+                    {
+                        if (diff == -1)
+                        {
+                            count++;
+                            max = Math.Max(max, count);
+                        }
+                        else if (diff == 1)
+                        {
+                            count = 2;
+                            max = Math.Max(max, count);
+                        }
+                        else
+                        {
+                            count = -1;
+                        }
+                    }
+                    else if (pre == -1)
+                    {
+                        if (count > 0 && diff == 1)
+                        {
+                            count++;
+                            max = Math.Max(max, count);
+                        }
+                        else if (diff == 1)
+                        {
+                            count = 2;
+                            max = Math.Max(max, count);
+                        }
+                        else
+                        {
+                            count = -1;
+                        }
+                    }
+                    else
+                    {
+                        if (diff == 1)
+                        {
+                            count = 2;
+                            max = Math.Max(max, count);
+                        }
+                        else
+                        {
+                            count = -1;
+                        }
+
+                    }
+                }
+
+                pre = diff;
+            }
+
+            return max;
+        }
+
+        public int AlternatingSubarray1(int[] nums)
+        {
+            int res = -1;
+            int n = nums.Length;
+            int firstIndex = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int length = i - firstIndex + 1;
+                if (nums[i] - nums[firstIndex] == (length - 1) % 2)
+                {
+                    res = Math.Max(res, length);
+                }
+                else
+                {
+                    if (nums[i] - nums[i - 1] == 1)
+                    {
+                        firstIndex = i - 1;
+                        res = Math.Max(res, 2);
+                    }
+                    else
+                    {
+                        firstIndex = i;
+                    }
+                }
+            }
+            return res;
+        }
+
+
+    }
+}
