e27f18ea712a7b188dbcaeb76adc0c6f912ff893
hongtao_luo <luohongtao621@hotmail.com>
2024-01-24 13:19:50 +0800

    438

--- leetCode/0401_0450/438_FindAllAnagramsInAStringAlg.cs
+++ leetCode/0401_0450/438_FindAllAnagramsInAStringAlg.cs
@@ -8,7 +8,7 @@ namespace leetCode._0401_0450
 {
     public class _438_FindAllAnagramsInAStringAlg
     {
-        public IList<int> FindAnagrams(string s, string p)
+        public IList<int> FindAnagrams1(string s, string p)
         {
             List<int> list = new List<int>();
             int n = s.Length;
@@ -42,5 +42,69 @@ namespace leetCode._0401_0450
 
             return list;
         }
+
+        public IList<int> FindAnagrams(string s, string p)
+        {
+            int sLen = s.Length, pLen = p.Length;
+
+            if (sLen < pLen)
+            {
+                return new List<int>();
+            }
+
+            IList<int> ans = new List<int>();
+            int[] count = new int[26];
+            for (int i = 0; i < pLen; ++i)
+            {
+                ++count[s[i] - 'a'];
+                --count[p[i] - 'a'];
+            }
+
+            int differ = 0;
+            for (int j = 0; j < 26; ++j)
+            {
+                if (count[j] != 0)
+                {
+                    ++differ;
+                }
+            }
+
+            if (differ == 0)
+            {
+                ans.Add(0);
+            }
+
+            for (int i = 0; i < sLen - pLen; ++i)
+            {
+                if (count[s[i] - 'a'] == 1)
+                {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从不同变得相同
+                    --differ;
+                }
+                else if (count[s[i] - 'a'] == 0)
+                {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从相同变得不同
+                    ++differ;
+                }
+                --count[s[i] - 'a'];
+
+                if (count[s[i + pLen] - 'a'] == -1)
+                {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从不同变得相同
+                    --differ;
+                }
+                else if (count[s[i + pLen] - 'a'] == 0)
+                {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从相同变得不同
+                    ++differ;
+                }
+                ++count[s[i + pLen] - 'a'];
+
+                if (differ == 0)
+                {
+                    ans.Add(i + 1);
+                }
+            }
+
+            return ans;
+        }
+
+
     }
 }
