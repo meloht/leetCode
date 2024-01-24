534931aa5d55722c608362f4f2d36ef8533df317
hongtao_luo <luohongtao621@hotmail.com>
2024-01-24 17:45:36 +0800

    2865

---
+++ TestLeetCode/2851_2900/2865_BeautifulTowersIUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using leetCode._2851_2900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2851_2900
+{
+    [TestClass]
+    public class _2865_BeautifulTowersIUnitTest
+    {
+        _2865_BeautifulTowersIAlg alg = new _2865_BeautifulTowersIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] maxHeights = [5, 3, 4, 1, 1];
+            long exp = 13;
+            var res = alg.MaximumSumOfHeights(maxHeights.ToList());
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] maxHeights = [6, 5, 3, 9, 2, 7];
+            long exp = 22;
+            var res = alg.MaximumSumOfHeights(maxHeights.ToList());
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] maxHeights = [3, 2, 5, 5, 2, 3];
+            long exp = 18;
+            var res = alg.MaximumSumOfHeights(maxHeights.ToList());
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] maxHeights = [1, 5, 2, 5, 6, 4, 6, 3, 4, 5];
+            long exp = 33;
+            var res = alg.MaximumSumOfHeights(maxHeights.ToList());
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2851_2900/2865_BeautifulTowersIAlg.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2851_2900
+{
+    public class _2865_BeautifulTowersIAlg
+    {
+        public long MaximumSumOfHeights(IList<int> maxHeights)
+        {
+            if (maxHeights.Count == 1)
+                return maxHeights[0];
+            long max = 0;
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < maxHeights.Count; i++)
+            {
+                int curr = maxHeights[i];
+                if (stack.Count > 0)
+                {
+                    if (stack.Peek() > curr)
+                    {
+                        stack.Clear();
+                        stack.Push(curr);
+                        if (i == 5)
+                        { 
+                        }
+                        long num = GetNum(i - 1, maxHeights);
+                        max = Math.Max(max, num);
+
+                    }
+                    else
+                    {
+                        stack.Push(curr);
+                    }
+                }
+                else
+                {
+                    stack.Push(curr);
+                }
+
+            }
+            if (stack.Count > 0)
+            {
+                long num = GetNum(maxHeights.Count - 1, maxHeights);
+                max = Math.Max(max, num);
+            }
+
+            return max;
+        }
+        private long GetNum(int peekIndex, IList<int> maxHeights)
+        {
+            long max = 0;
+            int prev = maxHeights[peekIndex];
+            max += prev;
+            for (int i = peekIndex - 1; i >= 0; i--)
+            {
+                int num = Math.Min(prev, maxHeights[i]);
+                max += num;
+                prev = num;
+            }
+            prev = maxHeights[peekIndex];
+            for (int i = peekIndex + 1; i < maxHeights.Count; i++)
+            {
+                int num = Math.Min(prev, maxHeights[i]);
+                max += num;
+                prev = num;
+            }
+
+            return max;
+        }
+    }
+}
