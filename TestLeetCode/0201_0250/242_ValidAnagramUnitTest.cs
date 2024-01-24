5d3fdb55051e79dd5d0f9312992cf169ae58a830
hongtao_luo <luohongtao621@hotmail.com>
2024-01-24 12:29:14 +0800

    242

---
+++ TestLeetCode/0201_0250/242_ValidAnagramUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _242_ValidAnagramUnitTest
+    {
+        _242_ValidAnagramAlg alg = new _242_ValidAnagramAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "anagram", t = "nagaram";
+            bool exp = true;
+            bool res = alg.IsAnagram(s, t);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "rat", t = "car";
+            bool exp = false;
+            bool res = alg.IsAnagram(s, t);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0201-0250/242_ValidAnagramAlg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _242_ValidAnagramAlg
+    {
+        public bool IsAnagram1(string s, string t)
+        {
+            int[] arr1 = GetArr(s);
+            int[] arr2 = GetArr(t);
+
+            return arr1.SequenceEqual(arr2);
+        }
+
+        private int[] GetArr(string s)
+        {
+            int[] arr = new int[26];
+            foreach (var item in s)
+            {
+                int ch = item - 'a';
+                arr[ch]++;
+            }
+            return arr;
+        }
+
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+            int[] arr = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                int ch1 = s[i] - 'a';
+                int ch2 = t[i] - 'a';
+                arr[ch1]++;
+                arr[ch2]--;
+
+            }
+
+            return arr.Count(p => p == 0) == 26;
+        }
+    }
+}
