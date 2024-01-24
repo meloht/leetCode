2713f031ba32c2cf65fe291116143cf2b98ff19d
hongtao_luo <luohongtao621@hotmail.com>
2024-01-24 13:40:06 +0800

    230

---
+++ TestLeetCode/0201_0250/230_KthSmallestElementInABstUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _230_KthSmallestElementInABstUnitTest
+    {
+        _230_KthSmallestElementInABstAlg alg = new _230_KthSmallestElementInABstAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [3, 1, 4, null, 2];
+            int k = 1;
+            var tree = Utils.BuildTree(root);
+            int res = alg.KthSmallest(tree, k);
+            int exp = 1;
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [5, 3, 6, 2, 4, null, null, 1];
+            int k = 3;
+            var tree = Utils.BuildTree(root);
+            int res = alg.KthSmallest(tree, k);
+            int exp = 3;
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0201-0250/230_KthSmallestElementInABstAlg.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _230_KthSmallestElementInABstAlg
+    {
+        int index = 0;
+        int K = 0;
+        int ans = -1;
+        public int KthSmallest1(TreeNode root, int k)
+        {
+            this.K = k;
+            Dfs(root);
+
+            return ans;
+        }
+        private void Dfs(TreeNode node)
+        {
+            if (node == null)
+                return;
+            Dfs(node.left);
+            index++;
+            if (index == K)
+            {
+                ans = node.val;
+                return;
+            }
+            Dfs(node.right);
+
+        }
+
+        public int KthSmallest(TreeNode root, int k)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            while (root != null || stack.Count > 0)
+            {
+                while (root != null)
+                {
+                    stack.Push(root);
+                    root = root.left;
+                }
+                root = stack.Pop();
+                --k;
+                if (k == 0)
+                {
+                    break;
+                }
+                root = root.right;
+            }
+            return root.val;
+        }
+    }
+}
