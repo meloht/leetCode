08270a4fec3adba8fb9aba176ad1767f5e163644
hongtao_luo <luohongtao621@hotmail.com>
2024-01-25 13:19:53 +0800

    437

---
+++ TestLeetCode/0401_0450/437_PathSumIIIUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0401_0450
+{
+    [TestClass]
+    public class _437_PathSumIIIUnitTest
+    {
+        _437_PathSumIIIAlg alg = new _437_PathSumIIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [10, 5, -3, 3, 2, null, 11, 3, -2, null, 1];
+            var tree = Utils.BuildTree(root);
+            int targetSum = 8;
+            int exp = 3;
+            int res = alg.PathSum(tree, targetSum);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1];
+            var tree = Utils.BuildTree(root);
+            int targetSum = 22;
+            int exp = 3;
+            int res = alg.PathSum(tree, targetSum);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/437_PathSumIIIAlg.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _437_PathSumIIIAlg
+    {
+        int count = 0;
+        int t = 0;
+        Dictionary<long, int> prefix = new Dictionary<long, int>();
+        public int PathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            t = targetSum;
+            prefix.Add(0, 1);
+            Dfs(root, root.val);
+            return count;
+        }
+
+        private void Dfs(TreeNode root,long val)
+        {
+            if (prefix.ContainsKey(val - t))
+            {
+                count += prefix[val - t];
+            }
+
+            if (prefix.ContainsKey(val))
+            {
+                prefix[val]++;
+            }
+            else
+            {
+                prefix.Add(val, 1);
+            }
+
+            if (root.left != null)
+            {
+                Dfs(root.left, val + root.left.val);
+            }
+            if (root.right != null)
+            {
+                Dfs(root.right, val + root.right.val);
+            }
+           
+            prefix[val]--;
+
+        }
+
+     
+
+        public int PathSum2(TreeNode root, int targetSum)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            Dfs1(root,targetSum);
+            return count;
+        }
+        private void Dfs1(TreeNode node, int targetSum)
+        {
+            if (node == null)
+            {
+                return ;
+            }
+            Dfs2(node, targetSum, node.val);
+            Dfs1(node.left,targetSum);
+            Dfs1(node.right, targetSum);
+        }
+        private void Dfs2(TreeNode node, int targetSum, long sum)
+        {
+            if (sum == targetSum)
+            {
+                count++;
+            }
+           
+            if (node.left != null)
+            {
+                Dfs2(node.left, targetSum, sum + node.left.val);
+            }
+            if (node.right != null)
+            {
+                Dfs2(node.right, targetSum, sum + node.right.val);
+            }
+           
+        }
+    }
+}
