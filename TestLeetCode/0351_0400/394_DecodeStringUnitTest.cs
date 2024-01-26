5359ac49c614ea60a6b4fae94a7e56071528a5d2
hongtao_luo <luohongtao621@hotmail.com>
2024-01-26 17:36:47 +0800

    394

---
+++ TestLeetCode/0351_0400/394_DecodeStringUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0351_0400
+{
+    [TestClass]
+    public class _394_DecodeStringUnitTest
+    {
+        _394_DecodeStringAlg alg = new _394_DecodeStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "3[a]2[bc]";
+            string exp = "aaabcbc";
+            var res = alg.DecodeString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "3[a2[c]]";
+            string exp = "accaccacc";
+            var res = alg.DecodeString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "2[abc]3[cd]ef";
+            string exp = "abcabccdcdcdef";
+            var res = alg.DecodeString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "abc3[cd]xyz";
+            string exp = "abccdcdcdxyz";
+            var res = alg.DecodeString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string s = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef";
+            string exp = "zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef";
+            var res = alg.DecodeString(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/394_DecodeStringAlg.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _394_DecodeStringAlg
+    {
+        public string DecodeString(string s)
+        {
+            if (s.Length == 1)
+                return s;
+
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            while (i < s.Length)
+            {
+                if (char.IsNumber(s[i]))
+                {
+                    var res = GetStr(s, i);
+                    i = res.Item2;
+                    sb.Append(res.Item1);
+
+                }
+                else if (s[i] != '[' && s[i] != ']')
+                {
+                    while (i < s.Length && !char.IsNumber(s[i]) && s[i] != '[' && s[i] != ']')
+                    {
+                        sb.Append(s[i]);
+                        i++;
+                    }
+                    continue;
+                }
+
+                i++;
+
+            }
+            return sb.ToString();
+        }
+
+        private void AddItem(StringBuilder sb, string item, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(item);
+            }
+        }
+
+        private Tuple<int, int> GetNum(string arr, int i)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (i < arr.Length && char.IsNumber(arr[i]))
+            {
+                sb.Append(arr[i]);
+                i++;
+            }
+
+            int num = int.Parse(sb.ToString());
+            return new Tuple<int, int>(num, i);
+        }
+        public Tuple<string, int> GetStr(string arr, int i)
+        {
+            StringBuilder sb = new StringBuilder();
+            int num = 1;
+            if (i < arr.Length && char.IsNumber(arr[i]))
+            {
+                var res = GetNum(arr, i);
+                num = res.Item1;
+                i = res.Item2;
+                i++;
+            }
+            
+            while (i < arr.Length && !char.IsNumber(arr[i]) && arr[i] != '[' && arr[i] != ']')
+            {
+                sb.Append(arr[i]);
+                i++;
+            }
+            if (i < arr.Length && char.IsNumber(arr[i]))
+            {
+                var res = GetStr(arr, i);
+                i = res.Item2;
+                sb.Append(res.Item1);
+            }
+
+            StringBuilder sb2 = new StringBuilder();
+            AddItem(sb2, sb.ToString(), num);
+            return new Tuple<string, int>(sb2.ToString(), i);
+        }
+
+    }
+}
