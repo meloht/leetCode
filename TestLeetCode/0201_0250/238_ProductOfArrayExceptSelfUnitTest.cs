5138640ea484e9499ded2f9ef19a3a6ce26fc07e
hongtao_luo <luohongtao621@hotmail.com>
2024-01-26 12:01:56 +0800

    238

---
+++ TestLeetCode/0201_0250/238_ProductOfArrayExceptSelfUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _238_ProductOfArrayExceptSelfUnitTest
+    {
+        _238_ProductOfArrayExceptSelfAlg alg = new _238_ProductOfArrayExceptSelfAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int[] exp = [24, 12, 8, 6];
+
+            int[] res = alg.ProductExceptSelf(nums);
+
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [-1, 1, 0, -3, 3];
+            int[] exp = [0, 0, 9, 0, 0];
+
+            int[] res = alg.ProductExceptSelf(nums);
+
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0201-0250/238_ProductOfArrayExceptSelfAlg.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _238_ProductOfArrayExceptSelfAlg
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int n = nums.Length;
+
+            int[] perfix = new int[n];
+            int[] suffix = new int[n];
+            perfix[0] = nums[0];
+            for (int i = 1; i < n; i++)
+            {
+                perfix[i] = nums[i] * perfix[i - 1];
+            }
+            suffix[n - 1] = nums[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+            {
+                suffix[i] = suffix[i + 1] * nums[i];
+            }
+            int[] result = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    result[i] = suffix[i + 1];
+                }
+                else if (i == n - 1)
+                {
+                    result[i] = perfix[i - 1];
+                }
+                else
+                {
+                    result[i] = perfix[i - 1] * suffix[i + 1];
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
