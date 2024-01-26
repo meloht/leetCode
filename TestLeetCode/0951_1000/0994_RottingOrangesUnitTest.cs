cdbd6c521ec08f0ef6995caa2c7837e49c5cc290
hongtao_luo <luohongtao621@hotmail.com>
2024-01-26 15:32:12 +0800

    994

---
+++ TestLeetCode/0951_1000/0994_RottingOrangesUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0951_1000
+{
+    [TestClass]
+    public class _0994_RottingOrangesUnitTest
+    {
+        _0994_RottingOrangesAlg alg = new _0994_RottingOrangesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[2, 1, 1], [1, 1, 0], [0, 1, 1]];
+            int exp = 4;
+            int res = alg.OrangesRotting(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[2, 1, 1], [0, 1, 1], [1, 0, 1]];
+            int exp = -1;
+            int res = alg.OrangesRotting(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] grid = [[0, 2]];
+            int exp = 0;
+            int res = alg.OrangesRotting(grid);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] grid = [[1], [2]];
+            int exp = 1;
+            int res = alg.OrangesRotting(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[][] grid = [[2, 2], [1, 1], [0, 0], [2, 0]];
+            int exp = 1;
+            int res = alg.OrangesRotting(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0994_RottingOrangesAlg.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0994_RottingOrangesAlg
+    {
+        public int OrangesRotting(int[][] grid)
+        {
+            Queue<int[]> queue = new Queue<int[]>();
+            int ans = 0;
+            List<int[]> list = new List<int[]>();
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 2)
+                    {
+                        AddQueue(grid, queue, i, j);
+                    }
+                    if (grid[i][j] == 1)
+                    {
+                        list.Add(new int[] { i, j });
+                    }
+                }
+            }
+            while (queue.Count > 0)
+            {
+                ans++;
+                int count = queue.Count;
+                List<int[]> ls = new List<int[]>();
+                for (int i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+                    grid[node[0]][node[1]] = 2;
+                    ls.Add(node);
+                  
+                }
+                foreach (var node in ls)
+                {
+                    AddQueue(grid, queue, node[0], node[1]);
+                }
+            }
+
+            foreach (var node in list)
+            {
+                if (grid[node[0]][node[1]] == 1)
+                {
+                    return -1;
+                }
+            }
+
+            return ans;
+        }
+        private void AddQueue(int[][] grid, Queue<int[]> queue, int i, int j)
+        {
+            if (i - 1 >= 0 && grid[i - 1][j] == 1)
+            {
+                queue.Enqueue(new int[] { i - 1, j });
+            }
+            if (j - 1 >= 0 && grid[i][j - 1] == 1)
+            {
+                queue.Enqueue(new int[] { i, j - 1 });
+            }
+            if (i + 1 < grid.Length && grid[i + 1][j] == 1)
+            {
+                queue.Enqueue(new int[] { i + 1, j });
+            }
+            if (j + 1 < grid[0].Length && grid[i][j + 1] == 1)
+            {
+                queue.Enqueue(new int[] { i, j + 1 });
+            }
+
+        }
+
+       
+    }
+}
