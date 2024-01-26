31256b00e62ffd9a9508208eaf89ddb655935675
hongtao_luo <luohongtao621@hotmail.com>
2024-01-26 14:07:40 +0800

    240

---
+++ TestLeetCode/0201_0250/240_SearchA2dMatrixIIUnitTest.cs
@@ -0,0 +1,63 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _240_SearchA2dMatrixIIUnitTest
+    {
+        _240_SearchA2dMatrixIIAlg alg = new _240_SearchA2dMatrixIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] matrix = [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]];
+            bool bl = alg.SearchMatrix(matrix, 5);
+            Assert.AreEqual(bl, true);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] matrix = [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]];
+            bool bl = alg.SearchMatrix(matrix, 20);
+            Assert.AreEqual(bl, false);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[][] matrix = [[-5]];
+            bool bl = alg.SearchMatrix(matrix, -10);
+            Assert.AreEqual(bl, false);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            int[][] matrix = [[1, 1]];
+            bool bl = alg.SearchMatrix(matrix, 2);
+            Assert.AreEqual(bl, false);
+        }
+
+        [TestMethod]
+        public void TestCase07()
+        {
+            int[][] matrix = [[1, 1]];
+            bool bl = alg.SearchMatrix(matrix, 0);
+            Assert.AreEqual(bl, false);
+        }
+
+        [TestMethod]
+        public void TestCase08()
+        {
+            int[][] matrix = [[1, 3, 5, 7, 9], [2, 4, 6, 8, 10], [11, 13, 15, 17, 19], [12, 14, 16, 18, 20], [21, 22, 23, 24, 25]];
+            bool bl = alg.SearchMatrix(matrix, 13);
+            Assert.AreEqual(bl, true);
+        }
+    }
+}
---
+++ leetCode/0201-0250/240_SearchA2dMatrixIIAlg.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _240_SearchA2dMatrixIIAlg
+    {
+        private int t = 0;
+        private int[][] mat;
+
+        public bool SearchMatrix1(int[][] matrix, int target)
+        {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            mat = matrix;
+            t = target;
+
+            if (m > n)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    var row = GetBinarySearch(i, m, true);
+                    if (row.Item1)
+                        return true;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    var row = GetBinarySearch(i, n, false);
+                    if (row.Item1)
+                        return true;
+                }
+            }
+           
+
+            return false;
+        }
+        private int GetArr(int i, int j, bool isCol)
+        {
+            if (isCol)
+            {
+                return mat[i][j];// col
+            }
+            else
+            {
+                return mat[j][i];//row
+            }
+
+        }
+        private Tuple<bool, int> GetBinarySearch(int col, int len, bool isCol)
+        {
+            int low = 0;
+            int high = len - 1;
+            while (low <= high)
+            {
+                int mid = (high - low) / 2 + low;
+                int val = GetArr(mid, col, isCol);
+                if (t > val)
+                {
+                    if (low == high)
+                    {
+                        return new Tuple<bool, int>(false, low);
+                    }
+                    if (mid == high)
+                    {
+                        return new Tuple<bool, int>(false, mid);
+                    }
+                    low = mid + 1;
+
+                }
+                else if (t < val)
+                {
+                    if (low == high)
+                    {
+                        int i = low - 1;
+                        if (i < 0)
+                        {
+                            i = 0;
+                        }
+                        return new Tuple<bool, int>(false, i);
+                    }
+                    if (mid == 0)
+                    {
+                        return new Tuple<bool, int>(false, 0);
+                    }
+                    high = mid - 1;
+
+
+                }
+                else
+                {
+                    return new Tuple<bool, int>(true, mid);
+                }
+            }
+
+            return new Tuple<bool, int>(false, Math.Min(high, low));
+        }
+
+
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            int m = matrix.Length, n = matrix[0].Length;
+            int x = 0, y = n - 1;
+            while (x < m && y >= 0)
+            {
+                if (matrix[x][y] == target)
+                {
+                    return true;
+                }
+                if (matrix[x][y] > target)
+                {
+                    --y;
+                }
+                else
+                {
+                    ++x;
+                }
+            }
+            return false;
+        }
+
+    }
+}
