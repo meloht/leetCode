f17184cac2e353e06d19b1a05ed40338ad129da0
hongtao luo <luohongtao621@163.com>
2024-01-27 14:09:24 +0800

    739

---
+++ TestLeetCode/0701_0750/739_DailyTemperaturesUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0701_0750
+{
+    [TestClass]
+    public class _739_DailyTemperaturesUnitTest
+    {
+        _739_DailyTemperaturesAlg alg = new _739_DailyTemperaturesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] temperatures = [73, 74, 75, 71, 69, 72, 76, 73];
+            int[] exp = [1, 1, 4, 2, 1, 1, 0, 0];
+            int[] res = alg.DailyTemperatures(temperatures);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] temperatures = [30, 40, 50, 60];
+            int[] exp = [1, 1, 1, 0];
+            int[] res = alg.DailyTemperatures(temperatures);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] temperatures = [30, 60, 90];
+            int[] exp = [1, 1, 0];
+            int[] res = alg.DailyTemperatures(temperatures);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0701_0750/739_DailyTemperaturesAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _739_DailyTemperaturesAlg
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            return null;
+        }
+    }
+}
