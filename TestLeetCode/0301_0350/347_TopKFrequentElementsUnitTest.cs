e93c5de5c08cb7e7d079adb75308c3b6f48206c5
hongtao luo <luohongtao621@163.com>
2024-01-27 15:04:14 +0800

    347

---
+++ TestLeetCode/0301_0350/347_TopKFrequentElementsUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0301_0350
+{
+    [TestClass]
+    public class _347_TopKFrequentElementsUnitTest
+    {
+        _347_TopKFrequentElementsAlg alg = new _347_TopKFrequentElementsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 1, 1, 2, 2, 3];
+            int k = 2;
+            int[] exp = [1, 2];
+            int[] res = alg.TopKFrequent(nums, k);
+
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1];
+            int k = 1;
+            int[] exp = [1];
+            int[] res = alg.TopKFrequent(nums, k);
+
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0301_0350/347_TopKFrequentElementsAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _347_TopKFrequentElementsAlg
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            List<int> result = new List<int>();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (int i in nums) 
+            {
+                if (dict.ContainsKey(i))
+                {
+                    dict[i]++;
+                }
+                else
+                {
+                    dict.Add(i, 1);
+                }
+            }
+
+            PriorityQueue<int,int> queue = new PriorityQueue<int,int>(Comparer<int>.Create((a, b) => b - a));
+            foreach (var item in dict)
+            {
+                queue.Enqueue(item.Key, item.Value);
+            }
+            for (int i = 0; i < k; i++)
+            {
+                result.Add(queue.Dequeue());
+            }
+            return result.ToArray();
+        }
+    }
+}
