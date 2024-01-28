d5760c28e20c164a22b68288a0a7a36c4de2795a
hongtao luo <luohongtao621@163.com>
2024-01-28 15:54:03 +0800

    287

---
+++ TestLeetCode/0251_0300/287_FindTheDuplicateNumberUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0251_0300
+{
+    [TestClass]
+    public class _287_FindTheDuplicateNumberUnitTest
+    {
+        _287_FindTheDuplicateNumberAlg alg = new _287_FindTheDuplicateNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 4, 2, 2];
+            int exp = 2;
+            int res = alg.FindDuplicate(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [3, 1, 3, 4, 2];
+            int exp = 3;
+            int res = alg.FindDuplicate(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [2, 2, 2, 2, 2];
+            int exp = 2;
+            int res = alg.FindDuplicate(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0251_0300/287_FindTheDuplicateNumberAlg.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _287_FindTheDuplicateNumberAlg
+    {
+        public int FindDuplicate1(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int prev = nums[i - 1];
+                int curr = nums[i];
+                if (prev == curr)
+                    return prev;
+            }
+            return 0;
+        }
+
+        public int FindDuplicate3(int[] nums)
+        {
+            int left = 1;
+            int right = nums.Length - 1;
+            while (left < right)
+            {
+                int mid = (right - left) / 2 + left;
+                int count = nums.Count(p => p <= mid);
+                if (count <= mid)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+
+            }
+            return left;
+        }
+
+        public int FindDuplicate(int[] nums)
+        {
+            int left = 1;
+            int right = nums.Length - 1;
+            int res = -1;
+            while (left <= right)
+            {
+                int mid = (right - left) / 2 + left;
+
+                res = mid;
+                int count = nums.Count(p => p <= mid);
+                if (count <= mid)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+
+            }
+            return left;
+        }
+
+        public int FindDuplicate2(int[] nums)
+        {
+            int slow = 0;
+            int fast = 0;
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            while (slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+            slow = 0;
+            while (slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+            return slow;
+        }
+    }
+}
