77d01899df80157d4809ef381f16d4788a9dbeff
hongtao luo <luohongtao621@163.com>
2024-01-28 13:37:19 +0800

    295

--- leetCode/0251_0300/295_FindMedianFromDataStreamAlg.cs
+++ leetCode/0251_0300/295_FindMedianFromDataStreamAlg.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,11 @@ namespace leetCode._0251_0300
     {
         public class MedianFinder
         {
+            PriorityQueue<int, int> queueLow = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b - a));
+            PriorityQueue<int, int> queueHigh = new PriorityQueue<int, int>();
+
+            private bool isCalc;
+            private double db = 0;
 
             public MedianFinder()
             {
@@ -18,13 +24,82 @@ namespace leetCode._0251_0300
 
             public void AddNum(int num)
             {
+                isCalc = false;
+                if (queueLow.Count == 0)
+                {
+                    queueLow.Enqueue(num, num);
+                }
+                else
+                {
+                    if (queueHigh.Count == 0)
+                    {
+                        if (num >= queueLow.Peek())
+                        {
+                            queueHigh.Enqueue(num, num);
+                        }
+                        else
+                        {
+                            queueLow.Enqueue(num, num);
+                            int nn = queueLow.Dequeue();
+                            queueHigh.Enqueue(nn, nn);
+                        }
+                    }
+                    else
+                    {
+                        if (queueLow.Count == queueHigh.Count)
+                        {
+                            if (queueLow.Peek() >= num)
+                            {
+                                queueLow.Enqueue(num, num);
+                            }
+                            else
+                            {
+                                queueHigh.Enqueue(num, num);
+                                int nn = queueHigh.Dequeue();
+                                queueLow.Enqueue(nn, nn);
+                            }
+                        }
+                        else
+                        {
+                            if (queueLow.Peek() >= num)
+                            {
+                                queueLow.Enqueue(num, num);
+                                int nn = queueLow.Dequeue();
+                                queueHigh.Enqueue(nn, nn);
+                            }
+                            else
+                            {
+                                queueHigh.Enqueue(num, num);
+                            }
+                        }
+                    }
+                }
 
             }
 
             public double FindMedian()
             {
-                return 0;
+                if (isCalc)
+                {
+                    return db;
+                }
+                isCalc = true;
+                if (queueHigh.Count != queueLow.Count)
+                {
+                    db = queueLow.Peek();
+                }
+                else
+                {
+                    double db1 = queueLow.Peek();
+                    double db2 = queueHigh.Peek();
+                    db = (db1 + db2) / 2;
+                }
+                return db;
             }
+
         }
+
+
+
     }
 }
--- leetCode/Program.cs
+++ leetCode/Program.cs
@@ -9,11 +9,27 @@ namespace leetCode
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            int r = 9; // 选择的元素个数
-            int n = numbers.Length;
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int r = 9; // 选择的元素个数
+            //int n = numbers.Length;
 
-            GenerateCombinations(numbers, n, r);
+            //GenerateCombinations(numbers, n, r);
+
+            PriorityQueue<int> priorityQueue = new PriorityQueue<int>();
+
+            // Enqueue some elements
+            priorityQueue.Enqueue(3);
+            priorityQueue.Enqueue(1);
+            priorityQueue.Enqueue(4);
+            priorityQueue.Enqueue(1);
+            priorityQueue.Enqueue(5);
+            priorityQueue.Enqueue(9);
+
+            // Dequeue elements (they will be in ascending order due to the min heap)
+            while (priorityQueue.Count > 0)
+            {
+                Console.WriteLine(priorityQueue.Dequeue());
+            }
         }
         static int Factorial(int n)
         {
@@ -48,6 +64,96 @@ namespace leetCode
             {
                 data[index] = arr[i];
                 GenerateCombinationsUtil(arr, data, i + 1, end, index + 1, r);
+            }
+        }
+
+
+        class PriorityQueue<T> where T : IComparable<T>
+        {
+            private List<T> heap;
+
+            public PriorityQueue()
+            {
+                heap = new List<T>();
+            }
+
+            public int Count
+            {
+                get { return heap.Count; }
+            }
+
+            public void Enqueue(T item)
+            {
+                heap.Add(item);
+                HeapifyUp(heap.Count - 1);
+            }
+
+            public T Dequeue()
+            {
+                if (heap.Count == 0)
+                    throw new InvalidOperationException("Queue is empty");
+
+                T root = heap[0];
+                heap[0] = heap[heap.Count - 1];
+                heap.RemoveAt(heap.Count - 1);
+
+                if (heap.Count > 1)
+                    HeapifyDown(0);
+
+                return root;
+            }
+
+            private void HeapifyUp(int index)
+            {
+                while (index > 0)
+                {
+                    int parent = (index - 1) / 2;
+
+                    if (heap[index].CompareTo(heap[parent]) < 0)
+                    {
+                        Swap(index, parent);
+                        index = parent;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            private void HeapifyDown(int index)
+            {
+                int leftChild, rightChild, smallest;
+
+                while (true)
+                {
+                    leftChild = 2 * index + 1;
+                    rightChild = 2 * index + 2;
+                    smallest = index;
+
+                    if (leftChild < heap.Count && heap[leftChild].CompareTo(heap[smallest]) < 0)
+                        smallest = leftChild;
+
+                    if (rightChild < heap.Count && heap[rightChild].CompareTo(heap[smallest]) < 0)
+                        smallest = rightChild;
+
+                    if (smallest != index)
+                    {
+                        Swap(index, smallest);
+                        index = smallest;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            private void Swap(int i, int j)
+            {
+                T temp = heap[i];
+                heap[i] = heap[j];
+                heap[j] = temp;
             }
         }
     }
