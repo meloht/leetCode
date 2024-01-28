e8f76f8ecc9a4d1046ba9b1f270897ae78e7c8bf
hongtao luo <luohongtao621@163.com>
2024-01-28 12:07:40 +0800

    295

---
+++ TestLeetCode/0251_0300/295_FindMedianFromDataStreamUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0251_0300._295_FindMedianFromDataStreamAlg;
+
+namespace TestLeetCode._0251_0300
+{
+    [TestClass]
+    public class _295_FindMedianFromDataStreamUnitTest
+    {
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            _295_FindMedianFromDataStreamAlg.MedianFinder medianFinder = new _295_FindMedianFromDataStreamAlg.MedianFinder();
+
+            medianFinder.AddNum(1);    // arr = [1]
+            medianFinder.AddNum(2);    // arr = [1, 2]
+            Assert.AreEqual(1.5d, medianFinder.FindMedian()); // 返回 1.5 ((1 + 2) / 2)
+            medianFinder.AddNum(3);    // arr[1, 2, 3]
+            Assert.AreEqual(2.0d, medianFinder.FindMedian()); // return 2.0
+        }
+    }
+}
---
+++ leetCode/0251_0300/295_FindMedianFromDataStreamAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _295_FindMedianFromDataStreamAlg
+    {
+        public class MedianFinder
+        {
+
+            public MedianFinder()
+            {
+
+            }
+
+            public void AddNum(int num)
+            {
+
+            }
+
+            public double FindMedian()
+            {
+                return 0;
+            }
+        }
+    }
+}
