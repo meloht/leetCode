ca1084de2acc20df126ba331ee2401eb73b8d14a
hongtao luo <luohongtao621@163.com>
2024-01-28 18:27:40 +0800

    416

---
+++ TestLeetCode/0401_0450/416_PartitionEqualSubsetSumUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0401_0450
+{
+    [TestClass]
+    public class _416_PartitionEqualSubsetSumUnitTest
+    {
+        _416_PartitionEqualSubsetSumAlg alg = new _416_PartitionEqualSubsetSumAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 5, 11, 5];
+            bool exp = true;
+            bool res = alg.CanPartition(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 5];
+            bool exp = false;
+            bool res = alg.CanPartition(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [2, 2, 1, 1];
+            bool exp = true;
+            bool res = alg.CanPartition(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/416_PartitionEqualSubsetSumAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _416_PartitionEqualSubsetSumAlg
+    {
+
+        public bool CanPartition(int[] nums)
+        {
+            if (nums.Length < 2)
+                return false;
+            int t = nums.Sum();
+            if (t % 2 > 0)
+                return false;
+            int num = t / 2;
+
+            int[] used = new int[nums.Length];
+
+            bool bl = Dfs(num, used, nums);
+
+            return bl;
+        }
+        private bool Dfs(int sum, int[] used, int[] nums)
+        {
+            if (sum == 0)
+                return true;
+
+            if (sum < 0)
+                return false;
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (used[j] == 1)
+                {
+                    continue;
+                }
+                used[j] = 1;
+                int rr = sum - nums[j];
+                if (rr < 0)
+                    break;
+                bool bl = Dfs(rr, used, nums);
+                used[j] = 0;
+                if (bl)
+                    return true;
+
+            }
+
+            return false;
+        }
+    }
+}
