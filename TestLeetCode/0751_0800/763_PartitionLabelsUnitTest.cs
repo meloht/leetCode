a21f98e5e6e8a05d5b9ebe9c2c4f5200ae8d02e5
hongtao luo <luohongtao621@163.com>
2024-01-28 14:45:20 +0800

    763

---
+++ TestLeetCode/0751_0800/763_PartitionLabelsUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0751_0800
+{
+    [TestClass]
+    public class _763_PartitionLabelsUnitTest
+    {
+        _763_PartitionLabelsAlg alg = new _763_PartitionLabelsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "ababcbacadefegdehijhklij";
+            int[] exp = [9, 7, 8];
+            int[] res = alg.PartitionLabels(s).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "eccbbbbdec";
+            int[] exp = [10];
+            int[] res = alg.PartitionLabels(s).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0751_0800/763_PartitionLabelsAlg.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _763_PartitionLabelsAlg
+    {
+        public IList<int> PartitionLabels1(string s)
+        {
+            List<int> res = new List<int>();
+            int i = 0;
+            int count = 0;
+            HashSet<char> set = new HashSet<char>();
+            while (i < s.Length)
+            {
+                count++;
+                var ch = s[i];
+                if (!set.Contains(ch))
+                {
+                    set.Add(ch);
+
+                }
+
+                bool bl = true;
+                for (int k = i + 1; k < s.Length; k++)
+                {
+                    var ch2 = s[k];
+                    if (set.Contains(ch2))
+                    {
+                        bl = false;
+                        break;
+                    }
+
+                }
+                if (bl)
+                {
+                    res.Add(count);
+                    set.Clear();
+                    count = 0;
+                }
+
+                i++;
+
+            }
+            return res;
+        }
+
+        public IList<int> PartitionLabels(string s)
+        {
+            List<int> res = new List<int>();
+
+            int[] arr = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                int index = s[i] - 'a';
+                arr[index] = i;
+            }
+            int start = 0;
+            int end = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int index = s[i] - 'a';
+                end = Math.Max(end, arr[index]);
+                if (end == i)
+                {
+                    res.Add(end - start + 1);
+                    start = end + 1;
+                }
+
+            }
+
+            return res;
+        }
+    }
+}
