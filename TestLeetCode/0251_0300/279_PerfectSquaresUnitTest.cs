3ff857872c115d499cd34f9b206a646c85e4fdd9
hongtao luo <luohongtao621@163.com>
2024-01-28 15:03:01 +0800

    279

---
+++ TestLeetCode/0251_0300/279_PerfectSquaresUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0251_0300
+{
+    [TestClass]
+    public class _279_PerfectSquaresUnitTest
+    {
+        _279_PerfectSquaresAlg alg = new _279_PerfectSquaresAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 12;
+            int exp = 3;
+            int res = alg.NumSquares(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 13;
+            int exp = 2;
+            int res = alg.NumSquares(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0251_0300/279_PerfectSquaresAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _279_PerfectSquaresAlg
+    {
+        public int NumSquares(int n)
+        {
+
+            return 0;
+        }
+    }
+}
