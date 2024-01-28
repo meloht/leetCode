a8205e26d277154437cf0de47f0054bb20f8befb
hongtao luo <luohongtao621@163.com>
2024-01-28 11:02:28 +0800

    239

---
+++ TestLeetCode/0201_0250/239_SlidingWindowMaximumUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0201_0250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0201_0250
+{
+    [TestClass]
+    public class _239_SlidingWindowMaximumUnitTest
+    {
+        _239_SlidingWindowMaximumAlg alg = new _239_SlidingWindowMaximumAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, -1, -3, 5, 3, 6, 7];
+            int k = 3;
+            int[] exp = [3, 3, 5, 5, 6, 7];
+            int[] res = alg.MaxSlidingWindow(nums, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1];
+            int k = 1;
+            int[] exp = [1];
+            int[] res = alg.MaxSlidingWindow(nums, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [-7, -8, 7, 5, 7, 1, 6, 0];
+            int k = 4;
+            int[] exp = [7, 7, 7, 7, 7];
+            int[] res = alg.MaxSlidingWindow(nums, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0201-0250/239_SlidingWindowMaximumAlg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0201_0250
+{
+    public class _239_SlidingWindowMaximumAlg
+    {
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            int n = nums.Length;
+            int m = n - k + 1;
+            int[] res = new int[m];
+            SortedList<int,int> set = new SortedList<int,int>(Comparer<int>.Create((x, y) => y - x));
+            int len = k - 1;
+            for (int i = 0; i < len; i++)
+            {
+                if (set.ContainsKey(nums[i]))
+                {
+                    set[nums[i]]++;
+                }
+                else
+                {
+                    set.Add(nums[i],1);
+                }
+              
+            }
+            for (int i = len, j = 0; i < n; i++, j++)
+            {
+                if (set.ContainsKey(nums[i]))
+                {
+                    set[nums[i]]++;
+                }
+                else
+                {
+                    set.Add(nums[i], 1);
+                }
+                foreach (var item in set)
+                {
+                    res[j] = item.Key;
+                    break;
+                }
+               
+                if (set.ContainsKey(nums[j]))
+                {
+                    set[nums[j]]--;
+                    if (set[nums[j]] == 0)
+                    {
+                        set.Remove(nums[j]);
+                    }
+                }
+               
+            }
+            return res;
+        }
+    }
+}
