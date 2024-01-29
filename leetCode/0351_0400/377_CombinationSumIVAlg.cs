1f969346ac2d1b10463ffd8545cb5191ba775bb5
hongtao_luo <luohongtao621@hotmail.com>
2024-01-29 14:49:59 +0800

    377

--- leetCode/0351_0400/377_CombinationSumIVAlg.cs
+++ leetCode/0351_0400/377_CombinationSumIVAlg.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,51 @@ namespace leetCode._0351_0400
     {
         public int CombinationSum4(int[] nums, int target)
         {
-            return 0;
+            int[] dp = new int[target + 1];
+            dp[0] = 1;
+            for (int i = 0; i <= target; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i >= nums[j])
+                    {
+                        dp[i] += dp[i - nums[j]];
+                    }
+                   
+                }
+            }
+
+            return dp[target];
         }
+
+        public int CombinationSum41(int[] nums, int target)
+        {
+            int[] dp = new int[target + 1];
+            Array.Fill(dp, -1);
+            int ans = Dfs(target, nums, dp);
+            return ans;
+        }
+
+        public int Dfs(int sum, int[] nums, int[] dp)
+        {
+            if (sum == 0)
+                return 1;
+            if (sum < 0)
+                return 0;
+            if (dp[sum] != -1)
+                return dp[sum];
+
+            int ans = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans += Dfs(sum - nums[i], nums, dp);
+            }
+            if (dp[sum] == -1)
+            {
+                dp[sum] = ans;
+            }
+            return ans;
+        }
+
     }
 }
