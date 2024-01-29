a6d5fcb430c5cc5d88f214092f74200112027bc2
hongtao_luo <luohongtao621@hotmail.com>
2024-01-29 15:08:20 +0800

    474

---
+++ TestLeetCode/0451_0500/474_OnesAndZeroesUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0451_0500
+{
+    [TestClass]
+    public class _474_OnesAndZeroesUnitTest
+    {
+        _474_OnesAndZeroesAlg alg = new _474_OnesAndZeroesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] strs = ["10", "0001", "111001", "1", "0"];
+            int m = 5, n = 3;
+            int exp = 4;
+            int res = alg.FindMaxForm(strs, m, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] strs = ["10", "0", "1"];
+            int m = 1, n = 1;
+            int exp = 2;
+            int res = alg.FindMaxForm(strs, m, n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/474_OnesAndZeroesAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _474_OnesAndZeroesAlg
+    {
+        public int FindMaxForm(string[] strs, int m, int n)
+        {
+
+            return 0;
+        }
+    }
+}
