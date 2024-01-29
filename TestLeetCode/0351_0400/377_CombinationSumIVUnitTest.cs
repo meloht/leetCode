0d81ba473177269ba751e40eddbd420e7b3014c6
hongtao_luo <luohongtao621@hotmail.com>
2024-01-29 12:33:17 +0800

    377

---
+++ TestLeetCode/0351_0400/377_CombinationSumIVUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0351_0400
+{
+    [TestClass]
+    public class _377_CombinationSumIVUnitTest
+    {
+        _377_CombinationSumIVAlg alg = new _377_CombinationSumIVAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3];
+            int target = 4;
+            int exp = 7;
+            int res = alg.CombinationSum4(nums, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [9];
+            int target = 3;
+            int exp = 0;
+            int res = alg.CombinationSum4(nums, target);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/377_CombinationSumIVAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _377_CombinationSumIVAlg
+    {
+        public int CombinationSum4(int[] nums, int target)
+        {
+            return 0;
+        }
+    }
+}
