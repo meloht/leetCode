2e79d51cc89f84f9a0211195a1a2b84a32c61b85
hongtao_luo <luohongtao621@hotmail.com>
2024-01-29 18:01:56 +0800

    474

--- TestLeetCode/0451_0500/474_OnesAndZeroesUnitTest.cs
+++ TestLeetCode/0451_0500/474_OnesAndZeroesUnitTest.cs
@@ -31,5 +31,37 @@ namespace TestLeetCode._0451_0500
             int res = alg.FindMaxForm(strs, m, n);
             Assert.AreEqual(exp, res);
         }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] strs = ["10", "0001", "111001", "1", "0"];
+            int m = 3, n = 4;
+            int exp = 3;
+            int res = alg.FindMaxForm(strs, m, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string[] strs = ["0", "11", "1000", "01", "0", "101", "1", "1", "1", "0", "0", "0", "0", "1", "0", "0110101", "0", "11", "01", "00", "01111", "0011", "1", "1000", "0", "11101", "1", "0", "10", "0111"];
+            int m = 9, n = 80;
+            int exp = 17;
+            int res = alg.FindMaxForm(strs, m, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string[] strs = ["011", "1", "11", "0", "010", "1", "10", "1", "1", "0", "0",
+                "0", "01111", "011", "11", "00", "11", "10", "1", "0", "0", "0", "0", "101", "001110", "1", "0", "1", "0", "0", "10", "00100", "0", "10", "1", "1", "1", "011", "11", "11", "10", "10",
+                "0000", "01", "1", "10", "0"];
+            int m = 44, n = 39;
+            int exp = 45;
+            int res = alg.FindMaxForm(strs, m, n);
+            Assert.AreEqual(exp, res);
+        }
     }
 }
--- leetCode/0451_0500/474_OnesAndZeroesAlg.cs
+++ leetCode/0451_0500/474_OnesAndZeroesAlg.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,59 @@ namespace leetCode._0451_0500
 {
     public class _474_OnesAndZeroesAlg
     {
+        int[,,] dp;
+        int Num0 = 0;
+        int Num1 = 0;
+        string[] Arr;
         public int FindMaxForm(string[] strs, int m, int n)
         {
-
-            return 0;
+            this.Num0 = m;
+            this.Num1 = n;
+            this.Arr = strs;
+            dp = new int[m + 1, n + 1, strs.Length];
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int k = 0; k < strs.Length; k++)
+                    {
+                        dp[i, j, k] = -1;
+                    }
+                }
+            }
+            int ans = Dfs(0, 0, 0, 0);
+            return ans;
         }
+
+        private int Dfs(int num0, int num1, int index, int count)
+        {
+            if (num1 > this.Num1 || num0 > this.Num0)
+                return 0;
+
+            if (index >= this.Arr.Length)
+            {
+                return count;
+            }
+
+            if (dp[num0, num1, index] != -1)
+                return dp[num0, num1, index];
+
+
+            int skip = Dfs(num0, num1, index + 1, count);
+
+            var str = this.Arr[index];
+
+            int n0 = str.Count(p => p == '0');
+            int n1 = str.Length - n0;
+
+            int choose = Dfs(num0 + n0, num1 + n1, index + 1, count + 1);
+
+            int ans = Math.Max(choose, skip);
+
+            dp[num0, num1, index] = ans;
+            return ans;
+        }
+
+
     }
 }
