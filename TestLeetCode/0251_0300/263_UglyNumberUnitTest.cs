322ebdb3019655321246d811d4a703672a944803
hongtao_luo <luohongtao621@hotmail.com>
2024-01-30 13:19:44 +0800

    263

---
+++ TestLeetCode/0251_0300/263_UglyNumberUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0251_0300
+{
+    [TestClass]
+    public class _263_UglyNumberUnitTest
+    {
+        _263_UglyNumberAlg alg = new _263_UglyNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 6;
+            bool exp = true;
+            var res = alg.IsUgly(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1;
+            bool exp = true;
+            var res = alg.IsUgly(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 14;
+            bool exp = false;
+            var res = alg.IsUgly(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0251_0300/263_UglyNumberAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _263_UglyNumberAlg
+    {
+        public bool IsUgly(int n)
+        {
+
+            if (n <= 0)
+                return false;
+            while (n % 2 == 0)
+            {
+                n /= 2;
+            }
+            while (n % 3 == 0)
+            {
+                n /= 3;
+            }
+            while (n % 5 == 0)
+            {
+                n /= 5;
+            }
+            return n == 1;
+        }
+
+
+    }
+}
