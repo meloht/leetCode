a47f04bcdc2639f2433e35d2a8eb847395bbe316
hongtao_luo <luohongtao621@hotmail.com>
2024-01-30 13:31:49 +0800

    2140

---
+++ TestLeetCode/2101_2150/2140_SolvingQuestionsWithBrainpowerUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2101_2150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2101_2150
+{
+    [TestClass]
+    public class _2140_SolvingQuestionsWithBrainpowerUnitTest
+    {
+        _2140_SolvingQuestionsWithBrainpowerAlg alg = new _2140_SolvingQuestionsWithBrainpowerAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] questions = [[3, 2], [4, 3], [4, 4], [2, 5]];
+            long exp = 5;
+            long res = alg.MostPoints(questions);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] questions = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]];
+            long exp = 7;
+            long res = alg.MostPoints(questions);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2101_2150/2140_SolvingQuestionsWithBrainpowerAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2101_2150
+{
+    public class _2140_SolvingQuestionsWithBrainpowerAlg
+    {
+        public long MostPoints(int[][] questions)
+        {
+            return 0;
+        }
+    }
+}
