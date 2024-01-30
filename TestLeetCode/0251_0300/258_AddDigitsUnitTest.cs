311ac87e8a9702f899fb3378a9a3c0f4fdc2c4c3
hongtao_luo <luohongtao621@hotmail.com>
2024-01-30 12:33:42 +0800

    258

---
+++ TestLeetCode/0251_0300/258_AddDigitsUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0251_0300
+{
+    [TestClass]
+    public class _258_AddDigitsUnitTest
+    {
+        _258_AddDigitsAlg alg = new _258_AddDigitsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int num = 38;
+            int exp = 2;
+            int res = alg.AddDigits(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int num = 0;
+            int exp = 0;
+            int res = alg.AddDigits(num);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0251_0300/258_AddDigitsAlg.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _258_AddDigitsAlg
+    {
+        public int AddDigits1(int num)
+        {
+            string s = num.ToString();
+            if (s.Length == 1)
+                return num;
+
+
+            int n = num;
+            while (s.Length > 1)
+            {
+                n = s.Sum(p => p - '0');
+                s = n.ToString();
+
+            }
+
+            return n;
+        }
+
+        public int AddDigits2(int num) 
+        {
+            while (num >= 10)
+            {
+                int n = 0;
+                while (num > 0)
+                {
+                    n += num % 10;
+                    num /= 10;
+                }
+                num = n;
+            }
+            return num;
+        }
+
+        public int AddDigits(int num)
+        {
+            return (num - 1) % 9 + 1;
+        }
+    }
+}
