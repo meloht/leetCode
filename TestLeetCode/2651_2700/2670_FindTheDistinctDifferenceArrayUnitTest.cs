c5d2a24632a390def4cc7ea540465b2b6cfd7ce6
hongtao_luo <luohongtao621@hotmail.com>
2024-01-31 12:51:45 +0800

    2670

---
+++ TestLeetCode/2651_2700/2670_FindTheDistinctDifferenceArrayUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2651_2700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2651_2700
+{
+    [TestClass]
+    public class _2670_FindTheDistinctDifferenceArrayUnitTest
+    {
+        _2670_FindTheDistinctDifferenceArrayAlg alg = new _2670_FindTheDistinctDifferenceArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 4, 5];
+            int[] exp = [-3, -1, 1, 3, 5];
+            var res = alg.DistinctDifferenceArray(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [3, 2, 3, 4, 2];
+            int[] exp = [-2, -1, 0, 2, 3];
+            var res = alg.DistinctDifferenceArray(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2651_2700/2670_FindTheDistinctDifferenceArrayAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2651_2700
+{
+    public class _2670_FindTheDistinctDifferenceArrayAlg
+    {
+        public int[] DistinctDifferenceArray(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+
+            int[] suffix = new int[nums.Length];
+            HashSet<int> set = new HashSet<int>();
+            int len = nums.Length - 1;
+            for (int i = len; i >= 1; i--)
+            {
+                set.Add(nums[i]);
+                suffix[i - 1] = set.Count;
+            }
+            set.Clear();
+
+            for (int i = 0; i <= len; i++)
+            {
+                set.Add(nums[i]);
+                res[i] = set.Count - suffix[i];
+            }
+            return res;
+        }
+    }
+}
