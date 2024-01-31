21d971784ddee8fc00a5bb1967a5b925fd63f57d
hongtao luo <luohongtao621@163.com>
2024-01-31 21:13:30 +0800

    1512

---
+++ TestLeetCode/1501_1550/1512_NumberOfGoodPairsUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._1501_1550
+{
+    [TestClass]
+    public class _1512_NumberOfGoodPairsUnitTest
+    {
+        _1512_NumberOfGoodPairsAlg alg = new _1512_NumberOfGoodPairsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 1, 1, 3];
+            int exp = 4;
+            int res = alg.NumIdenticalPairs(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1, 1, 1];
+            int exp = 6;
+            int res = alg.NumIdenticalPairs(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 2, 3];
+            int exp = 0;
+            int res = alg.NumIdenticalPairs(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1501_1550/1512_NumberOfGoodPairsAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1501_1550
+{
+    public class _1512_NumberOfGoodPairsAlg
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int ans = 0;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                if (dict.ContainsKey(num))
+                {
+                    ans += dict[num];
+                    dict[num]++;
+                }
+                else
+                {
+                    dict.Add(num, 1);
+                }
+
+            }
+            return ans;
+        }
+    }
+}
