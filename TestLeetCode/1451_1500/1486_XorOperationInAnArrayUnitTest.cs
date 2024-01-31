bb7b1aa8ac393580ce8ea854e623b5bfd7cf1a0d
hongtao luo <luohongtao621@163.com>
2024-01-31 20:53:25 +0800

    1486

---
+++ TestLeetCode/1451_1500/1486_XorOperationInAnArrayUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using leetCode._1451_1500;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._1451_1500
+{
+    [TestClass]
+    public class _1486_XorOperationInAnArrayUnitTest
+    {
+        _1486_XorOperationInAnArrayAlg alg = new _1486_XorOperationInAnArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 5, start = 0;
+            int exp = 8;
+            int res = alg.XorOperation(n, start);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 4, start = 3;
+            int exp = 8;
+            int res = alg.XorOperation(n, start);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 1, start = 7;
+            int exp = 7;
+            int res = alg.XorOperation(n, start);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int n = 10, start = 5;
+            int exp = 2;
+            int res = alg.XorOperation(n, start);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1451_1500/1486_XorOperationInAnArrayAlg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1451_1500
+{
+    public class _1486_XorOperationInAnArrayAlg
+    {
+        public int XorOperation(int n, int start)
+        {
+            int ans = start;
+            for (int i = 1; i < n; i++)
+            {
+                ans = ans ^ (start + 2 * i);
+            }
+            return ans;
+        }
+    }
+}
