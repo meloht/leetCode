e6b0d290063ca76185b854131b306b92e2be3327
hongtao_luo <luohongtao621@hotmail.com>
2024-01-31 13:27:04 +0800

    326

---
+++ TestLeetCode/0301_0350/326_PowerOfThreeUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0301_0350
+{
+    [TestClass]
+    public class _326_PowerOfThreeUnitTest
+    {
+        _326_PowerOfThreeAlg alg = new _326_PowerOfThreeAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            bool exp = true;
+            bool res = alg.IsPowerOfThree(27);
+            Assert.AreEqual(exp,res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            bool exp = false;
+            bool res = alg.IsPowerOfThree(0);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            bool exp = true;
+            bool res = alg.IsPowerOfThree(9);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            bool exp = false;
+            bool res = alg.IsPowerOfThree(45);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0301_0350/326_PowerOfThreeAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _326_PowerOfThreeAlg
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            while (n > 0 && n % 3 == 0)
+            {
+                n /= 3;
+            }
+            bool bl = n == 1;
+            return bl;
+        }
+        /// <summary>
+        /// 32 位有符号整数的范围内，最大的 3 的幂为 3^19=1162261467 
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public bool IsPowerOfThree2(int n)
+        {
+            return n > 0 && 1162261467 % n == 0;
+        }
+
+
+    }
+}
