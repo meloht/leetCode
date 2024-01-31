e1fb68a65a6030de11f4436039a754b93e24917a
hongtao_luo <luohongtao621@hotmail.com>
2024-01-31 14:04:04 +0800

    2413

---
+++ TestLeetCode/2401_2450/2413_SmallestEvenMultipleUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._2401_2450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2401_2450
+{
+    [TestClass]
+    public class _2413_SmallestEvenMultipleUnitTest
+    {
+        _2413_SmallestEvenMultipleAlg alg = new _2413_SmallestEvenMultipleAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int exp = 10;
+            int res = alg.SmallestEvenMultiple(5);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int exp = 6;
+            int res = alg.SmallestEvenMultiple(6);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2401_2450/2413_SmallestEvenMultipleAlg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2401_2450
+{
+    public class _2413_SmallestEvenMultipleAlg
+    {
+        public int SmallestEvenMultiple(int n)
+        {
+            if (n % 2 == 0)
+                return n;
+            return n * 2;
+           
+        }
+       
+    }
+}
