7b1e9c4943aade1d79b2cfd7e3afa25736244a9c
hongtao luo <luohongtao621@163.com>
2024-01-31 20:35:51 +0800

    2236

---
+++ TestLeetCode/2201_2250/2236_RootEqualsSumOfChildrenUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2201_2250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2201_2250
+{
+    [TestClass]
+    public class _2236_RootEqualsSumOfChildrenUnitTest
+    {
+        _2236_RootEqualsSumOfChildrenAlg alg = new _2236_RootEqualsSumOfChildrenAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [10, 4, 6];
+            var tree = Utils.BuildTree(root);
+            bool exp = true;
+            bool res = alg.CheckTree(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [5, 3, 1];
+            var tree = Utils.BuildTree(root);
+            bool exp = false;
+            var res = alg.CheckTree(tree);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2201_2250/2236_RootEqualsSumOfChildrenAlg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2201_2250
+{
+    public class _2236_RootEqualsSumOfChildrenAlg
+    {
+        public bool CheckTree(TreeNode root)
+        {
+            if (root.val == root.left.val + root.right.val)
+                return true;
+            return false;
+
+        }
+    }
+}
