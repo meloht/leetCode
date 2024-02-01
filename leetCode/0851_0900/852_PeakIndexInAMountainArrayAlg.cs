a47926f981412a3c83463a78ac08543ca333b21b
hongtao_luo <luohongtao621@hotmail.com>
2024-02-01 15:12:37 +0800

    852

--- leetCode/0851_0900/852_PeakIndexInAMountainArrayAlg.cs
+++ leetCode/0851_0900/852_PeakIndexInAMountainArrayAlg.cs
@@ -9,7 +9,7 @@ namespace leetCode._0851_0900
 {
     public class _852_PeakIndexInAMountainArrayAlg
     {
-        public int PeakIndexInMountainArray(int[] arr)
+        public int PeakIndexInMountainArray1(int[] arr)
         {
             int left = 0;
             int right = arr.Length - 1;
@@ -31,5 +31,45 @@ namespace leetCode._0851_0900
             }
             return -1;
         }
+
+        public int PeakIndexInMountainArray2(int[] arr)
+        {
+            int left = 1;
+            int right = arr.Length - 1;
+            while (left < right)
+            {
+                int mid = (right - left + 1) / 2 + left;
+                if (arr[mid] > arr[mid - 1])
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return right;
+        }
+
+        public int PeakIndexInMountainArray(int[] arr)
+        {
+            int n = arr.Length;
+            int left = 1, right = n - 2, ans = 0;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (arr[mid] > arr[mid + 1])
+                {
+                    ans = mid;
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return ans;
+        }
+
     }
 }
