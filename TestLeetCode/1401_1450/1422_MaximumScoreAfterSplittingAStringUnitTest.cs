c3acd40d4e239060ae47f28f112aa4e8f8faa93d
hongtao_luo <luohongtao621@hotmail.com>
2024-02-01 13:37:39 +0800

    1422

---
+++ TestLeetCode/1401_1450/1422_MaximumScoreAfterSplittingAStringUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._1401_1450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._1401_1450
+{
+    [TestClass]
+    public class _1422_MaximumScoreAfterSplittingAStringUnitTest
+    {
+        _1422_MaximumScoreAfterSplittingAStringAlg alg = new _1422_MaximumScoreAfterSplittingAStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "011101";
+            int exp = 5;
+            int res = alg.MaxScore(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "00111";
+            int exp = 5;
+            int res = alg.MaxScore(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "1111";
+            int exp = 3;
+            int res = alg.MaxScore(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "00";
+            int exp = 1;
+            int res = alg.MaxScore(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1401_1450/1422_MaximumScoreAfterSplittingAStringAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1401_1450
+{
+    public class _1422_MaximumScoreAfterSplittingAStringAlg
+    {
+        public int MaxScore(string s)
+        {
+            int[] suffix = new int[s.Length];
+            int n = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == '1')
+                {
+                    n++;
+                }
+                suffix[i] = n;
+            }
+            n = 0;
+            int ans = 0;
+            int len = s.Length - 1;
+            for (int i = 0; i < len; i++)
+            {
+                if (s[i] == '0')
+                {
+                    n++;
+                }
+                ans = Math.Max(ans, suffix[i + 1] + n);
+            }
+            return ans;
+        }
+    }
+}
