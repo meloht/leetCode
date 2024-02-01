23375c70aa6e618c0e8525db1ef9ae415daa2fdf
hongtao_luo <luohongtao621@hotmail.com>
2024-02-01 10:09:25 +0800

    1534

---
+++ TestLeetCode/1501_1550/1534_CountGoodTripletsUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._1501_1550
+{
+    [TestClass]
+    public class _1534_CountGoodTripletsUnitTest
+    {
+        _1534_CountGoodTripletsAlg alg = new _1534_CountGoodTripletsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [3, 0, 1, 1, 9, 7];
+            int a = 7, b = 2, c = 3;
+            int exp = 4;
+            int res = alg.CountGoodTriplets(arr, a, b, c);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [1, 1, 2, 2, 3];
+            int a = 0, b = 0, c = 1;
+            int exp = 0;
+            int res = alg.CountGoodTriplets(arr, a, b, c);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1501_1550/1534_CountGoodTripletsAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1501_1550
+{
+    public class _1534_CountGoodTripletsAlg
+    {
+        public int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            int ans = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int ii = arr[i];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int jj = arr[j];
+                    if (Math.Abs(ii - jj) > a)
+                    {
+                        continue;
+                    }
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+                        int kk = arr[k];
+                        if (Math.Abs(jj - kk) <= b && Math.Abs(ii - kk) <= c)
+                        {
+                            ans++;
+                        }
+                    }
+                }
+
+            }
+            return ans;
+        }
+    }
+}
