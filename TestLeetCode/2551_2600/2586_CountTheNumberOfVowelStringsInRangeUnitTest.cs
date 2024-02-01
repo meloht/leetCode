caa1b5f0580f8c75a7928b9d3286615a8c519b92
hongtao_luo <luohongtao621@hotmail.com>
2024-02-01 13:57:43 +0800

    2586

---
+++ TestLeetCode/2551_2600/2586_CountTheNumberOfVowelStringsInRangeUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2551_2600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._2551_2600
+{
+    [TestClass]
+    public class _2586_CountTheNumberOfVowelStringsInRangeUnitTest
+    {
+        _2586_CountTheNumberOfVowelStringsInRangeAlg alg = new _2586_CountTheNumberOfVowelStringsInRangeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["are", "amy", "u"];
+            int left = 0, right = 2;
+            int exp = 2;
+            int res = alg.VowelStrings(words, left, right);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["hey", "aeo", "mu", "ooo", "artro"];
+            int left = 1, right = 4;
+            int exp = 3;
+            int res = alg.VowelStrings(words, left, right);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2551_2600/2586_CountTheNumberOfVowelStringsInRangeAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2551_2600
+{
+    public class _2586_CountTheNumberOfVowelStringsInRangeAlg
+    {
+        public int VowelStrings(string[] words, int left, int right)
+        {
+            int ans = 0;
+            for (int i = left; i < words.Length && i <= right; i++)
+            {
+                var word = words[i];
+                if (IsVowel(word[0]) && IsVowel(word[word.Length - 1]))
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+        private bool IsVowel(char ch)
+        {
+            switch (ch)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    return true;
+                default:
+                    return false;
+            }
+
+        }
+    }
+}
