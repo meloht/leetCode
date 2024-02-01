8b79772adefce6dc8799f8bf4fdf8ddb99c4b0db
hongtao_luo <luohongtao621@hotmail.com>
2024-02-01 12:48:20 +0800

    867

---
+++ TestLeetCode/0851_0900/867_TransposeMatrixUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0851_0900
+{
+    [TestClass]
+    public class _867_TransposeMatrixUnitTest
+    {
+        _867_TransposeMatrixAlg alg = new _867_TransposeMatrixAlg();
+        
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+            int[][] exp = [[1, 4, 7], [2, 5, 8], [3, 6, 9]];
+            int[][] res = alg.Transpose(matrix);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] matrix = [[1, 2, 3], [4, 5, 6]];
+            int[][] exp = [[1, 4], [2, 5], [3, 6]];
+            int[][] res = alg.Transpose(matrix);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+    }
+}
---
+++ leetCode/0851_0900/867_TransposeMatrixAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _867_TransposeMatrixAlg
+    {
+        public int[][] Transpose(int[][] matrix)
+        {
+            return null;
+        }
+    }
+}
