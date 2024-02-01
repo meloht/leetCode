f79bbf1e0fd3abb052bd8d93fd4f0f11a54dc62c
hongtao_luo <luohongtao621@hotmail.com>
2024-02-01 15:06:55 +0800

    852

---
+++ TestLeetCode/0851_0900/852_PeakIndexInAMountainArrayUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode._0851_0900
+{
+    [TestClass]
+    public class _852_PeakIndexInAMountainArrayUnitTest
+    {
+        _852_PeakIndexInAMountainArrayAlg alg = new _852_PeakIndexInAMountainArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [0, 1, 0];
+            int exp = 1;
+            int res = alg.PeakIndexInMountainArray(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [0, 2, 1, 0];
+            int exp = 1;
+            int res = alg.PeakIndexInMountainArray(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] arr = [0, 10, 5, 2];
+            int exp = 1;
+            int res = alg.PeakIndexInMountainArray(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] arr = [3, 5, 3, 2, 0];
+            int exp = 1;
+            int res = alg.PeakIndexInMountainArray(arr);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/852_PeakIndexInAMountainArrayAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _852_PeakIndexInAMountainArrayAlg
+    {
+        public int PeakIndexInMountainArray(int[] arr)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            while (left <= right)
+            {
+                int mid = (right - left) / 2 + left;
+                if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+                {
+                    return mid;
+                }
+                else if (arr[mid] < arr[mid + 1])
+                {
+                    left = mid;
+                }
+                else if (arr[mid] < arr[mid - 1])
+                {
+                    right = mid;
+                }
+            }
+            return -1;
+        }
+    }
+}
