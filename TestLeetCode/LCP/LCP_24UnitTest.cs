26d7cfd9e6b1de5bdf901eee8b91b67b6d7ce447
hongtao_luo <luohongtao621@hotmail.com>
2024-02-01 15:56:12 +0800

    24

---
+++ TestLeetCode/LCP/LCP_24UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode.LCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLeetCode.LCP
+{
+    [TestClass]
+    public class LCP_24UnitTest
+    {
+        LCP_24 alg = new LCP_24();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 4, 5, 1, 6, 7];
+            int[] exp = [0, 0, 0, 5, 6, 7];
+            int[] res = alg.NumsGame(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4, 5];
+            int[] exp = [0, 0, 0, 0, 0];
+            int[] res = alg.NumsGame(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 1, 1, 2, 3, 4];
+            int[] exp = [0, 1, 2, 3, 3, 3];
+            int[] res = alg.NumsGame(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/LCP/LCP_24.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode.LCP
+{
+    public class LCP_24
+    {
+        public int[] NumsGame(int[] nums)
+        {
+            return null;
+        }
+    }
+}
