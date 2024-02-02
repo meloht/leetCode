180eb725d12d4374ee83868cb25e583ae1691c43
hongtao_luo <luohongtao621@hotmail.com>
2024-02-02 13:27:26 +0800

    290

---
+++ TestLeetCode/0251_0300/290_WordPatternUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0251_0300
+{
+    [TestClass]
+    public class _290_WordPatternUnitTest
+    {
+        _290_WordPatternAlg alg = new _290_WordPatternAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string pattern = "abba", s = "dog cat cat dog";
+            bool exp = true;
+            bool res = alg.WordPattern(pattern, s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string pattern = "abba", s = "dog cat cat fish";
+            bool exp = false;
+            bool res = alg.WordPattern(pattern, s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string pattern = "aaaa", s = "dog cat cat dog";
+            bool exp = false;
+            bool res = alg.WordPattern(pattern, s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string pattern = "abba", s = "dog dog dog dog";
+            bool exp = false;
+            bool res = alg.WordPattern(pattern, s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0251_0300/290_WordPatternAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _290_WordPatternAlg
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length != pattern.Length)
+                return false;
+
+            string[] map = new string[26];
+
+            Dictionary<string, char> dict = new Dictionary<string, char>();
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                int j = pattern[i] - 'a';
+                if (!dict.ContainsKey(words[i]) && string.IsNullOrEmpty(map[j]))
+                {
+                    map[j] = words[i];
+                    dict[words[i]] = pattern[i];
+                }
+                else
+                {
+                    if (map[j] != words[i])
+                        return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
