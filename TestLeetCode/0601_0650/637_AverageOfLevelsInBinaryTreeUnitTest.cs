f7efb20e5ec037874536af97673ed41b4e323c36
hongtao_luo <luohongtao621@hotmail.com>
2024-02-02 17:21:50 +0800

    637

---
+++ TestLeetCode/0601_0650/637_AverageOfLevelsInBinaryTreeUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _637_AverageOfLevelsInBinaryTreeUnitTest
+    {
+        _637_AverageOfLevelsInBinaryTreeAlg alg = new _637_AverageOfLevelsInBinaryTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [3, 9, 20, null, null, 15, 7];
+            double[] exp = [3.00000, 14.50000, 11.00000];
+            var tree = Utils.BuildTree(root);
+            double[] res = alg.AverageOfLevels(tree).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [3, 9, 20, 15, 7];
+            double[] exp = [3.00000, 14.50000, 11.00000];
+            var tree = Utils.BuildTree(root);
+            double[] res = alg.AverageOfLevels(tree).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0601_0650/637_AverageOfLevelsInBinaryTreeAlg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _637_AverageOfLevelsInBinaryTreeAlg
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            List<double> res=new List<double>();
+
+            if(root==null)
+                return res;
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while(queue.Count > 0) 
+            {
+                int count = queue.Count;
+                double avg = 0.0d;
+
+                for (int i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                    avg += node.val;
+                }
+                avg = Math.Round(avg / count, 5);
+                res.Add(avg);
+            }
+
+            return res;
+        }
+    }
+}
