327b4d10275413fb4faa25c50909a13fe5de5901
hongtao_luo <luohongtao621@hotmail.com>
2024-02-02 10:03:43 +0800

    1768

---
+++ TestLeetCode/1751_1800/1768_MergeStringsAlternatelyUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1751_1800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1751_1800
+{
+    [TestClass]
+    public class _1768_MergeStringsAlternatelyUnitTest
+    {
+        _1768_MergeStringsAlternatelyAlg alg = new _1768_MergeStringsAlternatelyAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string word1 = "abc", word2 = "pqr";
+            string exp = "apbqcr";
+            string res = alg.MergeAlternately(word1, word2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string word1 = "ab", word2 = "pqrs";
+            string exp = "apbqrs";
+            string res = alg.MergeAlternately(word1, word2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string word1 = "abcd", word2 = "pq";
+            string exp = "apbqcd";
+            string res = alg.MergeAlternately(word1, word2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1751_1800/1768_MergeStringsAlternatelyAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1751_1800
+{
+    public class _1768_MergeStringsAlternatelyAlg
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0, j = 0; i < word1.Length || j < word2.Length; i++, j++)
+            {
+                if (i < word1.Length)
+                {
+                    sb.Append(word1[i]);
+                }
+                if (j < word2.Length)
+                {
+                    sb.Append(word2[i]);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
