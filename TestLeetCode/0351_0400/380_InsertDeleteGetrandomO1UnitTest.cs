a9f9b89be2bfe8ed2725e5122d3592f2f8116f95
hongtao luo <luohongtao621@163.com>
2024-02-03 15:05:24 +0800

    380

---
+++ TestLeetCode/0351_0400/380_InsertDeleteGetrandomO1UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0351_0400._380_InsertDeleteGetrandomO1Alg;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _380_InsertDeleteGetrandomO1UnitTest
+    {
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            _380_InsertDeleteGetrandomO1Alg.RandomizedSet randomizedSet = new _380_InsertDeleteGetrandomO1Alg.RandomizedSet();
+
+            Assert.AreEqual(true, randomizedSet.Insert(1)); // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+            Assert.AreEqual(false, randomizedSet.Remove(2)); // 返回 false ，表示集合中不存在 2 。
+            Assert.AreEqual(true, randomizedSet.Insert(2)); // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+            randomizedSet.GetRandom(); // getRandom 应随机返回 1 或 2 。
+            Assert.AreEqual(true, randomizedSet.Remove(1)); // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+            Assert.AreEqual(false, randomizedSet.Insert(2)); // 2 已在集合中，所以返回 false 。
+            randomizedSet.GetRandom(); // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+        }
+    }
+}
---
+++ leetCode/0351_0400/380_InsertDeleteGetrandomO1Alg.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _380_InsertDeleteGetrandomO1Alg
+    {
+        public class RandomizedSet
+        {
+            Random random = new Random();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            List<int> list = new List<int>();
+            public RandomizedSet()
+            {
+
+            }
+
+            public bool Insert(int val)
+            {
+                if (dict.ContainsKey(val))
+                    return false;
+                dict.Add(val, list.Count);
+                list.Add(val);
+
+                return true;
+            }
+
+            public bool Remove(int val)
+            {
+                if (!dict.ContainsKey(val))
+                    return false;
+                int index = dict[val];
+                int last = list[list.Count - 1];
+                list[index] = last;
+                dict[last] = index;
+                dict.Remove(val);
+                list.RemoveAt(list.Count - 1);
+
+                return true;
+            }
+
+            public int GetRandom()
+            {
+                int index = random.Next(list.Count);
+
+                return list[index];
+            }
+        }
+    }
+}
