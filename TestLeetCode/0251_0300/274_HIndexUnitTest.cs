ffbd97a6751374f3c8d224733ea521cb5e734622
hongtao luo <luohongtao621@163.com>
2024-02-03 14:17:32 +0800

    274

---
+++ TestLeetCode/0251_0300/274_HIndexUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0251_0300
+{
+    [TestClass]
+    public class _274_HIndexUnitTest
+    {
+        _274_HIndexAlg alg = new _274_HIndexAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] citations = [3, 0, 6, 1, 5];
+            int exp = 3;
+            int res = alg.HIndex(citations);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] citations = [1, 3, 1];
+            int exp = 1;
+            int res = alg.HIndex(citations);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0251_0300/274_HIndexAlg.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _274_HIndexAlg
+    {
+        public int HIndex1(int[] citations)
+        {
+            int count = citations.Length;
+            Array.Sort(citations);
+            int max = 0;
+            for (int i = 0; i < citations.Length; i++)
+            {
+                int n = count - i;
+                if (n < max)
+                    return max;
+                int num = Math.Min(n, citations[i]);
+                max = Math.Max(max, num);
+
+            }
+            return max;
+        }
+
+        public int HIndex(int[] citations)
+        {
+            int count = citations.Length;
+            int[] countArr = new int[count + 1];
+            for (int i = 0; i < count; i++)
+            {
+                if (citations[i] >= count)
+                {
+                    countArr[count]++;
+                }
+                else
+                {
+                    countArr[citations[i]]++;
+                }
+            }
+            int num = 0;
+            for (int i = count; i >= 0; i--)
+            {
+                num += countArr[i];
+                if (num >= i)
+                    return i;
+            }
+
+            return 0;
+        }
+    }
+}
