c062a972e06d62dc4be102f60810be90315d47cb
hongtao luo <luohongtao621@163.com>
2024-02-03 20:23:15 +0800

    1690

---
+++ TestLeetCode/1651_1700/1690_StoneGameVIIUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._1651_1700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1651_1700
+{
+    [TestClass]
+    public class _1690_StoneGameVIIUnitTest
+    {
+        _1690_StoneGameVIIAlg alg = new _1690_StoneGameVIIAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] stones = [5, 3, 1, 4, 2];
+            int exp = 6;
+            int res = alg.StoneGameVII(stones);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] stones = [7, 90, 5, 1, 100, 10, 10, 2];
+            int exp = 122;
+            int res = alg.StoneGameVII(stones);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1651_1700/1690_StoneGameVIIAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1651_1700
+{
+    public class _1690_StoneGameVIIAlg
+    {
+        public int StoneGameVII(int[] stones)
+        {
+            return 0;
+        }
+    }
+}
