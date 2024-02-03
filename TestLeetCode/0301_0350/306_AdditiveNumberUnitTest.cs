3f278d002a5952add435507d13992c55437d3362
hongtao luo <luohongtao621@163.com>
2024-02-03 18:07:49 +0800

    306

---
+++ TestLeetCode/0301_0350/306_AdditiveNumberUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _306_AdditiveNumberUnitTest
+    {
+        _306_AdditiveNumberAlg alg = new _306_AdditiveNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string num = "112358";
+            bool exp = true;
+            bool res = alg.IsAdditiveNumber(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string num = "199100199";
+            bool exp = true;
+            bool res = alg.IsAdditiveNumber(num);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0301_0350/306_AdditiveNumberAlg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _306_AdditiveNumberAlg
+    {
+        public bool IsAdditiveNumber(string num)
+        {
+            string ss = Utils.AddStrings("1", "22");
+            return false;
+        }
+        private bool Dfs(string num,int i)
+        {
+
+            return false;
+        }
+    }
+}
