e4b7eefef92dd594dccb64b2103b1b4ade64b48c
hongtao_luo <luohongtao621@hotmail.com>
2024-02-04 13:27:04 +0800

    1502

---
+++ TestLeetCode/1501_1550/1502_CanMakeArithmeticProgressionFromequenceUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1501_1550
+{
+    [TestClass]
+    public class _1502_CanMakeArithmeticProgressionFromequenceUnitTest
+    {
+        _1502_CanMakeArithmeticProgressionFromequenceAlg alg = new _1502_CanMakeArithmeticProgressionFromequenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [3, 5, 1];
+            bool exp = true;
+            bool res = alg.CanMakeArithmeticProgression(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [1, 2, 4];
+            bool exp = false;
+            bool res = alg.CanMakeArithmeticProgression(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] arr = [0, 0, 1];
+            bool exp = false;
+            bool res = alg.CanMakeArithmeticProgression(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/1501_1550/1502_CanMakeArithmeticProgressionFromequenceAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1501_1550
+{
+    public class _1502_CanMakeArithmeticProgressionFromequenceAlg
+    {
+        public bool CanMakeArithmeticProgression(int[] arr)
+        {
+            Array.Sort(arr);
+            int diff = arr[1] - arr[0];
+            for (int i = 2; i < arr.Length; i++)
+            {
+                int prev = arr[i - 1];
+                int curr = arr[i];
+                int df= curr - prev;
+                if (df != diff)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
