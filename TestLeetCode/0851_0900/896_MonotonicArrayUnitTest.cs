75aed86f88fed775b8e990b8b9f8dcc49a13333f
hongtao_luo <luohongtao621@hotmail.com>
2024-02-04 16:53:14 +0800

    896

---
+++ TestLeetCode/0851_0900/896_MonotonicArrayUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _896_MonotonicArrayUnitTest
+    {
+        _896_MonotonicArrayAlg alg = new _896_MonotonicArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 2, 3];
+            bool exp = true;
+            bool res = alg.IsMonotonic(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [6, 5, 4, 4];
+            bool exp = true;
+            bool res = alg.IsMonotonic(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 3, 2];
+            bool exp = false;
+            bool res = alg.IsMonotonic(nums);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [2, 2, 2, 1, 4, 5];
+            bool exp = false;
+            bool res = alg.IsMonotonic(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/896_MonotonicArrayAlg.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _896_MonotonicArrayAlg
+    {
+        public bool IsMonotonic1(int[] nums)
+        {
+            if (nums.Length <= 1)
+                return true;
+            int num = 0;
+            if (nums[1] > nums[0])
+            {
+                num = 1;
+            }
+            else if (nums[1] < nums[0])
+            {
+                num = -1;
+            }
+            else
+            {
+                num = 0;
+            }
+            for (int i = 2; i < nums.Length; i++)
+            {
+                int prev = nums[i - 1];
+                int curr = nums[i];
+                if (num == 1)
+                {
+                    if (curr < prev)
+                        return false;
+                }
+                else if (num == -1)
+                {
+                    if (curr > prev)
+                        return false;
+                }
+                else
+                {
+                    if (curr > prev)
+                    {
+                        num = 1;
+                    }
+                    else if (curr < prev)
+                    {
+                        num = -1;
+                    }
+                    else
+                    {
+                        num = 0;
+                    }
+
+                }
+            }
+            return true;
+        }
+
+        public bool IsMonotonic(int[] nums)
+        {
+            if (nums.Length <= 1)
+                return true;
+
+            int n = nums.Length;
+            bool bl = false;
+            bool flag = false;
+            for (int i = 1; i < n; i++)
+            {
+                int prev = nums[i - 1];
+                int curr = nums[i];
+                if (flag == false)
+                {
+                    if (curr > prev)
+                    {
+                        bl = true;
+                        flag = true;
+                    }
+                    else if (curr < prev)
+                    {
+                        bl = false;
+                        flag = true;
+                    }
+                }
+                else
+                {
+                    if (bl)
+                    {
+                        if (curr < prev)
+                            return false;
+                    }
+                    else
+                    {
+                        if (curr > prev)
+                            return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
