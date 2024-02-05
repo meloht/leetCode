b0a66e3c46b6453ff21babcb6a0dbb97131a4f02
hongtao_luo <luohongtao621@hotmail.com>
2024-02-05 12:28:58 +0800

    392

---
+++ TestLeetCode/0351_0400/392_IsSubsequenceUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _392_IsSubsequenceUnitTest
+    {
+        _392_IsSubsequenceAlg alg = new _392_IsSubsequenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abc", t = "ahbgdc";
+            bool exp = true;
+            bool res = alg.IsSubsequence(s, t);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "axc", t = "ahbgdc";
+            bool exp = false;
+            bool res = alg.IsSubsequence(s, t);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "aaaaaa", t = "bbaaaa";
+            bool exp = false;
+            bool res = alg.IsSubsequence(s, t);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/392_IsSubsequenceAlg.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _392_IsSubsequenceAlg
+    {
+        public bool IsSubsequence1(string s, string t)
+        {
+            int j = 0;
+            int count = 0;
+            foreach (char item in s)
+            {
+                while (j < t.Length)
+                {
+                    if (item == t[j])
+                    {
+                        count++;
+                        j++;
+                        break;
+                    }
+                    j++;
+                }
+            }
+            return count == s.Length;
+        }
+
+        public bool IsSubsequence(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] dp = new int[m + 1, 26];
+            for (int i = 0; i < 26; i++)
+            {
+                dp[m, i] = m;
+            }
+            for (int i = m - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    if (t[i] == j + 'a')
+                    {
+                        dp[i, j] = i;
+                    }
+                    else
+                    {
+                        dp[i, j] = dp[i + 1, j];
+                    }
+                }
+            }
+            int add = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (dp[add, s[i] - 'a'] == m)
+                {
+                    return false;
+                }
+                add = dp[add, s[i] - 'a'] + 1;
+            }
+            return true;
+        }
+    }
+}
