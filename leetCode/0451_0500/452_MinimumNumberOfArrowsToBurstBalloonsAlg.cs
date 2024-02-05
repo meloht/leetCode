e7855fde76e71ffb916b94a0c7ad810ce6c86214
hongtao_luo <luohongtao621@hotmail.com>
2024-02-05 15:32:31 +0800

    452

--- leetCode/0451_0500/452_MinimumNumberOfArrowsToBurstBalloonsAlg.cs
+++ leetCode/0451_0500/452_MinimumNumberOfArrowsToBurstBalloonsAlg.cs
@@ -8,7 +8,7 @@ namespace leetCode._0451_0500
 {
     public class _452_MinimumNumberOfArrowsToBurstBalloonsAlg
     {
-        public int FindMinArrowShots(int[][] points)
+        public int FindMinArrowShots1(int[][] points)
         {
             var arr = points.OrderBy(p => p[0]).ToArray();
 
@@ -23,9 +23,12 @@ namespace leetCode._0451_0500
                 {
                     int begin2 = arr[j][0];
                     int end2 = arr[j][1];
+
                     next = j;
                     if (end >= begin2)
                     {
+                        begin = Math.Max(begin, begin2);
+                        end = Math.Min(end, end2);
                         count++;
                     }
                     else
@@ -41,10 +44,27 @@ namespace leetCode._0451_0500
                 {
                     i++;
                 }
-              
+
             }
 
             int ans = points.Length - count;
+            return ans;
+        }
+
+        public int FindMinArrowShots(int[][] points)
+        {
+            Array.Sort(points, (x, y) => x[1].CompareTo(y[1]));
+
+            int ans = 1;
+            int end = points[0][1];
+            foreach (var item in points)
+            {
+                if (end < item[0])
+                {
+                    end = item[1];
+                    ans++;
+                }
+            }
             return ans;
         }
     }
