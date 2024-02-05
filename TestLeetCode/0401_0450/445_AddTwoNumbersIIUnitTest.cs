f6d7a6f9ebac304d78d428f90ad4c271a65bbffe
hongtao_luo <luohongtao621@hotmail.com>
2024-02-05 16:03:07 +0800

    445

---
+++ TestLeetCode/0401_0450/445_AddTwoNumbersIIUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _445_AddTwoNumbersIIUnitTest
+    {
+        _445_AddTwoNumbersIIAlg alg = new _445_AddTwoNumbersIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] l1 = [7, 2, 4, 3], l2 = [5, 6, 4];
+            var ll1 = Utils.BuildListNode(l1);
+            var ll2 = Utils.BuildListNode(l2);
+            var res = alg.AddTwoNumbers(ll1, ll2);
+            int[] exp = [7, 8, 0, 7];
+            int[] resu = Utils.GetLinkNodeList(res);
+            Assert.IsTrue(exp.SequenceEqual(resu));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] l1 = [2, 4, 3], l2 = [5, 6, 4];
+            var ll1 = Utils.BuildListNode(l1);
+            var ll2 = Utils.BuildListNode(l2);
+            var res = alg.AddTwoNumbers(ll1, ll2);
+            int[] exp = [8, 0, 7];
+            int[] resu = Utils.GetLinkNodeList(res);
+            Assert.IsTrue(exp.SequenceEqual(resu));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] l1 = [0], l2 = [0];
+            var ll1 = Utils.BuildListNode(l1);
+            var ll2 = Utils.BuildListNode(l2);
+            var res = alg.AddTwoNumbers(ll1, ll2);
+            int[] exp = [0];
+            int[] resu = Utils.GetLinkNodeList(res);
+            Assert.IsTrue(exp.SequenceEqual(resu));
+        }
+    }
+}
---
+++ leetCode/0401_0450/445_AddTwoNumbersIIAlg.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _445_AddTwoNumbersIIAlg
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var l1r = Reverse(l1);
+            var l2r = Reverse(l2);
+
+            ListNode dummy = new ListNode(-1);
+            ListNode node3 = dummy;
+            int next = 0;
+            ListNode node1 = l1r;
+            ListNode node2 = l2r;
+            while (node1 != null || node2 != null || next > 0)
+            {
+                int num = 0;
+                if (node1 != null)
+                {
+                    num += node1.val;
+                }
+                if (node2 != null)
+                {
+                    num += node2.val;
+                }
+                if (next >0)
+                {
+                    num += next;
+                }
+                if (num > 9)
+                {
+                    next = 1;
+                    num = num % 10;
+                }
+                else
+                {
+                    next = 0;
+                }
+                node3.next = new ListNode(num);
+                node3 = node3.next;
+
+                if (node1 != null)
+                {
+                    node1 = node1.next;
+                }
+                if (node2 != null)
+                {
+                    node2 = node2.next;
+                }
+
+            }
+            var ans = Reverse(dummy.next);
+            return ans;
+        }
+
+        private ListNode Reverse(ListNode head)
+        {
+            ListNode dummy = new ListNode(-1);
+            while (head != null)
+            {
+                ListNode p = head;
+                head = head.next;
+                p.next = dummy.next;
+                dummy.next = p;
+            }
+            return dummy.next;
+        }
+    }
+}
