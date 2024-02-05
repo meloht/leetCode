2f58cd3e98b8f709c3b039d47102d30ff5a2d1ce
hongtao_luo <luohongtao621@hotmail.com>
2024-02-05 13:25:46 +0800

    289

---
+++ TestLeetCode/0251_0300/289_GameOfLifeUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0251_0300
+{
+    [TestClass]
+    public class _289_GameOfLifeUnitTest
+    {
+        _289_GameOfLifeAlg alg = new _289_GameOfLifeAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] board = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]];
+            int[][] exp = [[0, 0, 0], [1, 0, 1], [0, 1, 1], [0, 1, 0]];
+            alg.GameOfLife(board);
+            bool bl = Utils.IsSameArr(exp, board);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] board = [[1, 1], [1, 0]];
+            int[][] exp = [[1, 1], [1, 1]];
+            alg.GameOfLife(board);
+            bool bl = Utils.IsSameArr(exp, board);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0251_0300/289_GameOfLifeAlg.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _289_GameOfLifeAlg
+    {
+        public void GameOfLife(int[][] board)
+        {
+            int n = board.Length;
+            int m = board[0].Length;
+            int[,] op = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 }, { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 } };
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+
+                    int life = 0;
+                    int dead = 0;
+                    for (int k = 0; k < 8; k++)
+                    {
+                        int ii = i + op[k, 0];
+                        int jj = j + op[k, 1];
+                        if (ii >= 0 && ii < n && jj >= 0 && jj < m)
+                        {
+                            if (board[ii][jj] == 0 || board[ii][jj] == -1) // 0->1  -1 is dead  ,1->0 2 is life
+                            {
+                                dead++;
+                            }
+                            else
+                            {
+                                life++;
+                            }
+                        }
+                    }
+                    if (board[i][j] == 0 || board[i][j] == -1)
+                    {
+                        if (life == 3)
+                        {
+                            board[i][j] = -1;
+                        }
+                    }
+                    else
+                    {
+                        if (life < 2)
+                        {
+                            board[i][j] = 2;
+                        }
+                        else if (life == 2 || life == 3)
+                        {
+                            if (board[i][j] == 2)
+                            {
+                                board[i][j] = 1;
+                            }
+                        }
+                        else if (life > 3)
+                        {
+                            board[i][j] = 2;
+                        }
+                    }
+
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (board[i][j] == 2)
+                    {
+                        board[i][j] = 0;
+                    }
+                    else if (board[i][j] == -1)
+                    {
+                        board[i][j] = 1;
+                    }
+                }
+            }
+
+
+        }
+    }
+}
