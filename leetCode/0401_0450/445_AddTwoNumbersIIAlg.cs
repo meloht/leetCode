4ef88823c77809184d7051d0f30ce59f12aaa2a5
hongtao_luo <luohongtao621@hotmail.com>
2024-02-05 16:06:02 +0800

    445

--- leetCode/0401_0450/445_AddTwoNumbersIIAlg.cs
+++ leetCode/0401_0450/445_AddTwoNumbersIIAlg.cs
@@ -8,7 +8,7 @@ namespace leetCode._0401_0450
 {
     public class _445_AddTwoNumbersIIAlg
     {
-        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        public ListNode AddTwoNumbers1(ListNode l1, ListNode l2)
         {
             var l1r = Reverse(l1);
             var l2r = Reverse(l2);
@@ -70,6 +70,37 @@ namespace leetCode._0401_0450
                 dummy.next = p;
             }
             return dummy.next;
+        }
+
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            Stack<int> stack1 = new Stack<int>();
+            Stack<int> stack2 = new Stack<int>();
+            while (l1 != null)
+            {
+                stack1.Push(l1.val);
+                l1 = l1.next;
+            }
+            while (l2 != null)
+            {
+                stack2.Push(l2.val);
+                l2 = l2.next;
+            }
+            int carry = 0;
+            ListNode ans = null;
+            while (stack1.Count > 0 || stack2.Count > 0 || carry != 0)
+            {
+                int a = stack1.Count == 0 ? 0 : stack1.Pop();
+                int b = stack2.Count == 0 ? 0 : stack2.Pop();
+                int cur = a + b + carry;
+                carry = cur / 10;
+                cur %= 10;
+                ListNode curnode = new ListNode(cur);
+                curnode.next = ans;
+                ans = curnode;
+            }
+            return ans;
         }
     }
 }
