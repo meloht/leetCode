2ac7fd361800efbfaebfef93254028476f94dd49
hongtao luo <luohongtao621@163.com>
2024-02-06 21:26:42 +0800

    502

--- leetCode/0501_0550/502_IPOAlg.cs
+++ leetCode/0501_0550/502_IPOAlg.cs
@@ -8,7 +8,7 @@ namespace leetCode._0501_0550
 {
     public class _502_IPOAlg
     {
-        public int FindMaximizedCapital(int k, int w, int[] profits, int[] capital)
+        public int FindMaximizedCapital1(int k, int w, int[] profits, int[] capital)
         {
             PriorityQueue<int[], int> queue = new PriorityQueue<int[], int>(Comparer<int>.Create((a, b) => -a.CompareTo(b)));
             PriorityQueue<int[], int> queueTemp = new PriorityQueue<int[], int>();
@@ -28,6 +28,40 @@ namespace leetCode._0501_0550
                 {
                     var d = queue.Dequeue();
                     w += d[0];
+                    k--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return w;
+        }
+
+        public int FindMaximizedCapital(int k, int w, int[] profits, int[] capital)
+        {
+            int n = profits.Length;
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => -a.CompareTo(b)));
+            int[][] arr = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = new[] { profits[i], capital[i] };
+            }
+            Array.Sort(arr, (a, b) => a[1].CompareTo(b[1]));
+            int count = 0;
+            while (k > 0)
+            {
+                while (count < n && w >= arr[count][1])
+                {
+                    queue.Enqueue(arr[count][0], arr[count][0]);
+                    count++;
+                }
+
+                if (queue.Count > 0)
+                {
+                    var d = queue.Dequeue();
+                    w += d;
                     k--;
                 }
                 else
