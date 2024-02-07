1613d1f6ad7b9bbc63410218e2db6b825e1934df
hongtao_luo <luohongtao621@hotmail.com>
2024-02-07 13:56:46 +0800

    918

---
+++ TestLeetCode/0901_0950/918_MaximumSumCircularSubarrayUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0901_0950
+{
+    [TestClass]
+    public class _918_MaximumSumCircularSubarrayUnitTest
+    {
+        _918_MaximumSumCircularSubarrayAlg alg = new _918_MaximumSumCircularSubarrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, -2, 3, -2];
+            int exp = 3;
+            int res = alg.MaxSubarraySumCircular(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [5, -3, 5];
+            int exp = 10;
+            int res = alg.MaxSubarraySumCircular(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [3, -2, 2, -3];
+            int exp = 3;
+            int res = alg.MaxSubarraySumCircular(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [6, 9, -3];
+            int exp = 15;
+            int res = alg.MaxSubarraySumCircular(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] nums = [-2, 4, -5, 4, -5, 9, 4];
+            int exp = 15;
+            int res = alg.MaxSubarraySumCircular(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/918_MaximumSumCircularSubarrayAlg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _918_MaximumSumCircularSubarrayAlg
+    {
+        public int MaxSubarraySumCircular(int[] nums)
+        {
+            int n = nums.Length;
+            int max = int.MinValue;
+            int k = nums.Length * 2;
+            int i = 0;
+            int sum = 0;
+            Queue<int> queue = new Queue<int>();
+            while (k > 0)
+            {
+                int temp = sum;
+                int num = nums[i];
+                sum += num;
+                queue.Enqueue(num);
+                if (sum >= temp)
+                {
+                    max = Math.Max(max, sum);
+                }
+                else
+                {
+                    while (queue.Count > 0 && sum < temp)
+                    {
+                        sum -= queue.Dequeue();
+                    }
+                }
+                i = (i + 1) % n;
+                k--;
+            }
+            return max;
+        }
+        private int GetNax(int begin, int[] nums)
+        {
+            int n = nums.Length;
+            int k = nums.Length;
+            int i = begin;
+            int pre = 0;
+            int max = nums[begin];
+            while (k > 0)
+            {
+                pre = Math.Max(pre + nums[i], nums[i]);
+                max = Math.Max(max, pre);
+                i = (i + 1) % n;
+                k--;
+            }
+            return max;
+        }
+
+    }
+}
