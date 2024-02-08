429379c9d4d8324238b32dbf5a46f5f5a5c47076
hongtao luo <luohongtao621@163.com>
2024-02-08 18:41:42 +0800

    993

---
+++ TestLeetCode/0951_1000/0993_CousinsInBinaryTreeUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0951_1000
+{
+    [TestClass]
+    public class _0993_CousinsInBinaryTreeUnitTest
+    {
+        _0993_CousinsInBinaryTreeAlg alg = new _0993_CousinsInBinaryTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 2, 3, 4];
+            int x = 4, y = 3;
+            bool exp = false;
+            var tree = Utils.BuildTree(root);
+            bool res = alg.IsCousins(tree, x, y);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1, 2, 3, null, 4, null, 5];
+            int x = 5, y = 4;
+            bool exp = true;
+            var tree = Utils.BuildTree(root);
+            bool res = alg.IsCousins(tree, x, y);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [1, 2, 3, null, 4];
+            int x = 2, y = 3;
+            bool exp = false;
+            var tree = Utils.BuildTree(root);
+            bool res = alg.IsCousins(tree, x, y);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0993_CousinsInBinaryTreeAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0993_CousinsInBinaryTreeAlg
+    {
+        int xDept = -1;
+        int yDept = -1;
+        int X = 0;
+        int Y = 0;
+        public bool IsCousins(TreeNode root, int x, int y)
+        {
+            this.X = x; this.Y = y;
+            Dfs(root, 0);
+            if (this.xDept == -1||this.yDept==-1)
+                return false;
+            if (this.xDept == -2 || this.yDept == -2)
+                return false;
+
+            return this.xDept==this.yDept;
+        }
+        private void Dfs(TreeNode root,int dept)
+        {
+            if (root == null)
+                return;
+            dept++;
+            if (root.val == this.Y)
+            {
+                this.yDept = dept;
+            }
+            if (root.val == this.X)
+            {
+                this.xDept = dept;
+            }
+
+            if (this.xDept != -1 && this.yDept != -1)
+                return;
+          
+            Dfs(root.left, dept);
+            Dfs(root.right, dept);
+            if (root.left != null && root.right != null)
+            {
+                if (root.left.val == this.X && this.Y == root.right.val)
+                {
+                    this.xDept = -2;
+                    this.yDept = -2;
+                }
+               else if (root.right.val == this.X && this.Y == root.left.val)
+                {
+                    this.xDept = -2;
+                    this.yDept = -2;
+                }
+            }
+        }
+    }
+}
