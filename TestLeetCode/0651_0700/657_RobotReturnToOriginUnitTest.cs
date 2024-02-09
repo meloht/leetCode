eadbfd386a0bc853ddc2a7f96b41eac6527ab7f7
hongtao_luo <luohongtao621@hotmail.com>
2024-02-09 14:29:19 +0800

    657

---
+++ TestLeetCode/0651_0700/657_RobotReturnToOriginUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _657_RobotReturnToOriginUnitTest
+    {
+        _657_RobotReturnToOriginAlg alg = new _657_RobotReturnToOriginAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string moves = "UD";
+            bool exp = true;
+            bool res = alg.JudgeCircle(moves);
+            Assert.AreEqual(res, exp);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string moves = "LL";
+            bool exp = false;
+            bool res = alg.JudgeCircle(moves);
+            Assert.AreEqual(res, exp);
+        }
+    }
+}
---
+++ leetCode/0651_0700/657_RobotReturnToOriginAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _657_RobotReturnToOriginAlg
+    {
+        public bool JudgeCircle(string moves)
+        {
+            int x = 0;
+            int y = 0;
+            foreach (var move in moves)
+            {
+                if (move == 'U')
+                {
+                    y++;
+                }
+                else if (move == 'D')
+                {
+                    y--;
+                }
+                else if (move == 'R')
+                {
+                    x++;
+                }
+                else if (move == 'L')
+                {
+                    x--;
+                }
+            }
+            return x == 0 && y == 0;
+        }
+    }
+}
