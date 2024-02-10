44771d21fe17cb8e4e361480d9f11d6823557327
hongtao_luo <luohongtao621@hotmail.com>
2024-02-10 10:07:57 +0800

    1523

---
+++ TestLeetCode/1501_1550/1523_CountOddNumbersInAnIntervalRangeUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1501_1550
+{
+    [TestClass]
+    public class _1523_CountOddNumbersInAnIntervalRangeUnitTest
+    {
+        _1523_CountOddNumbersInAnIntervalRangeAlg alg = new _1523_CountOddNumbersInAnIntervalRangeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int low = 3, high = 7;
+            int exp = 3;
+            int res = alg.CountOdds(low, high);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int low = 8, high = 10;
+            int exp = 1;
+            int res = alg.CountOdds(low, high);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase023()
+        {
+            int low = 1, high = 8;
+            int exp = 4;
+            int res = alg.CountOdds(low, high);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1501_1550/1523_CountOddNumbersInAnIntervalRangeAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1501_1550
+{
+    public class _1523_CountOddNumbersInAnIntervalRangeAlg
+    {
+        public int CountOdds(int low, int high)
+        {
+            int num = high - low;
+            if (low % 2 == 0)
+            {
+                if (num % 2 == 0)
+                {
+                    return num / 2;
+                }
+                else
+                {
+
+                    return num / 2 + 1;
+                }
+            }
+            else
+            {
+                return num / 2 + 1;
+            }
+        }
+    }
+}
