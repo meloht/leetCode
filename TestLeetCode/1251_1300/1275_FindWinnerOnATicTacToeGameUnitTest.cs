0ce3ae04f5c77ede79655ada733e462017aef8e5
hongtao_luo <luohongtao621@hotmail.com>
2024-02-10 21:56:23 +0800

    1257

---
+++ TestLeetCode/1251_1300/1275_FindWinnerOnATicTacToeGameUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using leetCode._1251_1300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1251_1300
+{
+    [TestClass]
+    public class _1275_FindWinnerOnATicTacToeGameUnitTest
+    {
+        _1275_FindWinnerOnATicTacToeGameAlg alg = new _1275_FindWinnerOnATicTacToeGameAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] moves = [[0, 0], [2, 0], [1, 1], [2, 1], [2, 2]];
+            string exp = "A";
+            string res = alg.Tictactoe(moves);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] moves = [[0, 0], [1, 1], [0, 1], [0, 2], [1, 0], [2, 0]];
+            string exp = "B";
+            string res = alg.Tictactoe(moves);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] moves = [[0, 0], [1, 1], [2, 0], [1, 0], [1, 2], [2, 1], [0, 1], [0, 2], [2, 2]];
+            string exp = "Draw";
+            string res = alg.Tictactoe(moves);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] moves = [[0, 0], [1, 1]];
+            string exp = "Pending";
+            string res = alg.Tictactoe(moves);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[][] moves = [[2, 0], [1, 1], [0, 2], [2, 1], [1, 2], [1, 0], [0, 0], [0, 1]];
+            string exp = "B";
+            string res = alg.Tictactoe(moves);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1251_1300/1275_FindWinnerOnATicTacToeGameAlg.cs
@@ -0,0 +1,80 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1251_1300
+{
+    public class _1275_FindWinnerOnATicTacToeGameAlg
+    {
+        public string Tictactoe(int[][] moves)
+        {
+            int[] a1 = new int[3];
+            int[] a2 = new int[3];
+
+            int[] b1 = new int[3];
+            int[] b2 = new int[3];
+            int numA = 0; int numB = 0;
+            int numA1 = 0; int numB1 = 0;
+            int m = moves.Length;
+
+
+            for (int i = 0; i < m; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    a1[moves[i][0]]++;
+                    a2[moves[i][1]]++;
+
+                    if (a2[moves[i][1]] == 3 || a1[moves[i][0]] == 3)
+                    {
+                        return "A";
+                    }
+                    if (moves[i][0] == moves[i][1])
+                    {
+                        numA++;
+                    }
+                    if (moves[i][0] + moves[i][1] == 2)
+                    {
+                        numA1++;
+                    }
+                    if (numA == 3 || numA1 == 3)
+                    {
+                        return "A";
+                    }
+                }
+                else
+                {
+                    b1[moves[i][0]]++;
+                    b2[moves[i][1]]++;
+                    if (b1[moves[i][0]] == 3 || b2[moves[i][1]] == 3)
+                    {
+                        return "B";
+                    }
+                    if (moves[i][0] == moves[i][1])
+                    {
+                        numB++;
+                    }
+                    if (moves[i][0] + moves[i][1] == 2)
+                    {
+                        numB1++;
+                    }
+                    if (numB == 3 || numB1 == 3)
+                    {
+                        return "B";
+                    }
+
+                }
+            }
+            if (moves.Length == 9)
+            {
+                return "Draw";
+            }
+
+            return "Pending";
+        }
+
+    }
+}
