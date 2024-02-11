ca7ac2f6771bffaec73621e574c010afac871da3
hongtao_luo <luohongtao621@hotmail.com>
2024-02-11 21:49:25 +0800

    1232

---
+++ TestLeetCode/1201_1250/1232_CheckIfItIsAStraightLineUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1201_1250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1201_1250
+{
+    [TestClass]
+    public class _1232_CheckIfItIsAStraightLineUnitTest
+    {
+        _1232_CheckIfItIsAStraightLineAlg alg = new _1232_CheckIfItIsAStraightLineAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] coordinates = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]];
+            bool exp = true;
+            bool res = alg.CheckStraightLine(coordinates);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] coordinates = [[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]];
+            bool exp = false;
+            bool res = alg.CheckStraightLine(coordinates);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] coordinates = [[0, 0], [0, 1], [0, -1]];
+            bool exp = true;
+            bool res = alg.CheckStraightLine(coordinates);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1201_1250/1232_CheckIfItIsAStraightLineAlg.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._1201_1250
+{
+    public class _1232_CheckIfItIsAStraightLineAlg
+    {
+        public bool CheckStraightLine1(int[][] coordinates)
+        {
+            if (coordinates.Length <= 2)
+                return true;
+
+            int deltaX = coordinates[0][0], deltaY = coordinates[0][1];
+            int n = coordinates.Length;
+            for (int i = 0; i < n; i++)
+            {
+                coordinates[i][0] -= deltaX;
+                coordinates[i][1] -= deltaY;
+            }
+            int A = coordinates[1][1], B = -coordinates[1][0];
+            for (int i = 2; i < n; i++)
+            {
+                int x = coordinates[i][0], y = coordinates[i][1];
+                if (A * x + B * y != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool CheckStraightLine(int[][] coordinates)
+        {
+            int m = coordinates.Length;
+            // 将计算斜率（每个点与第一个点间的斜率）的除法转换为交换项的乘法，这样可避免判断除数为0的情况
+            int x = coordinates[0][0], y = coordinates[0][1];
+            int k = coordinates[1][0] - x, v = coordinates[1][1] - y;
+            for (int i = 2; i < m; i++)
+            {
+                if ((coordinates[i][0] - x) * v != (coordinates[i][1] - y) * k)
+                    return false;
+            }
+            return true;
+        }
+
+
+    }
+}
