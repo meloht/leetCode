08d54bc919ac25d4dc06db731eb1688089ee2813
hongtao_luo <luohongtao621@hotmail.com>
2024-02-11 17:58:25 +0800

    1041

---
+++ TestLeetCode/1001_1050/1041_RobotBoundedInCircleUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1041_RobotBoundedInCircleUnitTest
+    {
+        _1041_RobotBoundedInCircleAlg alg = new _1041_RobotBoundedInCircleAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string instructions = "GGLLGG";
+            bool exp = true;
+            bool res = alg.IsRobotBounded(instructions);
+            Assert.AreEqual(res, exp);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string instructions = "GG";
+            bool exp = false;
+            bool res = alg.IsRobotBounded(instructions);
+            Assert.AreEqual(res, exp);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string instructions = "GL";
+            bool exp = true;
+            bool res = alg.IsRobotBounded(instructions);
+            Assert.AreEqual(res, exp);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1041_RobotBoundedInCircleAlg.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1041_RobotBoundedInCircleAlg
+    {
+        public bool IsRobotBounded1(string instructions)
+        {
+
+            HashSet<string> set = new HashSet<string>();
+            int n = 10;
+            int direction = 3;
+            int x = 0;
+            int y = 0;
+            while (n > 0)
+            {
+                string key = $"{x}_{y}";
+                if (set.Contains(key))
+                    return true;
+                set.Add(key);
+                foreach (var ch in instructions)
+                {
+                    if (ch == 'G')
+                    {
+                        if (direction == 3)
+                        {
+                            y++;
+                        }
+                        else if (direction == 2)
+                        {
+                            y--;
+                        }
+                        else if (direction == -1)
+                        {
+                            x--;
+                        }
+                        else if (direction == 1)
+                        {
+                            x++;
+                        }
+
+                    }
+                    else if (ch == 'L')
+                    {
+                        switch (direction)
+                        {
+                            case -1:
+                                direction = 2;
+                                break;
+                            case 1:
+                                direction = 3;
+                                break;
+                            case 2:
+                                direction = 1;
+                                break;
+                            case 3:
+                                direction = -1;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    else if (ch == 'R')
+                    {
+                        switch (direction)
+                        {
+                            case -1:
+                                direction = 3;
+                                break;
+                            case 1:
+                                direction = 2;
+                                break;
+                            case 2:
+                                direction = -1;
+                                break;
+                            case 3:
+                                direction = 1;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+
+                }
+                n--;
+            }
+
+            return false;
+        }
+
+        public bool IsRobotBounded(string instructions)
+        {
+            int[][] direc = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+
+            int direcIndex = 0;
+            int x = 0, y = 0;
+
+            foreach (var instruction in instructions)
+            {
+                if (instruction == 'G')
+                {
+                    x += direc[direcIndex][0];
+                    y += direc[direcIndex][1];
+                }
+                else if (instruction == 'L')
+                {
+                    direcIndex += 3;
+                    direcIndex %= 4;
+                }
+                else
+                {
+                    direcIndex++;
+                    direcIndex %= 4;
+                }
+            }
+          
+            return direcIndex != 0 || (x == 0 && y == 0);
+
+        }
+    }
+}
