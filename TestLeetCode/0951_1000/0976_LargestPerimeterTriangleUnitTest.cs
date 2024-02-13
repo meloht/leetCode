03a78953ecebf40d84d1dc0c86b3feeb810a885c
hongtao_luo <luohongtao621@hotmail.com>
2024-02-13 20:39:24 +0800

    0976

---
+++ TestLeetCode/0951_1000/0976_LargestPerimeterTriangleUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0951_1000
+{
+    [TestClass]
+    public class _0976_LargestPerimeterTriangleUnitTest
+    {
+        _0976_LargestPerimeterTriangleAlg alg = new _0976_LargestPerimeterTriangleAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 1, 2];
+            int exp = 5;
+            int res = alg.LargestPerimeter(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 1, 10];
+            int exp = 0;
+            int res = alg.LargestPerimeter(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [3, 6, 2, 3];
+            int exp = 8;
+            int res = alg.LargestPerimeter(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0951_1000/0976_LargestPerimeterTriangleAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0976_LargestPerimeterTriangleAlg
+    {
+        public int LargestPerimeter(int[] nums)
+        {
+            Array.Sort(nums);
+
+            for (int j = nums.Length - 1; j >= 2; j--)
+            {
+                int a = nums[j];
+                int b = nums[j - 1] + nums[j - 2];
+
+                if (b > a)
+                    return a + b;
+            }
+
+            return 0;
+        }
+    }
+}
