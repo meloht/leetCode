463ae9d399621a395a310a8bc185cb3fc266e370
hongtao_luo <luohongtao621@hotmail.com>
2024-02-13 21:00:33 +0800

    860

---
+++ TestLeetCode/0851_0900/860_LemonadeChangeUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _860_LemonadeChangeUnitTest
+    {
+        _860_LemonadeChangeAlg alg = new _860_LemonadeChangeAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] bills = [5, 5, 5, 10, 20];
+            bool exp = true;
+            bool res = alg.LemonadeChange(bills);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] bills = [5, 5, 10, 10, 20];
+            bool exp = false;
+            bool res = alg.LemonadeChange(bills);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/0851_0900/860_LemonadeChangeAlg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _860_LemonadeChangeAlg
+    {
+        public bool LemonadeChange(int[] bills)
+        {
+            int num5 = 0;
+            int num10 = 0;
+            for (int i = 0; i < bills.Length; i++)
+            {
+                if (bills[i] == 5)
+                {
+                    num5++;
+                }
+                else if (bills[i] == 10)
+                {
+                    if (num5 == 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        num5--;
+                    }
+                    num10++;
+                }
+                else if (bills[i] == 20)
+                {
+                    if (num10 > 0)
+                    {
+                        num10--;
+                        if (num5 < 1)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            num5--;
+                        }
+                    }
+                    else
+                    {
+                        if (num5 < 3)
+                        {
+                            return false;
+                        }
+                        num5 -= 3;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
