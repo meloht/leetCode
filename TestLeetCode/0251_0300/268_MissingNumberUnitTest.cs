f4da6fa051a3e33d8d9102a0d16ddc9a478a52a0
hongtao_luo <luohongtao621@hotmail.com>
2024-02-14 21:14:57 +0800

    268

---
+++ TestLeetCode/0251_0300/268_MissingNumberUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0251_0300
+{
+    [TestClass]
+    public class _268_MissingNumberUnitTest
+    {
+        _268_MissingNumberAlg alg = new _268_MissingNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 0, 1];
+            int exp = 2;
+            int res = alg.MissingNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0, 1];
+            int exp = 2;
+            int res = alg.MissingNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+            int exp = 8;
+            int res = alg.MissingNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [0];
+            int exp = 1;
+            int res = alg.MissingNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0251_0300/268_MissingNumberAlg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _268_MissingNumberAlg
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int n = nums.Length;
+            int sum = (n + 1) * n / 2;
+            int total = nums.Sum();
+            int ans = sum - total;
+            return ans;
+        }
+    }
+}
