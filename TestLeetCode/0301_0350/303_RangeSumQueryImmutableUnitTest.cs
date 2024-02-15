353ac17bb5a5ddcef5ef5fd6f225fe9ece8f74d7
hongtao_luo <luohongtao621@hotmail.com>
2024-02-15 20:57:15 +0800

    303

---
+++ TestLeetCode/0301_0350/303_RangeSumQueryImmutableUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0301_0350._303_RangeSumQueryImmutableAlg;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _303_RangeSumQueryImmutableUnitTest
+    {
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+            Assert.AreEqual(numArray.SumRange(0, 2), 1); // return 1 ((-2) + 0 + 3)
+            Assert.AreEqual(numArray.SumRange(2, 5), -1); // return -1 (3 + (-5) + 2 + (-1)) 
+            Assert.AreEqual(numArray.SumRange(0, 5), -3); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+        }
+
+
+    }
+}
---
+++ leetCode/0301_0350/303_RangeSumQueryImmutableAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _303_RangeSumQueryImmutableAlg
+    {
+        public class NumArray
+        {
+            int[] arr;
+            public NumArray(int[] nums)
+            {
+                arr = new int[nums.Length];
+                arr[0] = nums[0];
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    arr[i] = arr[i - 1] + nums[i];
+                }
+            }
+
+            public int SumRange(int left, int right)
+            {
+                if (left == 0)
+                {
+                    return arr[right];
+                }
+                int num = arr[right] - arr[left - 1];
+                return num;
+            }
+        }
+    }
+}
