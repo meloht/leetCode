13d522df26879b2000fdec0f13d8f1a4635e8699
hongtao luo <luohongtao621@163.com>
2024-02-16 19:51:13 +0800

    909

--- TestLeetCode/0901_0950/909_SnakesAndLaddersUnitTest.cs
+++ TestLeetCode/0901_0950/909_SnakesAndLaddersUnitTest.cs
@@ -30,8 +30,74 @@ namespace Test._0901_0950
         [TestMethod]
         public void TestCase02()
         {
-            int[][] board = [[-1, -1], [-1, 3]];
+            int[][] board = [
+                [-1, -1], 
+                [-1, 3]];
             int exp = 1;
+            int res = alg.SnakesAndLadders(board);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] board = [
+                [-1, 7, -1], 
+                [-1, 6, 9], 
+                [-1, -1, 2]];
+            int exp = 1;
+            int res = alg.SnakesAndLadders(board);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] board = [
+                [-1, 4, -1], 
+                [6, 2, 6], 
+                [-1, 3, -1]];
+            int exp = 2;
+            int res = alg.SnakesAndLadders(board);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[][] board = [
+                [1, 1, -1],
+                [1, 1, 1], 
+                [-1, 1, 1]];
+            int exp = -1;
+            int res = alg.SnakesAndLadders(board);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            int[][] board = [
+                [-1, 1, 2, -1],
+                [2, 13, 15, -1],
+                [-1, 10, -1, -1], 
+                [-1, 6, 2, 8]];
+            int exp = 2;
+            int res = alg.SnakesAndLadders(board);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase07()
+        {
+            int[][] board = [
+                [-1, -1, 16, 31, 29, -1], 
+                [10, 20, -1, -1, 9, -1],
+                [7, -1, -1, -1, 10, 28],
+                [-1, 16, -1, 16, -1, -1],
+                [28, -1, 5, 25, -1, -1], 
+                [-1, 32, 30, -1, -1, -1]];
+            int exp = 2;
             int res = alg.SnakesAndLadders(board);
             Assert.AreEqual(exp, res);
         }
--- leetCode/0901_0950/909_SnakesAndLaddersAlg.cs
+++ leetCode/0901_0950/909_SnakesAndLaddersAlg.cs
@@ -11,41 +11,58 @@ namespace leetCode._0901_0950
         public int SnakesAndLadders(int[][] board)
         {
             int n = board.Length;
-            int[] arr = new int[n * n];
-            int m = 0;
-            for (int i = n - 1, k = 0; i >= 0; i--, k++)
+            if (board[0][0] != -1)
+                return -1;
+            if (n < 3)
+                return 1;
+
+            bool[] vis = new bool[n * n + 1];
+
+            Queue<Tuple<int, int>> queue = new Queue<Tuple<int, int>>();
+
+            queue.Enqueue(new Tuple<int, int>(1, 0));
+            int total = n * n;
+            while (queue.Count > 0)
             {
-                if (i % k == 0)
+                var item = queue.Dequeue();
+                for (int i = 1; i <= 6; i++)
                 {
-                    for (int j = 0; j < n; j++)
+                    int next = item.Item1 + i;
+                    if (next > total)
                     {
-                        arr[m] = board[i][j];
-                        m++;
+                        break;
                     }
-                }
-                else
-                {
-                    for (int j = n - 1; j >= 0; j++)
+                    var rowCol = GetNextColRow(next, n);
+                    if (board[rowCol.Item1][rowCol.Item2] > 0)
                     {
-                        arr[m] = board[i][j];
-                        m++;
+                        next = board[rowCol.Item1][rowCol.Item2];
                     }
+                    if (next == total)
+                    {
+                        return item.Item2 + 1;
+                    }
+                    if (!vis[next])
+                    {
+                        vis[next] = true;
+                        queue.Enqueue(new Tuple<int, int>(next, item.Item2 + 1));
+                    }
+
                 }
-            }
-            int ans = int.MaxValue;
-            Queue<int[]> queue = new Queue<int[]>();
-            
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] != -1)
-                {
-                    queue.Enqueue(new int[] { i,i});
-                }
-                
-                
             }
 
-            return ans;
+            return -1;
         }
+
+        private Tuple<int, int> GetNextColRow(int index, int n)
+        {
+            int row = (index - 1) / n, col = (index - 1) % n;
+            if (row % 2 == 1)
+            {
+                col = n - 1 - col;
+            }
+            return new Tuple<int, int>(n - 1 - row, col);
+        }
+
+
     }
 }
