e04623aedb4a19168ed6e09180d21d26596ccf16
hongtao luo <luohongtao621@163.com>
2024-02-17 17:47:48 +0800

    1319

---
+++ TestLeetCode/1301_1350/1319_NumberOfOperationsToMakeNetworkConnectedUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._1301_1350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1301_1350
+{
+    [TestClass]
+    public class _1319_NumberOfOperationsToMakeNetworkConnectedUnitTest
+    {
+        _1319_NumberOfOperationsToMakeNetworkConnectedAlg alg = new _1319_NumberOfOperationsToMakeNetworkConnectedAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 4;
+            int[][] connections = [[0, 1], [0, 2], [1, 2]];
+            int exp = 1;
+            int res = alg.MakeConnected(n, connections);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 6;
+            int[][] connections = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]];
+            int exp = 2;
+            int res = alg.MakeConnected(n, connections);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 6;
+            int[][] connections = [[0, 1], [0, 2], [0, 3], [1, 2]];
+            int exp = -1;
+            int res = alg.MakeConnected(n, connections);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int n = 5;
+            int[][] connections = [[0, 1], [0, 2], [3, 4], [2, 3]];
+            int exp = 0;
+            int res = alg.MakeConnected(n, connections);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1301_1350/1319_NumberOfOperationsToMakeNetworkConnectedAlg.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1301_1350
+{
+    public class _1319_NumberOfOperationsToMakeNetworkConnectedAlg
+    {
+        public int MakeConnected(int n, int[][] connections)
+        {
+            int num = connections.Length;
+
+            if (num < n - 1)
+            {
+                return -1;
+            }
+
+            UnionFind unionFind = new UnionFind(n);
+            foreach (var item in connections)
+            {
+                unionFind.Union(item[0], item[1]);
+            }
+            int count = unionFind.GetCount();
+           
+
+            return count - 1;
+        }
+
+        private class UnionFind
+        {
+            private int[] parent;
+            private int[] ranks;
+            private int count = 0;
+
+            public UnionFind(int n)
+            {
+                this.parent = new int[n];
+                this.ranks = new int[n];
+                this.count = n;
+                for (int i = 0; i < n; i++)
+                {
+                    parent[i] = i;
+                }
+            }
+
+            public void Union(int x, int y)
+            {
+                int rootX = Find(x);
+                int rootY = Find(y);
+                if (rootX == rootY)
+                {
+                    return;
+                }
+
+                int xRank = this.ranks[rootX];
+                int yRank = this.ranks[rootY];
+                if (xRank < yRank)
+                {
+                    this.parent[rootX] = rootY;
+                }
+                else if (xRank > yRank)
+                {
+                    this.parent[rootY] = rootX;
+                }
+                else
+                {
+                    parent[rootX] = rootY;
+                    this.ranks[rootY]++;
+                }
+                this.count--;
+            }
+            public int GetCount()
+            {
+                return this.count;
+            }
+
+            /// <summary>
+            /// 路径压缩
+            /// </summary>
+            /// <returns></returns>
+            public int Find(int x)
+            {
+                if (x != parent[x])
+                {
+                    parent[x] = Find(parent[x]);
+                }
+                return parent[x];
+            }
+
+        }
+    }
+}
