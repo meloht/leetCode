74b6f14a7e727eb5ccad14b9fbfab028b177874a
hongtao_luo <luohongtao621@hotmail.com>
2024-02-18 13:05:01 +0800

    589

---
+++ TestLeetCode/0551_0600/589_NAryTreePreorderTraversalUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._0551_0600;
+using leetCode.Model.NTreeNode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _589_NAryTreePreorderTraversalUnitTest
+    {
+        _589_NAryTreePreorderTraversalAlg alg = new _589_NAryTreePreorderTraversalAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, null, 3, 2, 4, null, 5, 6];
+            var tree = Node.Build(root);
+            int[] exp = [1, 3, 5, 6, 2, 4];
+            int[] res = alg.Preorder(tree).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1, null, 2, 3, 4, 5, null, null, 6, 7, null, 8, null, 9, 10, null, null, 11, null, 12, null, 13, null, null, 14];
+            var tree = Node.Build(root);
+            int[] exp = [1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13, 10];
+            int[] res = alg.Preorder(tree).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+    }
+}
---
+++ leetCode/0551_0600/589_NAryTreePreorderTraversalAlg.cs
@@ -0,0 +1,34 @@
+﻿using leetCode.Model.NTreeNode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _589_NAryTreePreorderTraversalAlg
+    {
+        public IList<int> Preorder(Node root)
+        {
+            List<int> list = new List<int>();
+            Dfs(root, list);
+            return list;
+        }
+
+        private void Dfs(Node root, List<int> list)
+        {
+            if (root == null)
+                return;
+            list.Add(root.val);
+            if (root.children != null)
+            {
+                foreach (var item in root.children)
+                {
+                    Dfs(item, list);
+                }
+            }
+
+        }
+    }
+}
