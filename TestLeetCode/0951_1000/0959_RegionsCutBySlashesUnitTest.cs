0238097f1a1c5c1f27a11b3da7d4112604ace4a6
hongtao_luo <luohongtao621@hotmail.com>
2024-02-18 14:11:51 +0800

    959

---
+++ TestLeetCode/0951_1000/0959_RegionsCutBySlashesUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0951_1000
+{
+    [TestClass]
+    public class _0959_RegionsCutBySlashesUnitTest
+    {
+        _0959_RegionsCutBySlashesAlg alg = new _0959_RegionsCutBySlashesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] grid = [" /", "/ "];
+            int exp = 2;
+            int res = alg.RegionsBySlashes(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] grid = [" /", "  "];
+            int exp = 1;
+            int res = alg.RegionsBySlashes(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] grid = ["/\\", "\\/"];
+            int exp = 5;
+            int res = alg.RegionsBySlashes(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0959_RegionsCutBySlashesAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0959_RegionsCutBySlashesAlg
+    {
+        public int RegionsBySlashes(string[] grid)
+        {
+            return 0;
+        }
+    }
+}
