6e9094ea8a727e838ac323b9d69d36f4ed35bc09
hongtao_luo <luohongtao621@hotmail.com>
2024-02-19 11:47:46 +0800

    590

---
+++ leetCode/0551_0600/590_NAryTreePostorderTraversalAlg.cs
@@ -0,0 +1,33 @@
+﻿using leetCode.Model.NTreeNode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _590_NAryTreePostorderTraversalAlg
+    {
+        public IList<int> Postorder(Node root)
+        {
+            List<int> list=new List<int>();
+            Dfs(root, list);
+            return list;
+        }
+        private void Dfs(Node root, List<int> list) 
+        {
+            if (root == null)
+                return;
+            if (root.children != null)
+            {
+                foreach (var item in root.children)
+                {
+                    Dfs(item, list);
+                }
+            }
+            list.Add(root.val);
+           
+        }
+    }
+}
