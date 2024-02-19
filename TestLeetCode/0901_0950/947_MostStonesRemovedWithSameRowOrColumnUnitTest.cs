83c1e26f9025ffab30b7edb1a0b8297c38ad9c4a
hongtao_luo <luohongtao621@hotmail.com>
2024-02-19 15:01:24 +0800

    947

---
+++ TestLeetCode/0901_0950/947_MostStonesRemovedWithSameRowOrColumnUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0901_0950
+{
+    [TestClass]
+    public class _947_MostStonesRemovedWithSameRowOrColumnUnitTest
+    {
+        _947_MostStonesRemovedWithSameRowOrColumnAlg alg = new _947_MostStonesRemovedWithSameRowOrColumnAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] stones = [[0, 0], [0, 1], [1, 0], [1, 2], [2, 1], [2, 2]];
+            int exp = 5;
+            int res = alg.RemoveStones(stones);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] stones = [[0, 0], [0, 2], [1, 1], [2, 0], [2, 2]];
+            int exp = 3;
+            int res = alg.RemoveStones(stones);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] stones = [[0, 0]];
+            int exp = 0;
+            int res = alg.RemoveStones(stones);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/947_MostStonesRemovedWithSameRowOrColumnAlg.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _947_MostStonesRemovedWithSameRowOrColumnAlg
+    {
+        public int RemoveStones1(int[][] stones)
+        {
+            int n = stones.Length;
+            UnionFind unionFind = new UnionFind(n);
+            for (int i = 0; i < n; i++)
+            {
+                var a = stones[i];
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (i == j)
+                        continue;
+                    var b = stones[j];
+                    if (a[0] != b[0] && a[1] == b[1])
+                    {
+                        unionFind.Union(i, j);
+                    }
+                    else if (a[1] != b[1] && a[0] == b[0])
+                    {
+                        unionFind.Union(i, j);
+                    }
+                }
+            }
+            int ans = n - unionFind.Count;
+
+            return ans;
+        }
+
+        private class UnionFind
+        {
+            private int[] parent;
+            private int[] ranks;
+            private int count = 0;
+            public UnionFind(int n)
+            {
+                this.count = n;
+                this.parent = new int[n];
+                this.ranks = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    parent[i] = i;
+                }
+            }
+
+            public void Union(int x, int y)
+            {
+                int rootX = Find(x);
+                int rootY = Find(y);
+                if (rootX == rootY)
+                {
+                    return;
+                }
+                int xRank = this.ranks[rootX];
+                int yRank = this.ranks[rootY];
+                if (xRank < yRank)
+                {
+                    this.parent[rootX] = rootY;
+                }
+                else if (xRank > yRank)
+                {
+                    this.parent[rootY] = rootX;
+                }
+                else
+                {
+                    parent[rootX] = rootY;
+                    this.ranks[rootY]++;
+                }
+                this.count--;
+
+            }
+
+            /// <summary>
+            /// 路径压缩
+            /// </summary>
+            /// <returns></returns>
+            public int Find(int x)
+            {
+                if (x != parent[x])
+                {
+                    parent[x] = Find(parent[x]);
+                }
+                return parent[x];
+            }
+
+            public int Count
+            {
+                get { return count; }
+            }
+
+        }
+
+
+        public int RemoveStones(int[][] stones)
+        {
+            UnionMap unionMap = new UnionMap();
+            foreach (var item in stones)
+            {
+                unionMap.Union(item[0] + 10001, item[1]);
+            }
+
+            return stones.Length - unionMap.Count;
+        }
+
+        private class UnionMap
+        {
+            private Dictionary<int, int> parent = new Dictionary<int, int>();
+            private int count = 0;
+
+            public int Count
+            {
+                get { return count; }
+            }
+
+            public int Find(int x)
+            {
+                if (!parent.ContainsKey(x))
+                {
+                    parent.Add(x, x);
+                    // 并查集集中新加入一个结点，结点的父亲结点是它自己，所以连通分量的总数 +1
+                    count++;
+                }
+                if (x != parent[x])
+                {
+                    parent[x] = Find(parent[x]);
+                }
+
+                return parent[x];
+            }
+
+            public void Union(int x, int y)
+            {
+                int rootX = Find(x);
+                int rootY = Find(y);
+                if (rootX == rootY)
+                    return;
+
+                parent[rootX] = rootY;
+                // 两个连通分量合并成为一个，连通分量的总数 -1
+                count--;
+
+            }
+        }
+    }
+}
