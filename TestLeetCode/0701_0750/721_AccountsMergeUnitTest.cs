9379a5f22acb02bf0af52f113536b0c827b72168
hongtao_luo <luohongtao621@hotmail.com>
2024-02-20 13:40:57 +0800

    721

---
+++ TestLeetCode/0701_0750/721_AccountsMergeUnitTest.cs
@@ -0,0 +1,63 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _721_AccountsMergeUnitTest
+    {
+        _721_AccountsMergeAlg alg = new _721_AccountsMergeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[][] accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"],
+            ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]];
+            IList<IList<string>> exp = [["John", "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com"],
+            ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]];
+
+            IList<IList<string>> res = alg.AccountsMerge(accounts);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[][] accounts = [["Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"], ["Kevin", "Kevin3@m.co", "Kevin5@m.co", "Kevin0@m.co"], ["Ethan", "Ethan5@m.co", "Ethan4@m.co", "Ethan0@m.co"], 
+            ["Hanzo", "Hanzo3@m.co", "Hanzo1@m.co", "Hanzo0@m.co"], ["Fern", "Fern5@m.co", "Fern1@m.co", "Fern0@m.co"]];
+            IList<IList<string>> exp = [["Ethan", "Ethan0@m.co", "Ethan4@m.co", "Ethan5@m.co"], ["Gabe", "Gabe0@m.co", "Gabe1@m.co", "Gabe3@m.co"], ["Hanzo", "Hanzo0@m.co", "Hanzo1@m.co", "Hanzo3@m.co"],
+            ["Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"], ["Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"]];
+
+            IList<IList<string>> res = alg.AccountsMerge(accounts);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[][] accounts = [["David", "David0@m.co", "David4@m.co", "David3@m.co"],
+            ["David", "David5@m.co", "David5@m.co", "David0@m.co"], 
+            ["David", "David1@m.co", "David4@m.co", "David0@m.co"],
+            ["David", "David0@m.co", "David1@m.co", "David3@m.co"], 
+            ["David", "David4@m.co", "David1@m.co", "David3@m.co"]];
+            IList<IList<string>> exp = [["David", "David0@m.co", "David1@m.co",
+                    "David3@m.co", "David4@m.co", "David5@m.co"]];
+
+            IList<IList<string>> res = alg.AccountsMerge(accounts);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+    }
+}
---
+++ leetCode/0701_0750/721_AccountsMergeAlg.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _721_AccountsMergeAlg
+    {
+        public IList<IList<string>> AccountsMerge(IList<IList<string>> accounts)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            int n = accounts.Count;
+            UnionFind union = new UnionFind(n);
+            for (int i = 0; i < n; i++)
+            {
+                var item = accounts[i];
+                for (int j = 1; j < item.Count; j++)
+                {
+                    string mail = item[j];
+                    if (dict.ContainsKey(mail) )
+                    {
+                        if (dict[mail] != i)
+                        {
+                            union.Union(dict[mail], i);
+                        }
+                    }
+                    else
+                    {
+                        
+                        dict.Add(mail, i);
+                    }
+                }
+            }
+
+            Dictionary<int, List<int>> map = new Dictionary<int, List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                int root = union.Find(i);
+                if (map.ContainsKey(root))
+                {
+                    map[root].Add(i);
+                }
+                else
+                {
+                    map.Add(root, new List<int>() { i });
+                }
+
+            }
+
+            List<IList<string>> res = new List<IList<string>>();
+
+            foreach (var item in map.Values)
+            {
+
+                string name = accounts[item[0]][0];
+                HashSet<string> set = new HashSet<string>();
+                List<string> list = new List<string>();
+                list.Add(name);
+                foreach (int i in item)
+                {
+                    var mails = accounts[i];
+                    for (int j = 1; j < mails.Count; j++)
+                    {
+                        set.Add(mails[j]);
+                    }
+
+                }
+
+                var ls = set.ToList();
+                ls.Sort(string.CompareOrdinal);
+                list.AddRange(ls);
+                res.Add(list);
+            }
+
+
+            return res;
+        }
+
+        private class UnionFind
+        {
+            private int[] parent;
+            private int[] ranks;
+            public UnionFind(int n)
+            {
+                this.parent = new int[n];
+                this.ranks = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    parent[i] = i;
+
+                }
+            }
+
+            public void Union(int x, int y)
+            {
+                int rootX = Find(x);
+                int rootY = Find(y);
+                if (rootX == rootY)
+                {
+                    return;
+                }
+                int xRank = this.ranks[rootX];
+                int yRank = this.ranks[rootY];
+                if (xRank < yRank)
+                {
+                    this.parent[rootX] = rootY;
+                }
+                else if (xRank > yRank)
+                {
+                    this.parent[rootY] = rootX;
+                }
+                else
+                {
+                    parent[rootX] = rootY;
+                    this.ranks[rootY]++;
+                }
+
+            }
+
+            /// <summary>
+            /// 路径压缩
+            /// </summary>
+            /// <returns></returns>
+            public int Find(int x)
+            {
+                if (x != parent[x])
+                {
+                    parent[x] = Find(parent[x]);
+                }
+                return parent[x];
+            }
+
+
+
+        }
+    }
+}
