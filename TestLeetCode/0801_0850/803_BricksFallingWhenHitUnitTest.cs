de1e1b458e7779daf3abef97f3657703d7067ee2
hongtao_luo <luohongtao621@hotmail.com>
2024-02-20 14:04:21 +0800

    803

---
+++ TestLeetCode/0801_0850/803_BricksFallingWhenHitUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _803_BricksFallingWhenHitUnitTest
+    {
+        _803_BricksFallingWhenHitAlg alg = new _803_BricksFallingWhenHitAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [
+                [1, 0, 0, 0], 
+                [1, 1, 1, 0]], hits = [[1, 0]];
+            int[] exp = [2];
+            int[] res = alg.HitBricks(grid, hits);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [
+                [1, 0, 0, 0], 
+                [1, 1, 0, 0]], hits = [[1, 1], [1, 0]];
+            int[] exp = [0, 0];
+            int[] res = alg.HitBricks(grid, hits);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+
+    }
+}
---
+++ leetCode/0801_0850/803_BricksFallingWhenHitAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _803_BricksFallingWhenHitAlg
+    {
+        public int[] HitBricks(int[][] grid, int[][] hits)
+        {
+
+            return null;
+        }
+    }
+}
