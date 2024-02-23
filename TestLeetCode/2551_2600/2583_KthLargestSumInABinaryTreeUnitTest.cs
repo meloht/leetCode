871e331272a0bcd87b37bca8a347f6998c7b6d4d
hongtao luo <luohongtao621@163.com>
2024-02-23 13:39:27 +0800

    2583

---
+++ TestLeetCode/2551_2600/2583_KthLargestSumInABinaryTreeUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode._2551_2600;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2551_2600
+{
+    [TestClass]
+    public class _2583_KthLargestSumInABinaryTreeUnitTest
+    {
+        _2583_KthLargestSumInABinaryTreeAlg alg=new _2583_KthLargestSumInABinaryTreeAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [5, 8, 9, 2, 1, 3, 7, 4, 6];
+            var tree=TreeNode.BuildTree(root);
+            int k = 2;
+            int exp = 13;
+            long res = alg.KthLargestLevelSum(tree, k);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1, 2, null, 3];
+            var tree = TreeNode.BuildTree(root);
+            int k = 1;
+            int exp = 3;
+            long res = alg.KthLargestLevelSum(tree, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2551_2600/2583_KthLargestSumInABinaryTreeAlg.cs
@@ -0,0 +1,45 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2551_2600
+{
+    public class _2583_KthLargestSumInABinaryTreeAlg
+    {
+        public long KthLargestLevelSum(TreeNode root, int k)
+        {
+            PriorityQueue<long, long> priorityQueue = new PriorityQueue<long, long>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+                long sum = 0;
+                for (int i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+                    sum += node.val;
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                priorityQueue.Enqueue(sum, sum);
+                if (priorityQueue.Count > k)
+                {
+                    priorityQueue.Dequeue();
+                }
+            }
+            if (priorityQueue.Count < k)
+                return -1;
+            return priorityQueue.Dequeue();
+        }
+    }
+}
