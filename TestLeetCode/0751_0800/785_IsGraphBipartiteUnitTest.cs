7386d247c9465ba139c223fa0fd39e677ee19080
hongtao luo <luohongtao621@163.com>
2024-02-25 19:53:15 +0800

    785

---
+++ TestLeetCode/0751_0800/785_IsGraphBipartiteUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _785_IsGraphBipartiteUnitTest
+    {
+        _785_IsGraphBipartiteAlg alg = new _785_IsGraphBipartiteAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] graph = [[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]];
+            bool exp = false;
+            bool res = alg.IsBipartite(graph);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] graph = [[1, 3], [0, 2], [1, 3], [0, 2]];
+            bool exp = true;
+            bool res = alg.IsBipartite(graph);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] graph = [[4], [], [4], [4], [0, 2, 3]];
+            bool exp = true;
+            bool res = alg.IsBipartite(graph);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] graph = [[], [2, 4, 6], [1, 4, 8, 9], [7, 8], [1, 2, 8, 9], [6, 9],
+            [1, 5, 7, 8, 9], [3, 6, 9], [2, 3, 4, 6, 9], 
+            [2, 4, 5, 6, 7, 8]];
+            bool exp = false;
+            bool res = alg.IsBipartite(graph);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/785_IsGraphBipartiteAlg.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _785_IsGraphBipartiteAlg
+    {
+        public bool IsBipartite(int[][] graph)
+        {
+            int n = graph.Length;
+            UnionFind union = new UnionFind(n);
+            foreach (var item in graph)
+            {
+                for (int i = 1; i < item.Length; i++)
+                {
+                    union.Union(item[i - 1], item[i]);
+                }
+
+            }
+          
+            for (int i = 0; i < n; i++)
+            {
+                int[] edge = graph[i];
+                foreach (var item in edge)
+                {
+                    if (union.IsConnect(i, item))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        private class UnionFind
+        {
+            private int[] parent;
+            private int[] ranks;
+            private int count;
+            private int[] size;
+
+            public UnionFind(int n)
+            {
+                this.parent = new int[n];
+                this.ranks = new int[n];
+                this.count = n;
+                this.size = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    parent[i] = i;
+                    this.size[i] = 1;
+                }
+            }
+
+            public void Union(int x, int y)
+            {
+                int rootX = Find(x);
+                int rootY = Find(y);
+                if (rootX == rootY)
+                {
+                    return;
+                }
+                int xRank = this.ranks[rootX];
+                int yRank = this.ranks[rootY];
+                if (xRank < yRank)
+                {
+                    this.parent[rootX] = rootY;
+                    this.size[rootY] += size[rootX];
+                }
+                else if (xRank > yRank)
+                {
+                    this.parent[rootY] = rootX;
+                    this.size[rootX] += size[rootY];
+                }
+                else
+                {
+                    parent[rootX] = rootY;
+                    this.ranks[rootY]++;
+                    this.size[rootY] += size[rootX];
+                }
+                this.count--;
+
+            }
+            public int GetSize(int x)
+            {
+                int root = Find(x);
+                return size[root];
+            }
+            public int Count
+            {
+                get { return this.count; }
+            }
+
+            /// <summary>
+            /// 路径压缩
+            /// </summary>
+            /// <returns></returns>
+            public int Find(int x)
+            {
+                if (x != parent[x])
+                {
+                    parent[x] = Find(parent[x]);
+                }
+                return parent[x];
+            }
+
+            public bool IsConnect(int x, int y)
+            {
+                int rootx = Find(x);
+                int rooty = Find(y);
+                if (rootx == rooty)
+                    return true;
+                return false;
+            }
+
+        }
+    }
+}
