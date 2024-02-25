f6601a3aa03f2e34be34b06ee724cd25a3c7a3fb
hongtao luo <luohongtao621@163.com>
2024-02-25 15:54:28 +0800

    765

---
+++ TestLeetCode/0751_0800/765_CouplesHoldingHandsUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _765_CouplesHoldingHandsUnitTest
+    {
+        _765_CouplesHoldingHandsAlg alg = new _765_CouplesHoldingHandsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] row = [0, 2, 1, 3];
+            int exp = 1;
+            int res = alg.MinSwapsCouples(row);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] row = [3, 2, 0, 1];
+            int exp = 0;
+            int res = alg.MinSwapsCouples(row);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0751_0800/765_CouplesHoldingHandsAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _765_CouplesHoldingHandsAlg
+    {
+        public int MinSwapsCouples(int[] row)
+        {
+            int ans = 0;
+            for (int i = 0; i < row.Length; i += 2)
+            {
+                int x = row[i];
+                int y = x ^ 1;
+                if (row[i + 1] == y)
+                {
+                    continue;
+                }
+                for (int j = 0; j < row.Length; j++)
+                {
+                    if (i == j)
+                        continue;
+                    if (row[j] == y)
+                    {
+                        int temp = row[i + 1];
+                        row[i + 1] = row[j];
+                        row[j] = temp;
+                        ans++;
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
