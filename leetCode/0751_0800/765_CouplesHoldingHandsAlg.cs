fb1c9db4154fca0b917ed21d99bee549965eae08
hongtao luo <luohongtao621@163.com>
2024-02-25 16:08:35 +0800

    765

--- leetCode/0751_0800/765_CouplesHoldingHandsAlg.cs
+++ leetCode/0751_0800/765_CouplesHoldingHandsAlg.cs
@@ -8,7 +8,7 @@ namespace leetCode._0751_0800
 {
     public class _765_CouplesHoldingHandsAlg
     {
-        public int MinSwapsCouples(int[] row)
+        public int MinSwapsCouples1(int[] row)
         {
             int ans = 0;
             for (int i = 0; i < row.Length; i += 2)
@@ -34,6 +34,98 @@ namespace leetCode._0751_0800
                 }
             }
             return ans;
+        }
+
+        public int MinSwapsCouples(int[] row)
+        {
+            int n = row.Length / 2;
+            UnionFind union = new UnionFind(n);
+
+            for (int i = 0; i < row.Length; i += 2)
+            {
+                var p1 = row[i];
+                var p2 = row[i + 1];
+                int p11 = p1 >> 1;
+                int p22 = p2 >> 1;
+                if (p11 != p22)
+                {
+                    union.Union(p11, p22);
+                }
+            }
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (union.Find(i) == i)
+                {
+                    ans++;
+                }
+            }
+
+            return n - ans;
+        }
+
+
+        private class UnionFind
+        {
+            private int[] parent;
+            private int[] ranks;
+            private int count;
+
+            public UnionFind(int n)
+            {
+                this.parent = new int[n];
+                this.ranks = new int[n];
+                this.count = n;
+                for (int i = 0; i < n; i++)
+                {
+                    parent[i] = i;
+                }
+            }
+
+            public void Union(int x, int y)
+            {
+                int rootX = Find(x);
+                int rootY = Find(y);
+                if (rootX == rootY)
+                {
+                    return;
+                }
+
+                int xRank = this.ranks[rootX];
+                int yRank = this.ranks[rootY];
+                if (xRank < yRank)
+                {
+                    this.parent[rootX] = rootY;
+                }
+                else if (xRank > yRank)
+                {
+                    this.parent[rootY] = rootX;
+                }
+                else
+                {
+                    parent[rootX] = rootY;
+                    this.ranks[rootY]++;
+                }
+                this.count--;
+            }
+            public int GetCount()
+            {
+                return this.count;
+            }
+
+            /// <summary>
+            /// 路径压缩
+            /// </summary>
+            /// <returns></returns>
+            public int Find(int x)
+            {
+                if (x != parent[x])
+                {
+                    parent[x] = Find(parent[x]);
+                }
+                return parent[x];
+            }
+
         }
     }
 }
