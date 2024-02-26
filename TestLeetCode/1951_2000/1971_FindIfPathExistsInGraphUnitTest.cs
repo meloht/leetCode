98036cce28f9d128261db1a0538be424bd030461
hongtao_luo <luohongtao621@hotmail.com>
2024-02-26 13:18:22 +0800

    1971

---
+++ TestLeetCode/1951_2000/1971_FindIfPathExistsInGraphUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._1951_2000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1951_2000
+{
+    [TestClass]
+    public class _1971_FindIfPathExistsInGraphUnitTest
+    {
+        _1971_FindIfPathExistsInGraphAlg alg = new _1971_FindIfPathExistsInGraphAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3;
+            int[][] edges = [[0, 1], [1, 2], [2, 0]];
+            int source = 0, destination = 2;
+            bool exp = true;
+            bool res = alg.ValidPath(n, edges, source, destination);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 6;
+            int[][] edges = [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]];
+            int source = 0, destination = 5;
+            bool exp = false;
+            bool res = alg.ValidPath(n, edges, source, destination);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1951_2000/1971_FindIfPathExistsInGraphAlg.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1951_2000
+{
+    public class _1971_FindIfPathExistsInGraphAlg
+    {
+        public bool ValidPath(int n, int[][] edges, int source, int destination)
+        {
+            UnionFind union = new UnionFind(n);
+            foreach (var item in edges)
+            {
+                union.Union(item[0], item[1]);
+            }
+
+            return union.IsConnect(source, destination);
+        }
+
+        private class UnionFind
+        {
+            private int[] parent;
+            private int[] ranks;
+            private int[] size;
+            private int count = 0;
+            public UnionFind(int n)
+            {
+                this.parent = new int[n];
+                this.ranks = new int[n];
+                this.size = new int[n];
+                this.count = n;
+                for (int i = 0; i < n; i++)
+                {
+                    parent[i] = i;
+                    this.size[i] = 1;
+                }
+            }
+
+            public void Union(int x, int y)
+            {
+                int rootX = Find(x);
+                int rootY = Find(y);
+                if (rootX == rootY)
+                {
+                    return;
+                }
+                int xRank = this.ranks[rootX];
+                int yRank = this.ranks[rootY];
+                if (xRank < yRank)
+                {
+                    this.parent[rootX] = rootY;
+                    this.size[rootY] += size[rootX];
+                }
+                else if (xRank > yRank)
+                {
+                    this.parent[rootY] = rootX;
+                    this.size[rootX] += size[rootY];
+                }
+                else
+                {
+                    parent[rootX] = rootY;
+                    this.ranks[rootY]++;
+                    this.size[rootY] += size[rootX];
+                }
+                this.count--;
+
+            }
+            /// <summary>
+            /// 路径压缩
+            /// </summary>
+            /// <returns></returns>
+            public int Find(int x)
+            {
+                if (x != parent[x])
+                {
+                    parent[x] = Find(parent[x]);
+                }
+                return parent[x];
+            }
+            public int Count
+            {
+                get { return count; }
+            }
+            public int GetSize(int x)
+            {
+                int root = Find(x);
+                return size[root];
+            }
+
+            public bool IsConnect(int x, int y)
+            {
+                int rootx = Find(x);
+                int rooty = Find(y);
+                if (rootx == rooty)
+                    return true;
+                return false;
+            }
+        }
+    }
+}
