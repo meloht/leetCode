ce0b3930bd810adf58b5af02cb927c4f2b8edd9c
hongtao_luo <luohongtao621@hotmail.com>
2024-02-26 12:29:39 +0800

    938

---
+++ TestLeetCode/0901_0950/938_RangeSumOfBstUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._0901_0950;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0901_0950
+{
+    [TestClass]
+    public class _938_RangeSumOfBstUnitTest
+    {
+        _938_RangeSumOfBstAlg alg = new _938_RangeSumOfBstAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [10, 5, 15, 3, 7, null, 18];
+            int low = 7, high = 15;
+            int exp = 32;
+            var tree = TreeNode.BuildTree(root);
+            int res = alg.RangeSumBST(tree, low, high);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [10, 5, 15, 3, 7, 13, 18, 1, null, 6];
+            int low = 6, high = 10;
+            int exp = 23;
+            var tree = TreeNode.BuildTree(root);
+            int res = alg.RangeSumBST(tree, low, high);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/938_RangeSumOfBstAlg.cs
@@ -0,0 +1,50 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _938_RangeSumOfBstAlg
+    {
+        int ans = 0;
+        public int RangeSumBST1(TreeNode root, int low, int high)
+        {
+            Dfs(root, low, high);
+            return ans;
+        }
+        private void Dfs(TreeNode root, int low, int high)
+        {
+            if (root == null)
+                return;
+
+            Dfs(root.left, low, high);
+
+            if (root.val >= low && root.val <= high)
+            {
+                ans += root.val;
+            }
+            if (root.val > high)
+                return;
+
+            Dfs(root.right, low, high);
+        }
+
+        public int RangeSumBST(TreeNode root, int low, int high)
+        {
+            if (root == null)
+                return 0;
+            if (root.val > high)
+            {
+                return RangeSumBST(root.left, low, high);
+            }
+            if (root.val < low)
+            {
+                return RangeSumBST(root.right, low, high);
+            }
+            return root.val + RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+        }
+    }
+}
