2dcce5bd71de1a1fb6e0f31ff908068f6174f988
hongtao_luo <luohongtao621@hotmail.com>
2024-02-28 16:49:33 +0800

    1361

---
+++ TestLeetCode/1351_1400/1361_ValidateBinaryTreeNodesUnitTest.cs
@@ -0,0 +1,65 @@
+﻿using leetCode._1351_1400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1351_1400
+{
+    [TestClass]
+    public class _1361_ValidateBinaryTreeNodesUnitTest
+    {
+        _1361_ValidateBinaryTreeNodesAlg alg = new _1361_ValidateBinaryTreeNodesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] leftChild = [1, -1, 3, -1], rightChild = [2, -1, -1, -1];
+            int n = 4;
+            bool exp = true;
+            bool res = alg.ValidateBinaryTreeNodes(n, leftChild, rightChild);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] leftChild = [1, -1, 3, -1], rightChild = [2, 3, -1, -1];
+            int n = 4;
+            bool exp = false;
+            bool res = alg.ValidateBinaryTreeNodes(n, leftChild, rightChild);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] leftChild = [1, 0], rightChild = [-1, -1];
+            int n = 2;
+            bool exp = false;
+            bool res = alg.ValidateBinaryTreeNodes(n, leftChild, rightChild);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] leftChild = [1, -1, -1, 4, -1, -1], rightChild = [2, -1, -1, 5, -1, -1];
+            int n = 6;
+            bool exp = false;
+            bool res = alg.ValidateBinaryTreeNodes(n, leftChild, rightChild);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] leftChild = [3, -1, 1, -1], rightChild = [-1, -1, 0, -1];
+            int n = 4;
+            bool exp = true;
+            bool res = alg.ValidateBinaryTreeNodes(n, leftChild, rightChild);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1351_1400/1361_ValidateBinaryTreeNodesAlg.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1351_1400
+{
+    public class _1361_ValidateBinaryTreeNodesAlg
+    {
+        public bool ValidateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild)
+        {
+            UnionFind union = new UnionFind(n);
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int left = leftChild[i];
+                int right = rightChild[i];
+                if (left != -1)
+                {
+                    union.Union(i, left);
+                    arr[left]++;
+                }
+                if (right != -1)
+                {
+                    union.Union(i, right);
+                    arr[right]++;
+                }
+            }
+
+            foreach (var item in arr)
+            {
+                if (item > 1)
+                    return false;
+            }
+
+
+            if (union.Count > 1)
+                return false;
+
+            return true;
+        }
+
+        private class UnionFind
+        {
+            private int[] parent;
+            private int[] ranks;
+            private int[] size;
+            private int count = 0;
+            public UnionFind(int n)
+            {
+                this.parent = new int[n];
+                this.ranks = new int[n];
+                this.size = new int[n];
+                this.count = n;
+                for (int i = 0; i < n; i++)
+                {
+                    parent[i] = i;
+                    this.size[i] = 1;
+                }
+            }
+
+            public void Union(int x, int y)
+            {
+                int rootX = Find(x);
+                int rootY = Find(y);
+                if (rootX == rootY)
+                {
+                    return;
+                }
+                int xRank = this.ranks[rootX];
+                int yRank = this.ranks[rootY];
+                if (xRank < yRank)
+                {
+                    this.parent[rootX] = rootY;
+                    this.size[rootY] += size[rootX];
+                }
+                else if (xRank > yRank)
+                {
+                    this.parent[rootY] = rootX;
+                    this.size[rootX] += size[rootY];
+                }
+                else
+                {
+                    parent[rootX] = rootY;
+                    this.ranks[rootY]++;
+                    this.size[rootY] += size[rootX];
+                }
+                this.count--;
+
+            }
+            /// <summary>
+            /// 路径压缩
+            /// </summary>
+            /// <returns></returns>
+            public int Find(int x)
+            {
+                if (x != parent[x])
+                {
+                    parent[x] = Find(parent[x]);
+                }
+                return parent[x];
+            }
+            public int Count
+            {
+                get { return count; }
+            }
+            public int GetSize(int x)
+            {
+                int root = Find(x);
+                return size[root];
+            }
+
+            public bool IsConnect(int x, int y)
+            {
+                int rootx = Find(x);
+                int rooty = Find(y);
+                if (rootx == rooty)
+                    return true;
+                return false;
+            }
+        }
+    }
+}
