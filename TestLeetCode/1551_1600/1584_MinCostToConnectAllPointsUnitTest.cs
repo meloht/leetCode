88db245cb92840a71ac71f0844603c36ae604d65
hongtao_luo <luohongtao621@hotmail.com>
2024-03-04 15:01:38 +0800

    1584

---
+++ TestLeetCode/1551_1600/1584_MinCostToConnectAllPointsUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._1551_1600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1551_1600
+{
+    [TestClass]
+    public class _1584_MinCostToConnectAllPointsUnitTest
+    {
+        _1584_MinCostToConnectAllPointsAlg alg = new _1584_MinCostToConnectAllPointsAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] points = [[0, 0], [2, 2], [3, 10], [5, 2], [7, 0]];
+            int exp = 20;
+            int res = alg.MinCostConnectPoints(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] points = [[3, 12], [-2, 5], [-4, 1]];
+            int exp = 18;
+            int res = alg.MinCostConnectPoints(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] points = [[0, 0], [1, 1], [1, 0], [-1, 1]];
+            int exp = 4;
+            int res = alg.MinCostConnectPoints(points);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] points = [[-1000000, -1000000], [1000000, 1000000]];
+            int exp = 4000000;
+            int res = alg.MinCostConnectPoints(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[][] points = [[0, 0]];
+            int exp = 0;
+            int res = alg.MinCostConnectPoints(points);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1551_1600/1584_MinCostToConnectAllPointsAlg.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1551_1600
+{
+
+    public class _1584_MinCostToConnectAllPointsAlg
+    {
+        public int MinCostConnectPoints(int[][] points)
+        {
+            int n = points.Length;
+            UnionFind union = new UnionFind(n);
+            List<Edge> list = new List<Edge>();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    list.Add(new Edge(i, j, GetDis(points, i, j)));
+                }
+            }
+            list.Sort((x, y) => x.Length.CompareTo(y.Length));
+            int res = 0;
+            int num = 1;
+            foreach (var item in list)
+            {
+                int len = item.Length;
+                if (!union.IsConnect(item.X, item.Y))
+                {
+                    union.Union(item.X, item.Y);
+                    res += len;
+                    num++;
+                    if (num == n)
+                    {
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+
+        private int GetDis(int[][] points, int x, int y)
+        {
+            int dis = Math.Abs(points[x][0] - points[y][0]) + Math.Abs(points[x][1] - points[y][1]);
+            return dis;
+        }
+        private class Edge
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Length { get; set; }
+
+            public Edge(int x, int y, int len)
+            {
+                this.X = x;
+                this.Y = y;
+                this.Length = len;
+            }
+        }
+        private class UnionFind
+        {
+            private int[] parent;
+            private int[] ranks;
+            private int[] size;
+            private int count = 0;
+            public UnionFind(int n)
+            {
+                this.parent = new int[n];
+                this.ranks = new int[n];
+                this.size = new int[n];
+                this.count = n;
+                for (int i = 0; i < n; i++)
+                {
+                    parent[i] = i;
+                    this.size[i] = 1;
+                }
+            }
+
+            public void Union(int x, int y)
+            {
+                int rootX = Find(x);
+                int rootY = Find(y);
+                if (rootX == rootY)
+                {
+                    return;
+                }
+                int xRank = this.ranks[rootX];
+                int yRank = this.ranks[rootY];
+                if (xRank < yRank)
+                {
+                    this.parent[rootX] = rootY;
+                    this.size[rootY] += size[rootX];
+                }
+                else if (xRank > yRank)
+                {
+                    this.parent[rootY] = rootX;
+                    this.size[rootX] += size[rootY];
+                }
+                else
+                {
+                    parent[rootX] = rootY;
+                    this.ranks[rootY]++;
+                    this.size[rootY] += size[rootX];
+                }
+                this.count--;
+
+            }
+            /// <summary>
+            /// 路径压缩
+            /// </summary>
+            /// <returns></returns>
+            public int Find(int x)
+            {
+                if (x != parent[x])
+                {
+                    parent[x] = Find(parent[x]);
+                }
+                return parent[x];
+            }
+            public int Count
+            {
+                get { return count; }
+            }
+            public int GetSize(int x)
+            {
+                int root = Find(x);
+                return size[root];
+            }
+
+            public bool IsConnect(int x, int y)
+            {
+                int rootx = Find(x);
+                int rooty = Find(y);
+                if (rootx == rooty)
+                    return true;
+                return false;
+            }
+        }
+    }
+}
