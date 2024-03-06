a09971b547435ed756a2072fe35649167692eadc
hongtao_luo <luohongtao621@hotmail.com>
2024-03-06 13:44:04 +0800

    350

---
+++ TestLeetCode/0301_0350/350_IntersectionOfTwoArraysIIUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _350_IntersectionOfTwoArraysIIUnitTest
+    {
+        _350_IntersectionOfTwoArraysIIAlg alg = new _350_IntersectionOfTwoArraysIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [1, 2, 2, 1], nums2 = [2, 2];
+            int[] exp = [2, 2];
+            int[] res = alg.Intersect(nums1, nums2);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4];
+            int[] exp = [4, 9];
+            int[] res = alg.Intersect(nums1, nums2);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0301_0350/350_IntersectionOfTwoArraysIIAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _350_IntersectionOfTwoArraysIIAlg
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var item in nums1)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+
+            }
+            List<int> result=new List<int>();
+            foreach (var item in nums2)
+            {
+                if (dict.ContainsKey(item) && dict[item] > 0)
+                {
+                    result.Add(item);
+                    dict[item]--;
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
