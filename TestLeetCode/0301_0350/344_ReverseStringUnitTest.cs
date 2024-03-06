7d4c24facfdb40fd0cdbbf5f48a528dbf08cd87f
hongtao_luo <luohongtao621@hotmail.com>
2024-03-06 12:16:54 +0800

    344

---
+++ TestLeetCode/0301_0350/344_ReverseStringUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _344_ReverseStringUnitTest
+    {
+        _344_ReverseStringAlg alg = new _344_ReverseStringAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[] s = ['h', 'e', 'l', 'l', 'o'];
+            char[] exp = ['o', 'l', 'l', 'e', 'h'];
+            alg.ReverseString(s);
+            Assert.IsTrue(exp.SequenceEqual(s));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[] s = ['H', 'a', 'n', 'n', 'a', 'h'];
+            char[] exp = ['h', 'a', 'n', 'n', 'a', 'H'];
+            alg.ReverseString(s);
+            Assert.IsTrue(exp.SequenceEqual(s));
+        }
+    }
+}
---
+++ leetCode/0301_0350/344_ReverseStringAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _344_ReverseStringAlg
+    {
+        public void ReverseString(char[] s)
+        {
+            Array.Reverse(s);
+        }
+    }
+}
