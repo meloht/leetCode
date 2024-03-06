c1a087c3cc7f70bbd56d54b02f8a2a2376ff6962
hongtao_luo <luohongtao621@hotmail.com>
2024-03-06 13:33:21 +0800

    349

---
+++ TestLeetCode/0301_0350/349_IntersectionOfTwoArraysUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _349_IntersectionOfTwoArraysUnitTest
+    {
+        _349_IntersectionOfTwoArraysAlg alg = new _349_IntersectionOfTwoArraysAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [1, 2, 2, 1], nums2 = [2, 2];
+            int[] exp = [2];
+            int[] res = alg.Intersection(nums1, nums2);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4];
+            int[] exp = [9, 4];
+            int[] res = alg.Intersection(nums1, nums2);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0301_0350/349_IntersectionOfTwoArraysAlg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _349_IntersectionOfTwoArraysAlg
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            HashSet<int> result = new HashSet<int>();
+            HashSet<int> set=new HashSet<int>(nums2);
+            foreach (var item in nums1)
+            {
+                if (set.Contains(item)) 
+                {
+                    result.Add(item);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
