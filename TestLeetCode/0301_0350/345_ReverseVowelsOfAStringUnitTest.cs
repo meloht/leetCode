3dcbff9ddb4042a1009843a3ea1d1f9b9cc8a03b
hongtao_luo <luohongtao621@hotmail.com>
2024-03-06 13:09:06 +0800

    345

---
+++ TestLeetCode/0301_0350/345_ReverseVowelsOfAStringUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _345_ReverseVowelsOfAStringUnitTest
+    {
+        _345_ReverseVowelsOfAStringAlg alg = new _345_ReverseVowelsOfAStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "hello";
+            string exp = "holle";
+            string res = alg.ReverseVowels(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "leetcode";
+            string exp = "leotcede";
+            string res = alg.ReverseVowels(s);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = " ";
+            string exp = " ";
+            string res = alg.ReverseVowels(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0301_0350/345_ReverseVowelsOfAStringAlg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _345_ReverseVowelsOfAStringAlg
+    {
+
+        public string ReverseVowels(string s)
+        {
+            char[] arr = s.ToCharArray();
+
+            int i = 0;
+            int j = s.Length - 1;
+            while (i <= j)
+            {
+
+                while (i <= j && arr[i] != 'a' && arr[i] != 'A'
+                       && arr[i] != 'e' && arr[i] != 'E'
+                        && arr[i] != 'i' && arr[i] != 'I'
+                         && arr[i] != 'o' && arr[i] != 'O'
+                         && arr[i] != 'u' && arr[i] != 'U')
+                {
+                    i++;
+                }
+
+                while (i <= j && arr[j] != 'a' && arr[j] != 'A'
+                       && arr[j] != 'e' && arr[j] != 'E'
+                        && arr[j] != 'i' && arr[j] != 'I'
+                         && arr[j] != 'o' && arr[j] != 'O'
+                         && arr[j] != 'u' && arr[j] != 'U')
+                {
+                    j--;
+                }
+                if (i > j)
+                {
+                    break;
+                }
+                Swap(arr, i, j);
+                i++;
+                j--;
+            }
+            return new string(arr);
+        }
+        private void Swap(char[] arr, int i, int j)
+        {
+            var temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
