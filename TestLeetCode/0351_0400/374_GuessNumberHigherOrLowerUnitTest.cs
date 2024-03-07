4f878690b0abef0155bfeae7194a233efb46cc90
hongtao_luo <luohongtao621@hotmail.com>
2024-03-07 12:35:24 +0800

    374

---
+++ TestLeetCode/0351_0400/374_GuessNumberHigherOrLowerUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _374_GuessNumberHigherOrLowerUnitTest
+    {
+        _374_GuessNumberHigherOrLowerAlg alg = new _374_GuessNumberHigherOrLowerAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 10, pick = 6;
+            alg.Pick=pick;
+            int exp = 6;
+            int res = alg.GuessNumber(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1, pick = 1;
+            alg.Pick = pick;
+            int exp = 1;
+            int res = alg.GuessNumber(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 2, pick = 1;
+            alg.Pick = pick;
+            int exp = 1;
+            int res = alg.GuessNumber(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int n = 2, pick = 2;
+            alg.Pick = pick;
+            int exp = 2;
+            int res = alg.GuessNumber(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/374_GuessNumberHigherOrLowerAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _374_GuessNumberHigherOrLowerAlg
+    {
+        public int Pick { get; set; }
+        public int GuessNumber(int n)
+        {
+            int left = 1;
+            int right = n;
+            while (left <= right) 
+            {
+                int mid = left + (right - left) / 2;
+                int res = guess(mid);
+                if (res == -1)
+                {
+                    right = mid - 1;
+                }
+                else if (res == 1)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+            return 0;
+        }
+
+        private int guess(int num)
+        {
+            return Pick.CompareTo(num);
+        }
+    }
+}
