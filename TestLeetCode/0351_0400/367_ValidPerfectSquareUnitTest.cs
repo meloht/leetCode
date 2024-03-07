5727267255841912f56561d2730ea7cc18e68bf5
hongtao_luo <luohongtao621@hotmail.com>
2024-03-07 12:20:55 +0800

    367

---
+++ TestLeetCode/0351_0400/367_ValidPerfectSquareUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _367_ValidPerfectSquareUnitTest
+    {
+        _367_ValidPerfectSquareAlg alg = new _367_ValidPerfectSquareAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int num = 16;
+            bool exp = true;
+            bool res = alg.IsPerfectSquare(num);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int num = 14;
+            bool exp = false;
+            bool res = alg.IsPerfectSquare(num);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/367_ValidPerfectSquareAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _367_ValidPerfectSquareAlg
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            int n = MySqrt(num);
+            if (n * n == num)
+            { 
+                return true;
+            }
+
+            return false;
+        }
+
+        public int MySqrt(int x)
+        {
+            int l = 0, r = x, ans = -1;
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                if ((long)mid * mid <= x)
+                {
+                    ans = mid;
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+            return ans;
+        }
+
+    }
+}
