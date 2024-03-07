4c08e7e73b4a3040191a8bbb2f1c698469de9e6b
hongtao_luo <luohongtao621@hotmail.com>
2024-03-07 13:07:32 +0800

    387

---
+++ TestLeetCode/0351_0400/387_FirstUniqueCharacterInAStringUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _387_FirstUniqueCharacterInAStringUnitTest
+    {
+        _387_FirstUniqueCharacterInAStringAlg alg = new _387_FirstUniqueCharacterInAStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "leetcode";
+            int exp = 0;
+            int res=alg.FirstUniqChar(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "loveleetcode";
+            int exp = 2;
+            int res = alg.FirstUniqChar(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "aabb";
+            int exp = -1;
+            int res = alg.FirstUniqChar(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/387_FirstUniqueCharacterInAStringAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _387_FirstUniqueCharacterInAStringAlg
+    {
+        public int FirstUniqChar(string s)
+        {
+            int[] dict = new int[26];
+
+            foreach (var item in s)
+            {
+                int index = item - 'a';
+                dict[index]++;
+
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                int index = s[i] - 'a';
+                if (dict[index] == 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
