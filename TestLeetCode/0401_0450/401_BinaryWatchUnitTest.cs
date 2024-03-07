13c6609be037dd56cc4d906c1d105dcee1deb92c
hongtao_luo <luohongtao621@hotmail.com>
2024-03-07 14:05:59 +0800

    401

---
+++ TestLeetCode/0401_0450/401_BinaryWatchUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _401_BinaryWatchUnitTest
+    {
+        _401_BinaryWatchAlg alg = new _401_BinaryWatchAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int turnedOn = 1;
+            string[] exp = ["0:01", "0:02", "0:04", "0:08", "0:16", "0:32", "1:00", "2:00", "4:00", "8:00"];
+            string[] res = alg.ReadBinaryWatch(turnedOn).ToArray();
+
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int turnedOn = 9;
+            string[] exp = [];
+            string[] res = alg.ReadBinaryWatch(turnedOn).ToArray();
+
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0401_0450/401_BinaryWatchAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _401_BinaryWatchAlg
+    {
+        public IList<string> ReadBinaryWatch(int turnedOn)
+        {
+            if (turnedOn > 8)
+            {
+                List<string> res = new List<string>();
+                return res;
+            }
+            HashSet<string> set = new HashSet<string>();
+            int[] h = [8, 4, 2, 1];
+            int[] m = [32, 16, 8, 4, 2, 1];
+            Dfs(h, m, 0, 0, 0, 0, turnedOn, set);
+            return set.ToList();
+        }
+
+        private void Dfs(int[] h, int[] m, int i, int j, int numH, int numM, int count, HashSet<string> res)
+        {
+            if (i >= h.Length || j >= m.Length)
+            {
+                return;
+            }
+
+            if (numH > 12 || numM > 59)
+            {
+                return;
+            }
+
+            if (count == 0)
+            {
+                res.Add($"{numH}:{numM.ToString().PadLeft(2, '0')}");
+                return;
+            }
+            int n1 = numH + h[i];
+            Dfs(h, m, i + 1, j, numH, numM, count, res);
+            Dfs(h, m, i + 1, j, n1, numM, count - 1, res);
+          
+
+            int n2 = numM + m[j];
+            Dfs(h, m, i, j + 1, numH, numM, count, res);
+            Dfs(h, m, i, j + 1, numH, n2, count - 1, res);
+
+            Dfs(h, m, i + 1, j + 1, numH, numM, count, res);
+            Dfs(h, m, i + 1, j + 1, n1, n2, count - 2, res);
+           
+        }
+    }
+}
