b28ef4e37db4ca5f417265b9e24a4c98be5dd523
hongtao_luo <luohongtao621@hotmail.com>
2024-03-08 13:58:49 +0800

    405

---
+++ TestLeetCode/0401_0450/405_ConvertANumberToHexadecimalUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _405_ConvertANumberToHexadecimalUnitTest
+    {
+        _405_ConvertANumberToHexadecimalAlg alg = new _405_ConvertANumberToHexadecimalAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int num = 26;
+            string exp = "1a";
+            string res = alg.ToHex(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int num = -1;
+            string exp = "ffffffff";
+            string res = alg.ToHex(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int num = -2147483648;
+            string exp = "80000000";
+            string res = alg.ToHex(num);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/405_ConvertANumberToHexadecimalAlg.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _405_ConvertANumberToHexadecimalAlg
+    {
+        public string ToHex(int num)
+        {
+            if (num == 0)
+                return "0";
+            char[] map = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < map.Length; i++)
+            {
+                dict.Add(map[i], i);
+            }
+            List<char> list = new List<char>();
+            long n = Math.Abs((long)num);
+            while (n > 0)
+            {
+                long nn = n % 16;
+                list.Add(map[nn]);
+                n = n / 16;
+            }
+            if (num < 0)
+            {
+                List<char> ls = new List<char>();
+                foreach (var item in list)
+                {
+                    int nn = dict[item];
+                    char f = map[15 - nn];
+                    ls.Add(f);
+                }
+                for (int i = ls.Count; i < 8; i++)
+                {
+                    ls.Add('f');
+                }
+
+                int index = 0;
+                while (true)
+                {
+                    if (index > ls.Count)
+                    {
+                        break;
+                    }
+
+                    int nn = dict[ls[index]] + 1;
+                    if (nn < 16)
+                    {
+                        ls[index] = map[nn];
+                        break;
+                    }
+                    else
+                    {
+                        ls[index] = '0';
+                    }
+                    index++;
+                }
+
+                list = ls;
+            }
+            list.Reverse();
+            return new string(list.ToArray());
+        }
+    }
+}
