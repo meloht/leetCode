a8a2e705e3fc47e4bf6fff0586f3b1f3dd708a8d
hongtao_luo <luohongtao621@hotmail.com>
2024-03-08 12:47:26 +0800

    404

---
+++ TestLeetCode/0401_0450/404_SumOfLeftLeavesUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._0401_0450;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _404_SumOfLeftLeavesUnitTest
+    {
+        _404_SumOfLeftLeavesAlg alg = new _404_SumOfLeftLeavesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [3, 9, 20, null, null, 15, 7];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 24;
+            int res = alg.SumOfLeftLeaves(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 0;
+            int res = alg.SumOfLeftLeaves(tree);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/404_SumOfLeftLeavesAlg.cs
@@ -0,0 +1,31 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _404_SumOfLeftLeavesAlg
+    {
+        int ans = 0;
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            Dfs(root);
+            return ans;
+        }
+        private void Dfs(TreeNode node)
+        {
+            if (node == null) 
+                return;
+
+            if (node.left != null && node.left.left == null && node.left.right == null)
+            {
+                ans += node.left.val;
+            }
+            Dfs(node.left);
+            Dfs(node.right);
+        }
+    }
+}
