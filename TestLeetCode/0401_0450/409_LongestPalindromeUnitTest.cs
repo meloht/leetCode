92e2027f58cf1b6bd78a4efc3c44b91bf53c85c3
hongtao luo <luohongtao621@163.com>
2024-03-09 19:06:33 +0800

    450

---
+++ TestLeetCode/0401_0450/409_LongestPalindromeUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _409_LongestPalindromeUnitTest
+    {
+        _409_LongestPalindromeAlg alg = new _409_LongestPalindromeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abccccdd";
+            int exp = 7;
+            int res = alg.LongestPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "a";
+            int exp = 1;
+            int res = alg.LongestPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "aaaaaccc";
+            int exp = 7;
+            int res = alg.LongestPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "AAAAAA";
+            int exp = 6;
+            int res = alg.LongestPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string s = "bb";
+            int exp = 2;
+            int res = alg.LongestPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/409_LongestPalindromeAlg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _409_LongestPalindromeAlg
+    {
+        public int LongestPalindrome(string s)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            foreach (var item in s)
+            {
+                if (map.ContainsKey(item))
+                {
+                    map[item]++;
+                }
+                else
+                {
+                    map.Add(item, 1);
+                }
+            }
+            int ans = 0;
+            foreach (var item in map)
+            {
+                if (item.Value > 1)
+                {
+                    if (item.Value % 2 == 0)
+                    {
+                        ans += item.Value;
+                    }
+                    else
+                    {
+                        ans = (item.Value - 1) + ans;
+                    }
+                }
+            }
+            if (ans < s.Length)
+            {
+                ans++;
+            }
+            return ans;
+        }
+    }
+}
