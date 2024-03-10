94f1ddc8eec42ca6a215f1c7e2d987fa417a013e
hongtao luo <luohongtao621@163.com>
2024-03-10 10:03:04 +0800

    412

---
+++ TestLeetCode/0401_0450/412_FizzBuzzUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _412_FizzBuzzUnitTest
+    {
+        _412_FizzBuzzAlg alg = new _412_FizzBuzzAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3;
+            string[] exp = ["1", "2", "Fizz"];
+            string[] res = alg.FizzBuzz(n).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 5;
+            string[] exp = ["1", "2", "Fizz", "4", "Buzz"];
+            string[] res = alg.FizzBuzz(n).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 15;
+            string[] exp = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"];
+            string[] res = alg.FizzBuzz(n).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0401_0450/412_FizzBuzzAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _412_FizzBuzzAlg
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            string[] arr = new string[n];
+            for (int i = 1; i <= n; i++)
+            {
+                bool flag3 = i % 3 == 0;
+                bool flag5 = i % 5 == 0;
+                if (flag3 && flag5)
+                {
+                    arr[i - 1] = "FizzBuzz";
+                }
+                else if (flag3)
+                {
+                    arr[i - 1] = "Fizz";
+                }
+                else if (flag5)
+                {
+                    arr[i - 1] = "Buzz";
+                }
+                else
+                {
+                    arr[i - 1] = i.ToString();
+                }
+            }
+            return arr;
+        }
+    }
+}
