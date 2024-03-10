350371a98b33c16c4814ce9d767910aac397a705
hongtao luo <luohongtao621@163.com>
2024-03-10 10:18:13 +0800

    414

---
+++ TestLeetCode/0401_0450/414_ThirdMaximumNumberUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _414_ThirdMaximumNumberUnitTest
+    {
+        _414_ThirdMaximumNumberAlg alg = new _414_ThirdMaximumNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 2, 1];
+            int exp = 1;
+            int res = alg.ThirdMax(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2];
+            int exp = 2;
+            int res = alg.ThirdMax(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [2, 2, 3, 1];
+            int exp = 1;
+            int res = alg.ThirdMax(nums);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [2, 2, 3, 2];
+            int exp = 3;
+            int res = alg.ThirdMax(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/414_ThirdMaximumNumberAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _414_ThirdMaximumNumberAlg
+    {
+        public int ThirdMax(int[] nums)
+        {
+            Array.Sort(nums);
+            if (nums.Length < 3)
+            {
+                return nums[nums.Length - 1];
+            }
+            int num =0;
+            int count = 0;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (count == 0)
+                {
+                    num = nums[i];
+                   
+                }
+                else
+                {
+                    if (num == nums[i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        num = nums[i];
+                    }
+                }
+                count++;
+                if (count == 3)
+                {
+                    return num;
+                }
+            }
+
+            return nums[nums.Length - 1];
+        }
+    }
+}
