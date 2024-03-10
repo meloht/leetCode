994ad2112ae365eaaeb291630e8033e22c6c14c1
hongtao luo <luohongtao621@163.com>
2024-03-10 09:47:28 +0800

    299

---
+++ TestLeetCode/0251_0300/299_BullsAndCowsUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0251_0300
+{
+    [TestClass]
+    public class _299_BullsAndCowsUnitTest
+    {
+        _299_BullsAndCowsAlg alg = new _299_BullsAndCowsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string secret = "1807", guess = "7810";
+            string exp = "1A3B";
+            string res = alg.GetHint(secret, guess);
+            Assert.AreEqual(exp, res);  
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            string secret = "1123", guess = "0111";
+            string exp = "1A1B";
+            string res = alg.GetHint(secret, guess);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string secret = "1122", guess = "1222";
+            string exp = "3A0B";
+            string res = alg.GetHint(secret, guess);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0251_0300/299_BullsAndCowsAlg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _299_BullsAndCowsAlg
+    {
+        public string GetHint(string secret, string guess)
+        {
+            int a = 0;
+            int b = 0;
+            int[] dict = new int[10];
+
+            foreach (var item in secret)
+            {
+                dict[item - '0']++;
+            }
+            for (int i = 0; i < guess.Length; i++)
+            {
+                int index = guess[i] - '0';
+                if (dict[index] > 0)
+                {
+                    if (guess[i] == secret[i])
+                    {
+                        a++;
+                        dict[index]--;
+                    }
+                }
+
+            }
+            for (int i = 0; i < guess.Length; i++)
+            {
+                int index = guess[i] - '0';
+                if (dict[index] > 0)
+                {
+                    if (guess[i] != secret[i])
+                    {
+                        b++;
+                        dict[index]--;
+                    }
+                }
+            }
+
+            return $"{a}A{b}B";
+        }
+    }
+}
