bb099307e08591cf833a40824746052e19aa43f6
hongtao_luo <luohongtao621@hotmail.com>
2024-03-11 13:50:59 +0800

    441

---
+++ TestLeetCode/0401_0450/441_ArrangingCoinsUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _441_ArrangingCoinsUnitTest
+    {
+        _441_ArrangingCoinsAlg alg = new _441_ArrangingCoinsAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 5;
+            int exp = 2;
+            int res = alg.ArrangeCoins(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 8;
+            int exp = 3;
+            int res = alg.ArrangeCoins(n);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 2;
+            int exp = 1;
+            int res = alg.ArrangeCoins(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int n = 1804289383;
+            int exp = 60070;
+            int res = alg.ArrangeCoins(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/441_ArrangingCoinsAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _441_ArrangingCoinsAlg
+    {
+        public int ArrangeCoins(int n)
+        {
+            long left = 1;
+            long right = n;
+            while (left <= right)
+            {
+                long mid = left + (right - left) / 2;
+                long num = (mid + 1) * mid / 2;
+                if (num > n)
+                {
+                    right = mid - 1;
+                }
+                else if (num == n)
+                {
+                    return (int)mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+
+            }
+            return (int)left - 1;
+        }
+
+        public int ArrangeCoins2(int n)
+        {
+            int left = 1, right = n;
+            while (left < right)
+            {
+                int mid = (right - left + 1) / 2 + left;
+                if ((long)mid * (mid + 1) <= (long)2 * n)
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+
+
+    }
+}
