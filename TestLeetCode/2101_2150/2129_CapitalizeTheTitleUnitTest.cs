7fde062c0d6d0ec93cdd64fd8fd2bb60c0fa33d3
hongtao_luo <luohongtao621@hotmail.com>
2024-03-11 13:03:53 +0800

    2129

---
+++ TestLeetCode/2101_2150/2129_CapitalizeTheTitleUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2101_2150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2101_2150
+{
+    [TestClass]
+    public class _2129_CapitalizeTheTitleUnitTest
+    {
+        _2129_CapitalizeTheTitleAlg alg = new _2129_CapitalizeTheTitleAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string title = "capiTalIze tHe titLe";
+            string exp = "Capitalize The Title";
+            string res = alg.CapitalizeTitle(title);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string title = "First leTTeR of EACH Word";
+            string exp = "First Letter of Each Word";
+            string res = alg.CapitalizeTitle(title);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string title = "i lOve leetcode";
+            string exp = "i Love Leetcode";
+            string res = alg.CapitalizeTitle(title);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2101_2150/2129_CapitalizeTheTitleAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2101_2150
+{
+    public class _2129_CapitalizeTheTitleAlg
+    {
+        public string CapitalizeTitle(string title)
+        {
+            string tt = title.ToLower();
+
+            char[] chars = new char[tt.Length];
+
+            int i = 0;
+            while (i < tt.Length)
+            {
+                int begin = i;
+                int count = 0;
+                while (i < tt.Length && tt[i] != ' ')
+                {
+                    chars[i] = tt[i];
+                    count++;
+                    i++;
+                }
+                if (count > 2)
+                {
+                    chars[begin] = char.ToUpper(chars[begin]);
+                }
+                if (i < tt.Length)
+                {
+                    chars[i] = tt[i];
+                }
+
+                i++;
+
+            }
+            return new string(chars);
+        }
+    }
+}
