999212cfe29bfce9139a599dcb23accf03e19840
hongtao_luo <luohongtao621@hotmail.com>
2024-03-12 13:57:02 +0800

    476

---
+++ TestLeetCode/0451_0500/476_NumberComplementUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _476_NumberComplementUnitTest
+    {
+        _476_NumberComplementAlg alg = new _476_NumberComplementAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int num = 5;
+            int exp = 2;
+            int res = alg.FindComplement(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int num = 1;
+            int exp = 0;
+            int res = alg.FindComplement(num);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int num = int.MaxValue;
+            int exp = 0;
+            int res = alg.FindComplement(num);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/476_NumberComplementAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _476_NumberComplementAlg
+    {
+        public int FindComplement(int num)
+        {
+            string x = Convert.ToString(num, 2);
+            char[] c = x.ToCharArray();
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == '1')
+                {
+                    c[i] = '0';
+                }
+                else
+                {
+                    c[i] = '1';
+                }
+            }
+            string ss = new string(c);
+            int n = Convert.ToInt32(ss, 2);
+            return n;
+        }
+    }
+}
