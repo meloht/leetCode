1d76b534c777673874b9f5e7cd1299b73ff60204
hongtao_luo <luohongtao621@hotmail.com>
2024-03-12 12:45:35 +0800

    461

---
+++ TestLeetCode/0451_0500/461_HammingDistanceUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _461_HammingDistanceUnitTest
+    {
+        _461_HammingDistanceAlg alg = new _461_HammingDistanceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int x = 1, y = 4;
+            int exp = 2;
+            int res=alg.HammingDistance(x,y);
+            Assert.AreEqual(exp,res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int x = 3, y = 1;
+            int exp = 1;
+            int res = alg.HammingDistance(x, y);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/461_HammingDistanceAlg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+
+    public class _461_HammingDistanceAlg
+    {
+        public int HammingDistance(int x, int y)
+        {
+            var num = x ^ y;
+            int ans = 0;
+            while (num > 0)
+            {
+                num = num & (num - 1);
+                ans++;
+            }
+            return ans;
+        }
+    }
+}
