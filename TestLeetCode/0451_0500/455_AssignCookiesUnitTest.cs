1c6691f081771620a201e2f1934e81cfb56f0d6a
hongtao_luo <luohongtao621@hotmail.com>
2024-03-12 12:34:32 +0800

    455

---
+++ TestLeetCode/0451_0500/455_AssignCookiesUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _455_AssignCookiesUnitTest
+    {
+        _455_AssignCookiesAlg alg = new _455_AssignCookiesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] g = [1, 2, 3], s = [1, 1];
+            int exp = 1;
+            int res = alg.FindContentChildren(g, s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] g = [1, 2], s = [1, 2, 3];
+            int exp = 2;
+            int res = alg.FindContentChildren(g, s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] g = [1, 2, 3], s = [3];
+            int exp = 1;
+            int res = alg.FindContentChildren(g, s);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0451_0500/455_AssignCookiesAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _455_AssignCookiesAlg
+    {
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+            int ans = 0;
+            int index = 0;
+            for (int i = 0; i < g.Length; i++)
+            {
+                for (int j = index; j < s.Length; j++)
+                {
+                    if (s[j] >= g[i])
+                    {
+                        ans++;
+                        index = j + 1;
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
