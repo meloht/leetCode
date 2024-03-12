ab5cd9b45a330c5606ee8dfa6ec5d1ab2cfc606d
hongtao_luo <luohongtao621@hotmail.com>
2024-03-12 11:58:55 +0800

    704

---
+++ TestLeetCode/0701_0750/704_BinarySearchUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _704_BinarySearchUnitTest
+    {
+        _704_BinarySearchAlg alg = new _704_BinarySearchAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [-1, 0, 3, 5, 9, 12];
+            int target = 9;
+            int exp = 4;
+            int res = alg.Search(nums, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [-1, 0, 3, 5, 9, 12];
+            int target = 2;
+            int exp = -1;
+            int res = alg.Search(nums, target);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0701_0750/704_BinarySearchAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _704_BinarySearchAlg
+    {
+        public int Search(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right) 
+            {
+                int mid = (right - left) / 2 + left;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+
+            }
+
+            return -1;
+        }
+    }
+}
