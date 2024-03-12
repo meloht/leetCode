e0d8071e231e6c0f43d64cdd3f3488c6e830fcbe
hongtao_luo <luohongtao621@hotmail.com>
2024-03-12 12:21:14 +0800

    448

---
+++ TestLeetCode/0401_0450/448_FindAllNumbersDisappearedInAnArrayUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _448_FindAllNumbersDisappearedInAnArrayUnitTest
+    {
+        _448_FindAllNumbersDisappearedInAnArrayAlg alg = new _448_FindAllNumbersDisappearedInAnArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 3, 2, 7, 8, 2, 3, 1];
+            int[] exp = [5, 6];
+            int[] res = alg.FindDisappearedNumbers(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1];
+            int[] exp = [2];
+            int[] res = alg.FindDisappearedNumbers(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0401_0450/448_FindAllNumbersDisappearedInAnArrayAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _448_FindAllNumbersDisappearedInAnArrayAlg
+    {
+        public IList<int> FindDisappearedNumbers1(int[] nums)
+        {
+            List<int> list = new List<int>();
+            HashSet<int> set = new HashSet<int>(nums);
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                if (!set.Contains(i))
+                {
+                    list.Add(i);
+                }
+            }
+            return list;
+        }
+
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            List<int> list = new List<int>();
+            foreach (var item in nums)
+            {
+                int n = (item - 1) % nums.Length;
+                nums[n] += nums.Length;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] <= nums.Length)
+                {
+                    list.Add(i + 1);
+                }
+            }
+            return list;
+        }
+    }
+}
