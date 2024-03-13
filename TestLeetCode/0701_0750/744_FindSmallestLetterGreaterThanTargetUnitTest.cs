10a87b3d7b293f0b22d4ea157bed854bdc6c3528
hongtao_luo <luohongtao621@hotmail.com>
2024-03-13 12:31:10 +0800

    744

---
+++ TestLeetCode/0701_0750/744_FindSmallestLetterGreaterThanTargetUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _744_FindSmallestLetterGreaterThanTargetUnitTest
+    {
+        _744_FindSmallestLetterGreaterThanTargetAlg alg = new _744_FindSmallestLetterGreaterThanTargetAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[] letters = ['c', 'f', 'j'];
+            char target = 'a';
+            char exp = 'c';
+            char res = alg.NextGreatestLetter(letters, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[] letters = ['c', 'f', 'j'];
+            char target = 'c';
+            char exp = 'f';
+            char res = alg.NextGreatestLetter(letters, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            char[] letters = ['x', 'x', 'y', 'y'];
+            char target = 'z';
+            char exp = 'x';
+            char res = alg.NextGreatestLetter(letters, target);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0701_0750/744_FindSmallestLetterGreaterThanTargetAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _744_FindSmallestLetterGreaterThanTargetAlg
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            if (target >= letters[letters.Length - 1])
+                return letters[0];
+            int left = 0;
+            int right = letters.Length - 1;
+
+            while (left < right)
+            {
+                int mid = (right - left) / 2 + left;
+                char c = letters[mid];
+                if (c <= target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            return letters[left];
+        }
+    }
+}
