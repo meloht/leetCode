546b1d78bf75197783a1955497efd38643475837
hongtao_luo <luohongtao621@hotmail.com>
2024-03-13 11:59:40 +0800

    2864

---
+++ TestLeetCode/2851_2900/2864_MaximumOddBinaryNumberUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._2851_2900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2851_2900
+{
+    [TestClass]
+    public class _2864_MaximumOddBinaryNumberUnitTest
+    {
+        _2864_MaximumOddBinaryNumberAlg alg = new _2864_MaximumOddBinaryNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "010";
+            string exp = "001";
+            string res = alg.MaximumOddBinaryNumber(s);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "0101";
+            string exp = "1001";
+            string res = alg.MaximumOddBinaryNumber(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2851_2900/2864_MaximumOddBinaryNumberAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2851_2900
+{
+    public class _2864_MaximumOddBinaryNumberAlg
+    {
+        public string MaximumOddBinaryNumber(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = s.Count(p => p == '1');
+            if (count > 1)
+            {
+                for (int i = 1; i < count; i++)
+                {
+                    sb.Append("1");
+                }
+            }
+          
+            int zero = s.Length - count;
+            for (int i = 0; i < zero; i++)
+            {
+                sb.Append("0");
+            }
+            sb.Append("1");
+
+            return sb.ToString();
+        }
+    }
+}
