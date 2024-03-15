3f806e58fe9274fd87aee9dc5ead28fef261cea4
hongtao_luo <luohongtao621@hotmail.com>
2024-03-15 13:12:25 +0800

    1539

---
+++ TestLeetCode/1501_1550/1539_KthMissingPositiveNumberUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1501_1550
+{
+    [TestClass]
+    public class _1539_KthMissingPositiveNumberUnitTest
+    {
+        _1539_KthMissingPositiveNumberAlg alg = new _1539_KthMissingPositiveNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [2, 3, 4, 7, 11];
+            int k = 5;
+            int exp = 9;
+            int res = alg.FindKthPositive(arr, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [1, 2, 3, 4];
+            int k = 2;
+            int exp = 6;
+            int res = alg.FindKthPositive(arr, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1501_1550/1539_KthMissingPositiveNumberAlg.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1501_1550
+{
+    public class _1539_KthMissingPositiveNumberAlg
+    {
+        public int FindKthPositive1(int[] arr, int k)
+        {
+            HashSet<int> set = new HashSet<int>(arr);
+            int max = arr.Max() + k;
+            int count = 0;
+            for (int i = 1; i <= max; i++)
+            {
+                if (set.Add(i))
+                {
+                    count++;
+                }
+                if (count == k)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int FindKthPositive2(int[] arr, int k)
+        {
+            int index = 0;
+            int missNum = -1;
+            int current = 1;
+            for (int i = 0; i <k; current++)
+            {
+                if (current == arr[index])
+                {
+                    index = (index + 1 < arr.Length) ? index + 1 : index;
+                }
+                else
+                {
+                    i++;
+                    missNum = current;
+                }
+               
+            }
+            return missNum;
+        }
+
+        public int FindKthPositive(int[] arr, int k)
+        {
+            if (arr[0] > k)
+            {
+                return k;
+            }
+
+            int l = 0, r = arr.Length;
+            while (l < r)
+            {
+                int mid = (l + r) >> 1;
+                int x = mid < arr.Length ? arr[mid] : int.MaxValue;
+                if (x - mid - 1 >= k)
+                {
+                    r = mid;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+
+            return k - (arr[l - 1] - (l - 1) - 1) + arr[l - 1];
+        }
+
+    }
+}
