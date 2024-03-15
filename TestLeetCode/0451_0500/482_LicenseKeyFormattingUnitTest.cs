e82c685e69cfe1f8bacd71d9db6ac7d8430c6f67
hongtao luo <luohongtao621@163.com>
2024-03-15 19:55:22 +0800

    482

---
+++ TestLeetCode/0451_0500/482_LicenseKeyFormattingUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0451_0500;
+using leetCode.WeeklyContest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _482_LicenseKeyFormattingUnitTest
+    {
+        _482_LicenseKeyFormattingAlg alg = new _482_LicenseKeyFormattingAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string S = "5F3Z-2e-9-w";
+            int k = 4;
+            string exp = "5F3Z-2E9W";
+            string res = alg.LicenseKeyFormatting(S, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string S = "2-5g-3-J";
+            int k = 2;
+            string exp = "2-5G-3J";
+            string res = alg.LicenseKeyFormatting(S, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/482_LicenseKeyFormattingAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _482_LicenseKeyFormattingAlg
+    {
+        public string LicenseKeyFormatting(string s, int k)
+        {
+            int count = s.Count(p => p != '-');
+            int d = count % k;
+            int first = k;
+            if (d > 0)
+            {
+                first = d;
+            }
+            StringBuilder sb = new StringBuilder();
+
+            int num = 0;
+            int i = 0;
+            for (; i < s.Length; i++)
+            {
+
+                if (s[i] != '-')
+                {
+                    sb.Append(char.ToUpper(s[i]));
+                    num++;
+                }
+                if (num == first)
+                {
+                    break;
+                }
+            }
+            List<string> list = new List<string>();
+            list.Add(sb.ToString());
+            sb.Clear();
+            num = 0;
+
+            for (i = i + 1; i < s.Length; i++)
+            {
+                if (s[i] != '-')
+                {
+                    sb.Append(char.ToUpper(s[i]));
+                    num++;
+                }
+                if (num == k)
+                {
+                    num = 0;
+                    list.Add(sb.ToString());
+                    sb.Clear();
+                }
+            }
+
+            return string.Join("-", list);
+        }
+    }
+}
