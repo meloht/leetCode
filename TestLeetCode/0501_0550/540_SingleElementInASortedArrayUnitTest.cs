5e4e240f74159d80ef62b69264dd3627bf1776c3
hongtao luo <luohongtao621@163.com>
2024-03-16 16:01:49 +0800

    540

---
+++ TestLeetCode/0501_0550/540_SingleElementInASortedArrayUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _540_SingleElementInASortedArrayUnitTest
+    {
+        _540_SingleElementInASortedArrayAlg alg = new _540_SingleElementInASortedArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 1, 2, 3, 3, 4, 4, 8, 8];
+            int exp = 2;
+            int res = alg.SingleNonDuplicate(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [3, 3, 7, 7, 10, 11, 11];
+            int exp = 10;
+            int res = alg.SingleNonDuplicate(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/540_SingleElementInASortedArrayAlg.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _540_SingleElementInASortedArrayAlg
+    {
+        public int SingleNonDuplicate(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (right - left) / 2 + left;
+
+                int low = mid - 1;
+                int high = mid + 1;
+                if (low >= 0 && nums[mid] == nums[low])
+                {
+                    if ((mid + 1) % 2 == 0)
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 2;
+                    }
+                }
+                else if (high < nums.Length && nums[mid] == nums[high])
+                {
+                    if ((mid + 2) % 2 == 0)
+                    {
+                        left = mid + 2;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+                else
+                {
+                    return nums[mid];
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// mid是偶数 mid+1=mid^1，mid是奇数 mid-1=mid^1
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+
+        public int SingleNonDuplicate1(int[] nums)
+        {
+            int low = 0, high = nums.Length - 1;
+            while (low < high)
+            {
+                int mid = (high - low) / 2 + low;
+                if (nums[mid] == nums[mid ^ 1])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+            return nums[low];
+        }
+
+        /// <summary>
+        ///  mid是偶数 mid&1=0，mid是奇数 mid&1=1
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int SingleNonDuplicate2(int[] nums)
+        {
+            int low = 0, high = nums.Length - 1;
+            while (low < high)
+            {
+                int mid = (high - low) / 2 + low;
+                mid -= mid & 1;
+                if (nums[mid] == nums[mid + 1])
+                {
+                    low = mid + 2;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+            return nums[low];
+        }
+
+
+    }
+}
