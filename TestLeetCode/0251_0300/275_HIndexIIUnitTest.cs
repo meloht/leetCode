6adaa970f251ccd631456ee2830d1b6308ed14e1
hongtao luo <luohongtao621@163.com>
2024-03-16 15:28:33 +0800

    275

---
+++ TestLeetCode/0251_0300/275_HIndexIIUnitTest.cs
@@ -0,0 +1,71 @@
+﻿using leetCode._0251_0300;
+using leetCode.WeeklyContest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0251_0300
+{
+    [TestClass]
+    public class _275_HIndexIIUnitTest
+    {
+        _275_HIndexIIAlg alg = new _275_HIndexIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] citations = [0, 1, 3, 5, 6];
+            int exp = 3;
+            int res = alg.HIndex(citations);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] citations = [1, 2, 100];
+            int exp = 2;
+            int res = alg.HIndex(citations);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] citations = [0];
+            int exp = 0;
+            int res = alg.HIndex(citations);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] citations = [100];
+            int exp = 1;
+            int res = alg.HIndex(citations);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] citations = [11, 15];
+            int exp = 2;
+            int res = alg.HIndex(citations);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            int[] citations = [0, 0, 4, 4];
+            int exp = 2;
+            int res = alg.HIndex(citations);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0251_0300/275_HIndexIIAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _275_HIndexIIAlg
+    {
+        public int HIndex(int[] citations)
+        {
+            if (citations.Length == 0)
+                return 0;
+            if (citations.Length == 1)
+                return Math.Min(1, citations[0]);
+            if (citations[0] >= citations.Length)
+                return citations.Length;
+            int left = 0;
+            int right = citations.Length - 1;
+
+            int ans = 0;
+            while (left <= right)
+            {
+                int mid = (right - left) / 2 + left;
+                int midVal = citations[mid];
+                int count = citations.Length - mid;
+
+                if (midVal < count)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    ans = count;
+                    right = mid - 1;
+                }
+
+            }
+            return ans;
+        }
+    }
+}
