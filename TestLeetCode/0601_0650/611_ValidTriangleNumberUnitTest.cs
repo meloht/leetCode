e7da97ee4f1b3ea38fad4155baf9f21679f0fb68
hongtao luo <luohongtao621@163.com>
2024-03-17 12:40:06 +0800

    611

---
+++ TestLeetCode/0601_0650/611_ValidTriangleNumberUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _611_ValidTriangleNumberUnitTest
+    {
+        _611_ValidTriangleNumberAlg alg = new _611_ValidTriangleNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 2, 3, 4];
+            int exp = 3;
+            int res = alg.TriangleNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [4, 2, 3, 4];
+            int exp = 4;
+            int res = alg.TriangleNumber(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/611_ValidTriangleNumberAlg.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _611_ValidTriangleNumberAlg
+    {
+        public int TriangleNumber1(int[] nums)
+        {
+            Array.Sort(nums);
+            int ans = 0;
+            int i = 0;
+            int len = nums.Length - 2;
+            int len1 = nums.Length - 1;
+            while (i < len)
+            {
+                int n1 = nums[i];
+                int j = i + 1;
+                while (j < len1)
+                {
+                    int n2 = nums[j];
+                    int nn = n1 + n2;
+
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        if (nums[k] < nn)
+                        {
+                            ans++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    j++;
+
+                }
+
+                i++;
+            }
+            return ans;
+        }
+
+        public int TriangleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            int ans = 0;
+            int i = 0;
+            int len = nums.Length - 2;
+            int len1 = nums.Length - 1;
+            while (i < len)
+            {
+                int n1 = nums[i];
+                int j = i + 1;
+                while (j < len1)
+                {
+                    int n2 = nums[j];
+                    int nn = n1 + n2;
+
+                    int left = j + 1;
+                    int right = nums.Length - 1;
+                    int kk = j;
+                    while (left <= right) 
+                    {
+                        int mid = (left + right) / 2;
+                        if (nums[mid] < nn)
+                        {
+                            kk = mid;
+                            left = mid + 1;
+                        }
+                        else
+                        {
+                            right = mid - 1;
+                        }
+                    }
+                    ans += kk - j;
+
+                    j++;
+
+                }
+
+                i++;
+            }
+            return ans;
+        }
+    }
+}
