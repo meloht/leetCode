3b718f9679b88f42e7f9bc4c5c882c73db68aedd
hongtao luo <luohongtao621@163.com>
2024-03-17 13:24:31 +0800

    2300

---
+++ TestLeetCode/2301_2350/2300_SuccessfulPairsOfSpellsAndPotionsUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2301_2350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2301_2350
+{
+    [TestClass]
+    public class _2300_SuccessfulPairsOfSpellsAndPotionsUnitTest
+    {
+        _2300_SuccessfulPairsOfSpellsAndPotionsAlg alg = new _2300_SuccessfulPairsOfSpellsAndPotionsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] spells = [5, 1, 3], potions = [1, 2, 3, 4, 5];
+            int success = 7;
+            int[] exp = [4, 0, 3];
+            int[] res = alg.SuccessfulPairs(spells, potions, success);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] spells = [3, 1, 2], potions = [8, 5, 8];
+            int success = 16;
+            int[] exp = [2, 0, 2];
+            int[] res = alg.SuccessfulPairs(spells, potions, success);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2301_2350/2300_SuccessfulPairsOfSpellsAndPotionsAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2301_2350
+{
+    public class _2300_SuccessfulPairsOfSpellsAndPotionsAlg
+    {
+        public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+        {
+            Array.Sort(potions);
+            int[] ans = new int[spells.Length];
+            for (int i = 0; i < spells.Length; i++)
+            {
+                var s = (long)spells[i];
+                int left = 0;
+                int right = potions.Length - 1;
+                int nn = potions.Length;
+                while (left <= right)
+                {
+                    int mid = (right - left) / 2 + left;
+                    long num = s * (long)potions[mid];
+
+                    if (num < success)
+                    {
+                        left = mid + 1;
+
+                    }
+                    else
+                    {
+                        nn = mid;
+                        right = mid - 1;
+                    }
+                }
+                ans[i] = potions.Length - nn;
+            }
+            return ans;
+        }
+    }
+}
