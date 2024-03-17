75da607986eb86047fdead537279b5f03938d339
hongtao luo <luohongtao621@163.com>
2024-03-17 15:17:27 +0800

    1498

---
+++ TestLeetCode/1451_1500/1498_NumberOfSubsequencesThatSatisfyTheGivenSumConditionUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using leetCode._1451_1500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1451_1500
+{
+    [TestClass]
+    public class _1498_NumberOfSubsequencesThatSatisfyTheGivenSumConditionUnitTest
+    {
+        _1498_NumberOfSubsequencesThatSatisfyTheGivenSumConditionAlg alg = new _1498_NumberOfSubsequencesThatSatisfyTheGivenSumConditionAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 5, 6, 7];
+            int target = 9;
+            int exp = 4;
+            int res=alg.NumSubseq(nums, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [3, 3, 6, 8];
+            int target = 10;
+            int exp = 6;
+            int res = alg.NumSubseq(nums, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [2, 3, 3, 4, 6, 7];
+            int target = 12;
+            int exp = 61;
+            int res = alg.NumSubseq(nums, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [14, 4, 6, 6, 20, 8, 5, 6, 8, 12, 6, 10, 14, 9, 17, 16, 9, 7, 14, 11, 14, 15, 13, 11, 10, 18, 13, 17, 17, 14, 17, 7, 9, 5, 10, 13, 8, 5, 18, 20, 7, 5, 5, 15, 19, 14];
+            int target = 22;
+            int exp = 272187084;
+            int res = alg.NumSubseq(nums, target);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1451_1500/1498_NumberOfSubsequencesThatSatisfyTheGivenSumConditionAlg.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1451_1500
+{
+    public class _1498_NumberOfSubsequencesThatSatisfyTheGivenSumConditionAlg
+    {
+        static int Mod = 1_000_000_007;
+        public int NumSubseq(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int ans = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int tt = target - nums[i];
+                if (tt < 0)
+                {
+                    break;
+                }
+                if (tt < nums[i])
+                {
+                    break;
+                }
+                int left = i;
+                int right = nums.Length - 1;
+                int kk = i;
+                while (left <= right)
+                {
+                    int mid = (right - left) / 2 + left;
+                    int num = nums[mid];
+                    if (num <= tt)
+                    {
+                        kk = mid;
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+                int numm = (int)(MyPow(2, kk - i) % Mod);
+                ans = (ans + numm) % Mod;
+
+            }
+
+
+            return ans;
+        }
+
+        private long MyPow(long x, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            if (n % 2 == 0)
+            {
+                var num = (x * x) % Mod;
+                return MyPow(num, n / 2);
+            }
+            else
+            {
+                var num = MyPow((x * x) % Mod, n / 2) % Mod;
+                if (n > 0)
+                {
+                    num = (num * x) % Mod;
+                }
+                return num;
+            }
+        }
+
+
+    }
+
+
+}
