61607b809ccf7dd7ee50335a6cb391e9097bb5b1
hongtao_luo <luohongtao621@hotmail.com>
2024-03-21 14:01:55 +0800

    282

---
+++ TestLeetCode/0251_0300/282_ExpressionAddOperatorsUnitTest.cs
@@ -0,0 +1,77 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0251_0300
+{
+    [TestClass]
+    public class _282_ExpressionAddOperatorsUnitTest
+    {
+        _282_ExpressionAddOperatorsAlg alg = new _282_ExpressionAddOperatorsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string num = "123";
+            int target = 6;
+            string[] exp = ["1+2+3", "1*2*3"];
+            string[] res = alg.AddOperators(num,target).ToArray();
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string num = "232";
+            int target = 8;
+            string[] exp = ["2*3+2", "2+3*2"];
+            string[] res = alg.AddOperators(num, target).ToArray();
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string num = "3456237490";
+            int target = 9191;
+            string[] exp = [];
+            string[] res = alg.AddOperators(num, target).ToArray();
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string num = "1234567890";
+            int target = 9191;
+            string[] exp = [];
+            string[] res = alg.AddOperators(num, target).ToArray();
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string num = "105";
+            int target = 5;
+            string[] exp = ["1*0+5", "10-5"];
+            string[] res = alg.AddOperators(num, target).ToArray();
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+    }
+}
---
+++ leetCode/0251_0300/282_ExpressionAddOperatorsAlg.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _282_ExpressionAddOperatorsAlg
+    {
+        int Target = 0;
+        static string[] op = ["+", "-", "*"];
+
+        public IList<string> AddOperators(string num, int target)
+        {
+            Target = target;
+  
+            List<string> res = new List<string>();
+            List<string> list = new List<string>();
+            list.Add(num[0].ToString());
+            Dfs(num, 1, list, res);
+            return res;
+        }
+        private void Dfs(string num, int index, List<string> list, List<string> res)
+        {
+
+            if (index >= num.Length)
+            {
+                string format = string.Join("", list);
+                int n = Calculate(format);
+                if (n == this.Target)
+                {
+                    res.Add(format);
+                }
+                return;
+            }
+            for (int i = index; i < num.Length; i++)
+            {
+                foreach (var item in op)
+                {
+                    list.Add(item);
+                    list.Add(num.Substring());
+
+                    Dfs(num, index + 1, list, res);
+
+                    list.RemoveAt(list.Count - 1);
+                    list.RemoveAt(list.Count - 1);
+                }
+            }
+           
+
+        }
+
+
+        public int Calculate(string s)
+        {
+            Stack<int> stack = new Stack<int>();
+            char preSign = '+';
+            int num = 0;
+            int n = s.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    num = num * 10 + s[i] - '0';
+                }
+                if (!char.IsDigit(s[i]) && s[i] != ' ' || i == n - 1)
+                {
+                    switch (preSign)
+                    {
+                        case '+':
+                            stack.Push(num);
+                            break;
+                        case '-':
+                            stack.Push(-num);
+                            break;
+                        case '*':
+                            stack.Push(stack.Pop() * num);
+                            break;
+                        default:
+                            stack.Push(stack.Pop() / num);
+                            break;
+                    }
+                    preSign = s[i];
+                    num = 0;
+                }
+            }
+            int ans = 0;
+            while (stack.Count > 0)
+            {
+                ans += stack.Pop();
+            }
+            return ans;
+        }
+    }
+}
