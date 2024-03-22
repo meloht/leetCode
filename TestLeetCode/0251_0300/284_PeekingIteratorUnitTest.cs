72cc1d1bc3d46bebe8980172ead45000ecf3dbef
hongtao_luo <luohongtao621@hotmail.com>
2024-03-22 12:46:06 +0800

    284

---
+++ TestLeetCode/0251_0300/284_PeekingIteratorUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Test._0251_0300
+{
+    [TestClass]
+    public class _284_PeekingIteratorUnitTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            List<int> nums = [1, 2, 3];
+            _284_PeekingIteratorAlg.PeekingIterator peekingIterator = new _284_PeekingIteratorAlg.PeekingIterator(nums.GetEnumerator());
+            Assert.AreEqual(1, peekingIterator.Next());    // 返回 1 ，指针移动到下一个元素 [1,2,3]
+            Assert.AreEqual(2, peekingIterator.Peek());    // 返回 2 ，指针未发生移动 [1,2,3]
+            Assert.AreEqual(2, peekingIterator.Next());    // 返回 2 ，指针移动到下一个元素 [1,2,3]
+            Assert.AreEqual(3, peekingIterator.Next());    // 返回 3 ，指针移动到下一个元素 [1,2,3]
+            Assert.AreEqual(false, peekingIterator.HasNext()); // 返回 False
+        }
+    }
+}
---
+++ leetCode/0251_0300/284_PeekingIteratorAlg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0251_0300
+{
+    public class _284_PeekingIteratorAlg
+    {
+        public class PeekingIterator
+        {
+            bool flag = false;
+            IEnumerator<int> enumerator;
+            // iterators refers to the first element of the array.
+            public PeekingIterator(IEnumerator<int> iterator)
+            {
+                enumerator = iterator;
+
+                flag = enumerator.MoveNext();
+            }
+
+            // Returns the next element in the iteration without advancing the iterator.
+            public int Peek()
+            {
+                return this.enumerator.Current;
+
+            }
+
+            // Returns the next element in the iteration and advances the iterator.
+            public int Next()
+            {
+                int res = this.enumerator.Current;
+                flag = this.enumerator.MoveNext();
+                return res;
+
+            }
+
+            // Returns false if the iterator is refering to the end of the array of true otherwise.
+            public bool HasNext()
+            {
+                return flag;
+            }
+        }
+    }
+}
