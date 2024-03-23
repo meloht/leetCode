c7d9d500bb805dc4e740c3309a390ee27c3a495f
hongtao luo <luohongtao621@163.com>
2024-03-23 11:04:37 +0800

    2549

---
+++ TestLeetCode/2501_2550/2549_CountDistinctNumbersOnBoardUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._2501_2550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2501_2550
+{
+    [TestClass]
+    public class _2549_CountDistinctNumbersOnBoardUnitTest
+    {
+        _2549_CountDistinctNumbersOnBoardAlg alg = new _2549_CountDistinctNumbersOnBoardAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 5;
+            int exp = 4;
+            int res=alg.DistinctIntegers(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 3;
+            int exp = 2;
+            int res = alg.DistinctIntegers(n);
+            Assert.AreEqual(exp, res);
+        }
+       
+    }
+}
---
+++ leetCode/2501_2550/2549_CountDistinctNumbersOnBoardAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2501_2550
+{
+    public class _2549_CountDistinctNumbersOnBoardAlg
+    {
+        public int DistinctIntegers1(int n)
+        {
+            HashSet<int> result = new HashSet<int>();
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(n);
+            result.Add(n);
+            while (queue.Count > 0)
+            {
+                int num = queue.Dequeue();
+
+                for (int j = 1; j <= num; j++)
+                {
+                    if (num % j == 1 && result.Add(j))
+                    {
+                        queue.Enqueue(j);
+                    }
+
+                }
+            }
+            return result.Count;
+        }
+        public int DistinctIntegers(int n)
+        {
+            return n == 1 ? 1 : n - 1;
+        }
+
+
+    }
+}
