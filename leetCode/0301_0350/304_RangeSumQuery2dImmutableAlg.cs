d2c4c1ab0924730e6e915b2814c640373de8fdcf
hongtao luo <luohongtao621@163.com>
2024-03-23 12:13:00 +0800

    304

--- leetCode/0301_0350/304_RangeSumQuery2dImmutableAlg.cs
+++ leetCode/0301_0350/304_RangeSumQuery2dImmutableAlg.cs
@@ -15,22 +15,13 @@ namespace leetCode._0301_0350
             public NumMatrix(int[][] matrix)
             {
                 int len = matrix[0].Length;
-                sumMatrix = new int[matrix.Length, len];
-                sumMatrix[0, 0] = matrix[0][0];
-                for (int i = 1; i < len; i++)
-                {
-                    sumMatrix[0, i] = sumMatrix[0, i - 1] + matrix[0][i];
-                }
-                for (int i = 1; i < matrix.Length; i++)
-                {
-                    sumMatrix[i, 0] = sumMatrix[i - 1, 0] + matrix[i][0];
-                }
+                sumMatrix = new int[matrix.Length + 1, len + 1];
 
-                for (int i = 1; i < matrix.Length; i++)
+                for (int i = 0; i < matrix.Length; i++)
                 {
-                    for (int j = 1; j < len; j++)
+                    for (int j = 0; j < len; j++)
                     {
-                        sumMatrix[i, j] = matrix[i][j] - sumMatrix[i - 1, j - 1] + sumMatrix[i - 1, j] + sumMatrix[i, j - 1];
+                        sumMatrix[i + 1, j + 1] = matrix[i][j] - sumMatrix[i, j] + sumMatrix[i, j + 1] + sumMatrix[i + 1, j];
                     }
                 }
 
@@ -38,19 +29,8 @@ namespace leetCode._0301_0350
 
             public int SumRegion(int row1, int col1, int row2, int col2)
             {
-                int sum = sumMatrix[row2, col2];
-                if (col1 - 1 >= 0)
-                {
-                    sum -= sumMatrix[row2, col1 - 1];
-                }
-                if (row1 - 1 >= 0)
-                {
-                    sum -= sumMatrix[row1 - 1, col2];
-                }
-                if (col1 - 1 >= 0 && row1 - 1 >= 0)
-                {
-                    sum += sumMatrix[row1 - 1, col1 - 1];
-                }
+                int sum = sumMatrix[row2 + 1, col2 + 1] - sumMatrix[row2 + 1, col1] - sumMatrix[row1, col2 + 1] + sumMatrix[row1, col1];
+
                 return sum;
             }
         }
