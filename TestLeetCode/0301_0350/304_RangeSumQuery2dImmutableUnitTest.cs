ee09b5ea0267ea4f6f0ca19e8e981d7595eb0bfd
hongtao luo <luohongtao621@163.com>
2024-03-23 12:04:53 +0800

    304

---
+++ TestLeetCode/0301_0350/304_RangeSumQuery2dImmutableUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _304_RangeSumQuery2dImmutableUnitTest
+    {
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] matrix = [[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]];
+            _304_RangeSumQuery2dImmutableAlg.NumMatrix numMatrix = new(matrix);
+            Assert.AreEqual(8, numMatrix.SumRegion(2, 1, 4, 3)); // return 8 (红色矩形框的元素总和)
+            Assert.AreEqual(11, numMatrix.SumRegion(1, 1, 2, 2)); // return 11 (绿色矩形框的元素总和)
+            Assert.AreEqual(12, numMatrix.SumRegion(1, 2, 2, 4)); // return 12 (蓝色矩形框的元素总和)
+        }
+    }
+}
---
+++ leetCode/0301_0350/304_RangeSumQuery2dImmutableAlg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _304_RangeSumQuery2dImmutableAlg
+    {
+        public class NumMatrix
+        {
+
+            int[,] sumMatrix;
+            public NumMatrix(int[][] matrix)
+            {
+                int len = matrix[0].Length;
+                sumMatrix = new int[matrix.Length, len];
+                sumMatrix[0, 0] = matrix[0][0];
+                for (int i = 1; i < len; i++)
+                {
+                    sumMatrix[0, i] = sumMatrix[0, i - 1] + matrix[0][i];
+                }
+                for (int i = 1; i < matrix.Length; i++)
+                {
+                    sumMatrix[i, 0] = sumMatrix[i - 1, 0] + matrix[i][0];
+                }
+
+                for (int i = 1; i < matrix.Length; i++)
+                {
+                    for (int j = 1; j < len; j++)
+                    {
+                        sumMatrix[i, j] = matrix[i][j] - sumMatrix[i - 1, j - 1] + sumMatrix[i - 1, j] + sumMatrix[i, j - 1];
+                    }
+                }
+
+            }
+
+            public int SumRegion(int row1, int col1, int row2, int col2)
+            {
+                int sum = sumMatrix[row2, col2];
+                if (col1 - 1 >= 0)
+                {
+                    sum -= sumMatrix[row2, col1 - 1];
+                }
+                if (row1 - 1 >= 0)
+                {
+                    sum -= sumMatrix[row1 - 1, col2];
+                }
+                if (col1 - 1 >= 0 && row1 - 1 >= 0)
+                {
+                    sum += sumMatrix[row1 - 1, col1 - 1];
+                }
+                return sum;
+            }
+        }
+    }
+}
