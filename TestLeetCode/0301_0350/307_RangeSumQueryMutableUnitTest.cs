4896a4b31f3e47a660139c163e415253853de58b
hongtao luo <luohongtao621@163.com>
2024-03-24 15:59:10 +0800

    307

---
+++ TestLeetCode/0301_0350/307_RangeSumQueryMutableUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _307_RangeSumQueryMutableUnitTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 5];
+            _307_RangeSumQueryMutableAlg.NumArray numArray = new _307_RangeSumQueryMutableAlg.NumArray(nums);
+
+            Assert.AreEqual(9, numArray.SumRange(0, 2)); // 返回 1 + 3 + 5 = 9
+            numArray.Update(1, 2);   // nums = [1,2,5]
+            Assert.AreEqual(8, numArray.SumRange(0, 2)); // 返回 1 + 2 + 5 = 8
+        }
+    }
+}
---
+++ leetCode/0301_0350/307_RangeSumQueryMutableAlg.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._0301_0350
+{
+    public class _307_RangeSumQueryMutableAlg
+    {
+        /// <summary>
+        /// 树状数组
+        /// </summary>
+        public class NumArray2
+        {
+            private int[] tree;
+            private int[] nums;
+            public NumArray2(int[] nums)
+            {
+                this.nums = nums;
+                tree = new int[nums.Length + 1];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    Add(i + 1, nums[i]);
+                }
+
+
+            }
+            private void Add(int index, int val)
+            {
+                while (index < tree.Length)
+                {
+                    tree[index] += val;
+                    index += LowBit(index);
+                }
+            }
+
+            public void Update(int index, int val)
+            {
+                Add(index + 1, val - nums[index]);
+                nums[index] = val;
+            }
+
+            public int SumRange(int left, int right)
+            {
+                return PrefixSum(right + 1) - PrefixSum(left);
+            }
+            private int PrefixSum(int index)
+            {
+                int sum = 0;
+                while (index > 0)
+                {
+                    sum += tree[index];
+                    index -= LowBit(index);
+                }
+                return sum;
+            }
+
+            private int LowBit(int x)
+            {
+                return x & (-x);
+            }
+        }
+
+        /// <summary>
+        /// 线段数+懒标记
+        /// </summary>
+        public class NumArray
+        {
+            private Node[] tree;
+            private int size;
+            public NumArray(int[] nums)
+            {
+                size = nums.Length;
+                tree = new Node[nums.Length * 4];
+                Build(0, 0, size - 1, nums);
+
+            }
+
+            public void Update(int index, int val)
+            {
+                UpdateNode(0, index, val);
+            }
+
+            public int SumRange(int left, int right)
+            {
+                return Query(0, left, right);
+            }
+
+
+            private int GetLeft(int node)
+            {
+                return node * 2 + 1;
+            }
+            private int GetRight(int node)
+            {
+                return node * 2 + 2;
+            }
+
+            private void Build(int node, int left, int right, int[] nums)
+            {
+                tree[node] = new Node() { Left = left, Right = right, Sum = nums[left] };
+                if (left == right)
+                {
+                    return;
+                }
+                int mid = left + (right - left) / 2;
+                int leftChild = GetLeft(node);
+                int rightChild = GetRight(node);
+                Build(leftChild, left, mid, nums);
+                Build(rightChild, mid + 1, right, nums);
+                tree[node].Sum = tree[leftChild].Sum + tree[rightChild].Sum;
+            }
+            private int Query(int node, int begin, int end)
+            {
+                if (begin <= tree[node].Left && tree[node].Right <= end)
+                {
+                    return tree[node].Sum;
+                }
+                int mid = tree[node].Left + (tree[node].Right - tree[node].Left) / 2;
+              
+                int sum = 0;
+                int leftChild = GetLeft(node);
+                int rightChild = GetRight(node);
+                if (begin <= mid)
+                {
+                    sum += Query(leftChild, begin, end);
+                }
+                if (end > mid)
+                {
+                    sum += Query(rightChild, begin, end);
+                }
+                return sum;
+            }
+
+            private void UpdateNode(int node, int x, int val)
+            {
+                if (x == tree[node].Left && tree[node].Right == x)
+                {
+                    tree[node].Sum = val;
+                    return;
+                }
+                int leftChild = GetLeft(node);
+                int rightChild = GetRight(node);
+                int mid = tree[node].Left + (tree[node].Right - tree[node].Left) / 2;
+
+                if (x <= mid)
+                {
+                    UpdateNode(leftChild, x, val);
+                }
+                else
+                {
+                    UpdateNode(rightChild, x, val);
+                }
+
+                tree[node].Sum = tree[leftChild].Sum + tree[rightChild].Sum;
+            }
+
+        }
+
+        private class Node
+        {
+            public int Left { get; set; }
+            public int Right { get; set; }
+            public int Sum { get; set; }
+            public int Add { get; set; }
+        }
+    }
+}
