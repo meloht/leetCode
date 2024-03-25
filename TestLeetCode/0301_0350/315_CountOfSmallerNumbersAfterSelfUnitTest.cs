06a60e74073d1c959c6e8f9547f6bf124fdeaa1d
hongtao_luo <luohongtao621@hotmail.com>
2024-03-25 14:07:39 +0800

    315

---
+++ TestLeetCode/0301_0350/315_CountOfSmallerNumbersAfterSelfUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _315_CountOfSmallerNumbersAfterSelfUnitTest
+    {
+        _315_CountOfSmallerNumbersAfterSelfAlg alg = new _315_CountOfSmallerNumbersAfterSelfAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [5, 2, 6, 1];
+            int[] exp = [2, 1, 1, 0];
+            int[] res = alg.CountSmaller(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [-1];
+            int[] exp = [0];
+            int[] res = alg.CountSmaller(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [-1, -1];
+            int[] exp = [0, 0];
+            int[] res = alg.CountSmaller(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [0, 1, 2];
+            int[] exp = [0, 0, 0];
+            int[] res = alg.CountSmaller(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0301_0350/315_CountOfSmallerNumbersAfterSelfAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _315_CountOfSmallerNumbersAfterSelfAlg
+    {
+        public IList<int> CountSmaller(int[] nums)
+        {
+            int[] arr = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr[i] = nums[i];
+            }
+            Array.Sort(arr);
+
+            int[] res = new int[arr.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Array.BinarySearch(arr, nums[i]);
+                int num = Math.Abs(i - index);
+                res[index] = num;
+            }
+
+            return res;
+        }
+
+    }
+}
