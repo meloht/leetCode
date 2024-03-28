ebbfcfebf2dc53d3386f43ba53b220ad3919b6a4
hongtao_luo <luohongtao621@hotmail.com>
2024-03-28 12:14:36 +0800

    495

---
+++ TestLeetCode/0451_0500/495_TeemoAttackingUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _495_TeemoAttackingUnitTest
+    {
+        _495_TeemoAttackingAlg alg = new _495_TeemoAttackingAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] timeSeries = [1, 4];
+            int duration = 2;
+            int exp = 4;
+            int res = alg.FindPoisonedDuration(timeSeries, duration);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] timeSeries = [1, 2];
+            int duration = 2;
+            int exp = 3;
+            int res = alg.FindPoisonedDuration(timeSeries, duration);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] timeSeries = [1];
+            int duration = 100000;
+            int exp = 100000;
+            int res = alg.FindPoisonedDuration(timeSeries, duration);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0451_0500/495_TeemoAttackingAlg.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _495_TeemoAttackingAlg
+    {
+        public int FindPoisonedDuration(int[] timeSeries, int duration)
+        {
+            int ans = 0;
+            if (timeSeries.Length == 1)
+            {
+                ans += duration;
+            }
+            for (int i = 1; i < timeSeries.Length; i++)
+            {
+                int prev = timeSeries[i - 1];
+                int curr = timeSeries[i];
+                int dd = prev + duration - 1;
+                if (dd >= curr)
+                {
+                    ans += (duration - (dd - curr + 1));
+                }
+                else
+                {
+                    ans += duration;
+                }
+
+            }
+
+            if (timeSeries.Length > 1)
+            {
+                ans += duration;
+            }
+
+            return ans;
+        }
+    }
+}
