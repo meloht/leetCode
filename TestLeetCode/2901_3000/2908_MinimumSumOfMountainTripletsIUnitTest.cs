82900ba6af23596bdb1e698b3c944c03f2dfa41e
hongtao_luo <luohongtao621@hotmail.com>
2024-03-29 11:38:11 +0800

    2908

---
+++ TestLeetCode/2901_3000/2908_MinimumSumOfMountainTripletsIUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2901_3000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2901_3000
+{
+    [TestClass]
+    public class _2908_MinimumSumOfMountainTripletsIUnitTest
+    {
+        _2908_MinimumSumOfMountainTripletsIAlg alg = new _2908_MinimumSumOfMountainTripletsIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [8, 6, 1, 5, 3];
+            int exp = 9;
+            int res = alg.MinimumSum(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [5, 4, 8, 7, 10, 2];
+            int exp = 13;
+            int res = alg.MinimumSum(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [6, 5, 4, 3, 4, 5];
+            int exp = -1;
+            int res = alg.MinimumSum(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2901_3000/2908_MinimumSumOfMountainTripletsIAlg.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2901_3000
+{
+    public class _2908_MinimumSumOfMountainTripletsIAlg
+    {
+        public int MinimumSum1(int[] nums)
+        {
+            int ans = int.MaxValue;
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int c1 = nums[i];
+                for (int j = i + 1; j < nums.Length - 1; j++)
+                {
+                    int c2 = nums[j];
+                    if (c2 <= c1)
+                        continue;
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        int c3 = nums[k];
+                        if (c3 >= c2)
+                            continue;
+                        ans = Math.Min(c1 + c2 + c3, ans);
+                    }
+                }
+            }
+            if (ans == int.MaxValue)
+                return -1;
+            return ans;
+        }
+
+        public int MinimumSum(int[] nums)
+        {
+            int ans = int.MaxValue;
+            int leftMin = int.MaxValue;
+            int len = nums.Length;
+            int[] left = new int[len];
+            for (int i = 1; i < len; i++)
+            {
+                leftMin = Math.Min(nums[i - 1], leftMin);
+                left[i] = leftMin;
+            }
+            int right = nums[len - 1];
+            for (int i = len - 2; i > 0; i--)
+            {
+                if (left[i] < nums[i] && nums[i] > right)
+                {
+                    ans = Math.Min(ans, left[i] + nums[i] + right);
+                }
+                right = Math.Min(right, nums[i]);
+            }
+            if (ans == int.MaxValue)
+                return -1;
+            return ans;
+        }
+    }
+}
