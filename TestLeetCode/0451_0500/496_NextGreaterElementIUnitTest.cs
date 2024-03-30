58056531f6acd04dd292f396d353adda9f6577fe
hongtao luo <luohongtao621@163.com>
2024-03-30 12:47:02 +0800

    496

---
+++ TestLeetCode/0451_0500/496_NextGreaterElementIUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _496_NextGreaterElementIUnitTest
+    {
+        _496_NextGreaterElementIAlg alg = new _496_NextGreaterElementIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [4, 1, 2];
+            int[] nums2 = [1, 3, 4, 2];
+            int[] exp = [-1, 3, -1];
+            int[] res = alg.NextGreaterElement(nums1, nums2);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [2, 4];
+            int[] nums2 = [1, 2, 3, 4];
+            int[] exp = [3, -1];
+            int[] res = alg.NextGreaterElement(nums1, nums2);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums1 = [1, 3, 5, 2, 4];
+            int[] nums2 = [6, 5, 4, 3, 2, 1, 7];
+            int[] exp = [7, 7, 7, 7, 7];
+            int[] res = alg.NextGreaterElement(nums1, nums2);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+    }
+}
---
+++ leetCode/0451_0500/496_NextGreaterElementIAlg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _496_NextGreaterElementIAlg
+    {
+        public int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                dict.Add(nums2[i], i);
+            }
+
+            int[] right = new int[nums2.Length];
+            right[nums2.Length - 1] = nums2[nums2.Length - 1];
+            Stack<int> stack = new Stack<int>();
+            for (int i = nums2.Length - 1; i >= 0; i--)
+            {
+                while (stack.Count > 0 && stack.Peek() < nums2[i])
+                {
+                    stack.Pop();
+                }
+
+                if (stack.Count >0)
+                {
+                    right[i] = stack.Peek();
+                   
+                }
+                else
+                {
+                    right[i] = -1;
+                }
+                stack.Push(nums2[i]);
+              
+            }
+
+            int[] arr = new int[nums1.Length];
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                if (dict.ContainsKey(nums1[i]))
+                {
+                    int index = dict[nums1[i]];
+                    arr[i] = right[index];
+                }
+            }
+
+            return arr;
+        }
+    }
+}
