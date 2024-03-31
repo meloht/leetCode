b7eb1be5303241dfdfccaa6eeff23a3ff515679d
hongtao luo <luohongtao621@163.com>
2024-03-31 15:26:28 +0800

    331

---
+++ TestLeetCode/0301_0350/331_VerifyPreorderSerializationOfABinaryTreeUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _331_VerifyPreorderSerializationOfABinaryTreeUnitTest
+    {
+        _331_VerifyPreorderSerializationOfABinaryTreeAlg alg = new _331_VerifyPreorderSerializationOfABinaryTreeAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#";
+            bool exp = true;
+            bool res = alg.IsValidSerialization(preorder);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string preorder = "1,#";
+            bool exp = false;
+            bool res = alg.IsValidSerialization(preorder);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string preorder = "9,#,#,1";
+            bool exp = false;
+            bool res = alg.IsValidSerialization(preorder);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string preorder = "#";
+            bool exp = true;
+            bool res = alg.IsValidSerialization(preorder);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0301_0350/331_VerifyPreorderSerializationOfABinaryTreeAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _331_VerifyPreorderSerializationOfABinaryTreeAlg
+    {
+        public bool IsValidSerialization(string preorder)
+        {
+            if (preorder[0] == '#' && preorder.Length > 2)
+                return false;
+            if (preorder[0] == '#' && preorder.Length == 1)
+                return true;
+            string[] nodes = preorder.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue(nodes[0]);
+            int index = 1;
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    queue.Dequeue();
+                    int dec = nodes.Length - index;
+                    if (dec < 2)
+                        return false;
+                    int len = index + 2;
+                    for (; index < len; index++)
+                    {
+                        if (nodes[index] != "#")
+                        {
+                            queue.Enqueue(nodes[index]);
+                        }
+                    }
+                }
+            }
+            if (index != nodes.Length)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
