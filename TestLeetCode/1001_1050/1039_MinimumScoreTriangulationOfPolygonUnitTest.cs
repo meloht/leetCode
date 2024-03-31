ff6806be1a6cd0b9987f7acf5761fcdc34cda5e2
hongtao luo <luohongtao621@163.com>
2024-03-31 17:43:56 +0800

    1039

---
+++ TestLeetCode/1001_1050/1039_MinimumScoreTriangulationOfPolygonUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1039_MinimumScoreTriangulationOfPolygonUnitTest
+    {
+        _1039_MinimumScoreTriangulationOfPolygonAlg alg = new _1039_MinimumScoreTriangulationOfPolygonAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] values = [1, 2, 3];
+            int exp = 6;
+            int res = alg.MinScoreTriangulation(values);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] values = [3, 7, 4, 5];
+            int exp = 144;
+            int res = alg.MinScoreTriangulation(values);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] values = [1, 3, 1, 4, 1, 5];
+            int exp = 13;
+            int res = alg.MinScoreTriangulation(values);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1039_MinimumScoreTriangulationOfPolygonAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1039_MinimumScoreTriangulationOfPolygonAlg
+    {
+        public int MinScoreTriangulation(int[] values)
+        {
+            int[,] dp = new int[values.Length, values.Length];
+            for (int i = values.Length - 3; i >= 0; i--)
+            {
+                for (int j = i + 2; j < values.Length; j++)
+                {
+                    int res = int.MaxValue;
+                    for (int k = i + 1; k < j; k++)
+                    {
+                        res = Math.Min(res, dp[i, k] + dp[k, j] + values[i] * values[j] * values[k]);
+                    }
+                    dp[i, j] = res;
+                }
+            }
+
+            return dp[0, values.Length - 1];
+        }
+    }
+}
