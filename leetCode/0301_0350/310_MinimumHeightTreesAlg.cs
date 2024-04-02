b29b403dfcd4e9b0a7462c9d9642fe5abf99de0c
hongtao luo <luohongtao621@163.com>
2024-04-02 20:12:00 +0800

    310

--- leetCode/0301_0350/310_MinimumHeightTreesAlg.cs
+++ leetCode/0301_0350/310_MinimumHeightTreesAlg.cs
@@ -10,7 +10,55 @@ namespace leetCode._0301_0350
     {
         public IList<int> FindMinHeightTrees(int n, int[][] edges)
         {
-            return null;
+            List<int> result = new List<int>();
+            if (n == 1)
+            {
+                result.Add(0);
+                return result;
+            }
+            int[] degree = new int[n];
+            List<List<int>> map = new List<List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                map.Add(new List<int>());
+            }
+            foreach (int[] edge in edges)
+            {
+                degree[edge[0]]++;
+                degree[edge[1]]++;
+                map[edge[0]].Add(edge[1]);
+                map[edge[1]].Add(edge[0]);
+            }
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (degree[i] == 1)
+                {
+                    queue.Enqueue(i);
+                }
+            }
+            while (queue.Count > 0)
+            {
+                result = new List<int>();
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    int curr = queue.Dequeue();
+                    result.Add(curr);
+
+                    List<int> neighbors = map[curr];
+
+                    foreach (var item in neighbors)
+                    {
+                        degree[item]--;
+                        if (degree[item] == 1)
+                        {
+                            queue.Enqueue(item);
+                        }
+                    }
+                }
+            }
+            return result;
         }
     }
 }
