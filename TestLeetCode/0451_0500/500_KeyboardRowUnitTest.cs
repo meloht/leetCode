b4634479ff49d842f4815fb8b274d01a456c245d
hongtao luo <luohongtao621@163.com>
2024-04-03 20:04:46 +0800

    500

---
+++ TestLeetCode/0451_0500/500_KeyboardRowUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _500_KeyboardRowUnitTest
+    {
+        _500_KeyboardRowAlg alg = new _500_KeyboardRowAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["Hello", "Alaska", "Dad", "Peace"];
+            string[] exp = ["Alaska", "Dad"];
+            string[] res = alg.FindWords(words);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["omk"];
+            string[] exp = [];
+            string[] res = alg.FindWords(words);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] words = ["adsdf", "sfd"];
+            string[] exp = ["adsdf", "sfd"];
+            string[] res = alg.FindWords(words);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0451_0500/500_KeyboardRowAlg.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _500_KeyboardRowAlg
+    {
+        public string[] FindWords(string[] words)
+        {
+            string s1 = "qwertyuiop";
+            string s2 = "asdfghjkl";
+            string s3 = "zxcvbnm";
+            int[] arr1 = GetArr(s1);
+            int[] arr2 = GetArr(s2);
+            int[] arr3 = GetArr(s3);
+
+            List<string> list = new List<string>();
+            foreach (var item in words)
+            {
+                int[] arr = null;
+                int a = char.ToLower(item[0]) - 'a';
+                if (arr1[a] == 1)
+                {
+                    arr = arr1;
+                }
+                else if (arr2[a] == 1)
+                {
+                    arr = arr2;
+                }
+                else if (arr3[a] == 1)
+                {
+                    arr = arr3;
+                }
+                if (arr == null)
+                {
+                    continue;
+                }
+                bool bl = true;
+                for (int i = 0; i < item.Length; i++)
+                {
+                    int b = char.ToLower(item[i]) - 'a';
+                    if (arr[b] == 0)
+                    {
+                        bl = false;
+                        break;
+                    }
+                }
+                if (bl)
+                {
+                    list.Add(item);
+                }
+            }
+
+            return list.ToArray();
+        }
+        private int[] GetArr(string s)
+        {
+            int[] arr = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                int a = s[i] - 'a';
+                arr[a] = 1;
+            }
+            return arr;
+        }
+    }
+}
