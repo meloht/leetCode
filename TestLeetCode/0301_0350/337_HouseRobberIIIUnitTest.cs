d44d6894308c78aa939f97561a7aa501eb977b6d
hongtao luo <luohongtao621@163.com>
2024-04-04 11:56:36 +0800

    337

---
+++ TestLeetCode/0301_0350/337_HouseRobberIIIUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0301_0350;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _337_HouseRobberIIIUnitTest
+    {
+        _337_HouseRobberIIIAlg alg = new _337_HouseRobberIIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [3, 2, 3, null, 3, null, 1];
+            int exp = 7;
+            var tree=TreeNode.BuildTree(root);
+            int res = alg.Rob(tree);
+            Assert.AreEqual(exp, res);
+
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [3, 4, 5, 1, 3, null, 1];
+            int exp = 9;
+            var tree = TreeNode.BuildTree(root);
+            int res = alg.Rob(tree);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/0301_0350/337_HouseRobberIIIAlg.cs
@@ -0,0 +1,30 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _337_HouseRobberIIIAlg
+    {
+        public int Rob(TreeNode root)
+        {
+            var res = Dfs(root);
+            return Math.Max(res.noRob, res.rob);
+        }
+        private (int rob, int noRob) Dfs(TreeNode root)
+        {
+            if (root == null)
+            {
+                return (0, 0);
+            }
+            var left = Dfs(root.left);
+            var right = Dfs(root.right);
+            int rob = left.noRob + right.noRob + root.val;
+            int noRob = Math.Max(left.rob, left.noRob) + Math.Max(right.rob, right.noRob);
+            return (rob, noRob);
+        }
+    }
+}
