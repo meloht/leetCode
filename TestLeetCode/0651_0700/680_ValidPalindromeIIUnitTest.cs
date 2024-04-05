024ce010096cd9d00c54109ad9b2292d10e8c8e1
hongtao luo <luohongtao621@163.com>
2024-04-05 11:49:46 +0800

    680

---
+++ TestLeetCode/0651_0700/680_ValidPalindromeIIUnitTest.cs
@@ -0,0 +1,68 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _680_ValidPalindromeIIUnitTest
+    {
+        _680_ValidPalindromeIIAlg alg = new _680_ValidPalindromeIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "aba";
+            bool exp = true;
+            bool res = alg.ValidPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "abca";
+            bool exp = true;
+            bool res = alg.ValidPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "abc";
+            bool exp = false;
+            bool res = alg.ValidPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "deeee";
+            bool exp = true;
+            bool res = alg.ValidPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string s = "cbbcc";
+            bool exp = true;
+            bool res = alg.ValidPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            string s = "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga";
+            bool exp = true;
+            bool res = alg.ValidPalindrome(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/680_ValidPalindromeIIAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _680_ValidPalindromeIIAlg
+    {
+        public bool ValidPalindrome(string s)
+        {
+
+            int i = 0;
+            int j = s.Length - 1;
+            while (i < j)
+            {
+                if (s[i] != s[j])
+                {
+                    return ValidPalindrome(s, i, j - 1) || ValidPalindrome(s, i + 1, j);
+                }
+                i++;
+                j--;
+            }
+            return true;
+        }
+
+        private bool ValidPalindrome(string s, int left, int right)
+        {
+            for (int i = left,j=right; i < j; i++,j--)
+            {
+                if (s[i] != s[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
