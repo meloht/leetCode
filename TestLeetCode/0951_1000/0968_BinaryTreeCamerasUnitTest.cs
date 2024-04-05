ccdad284066378f03632c920e34857024dba657c
hongtao luo <luohongtao621@163.com>
2024-04-05 09:59:37 +0800

    968

---
+++ TestLeetCode/0951_1000/0968_BinaryTreeCamerasUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._0951_1000;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0951_1000
+{
+    [TestClass]
+    public class _0968_BinaryTreeCamerasUnitTest
+    {
+        _0968_BinaryTreeCamerasAlg alg = new _0968_BinaryTreeCamerasAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [0, 0, null, 0, 0];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 1;
+            int res = alg.MinCameraCover(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [0, 0, null, 0, null, 0, null, null, 0];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 2;
+            int res = alg.MinCameraCover(tree);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0968_BinaryTreeCamerasAlg.cs
@@ -0,0 +1,45 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0968_BinaryTreeCamerasAlg
+    {
+        public int MinCameraCover(TreeNode root)
+        {
+            var res = Dfs(root);
+            long min = Math.Min(res.choose, res.byChild);
+            return (int)min;
+        }
+        private (long choose, long byFather, long byChild) Dfs(TreeNode node)
+        {
+            if (node == null)
+            {
+                return (int.MaxValue / 2, 0, 0);
+            }
+            var left = Dfs(node.left);
+            var right = Dfs(node.right);
+
+            long l_choose = Math.Min(left.choose, left.byFather);
+            l_choose = Math.Min(l_choose, left.byChild);
+
+            long r_choose = Math.Min(right.choose, right.byFather);
+            r_choose = Math.Min(r_choose, right.byChild);
+
+            long choose = l_choose + r_choose + 1;
+
+            long byFather = Math.Min(left.choose, left.byChild) + Math.Min(right.choose, right.byChild);
+            long byChild = Math.Min(left.choose + right.byChild, left.byChild + right.choose);
+            byChild = Math.Min(byChild, left.choose + right.choose);
+
+            return (choose, byFather, byChild);
+
+        }
+
+
+    }
+}
