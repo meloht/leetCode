76c2a72845fc625f2400784aa9e490dd2741675f
hongtao luo <luohongtao621@163.com>
2024-04-05 11:18:46 +0800

    501

---
+++ TestLeetCode/0501_0550/501_FindModeInBinarySearchTreeUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode._0501_0550;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _501_FindModeInBinarySearchTreeUnitTest
+    {
+        _501_FindModeInBinarySearchTreeAlg alg = new _501_FindModeInBinarySearchTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, null, 2, 2];
+            var tree = TreeNode.BuildTree(root);
+            int[] exp = [2];
+            int[] res = alg.FindMode(tree);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [0];
+            var tree = TreeNode.BuildTree(root);
+            int[] exp = [0];
+            int[] res = alg.FindMode(tree);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+    }
+}
---
+++ leetCode/0501_0550/501_FindModeInBinarySearchTreeAlg.cs
@@ -0,0 +1,89 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _501_FindModeInBinarySearchTreeAlg
+    {
+        public int[] FindMode1(TreeNode root)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            Dfs(root, dict);
+            List<int> list = new List<int>();
+            int max = dict.Values.Max();
+            foreach (var item in dict)
+            {
+                if (item.Value == max)
+                {
+                    list.Add(item.Key);
+                }
+
+            }
+            return list.ToArray();
+        }
+        private void Dfs(TreeNode node, Dictionary<int, int> dict)
+        {
+            if (node == null)
+                return;
+            if (dict.ContainsKey(node.val))
+            {
+                dict[node.val]++;
+            }
+            else
+            {
+                dict.Add(node.val, 1);
+            }
+            Dfs(node.left, dict);
+            Dfs(node.right, dict);
+        }
+
+        List<int> res = new List<int>();
+        int baseNum = 0;
+        int count = 0;
+        int maxCount = 0;
+        public int[] FindMode(TreeNode root)
+        {
+            Dfs(root);
+            return res.ToArray();
+        }
+
+        private void Dfs(TreeNode node)
+        {
+            if (node == null)
+                return;
+            Dfs(node.left);
+            Update(node.val);
+            Dfs(node.right);
+        }
+
+        private void Update(int val) 
+        {
+            if (val == baseNum)
+            {
+                count++;
+            }
+            else 
+            {
+                count = 1;
+                baseNum = val;
+
+            }
+            if (count == maxCount)
+            {
+                res.Add(baseNum);
+            }
+            else if (count > maxCount)
+            {
+                maxCount = count;
+                res.Clear();
+                res.Add(baseNum);
+            }
+        }
+
+
+    }
+}
