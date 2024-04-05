dd98ce50fd68d90de0132f3aa8993ed4452cf59f
hongtao luo <luohongtao621@163.com>
2024-04-05 12:56:29 +0800

    504

---
+++ TestLeetCode/0501_0550/504_Base7UnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _504_Base7UnitTest
+    {
+        _504_Base7Alg alg = new _504_Base7Alg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int num = 100;
+            string exp = "202";
+            string res = alg.ConvertToBase7(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int num = -7;
+            string exp = "-10";
+            string res = alg.ConvertToBase7(num);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/504_Base7Alg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _504_Base7Alg
+    {
+        public string ConvertToBase7(int num)
+        {
+            if (num == 0)
+                return "0";
+            List<int> list = new List<int>();
+            string flag = num < 0 ? "-" : "";
+            int n = Math.Abs(num);
+            int res = 0;
+            while (n > 0)
+            {
+                res = n % 7;
+                list.Add(res);
+                n = n / 7;
+            }
+            list.Reverse();
+            return $"{flag}{string.Join("", list)}";
+        }
+    }
+}
