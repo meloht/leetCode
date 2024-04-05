63a4c48e0cc258ed6de8b6f2df44253677d177df
hongtao luo <luohongtao621@163.com>
2024-04-05 10:53:03 +0800

    1026

---
+++ TestLeetCode/1001_1050/1026_MaximumDifferenceBetweenNodeAndAncestorUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._1001_1050;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1026_MaximumDifferenceBetweenNodeAndAncestorUnitTest
+    {
+        _1026_MaximumDifferenceBetweenNodeAndAncestorAlg alg = new _1026_MaximumDifferenceBetweenNodeAndAncestorAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [8, 3, 10, 1, 6, null, 14, null, null, 4, 7, 13];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 7;
+            int res = alg.MaxAncestorDiff(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1, null, 2, null, 0, 3];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 3;
+            int res = alg.MaxAncestorDiff(tree);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1026_MaximumDifferenceBetweenNodeAndAncestorAlg.cs
@@ -0,0 +1,32 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1026_MaximumDifferenceBetweenNodeAndAncestorAlg
+    {
+        int ans = int.MinValue;
+        public int MaxAncestorDiff(TreeNode root)
+        {
+            Dfs(root, root.val, root.val);
+            return ans;
+        }
+
+        private void Dfs(TreeNode root, int min, int max)
+        {
+            if (root == null)
+                return;
+
+            ans = Math.Max(ans, Math.Abs(max - root.val));
+            ans = Math.Max(ans, Math.Abs(min - root.val));
+            min = Math.Min(min, root.val);
+            max = Math.Max(max, root.val);
+            Dfs(root.left, min, max);
+            Dfs(root.right, min, max);
+        }
+    }
+}
