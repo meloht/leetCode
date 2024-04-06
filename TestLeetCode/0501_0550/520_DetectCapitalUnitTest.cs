09d2d36a0bf7a76e0324ed9070d6a8266ed37586
hongtao luo <luohongtao621@163.com>
2024-04-06 09:43:27 +0800

    520

---
+++ TestLeetCode/0501_0550/520_DetectCapitalUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _520_DetectCapitalUnitTest
+    {
+        _520_DetectCapitalAlg alg = new _520_DetectCapitalAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string word = "USA";
+            bool exp = true;
+            bool res=alg.DetectCapitalUse(word);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            string word = "FlaG";
+            bool exp = false;
+            bool res = alg.DetectCapitalUse(word);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/520_DetectCapitalAlg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _520_DetectCapitalAlg
+    {
+        public bool DetectCapitalUse(string word)
+        {
+            if (word.Length == 1)
+                return true;
+
+            if (char.IsUpper(word[0]) && char.IsUpper(word[1]))
+            {
+                for (int i = 1; i < word.Length; i++)
+                {
+                    if (char.IsLower(word[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if (char.IsUpper(word[0]) && char.IsLower(word[1]))
+            {
+                for (int i = 2; i < word.Length; i++)
+                {
+                    if (char.IsUpper(word[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (char.IsUpper(word[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
