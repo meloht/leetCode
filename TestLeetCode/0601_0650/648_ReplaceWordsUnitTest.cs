361b37c2f3e0c2534ba69c5969911f618f1219ea
hongtao luo <luohongtao621@163.com>
2024-04-06 13:54:14 +0800

    648

---
+++ TestLeetCode/0601_0650/648_ReplaceWordsUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _648_ReplaceWordsUnitTest
+    {
+        _648_ReplaceWordsAlg alg = new _648_ReplaceWordsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] dictionary = ["cat", "bat", "rat"];
+            string sentence = "the cattle was rattled by the battery";
+            string exp = "the cat was rat by the bat";
+            string res = alg.ReplaceWords(dictionary, sentence);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] dictionary = ["a", "b", "c"];
+            string sentence = "aadsfasf absbs bbab cadsfafs";
+            string exp = "a a b c";
+            string res = alg.ReplaceWords(dictionary, sentence);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/648_ReplaceWordsAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._0601_0650
+{
+    public class _648_ReplaceWordsAlg
+    {
+
+        public string ReplaceWords(IList<string> dictionary, string sentence)
+        {
+            HashSet<string> words = new HashSet<string>(dictionary);
+          
+            var ls = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in ls)
+            {
+                StringBuilder sbb = new StringBuilder();
+                for (int i = 0; i < item.Length; i++)
+                {
+                    sbb.Append(item[i]);
+                    var prefix = sbb.ToString();
+                    if (words.Contains(prefix))
+                    {
+                        break;
+                    }
+                }
+                sb.Append(sbb.ToString()).Append(" ");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+
+
+
+    }
+}
