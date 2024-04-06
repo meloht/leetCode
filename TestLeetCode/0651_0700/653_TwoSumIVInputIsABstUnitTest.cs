e890959b054bfa69a1c47773914cc182b8672ac7
hongtao luo <luohongtao621@163.com>
2024-04-06 12:26:22 +0800

    653

---
+++ TestLeetCode/0651_0700/653_TwoSumIVInputIsABstUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._0651_0700;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _653_TwoSumIVInputIsABstUnitTest
+    {
+        _653_TwoSumIVInputIsABstAlg alg = new _653_TwoSumIVInputIsABstAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [5, 3, 6, 2, 4, null, 7];
+            int k = 9;
+            bool exp = true;
+            var tree = TreeNode.BuildTree(root);
+            bool res = alg.FindTarget(tree, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [5, 3, 6, 2, 4, null, 7];
+            int k = 28;
+            bool exp = false;
+            var tree = TreeNode.BuildTree(root);
+            bool res = alg.FindTarget(tree, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/653_TwoSumIVInputIsABstAlg.cs
@@ -0,0 +1,39 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _653_TwoSumIVInputIsABstAlg
+    {
+        public bool FindTarget(TreeNode root, int k)
+        {
+            HashSet<int> set = new HashSet<int>();
+            return Dfs(root, k, set);
+        }
+
+        private bool Dfs(TreeNode root, int k, HashSet<int> ls)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            bool l = Dfs(root.left, k, ls);
+            if (l)
+                return true;
+
+            if (ls.Contains(k - root.val))
+            {
+                return true;
+            }
+            ls.Add(root.val);
+            return Dfs(root.right, k, ls);
+
+        }
+
+
+    }
+}
