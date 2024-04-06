4ac3c623ba20b8bc9d1ec9c91abdcf08b96248a6
hongtao luo <luohongtao621@163.com>
2024-04-06 11:46:59 +0800

    933

---
+++ TestLeetCode/0901_0950/933_NumberOfRecentCallsUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0901_0950._933_NumberOfRecentCallsAlg;
+
+namespace Test._0901_0950
+{
+    [TestClass]
+    public class _933_NumberOfRecentCallsUnitTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            RecentCounter recentCounter = new RecentCounter();
+            Assert.AreEqual(1, recentCounter.Ping(1));     // requests = [1]，范围是 [-2999,1]，返回 1
+            Assert.AreEqual(2, recentCounter.Ping(100));   // requests = [1, 100]，范围是 [-2900,100]，返回 2
+            Assert.AreEqual(3, recentCounter.Ping(3001));  // requests = [1, 100, 3001]，范围是 [1,3001]，返回 3
+            Assert.AreEqual(3, recentCounter.Ping(3002));  // requests = [1, 100, 3001, 3002]，范围是 [2,3002]，返回 3
+        }
+    }
+}
---
+++ leetCode/0901_0950/933_NumberOfRecentCallsAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _933_NumberOfRecentCallsAlg
+    {
+        public class RecentCounter
+        {
+            Queue<int> queue = new Queue<int>();
+            public RecentCounter()
+            {
+
+            }
+
+            public int Ping(int t)
+            {
+                queue.Enqueue(t);
+                int min = t - 3000;
+                while (queue.Peek() < min)
+                {
+                    queue.Dequeue();
+                }
+                return queue.Count;
+            }
+        }
+    }
+}
