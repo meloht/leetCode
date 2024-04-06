e990e0084b47452be09e16963669efaa00e24688
hongtao luo <luohongtao621@163.com>
2024-04-06 13:12:02 +0800

    1122

---
+++ TestLeetCode/1101_1150/1122_RelativeSortArrayUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1122_RelativeSortArrayUnitTest
+    {
+        _1122_RelativeSortArrayAlg alg = new _1122_RelativeSortArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], arr2 = [2, 1, 4, 3, 9, 6];
+            int[] exp = [2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19];
+            int[] res = alg.RelativeSortArray(arr1, arr2);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr1 = [28, 6, 22, 8, 44, 17], arr2 = [22, 28, 8, 6];
+            int[] exp = [22, 28, 8, 6, 17, 44];
+            int[] res = alg.RelativeSortArray(arr1, arr2);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/1101_1150/1122_RelativeSortArrayAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1122_RelativeSortArrayAlg
+    {
+        public int[] RelativeSortArray(int[] arr1, int[] arr2)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            foreach (var item in arr1)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+            List<int> list = new List<int>();
+            foreach (var item in arr2)
+            {
+                int count = dict[item];
+                for (int i = 0; i < count; i++)
+                {
+                    list.Add(item);
+                }
+                dict.Remove(item);
+            }
+
+            List<int> ls = new List<int>();
+            foreach (var item in dict)
+            {
+                int count = item.Value;
+                for (int i = 0; i < count; i++)
+                {
+                    ls.Add(item.Key);
+                }
+            }
+            ls.Sort();
+            list.AddRange(ls);
+            return list.ToArray();
+        }
+    }
+}
