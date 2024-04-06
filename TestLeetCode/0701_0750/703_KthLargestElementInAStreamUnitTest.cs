08c08b6cae63f48a826cd626570b5921df790ccd
hongtao luo <luohongtao621@163.com>
2024-04-06 12:46:57 +0800

    703

---
+++ TestLeetCode/0701_0750/703_KthLargestElementInAStreamUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0701_0750._703_KthLargestElementInAStreamAlg;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _703_KthLargestElementInAStreamUnitTest
+    {
+       
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+            Assert.AreEqual(4, kthLargest.Add(3));   // return 4
+            Assert.AreEqual(5, kthLargest.Add(5));   // return 5
+            Assert.AreEqual(5, kthLargest.Add(10));  // return 5
+            Assert.AreEqual(8, kthLargest.Add(9));   // return 8
+            Assert.AreEqual(8, kthLargest.Add(4));   // return 8
+        }
+    }
+}
---
+++ leetCode/0701_0750/703_KthLargestElementInAStreamAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _703_KthLargestElementInAStreamAlg
+    {
+        public class KthLargest
+        {
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>();
+            int Count = 0;
+            public KthLargest(int k, int[] nums)
+            {
+                Count = k;
+                foreach (var item in nums)
+                {
+                    queue.Enqueue(item, item);
+                }
+                while (queue.Count > k)
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            public int Add(int val)
+            {
+                queue.Enqueue(val, val);
+                if (queue.Count > Count)
+                {
+                    queue.Dequeue();
+                }
+                return queue.Peek();
+            }
+        }
+    }
+}
