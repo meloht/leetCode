7a91b99d09a3e3d8dd52d4af44edff9a9bba5527
hongtao luo <luohongtao621@163.com>
2024-04-06 11:32:04 +0800

    506

---
+++ TestLeetCode/0501_0550/506_RelativeRanksUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0501_0550;
+using leetCode.WeeklyContest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _506_RelativeRanksUnitTest
+    {
+        _506_RelativeRanksAlg alg = new _506_RelativeRanksAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] score = [5, 4, 3, 2, 1];
+            string[] exp = ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"];
+            string[] res = alg.FindRelativeRanks(score);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] score = [10, 3, 8, 9, 4];
+            string[] exp = ["Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"];
+            string[] res = alg.FindRelativeRanks(score);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+    }
+}
---
+++ leetCode/0501_0550/506_RelativeRanksAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _506_RelativeRanksAlg
+    {
+        public string[] FindRelativeRanks(int[] score)
+        {
+            PriorityQueue<int[], int> priorityQueue = new PriorityQueue<int[], int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+            string[] ranks = ["Gold Medal", "Silver Medal", "Bronze Medal"];
+
+            for (int i = 0; i < score.Length; i++)
+            {
+                priorityQueue.Enqueue([score[i], i], score[i]);
+            }
+
+            string[] res = new string[score.Length];
+            int index = 0;
+            while (priorityQueue.Count > 0)
+            {
+                var val = priorityQueue.Dequeue();
+                if (index < 3)
+                {
+                    res[val[1]] = ranks[index];
+                }
+                else
+                {
+                    res[val[1]] = (index + 1).ToString();
+                }
+                index++;
+
+            }
+            return res;
+        }
+    }
+}
