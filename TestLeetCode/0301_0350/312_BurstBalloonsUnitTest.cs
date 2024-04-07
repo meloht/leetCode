49143701a08d78d8c514dc26bb7b660cc8b471b9
hongtao_luo <luohongtao621@hotmail.com>
2024-04-07 11:38:54 +0800

    312

---
+++ TestLeetCode/0301_0350/312_BurstBalloonsUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _312_BurstBalloonsUnitTest
+    {
+        _312_BurstBalloonsAlg alg = new _312_BurstBalloonsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 1, 5, 8];
+            int exp = 167;
+            int res = alg.MaxCoins(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 5];
+            int exp = 10;
+            int res = alg.MaxCoins(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0301_0350/312_BurstBalloonsAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _312_BurstBalloonsAlg
+    {
+        public int MaxCoins(int[] nums)
+        {
+
+            int[][] arr=new int[nums.Length][];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr[i]= [nums[i], i];
+            }
+            Array.Sort(arr, (x, y) => x[0].CompareTo(y[0]));
+
+            bool[] used=new bool[nums.Length];
+
+
+            int ans = 0;
+
+
+           
+            return 0;
+        }
+    }
+}
