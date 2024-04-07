0e907cf6790bf895233a5aaf87fcb35878fab510
hongtao_luo <luohongtao621@hotmail.com>
2024-04-07 14:24:51 +0800

    313

---
+++ TestLeetCode/0301_0350/313_SuperUglyNumberUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _313_SuperUglyNumberUnitTest
+    {
+        _313_SuperUglyNumberAlg alg = new _313_SuperUglyNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 12;
+            int[] primes = [2, 7, 13, 19];
+            int exp = 32;
+            int res = alg.NthSuperUglyNumber(n, primes);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1;
+            int[] primes = [2, 3, 5];
+            int exp = 1;
+            int res = alg.NthSuperUglyNumber(n, primes);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0301_0350/313_SuperUglyNumberAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _313_SuperUglyNumberAlg
+    {
+        public int NthSuperUglyNumber(int n, int[] primes)
+        {
+            return 0;
+        }
+    }
+}
