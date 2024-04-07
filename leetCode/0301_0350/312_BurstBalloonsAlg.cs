3fc384e90862a2f9394f53de8443198b42c24b49
hongtao_luo <luohongtao621@hotmail.com>
2024-04-07 13:13:05 +0800

    312

--- leetCode/0301_0350/312_BurstBalloonsAlg.cs
+++ leetCode/0301_0350/312_BurstBalloonsAlg.cs
@@ -8,24 +8,46 @@ namespace leetCode._0301_0350
 {
     public class _312_BurstBalloonsAlg
     {
+        int[][] rec;
+        int[] val;
         public int MaxCoins(int[] nums)
         {
-
-            int[][] arr=new int[nums.Length][];
-            for (int i = 0; i < nums.Length; i++)
+            int len = nums.Length;
+            val = new int[len + 2];
+            for (int i = 1; i <= len; i++)
             {
-                arr[i]= [nums[i], i];
+                val[i] = nums[i - 1];
             }
-            Array.Sort(arr, (x, y) => x[0].CompareTo(y[0]));
+            val[0] = val[len + 1] = 1;
 
-            bool[] used=new bool[nums.Length];
+            rec = new int[len + 2][];
 
+            for (int i = 0; i <= len + 1; i++)
+            {
+                int[] arr = new int[len + 2];
+                Array.Fill(arr, -1);
+                rec[i] = arr;
+            }
 
-            int ans = 0;
+            return Dfs(0, len + 1);
+        }
+        private int Dfs(int left, int right)
+        {
+            if (left >= right - 1)
+                return 0;
 
+            if (rec[left][right] != -1)
+            {
+                return rec[left][right];
+            }
 
-           
-            return 0;
+            for (int i = left + 1; i < right; i++)
+            {
+                int sum = val[left] * val[i] * val[right];
+                sum += Dfs(left, i) + Dfs(i, right);
+                rec[left][right] = Math.Max(rec[left][right], sum);
+            }
+            return rec[left][right];
         }
     }
 }
