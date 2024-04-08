820ba961e441cb1d25ae7a92108e8f68a3968f9d
hongtao_luo <luohongtao621@hotmail.com>
2024-04-08 13:15:36 +0800

    541

--- leetCode/0501_0550/541_ReverseStringIIAlg.cs
+++ leetCode/0501_0550/541_ReverseStringIIAlg.cs
@@ -52,5 +52,29 @@ namespace leetCode._0501_0550
 
             return sb.ToString();
         }
+
+        public string ReverseStr1(string s, int k)
+        {
+            int n = s.Length;
+            char[] arr = s.ToCharArray();
+            for (int i = 0; i < n; i += 2 * k)
+            {
+                Reverse(arr, i, Math.Min(i + k, n) - 1);
+            }
+            return new string(arr);
+        }
+
+        public void Reverse(char[] arr, int left, int right)
+        {
+            while (left < right)
+            {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
     }
 }
