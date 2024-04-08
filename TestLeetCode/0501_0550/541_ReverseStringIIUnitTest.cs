a7e5497eadd2c8576c5d949637075e414db9572f
hongtao_luo <luohongtao621@hotmail.com>
2024-04-08 13:14:26 +0800

    541

---
+++ TestLeetCode/0501_0550/541_ReverseStringIIUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _541_ReverseStringIIUnitTest
+    {
+        _541_ReverseStringIIAlg alg = new _541_ReverseStringIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abcdefg";
+            int k = 2;
+            string exp = "bacdfeg";
+            string res = alg.ReverseStr(s, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "abcd";
+            int k = 2;
+            string exp = "bacd";
+            string res = alg.ReverseStr(s, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "abc";
+            int k = 4;
+            string exp = "cba";
+            string res = alg.ReverseStr(s, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/541_ReverseStringIIAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+
+    public class _541_ReverseStringIIAlg
+    {
+        public string ReverseStr(string s, int k)
+        {
+            if (s.Length < k)
+            {
+                return new string(s.Reverse().ToArray());
+            }
+
+            StringBuilder sb = new StringBuilder();
+            int k2 = 2 * k;
+
+            for (int i = 1; i <= s.Length; i++)
+            {
+                if (i % k2 == 0)
+                {
+                    var sub = s.Substring(i - k2, k);
+                    var rev = new string(sub.Reverse().ToArray());
+                    sb.Append(rev);
+                    sb.Append(s.Substring(i - k, k));
+                }
+                else
+                {
+                    if (i == s.Length)
+                    {
+                        int n = s.Length - sb.Length;
+                        if (n <= k)
+                        {
+                            string ss = s.Substring(s.Length - n);
+                            sb.Append(new string(ss.Reverse().ToArray()));
+                        }
+                        else
+                        {
+                            string ss = s.Substring(s.Length - n,k);
+                            sb.Append(new string(ss.Reverse().ToArray()));
+
+                            string sss = s.Substring(s.Length - n + k);
+                            sb.Append(sss);
+                        }
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
