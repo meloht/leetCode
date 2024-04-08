ce917c89e52286ec99672dc8c8b5025e391636ac
hongtao_luo <luohongtao621@hotmail.com>
2024-04-08 14:31:42 +0800

    557

---
+++ TestLeetCode/0551_0600/557_ReverseWordsInAStringIIIUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _557_ReverseWordsInAStringIIIUnitTest
+    {
+        _557_ReverseWordsInAStringIIIAlg alg = new _557_ReverseWordsInAStringIIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "Let's take LeetCode contest";
+            string exp = "s'teL ekat edoCteeL tsetnoc";
+            string res = alg.ReverseWords(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "Mr Ding";
+            string exp = "rM gniD";
+            string res = alg.ReverseWords(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/557_ReverseWordsInAStringIIIAlg.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _557_ReverseWordsInAStringIIIAlg
+    {
+        public string ReverseWords(string s)
+        {
+            s = " " + s;
+            char[] arr = s.ToCharArray();
+            int i = 0;
+            int j = 0;
+            while (i < s.Length)
+            {
+                if (arr[i] == ' ')
+                {
+                    j = i + 1;
+                    while (j < s.Length)
+                    {
+                        if (arr[j] == ' ')
+                            break;
+
+                        j++;
+                    }
+                   
+                    Reverse(arr, i + 1, j - 1);
+                    i = j;
+                    continue;
+                }
+                i++;
+            }
+            return new string(arr).Trim();
+        }
+        public void Reverse(char[] arr, int left, int right)
+        {
+            while (left < right)
+            {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        public string ReverseWords1(string s)
+        {
+            StringBuilder ret = new StringBuilder();
+            int length = s.Length;
+            int i = 0;
+            while (i < length)
+            {
+                int start = i;
+                while (i < length && s[i] != ' ')
+                {
+                    i++;
+                }
+                for (int p = start; p < i; p++)
+                {
+                    ret.Append(s[start + i - 1 - p]);
+                }
+                while (i < length && s[i] == ' ')
+                {
+                    i++;
+                    ret.Append(' ');
+                }
+            }
+            return ret.ToString();
+        }
+
+    }
+}
