078dbac185cd3e73edd1f42c764d91778da80f10
hongtao_luo <luohongtao621@hotmail.com>
2024-04-08 13:47:22 +0800

    551

---
+++ TestLeetCode/0551_0600/551_StudentAttendanceRecordIUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _551_StudentAttendanceRecordIUnitTest
+    {
+        _551_StudentAttendanceRecordIAlg alg = new _551_StudentAttendanceRecordIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "PPALLP";
+            bool exp = true;
+            bool res = alg.CheckRecord(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "PPALLL";
+            bool exp = false;
+            bool res = alg.CheckRecord(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/551_StudentAttendanceRecordIAlg.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _551_StudentAttendanceRecordIAlg
+    {
+        public bool CheckRecord(string s)
+        {
+            int a = s.Count(p => p == 'A');
+            int l = s[0] == 'L' ? 1 : 0;
+            if (a >= 2)
+                return false;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i - 1] == 'L' && s[i] == 'L')
+                {
+                    l++;
+                    if (l >= 3)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (l >= 3)
+                    {
+                        return false;
+                    }
+                    if (s[i] == 'L')
+                    {
+                        l = 1;
+                    }
+                    else
+                    {
+                        l = 0;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public bool CheckRecord1(string s)
+        {
+            int absents = 0, lates = 0;
+            int n = s.Length;
+            for (int i = 0; i < n; i++)
+            {
+                char c = s[i];
+                if (c == 'A')
+                {
+                    absents++;
+                    if (absents >= 2)
+                    {
+                        return false;
+                    }
+                }
+                if (c == 'L')
+                {
+                    lates++;
+                    if (lates >= 3)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    lates = 0;
+                }
+            }
+            return true;
+        }
+
+
+    }
+}
