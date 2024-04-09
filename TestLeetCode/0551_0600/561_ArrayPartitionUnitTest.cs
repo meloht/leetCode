c5d79d77006628a88b2a961e09108c0c5404ba01
hongtao_luo <luohongtao621@hotmail.com>
2024-04-09 12:16:55 +0800

    561

---
+++ TestLeetCode/0551_0600/561_ArrayPartitionUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _561_ArrayPartitionUnitTest
+    {
+        _561_ArrayPartitionAlg alg = new _561_ArrayPartitionAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 4, 3, 2];
+            int exp = 4;
+            int res = alg.ArrayPairSum(nums);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [6, 2, 6, 5, 1, 2];
+            int exp = 9;
+            int res = alg.ArrayPairSum(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/561_ArrayPartitionAlg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _561_ArrayPartitionAlg
+    {
+        public int ArrayPairSum(int[] nums)
+        {
+            int ans = 0;
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                ans += nums[i];
+            }
+            return ans;
+        }
+    }
+}
