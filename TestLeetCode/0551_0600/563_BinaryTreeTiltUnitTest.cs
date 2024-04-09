5e336f8ba1933c2a027cbf2d49da528e1074a04d
hongtao_luo <luohongtao621@hotmail.com>
2024-04-09 13:39:13 +0800

    563

---
+++ TestLeetCode/0551_0600/563_BinaryTreeTiltUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._0551_0600;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _563_BinaryTreeTiltUnitTest
+    {
+        _563_BinaryTreeTiltAlg alg = new _563_BinaryTreeTiltAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 2, 3];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 1;
+            int res = alg.FindTilt(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [4, 2, 9, 3, 5, null, 7];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 15;
+            int res = alg.FindTilt(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [21, 7, 14, 1, 1, 2, 2, 3, 3];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 9;
+            int res = alg.FindTilt(tree);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/563_BinaryTreeTiltAlg.cs
@@ -0,0 +1,30 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _563_BinaryTreeTiltAlg
+    {
+        public int FindTilt(TreeNode root)
+        {
+            var res = Dfs(root);
+            return res.abs;
+        }
+
+        private (int abs, int sum) Dfs(TreeNode root)
+        {
+            if (root == null)
+                return (0, 0);
+
+            var left = Dfs(root.left);
+            var right = Dfs(root.right);
+            int absSum = left.abs + right.abs + Math.Abs(left.sum - right.sum);
+            int sum = left.sum + right.sum + root.val;
+            return (absSum, sum);
+        }
+    }
+}
