dbe36e80f9020a968bdfcc5c45f7ca8896d0d625
hongtao_luo <luohongtao621@hotmail.com>
2024-04-09 17:54:31 +0800

    572

---
+++ TestLeetCode/0551_0600/572_SubtreeOfAnotherTreeUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using leetCode._0551_0600;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _572_SubtreeOfAnotherTreeUnitTest
+    {
+        _572_SubtreeOfAnotherTreeAlg alg = new _572_SubtreeOfAnotherTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [3, 4, 5, 1, 2], subRoot = [4, 1, 2];
+            var tree1 = TreeNode.BuildTree(root);
+            var tree2 = TreeNode.BuildTree(subRoot);
+            bool exp = true;
+            bool res = alg.IsSubtree(tree1, tree2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [3, 4, 5, 1, 2, null, null, null, null, 0], subRoot = [4, 1, 2];
+            var tree1 = TreeNode.BuildTree(root);
+            var tree2 = TreeNode.BuildTree(subRoot);
+            bool exp = false;
+            bool res = alg.IsSubtree(tree1, tree2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [1,1], subRoot = [1];
+            var tree1 = TreeNode.BuildTree(root);
+            var tree2 = TreeNode.BuildTree(subRoot);
+            bool exp = true;
+            bool res = alg.IsSubtree(tree1, tree2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/572_SubtreeOfAnotherTreeAlg.cs
@@ -0,0 +1,56 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _572_SubtreeOfAnotherTreeAlg
+    {
+        TreeNode _subRoot;
+        public bool IsSubtree(TreeNode root, TreeNode subRoot)
+        {
+            _subRoot = subRoot;
+
+            return Dfs(root, subRoot);
+        }
+        private bool Dfs(TreeNode root, TreeNode subRoot)
+        {
+            if (root == null)
+            {
+                if (subRoot == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (subRoot == null)
+                    return false;
+            }
+
+            var tree = subRoot;
+            if (root.val != subRoot.val)
+            {
+                tree = _subRoot;
+                bool l = Dfs(root.left, tree);
+                bool r = Dfs(root.right, tree);
+                return l || r;
+            }
+            else
+            {
+                bool l = Dfs(root.left, tree.left);
+                bool r = Dfs(root.right, tree.right);
+                return l && r;
+            }
+
+        }
+    }
+}
