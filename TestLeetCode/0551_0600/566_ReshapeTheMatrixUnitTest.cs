4004179edb11b52f68ade80ea423e4cc38cf7586
hongtao_luo <luohongtao621@hotmail.com>
2024-04-09 14:01:30 +0800

    566

---
+++ TestLeetCode/0551_0600/566_ReshapeTheMatrixUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _566_ReshapeTheMatrixUnitTest
+    {
+        _566_ReshapeTheMatrixAlg alg = new _566_ReshapeTheMatrixAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] mat = [[1, 2], [3, 4]];
+            int r = 1, c = 4;
+            int[][] exp = [[1, 2, 3, 4]];
+            int[][] res = alg.MatrixReshape(mat, r, c);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] mat = [[1, 2], [3, 4]];
+            int r = 2, c = 4;
+            int[][] exp = [[1, 2], [3, 4]];
+            int[][] res = alg.MatrixReshape(mat, r, c);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0551_0600/566_ReshapeTheMatrixAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _566_ReshapeTheMatrixAlg
+    {
+        public int[][] MatrixReshape(int[][] mat, int r, int c)
+        {
+            int n = mat.Length;
+            int m = mat[0].Length;
+
+            if (n * m != r * c)
+                return mat;
+            int[][] grid = new int[r][];
+            int col = 0;
+            int row = 0;
+            grid[row] = new int[c];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (col > 0 && col % c == 0)
+                    {
+                        row++;
+                        grid[row] = new int[c];
+                        col = 0;
+                    }
+                    grid[row][col] = mat[i][j];
+
+                    col++;
+                }
+            }
+
+            return grid;
+        }
+    }
+}
