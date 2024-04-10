b18c4cd5d4979379ba67ec54b1bd07d3c88f2ab3
hongtao_luo <luohongtao621@hotmail.com>
2024-04-10 15:05:57 +0800

    594

--- leetCode/0551_0600/594_LongestHarmoniousSubsequenceAlg.cs
+++ leetCode/0551_0600/594_LongestHarmoniousSubsequenceAlg.cs
@@ -10,7 +10,29 @@ namespace leetCode._0551_0600
     {
         public int FindLHS(int[] nums)
         {
-            return 0;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+            int ans = 0;
+            foreach (var item in dict)
+            {
+                int num = item.Value;
+                if (dict.ContainsKey(item.Key + 1))
+                {
+                    num += dict[item.Key + 1];
+                    ans = Math.Max(ans, num);
+                }
+            }
+            return ans;
         }
     }
 }
