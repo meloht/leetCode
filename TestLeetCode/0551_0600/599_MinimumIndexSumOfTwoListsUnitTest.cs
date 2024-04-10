e948363ac3fc1a7f3b72a6938560c863be843331
hongtao_luo <luohongtao621@hotmail.com>
2024-04-10 17:14:50 +0800

    599

---
+++ TestLeetCode/0551_0600/599_MinimumIndexSumOfTwoListsUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _599_MinimumIndexSumOfTwoListsUnitTest
+    {
+        _599_MinimumIndexSumOfTwoListsAlg alg = new _599_MinimumIndexSumOfTwoListsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"];
+            string[] list2 = ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"];
+            string[] exp = ["Shogun"];
+            string[] res = alg.FindRestaurant(list1, list2);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"];
+            string[] list2 = ["KFC", "Shogun", "Burger King"];
+            string[] exp = ["Shogun"];
+            string[] res = alg.FindRestaurant(list1, list2);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] list1 = ["sad", "happy", "good"];
+            string[] list2 = ["happy", "sad", "good"];
+            string[] exp = ["sad", "happy"];
+            string[] res = alg.FindRestaurant(list1, list2);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+
+}
---
+++ leetCode/0551_0600/599_MinimumIndexSumOfTwoListsAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _599_MinimumIndexSumOfTwoListsAlg
+    {
+        public string[] FindRestaurant(string[] list1, string[] list2)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            for (int i = 0; i < list1.Length; i++)
+            {
+                dict.Add(list1[i], i);
+            }
+            Stack<int[]> stack = new Stack<int[]>();
+           
+            for (int i = 0; i < list2.Length; i++)
+            {
+                if (dict.ContainsKey(list2[i]))
+                {
+                    int sum = dict[list2[i]] + i;
+                    if (stack.Count == 0)
+                    {
+                        stack.Push([i, sum]);
+                    }
+                    else
+                    {
+                        while (stack.Count > 0 && stack.Peek()[1] > sum)
+                        {
+                            stack.Pop();
+                        }
+                        if (stack.Count > 0 && stack.Peek()[1] == sum)
+                        {
+                            stack.Push([i, sum]);
+                        }
+                        else if (stack.Count == 0)
+                        {
+                            stack.Push([i, sum]);
+                        }
+                    }
+                }
+            }
+            string[] res = new string[stack.Count];
+            int index = 0;
+            while (stack.Count > 0)
+            {
+                res[index] = list2[stack.Pop()[0]];
+                index++;
+            }
+            return res;
+        }
+    }
+}
