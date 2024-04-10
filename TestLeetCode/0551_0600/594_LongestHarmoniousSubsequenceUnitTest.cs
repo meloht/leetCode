c7f0d61d49074315b84a50ff3e92136b6c612e7e
hongtao_luo <luohongtao621@hotmail.com>
2024-04-10 13:00:08 +0800

    594

---
+++ TestLeetCode/0551_0600/594_LongestHarmoniousSubsequenceUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _594_LongestHarmoniousSubsequenceUnitTest
+    {
+        _594_LongestHarmoniousSubsequenceAlg alg = new _594_LongestHarmoniousSubsequenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 2, 2, 5, 2, 3, 7];
+            int exp = 5;
+            int res = alg.FindLHS(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int exp = 2;
+            int res = alg.FindLHS(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 1, 1, 1];
+            int exp = 0;
+            int res = alg.FindLHS(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/594_LongestHarmoniousSubsequenceAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _594_LongestHarmoniousSubsequenceAlg
+    {
+        public int FindLHS(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
