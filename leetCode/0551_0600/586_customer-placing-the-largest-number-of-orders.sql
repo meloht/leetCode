65c3c816ed1e489340898018a789a20a43a5648e
hongtao_luo <luohongtao621@hotmail.com>
2024-04-10 12:33:08 +0800

    586

---
+++ leetCode/0551_0600/586_customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,13 @@
+﻿
+select customer_number from (
+select customer_number,count(*) as num from orders group by customer_number 
+order by num desc limit 1) t;
+
+SELECT
+    customer_number
+FROM
+    orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1
+;
