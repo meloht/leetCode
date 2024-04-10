bdeedee30068aa49a23810e72a65b41a466e1ce7
hongtao_luo <luohongtao621@hotmail.com>
2024-04-10 16:23:58 +0800

    598

---
+++ TestLeetCode/0551_0600/598_RangeAdditionIIUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _598_RangeAdditionIIUnitTest
+    {
+        _598_RangeAdditionIIAlg alg = new _598_RangeAdditionIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int m = 3, n = 3;
+            int[][] ops = [[2, 2], [3, 3]];
+            int exp = 4;
+            int res = alg.MaxCount(m, n, ops);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int m = 3, n = 3;
+            int[][] ops = [[2, 2], [3, 3], [3, 3], [3, 3], [2, 2], [3, 3], [3, 3], [3, 3], [2, 2], [3, 3], [3, 3], [3, 3]];
+            int exp = 4;
+            int res = alg.MaxCount(m, n, ops);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int m = 3, n = 3;
+            int[][] ops = [];
+            int exp = 9;
+            int res = alg.MaxCount(m, n, ops);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/598_RangeAdditionIIAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _598_RangeAdditionIIAlg
+    {
+        public int MaxCount(int m, int n, int[][] ops)
+        {
+            int xx = m;
+            int yy = n;
+            for (int i = 0; i < ops.Length; i++)
+            {
+                xx = Math.Min(ops[i][0], xx);
+                yy = Math.Min(ops[i][1], yy);
+
+            }
+            int count = xx * yy;
+
+            return count;
+        }
+    }
+}
