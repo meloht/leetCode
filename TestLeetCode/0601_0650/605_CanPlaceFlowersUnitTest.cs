46b644f70eb7fbf5b7777036b4ef1e4b49962ccd
hongtao_luo <luohongtao621@hotmail.com>
2024-04-11 14:28:44 +0800

    605

---
+++ TestLeetCode/0601_0650/605_CanPlaceFlowersUnitTest.cs
@@ -0,0 +1,75 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _605_CanPlaceFlowersUnitTest
+    {
+        _605_CanPlaceFlowersAlg alg = new _605_CanPlaceFlowersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] flowerbed = [1, 0, 0, 0, 1];
+            int n = 1;
+            bool exp = true;
+            bool res = alg.CanPlaceFlowers(flowerbed, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] flowerbed = [1, 0, 0, 0, 1];
+            int n = 2;
+            bool exp = false;
+            bool res = alg.CanPlaceFlowers(flowerbed, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] flowerbed = [1, 0, 0, 0, 0];
+            int n = 2;
+            bool exp = true;
+            bool res = alg.CanPlaceFlowers(flowerbed, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] flowerbed = [1, 0, 0, 0, 0, 1];
+            int n = 2;
+            bool exp = false;
+            bool res = alg.CanPlaceFlowers(flowerbed, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] flowerbed = [0, 0, 1, 0, 1];
+            int n = 1;
+            bool exp = true;
+            bool res = alg.CanPlaceFlowers(flowerbed, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            int[] flowerbed = [0, 0, 0];
+            int n = 2;
+            bool exp = true;
+            bool res = alg.CanPlaceFlowers(flowerbed, n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/605_CanPlaceFlowersAlg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _605_CanPlaceFlowersAlg
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            int count = 0;
+            int m = flowerbed.Length;
+            int prev = -1;
+            for (int i = 0; i < m; i++)
+            {
+                if (flowerbed[i] == 1)
+                {
+                    if (prev < 0)
+                    {
+                        count += i / 2;
+                    }
+                    else
+                    {
+                        count += (i - prev - 2) / 2;
+                    }
+                    if (count >= n)
+                    {
+                        return true;
+                    }
+                    prev = i;
+                }
+            }
+            if (prev < 0)
+            {
+                count += (m + 1) / 2;
+            }
+            else
+            {
+                count += (m - prev - 1) / 2;
+            }
+            return count >= n;
+
+        }
+    }
+}
