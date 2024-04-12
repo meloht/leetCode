066b1c1053f1ed4f9b4a008bfe13eef11e96a623
hongtao_luo <luohongtao621@hotmail.com>
2024-04-12 13:38:39 +0800

    617

---
+++ TestLeetCode/0601_0650/617_MergeTwoBinaryTreesUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._0601_0650;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _617_MergeTwoBinaryTreesUnitTest
+    {
+        _617_MergeTwoBinaryTreesAlg alg = new _617_MergeTwoBinaryTreesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root1 = [1, 3, 2, 5], root2 = [2, 1, 3, null, 4, null, 7];
+            int?[] exp = [3, 4, 5, 5, 4, null, 7];
+
+            var tree1 = TreeNode.BuildTree(root1);
+            var tree2 = TreeNode.BuildTree(root2);
+
+            var tree = alg.MergeTrees(tree1, tree2);
+            int?[] res = TreeNode.GetTreeLevelList(tree);
+
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root1 = [1], root2 = [1, 2];
+            int?[] exp = [2, 2];
+
+            var tree1 = TreeNode.BuildTree(root1);
+            var tree2 = TreeNode.BuildTree(root2);
+
+            var tree = alg.MergeTrees(tree1, tree2);
+            int?[] res = TreeNode.GetTreeLevelList(tree);
+
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0601_0650/617_MergeTwoBinaryTreesAlg.cs
@@ -0,0 +1,34 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _617_MergeTwoBinaryTreesAlg
+    {
+        public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+        {
+            if (root1 != null && root2 != null)
+            {
+                TreeNode node = new TreeNode(root1.val + root2.val);
+                node.left = MergeTrees(root1.left, root2.left);
+                node.right = MergeTrees(root1.right, root2.right);
+                return node;
+            }
+            else if (root1 != null)
+            {
+                return root1;
+            }
+            else if (root2 != null)
+            {
+                return root2;
+            }
+            return null;
+        }
+       
+
+    }
+}
