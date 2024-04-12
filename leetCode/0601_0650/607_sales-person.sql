835a551631a7c557b2ac01c36aad973f942e5627
hongtao_luo <luohongtao621@hotmail.com>
2024-04-12 13:11:15 +0800

    607

---
+++ leetCode/0601_0650/607_sales-person.sql
@@ -0,0 +1,5 @@
+﻿
+select name from SalesPerson where sales_id not in(
+ select b.sales_id from Company a inner join Orders b on a.com_id=b.com_id
+ inner join SalesPerson c on c.sales_id=b.sales_id
+ where a.name= 'RED')
