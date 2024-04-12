6dbb377a91431a325fd51b48b9e34e372ca47abd
hongtao_luo <luohongtao621@hotmail.com>
2024-04-12 11:56:27 +0800

    2923

---
+++ TestLeetCode/2901_3000/2923_FindChampionIUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2901_3000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2901_3000
+{
+    [TestClass]
+    public class _2923_FindChampionIUnitTest
+    {
+        _2923_FindChampionIAlg alg = new _2923_FindChampionIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[0, 1], [0, 0]];
+            int exp = 0;
+            int res = alg.FindChampion(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[0, 0, 1], [1, 0, 1], [0, 0, 0]];
+            int exp = 1;
+            int res = alg.FindChampion(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2901_3000/2923_FindChampionIAlg.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2901_3000
+{
+    public class _2923_FindChampionIAlg
+    {
+        public int FindChampion1(int[][] grid)
+        {
+            HashSet<int> result = new HashSet<int>();
+            HashSet<int> failed = new HashSet<int>();
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (i != j)
+                    {
+                        if (grid[i][j] == 1)
+                        {
+
+                            if (!failed.Contains(i))
+                            {
+                                result.Add(i);
+                            }
+                            failed.Add(j);
+                            result.Remove(j);
+                        }
+                        else
+                        {
+                            if (!failed.Contains(j))
+                            {
+                                result.Add(j);
+                            }
+
+                            failed.Add(i);
+                            result.Remove(i);
+                        }
+                    }
+                   
+                }
+            }
+            if (result.Count > 0)
+            {
+                return result.First();
+            }
+            return 0;
+        }
+        public int FindChampion(int[][] grid)
+        {
+            int n = grid.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int[] line = grid[i];
+                int sum = line.Sum();
+                if (sum == n - 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+    }
+}
