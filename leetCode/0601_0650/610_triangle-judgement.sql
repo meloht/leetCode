54d5f4e98daa2b3dc4f904976d449b57cf510b7e
hongtao_luo <luohongtao621@hotmail.com>
2024-04-12 13:22:55 +0800

    610

---
+++ leetCode/0601_0650/610_triangle-judgement.sql
@@ -0,0 +1,23 @@
+﻿select x,y,z ,
+case 
+when x>=y+z then 'No'
+when y>=x+z then 'No'
+when z>=y+x then 'No'
+else
+'Yes'
+end as triangle
+   from Triangle
+
+
+SELECT 
+    x,
+    y,
+    z,
+    CASE
+        WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+        ELSE 'No'
+    END AS 'triangle'
+FROM
+    triangle
+;
+
