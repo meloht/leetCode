82f1d820828f3e33fd2824d8291cb3c3229b6a90
hongtao luo <luohongtao621@163.com>
2024-04-13 15:59:00 +0800

    693

---
+++ TestLeetCode/0651_0700/693_BinaryNumberWithAlternatingBitsUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _693_BinaryNumberWithAlternatingBitsUnitTest
+    {
+        _693_BinaryNumberWithAlternatingBitsAlg alg = new _693_BinaryNumberWithAlternatingBitsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 5;
+            bool exp = true;
+            bool res = alg.HasAlternatingBits(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 7;
+            bool exp = false;
+            bool res = alg.HasAlternatingBits(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 11;
+            bool exp = false;
+            bool res = alg.HasAlternatingBits(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/693_BinaryNumberWithAlternatingBitsAlg.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _693_BinaryNumberWithAlternatingBitsAlg
+    {
+        public bool HasAlternatingBits1(int n)
+        {
+            string s = Convert.ToString(n, 2);
+            bool bl = s[0] == '1';
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (bl)
+                {
+                    if (s[i] == '0')
+                    {
+                        bl = false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (s[i] == '1')
+                    {
+                        bl = true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool HasAlternatingBits2(int n)
+        {
+            int prev = 2;
+            while (n != 0)
+            {
+                int cur = n % 2;
+                if (cur == prev)
+                {
+                    return false;
+                }
+                prev = cur;
+                n /= 2;
+            }
+            return true;
+        }
+
+        public bool HasAlternatingBits(int n)
+        {
+            int a = n ^ (n >> 1);
+            return (a & (a + 1)) == 0;
+        }
+
+
+
+    }
+}
