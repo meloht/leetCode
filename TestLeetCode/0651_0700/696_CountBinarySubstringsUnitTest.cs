b8487cdbde46d0d552ca2517b64c0f4e5d097d70
hongtao luo <luohongtao621@163.com>
2024-04-13 16:16:04 +0800

    696

---
+++ TestLeetCode/0651_0700/696_CountBinarySubstringsUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _696_CountBinarySubstringsUnitTest
+    {
+        _696_CountBinarySubstringsAlg alg = new _696_CountBinarySubstringsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "00110011";
+            int exp = 6;
+            int res = alg.CountBinarySubstrings(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "10101";
+            int exp = 4;
+            int res = alg.CountBinarySubstrings(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/696_CountBinarySubstringsAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _696_CountBinarySubstringsAlg
+    {
+        public int CountBinarySubstrings(string s)
+        {
+            return 0;
+        }
+    }
+}
