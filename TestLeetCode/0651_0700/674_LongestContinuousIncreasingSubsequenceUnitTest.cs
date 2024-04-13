d9ff91ee6812387bc7e42b0813ab32d318f8b215
hongtao luo <luohongtao621@163.com>
2024-04-13 13:31:01 +0800

    674

---
+++ TestLeetCode/0651_0700/674_LongestContinuousIncreasingSubsequenceUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _674_LongestContinuousIncreasingSubsequenceUnitTest
+    {
+        _674_LongestContinuousIncreasingSubsequenceAlg alg = new _674_LongestContinuousIncreasingSubsequenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 5, 4, 7];
+            int exp = 3;
+            int res = alg.FindLengthOfLCIS(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 2, 2, 2, 2];
+            int exp = 1;
+            int res = alg.FindLengthOfLCIS(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/674_LongestContinuousIncreasingSubsequenceAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _674_LongestContinuousIncreasingSubsequenceAlg
+    {
+        public int FindLengthOfLCIS(int[] nums)
+        {
+            if (nums.Length < 2)
+                return 1;
+            int ans = 1;
+            int count = 1;
+            int len = nums.Length;
+
+            for (int i = 1; i < len; i++)
+            {
+                if (nums[i - 1] < nums[i])
+                {
+                    count++;
+                    ans = Math.Max(ans, count);
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            return ans;
+        }
+    }
+}
