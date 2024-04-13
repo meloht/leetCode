2caf812567d5419b379c0927f814cbb5c2a571da
hongtao luo <luohongtao621@163.com>
2024-04-13 12:47:24 +0800

    661

---
+++ TestLeetCode/0651_0700/661_ImageSmootherUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _661_ImageSmootherUnitTest
+    {
+        _661_ImageSmootherAlg alg = new _661_ImageSmootherAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] img = [[1, 1, 1], [1, 0, 1], [1, 1, 1]];
+
+            int[][] exp = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+            int[][] res = alg.ImageSmoother(img);
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] img = [[100, 200, 100], [200, 50, 200], [100, 200, 100]];
+
+            int[][] exp = [[137, 141, 137], [141, 138, 141], [137, 141, 137]];
+            int[][] res = alg.ImageSmoother(img);
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+    }
+}
---
+++ leetCode/0651_0700/661_ImageSmootherAlg.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _661_ImageSmootherAlg
+    {
+        public int[][] ImageSmoother(int[][] img)
+        {
+            int m = img.Length;
+            int n = img[0].Length;
+            int[][] img2 = new int[m][];
+            for (int i = 0; i < m; i++)
+            {
+                img2[i] = new int[n];
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    img2[i][j] = Avg(img, i, j, m, n);
+                }
+            }
+            return img2;
+        }
+        private int Avg(int[][] img, int k, int h, int m, int n)
+        {
+            int mm2 = k + 1 >= m ? m : k + 2;
+            int nn2 = h + 1 >= n ? n : h + 2;
+            int sum = 0;
+            int count = 0;
+            int mm1 = k - 1 < 0 ? 0 : k - 1;
+            int nn1 = h - 1 < 0 ? 0 : h - 1;
+
+            for (int i = mm1; i < mm2; i++)
+            {
+                for (int j = nn1; j < nn2; j++)
+                {
+                    sum += img[i][j];
+                    count++;
+                }
+            }
+            int avg = sum / count;
+
+            return avg;
+        }
+    }
+}
