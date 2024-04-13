30288001c2b2e9e91b35d7a3e1879e09c2a7af1f
hongtao luo <luohongtao621@163.com>
2024-04-13 13:16:47 +0800

    671

---
+++ TestLeetCode/0651_0700/671_SecondMinimumNodeInABinaryTreeUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._0651_0700;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _671_SecondMinimumNodeInABinaryTreeUnitTest
+    {
+        _671_SecondMinimumNodeInABinaryTreeAlg alg = new _671_SecondMinimumNodeInABinaryTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [2, 2, 5, null, null, 5, 7];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 5;
+            int res = alg.FindSecondMinimumValue(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [2, 2, 2];
+            var tree = TreeNode.BuildTree(root);
+            int exp = -1;
+            int res = alg.FindSecondMinimumValue(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [1, 1, 3, 1, 1, 3, 4, 3, 1, 1, 8];
+            var tree = TreeNode.BuildTree(root);
+            int exp = 3;
+            int res = alg.FindSecondMinimumValue(tree);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/671_SecondMinimumNodeInABinaryTreeAlg.cs
@@ -0,0 +1,43 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _671_SecondMinimumNodeInABinaryTreeAlg
+    {
+        public int FindSecondMinimumValue(TreeNode root)
+        {
+            int ans = Dfs(root, root.val);
+
+            return ans;
+        }
+        private int Dfs(TreeNode root, int val)
+        {
+            if (root == null)
+                return -1;
+            if (root.val != val)
+            {
+                return root.val;
+            }
+            int left = Dfs(root.left, val);
+            int right = Dfs(root.right, val);
+            if (left != -1 && right != -1)
+            {
+                return Math.Min(left, right);
+            }
+            else if (right != -1)
+            {
+                return right;
+            }
+            else if (left != -1)
+            {
+                return left;
+            }
+            return -1;
+        }
+    }
+}
