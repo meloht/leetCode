5f802b78f72a3b47edf65347c255180e953b74af
hongtao luo <luohongtao621@163.com>
2024-04-13 11:51:05 +0800

    645

---
+++ TestLeetCode/0601_0650/645_SetMismatchUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _645_SetMismatchUnitTest
+    {
+        _645_SetMismatchAlg alg = new _645_SetMismatchAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 2, 4];
+            int[] exp = [2, 3];
+            int[] res = alg.FindErrorNums(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1];
+            int[] exp = [1, 2];
+            int[] res = alg.FindErrorNums(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0601_0650/645_SetMismatchAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _645_SetMismatchAlg
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            Dictionary<int, int> set = new Dictionary<int, int>();
+            int[] result = new int[2];
+            foreach (int num in nums) 
+            {
+                if (set.ContainsKey(num))
+                {
+                    set[num]++;
+                }
+                else
+                {
+                    set.Add(num, 1);
+                }
+            }
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                if (!set.ContainsKey(i))
+                {
+                    result[1] = i;
+                }
+                else if (set[i]>1) 
+                {
+                    result[0] = i;
+                }
+            }
+            return result;
+        }
+    }
+}
