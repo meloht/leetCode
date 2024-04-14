77943ecac617003141d1a20c9a73ee47ddfd7b34
hongtao luo <luohongtao621@163.com>
2024-04-14 14:17:49 +0800

    705

---
+++ TestLeetCode/0701_0750/705_DesignHashsetUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0701_0750._705_DesignHashsetAlg;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _705_DesignHashsetUnitTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            MyHashSet myHashSet = new MyHashSet();
+            myHashSet.Add(1);      // set = [1]
+            myHashSet.Add(2);      // set = [1, 2]
+            Assert.AreEqual(true, myHashSet.Contains(1)); // 返回 True
+            Assert.AreEqual(false, myHashSet.Contains(3)); // 返回 False ，（未找到）
+            myHashSet.Add(2);      // set = [1, 2]
+            Assert.AreEqual(true, myHashSet.Contains(2)); // 返回 True
+            myHashSet.Remove(2);   // set = [1]
+            Assert.AreEqual(false, myHashSet.Contains(2)); // 返回 False ，（已移除）
+        }
+    }
+}
---
+++ leetCode/0701_0750/705_DesignHashsetAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _705_DesignHashsetAlg
+    {
+        public class MyHashSet
+        {
+            int[] map = new int[1000001];
+            public MyHashSet()
+            {
+
+            }
+
+            public void Add(int key)
+            {
+                if (map[key] > 0)
+                    return;
+                map[key] = 1;
+            }
+
+            public void Remove(int key)
+            {
+                map[key] = 0;
+            }
+
+            public bool Contains(int key)
+            {
+                if (map[key] > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
