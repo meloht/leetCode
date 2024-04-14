3fddccaf30352c9ce0dd70a0dc82c72079d0c41c
hongtao luo <luohongtao621@163.com>
2024-04-14 14:06:18 +0800

    700

---
+++ TestLeetCode/0651_0700/700_SearchInABinarySearchTreeUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0651_0700;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _700_SearchInABinarySearchTreeUnitTest
+    {
+        _700_SearchInABinarySearchTreeAlg alg = new _700_SearchInABinarySearchTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [4, 2, 7, 1, 3];
+            int val = 2;
+            int?[] exp = [2, 1, 3];
+            var tree = TreeNode.BuildTree(root);
+            var rest = alg.SearchBST(tree, val);
+            int?[] res = TreeNode.GetTreeLevelList(rest);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [4, 2, 7, 1, 3];
+            int val = 5;
+            int?[] exp = [];
+            var tree = TreeNode.BuildTree(root);
+            var rest = alg.SearchBST(tree, val);
+            int?[] res = TreeNode.GetTreeLevelList(rest);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+
+}
---
+++ leetCode/0651_0700/700_SearchInABinarySearchTreeAlg.cs
@@ -0,0 +1,21 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _700_SearchInABinarySearchTreeAlg
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+                return null;
+            if(root.val==val)
+                return root;
+            return SearchBST(val < root.val ? root.left : root.right, val);
+        }
+    }
+}
