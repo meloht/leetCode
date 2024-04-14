032216dc46c168c2fe51a3b2319470c4440158c5
hongtao luo <luohongtao621@163.com>
2024-04-14 18:34:41 +0800

    706

---
+++ TestLeetCode/0701_0750/706_DesignHashmapUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0701_0750._706_DesignHashmapAlg;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _706_DesignHashmapUnitTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            MyHashMap myHashMap = new MyHashMap();
+            myHashMap.Put(1, 1); // myHashMap 现在为 [[1,1]]
+            myHashMap.Put(2, 2); // myHashMap 现在为 [[1,1], [2,2]]
+            Assert.AreEqual(1, myHashMap.Get(1));    // 返回 1 ，myHashMap 现在为 [[1,1], [2,2]]
+            Assert.AreEqual(-1, myHashMap.Get(3));    // 返回 -1（未找到），myHashMap 现在为 [[1,1], [2,2]]
+            myHashMap.Put(2, 1); // myHashMap 现在为 [[1,1], [2,1]]（更新已有的值）
+            Assert.AreEqual(1, myHashMap.Get(2));    // 返回 1 ，myHashMap 现在为 [[1,1], [2,1]]
+            myHashMap.Remove(2); // 删除键为 2 的数据，myHashMap 现在为 [[1,1]]
+            Assert.AreEqual(-1, myHashMap.Get(2));    // 返回 -1（未找到），myHashMap 现在为 [[1,1]]
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            MyHashMap myHashMap = new MyHashMap();
+            myHashMap.Put(769, 1); // myHashMap 现在为 [[1,1]]
+            myHashMap.Put(20000, 2); // myHashMap 现在为 [[1,1], [2,2]]
+            Assert.AreEqual(1, myHashMap.Get(769));    // 返回 1 ，myHashMap 现在为 [[1,1], [2,2]]
+            Assert.AreEqual(-1, myHashMap.Get(3));    // 返回 -1（未找到），myHashMap 现在为 [[1,1], [2,2]]
+            myHashMap.Put(2, 1); // myHashMap 现在为 [[1,1], [2,1]]（更新已有的值）
+            Assert.AreEqual(1, myHashMap.Get(2));    // 返回 1 ，myHashMap 现在为 [[1,1], [2,1]]
+            myHashMap.Remove(2); // 删除键为 2 的数据，myHashMap 现在为 [[1,1]]
+            myHashMap.Remove(769);
+            myHashMap.Remove(20000);
+            Assert.AreEqual(-1, myHashMap.Get(2));    // 返回 -1（未找到），myHashMap 现在为 [[1,1]]
+            Assert.AreEqual(-1, myHashMap.Get(20000));
+            Assert.AreEqual(-1, myHashMap.Get(769));
+        }
+    }
+}
---
+++ leetCode/0701_0750/706_DesignHashmapAlg.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _706_DesignHashmapAlg
+    {
+        public class MyHashMap1
+        {
+            int[] map;
+            public MyHashMap1()
+            {
+                map = new int[1000001];
+                Array.Fill(map, -1);
+            }
+
+            public void Put(int key, int value)
+            {
+                map[key] = value;
+            }
+
+            public int Get(int key)
+            {
+                return map[key];
+            }
+
+            public void Remove(int key)
+            {
+                map[key] = -1;
+            }
+        }
+
+        public class MyHashMap
+        {
+            private int baseNum = 769;
+            private LinkedList<Pair>[] data;
+
+            public MyHashMap()
+            {
+                data = new LinkedList<Pair>[baseNum];
+                for (int i = 0; i < baseNum; i++)
+                {
+                    data[i] = new LinkedList<Pair>();
+                }
+            }
+
+            public void Put(int key, int value)
+            {
+                System.Console.WriteLine($"{key},{value}");
+                int h = GetHash(key);
+                var iterator = data[h];
+                foreach (var item in iterator)
+                {
+                    if (item.Key == key)
+                    {
+                        item.Value = value;
+                        return;
+                    }
+                }
+                iterator.AddLast(new LinkedListNode<Pair>(new Pair(key, value)));
+            }
+
+            public int Get(int key)
+            {
+                int h = GetHash(key);
+                var iterator = data[h];
+                foreach (var item in iterator)
+                {
+                    if (item.Key == key)
+                    {
+                        return item.Value;
+                    }
+                }
+                return -1;
+            }
+
+            public void Remove(int key)
+            {
+                int h = GetHash(key);
+                var iterator = data[h];
+               
+                Pair pair = null;
+                foreach (var item in iterator)
+                {
+                    if (item.Key == key)
+                    {
+                        pair = item;
+                        
+                        break;
+                    }
+                }
+                if (pair != null)
+                {
+                    iterator.Remove(pair);
+                }
+
+            }
+            private int GetHash(int key)
+            {
+                return key % baseNum;
+
+            }
+
+            private class Pair
+            {
+                public int Key { get; set; }
+                public int Value { get; set; }
+                public Pair() { }
+                public Pair(int k, int v)
+                {
+                    this.Value = v;
+                    this.Key = k;
+                }
+
+            }
+        }
+    }
+}
