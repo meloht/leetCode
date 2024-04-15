dddc0a3ed0a05a2eb0456de52d13ec536e5bbfd8
hongtao_luo <luohongtao621@hotmail.com>
2024-04-15 13:40:34 +0800

    728

---
+++ TestLeetCode/0701_0750/728_SelfDividingNumbersUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _728_SelfDividingNumbersUnitTest
+    {
+        _728_SelfDividingNumbersAlg alg = new _728_SelfDividingNumbersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int left = 1, right = 22;
+            int[] exp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22];
+            int[] res = alg.SelfDividingNumbers(left, right).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int left = 1, right = 22;
+            int[] exp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22];
+            int[] res = alg.SelfDividingNumbers(left, right).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0701_0750/728_SelfDividingNumbersAlg.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _728_SelfDividingNumbersAlg
+    {
+        public IList<int> SelfDividingNumbers(int left, int right)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = left; i <= right; i++)
+            {
+                var list = GetNum(i);
+                bool bl = true;
+                foreach (var item in list)
+                {
+                    if (item==0||i % item != 0)
+                    {
+                        bl = false;
+                        break;
+                    }
+                }
+                if (bl)
+                {
+                    numbers.Add(i);
+                }
+            }
+            return numbers;
+        }
+
+        private List<int> GetNum(int num)
+        {
+            List<int> list = new List<int>();
+            if (num == 0)
+            {
+                list.Add(0);
+                return list;
+            }
+            int n = num;
+            while (n > 0)
+            {
+                list.Add(n % 10);
+                n = n / 10;
+            }
+           
+            return list;
+        }
+    }
+}
