fc8608156676d0f9d0a14e477d8723e30d2346bd
hongtao_luo <luohongtao621@hotmail.com>
2024-04-15 13:26:49 +0800

    724

---
+++ TestLeetCode/0701_0750/724_FindPivotIndexUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _724_FindPivotIndexUnitTest
+    {
+        _724_FindPivotIndexAlg alg = new _724_FindPivotIndexAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 7, 3, 6, 5, 6];
+            int exp = 3;
+            int res = alg.PivotIndex(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3];
+            int exp = -1;
+            int res = alg.PivotIndex(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [2, 1, -1];
+            int exp = 0;
+            int res = alg.PivotIndex(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0701_0750/724_FindPivotIndexAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+
+    public class _724_FindPivotIndexAlg
+    {
+        public int PivotIndex(int[] nums)
+        {
+            int[] arr = new int[nums.Length + 1];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr[i + 1] = arr[i] + nums[i];
+            }
+            int ans = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int left = arr[i];
+                int right = arr[nums.Length] - left - nums[i];
+                if (left == right)
+                {
+                    ans = i;
+                    break;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
