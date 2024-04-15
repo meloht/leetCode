9cfe846881c94c2139cad1c125a0e0ca7b943673
hongtao_luo <luohongtao621@hotmail.com>
2024-04-15 13:11:24 +0800

    717

---
+++ TestLeetCode/0701_0750/717_1BitAnd2BitCharactersUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _717_1BitAnd2BitCharactersUnitTest
+    {
+        _717_1BitAnd2BitCharactersAlg alg = new _717_1BitAnd2BitCharactersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] bits = [1, 0, 0];
+            bool exp = true;
+            bool res = alg.IsOneBitCharacter(bits);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] bits = [1, 1, 1, 0];
+            bool exp = false;
+            bool res = alg.IsOneBitCharacter(bits);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] bits = [0, 1, 0];
+            bool exp = false;
+            bool res = alg.IsOneBitCharacter(bits);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0701_0750/717_1BitAnd2BitCharactersAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _717_1BitAnd2BitCharactersAlg
+    {
+        public bool IsOneBitCharacter1(int[] bits)
+        {
+            int i = 0;
+            while (i < bits.Length)
+            {
+                if (bits[i] == 0)
+                {
+                    i++;
+                }
+                else if (bits[i] == 1)
+                {
+                    i += 2;
+                    if (i == bits.Length)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsOneBitCharacter(int[] bits)
+        {
+            int n = bits.Length, i = 0;
+            while (i < n - 1)
+            {
+                i += bits[i] + 1;
+            }
+            return i == n - 1;
+        }
+
+
+    }
+}
