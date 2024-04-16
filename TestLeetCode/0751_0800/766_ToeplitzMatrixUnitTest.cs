ede07878d2484acb9f15d98209cc045c25d6a093
hongtao_luo <luohongtao621@hotmail.com>
2024-04-16 13:53:10 +0800

    766

---
+++ TestLeetCode/0751_0800/766_ToeplitzMatrixUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _766_ToeplitzMatrixUnitTest
+    {
+        _766_ToeplitzMatrixAlg alg = new _766_ToeplitzMatrixAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] matrix = [[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]];
+            bool exp = true;
+            bool res = alg.IsToeplitzMatrix(matrix);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] matrix = [[1, 2], [2, 2]];
+            bool exp = false;
+            bool res = alg.IsToeplitzMatrix(matrix);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/766_ToeplitzMatrixAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _766_ToeplitzMatrixAlg
+    {
+        public bool IsToeplitzMatrix(int[][] matrix)
+        {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            for (int i = 0; i < n; i++)
+            {
+                int num = matrix[0][i];
+                for (int k = 1, j = i + 1; j < n && k < m; j++, k++)
+                {
+                    if (num != matrix[k][j])
+                        return false;
+                }
+            }
+            for (int i = 1; i < m; i++)
+            {
+                int num = matrix[i][0];
+                for (int k = i + 1, j = 1; j < n && k < m; j++, k++)
+                {
+                    if (num != matrix[k][j])
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
