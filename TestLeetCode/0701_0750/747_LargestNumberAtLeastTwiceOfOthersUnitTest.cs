192713531fc970e320f810ec735aec9f230d430e
hongtao_luo <luohongtao621@hotmail.com>
2024-04-16 12:34:14 +0800

    747

---
+++ TestLeetCode/0701_0750/747_LargestNumberAtLeastTwiceOfOthersUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _747_LargestNumberAtLeastTwiceOfOthersUnitTest
+    {
+        _747_LargestNumberAtLeastTwiceOfOthersAlg alg = new _747_LargestNumberAtLeastTwiceOfOthersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 6, 1, 0];
+            int exp = 1;
+            int res = alg.DominantIndex(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int exp = -1;
+            int res = alg.DominantIndex(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0701_0750/747_LargestNumberAtLeastTwiceOfOthersAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _747_LargestNumberAtLeastTwiceOfOthersAlg
+    {
+        public int DominantIndex(int[] nums)
+        {
+            int max = int.MinValue;
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    index = i;
+                    max = nums[i];
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == index)
+                {
+                    continue;
+                }
+                if (nums[i] != 0 && max / nums[i] < 2 && max % nums[i] != 0)
+                {
+                    return -1;
+                }
+            }
+            return index;
+        }
+    }
+}
