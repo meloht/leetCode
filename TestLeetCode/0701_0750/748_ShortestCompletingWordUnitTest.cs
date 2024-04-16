3f3a1ec219f4cfc1e004500c8ee1e87203d31e08
hongtao_luo <luohongtao621@hotmail.com>
2024-04-16 13:24:31 +0800

    748

---
+++ TestLeetCode/0701_0750/748_ShortestCompletingWordUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _748_ShortestCompletingWordUnitTest
+    {
+        _748_ShortestCompletingWordAlg alg = new _748_ShortestCompletingWordAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string licensePlate = "1s3 PSt";
+            string[] words = ["step", "steps", "stripe", "stepple"];
+            string exp = "steps";
+            string res = alg.ShortestCompletingWord(licensePlate, words);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string licensePlate = "1s3 456";
+            string[] words = ["looks", "pest", "stew", "show"];
+            string exp = "pest";
+            string res = alg.ShortestCompletingWord(licensePlate, words);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0701_0750/748_ShortestCompletingWordAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _748_ShortestCompletingWordAlg
+    {
+        public string ShortestCompletingWord(string licensePlate, string[] words)
+        {
+            string ans = "";
+            int min = int.MaxValue;
+            int[] dict = new int[26];
+            foreach (var item in licensePlate)
+            {
+                if (char.IsLetter(item))
+                {
+                    char ch = char.ToLower(item);
+                    int index = ch - 'a';
+                    dict[index]++;
+                }
+            }
+            foreach (var item in words)
+            {
+                var res = item.GroupBy(x => x).ToDictionary(x => x.Key - 'a', x => x.Count());
+                bool bl = true;
+                for (int i = 0; i < dict.Length; i++)
+                {
+                    if (dict[i] > 0)
+                    {
+                        if (!res.ContainsKey(i))
+                        {
+                            bl = false;
+                            break;
+
+                        }
+                        if (res[i] < dict[i])
+                        {
+                            bl = false;
+                            break;
+                        }
+                    }
+                }
+               
+                if (bl)
+                {
+                    if (item.Length < min)
+                    {
+                        ans = item;
+                        min = item.Length;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
