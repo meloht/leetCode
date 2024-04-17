52b66412b60bdf15e1d1e3ab1aa90631259405a6
hongtao_luo <luohongtao621@hotmail.com>
2024-04-17 13:23:43 +0800

    806

---
+++ TestLeetCode/0801_0850/806_NumberOfLinesToWriteStringUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _806_NumberOfLinesToWriteStringUnitTest
+    {
+        _806_NumberOfLinesToWriteStringAlg alg = new _806_NumberOfLinesToWriteStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] widths = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+            string s = "abcdefghijklmnopqrstuvwxyz";
+            int[] exp = [3, 60];
+            int[] res = alg.NumberOfLines(widths, s);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] widths = [4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+            string s = "bbbcccdddaaa";
+            int[] exp = [2, 4];
+            int[] res = alg.NumberOfLines(widths, s);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0801_0850/806_NumberOfLinesToWriteStringAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _806_NumberOfLinesToWriteStringAlg
+    {
+        public int[] NumberOfLines(int[] widths, string s)
+        {
+            int rows = 0;
+            int w = 0;
+            foreach (var item in s)
+            {
+                w += widths[item - 'a'];
+                if (w == 100)
+                {
+                    rows++;
+                    w = 0;
+                }
+                else if (w > 100)
+                {
+                    rows++;
+                    w = widths[item - 'a'];
+                }
+            }
+            if (w == 0)
+            {
+                return [rows, 100];
+            }
+            return [rows + 1, w];
+        }
+    }
+}
