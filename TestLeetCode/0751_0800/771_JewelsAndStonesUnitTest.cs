56f7033cd98e1e5a3566b043ead70c37cd527b10
hongtao_luo <luohongtao621@hotmail.com>
2024-04-17 12:19:34 +0800

    771

---
+++ TestLeetCode/0751_0800/771_JewelsAndStonesUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _771_JewelsAndStonesUnitTest
+    {
+        _771_JewelsAndStonesAlg alg = new _771_JewelsAndStonesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string jewels = "aA", stones = "aAAbbbb";
+            int exp = 3;
+            int res = alg.NumJewelsInStones(jewels, stones);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string jewels = "z", stones = "ZZ";
+            int exp = 0;
+            int res = alg.NumJewelsInStones(jewels, stones);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/771_JewelsAndStonesAlg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _771_JewelsAndStonesAlg
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            HashSet<char> set=new HashSet<char>(jewels);
+            int ans = 0;
+            foreach (var item in stones)
+            {
+                if (set.Contains(item))
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
