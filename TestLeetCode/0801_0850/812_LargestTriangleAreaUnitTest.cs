7a4bf86a5fcadcf8e635e3aa0aaa5dad3b501623
hongtao_luo <luohongtao621@hotmail.com>
2024-04-17 13:53:01 +0800

    812

---
+++ TestLeetCode/0801_0850/812_LargestTriangleAreaUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _812_LargestTriangleAreaUnitTest
+    {
+        _812_LargestTriangleAreaAlg alg = new _812_LargestTriangleAreaAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] points = [[0, 0], [0, 1], [1, 0], [0, 2], [2, 0]];
+            double exp = 2.00000;
+            double res = alg.LargestTriangleArea(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] points = [[1, 0], [0, 0], [0, 1]];
+            double exp = 0.50000;
+            double res = alg.LargestTriangleArea(points);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/812_LargestTriangleAreaAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _812_LargestTriangleAreaAlg
+    {
+        public double LargestTriangleArea(int[][] points)
+        {
+            double max = 0;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                for (int j = i + 1; j < points.Length; j++)
+                {
+                    for (int k = j + 1; k < points.Length; k++)
+                    {
+                        double res = GetArea(points[i], points[j], points[k]);
+                        if (res == 0)
+                            continue;
+                        max = Math.Max(max, res);
+                    }
+                }
+            }
+
+            return Math.Round(max, 5);
+        }
+
+        private double GetArea(int[] a, int[] b, int[] c)
+        {
+            double x = GetLine(a, b);
+            double y = GetLine(a, c);
+            double z = GetLine(b, c);
+
+            if (x + y <= z || x + z <= y || y + z <= x)
+            {
+                return 0;
+            }
+
+            double p = (x + y + z) / 2;
+
+            double area = p * (p - x) * (p - y) * (p - z);
+            return Math.Sqrt(area);
+        }
+        private double GetLine(int[] a, int[] b)
+        {
+            double num = (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1]);
+            if (num == 0)
+                return 0;
+            return Math.Sqrt(num);
+        }
+    }
+}
