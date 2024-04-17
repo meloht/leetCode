758d6bb5e2a5760ccecc81e4c3d9d52d97bb3e4d
hongtao_luo <luohongtao621@hotmail.com>
2024-04-17 12:34:38 +0800

    796

---
+++ TestLeetCode/0751_0800/796_RotateStringUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _796_RotateStringUnitTest
+    {
+        _796_RotateStringAlg alg = new _796_RotateStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abcde", goal = "cdeab";
+            bool exp = true;
+            bool res = alg.RotateString(s, goal);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "abcde", goal = "abced";
+            bool exp = false;
+            bool res = alg.RotateString(s, goal);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/796_RotateStringAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _796_RotateStringAlg
+    {
+        public bool RotateString(string s, string goal)
+        {
+            List<char> list = new List<char>(s);
+            for (int i = 0; i < s.Length; i++)
+            {
+                list.Add(s[i]);
+                list.RemoveAt(0);
+                if (goal == new string(list.ToArray()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool RotateString1(string s, string goal)
+        {
+            return s.Length == goal.Length && (s + s).Contains(goal);
+        }
+
+
+    }
+}
