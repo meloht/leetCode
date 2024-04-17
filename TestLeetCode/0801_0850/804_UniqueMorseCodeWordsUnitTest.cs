8e5386570abb2961ef882d9a0b4a9d9c04a9c1c9
hongtao_luo <luohongtao621@hotmail.com>
2024-04-17 13:06:07 +0800

    804

---
+++ TestLeetCode/0801_0850/804_UniqueMorseCodeWordsUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0751_0800;
+using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _804_UniqueMorseCodeWordsUnitTest
+    {
+        _804_UniqueMorseCodeWordsAlg alg = new _804_UniqueMorseCodeWordsAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["gin", "zen", "gig", "msg"];
+            int exp = 2;
+            int res = alg.UniqueMorseRepresentations(words);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["a"];
+            int exp = 1;
+            int res = alg.UniqueMorseRepresentations(words);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/804_UniqueMorseCodeWordsAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _804_UniqueMorseCodeWordsAlg
+    {
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            string[] map = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+
+            HashSet<string> set=new HashSet<string>();
+            foreach (string word in words) 
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (char c in word) 
+                {
+                    int i = c - 'a';
+                    sb.Append(map[i]);
+                }
+                set.Add(sb.ToString());
+            }
+
+            return set.Count;
+        }
+    }
+}
