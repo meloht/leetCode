f358fa1b8c4e5551a51c96ac0df7a9361d0a74a4
hongtao_luo <luohongtao621@hotmail.com>
2024-04-18 12:35:09 +0800

    821

---
+++ TestLeetCode/0801_0850/821_ShortestDistanceToACharacterUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _821_ShortestDistanceToACharacterUnitTest
+    {
+        _821_ShortestDistanceToACharacterAlg alg = new _821_ShortestDistanceToACharacterAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "loveleetcode";
+            char c = 'e';
+            int[] exp = [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0];
+            int[] res = alg.ShortestToChar(s, c);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "aaab";
+            char c = 'b';
+            int[] exp = [3, 2, 1, 0];
+            int[] res = alg.ShortestToChar(s, c);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/821_ShortestDistanceToACharacterAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _821_ShortestDistanceToACharacterAlg
+    {
+        public int[] ShortestToChar(string s, char c)
+        {
+            return null;
+        }
+    }
+}
