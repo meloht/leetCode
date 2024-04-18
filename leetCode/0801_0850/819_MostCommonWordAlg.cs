66e46e0013acc2fc3c33c129247738f22a43e31c
hongtao_luo <luohongtao621@hotmail.com>
2024-04-18 12:25:46 +0800

    819

--- leetCode/0801_0850/819_MostCommonWordAlg.cs
+++ leetCode/0801_0850/819_MostCommonWordAlg.cs
@@ -62,5 +62,56 @@ namespace leetCode._0801_0850
             }
             return word;
         }
+
+        public string MostCommonWord2(string paragraph, string[] banned)
+        {
+            ISet<string> bannedSet = new HashSet<string>();
+            foreach (string word in banned)
+            {
+                bannedSet.Add(word);
+            }
+            int maxFrequency = 0;
+            Dictionary<string, int> frequencies = new Dictionary<string, int>();
+            StringBuilder sb = new StringBuilder();
+            int length = paragraph.Length;
+            for (int i = 0; i <= length; i++)
+            {
+                if (i < length && char.IsLetter(paragraph[i]))
+                {
+                    sb.Append(char.ToLower(paragraph[i]));
+                }
+                else if (sb.Length > 0)
+                {
+                    string word = sb.ToString();
+                    if (!bannedSet.Contains(word))
+                    {
+                        if (!frequencies.ContainsKey(word))
+                        {
+                            frequencies.Add(word, 1);
+                        }
+                        else
+                        {
+                            frequencies[word]++;
+                        }
+                        maxFrequency = Math.Max(maxFrequency, frequencies[word]);
+                    }
+                    sb.Length = 0;
+                }
+            }
+            string mostCommon = "";
+            foreach (KeyValuePair<string, int> pair in frequencies)
+            {
+                string word = pair.Key;
+                int frequency = pair.Value;
+                if (frequency == maxFrequency)
+                {
+                    mostCommon = word;
+                    break;
+                }
+            }
+            return mostCommon;
+        }
+
+
     }
 }
