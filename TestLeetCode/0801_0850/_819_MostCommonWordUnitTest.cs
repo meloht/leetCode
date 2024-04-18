20da6daf6ada57555e453dac934e9089b8894f80
hongtao_luo <luohongtao621@hotmail.com>
2024-04-18 12:25:22 +0800

    819

---
+++ TestLeetCode/0801_0850/_819_MostCommonWordUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _819_MostCommonWordUnitTest
+    {
+        _819_MostCommonWordAlg alg = new _819_MostCommonWordAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+            string[] banned = ["hit"];
+
+            string exp = "ball";
+
+            string res = alg.MostCommonWord(paragraph, banned);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string paragraph = "a.";
+            string[] banned = [];
+
+            string exp = "a";
+
+            string res = alg.MostCommonWord(paragraph, banned);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            string paragraph = "a, a, a, a, b,b,b,c, c";
+            string[] banned = ["a"];
+
+            string exp = "b";
+
+            string res = alg.MostCommonWord(paragraph, banned);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/819_MostCommonWordAlg.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _819_MostCommonWordAlg
+    {
+        public string MostCommonWord(string paragraph, string[] banned)
+        {
+            HashSet<string> set = new HashSet<string>(banned.Length);
+            foreach (var item in banned)
+            {
+                set.Add(item.ToLower());
+            }
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            var arr = paragraph.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            int max = 0;
+            string word = "";
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i].EndsWith('!') || arr[i].EndsWith('?')
+                    || arr[i].EndsWith(',') || arr[i].EndsWith(';') 
+                    || arr[i].EndsWith('.')|| arr[i].EndsWith("'"))
+                {
+                    arr[i] = arr[i].Substring(0, arr[i].Length - 1);
+                }
+                if (arr[i].Contains('!') || arr[i].Contains('?')
+                   || arr[i].Contains(',') || arr[i].Contains(';')
+                   || arr[i].Contains('.'))
+                {
+                    var arrAdd = arr[i].Split(new char[] { ',', '!', '?','.',';','\'' },StringSplitOptions.RemoveEmptyEntries);
+                    if (arrAdd.Length > 0)
+                    {
+                        arr[i] = arrAdd[0];
+                    }
+                    for (int j = 1; j < arrAdd.Length; j++)
+                    {
+                        arr.Add(arrAdd[j]);
+                    }
+                  
+                }
+                string item = arr[i].ToLower();
+                if (!set.Contains(item))
+                {
+                    if (dict.ContainsKey(item))
+                    {
+                        dict[item]++;
+                    }
+                    else
+                    {
+                        dict.Add(item, 1);
+                    }
+                    if (dict[item] > max) 
+                    {
+                        max = dict[item];
+                        word = item;
+                    }
+                }
+            }
+            return word;
+        }
+    }
+}
