b97272b1a56edb46066575f16332382f7e1083a3
hongtao_luo <luohongtao621@hotmail.com>
2024-04-18 13:49:53 +0800

    824

---
+++ TestLeetCode/0801_0850/824_GoatLatinUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _824_GoatLatinUnitTest
+    {
+        _824_GoatLatinAlg alg = new _824_GoatLatinAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string sentence = "I speak Goat Latin";
+            string exp = "Imaa peaksmaaa oatGmaaaa atinLmaaaaa";
+            string res = alg.ToGoatLatin(sentence);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string sentence = "The quick brown fox jumped over the lazy dog";
+            string exp = "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa";
+            string res = alg.ToGoatLatin(sentence);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/824_GoatLatinAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _824_GoatLatinAlg
+    {
+        public string ToGoatLatin(string sentence)
+        {
+            string[] arr = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i][0] == 'a' || arr[i][0] == 'A'
+                    || arr[i][0] == 'e' || arr[i][0] == 'E'
+                    || arr[i][0] == 'i' || arr[i][0] == 'I'
+                    || arr[i][0] == 'o' || arr[i][0] == 'O'
+                     || arr[i][0] == 'u' || arr[i][0] == 'U')
+                {
+                    sb.Append(arr[i]).Append("ma");
+                }
+                else
+                {
+                    sb.Append(arr[i].Substring(1)).Append(arr[i][0]).Append("ma");
+                }
+                for (int j = 0; j < i + 1; j++)
+                {
+                    sb.Append("a");
+                }
+                sb.Append(" ");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
