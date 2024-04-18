10953951eb36e17a2750b809f36a8b0331c31738
hongtao_luo <luohongtao621@hotmail.com>
2024-04-18 14:26:47 +0800

    830

---
+++ TestLeetCode/0801_0850/830_PositionsOfLargeGroupsUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _830_PositionsOfLargeGroupsUnitTest
+    {
+        _830_PositionsOfLargeGroupsAlg alg = new _830_PositionsOfLargeGroupsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abbxxxxzzy";
+            IList<IList<int>> exp = [[3, 6]];
+            IList<IList<int>> res = alg.LargeGroupPositions(s);
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "abc";
+            IList<IList<int>> exp = [];
+            IList<IList<int>> res = alg.LargeGroupPositions(s);
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "abcdddeeeeaabbbcd";
+            IList<IList<int>> exp = [[3, 5], [6, 9], [12, 14]];
+            IList<IList<int>> res = alg.LargeGroupPositions(s);
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "aba";
+            IList<IList<int>> exp = [];
+            IList<IList<int>> res = alg.LargeGroupPositions(s);
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0801_0850/830_PositionsOfLargeGroupsAlg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+
+    public class _830_PositionsOfLargeGroupsAlg
+    {
+        public IList<IList<int>> LargeGroupPositions(string s)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+            int count = 1;
+            char prev = s[0];
+            int begin = 0;
+            int end = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == prev)
+                {
+                    count++;
+                    end = i;
+                }
+                else
+                {
+                    prev= s[i];
+                    if (count >= 3)
+                    {
+                        res.Add([begin, end]);
+                    }
+                    count = 1;
+                    begin = i;
+                    end = i;
+                }
+            }
+            if (count >= 3)
+            {
+                res.Add([begin, end]);
+            }
+            res.Sort((x, y) => x[0].CompareTo(y[0]));
+            return res;
+        }
+    }
+}
