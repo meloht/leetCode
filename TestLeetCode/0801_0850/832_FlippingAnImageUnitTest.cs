42ecd7742435780a0579bd8bd1676443b0c84993
hongtao_luo <luohongtao621@hotmail.com>
2024-04-19 11:59:30 +0800

    832

---
+++ TestLeetCode/0801_0850/832_FlippingAnImageUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _832_FlippingAnImageUnitTest
+    {
+        _832_FlippingAnImageAlg alg = new _832_FlippingAnImageAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] image = [[1, 1, 0], [1, 0, 1], [0, 0, 0]];
+            int[][] exp = [[1, 0, 0], [0, 1, 0], [1, 1, 1]];
+            int[][] res = alg.FlipAndInvertImage(image);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] image = [[1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0]];
+            int[][] exp = [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 1], [1, 0, 1, 0]];
+            int[][] res = alg.FlipAndInvertImage(image);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0801_0850/832_FlippingAnImageAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _832_FlippingAnImageAlg
+    {
+        public int[][] FlipAndInvertImage(int[][] image)
+        {
+            int m = image.Length;
+            int n = image[0].Length;
+           
+            for (int i = 0; i < m; i++)
+            {
+                Array.Reverse(image[i]);
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    image[i][j] = image[i][j] == 1 ? 0 : 1;
+                }
+            }
+            return image;
+        }
+    }
+}
