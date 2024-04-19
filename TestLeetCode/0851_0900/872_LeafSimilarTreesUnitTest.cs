0d61e7b905d76ef56729da078f2bb4071f9e94c1
hongtao_luo <luohongtao621@hotmail.com>
2024-04-19 13:50:00 +0800

    872

---
+++ TestLeetCode/0851_0900/872_LeafSimilarTreesUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode._0851_0900;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _872_LeafSimilarTreesUnitTest
+    {
+        _872_LeafSimilarTreesAlg alg = new _872_LeafSimilarTreesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root1 = [3, 5, 1, 6, 2, 9, 8, null, null, 7, 4],
+                root2 = [3, 5, 1, 6, 7, 4, 2, null, null, null, null, null, null, 9, 8];
+
+            var tree1 = TreeNode.BuildTree(root1);
+            var tree2 = TreeNode.BuildTree(root2);
+            bool exp = true;
+            bool res = alg.LeafSimilar(tree1, tree2);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root1 = [1, 2, 3], root2 = [1, 3, 2];
+
+            var tree1 = TreeNode.BuildTree(root1);
+            var tree2 = TreeNode.BuildTree(root2);
+            bool exp = false;
+            bool res = alg.LeafSimilar(tree1, tree2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/872_LeafSimilarTreesAlg.cs
@@ -0,0 +1,36 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _872_LeafSimilarTreesAlg
+    {
+        public bool LeafSimilar(TreeNode root1, TreeNode root2)
+        {
+            List<int> list = new List<int>();
+            Dfs(root1, list);
+
+            List<int> list2 = new List<int>();
+            Dfs(root2, list2);
+            return list2.SequenceEqual(list);
+        }
+        private void Dfs(TreeNode root1, List<int> list)
+        {
+            if (root1 == null)
+                return;
+            if (root1.left == null && root1.right == null)
+            {
+                list.Add(root1.val);
+            }
+            Dfs(root1.left, list);
+            Dfs(root1.right, list);
+
+        }
+
+       
+    }
+}
