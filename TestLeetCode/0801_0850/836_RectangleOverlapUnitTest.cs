58a53ae7925f9d70d67e7a5a15c8306b0a4ad8d5
hongtao_luo <luohongtao621@hotmail.com>
2024-04-19 12:17:31 +0800

    836

---
+++ TestLeetCode/0801_0850/836_RectangleOverlapUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _836_RectangleOverlapUnitTest
+    {
+        _836_RectangleOverlapAlg alg = new _836_RectangleOverlapAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] rec1 = [0, 0, 2, 2], rec2 = [1, 1, 3, 3];
+            bool exp = true;
+            bool res = alg.IsRectangleOverlap(rec1, rec2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] rec1 = [0, 0, 1, 1], rec2 = [1, 0, 2, 1];
+            bool exp = false;
+            bool res = alg.IsRectangleOverlap(rec1, rec2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] rec1 = [0, 0, 1, 1], rec2 = [2, 2, 3, 3];
+            bool exp = false;
+            bool res = alg.IsRectangleOverlap(rec1, rec2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/836_RectangleOverlapAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _836_RectangleOverlapAlg
+    {
+        public bool IsRectangleOverlap(int[] rec1, int[] rec2)
+        {
+            int x1 = rec1[0];
+            int y1 = rec1[1];
+            int x2 = rec1[2];
+            int y2 = rec1[3];
+
+            int a1 = rec2[0];
+            int b1 = rec2[1];
+            int a2 = rec2[2];
+            int b2 = rec2[3];
+            if (x1 >= a2) 
+            {
+                return false;
+            }
+            if (x2 <= a1)
+            {
+                return false;
+            }
+            if (y1 >= b2)
+            {
+                return false;
+            }
+            if (y2 <= b1)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
