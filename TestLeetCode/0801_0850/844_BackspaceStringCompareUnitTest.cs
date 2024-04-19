a55e7db52ae4d604fb2a960149e3a146be9fc749
hongtao_luo <luohongtao621@hotmail.com>
2024-04-19 12:33:23 +0800

    844

---
+++ TestLeetCode/0801_0850/844_BackspaceStringCompareUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _844_BackspaceStringCompareUnitTest
+    {
+        _844_BackspaceStringCompareAlg alg = new _844_BackspaceStringCompareAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "ab#c", t = "ad#c";
+            bool exp = true;
+            bool res = alg.BackspaceCompare(s, t);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "ab##", t = "c#d#";
+            bool exp = true;
+            bool res = alg.BackspaceCompare(s, t);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "a#c", t = "b";
+            bool exp = false;
+            bool res = alg.BackspaceCompare(s, t);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "xywrrmp", t = "xywrrmu#p";
+            bool exp = true;
+            bool res = alg.BackspaceCompare(s, t);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/844_BackspaceStringCompareAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _844_BackspaceStringCompareAlg
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
+            string s1 = GetStr(s);
+            string t1 = GetStr(t);
+
+            return s1 == t1;
+        }
+        private string GetStr(string s)
+        {
+            List<char> st = new List<char>();
+            foreach (var item in s)
+            {
+                if (item == '#')
+                {
+                    if (st.Count > 0)
+                    {
+                        st.RemoveAt(st.Count - 1);
+                    }
+                }
+                else
+                {
+                    st.Add(item);
+                }
+            }
+
+            return new string(st.ToArray());
+        }
+    }
+}
