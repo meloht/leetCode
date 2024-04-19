e879ef93ffbd57061c2e965f7a0d7ba16c5202c4
hongtao_luo <luohongtao621@hotmail.com>
2024-04-19 13:30:49 +0800

    886

---
+++ TestLeetCode/0851_0900/868_BinaryGapUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _868_BinaryGapUnitTest
+    {
+        _868_BinaryGapAlg alg = new _868_BinaryGapAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 22;
+            int exp = 2;
+            int res = alg.BinaryGap(n);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 8;
+            int exp = 0;
+            int res = alg.BinaryGap(n);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 5;
+            int exp = 2;
+            int res = alg.BinaryGap(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/868_BinaryGapAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _868_BinaryGapAlg
+    {
+        public int BinaryGap(int n)
+        {
+            string s = Convert.ToString(n, 2);
+            int ans = 0;
+            int i = 0;
+            int j = 0;
+            while (i < s.Length)
+            {
+                if (s[i] == '1')
+                {
+                    j = i + 1;
+                    while (j < s.Length)
+                    {
+                        if (s[j] == '1')
+                        {
+                            ans = Math.Max(ans, j - i);
+                            i = j;
+                            break;
+                        }
+                        j++;
+                    }
+                    i = j;
+                    continue;
+                }
+                i++;
+            }
+
+            return ans;
+        }
+    }
+}
