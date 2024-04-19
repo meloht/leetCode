e8476b072d38e16e4de169b00d36d331417bf3e9
hongtao_luo <luohongtao621@hotmail.com>
2024-04-19 13:18:43 +0800

    859

---
+++ TestLeetCode/0851_0900/859_BuddyStringsUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._0851_0900;
+using leetCode.WeeklyContest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _859_BuddyStringsUnitTest
+    {
+        _859_BuddyStringsAlg alg = new _859_BuddyStringsAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "ab", goal = "ba";
+            bool exp = true;
+            bool res = alg.BuddyStrings(s, goal);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "ab", goal = "ab";
+            bool exp = false;
+            bool res = alg.BuddyStrings(s, goal);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "aa", goal = "aa";
+            bool exp = true;
+            bool res = alg.BuddyStrings(s, goal);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "abcd", goal = "badc";
+            bool exp = false;
+            bool res = alg.BuddyStrings(s, goal);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/859_BuddyStringsAlg.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _859_BuddyStringsAlg
+    {
+        public bool BuddyStrings(string s, string goal)
+        {
+            if (s.Length != goal.Length)
+                return false;
+            int[] map = GetMap(s);
+            int[] map2 = GetMap(goal);
+
+            if (map.SequenceEqual(map2))
+            {
+                int n1 = map.Count(p => p > 0);
+                int n2 = map2.Count(p => p > 0);
+                if (n1 == n2 && n1 == goal.Length && s == goal)
+                {
+                    return false;
+                }
+                int num = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] != goal[i])
+                    {
+                        num++;
+                    }
+                }
+                if (num > 2)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+        private int[] GetMap(string s)
+        {
+            int[] map = new int[26];
+            foreach (var item in s)
+            {
+                int index = item - 'a';
+                map[index]++;
+            }
+            return map;
+        }
+    }
+}
