917efce141183763154423f4ed5e69000339d777
hongtao luo <luohongtao621@163.com>
2024-04-20 18:16:34 +0800

    905

---
+++ TestLeetCode/0851_0900/905_SortArrayByParityUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _905_SortArrayByParityUnitTest
+    {
+        _905_SortArrayByParityAlg alg = new _905_SortArrayByParityAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 1, 2, 4];
+            int[] exp = [2, 4, 3, 1];
+            int[] res = alg.SortArrayByParity(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0];
+            int[] exp = [0];
+            int[] res = alg.SortArrayByParity(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0851_0900/905_SortArrayByParityAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _905_SortArrayByParityAlg
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            List<int> result1 = new List<int>();
+            List<int> result2 = new List<int>();
+            foreach (var item in nums)
+            {
+                if (item % 2 == 0)
+                {
+                    result1.Add(item);
+                }
+                else
+                {
+                    result2.Add(item);
+                }
+            }
+            result1.AddRange(result2);
+            return result1.ToArray();
+        }
+    }
+}
