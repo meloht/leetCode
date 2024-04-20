9c22dcd29c92ac0653dccd488fad85a1bed9a302
hongtao luo <luohongtao621@163.com>
2024-04-20 18:00:27 +0800

    892

---
+++ TestLeetCode/0851_0900/892_SurfaceAreaOf3dShapesUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _892_SurfaceAreaOf3dShapesUnitTest
+    {
+        _892_SurfaceAreaOf3dShapesAlg alg = new _892_SurfaceAreaOf3dShapesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[1, 2], [3, 4]];
+            int exp = 34;
+            int res = alg.SurfaceArea(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[1, 1, 1], [1, 0, 1], [1, 1, 1]];
+            int exp = 32;
+            int res = alg.SurfaceArea(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] grid = [[2, 2, 2], [2, 1, 2], [2, 2, 2]];
+            int exp = 46;
+            int res = alg.SurfaceArea(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/892_SurfaceAreaOf3dShapesAlg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _892_SurfaceAreaOf3dShapesAlg
+    {
+        public int SurfaceArea(int[][] grid)
+        {
+            int m = grid.Length;
+            int n = grid[0].Length;
+            int ans = m * n * 2;
+
+            int[][] dir = [[-1, 0], [1, 0], [0, 1], [0, -1]];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] > 0)
+                    {
+                        foreach (int[] item in dir)
+                        {
+                            int x = item[0] + i;
+                            int y = item[1] + j;
+                            if (InRange(m, n, x, y))
+                            {
+
+                                if (grid[x][y] < grid[i][j])
+                                {
+                                    ans += (grid[i][j] - grid[x][y]);
+                                }
+                            }
+                            else
+                            {
+                                ans += grid[i][j];
+                            }
+                        }
+                    }
+                    else
+                    {
+                        ans -= 2;
+                    }
+                }
+            }
+            return ans;
+        }
+        private bool InRange(int m, int n, int x, int y)
+        {
+            if (x >= 0 && x < m && y >= 0 && y < n)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
