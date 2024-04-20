e0e9767e29589c20390b74b4b3d1d260d055f47c
hongtao luo <luohongtao621@163.com>
2024-04-20 16:22:12 +0800

    883

---
+++ TestLeetCode/0851_0900/883_ProjectionAreaOf3dShapesUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _883_ProjectionAreaOf3dShapesUnitTest
+    {
+        _883_ProjectionAreaOf3dShapesAlg alg = new _883_ProjectionAreaOf3dShapesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[1, 2], [3, 4]];
+            int exp = 17;
+            int res = alg.ProjectionArea(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[2]];
+            int exp = 5;
+            int res = alg.ProjectionArea(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] grid = [[1, 0], [0, 2]];
+            int exp = 8;
+            int res = alg.ProjectionArea(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/883_ProjectionAreaOf3dShapesAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _883_ProjectionAreaOf3dShapesAlg
+    {
+        public int ProjectionArea(int[][] grid)
+        {
+            int m = grid.Length;
+            int n = grid[0].Length;
+            int xy0 = 0;
+            int yz = 0;
+            int xz = 0;
+            for (int i = 0; i < m; i++)
+            {
+                int max = 0;
+                int max1 = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] == 0)
+                    {
+                        xy0++;
+                    }
+                    else
+                    {
+                        max = Math.Max(max, grid[i][j]);
+                    }
+                    max1 = Math.Max(max1, grid[j][i]);
+                }
+                yz += max;
+                xz += max1;
+
+            }
+
+            int ans = m * n - xy0 + yz + xz;
+
+            return ans;
+        }
+    }
+}
