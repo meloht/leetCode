472a135318403904c4213de76c96221cf1db1385
hongtao luo <luohongtao621@163.com>
2024-04-21 15:47:08 +0800

    929

---
+++ TestLeetCode/0901_0950/929_UniqueEmailAddressesUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0901_0950
+{
+    [TestClass]
+    public class _929_UniqueEmailAddressesUnitTest
+    {
+        _929_UniqueEmailAddressesAlg alg = new _929_UniqueEmailAddressesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] emails = ["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"];
+            int exp = 2;
+            int res = alg.NumUniqueEmails(emails);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] emails = ["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"];
+            int exp = 3;
+            int res = alg.NumUniqueEmails(emails);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] emails = ["fg.r.u.uzj+o.pw@kziczvh.com", "r.cyo.g+d.h+b.ja@tgsg.z.com", "fg.r.u.uzj+o.f.d@kziczvh.com", "r.cyo.g+ng.r.iq@tgsg.z.com", "fg.r.u.uzj+lp.k@kziczvh.com", "r.cyo.g+n.h.e+n.g@tgsg.z.com", "fg.r.u.uzj+k+p.j@kziczvh.com", "fg.r.u.uzj+w.y+b@kziczvh.com", "r.cyo.g+x+d.c+f.t@tgsg.z.com", "r.cyo.g+x+t.y.l.i@tgsg.z.com", "r.cyo.g+brxxi@tgsg.z.com", "r.cyo.g+z+dr.k.u@tgsg.z.com", "r.cyo.g+d+l.c.n+g@tgsg.z.com", "fg.r.u.uzj+vq.o@kziczvh.com", "fg.r.u.uzj+uzq@kziczvh.com", "fg.r.u.uzj+mvz@kziczvh.com", "fg.r.u.uzj+taj@kziczvh.com", "fg.r.u.uzj+fek@kziczvh.com"];
+            int exp = 2;
+            int res = alg.NumUniqueEmails(emails);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/929_UniqueEmailAddressesAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _929_UniqueEmailAddressesAlg
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            HashSet<string> addresses = new HashSet<string>();
+            foreach (var item in emails)
+            {
+                string mail = item.Substring(0, item.IndexOf('@') + 1);
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var ch in mail)
+                {
+                    if (char.IsLetter(ch))
+                    {
+                        sb.Append(ch);
+                    }
+                    else if (ch == '+')
+                    {
+                        break;
+                    }
+                   
+                }
+                sb.Append(item.Substring(item.IndexOf('@')));
+                addresses.Add(sb.ToString());
+            }
+            return addresses.Count;
+        }
+    }
+}
