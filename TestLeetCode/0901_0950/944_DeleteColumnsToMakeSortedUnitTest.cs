38d5236508b8792987159f5098f0b68370bf9a56
hongtao luo <luohongtao621@163.com>
2024-04-21 20:56:20 +0800

    944

---
+++ TestLeetCode/0901_0950/944_DeleteColumnsToMakeSortedUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0901_0950
+{
+    [TestClass]
+    public class _944_DeleteColumnsToMakeSortedUnitTest
+    {
+        _944_DeleteColumnsToMakeSortedAlg alg = new _944_DeleteColumnsToMakeSortedAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] strs = ["cba", "daf", "ghi"];
+            int exp = 1;
+            int res = alg.MinDeletionSize(strs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] strs = ["a", "b"];
+            int exp = 0;
+            int res = alg.MinDeletionSize(strs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] strs = ["zyx", "wvu", "tsr"];
+            int exp = 3;
+            int res = alg.MinDeletionSize(strs);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/944_DeleteColumnsToMakeSortedAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _944_DeleteColumnsToMakeSortedAlg
+    {
+        public int MinDeletionSize(string[] strs)
+        {
+            int m = strs.Length;
+            int n = strs[0].Length;
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 1; j < m; j++)
+                {
+                    char prev = strs[j - 1][i];
+                    char curr = strs[j][i];
+                    if (prev > curr)
+                    {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
