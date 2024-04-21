aa78b72a489709d18631e8c14d7c6ecd62bc1858
hongtao luo <luohongtao621@163.com>
2024-04-21 16:48:00 +0800

    941

---
+++ TestLeetCode/0901_0950/941_ValidMountainArrayUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0901_0950
+{
+    [TestClass]
+    public class _941_ValidMountainArrayUnitTest
+    {
+        _941_ValidMountainArrayAlg alg = new _941_ValidMountainArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [2, 1];
+            bool exp = false;
+            bool res = alg.ValidMountainArray(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [3, 5, 5];
+            bool exp = false;
+            bool res = alg.ValidMountainArray(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] arr = [0, 3, 2, 1];
+            bool exp = true;
+            bool res = alg.ValidMountainArray(arr);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/941_ValidMountainArrayAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+
+    public class _941_ValidMountainArrayAlg
+    {
+        public bool ValidMountainArray(int[] arr)
+        {
+            if (arr.Length < 3)
+                return false;
+
+            int mid = 0;
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    mid = i;
+                }
+            }
+            if (mid == 0 || mid == arr.Length-1)
+                return false;
+            for (int i = 1; i < mid; i++)
+            {
+                if (arr[i - 1] >= arr[i])
+                    return false;
+            }
+            for (int i = mid + 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1] <= arr[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
