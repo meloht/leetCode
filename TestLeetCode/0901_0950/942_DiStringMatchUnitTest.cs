9cc701ba8f6cc5785d12f6a11098f637145fa9d0
hongtao luo <luohongtao621@163.com>
2024-04-21 20:41:57 +0800

    942

---
+++ TestLeetCode/0901_0950/942_DiStringMatchUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0901_0950
+{
+    [TestClass]
+    public class _942_DiStringMatchUnitTest
+    {
+        _942_DiStringMatchAlg alg = new _942_DiStringMatchAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "IDID";
+            int[] exp = [0, 4, 1, 3, 2];
+            int[] res = alg.DiStringMatch(s);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "III";
+            int[] exp = [0, 1, 2, 3];
+            int[] res = alg.DiStringMatch(s);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "DDI";
+            int[] exp = [3, 2, 0, 1];
+            int[] res = alg.DiStringMatch(s);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0901_0950/942_DiStringMatchAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _942_DiStringMatchAlg
+    {
+        public int[] DiStringMatch(string s)
+        {
+            int[] ans = new int[s.Length + 1];
+            int left = 0;
+            int right = s.Length;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'I')
+                {
+                    ans[i] = left;
+                    left++;
+                    if (i == s.Length - 1)
+                    {
+                        ans[i+1] = left;
+                    }
+                }
+                else
+                {
+                    ans[i] = right;
+                    right--;
+                    if (i == s.Length - 1)
+                    {
+                        ans[i + 1] = right;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
