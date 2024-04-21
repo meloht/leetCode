f677221445cd9d53c68ddbea064d6c889fd29aad
hongtao luo <luohongtao621@163.com>
2024-04-21 21:22:43 +0800

    953

---
+++ TestLeetCode/0901_0950/953_VerifyingAnAlienDictionaryUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0901_0950
+{
+    [TestClass]
+    public class _953_VerifyingAnAlienDictionaryUnitTest
+    {
+        _953_VerifyingAnAlienDictionaryAlg alg = new _953_VerifyingAnAlienDictionaryAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["hello", "leetcode"];
+            string order = "hlabcdefgijkmnopqrstuvwxyz";
+            bool exp = true;
+            bool res = alg.IsAlienSorted(words, order);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["word", "world", "row"];
+            string order = "worldabcefghijkmnpqstuvxyz";
+            bool exp = false;
+            bool res = alg.IsAlienSorted(words, order);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] words = ["apple", "app"];
+            string order = "abcdefghijklmnopqrstuvwxyz";
+            bool exp = false;
+            bool res = alg.IsAlienSorted(words, order);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string[] words = ["kuvp", "q"];
+            string order = "ngxlkthsjuoqcpavbfdermiywz";
+            bool exp = true;
+            bool res = alg.IsAlienSorted(words, order);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/953_VerifyingAnAlienDictionaryAlg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _953_VerifyingAnAlienDictionaryAlg
+    {
+        public bool IsAlienSorted(string[] words, string order)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < order.Length; i++)
+            {
+                dict.Add(order[i], i);
+            }
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                int len = Math.Min(words[i - 1].Length, words[i].Length);
+                bool bl = false;
+                for (int j = 0; j < len; j++)
+                {
+                    int prev = dict[words[i - 1][j]];
+                    int curr = dict[words[i][j]];
+                    if (prev > curr)
+                    {
+                        return false;
+
+                    }
+                    else if (curr > prev)
+                    {
+                        bl = true;
+                        break;
+                    }
+
+
+                }
+                if (bl == false && words[i - 1].Length > words[i].Length)
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
