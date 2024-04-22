0b0a453e24eeeff9986f7693c11f71efe40ce6ed
hongtao luo <luohongtao621@163.com>
2024-04-22 20:19:16 +0800

    997

---
+++ TestLeetCode/0951_1000/0997_FindTheTownJudgeUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0951_1000
+{
+    [TestClass]
+    public class _0997_FindTheTownJudgeUnitTest
+    {
+        _0997_FindTheTownJudgeAlg alg = new _0997_FindTheTownJudgeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 2;
+            int[][] trust = [[1, 2]];
+            int exp = 2;
+            int res = alg.FindJudge(n, trust);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 3;
+            int[][] trust = [[1, 3], [2, 3]];
+            int exp = 3;
+            int res = alg.FindJudge(n, trust);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 3;
+            int[][] trust = [[1, 3], [2, 3], [3, 1]];
+            int exp = -1;
+            int res = alg.FindJudge(n, trust);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int n = 4;
+            int[][] trust = [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]];
+            int exp = 3;
+            int res = alg.FindJudge(n, trust);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0997_FindTheTownJudgeAlg.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0997_FindTheTownJudgeAlg
+    {
+        public int FindJudge(int n, int[][] trust)
+        {
+
+            Dictionary<int, HashSet<int>> dict = new Dictionary<int, HashSet<int>>();
+            foreach (var item in trust)
+            {
+                if (dict.ContainsKey(item[0]))
+                {
+                    dict[item[0]].Add(item[1]);
+                }
+                else
+                {
+                    HashSet<int> sett = new HashSet<int>();
+                    sett.Add(item[1]);
+                    dict.Add(item[0], sett);
+                }
+            }
+
+            if (dict.Count != n - 1)
+                return -1;
+            int ans = -1;
+            for (int i = 1; i <= n; i++)
+            {
+                if (!dict.ContainsKey(i))
+                {
+                    ans = i;
+                    break;
+                }
+            }
+            if (ans == -1)
+                return -1;
+            foreach (var item in dict)
+            {
+                if (!item.Value.Contains(ans))
+                    return -1;
+            }
+            return ans;
+        }
+
+        public int FindJudge1(int n, int[][] trust)
+        {
+            int[] inDegrees = new int[n + 1];
+            int[] outDegrees = new int[n + 1];
+            foreach (int[] edge in trust)
+            {
+                int x = edge[0], y = edge[1];
+                ++inDegrees[y];
+                ++outDegrees[x];
+            }
+            for (int i = 1; i <= n; ++i)
+            {
+                if (inDegrees[i] == n - 1 && outDegrees[i] == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
