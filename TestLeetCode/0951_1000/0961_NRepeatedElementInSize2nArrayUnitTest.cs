a3f5231feba6667dc9d377a22d37d83e11e14619
hongtao_luo <luohongtao621@hotmail.com>
2024-04-22 12:37:47 +0800

    961

---
+++ TestLeetCode/0951_1000/0961_NRepeatedElementInSize2nArrayUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0951_1000
+{
+    [TestClass]
+    public class _0961_NRepeatedElementInSize2nArrayUnitTest
+    {
+        _0961_NRepeatedElementInSize2nArrayAlg alg = new _0961_NRepeatedElementInSize2nArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 3];
+            int exp = 3;
+            int res = alg.RepeatedNTimes(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 1, 2, 5, 3, 2];
+            int exp = 2;
+            int res = alg.RepeatedNTimes(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [5, 1, 5, 2, 5, 3, 5, 4];
+            int exp = 5;
+            int res = alg.RepeatedNTimes(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0961_NRepeatedElementInSize2nArrayAlg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0961_NRepeatedElementInSize2nArrayAlg
+    {
+        public int RepeatedNTimes(int[] nums)
+        {
+            HashSet<int> result = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                if(result.Add(item)==false)
+                    return item;
+
+            }
+            return 0;
+        }
+    }
+}
