224db0714de4223b17339b36ffe3d98e947b2bc4
hongtao luo <luohongtao621@163.com>
2024-04-22 20:52:29 +0800

    1002

---
+++ TestLeetCode/1001_1050/1002_FindCommonCharactersUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1002_FindCommonCharactersUnitTest
+    {
+        _1002_FindCommonCharactersAlg alg = new _1002_FindCommonCharactersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["bella", "label", "roller"];
+            string[] exp = ["e", "l", "l"];
+            string[] res = alg.CommonChars(words).ToArray();
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["cool", "lock", "cook"];
+            string[] exp = ["c", "o"];
+            string[] res = alg.CommonChars(words).ToArray();
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+    }
+}
---
+++ leetCode/1001_1050/1002_FindCommonCharactersAlg.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1002_FindCommonCharactersAlg
+    {
+        public IList<string> CommonChars(string[] words)
+        {
+            if (words.Length == 1)
+                return words;
+            Dictionary<char, int> dict = GetDict(words[0]);
+
+            for (int i = 1; i < words.Length; i++)
+            {
+
+                Dictionary<char, int> set = new Dictionary<char, int>();
+                string s = words[i];
+                foreach (var item in s)
+                {
+                    if (dict.ContainsKey(item) && dict[item] > 0)
+                    {
+                        if (set.ContainsKey(item))
+                        {
+                            set[item]++;
+                        }
+                        else
+                        {
+                            set.Add(item, 1);
+                        }
+                        dict[item]--;
+                    }
+                }
+
+                dict = set;
+
+            }
+            List<string> ans = new List<string>();
+            foreach (var item in dict)
+            {
+                for (int i = 0; i < item.Value; i++)
+                {
+                    ans.Add(item.Key.ToString());
+                }
+            }
+            return ans;
+        }
+        private Dictionary<char, int> GetDict(string s)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            foreach (var item in s)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+            return dict;
+
+        }
+    }
+}
