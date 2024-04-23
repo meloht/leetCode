7ea0cdba4d04362ba8b50532f554920a19e5800e
hongtao luo <luohongtao621@163.com>
2024-04-23 20:55:29 +0800

    1018

---
+++ TestLeetCode/1001_1050/1018_BinaryPrefixDivisibleBy5UnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1018_BinaryPrefixDivisibleBy5UnitTest
+    {
+        _1018_BinaryPrefixDivisibleBy5Alg alg = new _1018_BinaryPrefixDivisibleBy5Alg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [0, 1, 1];
+            bool[] exp = [true, false, false];
+            bool[] res = alg.PrefixesDivBy5(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1, 1];
+            bool[] exp = [false, false, false];
+            bool[] res = alg.PrefixesDivBy5(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 1, 0, 0, 0, 1, 0, 0, 1];
+            bool[] exp = [false, false, false, false, false, false, false, false, false];
+            bool[] res = alg.PrefixesDivBy5(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/1001_1050/1018_BinaryPrefixDivisibleBy5Alg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1018_BinaryPrefixDivisibleBy5Alg
+    {
+        public IList<bool> PrefixesDivBy5(int[] nums)
+        {
+            List<bool> result = new List<bool>();
+            int prefix = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                prefix = ((prefix << 1) + nums[i]) % 5;
+                result.Add(prefix == 0);
+            }
+
+            return result;
+        }
+    }
+}
