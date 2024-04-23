3a0f21a4e65c4604a0ab1831b682b8c4791d9aef
hongtao_luo <luohongtao621@hotmail.com>
2024-04-23 13:16:41 +0800

    999

---
+++ TestLeetCode/0951_1000/0999_AvailableCapturesForRookUnitTest.cs
@@ -0,0 +1,72 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0951_1000
+{
+    [TestClass]
+    public class _0999_AvailableCapturesForRookUnitTest
+    {
+        _0999_AvailableCapturesForRookAlg alg = new _0999_AvailableCapturesForRookAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[][] board = [
+            ['.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', 'p', '.', '.', '.', '.'],
+            ['.', '.', '.', 'R', '.', '.', '.', 'p'],
+            ['.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', 'p', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.']];
+
+            int exp = 3;
+            int res = alg.NumRookCaptures(board);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[][] board = [
+            ['.', '.', '.', '.', '.', '.', '.', '.'], 
+            ['.', 'p', 'p', 'p', 'p', 'p', '.', '.'], 
+            ['.', 'p', 'p', 'B', 'p', 'p', '.', '.'], 
+            ['.', 'p', 'B', 'R', 'B', 'p', '.', '.'], 
+            ['.', 'p', 'p', 'B', 'p', 'p', '.', '.'], 
+            ['.', 'p', 'p', 'p', 'p', 'p', '.', '.'], 
+            ['.', '.', '.', '.', '.', '.', '.', '.'], 
+            ['.', '.', '.', '.', '.', '.', '.', '.']];
+
+            int exp = 0;
+            int res = alg.NumRookCaptures(board);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            char[][] board = [
+            ['.', '.', '.', '.', '.', '.', '.', '.'], 
+            ['.', '.', '.', 'p', '.', '.', '.', '.'], 
+            ['.', '.', '.', 'p', '.', '.', '.', '.'], 
+            ['p', 'p', '.', 'R', '.', 'p', 'B', '.'], 
+            ['.', '.', '.', '.', '.', '.', '.', '.'], 
+            ['.', '.', '.', 'B', '.', '.', '.', '.'], 
+            ['.', '.', '.', 'p', '.', '.', '.', '.'], 
+            ['.', '.', '.', '.', '.', '.', '.', '.']];
+
+            int exp = 3;
+            int res = alg.NumRookCaptures(board);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/0951_1000/0999_AvailableCapturesForRookAlg.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0999_AvailableCapturesForRookAlg
+    {
+        public int NumRookCaptures(char[][] board)
+        {
+            int m = board.Length;
+            int n = board[0].Length;
+            int ans = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (board[i][j] != 'R')
+                    {
+                        continue;
+                    }
+
+
+                    ans += GetColNum(i + 1, m, j, board);
+                    ans += GetColNum1(0, i, j, board);
+                    ans += GetRowNum1(0, j, i, board);
+                    ans += GetRowNum(j + 1, n, i, board);
+                    return ans;
+
+                }
+            }
+
+            return 0;
+        }
+        private int GetColNum(int begin, int end, int j, char[][] board)
+        {
+            for (int k = begin; k < end; k++)
+            {
+                if (board[k][j] == 'B')
+                {
+                    break;
+                }
+                if (board[k][j] == 'p')
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        private int GetColNum1(int begin, int end, int j, char[][] board)
+        {
+            for (int k = end - 1; k >= begin; k--)
+            {
+                if (board[k][j] == 'B')
+                {
+                    break;
+                }
+                if (board[k][j] == 'p')
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        private int GetRowNum(int begin, int end, int i, char[][] board)
+        {
+            for (int k = begin; k < end; k++)
+            {
+                if (board[i][k] == 'B')
+                {
+                    break;
+                }
+                if (board[i][k] == 'p')
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        private int GetRowNum1(int begin, int end, int i, char[][] board)
+        {
+            for (int k = end - 1; k >= begin; k--)
+            {
+                if (board[i][k] == 'B')
+                {
+                    break;
+                }
+                if (board[i][k] == 'p')
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
