2cf6271a324ec19f69def1ec63dacdba3ea65ab7
hongtao_luo <luohongtao621@hotmail.com>
2024-04-23 13:52:01 +0800

    1005

---
+++ TestLeetCode/1001_1050/1005_MaximizeSumOfArrayAfterKNegationsUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1005_MaximizeSumOfArrayAfterKNegationsUnitTest
+    {
+        _1005_MaximizeSumOfArrayAfterKNegationsAlg alg = new _1005_MaximizeSumOfArrayAfterKNegationsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 2, 3];
+            int k = 1;
+            int exp = 5;
+            int res = alg.LargestSumAfterKNegations(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [3, -1, 0, 2];
+            int k = 3;
+            int exp = 6;
+            int res = alg.LargestSumAfterKNegations(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [2, -3, -1, 5, -4];
+            int k = 2;
+            int exp = 13;
+            int res = alg.LargestSumAfterKNegations(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [-8, 3, -5, -3, -5, -2];
+            int k = 6;
+            int exp = 22;
+            int res = alg.LargestSumAfterKNegations(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1005_MaximizeSumOfArrayAfterKNegationsAlg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1005_MaximizeSumOfArrayAfterKNegationsAlg
+    {
+        public int LargestSumAfterKNegations(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int indexZero = -1;
+            int indexMin = -1;
+            int min = int.MaxValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0 && indexZero == -1)
+                {
+                    indexZero = i;
+                }
+                if (Math.Abs(nums[i]) <= min)
+                {
+                    indexMin = i;
+                    min = Math.Abs(nums[i]);
+                }
+                if (nums[i] < 0 && k > 0)
+                {
+                    nums[i] = -nums[i];
+                    k--;
+                }
+
+            }
+            if (k > 0)
+            {
+                if (indexZero != -1)
+                {
+                    return nums.Sum();
+                }
+                else
+                {
+                    k = k % 2;
+                    if (k == 1)
+                    {
+                        nums[indexMin] = -nums[indexMin];
+                    }
+
+                    return nums.Sum();
+                }
+            }
+            else
+            {
+                return nums.Sum();
+            }
+        }
+    }
+}
