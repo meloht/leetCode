f3cf4d0d7b5f59cc53d8b1d6380eef1503292c50
hongtao luo <luohongtao621@163.com>
2024-04-23 20:11:35 +0800

    1013

---
+++ TestLeetCode/1001_1050/1013_PartitionArrayIntoThreePartsWithEqualSumUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1013_PartitionArrayIntoThreePartsWithEqualSumUnitTest
+    {
+        _1013_PartitionArrayIntoThreePartsWithEqualSumAlg alg = new _1013_PartitionArrayIntoThreePartsWithEqualSumAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1];
+            bool exp = true;
+            bool res=alg.CanThreePartsEqualSum(arr);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1];
+            bool exp = false;
+            bool res = alg.CanThreePartsEqualSum(arr);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] arr = [3, 3, 6, 5, -2, 2, 5, 1, -9, 4];
+            bool exp = true;
+            bool res = alg.CanThreePartsEqualSum(arr);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/1001_1050/1013_PartitionArrayIntoThreePartsWithEqualSumAlg.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1013_PartitionArrayIntoThreePartsWithEqualSumAlg
+    {
+        public bool CanThreePartsEqualSum(int[] arr)
+        {
+            int sum = arr.Sum();
+            if (sum % 3 != 0)
+                return false;
+            int avg = sum / 3;
+
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                num1 += arr[i];
+                if (num1 != avg)
+                {
+                    continue;
+                }
+                for (int j = i + 1; j < arr.Length - 1; j++)
+                {
+                    num2 += arr[j];
+                    if (num2 != avg)
+                    {
+                        continue;
+                    }
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+                        num3 += arr[k];
+
+                        if (k == arr.Length - 1)
+                        {
+                            if (num1 == num2 && num2 == num3)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
