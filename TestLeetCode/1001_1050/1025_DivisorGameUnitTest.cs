0e07c6633093bec73555c1fced83ec62e6bd88d7
hongtao_luo <luohongtao621@hotmail.com>
2024-04-24 13:19:50 +0800

    1025

---
+++ TestLeetCode/1001_1050/1025_DivisorGameUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1025_DivisorGameUnitTest
+    {
+        _1025_DivisorGameAlg alg = new _1025_DivisorGameAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 2;
+            bool exp = true;
+            bool res = alg.DivisorGame(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 3;
+            bool exp = false;
+            bool res = alg.DivisorGame(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1025_DivisorGameAlg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1025_DivisorGameAlg
+    {
+        public bool DivisorGame(int n)
+        {
+            if (n % 2 == 1)
+                return false;
+
+            return true;
+        }
+    }
+}
