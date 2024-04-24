d53c2677f63db5b3de898e2d68240c7918147198
hongtao_luo <luohongtao621@hotmail.com>
2024-04-24 13:05:08 +0800

    1022

---
+++ TestLeetCode/1001_1050/1022_SumOfRootToLeafBinaryNumbersUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._1001_1050;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1022_SumOfRootToLeafBinaryNumbersUnitTest
+    {
+        _1022_SumOfRootToLeafBinaryNumbersAlg alg = new _1022_SumOfRootToLeafBinaryNumbersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 0, 1, 0, 1, 0, 1];
+            int exp = 22;
+            var tree = TreeNode.BuildTree(root);
+            int res = alg.SumRootToLeaf(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [0];
+            int exp = 0;
+            var tree = TreeNode.BuildTree(root);
+            int res = alg.SumRootToLeaf(tree);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1022_SumOfRootToLeafBinaryNumbersAlg.cs
@@ -0,0 +1,38 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+
+    public class _1022_SumOfRootToLeafBinaryNumbersAlg
+    {
+        int ans = 0;
+        public int SumRootToLeaf(TreeNode root)
+        {
+            Dfs(root, 0);
+            return ans;
+        }
+
+        private void Dfs(TreeNode node, int num)
+        {
+            if (node == null)
+                return ;
+      
+            if (num > 0)
+            {
+                num = num << 1;
+            }
+            num += node.val;
+            if (node.left == null && node.right == null)
+            {
+                ans += num;
+            }
+            Dfs(node.left, num);
+            Dfs(node.right, num);
+        }
+    }
+}
