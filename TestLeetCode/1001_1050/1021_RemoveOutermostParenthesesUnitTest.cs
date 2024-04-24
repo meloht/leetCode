47ddb0818550e7a8565de9eed25c3fe382bd314c
hongtao_luo <luohongtao621@hotmail.com>
2024-04-24 12:27:21 +0800

    1021

---
+++ TestLeetCode/1001_1050/1021_RemoveOutermostParenthesesUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1021_RemoveOutermostParenthesesUnitTest
+    {
+        _1021_RemoveOutermostParenthesesAlg alg = new _1021_RemoveOutermostParenthesesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "(()())(())";
+            string exp = "()()()";
+            string res = alg.RemoveOuterParentheses(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "(()())(())(()(()))";
+            string exp = "()()()()(())";
+            string res = alg.RemoveOuterParentheses(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "()()";
+            string exp = "";
+            string res = alg.RemoveOuterParentheses(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1021_RemoveOutermostParenthesesAlg.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1021_RemoveOutermostParenthesesAlg
+    {
+        public string RemoveOuterParentheses(string s)
+        {
+            StringBuilder ans = new StringBuilder();
+            Stack<char> stack = new Stack<char>();
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in s)
+            {
+                sb.Append(item);
+                if (item == ')')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    stack.Push(item);
+                }
+                if (stack.Count == 0)
+                {
+                    string ss = sb.ToString();
+                    if (ss.Length > 0)
+                    {
+                        ans.Append(ss.Substring(1, ss.Length - 2));
+                    }
+
+                    sb.Clear();
+                }
+            }
+
+
+            return ans.ToString();
+        }
+
+        public string RemoveOuterParentheses2(string s)
+        {
+            StringBuilder res = new StringBuilder();
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == ')')
+                {
+                    stack.Pop();
+                }
+                if (stack.Count > 0)
+                {
+                    res.Append(c);
+                }
+                if (c == '(')
+                {
+                    stack.Push(c);
+                }
+            }
+            return res.ToString();
+        }
+
+        public string RemoveOuterParentheses3(string s)
+        {
+            int level = 0;
+            StringBuilder res = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (c == ')')
+                {
+                    level--;
+                }
+                if (level > 0)
+                {
+                    res.Append(c);
+                }
+                if (c == '(')
+                {
+                    level++;
+                }
+            }
+            return res.ToString();
+        }
+
+
+    }
+}
