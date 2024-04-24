96660d1dc6ae4c3e388e40709fbfd6cb1d24befa
hongtao_luo <luohongtao621@hotmail.com>
2024-04-24 13:38:55 +0800

    1030

---
+++ TestLeetCode/1001_1050/1030_MatrixCellsInDistanceOrderUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1030_MatrixCellsInDistanceOrderUnitTest
+    {
+        _1030_MatrixCellsInDistanceOrderAlg alg = new _1030_MatrixCellsInDistanceOrderAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int rows = 1, cols = 2, rCenter = 0, cCenter = 0;
+            int[][] exp = [[0, 0], [0, 1]];
+            int[][] res = alg.AllCellsDistOrder(rows, cols, rCenter, cCenter);
+            bool bl = Utils.IsSame(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int rows = 2, cols = 2, rCenter = 0, cCenter = 1;
+            int[][] exp = [[0, 1], [0, 0], [1, 1], [1, 0]];
+            int[][] res = alg.AllCellsDistOrder(rows, cols, rCenter, cCenter);
+            bool bl = Utils.IsSame(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int rows = 2, cols = 3, rCenter = 1, cCenter = 2;
+            int[][] exp = [[1, 2], [0, 2], [1, 1], [0, 1], [1, 0], [0, 0]];
+            int[][] res = alg.AllCellsDistOrder(rows, cols, rCenter, cCenter);
+            bool bl = Utils.IsSame(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1030_MatrixCellsInDistanceOrderAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1030_MatrixCellsInDistanceOrderAlg
+    {
+        public int[][] AllCellsDistOrder(int rows, int cols, int rCenter, int cCenter)
+        {
+            List<int[]> list = new List<int[]>();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    list.Add([Math.Abs(i - rCenter) + Math.Abs(j - cCenter), i, j]);
+                }
+            }
+
+            list.Sort((x, y) => x[0].CompareTo(y[0]));
+
+            int[][] res = new int[list.Count][];
+            for (int i = 0; i < list.Count; i++)
+            {
+                res[i] = [list[i][1], list[i][2]];
+            }
+
+            return res;
+        }
+    }
+}
