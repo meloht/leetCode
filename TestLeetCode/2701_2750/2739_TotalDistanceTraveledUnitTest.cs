c1997766071a4c9a6fae3c9f8cbbefa5f6342e18
hongtao luo <luohongtao621@163.com>
2024-04-25 19:26:20 +0800

    2739

---
+++ TestLeetCode/2701_2750/2739_TotalDistanceTraveledUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2739_TotalDistanceTraveledUnitTest
+    {
+        _2739_TotalDistanceTraveledAlg alg = new _2739_TotalDistanceTraveledAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int mainTank = 5, additionalTank = 10;
+            int exp = 60;
+            int res = alg.DistanceTraveled(mainTank, additionalTank);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int mainTank = 1, additionalTank = 2;
+            int exp = 10;
+            int res = alg.DistanceTraveled(mainTank, additionalTank);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2701_2750/2739_TotalDistanceTraveledAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2739_TotalDistanceTraveledAlg
+    {
+        public int DistanceTraveled(int mainTank, int additionalTank)
+        {
+            int ans = 0;
+            int i = mainTank;
+           
+            while (i >= 5) 
+            {
+                i -= 5;
+                ans += 50;
+                if (additionalTank > 0)
+                {
+                    i++;
+                    additionalTank--;
+                }
+            }
+          
+            if (i > 0)
+            {
+                ans += i * 10;
+            }
+            return ans;
+        }
+    }
+}
