d3508d9970de7d0f2c31e3abc732499e1e6e092f
hongtao_luo <luohongtao621@hotmail.com>
2024-04-25 13:54:33 +0800

    1037

---
+++ TestLeetCode/1001_1050/1037_ValidBoomerangUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1037_ValidBoomerangUnitTest
+    {
+        _1037_ValidBoomerangAlg alg = new _1037_ValidBoomerangAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] points = [[1, 1], [2, 3], [3, 2]];
+            bool exp = true;
+            bool res = alg.IsBoomerang(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] points = [[1, 1], [2, 2], [3, 3]];
+            bool exp = false;
+            bool res = alg.IsBoomerang(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] points = [[0, 0], [1, 0], [2, 2]];
+            bool exp = true;
+            bool res = alg.IsBoomerang(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] points = [[1, 1], [2, 2], [7, 7]];
+            bool exp = false;
+            bool res = alg.IsBoomerang(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[][] points = [[0, 1], [2, 1], [0, 0]];
+            bool exp = true;
+            bool res = alg.IsBoomerang(points);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1037_ValidBoomerangAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1037_ValidBoomerangAlg
+    {
+        public bool IsBoomerang(int[][] points)
+        {
+            HashSet<string> set = new HashSet<string>();
+            foreach (int[] point in points)
+            {
+                set.Add($"{point[0]}-{point[1]}");
+            }
+            if (set.Count < 3)
+                return false;
+            var a = GetXY(points[0][0], points[0][1], points[1][0], points[1][1]);
+            var b = GetXY(points[1][0], points[1][1], points[2][0], points[2][1]);
+
+            if (a == b)
+                return false;
+
+
+
+            return true;
+        }
+
+        private double GetXY(int a, int b, int x, int y)
+        {
+            double aa = x - a;
+            double bb = y - b;
+
+            if (aa == 0)
+                return double.MinValue;
+
+            return bb / aa;
+        }
+        /// <summary>
+        /// 计算从 points[0]\textit{points}[0]points[0] 开始，分别指向 points[1]\textit{points}[1]points[1] 和 points[2]\textit{points}[2]points[2] 的向量 v⃗1\vec{v}_1 v 1 ​ 和 v⃗2\vec{v}_2 v 2 ​ 。
+        /// 「三点各不相同且不在一条直线上」等价于「这两个向量的叉乘结果不为0
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        public bool IsBoomerang1(int[][] points)
+        {
+            int[] v1 = { points[1][0] - points[0][0], points[1][1] - points[0][1] };
+            int[] v2 = { points[2][0] - points[0][0], points[2][1] - points[0][1] };
+            return v1[0] * v2[1] - v1[1] * v2[0] != 0;
+        }
+
+
+    }
+}
