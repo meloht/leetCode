de79fdec3ffd3c7fcee3cb4f06275906ddeb8ca7
hongtao luo <luohongtao621@163.com>
2024-04-25 20:08:24 +0800

    1051

---
+++ TestLeetCode/1051_1100/1051_HeightCheckerUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1051_1100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1051_1100
+{
+    [TestClass]
+    public class _1051_HeightCheckerUnitTest
+    {
+        _1051_HeightCheckerAlg alg = new _1051_HeightCheckerAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] heights = [1, 1, 4, 2, 1, 3];
+            int exp = 3;
+            int res = alg.HeightChecker(heights);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] heights = [5, 1, 2, 3, 4];
+            int exp = 5;
+            int res = alg.HeightChecker(heights);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] heights = [1, 2, 3, 4, 5];
+            int exp = 0;
+            int res = alg.HeightChecker(heights);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1051_1100/1051_HeightCheckerAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1051_1100
+{
+    public class _1051_HeightCheckerAlg
+    {
+        public int HeightChecker(int[] heights)
+        {
+            int[] ans = new int[heights.Length];
+            heights.CopyTo(ans, 0);
+            Array.Sort(ans);
+            int count = 0;
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if (heights[i] != ans[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
