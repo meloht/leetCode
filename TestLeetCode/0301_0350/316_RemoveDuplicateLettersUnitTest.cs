a4f2d1ca4e44df6afcfcfda9dccfaf69e0b8bb9a
hongtao_luo <luohongtao621@hotmail.com>
2024-04-26 14:03:10 +0800

    316

---
+++ TestLeetCode/0301_0350/316_RemoveDuplicateLettersUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _316_RemoveDuplicateLettersUnitTest
+    {
+        _316_RemoveDuplicateLettersAlg alg = new _316_RemoveDuplicateLettersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "bcabc";
+            string exp = "abc";
+            string res = alg.RemoveDuplicateLetters(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "cbacdcbc";
+            string exp = "acdb";
+            string res = alg.RemoveDuplicateLetters(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0301_0350/316_RemoveDuplicateLettersAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _316_RemoveDuplicateLettersAlg
+    {
+        public string RemoveDuplicateLetters(string s)
+        {
+            int[] arr = new int[26];
+            List<int> list = new List<int>();
+            foreach (var item in s)
+            {
+                int i = item - 'a';
+                arr[i]++;
+                if (arr[i] > 1)
+                {
+                    list.Add(i);
+                }
+            }
+            list.Sort();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int k = s[i] - 'a';
+                if (arr[k] > 1)
+                {
+                    
+                }
+            }
+
+
+            return "";
+        }
+    }
+}
