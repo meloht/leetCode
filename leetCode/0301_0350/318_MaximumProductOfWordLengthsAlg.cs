8594cb289521131d86c01bb04e7e1468e14704a4
hongtao luo <luohongtao621@163.com>
2024-04-27 12:54:40 +0800

    318

--- leetCode/0301_0350/318_MaximumProductOfWordLengthsAlg.cs
+++ leetCode/0301_0350/318_MaximumProductOfWordLengthsAlg.cs
@@ -10,7 +10,43 @@ namespace leetCode._0301_0350
     {
         public int MaxProduct(string[] words)
         {
-            return 0;
+            int[][] map = new int[words.Length][];
+            for (int i = 0; i < words.Length; i++)
+            {
+                map[i] = GetArr(words[i]);
+            }
+            int ans = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    if (IsContain(map[i], map[j]))
+                    {
+                        continue;
+                    }
+                    ans = Math.Max(ans, words[i].Length * words[j].Length);
+                }
+            }
+            return ans;
+        }
+        private int[] GetArr(string s)
+        {
+            int[] arr = new int[26];
+            foreach (var item in s)
+            {
+                int index = item - 'a';
+                arr[index]++;
+            }
+            return arr;
+        }
+        private bool IsContain(int[] arr1, int[] arr2)
+        {
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] > 0 && arr2[i] > 0)
+                    return true;
+            }
+            return false;
         }
     }
 }
