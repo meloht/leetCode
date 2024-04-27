38f4358d9574cf9ff215b2502098d385f7ea6325
hongtao luo <luohongtao621@163.com>
2024-04-27 11:10:47 +0800

    2639

---
+++ TestLeetCode/2601_2650/2639_FindTheWidthOfColumnsOfAGridUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._2601_2650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2601_2650
+{
+    [TestClass]
+    public class _2639_FindTheWidthOfColumnsOfAGridUnitTest
+    {
+        _2639_FindTheWidthOfColumnsOfAGridAlg alg = new _2639_FindTheWidthOfColumnsOfAGridAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[1], [22], [333]];
+            int[] exp = [3];
+            int[] res = alg.FindColumnWidth(grid);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[-15, 1, 3], [15, 7, 12], [5, 6, -2]];
+            int[] exp = [3, 1, 2];
+            int[] res = alg.FindColumnWidth(grid);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2601_2650/2639_FindTheWidthOfColumnsOfAGridAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2601_2650
+{
+    public class _2639_FindTheWidthOfColumnsOfAGridAlg
+    {
+        public int[] FindColumnWidth(int[][] grid)
+        {
+            int m = grid.Length;
+            int n = grid[0].Length;
+            int[] ans = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int len = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    len = Math.Max(len, grid[j][i].ToString().Length);
+                }
+                ans[i] = len;
+            }
+
+            return ans;
+        }
+    }
+}
