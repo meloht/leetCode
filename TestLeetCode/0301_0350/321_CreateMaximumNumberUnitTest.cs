28a09486ce8e0d379f783e09ae8503b4bd38ba0d
hongtao_luo <luohongtao621@hotmail.com>
2024-04-28 14:07:53 +0800

    321

---
+++ TestLeetCode/0301_0350/321_CreateMaximumNumberUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _321_CreateMaximumNumberUnitTest
+    {
+        _321_CreateMaximumNumberAlg alg = new _321_CreateMaximumNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [3, 4, 6, 5], nums2 = [9, 1, 2, 5, 8, 3];
+            int k = 5;
+            int[] exp = [9, 8, 6, 5, 3];
+            int[] res = alg.MaxNumber(nums1, nums2, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [6, 7], nums2 = [6, 0, 4];
+            int k = 3;
+            int[] exp = [6, 7, 6, 0, 4];
+            int[] res = alg.MaxNumber(nums1, nums2, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums1 = [3, 9], nums2 = [8, 9];
+            int k = 3;
+            int[] exp = [9, 8, 9];
+            int[] res = alg.MaxNumber(nums1, nums2, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0301_0350/321_CreateMaximumNumberAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    
+    public class _321_CreateMaximumNumberAlg
+    {
+        public int[] MaxNumber(int[] nums1, int[] nums2, int k)
+        {
+            return null;
+        }
+    }
+}
