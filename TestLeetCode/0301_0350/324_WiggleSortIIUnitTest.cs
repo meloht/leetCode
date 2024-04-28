9f176f3c25fdccaf5eecc3c836e308a858a96eb1
hongtao luo <luohongtao621@163.com>
2024-04-28 20:18:31 +0800

    324

---
+++ TestLeetCode/0301_0350/324_WiggleSortIIUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _324_WiggleSortIIUnitTest
+    {
+        _324_WiggleSortIIAlg alg = new _324_WiggleSortIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 5, 1, 1, 6, 4];
+            int[] exp = [1, 6, 1, 5, 1, 4];
+            alg.WiggleSort(nums);
+            Assert.IsTrue(exp.SequenceEqual(nums));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 3, 2, 2, 3, 1];
+            int[] exp = [2, 3, 1, 3, 1, 2];
+            alg.WiggleSort(nums);
+            Assert.IsTrue(exp.SequenceEqual(nums));
+        }
+    }
+}
---
+++ leetCode/0301_0350/324_WiggleSortIIAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _324_WiggleSortIIAlg
+    {
+        public void WiggleSort(int[] nums)
+        {
+            int n = nums.Length;
+            int[] arr = new int[n];
+            Array.Copy(nums, arr, n);
+            Array.Sort(arr);
+            int x = (n + 1) / 2;
+            for (int i = 0, j = x - 1, k = n - 1; i < n; i += 2, j--, k--)
+            {
+                nums[i] = arr[j];
+                if (i + 1 < n)
+                {
+                    nums[i + 1] = arr[k];
+                }
+            }
+
+        }
+    }
+}
