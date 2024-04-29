3d3883661f4926493493e4289ee4e5d04e00acec
hongtao_luo <luohongtao621@hotmail.com>
2024-04-29 13:40:24 +0800

    329

---
+++ TestLeetCode/0301_0350/329_LongestIncreasingPathInAMatrixUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _329_LongestIncreasingPathInAMatrixUnitTest
+    {
+        _329_LongestIncreasingPathInAMatrixAlg alg = new _329_LongestIncreasingPathInAMatrixAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] matrix = [[9, 9, 4], [6, 6, 8], [2, 1, 1]];
+            int exp = 4;
+            int res = alg.LongestIncreasingPath(matrix);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] matrix = [[3, 4, 5], [3, 2, 6], [2, 2, 1]];
+            int exp = 4;
+            int res = alg.LongestIncreasingPath(matrix);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] matrix = [[1]];
+            int exp = 1;
+            int res = alg.LongestIncreasingPath(matrix);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] matrix = [
+                [1, 4, 7, 9], 
+                [0, 3, 8, 5], 
+                [3, 6, 0, 6], 
+                [1, 4, 5, 6]];
+            int exp = 5;
+            int res = alg.LongestIncreasingPath(matrix);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0301_0350/329_LongestIncreasingPathInAMatrixAlg.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _329_LongestIncreasingPathInAMatrixAlg
+    {
+        int[][] dir = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+        int ans = 0;
+        public int LongestIncreasingPath(int[][] matrix)
+        {
+          
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+           
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                    bool[,] vis = new bool[m, n];
+                    Dfs(matrix, -1, 0, vis, i, j);
+                }
+            }
+            return ans;
+        }
+        private void Dfs(int[][] matrix, int prev,int len, bool[,] vis, int i, int j)
+        {
+            if (InRange(i, j, matrix) == false)
+            {
+                return;
+            }
+            if (vis[i, j])
+                return;
+            if (matrix[i][j] <= prev)
+                return;
+            vis[i, j] = true;
+
+          
+            len++;
+            ans = Math.Max(ans, len);
+            foreach (var item in dir)
+            {
+                int x = i + item[0];
+                int y = j + item[1];
+                if (InRange(x, y, matrix) == false)
+                {
+                    continue;
+                }
+                Dfs(matrix, matrix[i][j], len, vis, x, y);
+            }
+
+            vis[i, j] = false;
+
+        }
+        private bool InRange(int i, int j, int[][] matrix)
+        {
+            if (i >= matrix.Length || i < 0 || j >= matrix[0].Length || j < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
