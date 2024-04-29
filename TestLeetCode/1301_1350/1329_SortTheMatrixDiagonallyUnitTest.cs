b1c8897c1e86a9da9e5fa7631a134bff7bb331f6
hongtao_luo <luohongtao621@hotmail.com>
2024-04-29 12:03:21 +0800

    1329

---
+++ TestLeetCode/1301_1350/1329_SortTheMatrixDiagonallyUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._1301_1350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1301_1350
+{
+    [TestClass]
+    public class _1329_SortTheMatrixDiagonallyUnitTest
+    {
+        _1329_SortTheMatrixDiagonallyAlg alg = new _1329_SortTheMatrixDiagonallyAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] mat = [[3, 3, 1, 1], [2, 2, 1, 2], [1, 1, 1, 2]];
+            int[][] exp = [[1, 1, 1, 1], [1, 2, 2, 2], [1, 2, 3, 3]];
+            int[][] res = alg.DiagonalSort(mat);
+            bool bl = Utils.IsSameList(res, exp);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] mat = [[11, 25, 66, 1, 69, 7], [23, 55, 17, 45, 15, 52], [75, 31, 36, 44, 58, 8], [22, 27, 33, 25, 68, 4], [84, 28, 14, 11, 5, 50]];
+            int[][] exp = [[5, 17, 4, 1, 52, 7], [11, 11, 25, 45, 8, 69], [14, 23, 25, 44, 58, 15], [22, 27, 31, 36, 50, 66], [84, 28, 75, 33, 55, 68]];
+            int[][] res = alg.DiagonalSort(mat);
+            bool bl = Utils.IsSameList(res, exp);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/1301_1350/1329_SortTheMatrixDiagonallyAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1301_1350
+{
+    public class _1329_SortTheMatrixDiagonallyAlg
+    {
+        public int[][] DiagonalSort(int[][] mat)
+        {
+            int m = mat.Length;
+            int n = mat[0].Length;
+            for (int i = 0; i < m; i++)
+            {
+                List<int> list = new List<int>();
+                for (int j = 0, k = i; j < n && k < m; j++, k++)
+                {
+                    list.Add(mat[k][j]);
+                }
+                list.Sort();
+                for (int j = 0, k = i; j < n && k < m; j++, k++)
+                {
+                    mat[k][j] = list[j];
+                }
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                List<int> list = new List<int>();
+                for (int i = 0, k = j; i < m && k < n; i++, k++)
+                {
+                    list.Add(mat[i][k]);
+                }
+                list.Sort();
+                for (int i = 0, k = j; i < m && k < n; i++, k++)
+                {
+                    mat[i][k] = list[i];
+                }
+            }
+            return mat;
+        }
+    }
+}
