dedcbf0054a1aab06d85668cb883c0f4b66d6a92
hongtao_luo <luohongtao621@hotmail.com>
2024-04-29 12:44:27 +0800

    328

---
+++ TestLeetCode/0301_0350/328_OddEvenLinkedListUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode;
+using leetCode._0301_0350;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _328_OddEvenLinkedListUnitTest
+    {
+        _328_OddEvenLinkedListAlg alg = new _328_OddEvenLinkedListAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [1, 2, 3, 4, 5];
+            int[] exp = [1, 3, 5, 2, 4];
+            var tree = ListNode.BuildListNode(head);
+            var res = alg.OddEvenList(tree);
+
+            int[] arr = ListNode.GetLinkNodeList(res);
+            Assert.IsTrue(arr.SequenceEqual(exp));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [2, 1, 3, 5, 6, 4, 7];
+            int[] exp = [2, 3, 6, 7, 1, 5, 4];
+            var tree = ListNode.BuildListNode(head);
+            var res = alg.OddEvenList(tree);
+
+            int[] arr = ListNode.GetLinkNodeList(res);
+            Assert.IsTrue(arr.SequenceEqual(exp));
+        }
+    }
+}
---
+++ leetCode/0301_0350/328_OddEvenLinkedListAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _328_OddEvenLinkedListAlg
+    {
+        public ListNode OddEvenList(ListNode head)
+        {
+            ListNode even = new ListNode(0);
+            ListNode odd = new ListNode(0);
+            ListNode oddNode = odd;
+            ListNode evenNode = even;
+
+            ListNode node = head;
+            int index = 1;
+            while (node != null)
+            {
+                if (index % 2 == 0)
+                {
+                    evenNode.next = new ListNode(node.val);
+                    evenNode = evenNode.next;
+
+                }
+                else
+                {
+                    oddNode.next = new ListNode(node.val);
+                    oddNode = oddNode.next;
+                }
+                node = node.next;
+                index++;
+            }
+            oddNode.next = even.next;
+            return odd.next;
+        }
+    }
+}
