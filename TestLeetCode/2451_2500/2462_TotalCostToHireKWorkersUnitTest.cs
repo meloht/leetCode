6c6abd0ab92c75a98d6ce425501d0581323a8e84
hongtao luo <luohongtao621@163.com>
2024-05-01 19:54:20 +0800

    2462

---
+++ TestLeetCode/2451_2500/2462_TotalCostToHireKWorkersUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2451_2500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2451_2500
+{
+    [TestClass]
+    public class _2462_TotalCostToHireKWorkersUnitTest
+    {
+        _2462_TotalCostToHireKWorkersAlg alg = new _2462_TotalCostToHireKWorkersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] costs = [17, 12, 10, 2, 7, 2, 11, 20, 8];
+            int k = 3, candidates = 4;
+            long exp = 11;
+            long res = alg.TotalCost(costs, k, candidates);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] costs = [1, 2, 4, 1];
+            int k = 3, candidates = 3;
+            long exp = 4;
+            long res = alg.TotalCost(costs, k, candidates);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2451_2500/2462_TotalCostToHireKWorkersAlg.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2451_2500
+{
+    public class _2462_TotalCostToHireKWorkersAlg
+    {
+        public long TotalCost(int[] costs, int k, int candidates)
+        {
+            int n = costs.Length;
+    
+            PriorityQueue<Tuple<int, int>, int[]> pq = new PriorityQueue<Tuple<int, int>, int[]>(new MyCompare());
+
+
+            int left = candidates - 1, right = n - candidates;
+            if (left + 1 < right)
+            {
+                for (int i = 0; i <= left; ++i)
+                {
+                    pq.Enqueue(new Tuple<int, int>(costs[i], i), [costs[i], i]);
+                }
+                for (int i = right; i < n; ++i)
+                {
+                    pq.Enqueue(new Tuple<int, int>(costs[i], i), [costs[i], i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n; ++i)
+                {
+                    pq.Enqueue(new Tuple<int, int>(costs[i], i), [costs[i], i]);
+                }
+            }
+            long ans = 0;
+            for (int i = 0; i < k; ++i)
+            {
+                Tuple<int, int> pair = pq.Dequeue();
+                int cost = pair.Item1, id = pair.Item2;
+                ans += cost;
+                if (left + 1 < right)
+                {
+                    if (id <= left)
+                    {
+                        ++left;
+                        pq.Enqueue(new Tuple<int, int>(costs[left], left), [costs[left], left]);
+                    }
+                    else
+                    {
+                        --right;
+                        pq.Enqueue(new Tuple<int, int>(costs[right], right), [costs[right], right]);
+                    }
+                }
+            }
+            return ans;
+           
+        }
+
+        public class MyCompare : IComparer<int[]>
+        {
+            public int Compare(int[]? x, int[]? y)
+            {
+                return x[0] == y[0] ? x[1].CompareTo(y[1]) : x[0].CompareTo(y[0]);
+            }
+        }
+      
+
+    }
+}
