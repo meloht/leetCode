61f7c8e86f8cfe2ae2fff7666e70a3ab9e56b8da
hongtao luo <luohongtao621@163.com>
2024-05-02 10:36:07 +0800

    857

---
+++ TestLeetCode/0851_0900/857_MinimumCostToHireKWorkersUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _857_MinimumCostToHireKWorkersUnitTest
+    {
+        _857_MinimumCostToHireKWorkersAlg alg = new _857_MinimumCostToHireKWorkersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] quality = [10, 20, 5], wage = [70, 50, 30];
+            int k = 2;
+            double exp = 105.00000;
+            double res = alg.MincostToHireWorkers(quality, wage, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] quality = [3, 1, 10, 10, 1], wage = [4, 8, 2, 2, 7];
+            int k = 3;
+            double exp = 30.66667;
+            double res = alg.MincostToHireWorkers(quality, wage, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/857_MinimumCostToHireKWorkersAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    
+    public class _857_MinimumCostToHireKWorkersAlg
+    {
+        public double MincostToHireWorkers(int[] quality, int[] wage, int k)
+        {
+            int n = quality.Length;
+            int[] h = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                h[i] = i;
+            }
+            Array.Sort(h, (a, b) => {
+                return quality[b] * wage[a] - quality[a] * wage[b];
+            });
+            double res = 1e9;
+            double totalq = 0.0;
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            for (int i = 0; i < k - 1; i++)
+            {
+                totalq += quality[h[i]];
+                pq.Enqueue(quality[h[i]], -quality[h[i]]);
+            }
+            for (int i = k - 1; i < n; i++)
+            {
+                int idx = h[i];
+                totalq += quality[idx];
+                pq.Enqueue(quality[idx], -quality[idx]);
+                double totalc = ((double)wage[idx] / quality[idx]) * totalq;
+                res = Math.Min(res, totalc);
+                totalq -= pq.Dequeue();
+            }
+            return res;
+        }
+    }
+}
