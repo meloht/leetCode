fdad684fa1e4be9603b62e6deb584e2000848db3
hongtao luo <luohongtao621@163.com>
2024-05-05 14:15:43 +0800

    1652

---
+++ TestLeetCode/1651_1700/1652_DefuseTheBombUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._1651_1700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1651_1700
+{
+    [TestClass]
+    public class _1652_DefuseTheBombUnitTest
+    {
+        _1652_DefuseTheBombAlg alg = new _1652_DefuseTheBombAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] code = [5, 7, 1, 4];
+            int k = 3;
+            int[] exp = [12, 10, 16, 13];
+            int[] res = alg.Decrypt(code, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] code = [1, 2, 3, 4];
+            int k = 0;
+            int[] exp = [0, 0, 0, 0];
+            int[] res = alg.Decrypt(code, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] code = [2, 4, 9, 3];
+            int k = -2;
+            int[] exp = [12, 5, 6, 13];
+            int[] res = alg.Decrypt(code, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/1651_1700/1652_DefuseTheBombAlg.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1651_1700
+{
+    public class _1652_DefuseTheBombAlg
+    {
+        public int[] Decrypt1(int[] code, int k)
+        {
+            int kk = Math.Abs(k);
+            int[] res = new int[code.Length];
+
+            int num = 0;
+            int i = 1;
+            int ii = 1;
+            if (k > 0)
+            {
+                for (int j = 0; i < code.Length && j < kk; i++, j++)
+                {
+                    num += code[i];
+                    if (i == code.Length - 1)
+                    {
+                        i = -1;
+                    }
+
+                }
+            }
+            else if (k < 0)
+            {
+                i = code.Length - 1;
+                ii = code.Length - 1;
+                for (int j = 0; i >= 0 && j < kk; i--, j++)
+                {
+                    num += code[i];
+                    if (i == 0)
+                    {
+                        i = code.Length;
+                    }
+                }
+                i++;
+                ii++;
+                i = i % code.Length;
+                ii = ii % code.Length;
+            }
+          
+            res[0] = num;
+
+            for (int j = 1; j < code.Length; j++)
+            {
+                if (k == 0)
+                {
+                    res[j] = 0;
+                }
+                else if (k > 0)
+                {
+                    num += code[i];
+                    num -= code[ii];
+                    res[j] = num;
+                    i++;
+                    ii++;
+                    i = i % code.Length;
+                    ii = ii % code.Length;
+                }
+                else
+                {
+                    num += code[ii];
+                    num -= code[i];
+                    res[j] = num;
+                    i++;
+                    ii++;
+                    i = i % code.Length;
+                    ii = ii % code.Length;
+                }
+            }
+            return res;
+        }
+
+        public int[] Decrypt(int[] code, int k)
+        {
+            int n = code.Length;
+            if (k == 0)
+            {
+                return new int[n];
+            }
+            int[] res = new int[n];
+            int[] newCode = new int[n * 2];
+            Array.Copy(code, 0, newCode, 0, n);
+            Array.Copy(code, 0, newCode, n, n);
+            code = newCode;
+            int l = k > 0 ? 1 : n + k;
+            int r = k > 0 ? k : n - 1;
+            int w = 0;
+            for (int i = l; i <= r; i++)
+            {
+                w += code[i];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                res[i] = w;
+                w -= code[l];
+                w += code[r + 1];
+                l++;
+                r++;
+            }
+            return res;
+        }
+
+
+    }
+}
