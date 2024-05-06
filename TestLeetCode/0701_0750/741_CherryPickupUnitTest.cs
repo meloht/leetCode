f91683295280a5b7a26e7e3c14375536deca327f
hongtao_luo <luohongtao621@hotmail.com>
2024-05-06 14:03:53 +0800

    741

---
+++ TestLeetCode/0701_0750/741_CherryPickupUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _741_CherryPickupUnitTest
+    {
+        _741_CherryPickupAlg alg = new _741_CherryPickupAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[0, 1, -1], [1, 0, -1], [1, 1, 1]];
+            int exp = 5;
+            int res = alg.CherryPickup(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[1, 1, -1], [1, -1, 1], [-1, 1, 1]];
+            int exp = 0;
+            int res = alg.CherryPickup(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
