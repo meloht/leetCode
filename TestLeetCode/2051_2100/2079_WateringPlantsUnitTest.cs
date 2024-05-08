15a123f6af10bcfbe2669c5d23f9cef930e827a2
hongtao_luo <luohongtao621@hotmail.com>
2024-05-08 12:12:28 +0800

    2079

---
+++ TestLeetCode/2051_2100/2079_WateringPlantsUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._2051_2100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2051_2100
+{
+    [TestClass]
+    public class _2079_WateringPlantsUnitTest
+    {
+        _2079_WateringPlantsAlg alg = new _2079_WateringPlantsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] plants = [2, 2, 3, 3];
+            int capacity = 5;
+            int exp = 14;
+            int res = alg.WateringPlants(plants, capacity);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] plants = [1, 1, 1, 4, 2, 3];
+            int capacity = 4;
+            int exp = 30;
+            int res = alg.WateringPlants(plants, capacity);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] plants = [7, 7, 7, 7, 7, 7, 7];
+            int capacity = 8;
+            int exp = 49;
+            int res = alg.WateringPlants(plants, capacity);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2051_2100/2079_WateringPlantsAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2051_2100
+{
+    public class _2079_WateringPlantsAlg
+    {
+        public int WateringPlants(int[] plants, int capacity)
+        {
+            int num = capacity;
+            int count = 0;
+            for (int i = 0; i < plants.Length; i++)
+            {
+                if (num >= plants[i])
+                {
+                    num -= plants[i];
+                    count++;
+                }
+                else
+                {
+                    count += i  * 2;
+                    num = capacity;
+                    num -= plants[i];
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
