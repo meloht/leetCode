48268fe61c56d34ed9021a0da58587d673c0c1fc
hongtao_luo <luohongtao621@hotmail.com>
2024-05-08 13:56:24 +0800

    335

---
+++ TestLeetCode/0301_0350/335_SelfCrossingUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _335_SelfCrossingUnitTest
+    {
+        _335_SelfCrossingAlg alg = new _335_SelfCrossingAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] distance = [2, 1, 1, 2];
+            bool exp = true;
+            bool res = alg.IsSelfCrossing(distance);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] distance = [1, 2, 3, 4];
+            bool exp = false;
+            bool res = alg.IsSelfCrossing(distance);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] distance = [1, 1, 1, 1];
+            bool exp = true;
+            bool res = alg.IsSelfCrossing(distance);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] distance = [1, 1, 2, 1, 1];
+            bool exp = true;
+            bool res = alg.IsSelfCrossing(distance);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] distance = [1, 1, 2, 2, 1, 1];
+            bool exp = true;
+            bool res = alg.IsSelfCrossing(distance);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
