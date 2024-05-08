037143bac76a1af4937f757703c06382914da89c
hongtao_luo <luohongtao621@hotmail.com>
2024-05-08 14:22:07 +0800

    336

---
+++ TestLeetCode/0301_0350/336_PalindromePairsUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _336_PalindromePairsUnitTest
+    {
+        _336_PalindromePairsAlg alg = new _336_PalindromePairsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["abcd", "dcba", "lls", "s", "sssll"];
+            IList<IList<int>> exp = [[0, 1], [1, 0], [3, 2], [2, 4]];
+            IList<IList<int>> res = alg.PalindromePairs(words);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["bat", "tab", "cat"];
+            IList<IList<int>> exp = [[0, 1], [1, 0]];
+            IList<IList<int>> res = alg.PalindromePairs(words);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] words = ["a", ""];
+            IList<IList<int>> exp = [[0, 1], [1, 0]];
+            IList<IList<int>> res = alg.PalindromePairs(words);
+
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.IsTrue(bl);
+
+        }
+    }
+}
---
+++ leetCode/0301_0350/336_PalindromePairsAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _336_PalindromePairsAlg
+    {
+        public IList<IList<int>> PalindromePairs(string[] words)
+        {
+
+            return null;
+        }
+    }
+}
