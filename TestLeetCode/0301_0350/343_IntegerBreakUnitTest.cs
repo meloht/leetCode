c79c43f8823226889736cd8b4be623748cf50e3d
hongtao_luo <luohongtao621@hotmail.com>
2024-05-09 15:22:18 +0800

    343

---
+++ TestLeetCode/0301_0350/343_IntegerBreakUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0301_0350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0301_0350
+{
+    [TestClass]
+    public class _343_IntegerBreakUnitTest
+    {
+        _343_IntegerBreakAlg alg = new _343_IntegerBreakAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 2;
+            int exp = 1;
+            int res = alg.IntegerBreak(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 10;
+            int exp = 36;
+            int res = alg.IntegerBreak(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0301_0350/343_IntegerBreakAlg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0301_0350
+{
+    public class _343_IntegerBreakAlg
+    {
+        public int IntegerBreak(int n)
+        {
+            if (n < 7)
+            {
+                switch (n)
+                {
+                    case 2:
+                        return 1;
+                    case 3:
+                        return 2;
+                    case 4:
+                        return 4;
+                    case 5:
+                        return 6;
+                    case 6:
+                        return 9;
+                    default:
+                        return 0;
+                }
+            }
+            int[] dp = new int[n];
+            dp[0] = 1;
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+            dp[4] = 6;
+            dp[5] = 9;
+
+            for (int i = 6; i < n; i++)
+            {
+                dp[i] = Math.Max(dp[i - 2] * 2, dp[i - 3] * 3);
+
+            }
+            return dp[n - 1];
+        }
+    }
+}
