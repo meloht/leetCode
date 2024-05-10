22c48e79fc9d441731dd2c9cd4972d686198dc94
hongtao_luo <luohongtao621@hotmail.com>
2024-05-10 11:43:19 +0800

    2960

---
+++ TestLeetCode/2951_3000/2960_CountTestedDevicesAfterTestOperationsUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2951_3000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2951_3000
+{
+    [TestClass]
+    public class _2960_CountTestedDevicesAfterTestOperationsUnitTest
+    {
+        _2960_CountTestedDevicesAfterTestOperationsAlg alg = new _2960_CountTestedDevicesAfterTestOperationsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] batteryPercentages = [1, 1, 2, 1, 3];
+            int exp = 3;
+            int res = alg.CountTestedDevices(batteryPercentages);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] batteryPercentages = [0, 1, 2];
+            int exp = 2;
+            int res = alg.CountTestedDevices(batteryPercentages);
+            Assert.AreEqual(exp, res);
+        }
+
+
+    }
+}
---
+++ leetCode/2951_3000/2960_CountTestedDevicesAfterTestOperationsAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2951_3000
+{
+    public class _2960_CountTestedDevicesAfterTestOperationsAlg
+    {
+        public int CountTestedDevices(int[] batteryPercentages)
+        {
+            int count = 0;
+            for (int i = 0; i < batteryPercentages.Length; i++)
+            {
+                if (batteryPercentages[i] > 0)
+                {
+                    count++;
+                    for (int j = i + 1; j < batteryPercentages.Length; j++)
+                    {
+                        batteryPercentages[j] = Math.Max(0, batteryPercentages[j] - 1);
+                    }
+                }
+            }
+            return count;
+        }
+
+        public int CountTestedDevices1(int[] batteryPercentages)
+        {
+            int need = 0;
+            foreach (int battery in batteryPercentages)
+            {
+                if (battery > need)
+                {
+                    need++;
+                }
+            }
+            return need;
+        }
+
+
+    }
+}
