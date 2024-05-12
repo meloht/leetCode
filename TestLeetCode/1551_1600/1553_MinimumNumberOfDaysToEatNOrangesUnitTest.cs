359e960c5ca05fd3f6f865d18745b4f520c38082
hongtao luo <luohongtao621@163.com>
2024-05-12 17:44:33 +0800

    1553

---
+++ TestLeetCode/1551_1600/1553_MinimumNumberOfDaysToEatNOrangesUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._1551_1600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1551_1600
+{
+    [TestClass]
+    public class _1553_MinimumNumberOfDaysToEatNOrangesUnitTest
+    {
+        _1553_MinimumNumberOfDaysToEatNOrangesAlg alg = new _1553_MinimumNumberOfDaysToEatNOrangesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 10;
+            int exp = 4;
+            int res = alg.MinDays(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 6;
+            int exp = 3;
+            int res = alg.MinDays(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 1;
+            int exp = 1;
+            int res = alg.MinDays(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int n = 56;
+            int exp = 6;
+            int res = alg.MinDays(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int n = 638826798;
+            int exp = 6;
+            int res = alg.MinDays(n);
+           // Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1551_1600/1553_MinimumNumberOfDaysToEatNOrangesAlg.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1551_1600
+{
+    public class _1553_MinimumNumberOfDaysToEatNOrangesAlg
+    {
+       
+        public int MinDays(int n)
+        {
+            Dictionary<int, int> dp = new Dictionary<int, int>();
+            return Dfs(n, dp);
+        }
+
+        private int Dfs(int n, Dictionary<int, int> dp)
+        {
+            if (dp.ContainsKey(n))
+            {
+                return dp[n];
+            }
+            if (n <= 0)
+            {
+                return 0;
+            }
+
+            int ans = 0;
+            if (n % 2 == 0 && n % 3 == 0)
+            {
+                ans = Math.Min(Dfs(n / 2, dp) + 1, Dfs(n / 3, dp) + 1);
+            }
+            else if (n % 2 == 0)
+            {
+                ans = Math.Min(Dfs(n / 2, dp) + 1, Dfs(n - 1, dp) + 1);
+            }
+            else if (n % 3 == 0)
+            {
+                ans = Math.Min(Dfs(n / 3, dp) + 1, Dfs(n - 1, dp) + 1);
+            }
+            else
+            {
+                ans = Dfs(n - 1, dp) + 1;
+            }
+            if (!dp.ContainsKey(n))
+            {
+                dp.Add(n, ans);
+            }
+            return ans;
+
+        }
+    }
+}
