5c6c3b4acbf05aef7d73190961e8a0e519cc8b2e
hongtao_luo <luohongtao621@hotmail.com>
2024-05-13 13:20:50 +0800

    363

---
+++ TestLeetCode/0351_0400/363_MaxSumOfRectangleNoLargerThanKUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _363_MaxSumOfRectangleNoLargerThanKUnitTest
+    {
+        _363_MaxSumOfRectangleNoLargerThanKAlg alg = new _363_MaxSumOfRectangleNoLargerThanKAlg();
+        
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] matrix = [[1, 0, 1], [0, -2, 3]];
+            int k = 2;
+            int exp = 2;
+            int res = alg.MaxSumSubmatrix(matrix, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] matrix = [[2, 2, -1]];
+            int k = 3;
+            int exp = 3;
+            int res = alg.MaxSumSubmatrix(matrix, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/363_MaxSumOfRectangleNoLargerThanKAlg.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _363_MaxSumOfRectangleNoLargerThanKAlg
+    {
+        public int MaxSumSubmatrix(int[][] matrix, int k)
+        {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+
+            int[,] sumMat = new int[m, n];
+
+            sumMat[0, 0] = matrix[0][0];
+
+            for (int i = 1; i < m; i++)
+            {
+                sumMat[i, 0] = sumMat[i - 1, 0] + matrix[i][0];
+            }
+            for (int i = 1; i < n; i++)
+            {
+                sumMat[0, i] = sumMat[0, i - 1] + matrix[0][i];
+            }
+
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    sumMat[i, j] = sumMat[i - 1, j] + sumMat[i, j - 1] + matrix[i][j] - sumMat[i - 1, j - 1];
+                }
+            }
+
+            int ans = int.MinValue;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = m - 1; j >= i; j--)
+                {
+                    for (int col1 = 0; col1 < n; col1++)
+                    {
+                        for (int col2 = n - 1; col2 >= col1; col2--)
+                        {
+                            int sum = sumMat[j, col2];
+                            if (i > 0)
+                            {
+                                sum -= sumMat[i - 1, col2];
+                            }
+                            if (col1 > 0)
+                            {
+                                sum -= sumMat[j, col1 - 1];
+                            }
+                            if (i > 0 && col1 > 0)
+                            {
+                                sum += sumMat[i - 1, col1 - 1];
+                            }
+
+                            if (sum == k)
+                                return k;
+                            if (sum < k)
+                            {
+                                ans = Math.Max(ans, sum);
+                            }
+
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
