3f70cc069c827564dfded16a64234e781ee21a0b
hongtao_luo <luohongtao621@hotmail.com>
2024-05-14 12:41:57 +0800

    2244

---
+++ TestLeetCode/2201_2250/_2244_MinimumRoundsToCompleteAllTasksUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._2201_2250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2201_2250
+{
+    [TestClass]
+    public class _2244_MinimumRoundsToCompleteAllTasksUnitTest
+    {
+        _2244_MinimumRoundsToCompleteAllTasksAlg alg = new _2244_MinimumRoundsToCompleteAllTasksAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] tasks = [2, 2, 3, 3, 2, 4, 4, 4, 4, 4];
+            int exp = 4;
+            int res = alg.MinimumRounds(tasks);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] tasks = [2, 3, 3];
+            int exp = -1;
+            int res = alg.MinimumRounds(tasks);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] tasks = [5, 5, 5, 5];
+            int exp = 2;
+            int res = alg.MinimumRounds(tasks);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] tasks = [66, 66, 63, 61, 63, 63, 64, 66, 66, 65, 66, 65, 61, 67, 68, 66, 62, 67, 61, 64, 66, 60, 69, 66, 65, 68, 63, 60, 67, 62, 68, 60, 66, 64, 60, 60, 60, 62, 66, 64, 63, 65, 60, 69, 63, 68, 68, 69, 68, 61];
+            int exp = 20;
+            int res = alg.MinimumRounds(tasks);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2201_2250/2244_MinimumRoundsToCompleteAllTasksAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2201_2250
+{
+    public class _2244_MinimumRoundsToCompleteAllTasksAlg
+    {
+        public int MinimumRounds(int[] tasks)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (int num in tasks)
+            {
+                if (!dict.ContainsKey(num))
+                {
+                    dict.Add(num, 1);
+                }
+                else
+                {
+                    dict[num]++;
+                }
+            }
+
+            int count = 0;
+            foreach (var item in dict)
+            {
+                
+                int num = dict[item.Key];
+                if (num == 1)
+                {
+                    return -1;
+                }
+                if (num % 3 == 0)
+                {
+                    count += num / 3;
+                }
+
+                else
+                {
+                    count += num / 3;
+                    count++;
+                }
+            }
+            return count;
+        }
+
+
+    }
+}
