7c1f87968f33184e5c83d865b4169df6e482cd0c
hongtao_luo <luohongtao621@hotmail.com>
2024-05-14 13:55:15 +0800

    371

---
+++ TestLeetCode/0351_0400/371_SumOfTwoIntegersUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _371_SumOfTwoIntegersUnitTest
+    {
+        _371_SumOfTwoIntegersAlg alg = new _371_SumOfTwoIntegersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int a = 1, b = 2;
+            int exp = 3;
+            int res = alg.GetSum(a, b);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int a = 2, b = 3;
+            int exp = 5;
+            int res = alg.GetSum(a, b);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int a = 1, b = -1;
+            int exp = 0;
+            int res = alg.GetSum(a, b);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/371_SumOfTwoIntegersAlg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _371_SumOfTwoIntegersAlg
+    {
+        public int GetSum(int a, int b)
+        {
+            while (b != 0)
+            {
+                int carry = (a & b) << 1;
+                a = a ^ b;
+                b = carry;
+            }
+            return a;
+
+        }
+
+
+    }
+}
