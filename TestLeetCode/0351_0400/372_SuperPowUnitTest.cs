4e001ef9dcbe2e6969a7cb0f6c0178aaef9fc944
hongtao luo <luohongtao621@163.com>
2024-05-14 20:13:09 +0800

    372

---
+++ TestLeetCode/0351_0400/372_SuperPowUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _372_SuperPowUnitTest
+    {
+        _372_SuperPowAlg alg = new _372_SuperPowAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int a = 2;
+            int[] b = [3];
+            int exp = 8;
+            int res = alg.SuperPow(a, b);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int a = 2;
+            int[] b = [1, 0];
+            int exp = 1024;
+            int res = alg.SuperPow(a, b);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int a = 1;
+            int[] b = [4, 3, 3, 8, 5, 2];
+            int exp = 1;
+            int res = alg.SuperPow(a, b);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int a = 2147483647;
+            int[] b = [2, 0, 0];
+            int exp = 1198;
+            int res = alg.SuperPow(a, b);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/372_SuperPowAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _372_SuperPowAlg
+    {
+        const int MOD = 1337;
+
+        public int SuperPow(int a, int[] b)
+        {
+            int ans = 1;
+            for (int i = b.Length - 1; i >= 0; --i)
+            {
+                ans = (int)((long)ans * Pow(a, b[i]) % MOD);
+                a = Pow(a, 10);
+            }
+            return ans;
+        }
+
+        public int Pow(int x, int n)
+        {
+            int res = 1;
+            while (n != 0)
+            {
+                if (n % 2 != 0)
+                {
+                    res = (int)((long)res * x % MOD);
+                }
+                x = (int)((long)x * x % MOD);
+                n /= 2;
+            }
+            return res;
+        }
+
+
+    }
+}
