e3c4cfabe9be9add80352bcc6bad388a792d65be
hongtao luo <luohongtao621@163.com>
2024-05-15 19:39:56 +0800

    2589

--- leetCode/2551_2600/2589_MinimumTimeToCompleteAllTasksAlg.cs
+++ leetCode/2551_2600/2589_MinimumTimeToCompleteAllTasksAlg.cs
@@ -10,9 +10,29 @@ namespace leetCode._2551_2600
     {
         public int FindMinimumTime(int[][] tasks)
         {
-            Array.Sort(tasks, (x, y) => x[0].CompareTo(y[0]));
+            Array.Sort(tasks, (x, y) => x[1].CompareTo(y[1]));
+            int n = tasks.Length;
+            int[] run = new int[tasks[n - 1][1] + 1];
+            int res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int start = tasks[i][0], end = tasks[i][1], duration = tasks[i][2];
+                for (int j = start; j <= end; j++)
+                {
+                    duration -= run[j];
+                }
+                res += Math.Max(duration, 0);
+                for (int j = end; j >= 0 && duration > 0; j--)
+                {
+                    if (run[j] == 0)
+                    {
+                        duration--;
+                        run[j] = 1;
+                    }
+                }
+            }
+            return res;
 
-            return 0;
         }
     }
 }
