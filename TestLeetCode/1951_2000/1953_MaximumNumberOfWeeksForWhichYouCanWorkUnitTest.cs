98c2b9173d3512fdcf553248dde89ab1096b30c5
hongtao_luo <luohongtao621@hotmail.com>
2024-05-16 14:00:33 +0800

    1953

---
+++ TestLeetCode/1951_2000/1953_MaximumNumberOfWeeksForWhichYouCanWorkUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._1951_2000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1951_2000
+{
+    [TestClass]
+    public class _1953_MaximumNumberOfWeeksForWhichYouCanWorkUnitTest
+    {
+        _1953_MaximumNumberOfWeeksForWhichYouCanWorkAlg alg = new _1953_MaximumNumberOfWeeksForWhichYouCanWorkAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] milestones = [1, 2, 3];
+            long exp = 6;
+            long res = alg.NumberOfWeeks(milestones);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] milestones = [5, 2, 1];
+            long exp = 7;
+            long res = alg.NumberOfWeeks(milestones);
+            Assert.AreEqual(exp, res);
+        }
+
+
+    }
+}
---
+++ leetCode/1951_2000/1953_MaximumNumberOfWeeksForWhichYouCanWorkAlg.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._0251_0300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace leetCode._1951_2000
+{
+    public class _1953_MaximumNumberOfWeeksForWhichYouCanWorkAlg
+    {
+        public long NumberOfWeeks(int[] milestones)
+        {
+            int max = milestones[0];
+
+            long total = 0;
+            foreach (int item in milestones)
+            {
+                max = Math.Max(item, max);
+                total += item;
+            }
+
+            total -= max;
+
+            if (max > total + 1)
+            {
+                return total * 2 + 1;
+            }
+            else
+            {
+                return max + total;
+            }
+        }
+    }
+}
