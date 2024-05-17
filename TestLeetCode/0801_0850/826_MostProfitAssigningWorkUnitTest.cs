6a20d9ab5d03ff87b156404304cad1e702c1fc75
hongtao_luo <luohongtao621@hotmail.com>
2024-05-17 13:12:07 +0800

    826

---
+++ TestLeetCode/0801_0850/826_MostProfitAssigningWorkUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _826_MostProfitAssigningWorkUnitTest
+    {
+        _826_MostProfitAssigningWorkAlg alg = new _826_MostProfitAssigningWorkAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] difficulty = [2, 4, 6, 8, 10], profit = [10, 20, 30, 40, 50],
+                worker = [4, 5, 6, 7];
+            int exp = 100;
+            int res=alg.MaxProfitAssignment(difficulty, profit, worker);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] difficulty = [85, 47, 57], profit = [24, 66, 99], worker = [40, 25, 25];
+            int exp = 0;
+            int res = alg.MaxProfitAssignment(difficulty, profit, worker);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] difficulty = [68, 35, 52, 47, 86], profit = [67, 17, 1, 81, 3], worker = [92, 10, 85, 84, 82];
+            int exp = 324;
+            int res = alg.MaxProfitAssignment(difficulty, profit, worker);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] 
+                difficulty = [23, 30, 35, 35, 43, 46, 47, 81, 83, 98], 
+                    profit = [8, 11, 11, 20, 33, 37, 60, 72, 87, 95],
+                worker = [95, 46, 47, 97, 11, 35, 99, 56, 41, 92];
+            int exp = 553;
+            int res = alg.MaxProfitAssignment(difficulty, profit, worker);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/826_MostProfitAssigningWorkAlg.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _826_MostProfitAssigningWorkAlg
+    {
+        public int MaxProfitAssignment(int[] difficulty, int[] profit, int[] worker)
+        {
+            int ans = 0;
+            int[][] nums = new int[difficulty.Length][];
+
+            for (int i = 0; i < difficulty.Length; i++)
+            {
+                nums[i] = [difficulty[i], profit[i]];
+            }
+            Array.Sort(nums, (x, y) => x[0].CompareTo(y[0]));
+            int max = nums[0][1];
+            for (int i = 1; i < difficulty.Length; i++)
+            {
+                max = Math.Max(max, nums[i][1]);
+                nums[i][1] = max;
+
+            }
+
+            foreach (var item in worker)
+            {
+                int index = BinarySearchRight(nums, item);
+                ans += index;
+            }
+            return ans;
+        }
+
+        public int BinarySearchRight(int[][] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            if (target >= nums[right][0])
+            {
+                return nums[right][1];
+            }
+            if (target < nums[0][0])
+            {
+                return 0;
+            }
+            int ans = 0;
+            while (left <= right)
+            {
+                int mid = (right - left) / 2 + left;
+                if (nums[mid][0] <= target)
+                {
+                    left = mid + 1;
+                    ans = nums[mid][1];
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+
+            }
+            return ans;
+        }
+    }
+}
