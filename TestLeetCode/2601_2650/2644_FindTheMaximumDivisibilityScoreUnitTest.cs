5af7016a6a495bfa98ea8a2b57a549fba80e0cc9
hongtao luo <luohongtao621@163.com>
2024-05-18 09:48:45 +0800

    2644

---
+++ TestLeetCode/2601_2650/2644_FindTheMaximumDivisibilityScoreUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2601_2650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2601_2650
+{
+    [TestClass]
+    public class _2644_FindTheMaximumDivisibilityScoreUnitTest
+    {
+        _2644_FindTheMaximumDivisibilityScoreAlg alg = new _2644_FindTheMaximumDivisibilityScoreAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 7, 9, 3, 9], divisors = [5, 2, 3];
+            int exp = 3;
+            int res = alg.MaxDivScore(nums, divisors);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [20, 14, 21, 10], divisors = [5, 7, 5];
+            int exp = 5;
+            int res = alg.MaxDivScore(nums, divisors);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [12], divisors = [10, 16];
+            int exp = 10;
+            int res = alg.MaxDivScore(nums, divisors);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2601_2650/2644_FindTheMaximumDivisibilityScoreAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2601_2650
+{
+    public class _2644_FindTheMaximumDivisibilityScoreAlg
+    {
+        public int MaxDivScore(int[] nums, int[] divisors)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int max = 0;
+            foreach (var item in divisors)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    continue;
+                }
+                int count = 0;
+                foreach (var number in nums)
+                {
+                    if (number % item == 0)
+                    {
+                        count++;
+                    }
+                }
+                dict.Add(item, count);
+                max = Math.Max(max, count);
+            }
+
+            int ans = dict.Where(p => p.Value == max).Min(p => p.Key);
+
+            return ans;
+        }
+    }
+}
