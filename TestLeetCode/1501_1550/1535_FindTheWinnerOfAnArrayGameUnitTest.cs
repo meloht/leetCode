483145bc5f5f63215fa9549caff429bdb21b2641
hongtao luo <luohongtao621@163.com>
2024-05-19 10:56:28 +0800

    1535

---
+++ TestLeetCode/1501_1550/1535_FindTheWinnerOfAnArrayGameUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1501_1550
+{
+    [TestClass]
+    public class _1535_FindTheWinnerOfAnArrayGameUnitTest
+    {
+        _1535_FindTheWinnerOfAnArrayGameAlg alg = new _1535_FindTheWinnerOfAnArrayGameAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [2, 1, 3, 5, 4, 6, 7];
+            int k = 2;
+            int exp = 5;
+            int res = alg.GetWinner(arr, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [3, 2, 1];
+            int k = 10;
+            int exp = 3;
+            int res = alg.GetWinner(arr, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] arr = [1, 9, 8, 2, 3, 7, 6, 4, 5];
+            int k = 7;
+            int exp = 9;
+            int res = alg.GetWinner(arr, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] arr = [1, 11, 22, 33, 44, 55, 66, 77, 88, 99];
+            int k = 1000000000;
+            int exp = 99;
+            int res = alg.GetWinner(arr, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1501_1550/1535_FindTheWinnerOfAnArrayGameAlg.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1501_1550
+{
+    public class _1535_FindTheWinnerOfAnArrayGameAlg
+    {
+        public int GetWinner1(int[] arr, int k)
+        {
+            if (k >= arr.Length - 1)
+            {
+                int max = arr.Max();
+                return max;
+            }
+
+            var list = arr.ToList();
+
+            int count = k;
+
+            while (count > 0)
+            {
+                if (list[0] > list[1])
+                {
+                    count--;
+                    int num = list[1];
+                    list.RemoveAt(1);
+                    list.Add(num);
+                }
+                else
+                {
+
+                    count = k - 1;
+                    int num = list[0];
+                    list.RemoveAt(0);
+                    list.Add(num);
+
+                }
+                if (count == 0)
+                {
+                    return list[0];
+                }
+            }
+
+            return 0;
+        }
+
+        public int GetWinner(int[] arr, int k)
+        {
+            int prev = Math.Max(arr[0], arr[1]);
+            if (k == 1)
+            {
+                return prev;
+            }
+            int consecutive = 1;
+            int maxNum = prev;
+            int length = arr.Length;
+            for (int i = 2; i < length; i++)
+            {
+                int curr = arr[i];
+                if (prev > curr)
+                {
+                    consecutive++;
+                    if (consecutive == k)
+                    {
+                        return prev;
+                    }
+                }
+                else
+                {
+                    prev = curr;
+                    consecutive = 1;
+                }
+                maxNum = Math.Max(maxNum, curr);
+            }
+            return maxNum;
+
+        }
+    }
+}
