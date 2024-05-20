26f03f0e60888b5c8f7c6d8bd316ec9b6b5984bd
hongtao_luo <luohongtao621@hotmail.com>
2024-05-20 14:00:48 +0800

    1542

---
+++ TestLeetCode/1501_1550/1542_FindLongestAwesomeSubstringUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1501_1550
+{
+    [TestClass]
+    public class _1542_FindLongestAwesomeSubstringUnitTest
+    {
+        _1542_FindLongestAwesomeSubstringAlg alg = new _1542_FindLongestAwesomeSubstringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "3242415";
+            int exp = 5;
+            int res = alg.LongestAwesome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "12345678";
+            int exp = 1;
+            int res = alg.LongestAwesome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "213123";
+            int exp = 6;
+            int res = alg.LongestAwesome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "00";
+            int exp = 2;
+            int res = alg.LongestAwesome(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string s = "9498331";
+            int exp = 3;
+            int res = alg.LongestAwesome(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1501_1550/1542_FindLongestAwesomeSubstringAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1501_1550
+{
+    public class _1542_FindLongestAwesomeSubstringAlg
+    {
+        public int LongestAwesome(string s)
+        {
+            Dictionary<int, int> prefix = new Dictionary<int, int>();
+            prefix.Add(0, -1);
+            int seq = 0;
+            int ans = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int digit = s[i] - '0';
+                seq ^= (1 << digit);
+                if (prefix.ContainsKey(seq))
+                {
+                    ans = Math.Max(ans, i - prefix[seq]);
+                }
+                else
+                {
+                    prefix.Add(seq, i);
+                }
+                for (int k = 0; k < 10; ++k)
+                {
+                    if (prefix.ContainsKey(seq ^ (1 << k)))
+                    {
+                        ans = Math.Max(ans, i - prefix[seq ^ (1 << k)]);
+                    }
+                }
+
+
+            }
+           
+           
+            return ans;
+        }
+    }
+}
