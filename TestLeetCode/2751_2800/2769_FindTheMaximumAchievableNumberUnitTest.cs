e5e8f7021017d75cbc6227047a5fbece48cc25a7
hongtao_luo <luohongtao621@hotmail.com>
2024-05-21 12:08:42 +0800

    2769

---
+++ TestLeetCode/2751_2800/2769_FindTheMaximumAchievableNumberUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2751_2800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2751_2800
+{
+    [TestClass]
+    public class _2769_FindTheMaximumAchievableNumberUnitTest
+    {
+        _2769_FindTheMaximumAchievableNumberAlg alg = new _2769_FindTheMaximumAchievableNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int num = 4, t = 1;
+            int exp = 6;
+            int res = alg.TheMaximumAchievableX(num, t);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int num = 3, t = 2;
+            int exp = 7;
+            int res = alg.TheMaximumAchievableX(num, t);
+            Assert.AreEqual(exp, res);
+        }
+
+
+    }
+}
---
+++ leetCode/2751_2800/2769_FindTheMaximumAchievableNumberAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2751_2800
+{
+    public class _2769_FindTheMaximumAchievableNumberAlg
+    {
+        public int TheMaximumAchievableX(int num, int t)
+        {
+            int ans = num + 2*t;
+            return ans;
+        }
+    }
+}
