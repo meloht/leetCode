fec8c2ba90e2f7086abef0a25c2f1cbf94dc23f7
hongtao_luo <luohongtao621@hotmail.com>
2024-05-22 12:15:49 +0800

    2225

---
+++ TestLeetCode/2201_2250/2225_FindPlayersWithZeroOrOneLossesUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2201_2250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2201_2250
+{
+    [TestClass]
+    public class _2225_FindPlayersWithZeroOrOneLossesUnitTest
+    {
+        _2225_FindPlayersWithZeroOrOneLossesAlg alg = new _2225_FindPlayersWithZeroOrOneLossesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] matches = [[1, 3], [2, 3], [3, 6], [5, 6], [5, 7], [4, 5], [4, 8], [4, 9], [10, 4], [10, 9]];
+            IList<IList<int>> exp = [[1, 2, 10], [4, 5, 7, 8]];
+            IList<IList<int>> res = alg.FindWinners(matches);
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.AreEqual(bl, true);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] matches = [[2, 3], [1, 3], [5, 4], [6, 4]];
+            IList<IList<int>> exp = [[1, 2, 5, 6], []];
+            IList<IList<int>> res = alg.FindWinners(matches);
+            bool bl = Utils.IsSameList(exp, res);
+            Assert.AreEqual(bl, true);
+        }
+    }
+}
---
+++ leetCode/2201_2250/2225_FindPlayersWithZeroOrOneLossesAlg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2201_2250
+{
+    public class _2225_FindPlayersWithZeroOrOneLossesAlg
+    {
+        public IList<IList<int>> FindWinners(int[][] matches)
+        {
+            Dictionary<int, int> dictWinner = new Dictionary<int, int>();
+            Dictionary<int, int> dictLoser = new Dictionary<int, int>();
+            List<int> listWinner = new List<int>();
+            List<int> listLoser = new List<int>();
+            foreach (var item in matches)
+            {
+                if (dictWinner.ContainsKey(item[0]))
+                {
+                    dictWinner[item[0]]++;
+                }
+                else
+                {
+                    dictWinner.Add(item[0], 1);
+                }
+
+                if (dictLoser.ContainsKey(item[1]))
+                {
+                    dictLoser[item[1]]++;
+                }
+                else
+                {
+                    dictLoser.Add(item[1], 1);
+                }
+            }
+            foreach (var item in dictWinner.Keys)
+            {
+                if (!dictLoser.ContainsKey(item))
+                {
+                    listWinner.Add(item);
+                }
+            }
+
+            foreach (var item in dictLoser)
+            {
+                if (item.Value==1)
+                {
+                    listLoser.Add(item.Key);
+                }
+            }
+
+            listWinner.Sort();
+            listLoser.Sort();
+
+            return [listWinner,listLoser];
+        }
+    }
+}
