57e5aeb78c18c0174c624d9101be9835ec4d7d4f
hongtao_luo <luohongtao621@hotmail.com>
2024-05-23 12:06:42 +0800

    2831

---
+++ TestLeetCode/2801_2850/2831_FindTheLongestEqualSubarrayUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2801_2850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2801_2850
+{
+    [TestClass]
+    public class _2831_FindTheLongestEqualSubarrayUnitTest
+    {
+        _2831_FindTheLongestEqualSubarrayAlg alg = new _2831_FindTheLongestEqualSubarrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 2, 3, 1, 3]; int k = 3;
+            int exp = 3;
+            int res = alg.LongestEqualSubarray(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1, 2, 2, 1, 1]; int k = 2;
+            int exp = 4;
+            int res = alg.LongestEqualSubarray(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2801_2850/2831_FindTheLongestEqualSubarrayAlg.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._2801_2850
+{
+    public class _2831_FindTheLongestEqualSubarrayAlg
+    {
+        public int LongestEqualSubarray(IList<int> nums, int k)
+        {
+            int n = nums.Count;
+            List<int>[] posLists = new List<int>[n + 1];
+            for (int i = 0; i < posLists.Length; i++)
+            {
+                posLists[i] = new List<int>();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int x = nums[i];
+                posLists[x].Add(i - posLists[x].Count);
+            }
+
+            int ans = 0;
+            foreach (List<int> pos in posLists)
+            {
+                if (pos.Count <= ans)
+                {
+                    continue; // 无法让 ans 变得更大
+                }
+                int left = 0;
+                for (int right = 0; right < pos.Count; right++)
+                {
+                    while (pos[right] - pos[left] > k)
+                    { // 要删除的数太多了
+                        left++;
+                    }
+                    ans = Math.Max(ans, right - left + 1);
+                }
+            }
+            return ans;
+
+
+        }
+    }
+}
