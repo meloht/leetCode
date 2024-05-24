65d0b0922fc4d908d763eb2402faada9288cbe66
hongtao_luo <luohongtao621@hotmail.com>
2024-05-24 11:54:08 +0800

    1673

---
+++ TestLeetCode/1651_1700/1673_FindTheMostCompetitiveSubsequenceUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1651_1700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1651_1700
+{
+    [TestClass]
+    public class _1673_FindTheMostCompetitiveSubsequenceUnitTest
+    {
+        _1673_FindTheMostCompetitiveSubsequenceAlg alg = new _1673_FindTheMostCompetitiveSubsequenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 5, 2, 6]; int k = 2;
+            int[] exp = [2, 6];
+            int[] res = alg.MostCompetitive(nums, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 4, 3, 3, 5, 4, 9, 6]; int k = 4;
+            int[] exp = [2, 3, 3, 4];
+            int[] res = alg.MostCompetitive(nums, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/1651_1700/1673_FindTheMostCompetitiveSubsequenceAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1651_1700
+{
+    public class _1673_FindTheMostCompetitiveSubsequenceAlg
+    {
+        public int[] MostCompetitive(int[] nums, int k)
+        {
+            Stack<int> stack = new Stack<int>();
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                while (stack.Count > 0 && n - i + stack.Count > k && stack.Peek() > nums[i])
+                {
+                    stack.Pop();
+                }
+                stack.Push(nums[i]);
+            }
+            int[] res = new int[k];
+            while (stack.Count > k)
+            {
+                stack.Pop();
+            }
+            for (int i = k - 1; i >= 0; i--)
+            {
+                res[i] = stack.Pop();
+            }
+            return res;
+
+        }
+    }
+}
