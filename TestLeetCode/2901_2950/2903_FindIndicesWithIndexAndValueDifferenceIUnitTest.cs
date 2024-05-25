961bea7046d6a4917b547d458d15a76b6acce166
hongtao luo <luohongtao621@163.com>
2024-05-25 14:06:34 +0800

    2903

---
+++ TestLeetCode/2901_2950/2903_FindIndicesWithIndexAndValueDifferenceIUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._2901_2950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2901_2950
+{
+    [TestClass]
+    public class _2903_FindIndicesWithIndexAndValueDifferenceIUnitTest
+    {
+        _2903_FindIndicesWithIndexAndValueDifferenceIAlg alg = new _2903_FindIndicesWithIndexAndValueDifferenceIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [5, 1, 4, 1]; int indexDifference = 2, valueDifference = 4;
+            int[] exp = [0, 3];
+            int[] res = alg.FindIndices(nums, indexDifference, valueDifference);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 1]; int indexDifference = 0, valueDifference = 0;
+            int[] exp = [0, 0];
+            int[] res = alg.FindIndices(nums, indexDifference, valueDifference);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 2, 3]; int indexDifference = 2, valueDifference = 4;
+            int[] exp = [-1, -1];
+            int[] res = alg.FindIndices(nums, indexDifference, valueDifference);
+            bool bl = exp.SequenceEqual(res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/2901_2950/2903_FindIndicesWithIndexAndValueDifferenceIAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2901_2950
+{
+    public class _2903_FindIndicesWithIndexAndValueDifferenceIAlg
+    {
+        public int[] FindIndices(int[] nums, int indexDifference, int valueDifference)
+        {
+            int minIndex = 0, maxIndex = 0;
+            for (int j = indexDifference; j < nums.Length; j++)
+            {
+                int i = j - indexDifference;
+                if (nums[i] < nums[minIndex])
+                {
+                    minIndex = i;
+                }
+                if (nums[j] - nums[minIndex] >= valueDifference)
+                {
+                    return new int[] { minIndex, j };
+                }
+                if (nums[i] > nums[maxIndex])
+                {
+                    maxIndex = i;
+                }
+                if (nums[maxIndex] - nums[j] >= valueDifference)
+                {
+                    return new int[] { maxIndex, j };
+                }
+            }
+            return new int[] { -1, -1 };
+
+
+        }
+    }
+}
