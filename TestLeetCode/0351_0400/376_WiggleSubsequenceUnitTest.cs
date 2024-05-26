f7832b7b84774b307a5d3bb5316ae4c698b1a0d6
hongtao luo <luohongtao621@163.com>
2024-05-26 19:09:20 +0800

    376

---
+++ TestLeetCode/0351_0400/376_WiggleSubsequenceUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _376_WiggleSubsequenceUnitTest
+    {
+        _376_WiggleSubsequenceAlg alg = new _376_WiggleSubsequenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 7, 4, 9, 2, 5];
+            int exp = 6;
+            int res = alg.WiggleMaxLength(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 17, 5, 10, 13, 15, 10, 5, 16, 8];
+            int exp = 7;
+            int res = alg.WiggleMaxLength(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            int exp = 2;
+            int res = alg.WiggleMaxLength(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/376_WiggleSubsequenceAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _376_WiggleSubsequenceAlg
+    {
+        public int WiggleMaxLength(int[] nums)
+        {
+            int n = nums.Length;
+            if (n < 2)
+            {
+                return n;
+            }
+            int up = 1, down = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    up = down + 1;
+                }
+                else if (nums[i] < nums[i - 1])
+                {
+                    down = up + 1;
+                }
+            }
+            return Math.Max(up, down);
+        }
+    }
+}
