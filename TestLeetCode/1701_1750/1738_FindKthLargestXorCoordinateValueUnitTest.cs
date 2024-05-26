c16e5268732cbebd12559fe1c01947f34d711c22
hongtao luo <luohongtao621@163.com>
2024-05-26 13:27:26 +0800

    1738

---
+++ TestLeetCode/1701_1750/1738_FindKthLargestXorCoordinateValueUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using leetCode._1701_1750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1701_1750
+{
+    [TestClass]
+    public class _1738_FindKthLargestXorCoordinateValueUnitTest
+    {
+        _1738_FindKthLargestXorCoordinateValueAlg alg = new _1738_FindKthLargestXorCoordinateValueAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] matrix = [[5, 2], [1, 6]];
+            int k = 1;
+            int exp = 7;
+            int res = alg.KthLargestValue(matrix, k);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] matrix = [[5, 2], [1, 6]];
+            int k = 2;
+            int exp = 5;
+            int res = alg.KthLargestValue(matrix, k);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] matrix = [[5, 2], [1, 6]];
+            int k = 3;
+            int exp = 4;
+            int res = alg.KthLargestValue(matrix, k);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] matrix = [[5, 2], [1, 6]];
+            int k = 4;
+            int exp = 0;
+            int res = alg.KthLargestValue(matrix, k);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/1701_1750/1738_FindKthLargestXorCoordinateValueAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1701_1750
+{
+    public class _1738_FindKthLargestXorCoordinateValueAlg
+    {
+        public int KthLargestValue(int[][] matrix, int k)
+        {
+            int m = matrix.Length, n = matrix[0].Length;
+            int[,] pre = new int[m + 1, n + 1];
+            List<int> results = new List<int>();
+            for (int i = 1; i <= m; ++i)
+            {
+                for (int j = 1; j <= n; ++j)
+                {
+                    pre[i, j] = pre[i - 1, j] ^ pre[i, j - 1] ^ pre[i - 1, j - 1] ^ matrix[i - 1][j - 1];
+                    results.Add(pre[i, j]);
+                }
+            }
+
+            results.Sort((x, y) => y.CompareTo(x));
+
+            return results[k - 1];
+
+        }
+    }
+}
