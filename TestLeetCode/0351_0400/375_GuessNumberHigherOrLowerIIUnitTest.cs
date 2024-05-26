3e8fae1a7a3a58762a07f6987c97b17cc0906d06
hongtao luo <luohongtao621@163.com>
2024-05-26 18:44:56 +0800

    375

---
+++ TestLeetCode/0351_0400/375_GuessNumberHigherOrLowerIIUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _375_GuessNumberHigherOrLowerIIUnitTest
+    {
+        _375_GuessNumberHigherOrLowerIIAlg alg = new _375_GuessNumberHigherOrLowerIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 10;
+            int exp = 16;
+            int res = alg.GetMoneyAmount(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1;
+            int exp = 0;
+            int res = alg.GetMoneyAmount(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 2;
+            int exp = 1;
+            int res = alg.GetMoneyAmount(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/375_GuessNumberHigherOrLowerIIAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _375_GuessNumberHigherOrLowerIIAlg
+    {
+        static int N = 210;
+        int[,] cache = new int[N, N];
+        public int GetMoneyAmount(int n)
+        {
+
+            return Dfs(1, n);
+        }
+        private int Dfs(int l, int r)
+        {
+            if (l >= r) return 0;
+            if (cache[l, r] != 0) return cache[l, r];
+            int ans = int.MaxValue;
+            for (int x = l; x <= r; x++)
+            {
+                // 当选择的数位 x 时，至少需要 cur 才能猜中数字
+                int cur = Math.Max(Dfs(l, x - 1), Dfs(x + 1, r)) + x;
+                // 在所有我们可以决策的数值之间取最优
+                ans = Math.Min(ans, cur);
+            }
+            cache[l, r] = ans;
+            return ans;
+        }
+
+    }
+}
