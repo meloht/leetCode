1f97364e47bf88a1dd20a1b4aec814c817e1cdee
hongtao_luo <luohongtao621@hotmail.com>
2024-05-27 13:06:29 +0800

    2028

---
+++ TestLeetCode/2001_2050/2028_FindMissingObservationsUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using leetCode._2001_2050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2001_2050
+{
+    [TestClass]
+    public class _2028_FindMissingObservationsUnitTest
+    {
+        _2028_FindMissingObservationsAlg alg = new _2028_FindMissingObservationsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] rolls = [3, 2, 4, 3];
+            int mean = 4, n = 2;
+            int[] exp = [6, 6];
+            int[] res = alg.MissingRolls(rolls, mean, n);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] rolls = [1, 5, 6];
+            int mean = 3, n = 4;
+            int[] exp = [2, 3, 2, 2];
+            int[] res = alg.MissingRolls(rolls, mean, n);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] rolls = [1, 2, 3, 4];
+            int mean = 6, n = 4;
+            int[] exp = [];
+            int[] res = alg.MissingRolls(rolls, mean, n);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] rolls = [1];
+            int mean = 3, n = 1;
+            int[] exp = [5];
+            int[] res = alg.MissingRolls(rolls, mean, n);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2001_2050/2028_FindMissingObservationsAlg.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2001_2050
+{
+    public class _2028_FindMissingObservationsAlg
+    {
+        public int[] MissingRolls(int[] rolls, int mean, int n)
+        {
+            int sum = rolls.Sum();
+            int count = rolls.Length + n;
+            int num = mean * count - sum;
+            int num1 = num / n;
+            if (num > n * 6 || num <= 0 || num < n)
+            {
+                return [];
+            }
+
+            if (num % n == 0)
+            {
+
+                int[] result = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    result[i] = num1;
+                }
+                return result;
+            }
+            else
+            {
+                int num2 = num % n;
+                int[] result = new int[n];
+                for (int i = 0, j = num2; i < n; i++, j--)
+                {
+                    if (j > 0)
+                    {
+                        result[i] = num1 + 1;
+                    }
+                    else
+                    {
+                        result[i] = num1;
+                    }
+
+                }
+                return result;
+            }
+
+        }
+    }
+}
