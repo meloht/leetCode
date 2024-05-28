2dbdcba291c1f13d5a9773ef7832f2f95bf94372
hongtao_luo <luohongtao621@hotmail.com>
2024-05-28 12:30:59 +0800

    378

---
+++ TestLeetCode/0351_0400/378_KthSmallestElementInASortedMatrixUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _378_KthSmallestElementInASortedMatrixUnitTest
+    {
+        _378_KthSmallestElementInASortedMatrixAlg alg = new _378_KthSmallestElementInASortedMatrixAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] matrix = [
+                [1, 5, 9], 
+                [10, 11, 13], 
+                [12, 13, 15]];
+            int k = 8;
+            int exp = 13;
+            int res = alg.KthSmallest(matrix, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] matrix = [[-5]];
+            int k = 1;
+            int exp = -5;
+            int res = alg.KthSmallest(matrix, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/378_KthSmallestElementInASortedMatrixAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+
+    public class _378_KthSmallestElementInASortedMatrixAlg
+    {
+        public int KthSmallest(int[][] matrix, int k)
+        {
+            PriorityQueue<int, int> queue =
+                new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+
+            int n = matrix.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    queue.Enqueue(matrix[i][j], matrix[i][j]);
+                    if (queue.Count > k)
+                    {
+                        queue.Dequeue();
+                    }
+                }
+            }
+
+            return queue.Peek();
+        }
+    }
+}
