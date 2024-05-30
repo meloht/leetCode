60c8178e8d7ce149ece35cdc3a8249c9edca869f
hongtao_luo <luohongtao621@hotmail.com>
2024-05-30 12:43:37 +0800

    382

---
+++ TestLeetCode/0351_0400/382_LinkedListRandomNodeUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using leetCode;
+using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _382_LinkedListRandomNodeUnitTest
+    {
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3];
+            var head = ListNode.BuildListNode(nums);
+            _382_LinkedListRandomNodeAlg alg = new _382_LinkedListRandomNodeAlg(head);
+            alg.GetRandom();
+
+        }
+    }
+}
---
+++ leetCode/0351_0400/382_LinkedListRandomNodeAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _382_LinkedListRandomNodeAlg
+    {
+        Random random = new Random();
+        List<int> list = new List<int>();
+        public _382_LinkedListRandomNodeAlg(ListNode head)
+        {
+            ListNode node= head;
+            while (node != null) 
+            {
+                list.Add(node.val);
+                node = node.next;
+            }
+        }
+
+        public int GetRandom()
+        {
+            int index = random.Next(list.Count);
+
+            return list[index];
+        }
+    }
+}
