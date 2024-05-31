316b42cdda5b61cc95cc5f8cda6cff00ffe86e8e
hongtao_luo <luohongtao621@hotmail.com>
2024-05-31 12:42:18 +0800

    384

---
+++ TestLeetCode/0351_0400/384_ShuffleAnArrayUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0501_0550._528_RandomPickWithWeightAlg;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _384_ShuffleAnArrayUnitTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            _384_ShuffleAnArrayAlg solution = new _384_ShuffleAnArrayAlg([1, 2, 3]);
+            solution.Shuffle();    // 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。例如，返回 [3, 1, 2]
+            solution.Reset();      // 重设数组到它的初始状态 [1, 2, 3] 。返回 [1, 2, 3]
+            solution.Shuffle();    // 随机返回数组 [1, 2, 3] 打乱后的结果。例如，返回 [1, 3, 2]
+        }
+    }
+}
---
+++ leetCode/0351_0400/384_ShuffleAnArrayAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _384_ShuffleAnArrayAlg
+    {
+        Random random = new Random();
+        int[] orginal = null;
+        public _384_ShuffleAnArrayAlg(int[] nums)
+        {
+            orginal = nums;
+        }
+
+        public int[] Reset()
+        {
+            return orginal;
+        }
+
+        public int[] Shuffle()
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < orginal.Length; i++)
+            {
+                list.Add(i);
+            }
+            int[] res = new int[list.Count];
+            int j = 0;
+            while (list.Count > 0)
+            {
+                int index = random.Next(list.Count);
+                int indexOriginal = list[index];
+                res[j] = orginal[indexOriginal];
+                list.RemoveAt(index);
+                j++;
+            }
+            return res;
+        }
+    }
+}
