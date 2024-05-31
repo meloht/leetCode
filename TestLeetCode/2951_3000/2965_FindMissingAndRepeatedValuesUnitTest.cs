cdb77623bcc496bc6a04ea910a40d38e052d28c4
hongtao_luo <luohongtao621@hotmail.com>
2024-05-31 12:14:39 +0800

    2965

---
+++ TestLeetCode/2951_3000/2965_FindMissingAndRepeatedValuesUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2951_3000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2951_3000
+{
+    [TestClass]
+    public class _2965_FindMissingAndRepeatedValuesUnitTest
+    {
+        _2965_FindMissingAndRepeatedValuesAlg alg = new _2965_FindMissingAndRepeatedValuesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[1, 3], [2, 2]];
+            int[] exp = [2, 4];
+            int[] res = alg.FindMissingAndRepeatedValues(grid);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[9, 1, 7], [8, 9, 2], [3, 4, 6]];
+            int[] exp = [9, 5];
+            int[] res = alg.FindMissingAndRepeatedValues(grid);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2951_3000/2965_FindMissingAndRepeatedValuesAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2951_3000
+{
+    public class _2965_FindMissingAndRepeatedValuesAlg
+    {
+        public int[] FindMissingAndRepeatedValues(int[][] grid)
+        {
+            int n = grid.Length;
+            HashSet<int> dict = new HashSet<int>();
+            int a = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid.Length; j++)
+                {
+                    if (dict.Contains(grid[i][j]))
+                    {
+                        a = grid[i][j];
+                    }
+                    else
+                    {
+                        dict.Add(grid[i][j]);
+                    }
+                }
+            }
+            int total = (1 + n * n) * (n * n) / 2;
+            int b = total - dict.Sum();
+            return [a, b];
+        }
+    }
+}
