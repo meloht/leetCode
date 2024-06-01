7c0e1b534cdc1f75667b357068474428c13deecd
hongtao luo <luohongtao621@163.com>
2024-06-01 17:15:30 +0800

    2928

---
+++ TestLeetCode/2901_2950/2928_DistributeCandiesAmongChildrenIUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2901_2950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2901_2950
+{
+    [TestClass]
+    public class _2928_DistributeCandiesAmongChildrenIUnitTest
+    {
+        _2928_DistributeCandiesAmongChildrenIAlg alg = new _2928_DistributeCandiesAmongChildrenIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 5, limit = 2;
+            int exp = 3;
+            int res = alg.DistributeCandies(n, limit);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 3, limit = 3;
+            int exp = 10;
+            int res = alg.DistributeCandies(n, limit);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2901_2950/2928_DistributeCandiesAmongChildrenIAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2901_2950
+{
+    public class _2928_DistributeCandiesAmongChildrenIAlg
+    {
+        public int DistributeCandies1(int n, int limit)
+        {
+            int ans = 0;
+            for (int i = 0; i <= limit; i++)
+            {
+                for (int j = 0; j <= limit; j++)
+                {
+                    if (i + j > n)
+                    {
+                        break;
+                    }
+                    if (n - i - j <= limit)
+                    {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+
+        }
+        public int DistributeCandies(int n, int limit)
+        {
+            int ans = 0;
+            for (int i = 0; i <= Math.Min(limit, n); i++)
+            {
+                if (n - i > 2 * limit)
+                {
+                    continue;
+                }
+                ans += Math.Min(n - i, limit) - Math.Max(0, n - i - limit) + 1;
+            }
+            return ans;
+        }
+
+
+
+    }
+}
