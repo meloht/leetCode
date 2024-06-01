6a3f070d0a2fc162be22c81594b24e2dcc32e698
hongtao luo <luohongtao621@163.com>
2024-06-01 17:35:25 +0800

    386

---
+++ TestLeetCode/0351_0400/386_LexicographicalNumbersUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _386_LexicographicalNumbersUnitTest
+    {
+        _386_LexicographicalNumbersAlg alg = new _386_LexicographicalNumbersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 13;
+            int[] exp = [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9];
+            int[] res = alg.LexicalOrder(n).ToArray();
+
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 2;
+            int[] exp = [1, 2];
+            int[] res = alg.LexicalOrder(n).ToArray();
+
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0351_0400/386_LexicographicalNumbersAlg.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _386_LexicographicalNumbersAlg
+    {
+        public IList<int> LexicalOrder1(int n)
+        {
+            List<string> ls = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                ls.Add(i.ToString());
+            }
+            ls.Sort();
+            List<int> res = ls.Select(p => int.Parse(p)).ToList();
+
+            return res;
+        }
+
+        public IList<int> LexicalOrder(int n)
+        {
+            IList<int> ret = new List<int>();
+            int number = 1;
+            for (int i = 0; i < n; i++)
+            {
+                ret.Add(number);
+                if (number * 10 <= n)
+                {
+                    number *= 10;
+                }
+                else
+                {
+                    while (number % 10 == 9 || number + 1 > n)
+                    {
+                        number /= 10;
+                    }
+                    number++;
+                }
+            }
+            return ret;
+        }
+
+
+    }
+}
