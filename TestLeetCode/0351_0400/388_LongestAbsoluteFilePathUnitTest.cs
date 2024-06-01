f8912def8eea1a032a6022f6771d502659bc23e0
hongtao luo <luohongtao621@163.com>
2024-06-01 17:53:56 +0800

    388

---
+++ TestLeetCode/0351_0400/388_LongestAbsoluteFilePathUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _388_LongestAbsoluteFilePathUnitTest
+    {
+        _388_LongestAbsoluteFilePathAlg alg = new _388_LongestAbsoluteFilePathAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string input = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext";
+            int exp = 20;
+            int res = alg.LengthLongestPath(input);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
+            int exp = 32;
+            int res = alg.LengthLongestPath(input);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/388_LongestAbsoluteFilePathAlg.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+   
+    public class _388_LongestAbsoluteFilePathAlg
+    {
+        public int LengthLongestPath(string input)
+        {
+            int n = input.Length;
+            int pos = 0;
+            int ans = 0;
+            Stack<int> stack = new Stack<int>();
+
+            while (pos < n)
+            {
+                /* 检测当前文件的深度 */
+                int depth = 1;
+                while (pos < n && input[pos] == '\t')
+                {
+                    pos++;
+                    depth++;
+                }
+                /* 统计当前文件名的长度 */
+                bool isFile = false;
+                int len = 0;
+                while (pos < n && input[pos] != '\n')
+                {
+                    if (input[pos] == '.')
+                    {
+                        isFile = true;
+                    }
+                    len++;
+                    pos++;
+                }
+                /* 跳过当前的换行符 */
+                pos++;
+
+                while (stack.Count >= depth)
+                {
+                    stack.Pop();
+                }
+                if (stack.Count > 0)
+                {
+                    len += stack.Peek() + 1;
+                }
+                if (isFile)
+                {
+                    ans = Math.Max(ans, len);
+                }
+                else
+                {
+                    stack.Push(len);
+                }
+            }
+            return ans;
+
+
+        }
+    }
+}
