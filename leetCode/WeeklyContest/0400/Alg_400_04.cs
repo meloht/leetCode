58f201e508f79ea66580001e6161f436ff8e2c5f
hongtao luo <luohongtao621@163.com>
2024-06-02 12:57:14 +0800

    400

--- leetCode/WeeklyContest/0400/Alg_400_04.cs
+++ leetCode/WeeklyContest/0400/Alg_400_04.cs
@@ -8,7 +8,7 @@ namespace leetCode.WeeklyContest._400
 {
     public class Alg_400_04
     {
-        public int MinimumDifference(int[] nums, int k)
+        public int MinimumDifference1(int[] nums, int k)
         {
             int ans = int.MaxValue;
             for (int i = 0; i < nums.Length; i++)
@@ -19,6 +19,39 @@ namespace leetCode.WeeklyContest._400
                 {
                     nums[j] &= x;
                     ans = Math.Min(ans, Math.Abs(nums[j] - k));
+                }
+            }
+
+            return ans;
+        }
+
+        public int MinimumDifference(int[] nums, int k)
+        {
+            List<int> list = new List<int>();
+            int ans = int.MaxValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                list.Add(nums[i]);
+                List<int> ls=new List<int>();
+
+                foreach (var item in list)
+                {
+                    int num = item & nums[i];
+                    if (ls.Count > 0 && ls[ls.Count - 1] == num)
+                    {
+                        continue;
+                    }
+                    ls.Add(num);
+                }
+                list = ls;
+                foreach (var item in list)
+                {
+                    int d = Math.Abs(item - k);
+                    if (ans == int.MaxValue || ans > d)
+                    {
+                        ans = d;
+                    }
                 }
             }
 
