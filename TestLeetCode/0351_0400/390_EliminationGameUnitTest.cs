e495fa09674096bd0a943b08886f9cf622175027
hongtao_luo <luohongtao621@hotmail.com>
2024-06-03 14:00:08 +0800

    390

---
+++ TestLeetCode/0351_0400/390_EliminationGameUnitTest.cs
@@ -0,0 +1,68 @@
+﻿using leetCode._0351_0400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _390_EliminationGameUnitTest
+    {
+        _390_EliminationGameAlg alg = new _390_EliminationGameAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 9;
+            int exp = 6;
+            int res = alg.LastRemaining(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1;
+            int exp = 1;
+            int res = alg.LastRemaining(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 1000000;
+            int exp = 481110;
+            int res = alg.LastRemaining(n);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int n = 10;
+            int exp = 8;
+            int res = alg.LastRemaining(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int n = 11;
+            int exp = 8;
+            int res = alg.LastRemaining(n);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase06()
+        {
+            int n = 16;
+            int exp = 6;
+            int res = alg.LastRemaining(n);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0351_0400/390_EliminationGameAlg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _390_EliminationGameAlg
+    {
+        public int LastRemaining(int n)
+        {
+            int first = 1;
+            int last = n;
+            if (first == last)
+                return last;
+            int step = 2;
+            if (n % 2 > 0)
+            {
+                last--;
+            }
+            first++;
+            bool isLeft = false;
+            int count = n / 2;
+            while (first < last)
+            {
+                if (isLeft)
+                {
+                    if (count % 2 > 0)
+                    {
+                        last -= step;
+                    }
+                    first += step;
+                    isLeft = false;
+
+                }
+                else
+                {
+                    if (count % 2 > 0)
+                    {
+                        first += step;
+                    }
+                    last -= step;
+                    isLeft = true;
+                }
+                count /= 2;
+                if (count == 1)
+                {
+                    return last;
+                }
+                step += step;
+               
+            }
+
+            return first;
+        }
+    }
+}
