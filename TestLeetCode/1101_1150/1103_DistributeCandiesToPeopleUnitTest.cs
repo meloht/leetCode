0fb26ce8997889d718b429bf5fe0e86d1a7771de
hongtao_luo <luohongtao621@hotmail.com>
2024-06-03 12:25:36 +0800

    1103

---
+++ TestLeetCode/1101_1150/1103_DistributeCandiesToPeopleUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1103_DistributeCandiesToPeopleUnitTest
+    {
+        _1103_DistributeCandiesToPeopleAlg alg = new _1103_DistributeCandiesToPeopleAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int candies = 7, num_people = 4;
+            int[] exp = [1, 2, 3, 1];
+            int[] res = alg.DistributeCandies(candies, num_people);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int candies = 10, num_people = 3;
+            int[] exp = [5, 2, 3];
+            int[] res = alg.DistributeCandies(candies, num_people);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+    }
+}
---
+++ leetCode/1101_1150/1103_DistributeCandiesToPeopleAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1103_DistributeCandiesToPeopleAlg
+    {
+        public int[] DistributeCandies(int candies, int num_people)
+        {
+            int[] ans = new int[num_people];
+            int num = 1;
+            int i = 0;
+            while (candies > 0)
+            {
+                if (candies > num)
+                {
+                    ans[i] += num;
+                    candies -= num;
+                }
+                else
+                {
+                    ans[i] += candies;
+                    candies = 0;
+                }
+                i++;
+                if (i % num_people == 0)
+                {
+                    i = 0;
+                }
+                num++;
+            }
+            return ans;
+        }
+    }
+}
