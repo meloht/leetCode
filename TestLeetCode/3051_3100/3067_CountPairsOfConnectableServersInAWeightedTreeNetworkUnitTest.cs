00ce936b434b327520906e49ff244a3317dad934
hongtao_luo <luohongtao621@hotmail.com>
2024-06-04 12:55:46 +0800

    3067

---
+++ TestLeetCode/3051_3100/3067_CountPairsOfConnectableServersInAWeightedTreeNetworkUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3051_3100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3051_3100
+{
+    [TestClass]
+    public class _3067_CountPairsOfConnectableServersInAWeightedTreeNetworkUnitTest
+    {
+        _3067_CountPairsOfConnectableServersInAWeightedTreeNetworkAlg alg = new _3067_CountPairsOfConnectableServersInAWeightedTreeNetworkAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] edges = [[0, 1, 1], [1, 2, 5], [2, 3, 13], [3, 4, 9], [4, 5, 2]];
+            int signalSpeed = 1;
+            int[] exp = [0, 4, 6, 6, 4, 0];
+            int[] res = alg.CountPairsOfConnectableServers(edges, signalSpeed);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] edges = [[0, 6, 3], [6, 5, 3], [0, 3, 1], [3, 2, 7], [3, 1, 6], [3, 4, 2]];
+            int signalSpeed = 3;
+            int[] exp = [2, 0, 0, 0, 0, 0, 2];
+            int[] res = alg.CountPairsOfConnectableServers(edges, signalSpeed);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/3051_3100/3067_CountPairsOfConnectableServersInAWeightedTreeNetworkAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3051_3100
+{
+    public class _3067_CountPairsOfConnectableServersInAWeightedTreeNetworkAlg
+    {
+        public int[] CountPairsOfConnectableServers(int[][] edges, int signalSpeed)
+        {
+
+            return null;
+        }
+    }
+}
