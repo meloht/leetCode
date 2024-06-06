61dc1bd4f1587b723769943090e17d738e37995c
hongtao luo <luohongtao621@163.com>
2024-06-06 20:17:03 +0800

    2938

---
+++ TestLeetCode/2901_2950/2938_SeparateBlackAndWhiteBallsUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2901_2950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2901_2950
+{
+    [TestClass]
+    public class _2938_SeparateBlackAndWhiteBallsUnitTest
+    {
+        _2938_SeparateBlackAndWhiteBallsAlg alg = new _2938_SeparateBlackAndWhiteBallsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "101";
+            int exp = 1;
+            long res = alg.MinimumSteps(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "100";
+            int exp = 2;
+            long res = alg.MinimumSteps(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "0111";
+            int exp = 0;
+            long res = alg.MinimumSteps(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2901_2950/2938_SeparateBlackAndWhiteBallsAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2901_2950
+{
+    public class _2938_SeparateBlackAndWhiteBallsAlg
+    {
+        public long MinimumSteps(string s)
+        {
+            int count = s.Count(p => p == '0');
+
+            long ans = 0;
+            List<int> ls1=new List<int>();
+            List<int> ls0 = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                if (s[i] == '1')
+                {
+                    ls1.Add(i);
+                }
+            }
+            for (int i = count; i < s.Length; i++)
+            {
+                if (s[i] == '0')
+                {
+                    ls0.Add(i);
+                }
+            }
+
+            for (int i = ls0.Count-1 ; i >=0; i--)
+            {
+                ans += (ls0[i] - ls1[i]);
+            }
+
+            return ans;
+        }
+    }
+}
