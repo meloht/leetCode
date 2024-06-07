1e4bf1588945386b355b2cc313d334c72384b1ad
hongtao_luo <luohongtao621@hotmail.com>
2024-06-07 13:35:53 +0800

    400

---
+++ TestLeetCode/0351_0400/400_NthDigitUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0351_0400;
+using leetCode.WeeklyContest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0351_0400
+{
+    [TestClass]
+    public class _400_NthDigitUnitTest
+    {
+        _400_NthDigitAlg alg = new _400_NthDigitAlg();
+       
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3;
+            int exp = 3;
+            int res = alg.FindNthDigit(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 11;
+            int exp = 0;
+            int res = alg.FindNthDigit(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0351_0400/400_NthDigitAlg.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0351_0400
+{
+    public class _400_NthDigitAlg
+    {
+        public int FindNthDigit(int n)
+        {
+            int low = 1, high = 9;
+            while (low < high)
+            {
+                int mid = (high - low) / 2 + low;
+                if (TotalDigits(mid) < n)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+            int d = low;
+            int prevDigits = TotalDigits(d - 1);
+            int index = n - prevDigits - 1;
+            int start = (int)Math.Pow(10, d - 1);
+            int num = start + index / d;
+            int digitIndex = index % d;
+            int digit = (num / (int)(Math.Pow(10, d - digitIndex - 1))) % 10;
+            return digit;
+
+
+        }
+
+
+        public int TotalDigits(int length)
+        {
+            int digits = 0;
+            int curLength = 1, curCount = 9;
+            while (curLength <= length)
+            {
+                digits += curLength * curCount;
+                curLength++;
+                curCount *= 10;
+            }
+            return digits;
+        }
+
+    }
+}
