2c1c118fb63cb1f8df749867c0ae5ce7abb531d6
hongtao_luo <luohongtao621@hotmail.com>
2024-06-07 12:02:06 +0800

    3038

---
+++ TestLeetCode/3001_3050/3038_MaximumNumberOfOperationsWithTheSameScoreIUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._3001_3050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3001_3050
+{
+    [TestClass]
+    public class _3038_MaximumNumberOfOperationsWithTheSameScoreIUnitTest
+    {
+        _3038_MaximumNumberOfOperationsWithTheSameScoreIAlg alg = new _3038_MaximumNumberOfOperationsWithTheSameScoreIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 2, 1, 4, 5];
+            int exp = 2;
+            int res = alg.MaxOperations(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [3, 2, 6, 1, 4];
+            int exp = 2;
+            int res = alg.MaxOperations(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3001_3050/3038_MaximumNumberOfOperationsWithTheSameScoreIAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3001_3050
+{
+    public class _3038_MaximumNumberOfOperationsWithTheSameScoreIAlg
+    {
+        public int MaxOperations(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
