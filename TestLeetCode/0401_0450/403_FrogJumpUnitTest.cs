5c8056a91a933d4058b7dbd66a19ba62530de99d
hongtao luo <luohongtao621@163.com>
2024-06-08 17:17:49 +0800

    403

---
+++ TestLeetCode/0401_0450/403_FrogJumpUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _403_FrogJumpUnitTest
+    {
+        _403_FrogJumpAlg alg = new _403_FrogJumpAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] stones = [0, 1, 3, 5, 6, 8, 12, 17];
+            bool exp = true;
+            bool res = alg.CanCross(stones);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] stones = [0, 1, 2, 3, 4, 8, 9, 11];
+            bool exp = false;
+            bool res = alg.CanCross(stones);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/403_FrogJumpAlg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _403_FrogJumpAlg
+    {
+        public bool CanCross(int[] stones)
+        {
+            int n = stones.Length;
+
+            bool[,] dp = new bool[n, n];
+            dp[0, 0] = true;
+            for (int i = 1; i < n; i++)
+            {
+                if (stones[i] - stones[i - 1] > i)
+                {
+                    return false;
+                }
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i-1; j >=0; j--)
+                {
+                    int k = stones[i] - stones[j];
+                    if (k > j + 1)
+                    {
+                        break;
+                    }
+                    dp[i, k] = dp[j, k - 1] || dp[j, k] || dp[j, k + 1];
+                    if (i == n - 1 && dp[i, k])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+       
+
+    }
+}
