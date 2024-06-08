fba65aee34f621d7b68d7cdb065ec20c7374d1bf
hongtao luo <luohongtao621@163.com>
2024-06-08 16:11:57 +0800

    402

---
+++ TestLeetCode/0401_0450/402_RemoveKDigitsUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _402_RemoveKDigitsUnitTest
+    {
+        _402_RemoveKDigitsAlg alg = new _402_RemoveKDigitsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string num = "1432219";
+            int k = 3;
+            string exp = "1219";
+            string res = alg.RemoveKdigits(num, k);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string num = "10200";
+            int k = 1;
+            string exp = "200";
+            string res = alg.RemoveKdigits(num, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string num = "10";
+            int k = 2;
+            string exp = "0";
+            string res = alg.RemoveKdigits(num, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string num = "112";
+            int k = 1;
+            string exp = "11";
+            string res = alg.RemoveKdigits(num, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/402_RemoveKDigitsAlg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _402_RemoveKDigitsAlg
+    {
+        public string RemoveKdigits(string num, int k)
+        {
+            if (k == num.Length)
+                return "0";
+
+            Stack<int> stack = new Stack<int>();
+
+            int nn = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                while (stack.Count > 0 && nn < k && num[stack.Peek()] > num[i])
+                {
+                    stack.Pop();
+                    nn++;
+
+                }
+                stack.Push(i);
+            }
+
+            while (nn < k)
+            {
+                stack.Pop();
+                nn++;
+            }
+
+            if (stack.Count == 0)
+                return "0";
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+            var ss = stack.ToArray();
+
+            for (int i = ss.Length - 1; i >= 0; i--)
+            {
+
+                if (stringBuilder.Length == 0 && num[ss[i]] == '0')
+                {
+                    continue;
+                }
+                stringBuilder.Append(num[ss[i]]);
+            }
+
+            if (stringBuilder.Length == 0)
+            {
+                return "0";
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
