5d4bae24446dbaee5efd9e43e1671a3d2d3e59c6
hongtao luo <luohongtao621@163.com>
2024-06-10 14:28:19 +0800

    406

---
+++ TestLeetCode/0401_0450/406_QueueReconstructionByHeightUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _406_QueueReconstructionByHeightUnitTest
+    {
+        _406_QueueReconstructionByHeightAlg alg = new _406_QueueReconstructionByHeightAlg();
+
+        [TestMethod]
+        public void Test1()
+        {
+            int[][] people = [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]];
+            int[][] exp = [[5, 0], [7, 0], [5, 2], [6, 1], [4, 4], [7, 1]];
+            int[][] res = alg.ReconstructQueue(people);
+            bool bl = Utils.IsSameList(res, exp);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            int[][] people = [[6, 0], [5, 0], [4, 0], [3, 2], [2, 2], [1, 4]];
+            int[][] exp = [[4, 0], [5, 0], [2, 2], [3, 2], [1, 4], [6, 0]];
+            int[][] res = alg.ReconstructQueue(people);
+            bool bl = Utils.IsSameList(res, exp);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0401_0450/406_QueueReconstructionByHeightAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+
+    public class _406_QueueReconstructionByHeightAlg
+    {
+        public int[][] ReconstructQueue(int[][] people)
+        {
+            Array.Sort(people, (x, y) =>
+            {
+                if (x[0].CompareTo(y[0]) == 0)
+                {
+                    return x[1].CompareTo(y[1]);
+                }
+                return y[0].CompareTo(x[0]);
+            });
+
+            List<int[]> res = new List<int[]>();
+
+            foreach (var item in people)
+            {
+                if (res.Count <= item[1])
+                {
+                    res.Add(item);
+                }
+                else if (res.Count > item[1])
+                {
+                    res.Insert(item[1], item);
+                }
+            }
+
+            return res.ToArray();
+        }
+
+
+    }
+}
