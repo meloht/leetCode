eacd02b9d3da5a9befa8fa6bbdaa939052e2351a
hongtao luo <luohongtao621@163.com>
2024-06-10 13:31:01 +0800

    881

---
+++ TestLeetCode/0851_0900/881_BoatsToSavePeopleUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _881_BoatsToSavePeopleUnitTest
+    {
+        _881_BoatsToSavePeopleAlg alg = new _881_BoatsToSavePeopleAlg();
+
+        [TestMethod]
+        public void Test1()
+        {
+            int[] people = [1, 2];
+            int limit = 3;
+            int exp = 1;
+            int res = alg.NumRescueBoats(people, limit);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            int[] people = [3, 2, 2, 1];
+            int limit = 3;
+            int exp = 3;
+            int res = alg.NumRescueBoats(people, limit);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            int[] people = [3, 5, 3, 4];
+            int limit = 5;
+            int exp = 4;
+            int res = alg.NumRescueBoats(people, limit);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            int[] people = [5, 1, 4, 2];
+            int limit = 6;
+            int exp = 2;
+            int res = alg.NumRescueBoats(people, limit);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/881_BoatsToSavePeopleAlg.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _881_BoatsToSavePeopleAlg
+    {
+        public int NumRescueBoats(int[] people, int limit)
+        {
+            Array.Sort(people);
+            int ans = 0;
+            for (int i = 0, j = people.Length - 1; i <= j; i++, j--)
+            {
+                if (i == j)
+                {
+                    ans++;
+                }
+                else
+                {
+                    if (people[i] + people[j] <= limit)
+                    {
+                        ans++;
+                    }
+                    else
+                    {
+                        while (j > i && people[i] + people[j] > limit)
+                        {
+                            j--;
+                            ans++;
+                        }
+                        ans++;
+                    }
+                }
+
+
+            }
+            return ans;
+        }
+
+        public int NumRescueBoats1(int[] people, int limit)
+        {
+            int ans = 0;
+            Array.Sort(people);
+            int light = 0, heavy = people.Length - 1;
+            while (light <= heavy)
+            {
+                if (people[light] + people[heavy] <= limit)
+                {
+                    ++light;
+                }
+                --heavy;
+                ++ans;
+            }
+            return ans;
+        }
+
+
+    }
+}
