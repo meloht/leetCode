3cc2f7d073cd74f2f9041c23d11eca7d4fb00642
hongtao luo <luohongtao621@163.com>
2024-06-11 21:22:58 +0800

    419

--- leetCode/0401_0450/419_BattleshipsInABoardAlg.cs
+++ leetCode/0401_0450/419_BattleshipsInABoardAlg.cs
@@ -10,7 +10,18 @@ namespace leetCode._0401_0450
     {
         public int CountBattleships(char[][] board)
         {
-            return 0;
+            int ans = 0;
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    if (board[i][j]=='X'&&(j == 0 || board[i][j - 1] != 'X') && (i == 0 || board[i - 1][j] != 'X'))
+                    {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
         }
     }
 }
