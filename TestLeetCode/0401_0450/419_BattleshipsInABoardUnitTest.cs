78f044d4662dbf033191b71214621320c5696066
hongtao_luo <luohongtao621@hotmail.com>
2024-06-11 12:44:48 +0800

    419

---
+++ TestLeetCode/0401_0450/419_BattleshipsInABoardUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0401_0450;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _419_BattleshipsInABoardUnitTest
+    {
+        _419_BattleshipsInABoardAlg alg = new _419_BattleshipsInABoardAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[][] board = [['X', '.', '.', 'X'], ['.', '.', '.', 'X'], ['.', '.', '.', 'X']];
+            int exp = 2;
+            int res = alg.CountBattleships(board);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[][] board = [['.']];
+            int exp = 0;
+            int res = alg.CountBattleships(board);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/419_BattleshipsInABoardAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _419_BattleshipsInABoardAlg
+    {
+        public int CountBattleships(char[][] board)
+        {
+            return 0;
+        }
+    }
+}
