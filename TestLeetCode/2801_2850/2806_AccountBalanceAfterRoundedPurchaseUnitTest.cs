531c604993023ef50458e874b0a04a4dac11bfab
hongtao_luo <luohongtao621@hotmail.com>
2024-06-12 12:15:48 +0800

    2806

---
+++ TestLeetCode/2801_2850/2806_AccountBalanceAfterRoundedPurchaseUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._2801_2850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2801_2850
+{
+    [TestClass]
+    public class _2806_AccountBalanceAfterRoundedPurchaseUnitTest
+    {
+        _2806_AccountBalanceAfterRoundedPurchaseAlg alg = new _2806_AccountBalanceAfterRoundedPurchaseAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int purchaseAmount = 9;
+            int exp = 90;
+            int res = alg.AccountBalanceAfterPurchase(purchaseAmount);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int purchaseAmount = 15;
+            int exp = 80;
+            int res = alg.AccountBalanceAfterPurchase(purchaseAmount);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int purchaseAmount = 11;
+            int exp = 90;
+            int res = alg.AccountBalanceAfterPurchase(purchaseAmount);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int purchaseAmount = 20;
+            int exp = 80;
+            int res = alg.AccountBalanceAfterPurchase(purchaseAmount);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2801_2850/2806_AccountBalanceAfterRoundedPurchaseAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2801_2850
+{
+
+    public class _2806_AccountBalanceAfterRoundedPurchaseAlg
+    {
+        public int AccountBalanceAfterPurchase(int purchaseAmount)
+        {
+            int count = purchaseAmount / 10;
+            int dis = purchaseAmount % 10;
+            int num = 0;
+            if (count > 0)
+            {
+                num = 10 * count;
+            }
+
+            if (dis > 0)
+            {
+                int num1 = Math.Abs((num + 10) - purchaseAmount);
+                int num2 = Math.Abs(num - purchaseAmount);
+
+                if (num1 <= num2)
+                {
+                    num += 10;
+                }
+
+            }
+            return 100 - num;
+        }
+
+        public int AccountBalanceAfterPurchase1(int purchaseAmount)
+        {
+            int r = purchaseAmount % 10;
+            if (r < 5)
+            {
+                purchaseAmount -= r;
+            }
+            else
+            {
+                purchaseAmount += 10 - r;
+            }
+            return 100 - purchaseAmount;
+        }
+
+    }
+}
