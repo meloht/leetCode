079e52a7efd2a2f78836f75a7b3fb092ed671a1d
hongtao_luo <luohongtao621@hotmail.com>
2024-06-13 13:46:20 +0800

    2813

---
+++ TestLeetCode/2801_2850/2813_MaximumEleganceOfAKLengthSubsequenceUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._2801_2850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2801_2850
+{
+    [TestClass]
+    public class _2813_MaximumEleganceOfAKLengthSubsequenceUnitTest
+    {
+        _2813_MaximumEleganceOfAKLengthSubsequenceAlg alg = new _2813_MaximumEleganceOfAKLengthSubsequenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] items = [[3, 2], [5, 1], [10, 1]];
+            int k = 2;
+            long exp = 17;
+            long res = alg.FindMaximumElegance(items, k);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] items = [[3, 1], [3, 1], [2, 2], [5, 3]];
+            int k = 3;
+            long exp = 19;
+            long res = alg.FindMaximumElegance(items, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] items = [[1, 1], [2, 1], [3, 1]];
+            int k = 3;
+            long exp = 7;
+            long res = alg.FindMaximumElegance(items, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2801_2850/2813_MaximumEleganceOfAKLengthSubsequenceAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2801_2850
+{
+    public class _2813_MaximumEleganceOfAKLengthSubsequenceAlg
+    {
+        public long FindMaximumElegance(int[][] items, int k)
+        {
+            Array.Sort(items, (x, y) =>
+            {
+                return y[0].CompareTo(x[0]);
+            });
+            HashSet<int> catSet = new HashSet<int>();
+            long ans = 0;
+            long res = 0;
+            Stack<int> stack=new Stack<int>();
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (i < k)
+                {
+                    ans += items[i][0];
+                    if (!catSet.Add(items[i][1]))
+                    {
+                        stack.Push(items[i][0]);
+                    }
+                }
+                else if (stack.Count > 0 && catSet.Add(items[i][1]))
+                {
+                    ans += items[i][0] - stack.Pop();
+                }
+                res = Math.Max(res, ans + (long)catSet.Count * catSet.Count);
+            }
+            return res;
+        }
+    }
+}
