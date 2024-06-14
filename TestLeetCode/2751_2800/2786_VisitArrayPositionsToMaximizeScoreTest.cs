0230ed18253f56685dcfa080c99a4a6f2cd82572
hongtao_luo <luohongtao621@hotmail.com>
2024-06-14 13:39:43 +0800

    2786

---
+++ TestLeetCode/2751_2800/2786_VisitArrayPositionsToMaximizeScoreTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2751_2800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2751_2800
+{
+    [TestClass]
+    public class _2786_VisitArrayPositionsToMaximizeScoreTest
+    {
+        _2786_VisitArrayPositionsToMaximizeScoreAlg alg = new _2786_VisitArrayPositionsToMaximizeScoreAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 3, 6, 1, 9, 2];
+            int x = 5;
+            long exp = 13;
+            long res = alg.MaxScore(nums, x);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 4, 6, 8];
+            int x = 3;
+            long exp = 20;
+            long res = alg.MaxScore(nums, x);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2751_2800/2786_VisitArrayPositionsToMaximizeScoreAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2751_2800
+{
+    public class _2786_VisitArrayPositionsToMaximizeScoreAlg
+    {
+        public long MaxScore(int[] nums, int x)
+        {
+            long res = nums[0];
+            long[] dp = [int.MinValue, int.MinValue];
+            dp[nums[0] % 2] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int p = nums[i] % 2;
+                long curr = Math.Max(dp[p] + nums[i], dp[1 - p] - x + nums[i]);
+                res = Math.Max(res, curr);
+                dp[p] = Math.Max(dp[p], curr);
+            }
+            return res;
+        }
+    }
+}
