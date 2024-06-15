dc96b185cb1b80230f228c7e7276e6d7167f63d8
hongtao luo <luohongtao621@163.com>
2024-06-15 17:09:58 +0800

    2779

---
+++ TestLeetCode/2751_2800/2779_MaximumBeautyOfAnArrayAfterApplyingOperationTest.cs
@@ -0,0 +1,54 @@
+﻿using leetCode._2751_2800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2751_2800
+{
+    [TestClass]
+    public class _2779_MaximumBeautyOfAnArrayAfterApplyingOperationTest
+    {
+        _2779_MaximumBeautyOfAnArrayAfterApplyingOperationAlg alg = new _2779_MaximumBeautyOfAnArrayAfterApplyingOperationAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 6, 1, 2];
+            int k = 2;
+            int exp = 3;
+            int res=alg.MaximumBeauty(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1, 1, 1];
+            int k = 10;
+            int exp = 4;
+            int res = alg.MaximumBeauty(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [13, 46, 71];
+            int k = 29;
+            int exp = 3;
+            int res = alg.MaximumBeauty(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [1];
+            int k = 1;
+            int exp = 1;
+            int res = alg.MaximumBeauty(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2751_2800/2779_MaximumBeautyOfAnArrayAfterApplyingOperationAlg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2751_2800
+{
+    public class _2779_MaximumBeautyOfAnArrayAfterApplyingOperationAlg
+    {
+        public int MaximumBeauty(int[] nums, int k)
+        {
+            if (nums.Length == 1)
+                return 1;
+            Array.Sort(nums);
+            int ans = 0;
+            int k2 = k * 2;
+
+            for (int i = 0, j = 1; i < nums.Length && j < nums.Length;)
+            {
+                while (j < nums.Length && nums[j] - nums[i] <= k2)
+                {
+                    ans = Math.Max(ans, j - i + 1);
+                    j++;
+
+                }
+                i++;
+
+            }
+            return ans;
+        }
+
+        public int MaximumBeauty1(int[] nums, int k)
+        {
+            int res = 0, n = nums.Length;
+            Array.Sort(nums);
+            for (int i = 0, j = 0; i < n; i++)
+            {
+                while (nums[i] - 2 * k > nums[j])
+                {
+                    j++;
+                }
+                res = Math.Max(res, i - j + 1);
+            }
+            return res;
+        }
+
+
+    }
+}
