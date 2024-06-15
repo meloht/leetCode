e8fad8831d4ae19987c3d531af8f0282ec785770
hongtao luo <luohongtao621@163.com>
2024-06-15 17:46:24 +0800

    413

---
+++ TestLeetCode/0401_0450/413_ArithmeticSlicesTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _413_ArithmeticSlicesTest
+    {
+        _413_ArithmeticSlicesAlg alg = new _413_ArithmeticSlicesAlg();
+        
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int exp = 3;
+            int res=alg.NumberOfArithmeticSlices(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1,];
+            int exp = 0;
+            int res = alg.NumberOfArithmeticSlices(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0401_0450/413_ArithmeticSlicesAlg.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _413_ArithmeticSlicesAlg
+    {
+        public int NumberOfArithmeticSlices(int[] nums)
+        {
+            if (nums.Length ==1)
+            {
+                return 0;
+            }
+            int ans = 0;
+            int dis = int.MaxValue;
+            int count = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int pre=nums[i-1];
+                int curr=nums[i];
+                if (dis == int.MaxValue)
+                {
+                    dis = curr - pre;
+                    count = 2;
+                }
+                else
+                {
+                    if (curr - pre == dis)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        ans += GetCount(count);
+                        dis= curr - pre;
+                        count = 2;
+                    }
+                }
+               
+            }
+            ans += GetCount(count);
+            return ans;
+        }
+        private int GetCount(int len)
+        {
+            int count = 0;
+            for (int i = 3; i <= len; i++)
+            {
+                count += (len - i + 1);
+            }
+            return count;
+        }
+
+        public int NumberOfArithmeticSlices1(int[] nums)
+        {
+            int n = nums.Length;
+            if (n == 1)
+            {
+                return 0;
+            }
+
+            int d = nums[0] - nums[1], t = 0;
+            int ans = 0;
+            // 因为等差数列的长度至少为 3，所以可以从 i=2 开始枚举
+            for (int i = 2; i < n; ++i)
+            {
+                if (nums[i - 1] - nums[i] == d)
+                {
+                    ++t;
+                }
+                else
+                {
+                    d = nums[i - 1] - nums[i];
+                    t = 0;
+                }
+                ans += t;
+            }
+            return ans;
+        }
+
+
+    }
+}
