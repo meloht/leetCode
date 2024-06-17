26e886dc15c73f3c0d0e301cb22f0a543fc483c6
hongtao luo <luohongtao621@163.com>
2024-06-17 21:46:21 +0800

    421

---
+++ TestLeetCode/0401_0450/421_MaximumXorOfTwoNumbersInAnArrayTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _421_MaximumXorOfTwoNumbersInAnArrayTest
+    {
+        _421_MaximumXorOfTwoNumbersInAnArrayAlg alg = new _421_MaximumXorOfTwoNumbersInAnArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 10, 5, 25, 2, 8];
+            int exp = 28;
+            int res = alg.FindMaximumXOR(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [14, 70, 53, 83, 49, 91, 36, 80, 92, 51, 66, 70];
+            int exp = 127;
+            int res = alg.FindMaximumXOR(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/421_MaximumXorOfTwoNumbersInAnArrayAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _421_MaximumXorOfTwoNumbersInAnArrayAlg
+    {
+        public int FindMaximumXOR(int[] nums)
+        {
+            int max = nums.Max();
+            int highBit = 31 - int.LeadingZeroCount(max);
+            int ans = 0;
+            int mask = 0;
+            HashSet<int> set = new HashSet<int>();
+            for (int i = highBit; i >= 0; i--)
+            {
+                set.Clear();
+                mask |= (1 << i);
+                int newAns = ans | (1 << i);
+
+                foreach (int item in nums)
+                {
+                    int x = item;
+                    x &= mask;
+                    if (set.Contains(newAns ^ x))
+                    {
+                        ans = newAns;
+                        break;
+                    }
+                    set.Add(x);
+                }
+            }
+            return ans;
+        }
+    }
+}
