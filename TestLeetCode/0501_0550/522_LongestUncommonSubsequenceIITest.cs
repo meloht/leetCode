e9efb0ed342d5baa5dc28239066fa01da31ebb1c
hongtao_luo <luohongtao621@hotmail.com>
2024-06-17 12:42:37 +0800

    522

---
+++ TestLeetCode/0501_0550/522_LongestUncommonSubsequenceIITest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _522_LongestUncommonSubsequenceIITest
+    {
+        _522_LongestUncommonSubsequenceIIAlg alg = new _522_LongestUncommonSubsequenceIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] strs = ["aba", "cdc", "eae"];
+            int exp = 3;
+            int res = alg.FindLUSlength(strs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] strs = ["aaa", "aaa", "aa"];
+            int exp = -1;
+            int res = alg.FindLUSlength(strs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] strs = ["aabbcc", "aabbcc", "cb"];
+            int exp = 2;
+            int res = alg.FindLUSlength(strs);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/522_LongestUncommonSubsequenceIIAlg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _522_LongestUncommonSubsequenceIIAlg
+    {
+        public int FindLUSlength(string[] strs)
+        {
+            var dict = strs.GroupBy(p => p).ToDictionary(p => p.Key, p => p.Count());
+
+            List<string> list = dict.Keys.OrderByDescending(p => p.Length).ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (dict[list[i]] != 1)
+                {
+                    continue;
+                }
+                string s = list[i];
+                bool bl = false;
+                for (int j = 0; j < i; j++)
+                {
+                    string s2 = list[j];
+                  
+                    for (int k = 0, h = 0; k < s2.Length && h < s.Length; k++)
+                    {
+                        if (s2[k] == s[h])
+                        {
+                            if (h == s.Length - 1)
+                            {
+                                bl = true;
+                                break;
+                            }
+                            h++;
+                        }
+                       
+                    }
+                    if (bl)
+                    {
+                        break;
+                    }
+                }
+
+                if (bl==false)
+                {
+                    return s.Length;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
