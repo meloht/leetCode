d69af6736f3528c3a7774f667b6146fb55c5b2ac
hongtao_luo <luohongtao621@hotmail.com>
2024-06-18 13:00:27 +0800

    2288

---
+++ TestLeetCode/2251_2300/2288_ApplyDiscountToPricesTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._2251_2300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2251_2300
+{
+    [TestClass]
+    public class _2288_ApplyDiscountToPricesTest
+    {
+        _2288_ApplyDiscountToPricesAlg alg = new _2288_ApplyDiscountToPricesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string sentence = "there are $1 $2 and 5$ candies in the shop";
+            int discount = 50;
+            string exp= "there are $0.50 $1.00 and 5$ candies in the shop";
+            string res = alg.DiscountPrices(sentence, discount);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string sentence = "1 2 $3 4 $5 $6 7 8$ $9 $10$";
+            int discount = 100;
+            string exp = "1 2 $0.00 4 $0.00 $0.00 7 8$ $0.00 $10$";
+            string res = alg.DiscountPrices(sentence, discount);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string sentence = "ka3caz4837h6ada4 r1 $602";
+            int discount = 9;
+            string exp = "ka3caz4837h6ada4 r1 $547.82";
+            string res = alg.DiscountPrices(sentence, discount);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2251_2300/2288_ApplyDiscountToPricesAlg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2288_ApplyDiscountToPricesAlg
+    {
+        public string DiscountPrices(string sentence, int discount)
+        {
+            double d = discount;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                sb.Append(sentence[i]);
+                if (sentence[i] != '$')
+                {
+                    continue;
+                }
+                if (i != 0 && sentence[i - 1] != ' ')
+                    continue;
+                
+                bool bl = false;
+                double num = 0d;
+                int idx = i;
+                for (int j = i + 1; j < sentence.Length; j++)
+                {
+                    if (char.IsDigit(sentence[j]))
+                    {
+                        num *= 10;
+                        num += sentence[j] - '0';
+                        idx = j;
+                        bl = true;
+                    }
+                    else if (sentence[j] == ' ')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        bl = false;
+                        break;
+                    }
+                }
+                if (bl)
+                {
+                    i = idx;
+                    num = num - num * (d / 100);
+                    num = Math.Round(num, 2);
+                    sb.Append(num.ToString("0.00"));
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
