7d258e8db9eb1242ea0ec87c2dcd3202a01200c2
hongtao luo <luohongtao621@163.com>
2024-06-19 21:21:32 +0800

    2713

--- leetCode/2701_2750/2713_MaximumStrictlyIncreasingCellsInAMatrixAlg.cs
+++ leetCode/2701_2750/2713_MaximumStrictlyIncreasingCellsInAMatrixAlg.cs
@@ -8,89 +8,49 @@ namespace leetCode._2701_2750
 {
     public class _2713_MaximumStrictlyIncreasingCellsInAMatrixAlg
     {
-        int m = 0;
-        int n = 0;
+
         public int MaxIncreasingCells(int[][] mat)
         {
-            m = mat.Length;
-            n = mat[0].Length;
-            int[,] dp = new int[m, n];
+            int m = mat.Length;
+            int n = mat[0].Length;
 
-            int ans = 0;
+            Dictionary<int, List<int[]>> dict = new Dictionary<int, List<int[]>>();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dict.TryAdd(mat[i][j], new List<int[]>());
+                    dict[mat[i][j]].Add([i, j]);
 
-            int[] rowMin = new int[m];
-            int[] colMin = new int[n];
+                }
+            }
             int[] rowMax = new int[m];
             int[] colMax = new int[n];
-            for (int i = 0; i < m; i++)
-            {
-                int min = mat[i][0];
-                int idx = 0;
-                rowMax[i] = mat[i].Max();
 
-                for (int j = 1; j < n; j++)
-                {
-                    if (mat[i][j] < min)
-                    {
-                        min = mat[i][j];
-                        idx = j;
-                    }
-                }
-                rowMin[i] = idx;
-            }
-            for (int i = 0; i < n; i++)
+            List<int> keys=dict.Keys.ToList();
+            keys.Sort();
+            foreach (var item in keys)
             {
-                int max = mat[0][i];
-                int min = mat[0][i];
-                int idx = 0;
-                for (int j = 1; j < m; j++)
+                var pos = dict[item];
+                List<int> res = new List<int>();
+                foreach (var arr in pos)
                 {
-                    if (mat[j][i] < min)
-                    {
-                        min = mat[j][i];
-                        idx = j;
-                    }
-                    max=Math.Max(max, mat[j][i]);
+                    res.Add(Math.Max(rowMax[arr[0]], colMax[arr[1]]) + 1);
                 }
-                colMin[i] = idx;
-                colMax[i] = max;
-            }
 
-            for (int i = 0; i < m; i++)
-            {
-                ans = Math.Max(Dfs(mat, i, rowMin[i], dp), ans);
+                for (int i = 0; i < pos.Count; i++)
+                {
+                    int[] arr = pos[i];
+                    int d = res[i];
+                    rowMax[arr[0]] = Math.Max(rowMax[arr[0]], d);
+                    colMax[arr[1]] = Math.Max(colMax[arr[1]], d);
+                }
             }
-            for (int i = 0; i < n; i++)
-            {
-                ans = Math.Max(Dfs(mat, colMin[i], i, dp), ans);
-            }
-            return ans;
+          
+            return rowMax.Max();
         }
 
 
-        private int Dfs(int[][] mat, int row, int col, int[,] dp)
-        {
-            if (dp[row, col] != 0)
-                return dp[row, col];
-            int ans = 1;
-            for (int i = 0; i < m; i++)
-            {
-                if (mat[i][col] > mat[row][col])
-                {
-                    ans = Math.Max(Dfs(mat, i, col, dp) + 1, ans);
-                }
-               
-            }
-            for (int i = 0; i < n; i++)
-            {
-                if (mat[row][i] > mat[row][col])
-                {
-                    ans = Math.Max(Dfs(mat, row, i, dp) + 1, ans);
-                }
-               
-            }
-            dp[row, col] = ans;
-            return ans;
-        }
+
     }
 }
