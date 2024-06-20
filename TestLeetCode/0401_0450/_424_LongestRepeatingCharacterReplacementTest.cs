1f41bda2eb328726f269089cf414b891e26f1602
hongtao_luo <luohongtao621@hotmail.com>
2024-06-20 13:53:25 +0800

    424

---
+++ TestLeetCode/0401_0450/_424_LongestRepeatingCharacterReplacementTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _424_LongestRepeatingCharacterReplacementTest
+    {
+        _424_LongestRepeatingCharacterReplacementAlg alg = new _424_LongestRepeatingCharacterReplacementAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "ABAB";
+            int k = 2;
+            int exp = 4;
+            int res = alg.CharacterReplacement(s, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "AABABBA";
+            int k = 1;
+            int exp = 4;
+            int res = alg.CharacterReplacement(s, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/424_LongestRepeatingCharacterReplacementAlg.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _424_LongestRepeatingCharacterReplacementAlg
+    {
+        public int CharacterReplacement(string s, int k)
+        {
+            if (s.Length <= k + 1)
+                return s.Length;
+            int left = 0;
+            int right = 0;
+            int maxChar = 0;
+            int maxCharIdx = 0;
+            int len = 0;
+
+            int[] arr = new int[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int idx = s[i] - 'A';
+                right++;
+                len++;
+
+                arr[idx]++;
+                if (arr[idx] > maxChar)
+                {
+                    maxChar = arr[idx];
+                    maxCharIdx = idx;
+                }
+
+                if (len - maxChar >= k)
+                    break;
+            }
+            int ans = len;
+            for (int i = right; i < s.Length; i++)
+            {
+                int idx = s[i] - 'A';
+                arr[idx]++;
+                if (arr[idx] > maxChar)
+                {
+                    maxChar = arr[idx];
+                    maxCharIdx = idx;
+                }
+
+                len++;
+                while (len - maxChar > k)
+                {
+                    int pre = s[left] - 'A';
+                    arr[pre]--;
+                    if (pre == maxCharIdx)
+                    {
+                        var res = GetMax(arr);
+                        maxChar = res.max;
+                        maxCharIdx = res.idx;
+                    }
+                    left++;
+                    len--;
+
+                }
+                ans = Math.Max(ans, len);
+            }
+
+            return ans;
+        }
+
+        private (int max, int idx) GetMax(int[] arr)
+        {
+            int ans = 0;
+            int index = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > ans)
+                {
+                    ans = arr[i];
+                    index = i;
+                }
+            }
+            return (ans, index);
+        }
+
+        public int CharacterReplacement1(string s, int k)
+        {
+            int[] arr = new int[26];
+            int left = 0;
+            int right = 0;
+            int n = s.Length;
+            int maxChar = 0;
+
+            while (right < n)
+            {
+                int idx = s[right] - 'A';
+                arr[idx]++;
+                maxChar = Math.Max(maxChar, arr[idx]);
+                if (right - left + 1 - maxChar > k)
+                {
+                    arr[s[left] - 'A']--;
+                    left++;
+                }
+                right++;
+
+            }
+            return right - left;
+        }
+    }
+}
