a1a5363e0f16ee09ed0267903a1900db2dc26e3e
hongtao luo <luohongtao621@163.com>
2024-06-21 22:44:55 +0800

    1071

---
+++ TestLeetCode/1051_1100/1071_GreatestCommonDivisorOfStringsTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1051_1100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1051_1100
+{
+    [TestClass]
+    public class _1071_GreatestCommonDivisorOfStringsTest
+    {
+        _1071_GreatestCommonDivisorOfStringsAlg alg = new _1071_GreatestCommonDivisorOfStringsAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string str1 = "ABCABC", str2 = "ABC";
+            string exp = "ABC";
+            string res=alg.GcdOfStrings(str1, str2);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string str1 = "ABABAB", str2 = "ABAB";
+            string exp = "AB";
+            string res = alg.GcdOfStrings(str1, str2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string str1 = "LEET", str2 = "CODE";
+            string exp = "";
+            string res = alg.GcdOfStrings(str1, str2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1051_1100/1071_GreatestCommonDivisorOfStringsAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1051_1100
+{
+    public class _1071_GreatestCommonDivisorOfStringsAlg
+    {
+        public string GcdOfStrings(string str1, string str2)
+        {
+            if (str1 + str2 != str2 + str1)
+                return "";
+            int n = Gcd(str2.Length, str1.Length);
+
+            return str1.Substring(0, n);
+        }
+
+        public int Gcd(int x, int y)
+        {
+            return x == 0 ? y : Gcd(y % x, x);
+        }
+
+    }
+}
