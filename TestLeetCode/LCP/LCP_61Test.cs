63217765b6c44e35911957a3238979b3be4f65d7
hongtao_luo <luohongtao621@hotmail.com>
2024-06-21 12:20:37 +0800

    61

---
+++ TestLeetCode/LCP/LCP_61Test.cs
@@ -0,0 +1,33 @@
+﻿using leetCode.LCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.LCP
+{
+    [TestClass]
+    public class LCP_61Test
+    {
+        LCP_61 alg = new LCP_61();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] temperatureA = [21, 18, 18, 18, 31], temperatureB = [34, 32, 16, 16, 17];
+            int exp = 2;
+            int res = alg.TemperatureTrend(temperatureA, temperatureB);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] temperatureA = [5, 10, 16, -6, 15, 11, 3], temperatureB = [16, 22, 23, 23, 25, 3, -16];
+            int exp = 3;
+            int res = alg.TemperatureTrend(temperatureA, temperatureB);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/LCP/LCP_61.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode.LCP
+{
+    public class LCP_61
+    {
+        public int TemperatureTrend(int[] temperatureA, int[] temperatureB)
+        {
+            int len = temperatureA.Length - 1;
+            int n = 0;
+            int ans = 0;
+            for (int i = 0; i < len; i++)
+            {
+                int n1 = temperatureA[i] - temperatureA[i + 1];
+                int n2 = temperatureB[i] - temperatureB[i + 1];
+                if ((n1 == 0 && n2 == 0) || (n1 > 0 && n2 > 0) || (n1 < 0 && n2 < 0))
+                {
+                    n++;
+                }
+                else
+                {
+                    ans = Math.Max(n, ans);
+                    n = 0;
+                }
+            }
+            ans = Math.Max(n, ans);
+            return ans;
+        }
+    }
+}
