150868f0f2ba2fe0cef865038e214c21a005102d
hongtao luo <luohongtao621@163.com>
2024-06-22 09:50:43 +0800

    2663

---
+++ TestLeetCode/2651_2700/2663_LexicographicallySmallestBeautifulStringTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2651_2700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2651_2700
+{
+    [TestClass]
+    public class _2663_LexicographicallySmallestBeautifulStringTest
+    {
+        _2663_LexicographicallySmallestBeautifulStringAlg alg=new _2663_LexicographicallySmallestBeautifulStringAlg();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string s = "abcz";
+            int k = 26;
+            string exp = "abda";
+            string res = alg.SmallestBeautifulString(s, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string s = "dc";
+            int k = 4;
+            string exp = "";
+            string res = alg.SmallestBeautifulString(s, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2651_2700/2663_LexicographicallySmallestBeautifulStringAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2651_2700
+{
+    public class _2663_LexicographicallySmallestBeautifulStringAlg
+    {
+        public string SmallestBeautifulString(string s, int k)
+        {
+            return null;
+        }
+    }
+}
