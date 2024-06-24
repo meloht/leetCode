e27b9008c30a8afa7b857ac02d1be74fb204d41f
hongtao_luo <luohongtao621@hotmail.com>
2024-06-24 13:31:43 +0800

    503

---
+++ TestLeetCode/0501_0550/503_NextGreaterElementIITest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _503_NextGreaterElementIITest
+    {
+        _503_NextGreaterElementIIAlg alg = new _503_NextGreaterElementIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 1];
+            int[] exp = [2, -1, 2];
+            int[] res = alg.NextGreaterElements(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4, 3];
+            int[] exp = [2, 3, 4, -1, 4];
+            int[] res = alg.NextGreaterElements(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0501_0550/503_NextGreaterElementIIAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _503_NextGreaterElementIIAlg
+    {
+        public int[] NextGreaterElements(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+            Array.Fill(res, -1);
+
+            Stack<int> stack = new Stack<int>();
+            int max = nums.Max();
+            int maxIdx = Array.IndexOf(nums, max);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (stack.Count > 0 && nums[stack.Peek()] < nums[i])
+                {
+                    var item = stack.Pop();
+                    res[item] = nums[i];
+                }
+                stack.Push(i);
+            }
+            if (stack.Count == 0 || (stack.Count == 1 && nums[stack.Peek()] == max))
+            {
+                return res;
+            }
+            for (int i = 0; i <= maxIdx; i++)
+            {
+                while (stack.Count > 0 && nums[stack.Peek()] < nums[i])
+                {
+                    var item = stack.Pop();
+                    res[item] = nums[i];
+                }
+                if (stack.Count == 0 || (stack.Count == 1 && nums[stack.Peek()] == max))
+                {
+                    return res;
+                }
+            }
+
+            return res;
+        }
+    }
+}
