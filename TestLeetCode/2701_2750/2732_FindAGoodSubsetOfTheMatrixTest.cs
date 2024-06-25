af85a75398763d10948f8e01e1bd374bac74e265
hongtao_luo <luohongtao621@hotmail.com>
2024-06-25 13:51:56 +0800

    2732

---
+++ TestLeetCode/2701_2750/2732_FindAGoodSubsetOfTheMatrixTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2732_FindAGoodSubsetOfTheMatrixTest
+    {
+        _2732_FindAGoodSubsetOfTheMatrixAlg alg = new _2732_FindAGoodSubsetOfTheMatrixAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 1, 1]];
+            int[] exp = [0, 1];
+            int[] res = alg.GoodSubsetofBinaryMatrix(grid).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[0]];
+            int[] exp = [0];
+            int[] res = alg.GoodSubsetofBinaryMatrix(grid).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] grid = [[1, 1, 1], [1, 1, 1]];
+            int[] exp = [];
+            int[] res = alg.GoodSubsetofBinaryMatrix(grid).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2701_2750/2732_FindAGoodSubsetOfTheMatrixAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2732_FindAGoodSubsetOfTheMatrixAlg
+    {
+        public IList<int> GoodSubsetofBinaryMatrix(int[][] grid)
+        {
+            return null;
+        }
+    }
+}
