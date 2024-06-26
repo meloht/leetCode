370cae6e5aae1cd1aca1fe85283044e49dc5b3f1
hongtao_luo <luohongtao621@hotmail.com>
2024-06-26 14:00:20 +0800

    2741

---
+++ TestLeetCode/2701_2750/2741_SpecialPermutationsTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2741_SpecialPermutationsTest
+    {
+        _2741_SpecialPermutationsAlg alg = new _2741_SpecialPermutationsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 3, 6];
+            int exp = 2;
+            int res = alg.SpecialPerm(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 3, 6];
+            int exp = 2;
+            int res = alg.SpecialPerm(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192];
+            int exp = 178290591;
+            int res = alg.SpecialPerm(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [20, 100, 50, 5, 10, 70, 7];
+            int exp = 48;
+            int res = alg.SpecialPerm(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2701_2750/2741_SpecialPermutationsAlg.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace leetCode._2701_2750
+{
+    public class _2741_SpecialPermutationsAlg
+    {
+        int Mod = 1_000_000_007;
+        public int SpecialPerm(int[] nums)
+        {
+            Dictionary<int, HashSet<int>> dict = new Dictionary<int, HashSet<int>>();
+            HashSet<int> sets = new HashSet<int>();
+
+            int ans = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                HashSet<int> set = new HashSet<int>();
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    if (nums[i] % nums[j] == 0)
+                    {
+                        set.Add(j);
+                    }
+                    else if (nums[j] % nums[i] == 0)
+                    {
+                        set.Add(j);
+                    }
+                }
+                dict.Add(i, set);
+            }
+            int[][] dp = new int[nums.Length + 1][];
+            for (int i = 0; i < nums.Length + 1; i++)
+            {
+                dp[i] = new int[nums.Length];
+                Array.Fill(dp[i], -1);
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sets.Add(i);
+                ans = (ans + Dfs(i,  sets, dict, dp) % Mod) % Mod;
+                sets.Remove(i);
+            }
+
+
+            return ans;
+        }
+        private int Dfs(int pre,  HashSet<int> sets, Dictionary<int, HashSet<int>> dict, int[][] dp)
+        {
+            if (sets.Count == dict.Count)
+                return 1;
+
+            if (dp[sets.Count][pre] != -1)
+                return dp[sets.Count][pre];
+            int ans = 0;
+            var list = dict[pre];
+            foreach (var item in list)
+            {
+                if (sets.Contains(item))
+                    continue;
+                sets.Add(item);
+
+                ans = (ans + Dfs(item, sets, dict, dp) % Mod) % Mod;
+                sets.Remove(item);
+            }
+            dp[sets.Count][pre] = ans;
+            return ans;
+        }
+    }
+}
