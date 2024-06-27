db10c08586e0952be5630a66b67ab93cd3b8b6d4
hongtao_luo <luohongtao621@hotmail.com>
2024-06-27 13:52:39 +0800

    2734

---
+++ TestLeetCode/2701_2750/2734_LexicographicallySmallestStringAfterSubstringOperationTest.cs
@@ -0,0 +1,49 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2734_LexicographicallySmallestStringAfterSubstringOperationTest
+    {
+        _2734_LexicographicallySmallestStringAfterSubstringOperationAlg alg = new _2734_LexicographicallySmallestStringAfterSubstringOperationAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "cbabc";
+            string exp = "baabc";
+            string res = alg.SmallestString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "acbbc";
+            string exp = "abaab";
+            string res = alg.SmallestString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "leetcode";
+            string exp = "kddsbncd";
+            string res = alg.SmallestString(s);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "aa";
+            string exp = "az";
+            string res = alg.SmallestString(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2701_2750/2734_LexicographicallySmallestStringAfterSubstringOperationAlg.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2734_LexicographicallySmallestStringAfterSubstringOperationAlg
+    {
+        public string SmallestString(string s)
+        {
+            int left = -1;
+            int right = s.Length - 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != 'a' && left == -1)
+                {
+                    left = i;
+                }
+                if (s[i] != 'a')
+                {
+                    right = i;
+                }
+                else
+                {
+                    if (right != s.Length - 1)
+                    {
+                        break;
+                    }
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            if (left == -1)
+            {
+                char[] chars = s.ToCharArray();
+                chars[s.Length - 1] = 'z';
+                return new string(chars);
+            }
+            sb.Append(s.Substring(0, left));
+            for (int i = left; i <= right; i++)
+            {
+                char ch = (char)(s[i] - 1);
+                sb.Append(ch);
+            }
+            if (right != s.Length - 1)
+            {
+                sb.Append(s.Substring(right + 1));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
