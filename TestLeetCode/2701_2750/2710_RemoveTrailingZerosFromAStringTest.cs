7cdf35bc87fb546ae30e12b139fec50bd3a5e6aa
luohongtao <luohongtao621@hotmail.com>
2024-06-29 11:36:32 +0800

    2710

---
+++ TestLeetCode/2701_2750/2710_RemoveTrailingZerosFromAStringTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2710_RemoveTrailingZerosFromAStringTest
+    {
+        _2710_RemoveTrailingZerosFromAStringAlg alg = new _2710_RemoveTrailingZerosFromAStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string num = "51230100";
+            string exp = "512301";
+            string res = alg.RemoveTrailingZeros(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string num = "123";
+            string exp = "123";
+            string res = alg.RemoveTrailingZeros(num);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2701_2750/2710_RemoveTrailingZerosFromAStringAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2710_RemoveTrailingZerosFromAStringAlg
+    {
+        public string RemoveTrailingZeros(string num)
+        {
+            int count = 0;
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                if (num[i] == '0')
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (count == num.Length)
+                return string.Empty;
+            return num.Substring(0, num.Length - count);
+        }
+    }
+}
