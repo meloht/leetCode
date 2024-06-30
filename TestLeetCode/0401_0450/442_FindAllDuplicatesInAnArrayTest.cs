91683b0e0c7464b8fc403ebe87f1643427dd44e9
luohongtao <luohongtao621@hotmail.com>
2024-06-30 16:08:30 +0800

    442

---
+++ TestLeetCode/0401_0450/442_FindAllDuplicatesInAnArrayTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _442_FindAllDuplicatesInAnArrayTest
+    {
+        _442_FindAllDuplicatesInAnArrayAlg alg = new _442_FindAllDuplicatesInAnArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 3, 2, 7, 8, 2, 3, 1];
+            int[] exp = [2, 3];
+            int[] res = alg.FindDuplicates(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1, 2];
+            int[] exp = [1];
+            int[] res = alg.FindDuplicates(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1];
+            int[] exp = [];
+            int[] res = alg.FindDuplicates(nums).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0401_0450/442_FindAllDuplicatesInAnArrayAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _442_FindAllDuplicatesInAnArrayAlg
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int x = Math.Abs(nums[i]);
+                if (nums[x - 1] > 0)
+                {
+                    nums[x - 1] = -nums[x - 1];
+                }
+                else
+                {
+                    result.Add(x);
+                }
+            }
+            return result;
+        }
+    }
+}
