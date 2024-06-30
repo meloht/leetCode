bbb41ce13b867cbe8afd37ff9c9195597ba632de
luohongtao <luohongtao621@hotmail.com>
2024-06-30 13:52:18 +0800

    435

--- leetCode/0401_0450/435_NonOverlappingIntervalsAlg.cs
+++ leetCode/0401_0450/435_NonOverlappingIntervalsAlg.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using leetCode._3051_3100;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,33 @@ namespace leetCode._0401_0450
 {
     public class _435_NonOverlappingIntervalsAlg
     {
+      
+
+
         public int EraseOverlapIntervals(int[][] intervals)
         {
-            return 0;
+            if (intervals.Length == 0)
+                return 0;
+
+            Array.Sort(intervals, (x, y) => x[1].CompareTo(y[1]));
+
+            int n = intervals.Length;
+            int ans = 1;
+            int right = intervals[0][1];
+
+            for (int i = 1; i < intervals.Length; i++)
+            {
+
+                if (intervals[i][0] >= right)
+                {
+                    ans++;
+                    right = intervals[i][1];
+                }
+            }
+
+            return n - ans;
+
         }
+
     }
 }
