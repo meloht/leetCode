634ce19705c62d9b402097c3e29e5acca5372c05
luohongtao <luohongtao621@hotmail.com>
2024-07-01 20:01:40 +0800

    2065

--- leetCode/2051_2100/2065_MaximumPathQualityOfAGraphAlg.cs
+++ leetCode/2051_2100/2065_MaximumPathQualityOfAGraphAlg.cs
@@ -8,17 +8,59 @@ namespace leetCode._2051_2100
 {
     public class _2065_MaximumPathQualityOfAGraphAlg
     {
-        int MaxTime = 0;
+        int ans = 0;
+        int[] values;
+        int maxTime;
+        IList<int[]>[] g;
+        bool[] visited;
+
         public int MaximalPathQuality(int[] values, int[][] edges, int maxTime)
         {
+            this.values = values;
+            this.maxTime = maxTime;
+            int n = values.Length;
+            g = new IList<int[]>[n];
+            for (int i = 0; i < n; i++)
+            {
+                g[i] = new List<int[]>();
+            }
+            foreach (int[] edge in edges)
+            {
+                g[edge[0]].Add(new int[] { edge[1], edge[2] });
+                g[edge[1]].Add(new int[] { edge[0], edge[2] });
+            }
 
-            
-            
-            return 0;
+            visited = new bool[n];
+            visited[0] = true;
+
+            DFS(0, 0, values[0]);
+            return ans;
         }
-        private int Dfs(int[] values, int[][] edges,int cost) 
+
+        public void DFS(int u, int time, int value)
         {
-            return 0;
+            if (u == 0)
+            {
+                ans = Math.Max(ans, value);
+            }
+            foreach (int[] arr in g[u])
+            {
+                int v = arr[0], dist = arr[1];
+                if (time + dist <= maxTime)
+                {
+                    if (!visited[v])
+                    {
+                        visited[v] = true;
+                        DFS(v, time + dist, value + values[v]);
+                        visited[v] = false;
+                    }
+                    else
+                    {
+                        DFS(v, time + dist, value);
+                    }
+                }
+            }
         }
+
     }
 }
