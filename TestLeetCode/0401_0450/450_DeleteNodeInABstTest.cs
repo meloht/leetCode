a5e936b7e834d59a8b440a29f33d1e46e4ff2f51
luohongtao <luohongtao621@hotmail.com>
2024-07-02 22:19:33 +0800

    450

---
+++ TestLeetCode/0401_0450/450_DeleteNodeInABstTest.cs
@@ -0,0 +1,64 @@
+﻿using leetCode._0401_0450;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _450_DeleteNodeInABstTest
+    {
+        _450_DeleteNodeInABstAlg alg = new _450_DeleteNodeInABstAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [5, 3, 6, 2, 4, null, 7];
+            int key = 3;
+            var node = TreeNode.BuildTree(root);
+            int?[] exp = [5, 4, 6, 2, null, null, 7];
+            var tree = alg.DeleteNode(node, key);
+            int?[] res = TreeNode.GetTreeLevelList(tree);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [5, 3, 6, 2, 4, null, 7];
+            int key = 0;
+            var node = TreeNode.BuildTree(root);
+            int?[] exp = [5, 3, 6, 2, 4, null, 7];
+            var tree = alg.DeleteNode(node, key);
+            int?[] res = TreeNode.GetTreeLevelList(tree);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [];
+            int key = 0;
+            var node = TreeNode.BuildTree(root);
+            int?[] exp = [];
+            var tree = alg.DeleteNode(node, key);
+            int?[] res = TreeNode.GetTreeLevelList(tree);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int?[] root = [50, 30, 70, null, 40, 60, 80];
+            int key = 50;
+            var node = TreeNode.BuildTree(root);
+            int?[] exp = [70, 60, 80, 30, null, null, null, null, 40];
+            var tree = alg.DeleteNode(node, key);
+            int?[] res = TreeNode.GetTreeLevelList(tree);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0401_0450/450_DeleteNodeInABstAlg.cs
@@ -0,0 +1,121 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _450_DeleteNodeInABstAlg
+    {
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if (root == null)
+                return root;
+            if (root.val == key)
+            {
+                if (root.right != null)
+                {
+                    var left = root.left;
+                    var newRoot = GetLeftMin(root.right);
+                    newRoot.left = left;
+                    return root.right;
+                }
+                else
+                {
+                    return root.left;
+                }
+            }
+            bool bl = Dfs(root.left, root, true, key);
+            if (!bl)
+            {
+                Dfs(root.right, root, false, key);
+            }
+
+            return root;
+        }
+
+        private bool Dfs(TreeNode node, TreeNode parent, bool isLeft, int key)
+        {
+            if (node == null)
+                return false;
+
+            if (node.val == key)
+            {
+                if (isLeft)
+                {
+                    var leftt = node.left;
+                    var newRoot = GetLeftMin(node.right);
+                    if (newRoot != null)
+                    {
+                        newRoot.left = leftt;
+                        parent.left = node.right;
+
+                    }
+                    else
+                    {
+                        parent.left = leftt;
+                    }
+                }
+                else
+                {
+                    var rightt = node.right;
+
+                    if (node.left != null)
+                    {
+                        parent.right = node.left;
+                        var ri = GetRightMax(node.left);
+                        ri.right = rightt;
+
+                    }
+                    else
+                    {
+                        parent.right = rightt;
+                    }
+                }
+
+                return true;
+            }
+
+            bool bl = Dfs(node.left, node, true, key);
+            if (!bl)
+            {
+                return Dfs(node.right, node, false, key);
+            }
+            return bl;
+
+        }
+
+        private TreeNode GetLeftMin(TreeNode node)
+        {
+            if (node == null)
+                return null;
+
+            var left = GetLeftMin(node.left);
+            if (left != null)
+            {
+                return left;
+            }
+
+            return node;
+
+        }
+        private TreeNode GetRightMax(TreeNode node)
+        {
+            if (node == null)
+                return null;
+
+            var right = GetRightMax(node.right);
+            if (right != null)
+            {
+                return right;
+            }
+
+            return node;
+
+        }
+    }
+}
