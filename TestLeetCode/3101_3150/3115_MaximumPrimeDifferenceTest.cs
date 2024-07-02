5a81635606cc8db49168b820d30764f47e89bb80
hongtao_luo <luohongtao621@hotmail.com>
2024-07-02 13:12:37 +0800

    3115

---
+++ TestLeetCode/3101_3150/3115_MaximumPrimeDifferenceTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3115_MaximumPrimeDifferenceTest
+    {
+        _3115_MaximumPrimeDifferenceAlg alg = new _3115_MaximumPrimeDifferenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 2, 9, 5, 3];
+            int exp = 3;
+            int res = alg.MaximumPrimeDifference(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [4, 8, 2, 8];
+            int exp = 0;
+            int res = alg.MaximumPrimeDifference(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3115_MaximumPrimeDifferenceAlg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3115_MaximumPrimeDifferenceAlg
+    {
+        public int MaximumPrimeDifference(int[] nums)
+        {
+
+            int i = 0;
+            int j = nums.Length - 1;
+            bool bl = false;
+
+            while (i < j)
+            {
+                if (!IsPrime(nums[i]))
+                {
+                    i++;
+                    continue;
+                }
+                bl = true;
+                break;
+            }
+
+            while (i < j)
+            {
+
+                if (IsPrime(nums[j]) && bl)
+                {
+                    return j - i;
+                }
+                j--;
+
+            }
+            return 0;
+        }
+
+        public bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            if (n <= 3)
+                return true;
+
+            if (n % 2 == 0 || n % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
