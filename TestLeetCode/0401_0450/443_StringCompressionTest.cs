0bad41c7528888043edf6e42ecbfa5f110ab903d
hongtao_luo <luohongtao621@hotmail.com>
2024-07-02 13:59:05 +0800

    443

---
+++ TestLeetCode/0401_0450/443_StringCompressionTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _443_StringCompressionTest
+    {
+        _443_StringCompressionAlg alg = new _443_StringCompressionAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[] chars = ['a', 'a', 'b', 'b', 'c', 'c', 'c'];
+            int exp = 6;
+            int res = alg.Compress(chars);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[] chars = ['a'];
+            int exp = 1;
+            int res = alg.Compress(chars);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            char[] chars = ['a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'];
+            int exp = 4;
+            int res = alg.Compress(chars);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/443_StringCompressionAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _443_StringCompressionAlg
+    {
+        public int Compress(char[] chars)
+        {
+            int j = 0;
+            int i = 0;
+            while (i < chars.Length && j < chars.Length)
+            {
+                int cnt = 1;
+                int ch = j;
+                while (j < chars.Length - 1 && chars[j] == chars[j + 1])
+                {
+                    cnt++;
+                    j++;
+                }
+                j++;
+                chars[i] = chars[ch];
+                if (cnt > 1)
+                {
+                    i++;
+                    int idx = i;
+                    while (cnt > 0)
+                    {
+                        chars[i++] = (char)(cnt % 10 + '0');
+                        cnt /= 10;
+                    }
+                    Reverse(chars, idx, i - 1);
+                    continue;
+                }
+
+                i++;
+            }
+            return i;
+        }
+        public void Reverse(char[] chars, int left, int right)
+        {
+            while (left < right)
+            {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+    }
+}
