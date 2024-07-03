20b4f3c6ff7fd86f67a278694120d6a6777aa97b
hongtao_luo <luohongtao621@hotmail.com>
2024-07-03 13:13:48 +0800

    446

---
+++ TestLeetCode/0401_0450/446_ArithmeticSlicesIISubsequenceTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0401_0450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0401_0450
+{
+    [TestClass]
+    public class _446_ArithmeticSlicesIISubsequenceTest
+    {
+        _446_ArithmeticSlicesIISubsequenceAlg alg = new _446_ArithmeticSlicesIISubsequenceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 4, 6, 8, 10];
+            int exp = 7;
+            int res=alg.NumberOfArithmeticSlices(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [7, 7, 7, 7, 7];
+            int exp = 16;
+            int res = alg.NumberOfArithmeticSlices(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0401_0450/446_ArithmeticSlicesIISubsequenceAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0401_0450
+{
+    public class _446_ArithmeticSlicesIISubsequenceAlg
+    {
+        public int NumberOfArithmeticSlices(int[] nums)
+        {
+
+            return 0;
+        }
+    }
+}
