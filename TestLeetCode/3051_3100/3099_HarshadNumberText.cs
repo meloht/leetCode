c6538efadda923039e1bbafe2cce52c4d9169b70
hongtao_luo <luohongtao621@hotmail.com>
2024-07-03 12:36:14 +0800

    3099

---
+++ TestLeetCode/3051_3100/3099_HarshadNumberText.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3051_3100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3051_3100
+{
+    [TestClass]
+    public class _3099_HarshadNumberText
+    {
+        _3099_HarshadNumberAlg alg = new _3099_HarshadNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int x = 18;
+            int exp = 9;
+            int res = alg.SumOfTheDigitsOfHarshadNumber(x);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int x = 23;
+            int exp = -1;
+            int res = alg.SumOfTheDigitsOfHarshadNumber(x);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3051_3100/3099_HarshadNumberAlg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3051_3100
+{
+    public class _3099_HarshadNumberAlg
+    {
+        public int SumOfTheDigitsOfHarshadNumber(int x)
+        {
+            int sum = 0;
+            int n = x;
+            while (n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            if (x % sum == 0)
+                return sum;
+            return -1;
+        }
+    }
+}
