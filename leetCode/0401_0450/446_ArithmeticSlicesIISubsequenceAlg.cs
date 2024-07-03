9caaba1b3fed8678161d9238b871a198ae7cf498
luohongtao <luohongtao621@hotmail.com>
2024-07-03 21:15:28 +0800

    446

--- leetCode/0401_0450/446_ArithmeticSlicesIISubsequenceAlg.cs
+++ leetCode/0401_0450/446_ArithmeticSlicesIISubsequenceAlg.cs
@@ -10,8 +10,36 @@ namespace leetCode._0401_0450
     {
         public int NumberOfArithmeticSlices(int[] nums)
         {
+            int ans = 0;
+            int n = nums.Length;
+            Dictionary<long, int>[] f = new Dictionary<long, int>[n];
+            for (int i = 0; i < n; ++i)
+            {
+                f[i] = new Dictionary<long, int>();
+            }
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < i; ++j)
+                {
+                    long d = 1L * nums[i] - nums[j];
+                    int cnt = f[j].ContainsKey(d) ? f[j][d] : 0;
+                    ans += cnt;
+                    if (f[i].ContainsKey(d))
+                    {
+                        f[i][d] += cnt + 1;
+                    }
+                    else
+                    {
+                        f[i].Add(d, cnt + 1);
+                    }
+                }
+            }
+            return ans;
 
-            return 0;
+
         }
+
+
+
     }
 }
