1f65699addb52593132700755bf5b48575808c76
hongtao_luo <luohongtao621@hotmail.com>
2024-07-05 13:31:32 +0800

    3033

---
+++ TestLeetCode/3001_3050/3033_ModifyTheMatrixTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3001_3050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3001_3050
+{
+    [TestClass]
+    public class _3033_ModifyTheMatrixTest
+    {
+        _3033_ModifyTheMatrixAlg alg = new _3033_ModifyTheMatrixAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] matrix = [[1, 2, -1], [4, -1, 6], [7, 8, 9]];
+            int[][] exp = [[1, 2, 9], [4, 8, 6], [7, 8, 9]];
+            int[][] res = alg.ModifiedMatrix(matrix);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] matrix = [[1, 2, -1], [4, -1, 6], [7, 8, 9]];
+            int[][] exp = [[1, 2, 9], [4, 8, 6], [7, 8, 9]];
+            int[][] res = alg.ModifiedMatrix(matrix);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/3001_3050/3033_ModifyTheMatrixAlg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3001_3050
+{
+    public class _3033_ModifyTheMatrixAlg
+    {
+        public int[][] ModifiedMatrix(int[][] matrix)
+        {
+            int n = matrix.Length;
+            int m = matrix[0].Length;
+            int[] maxCol = new int[m];
+
+            for (int i = 0; i < m; i++)
+            {
+                int max = int.MinValue;
+                for (int j = 0; j < n; j++)
+                {
+                    max = Math.Max(max, matrix[j][i]);
+                }
+                maxCol[i] = max;
+            }
+            int[][] ans = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                ans[i] = new int[m];
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i][j] == -1)
+                    {
+                        ans[i][j] = maxCol[j];
+                    }
+                    else
+                    {
+                        ans[i][j]=matrix[i][j];
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
