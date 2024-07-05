3ef9693d33b97825b20daa69150e7adf175cde7c
hongtao luo <luohongtao621@163.com>
2024-07-05 22:01:56 +0800

    453

---
+++ TestLeetCode/0451_0500/453_MinimumMovesToEqualArrayElementsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _453_MinimumMovesToEqualArrayElementsTest
+    {
+        _453_MinimumMovesToEqualArrayElementsAlg alg = new _453_MinimumMovesToEqualArrayElementsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3];
+            int exp = 3;
+            int res = alg.MinMoves(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1, 1];
+            int exp = 0;
+            int res = alg.MinMoves(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/453_MinimumMovesToEqualArrayElementsAlg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _453_MinimumMovesToEqualArrayElementsAlg
+    {
+        public int MinMoves(int[] nums)
+        {
+            int min=nums.Min();
+            int res = 0;
+            foreach (var item in nums)
+            {
+                res += item - min;
+            }
+
+            return res;
+        }
+    }
+}
