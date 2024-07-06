92d509831b62f847cea9f2cacd0bc1688bdef8fb
hongtao luo <luohongtao621@163.com>
2024-07-06 13:11:19 +0800

    3101

---
+++ TestLeetCode/3101_3150/3101_CountAlternatingSubarraysTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3101_CountAlternatingSubarraysTest
+    {
+        _3101_CountAlternatingSubarraysAlg alg = new _3101_CountAlternatingSubarraysAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [0, 1, 1, 1];
+            long exp = 5;
+            long res = alg.CountAlternatingSubarrays(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 0, 1, 0];
+            long exp = 10;
+            long res = alg.CountAlternatingSubarrays(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3101_CountAlternatingSubarraysAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3101_CountAlternatingSubarraysAlg
+    {
+        public long CountAlternatingSubarrays(int[] nums)
+        {
+            long ans = 1;
+            long len = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    len++;
+                }
+                else
+                {
+                    len = 1;
+                }
+                ans += len;
+            }
+            return ans;
+        }
+    }
+}
