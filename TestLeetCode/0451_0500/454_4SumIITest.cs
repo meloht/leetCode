9759ba1755e50ac0c4eeba6328ac09c10c479ac4
hongtao luo <luohongtao621@163.com>
2024-07-06 14:00:08 +0800

    454

---
+++ TestLeetCode/0451_0500/454_4SumIITest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _454_4SumIITest
+    {
+        _454_4SumIIAlg alg = new _454_4SumIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [1, 2], nums2 = [-2, -1], nums3 = [-1, 2], nums4 = [0, 2];
+            int exp = 2;
+            int res = alg.FourSumCount(nums1, nums2, nums3, nums4);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0];
+            int exp = 1;
+            int res = alg.FourSumCount(nums1, nums2, nums3, nums4);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/454_4SumIIAlg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _454_4SumIIAlg
+    {
+        public int FourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4)
+        {
+            Dictionary<long, int> dict1 = GetDict(nums1, nums2);
+            Dictionary<long, int> dict2 = GetDict(nums3, nums4);
+            int ans = 0;
+            foreach (var item in dict1)
+            {
+                long val = -item.Key;
+                if (dict2.ContainsKey(val))
+                {
+                    ans += (item.Value * dict2[val]);
+                }
+            }
+
+
+            return ans;
+        }
+
+        private Dictionary<long, int> GetDict(int[] nums1, int[] nums2)
+        {
+            Dictionary<long, int> dict = new Dictionary<long, int>();
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    long val = nums1[i] + nums2[j];
+                    if (dict.ContainsKey(val))
+                    {
+                        dict[val]++;
+                    }
+                    else
+                    {
+                        dict.Add(val, 1);
+                    }
+                }
+            }
+            return dict;
+        }
+    }
+}
