98b32109e8e3014a378008c922a8ff7fadb5a5d6
hongtao luo <luohongtao621@163.com>
2024-07-06 16:50:15 +0800

    456

---
+++ TestLeetCode/0451_0500/456_132PatternTest.cs
@@ -0,0 +1,87 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _456_132PatternTest
+    {
+        _456_132PatternAlg alg = new _456_132PatternAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 4];
+            bool exp = false;
+            bool res = alg.Find132pattern(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [3, 1, 4, 2];
+            bool exp = true;
+            bool res = alg.Find132pattern(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [-1, 3, 2, 0];
+            bool exp = true;
+            bool res = alg.Find132pattern(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [1, 0, 1, -4, -3];
+            bool exp = false;
+            bool res = alg.Find132pattern(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] nums = [1, 3, 2, 4, 5, 6, 7, 8, 9, 10];
+            bool exp = true;
+            bool res = alg.Find132pattern(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            int[] nums = [3, 5, 0, 3, 4];
+            bool exp = true;
+            bool res = alg.Find132pattern(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase07()
+        {
+            int[] nums = [-2, 1, 2, -2, 1, 2];
+            bool exp = true;
+            bool res = alg.Find132pattern(nums);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase08()
+        {
+            int[] nums = [2, 3, 1, 2];
+            bool exp = false;
+            bool res = alg.Find132pattern(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/456_132PatternAlg.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _456_132PatternAlg
+    {
+        public bool Find132pattern1(int[] nums)
+        {
+            int[] leftMin = new int[nums.Length];
+            int min = nums[0];
+            leftMin[0] = min;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                min = Math.Min(min, nums[i]);
+                leftMin[i] = min;
+            }
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>();
+            for (int i = nums.Length - 1, j = nums.Length; i >= 0; i--, j--)
+            {
+                if (j < nums.Length)
+                {
+                    while (queue.Count > 0 && queue.Peek() <= leftMin[i])
+                    {
+                        queue.Dequeue();
+                    }
+                    if (nums[j] > leftMin[i])
+                    {
+                        queue.Enqueue(nums[j], nums[j]);
+                    }
+                }
+
+                if (i - 1 >= 0 && queue.Count > 0 && nums[i] > queue.Peek())
+                    return true;
+
+
+            }
+            return false;
+        }
+
+        public bool Find132pattern(int[] nums)
+        {
+            int n = nums.Length;
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(nums[n - 1]);
+
+            int max = int.MinValue;
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (nums[i] < max)
+                {
+                    return true;
+                }
+                while (stack.Count > 0 && nums[i] > stack.Peek())
+                {
+                    max = stack.Pop();
+                }
+                if (nums[i] > max)
+                {
+                    stack.Push(nums[i]);
+                }
+            }
+            return false;
+        }
+    }
+}
