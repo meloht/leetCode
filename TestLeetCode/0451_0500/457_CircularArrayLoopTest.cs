32d61d0496952eb4bcb06ae6000459b4b336ea5d
hongtao luo <luohongtao621@163.com>
2024-07-07 17:04:15 +0800

    457

---
+++ TestLeetCode/0451_0500/457_CircularArrayLoopTest.cs
@@ -0,0 +1,59 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _457_CircularArrayLoopTest
+    {
+        _457_CircularArrayLoopAlg alg = new _457_CircularArrayLoopAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, -1, 1, 2, 2];
+            bool exp = true;
+            bool res = alg.CircularArrayLoop(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [-1, 2];
+            bool exp = false;
+            bool res = alg.CircularArrayLoop(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [-2, 1, -1, -2, -2];
+            bool exp = false;
+            bool res = alg.CircularArrayLoop(nums);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [1, -1, 5, 1, 4];
+            bool exp = true;
+            bool res = alg.CircularArrayLoop(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] nums = [1, 2, 3, 4, 5];
+            bool exp = true;
+            bool res = alg.CircularArrayLoop(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/457_CircularArrayLoopAlg.cs
@@ -0,0 +1,57 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _457_CircularArrayLoopAlg
+    {
+        public bool CircularArrayLoop(int[] nums)
+        {
+            int n = nums.Length;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                    continue;
+
+                int slow = i;
+                int fast = Next(nums, i);
+                while (nums[slow] * nums[fast] > 0 && nums[slow] * nums[Next(nums, fast)] > 0)
+                {
+                    if (slow == fast)
+                    {
+                        if (slow != Next(nums, slow))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    slow = Next(nums, slow);
+                    fast = Next(nums, Next(nums, fast));
+                }
+                int add = i;
+                while (nums[add] * nums[Next(nums, add)] > 0)
+                {
+                    int temp = add;
+                    add = Next(nums, add);
+                    nums[temp] = 0;
+                }
+            }
+
+
+            return false;
+        }
+        private int Next(int[] nums, int cur)
+        {
+            int n = nums.Length;
+            return ((cur + nums[cur]) % n + n) % n;
+        }
+    }
+}
