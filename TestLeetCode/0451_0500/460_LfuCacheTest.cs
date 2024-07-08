294e4e9e576205fd638ffb3b05f35dbb625ab1bb
hongtao luo <luohongtao621@163.com>
2024-07-08 22:50:52 +0800

    460

---
+++ TestLeetCode/0451_0500/460_LfuCacheTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0451_0500._460_LfuCacheAlg;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _460_LfuCacheTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            LFUCache lfu = new LFUCache(2);
+            lfu.Put(1, 1);   // cache=[1,_], cnt(1)=1
+            lfu.Put(2, 2);   // cache=[2,1], cnt(2)=1, cnt(1)=1
+            int res = lfu.Get(1);      // 返回 1
+            Assert.AreEqual(1, res);
+            // cache=[1,2], cnt(2)=1, cnt(1)=2
+            lfu.Put(3, 3);   // 去除键 2 ，因为 cnt(2)=1 ，使用计数最小
+                             // cache=[3,1], cnt(3)=1, cnt(1)=2
+            res = lfu.Get(2);      // 返回 -1（未找到）
+            Assert.AreEqual(-1, res);
+            res = lfu.Get(3);      // 返回 3
+            Assert.AreEqual(3, res); // cache=[3,1], cnt(3)=2, cnt(1)=2
+
+            lfu.Put(4, 4);   // 去除键 1 ，1 和 3 的 cnt 相同，但 1 最久未使用
+                             // cache=[4,3], cnt(4)=1, cnt(3)=2
+            res = lfu.Get(1);      // 返回 -1（未找到）
+            Assert.AreEqual(-1, res);
+            res = lfu.Get(3);      // 返回 3
+            Assert.AreEqual(3, res);                // cache=[3,4], cnt(4)=1, cnt(3)=3
+            res = lfu.Get(4);      // 返回 4  // cache=[3,4], cnt(4)=2, cnt(3)=3
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/460_LfuCacheAlg.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0451_0500._460_LfuCacheAlg;
+
+namespace leetCode._0451_0500
+{
+    public class _460_LfuCacheAlg
+    {
+        public class LFUCache
+        {
+            private int Capacity = 0;
+
+            private Dictionary<int, LinkedListNode<Data>> dict = new Dictionary<int, LinkedListNode<Data>>();
+            private Dictionary<int, LinkedListNode<LinkedList<Data>>> dictCount = new Dictionary<int, LinkedListNode<LinkedList<Data>>>();
+
+            LinkedList<LinkedList<Data>> countLink = new LinkedList<LinkedList<Data>>();
+
+            public LFUCache(int capacity)
+            {
+                this.Capacity = capacity;
+
+            }
+
+            public int Get(int key)
+            {
+                if (dict.ContainsKey(key))
+                {
+                    var node = dict[key];
+                    AddCount(node, key);
+                    return node.Value.Val;
+                }
+                return -1;
+            }
+
+            public void Put(int key, int value)
+            {
+                if (dict.Count == this.Capacity && !dict.ContainsKey(key))
+                {
+                    if (countLink.Last != null)
+                    {
+                        if (countLink.Last.Value.Last != null)
+                        {
+                            var last = countLink.Last.Value.Last;
+                            countLink.Last.Value.RemoveLast();
+                            dict.Remove(last.Value.Key);
+
+                            if (countLink.Last.Value.Count == 0)
+                            {
+                                var last1 = countLink.Last;
+                                countLink.RemoveLast();
+                                dictCount.Remove(last.Value.Count);
+                            }
+                        }
+                    }
+                }
+                Data data = new Data { Count = 1, Val = value, Key = key };
+
+                if (dict.ContainsKey(key))
+                {
+                    var node = dict[key];
+                    node.Value.Val = value;
+                    AddCount(node, key);
+
+                }
+                else
+                {
+                    if (!dictCount.ContainsKey(1))
+                    {
+                        LinkedList<Data> nodeList = new LinkedList<Data>();
+                        var node = nodeList.AddLast(data);
+                        dict.Add(key, node);
+
+                        var link = countLink.AddLast(nodeList);
+                        dictCount.Add(1, link);
+                    }
+                    else
+                    {
+                        var node = dictCount[1].Value.AddLast(data);
+                        dict.Add(key, node);
+                    }
+                }
+            }
+
+            private void AddCount(LinkedListNode<Data> node, int key)
+            {
+                var link = dictCount[node.Value.Count];
+                link.Value.Remove(node);
+                if (link.Value.Count == 0)
+                {
+                    dictCount.Remove(node.Value.Count);
+                    countLink.Remove(link);
+                }
+                node.Value.Count++;
+                if (dictCount.ContainsKey(node.Value.Count))
+                {
+                    var linkNode = dictCount[node.Value.Count];
+                    var newNode = linkNode.Value.AddLast(node.Value);
+                    dict[key] = newNode;
+                }
+                else
+                {
+                    LinkedList<Data> nodeList = new LinkedList<Data>();
+                    var newNode = nodeList.AddLast(node.Value);
+                    dict[key] = newNode;
+
+                    var link1 = countLink.AddFirst(nodeList);
+                    dictCount.Add(node.Value.Count, link1);
+                }
+            }
+        }
+
+        public class Data
+        {
+            public int Key { get; set; }
+            public int Count { get; set; }
+            public int Val { get; set; }
+        }
+        //public class Node
+        //{
+        //    public Node Prev { get; set; }
+        //    public Node Next { get; set; }
+
+
+
+        //    public Node Insert(Node node)
+        //    {  // 在 this 后插入 node
+        //        node.Prev = this;
+        //        node.Next = this.Next;
+        //        node.Prev.Next = node;
+        //        node.Next.Prev = node;
+        //        return node;
+        //    }
+
+        //    public void Remove()
+        //    {
+        //        this.Prev.Next = this.Next;
+        //        this.Next.Prev = this.Prev;
+        //    }
+
+        //}
+
+
+
+
+    }
+}
