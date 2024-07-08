b9e00409285e6f2f045cbf704f3ed3cac78c891b
hongtao_luo <luohongtao621@hotmail.com>
2024-07-08 13:48:20 +0800

    设计

---
+++ leetCodeTemplates/设计/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCodeTemplates.设计
+{
+    internal class Class1
+    {
+    }
+}
