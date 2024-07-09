5a798865e24c5a840da102430d3524976f231757
hongtao_luo <luohongtao621@hotmail.com>
2024-07-09 14:10:16 +0800

    3102

---
+++ TestLeetCode/3101_3150/3102_MinimizeManhattanDistancesTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3102_MinimizeManhattanDistancesTest
+    {
+        _3102_MinimizeManhattanDistancesAlg alg = new _3102_MinimizeManhattanDistancesAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] points = [[3, 10], [5, 15], [10, 2], [4, 4]];
+            int exp = 12;
+            int res = alg.MinimumDistance(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] points = [[1, 1], [1, 1], [1, 1]];
+            int exp = 0;
+            int res = alg.MinimumDistance(points);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3102_MinimizeManhattanDistancesAlg.cs
@@ -0,0 +1,85 @@
+﻿using leetCode._3051_3100;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3102_MinimizeManhattanDistancesAlg
+    {
+        public int MinimumDistance(int[][] points)
+        {
+            SortedList<int, int> xs = new SortedList<int, int>();
+            SortedList<int, int> ys = new SortedList<int, int>();
+
+
+            foreach (var item in points)
+            {
+                int x = item[0] + item[1];
+                int y = item[1] - item[0];
+               
+                if (xs.ContainsKey(x))
+                {
+                    xs[x]++;
+                }
+                else
+                {
+                    xs.Add(x, 1);
+                }
+
+                if (ys.ContainsKey(y))
+                {
+                    ys[y]++;
+                }
+                else
+                {
+                    ys.Add(y, 1);
+                }
+            }
+
+
+            int ans = int.MaxValue;
+            foreach (var item in points)
+            {
+                int x = item[0] + item[1];
+                int y = item[1] - item[0];
+                xs[x]--;
+                if (xs[x] == 0)
+                {
+                    xs.Remove(x);
+                }
+                ys[y]--;
+                if (ys[y] == 0)
+                {
+                    ys.Remove(y);
+                }
+
+                ans = Math.Min(ans, Math.Max(xs.Last().Key - xs.First().Key, ys.Last().Key - ys.First().Key));
+              
+
+                if (xs.ContainsKey(x))
+                {
+                    xs[x]++;
+                }
+                else
+                {
+                    xs.Add(x, 1);
+                }
+
+                if (ys.ContainsKey(y))
+                {
+                    ys[y]++;
+                }
+                else
+                {
+                    ys.Add(y, 1);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
