6df930c6112e0b69a8acf5ff79a2e5cbf3a754b9
hongtao_luo <luohongtao621@hotmail.com>
2024-07-10 13:40:13 +0800

    2970

---
+++ TestLeetCode/2951_3000/2970_CountTheNumberOfIncremovableSubarraysITest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2951_3000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2951_3000
+{
+    [TestClass]
+    public class _2970_CountTheNumberOfIncremovableSubarraysITest
+    {
+        _2970_CountTheNumberOfIncremovableSubarraysIAlg alg = new _2970_CountTheNumberOfIncremovableSubarraysIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int exp = 10;
+            int res = alg.IncremovableSubarrayCount(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [6, 5, 7, 8];
+            int exp = 7;
+            int res = alg.IncremovableSubarrayCount(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [8, 7, 6, 6];
+            int exp = 3;
+            int res = alg.IncremovableSubarrayCount(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2951_3000/2970_CountTheNumberOfIncremovableSubarraysIAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2951_3000
+{
+    public class _2970_CountTheNumberOfIncremovableSubarraysIAlg
+    {
+        public int IncremovableSubarrayCount(int[] nums)
+        {
+
+            int n = nums.Length;
+            int i = 0;
+            while (i < n - 1 && nums[i] < nums[i + 1])
+            {
+                i++;
+            }
+            if (i == n - 1)
+            {
+                return n * (n + 1) / 2;
+            }
+            int ans = i + 2;
+            for (int j = n - 1; j == n - 1 || nums[j] < nums[j + 1]; j--)
+            {
+                while (i >= 0 && nums[i] >= nums[j])
+                {
+                    i--;
+                }
+                ans += i + 2;
+            }
+
+            return ans;
+        }
+    }
+}
