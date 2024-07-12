4b40a7497d70ed99b897ee57813d12cd594525cd
hongtao_luo <luohongtao621@hotmail.com>
2024-07-12 13:20:35 +0800

    2974

---
+++ TestLeetCode/2951_3000/2974_MinimumNumberGameTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._2951_3000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2951_3000
+{
+    [TestClass]
+    public class _2974_MinimumNumberGameTest
+    {
+        _2974_MinimumNumberGameAlg alg = new _2974_MinimumNumberGameAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [5, 4, 2, 3];
+            int[] exp = [3, 2, 5, 4];
+            int[] res = alg.NumberGame(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 5];
+            int[] exp = [5, 2];
+            int[] res = alg.NumberGame(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2951_3000/2974_MinimumNumberGameAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2951_3000
+{
+    public class _2974_MinimumNumberGameAlg
+    {
+        public int[] NumberGame(int[] nums)
+        {
+            int[] arr = new int[nums.Length];
+            Array.Sort(nums);
+            int j = 0;
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                int n1 = nums[i];
+                int n2 = nums[i + 1];
+
+                arr[j++] = n2;
+                arr[j++] = n1;
+            }
+            return arr;
+        }
+    }
+}
