d0948e81f38884981ae5ac55efeb4c353c3acbff
hongtao_luo <luohongtao621@hotmail.com>
2024-07-15 12:21:25 +0800

    462

---
+++ TestLeetCode/0451_0500/462_MinimumMovesToEqualArrayElementsIITest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _462_MinimumMovesToEqualArrayElementsIITest
+    {
+        _462_MinimumMovesToEqualArrayElementsIIAlg alg = new _462_MinimumMovesToEqualArrayElementsIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3];
+            int exp = 2;
+            int res = alg.MinMoves2(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 10, 2, 9];
+            int exp = 16;
+            int res = alg.MinMoves2(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/462_MinimumMovesToEqualArrayElementsIIAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _462_MinimumMovesToEqualArrayElementsIIAlg
+    {
+        public int MinMoves2(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
