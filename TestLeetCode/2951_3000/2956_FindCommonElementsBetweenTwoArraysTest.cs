8354b0fb8ad0fd3c6414e570ad43323c753db8a3
hongtao_luo <luohongtao621@hotmail.com>
2024-07-16 12:18:05 +0800

    2956

---
+++ TestLeetCode/2951_3000/2956_FindCommonElementsBetweenTwoArraysTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2951_3000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2951_3000
+{
+    [TestClass]
+    public class _2956_FindCommonElementsBetweenTwoArraysTest
+    {
+        _2956_FindCommonElementsBetweenTwoArraysAlg alg = new _2956_FindCommonElementsBetweenTwoArraysAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [2, 3, 2], nums2 = [1, 2];
+            int[] exp = [2, 1];
+            int[] res = alg.FindIntersectionValues(nums1, nums2);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [4, 3, 2, 3, 1], nums2 = [2, 2, 5, 2, 3, 6];
+            int[] exp = [3, 4];
+            int[] res = alg.FindIntersectionValues(nums1, nums2);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums1 = [3, 4, 2, 3], nums2 = [1, 5];
+            int[] exp = [0, 0];
+            int[] res = alg.FindIntersectionValues(nums1, nums2);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2951_3000/2956_FindCommonElementsBetweenTwoArraysAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2951_3000
+{
+    public class _2956_FindCommonElementsBetweenTwoArraysAlg
+    {
+        public int[] FindIntersectionValues(int[] nums1, int[] nums2)
+        {
+            var dict1 = nums1.ToHashSet();
+            var dict2 = nums2.ToHashSet();
+
+            int ans1 = 0;
+            int ans2 = 0;
+
+            foreach (var item in nums1)
+            {
+                if (dict2.Contains(item)) 
+                {
+                    ans1++;
+                }
+            }
+            foreach (var item in nums2)
+            {
+                if (dict1.Contains(item))
+                {
+                    ans2++;
+                }
+            }
+
+            return [ans1, ans2];
+        }
+    }
+}
