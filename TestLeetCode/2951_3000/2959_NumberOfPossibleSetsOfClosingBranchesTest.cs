85901afa211151453737211646c8c7d75769f7d2
hongtao_luo <luohongtao621@hotmail.com>
2024-07-17 14:19:08 +0800

    2959

---
+++ TestLeetCode/2951_3000/2959_NumberOfPossibleSetsOfClosingBranchesTest.cs
@@ -0,0 +1,54 @@
+﻿using leetCode._2951_3000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2951_3000
+{
+    [TestClass]
+    public class _2959_NumberOfPossibleSetsOfClosingBranchesTest
+    {
+        _2959_NumberOfPossibleSetsOfClosingBranchesAlg alg = new _2959_NumberOfPossibleSetsOfClosingBranchesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3, maxDistance = 5;
+            int[][] roads = [[0, 1, 2], [1, 2, 10], [0, 2, 10]];
+            int exp = 5;
+            int res = alg.NumberOfSets(n, maxDistance, roads);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 3, maxDistance = 5;
+            int[][] roads = [[0, 1, 20], [0, 1, 10], [1, 2, 2], [0, 2, 2]];
+            int exp = 7;
+            int res = alg.NumberOfSets(n, maxDistance, roads);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 1, maxDistance = 10;
+            int[][] roads = [];
+            int exp = 2;
+            int res = alg.NumberOfSets(n, maxDistance, roads);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int n = 4, maxDistance = 17;
+            int[][] roads = [[2, 1, 28], [2, 0, 6], [1, 0, 28], [1, 0, 24], [1, 0, 18], [1, 0, 25], [0, 3, 10]];
+            int exp = 8;
+            int res = alg.NumberOfSets(n, maxDistance, roads);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2951_3000/2959_NumberOfPossibleSetsOfClosingBranchesAlg.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._2951_3000
+{
+    public class _2959_NumberOfPossibleSetsOfClosingBranchesAlg
+    {
+        public int NumberOfSets(int n, int maxDistance, int[][] roads)
+        {
+            int[][] g = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] row = new int[n];
+                Array.Fill(row, int.MaxValue / 2); // 防止加法溢出
+                g[i] = row;
+            }
+            foreach (int[] e in roads)
+            {
+                int x = e[0];
+                int y = e[1];
+                int wt = e[2];
+                g[x][y] = Math.Min(g[x][y], wt);
+                g[y][x] = Math.Min(g[y][x], wt);
+            }
+
+            int[][] f = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                f[i] = new int[n];
+            }
+            int ans = 0;
+            for (int s = 0; s < (1 << n); s++)
+            { // 枚举子集
+                ans += Check(s, n, f, g, maxDistance);
+            }
+            return ans;
+
+        }
+
+        private int Check(int s, int n, int[][] f, int[][] g, int maxDistance)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if ((s >> i & 1) == 1)
+                {
+                   
+                    Array.Copy(g[i], 0, f[i], 0, n);
+                }
+            }
+
+            // Floyd 算法（只考虑在 s 中的节点）
+            for (int k = 0; k < n; k++)
+            {
+                if (((s >> k) & 1) == 0) 
+                    continue;
+                for (int i = 0; i < n; i++)
+                {
+                    if (((s >> i) & 1) == 0)
+                        continue;
+                    for (int j = 0; j < n; j++)
+                    {
+                        f[i][j] = Math.Min(f[i][j], f[i][k] + f[k][j]);
+                    }
+                }
+            }
+
+            // 判断保留的节点之间的最短路是否均不超过 maxDistance
+            for (int i = 0; i < n; i++)
+            {
+                if (((s >> i) & 1) == 0) 
+                    continue;
+                for (int j = 0; j < i; j++)
+                {
+                    if (((s >> j) & 1) == 1 && f[i][j] > maxDistance)
+                    {
+                        return 0;
+                    }
+                }
+            }
+            return 1;
+
+        }
+    }
+}
