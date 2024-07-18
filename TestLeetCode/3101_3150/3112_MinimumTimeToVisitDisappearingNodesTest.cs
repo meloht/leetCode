d93437bf60b86751ffdc1f11d716abe706828b10
hongtao_luo <luohongtao621@hotmail.com>
2024-07-18 13:32:57 +0800

    3112

---
+++ TestLeetCode/3101_3150/3112_MinimumTimeToVisitDisappearingNodesTest.cs
@@ -0,0 +1,48 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3112_MinimumTimeToVisitDisappearingNodesTest
+    {
+        _3112_MinimumTimeToVisitDisappearingNodesAlg alg = new _3112_MinimumTimeToVisitDisappearingNodesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3;int[][] edges = [[0, 1, 2], [1, 2, 1], [0, 2, 4]];
+            int[] disappear = [1, 1, 5];
+            int[] exp = [0, -1, 4];
+            int[] res = alg.MinimumTime(n, edges, disappear);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 3; int[][] edges = [[0, 1, 2], [1, 2, 1], [0, 2, 4]];
+            int[] disappear = [1, 3, 5];
+            int[] exp = [0, 2, 3];
+            int[] res = alg.MinimumTime(n, edges, disappear);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 2; int[][] edges = [[0, 1, 1]];
+            int[] disappear = [1, 1];
+            int[] exp = [0, -1];
+            int[] res = alg.MinimumTime(n, edges, disappear);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+    }
+}
---
+++ leetCode/3101_3150/3112_MinimumTimeToVisitDisappearingNodesAlg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3112_MinimumTimeToVisitDisappearingNodesAlg
+    {
+        public int[] MinimumTime(int n, int[][] edges, int[] disappear)
+        {
+            List<int[]>[] g = new List<int[]>[n];
+            for (int i = 0; i < n; i++)
+            {
+                g[i] = new List<int[]>();
+            }
+            foreach (int[] e in edges)
+            {
+                int x = e[0];
+                int y = e[1];
+                int wt = e[2];
+                g[x].Add([y, wt]);
+                g[y].Add([x, wt]);
+            }
+            int[] dis = new int[n];
+            Array.Fill(dis, -1);
+            dis[0] = 0;
+            PriorityQueue<int[], int> pq = new PriorityQueue<int[], int>();
+            pq.Enqueue([0, 0], 0);
+            while (pq.Count > 0) 
+            {
+                int[] p=pq.Dequeue();
+                int dx = p[0];
+                int x = p[1];
+                if (dx > dis[x])
+                {
+                    continue;
+                }
+
+                foreach (int[] e in g[x])
+                {
+                    int y = e[0];
+                    int newdis = dx + e[1];
+                    if (newdis < disappear[y] && (dis[y] < 0 || newdis < dis[y]))
+                    {
+                        dis[y] = newdis;
+                        pq.Enqueue([newdis, y], newdis);
+                    }
+                }
+            }
+            return dis;
+        }
+    }
+}
