2e717da1895f3b2ff26dd834db7664804ee4a257
hongtao_luo <luohongtao621@hotmail.com>
2024-07-19 13:40:15 +0800

    3096

---
+++ TestLeetCode/3051_3100/3096_MinimumLevelsToGainMorePointsTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._3051_3100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3051_3100
+{
+    [TestClass]
+    public class _3096_MinimumLevelsToGainMorePointsTest
+    {
+        _3096_MinimumLevelsToGainMorePointsAlg alg = new _3096_MinimumLevelsToGainMorePointsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] possible = [1, 0, 1, 0];
+            int exp = 1;
+            int res = alg.MinimumLevels(possible);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] possible = [1, 1, 1, 1, 1];
+            int exp = 3;
+            int res = alg.MinimumLevels(possible);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] possible = [0, 0];
+            int exp = -1;
+            int res = alg.MinimumLevels(possible);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] possible = [1, 1];
+            int exp = -1;
+            int res = alg.MinimumLevels(possible);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3051_3100/3096_MinimumLevelsToGainMorePointsAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3051_3100
+{
+    public class _3096_MinimumLevelsToGainMorePointsAlg
+    {
+        public int MinimumLevels(int[] possible)
+        {
+            int[] sums = new int[possible.Length];
+            sums[0] = possible[0];
+            int n = possible.Length;
+            for (int i = 1; i < n; i++)
+            {
+                sums[i] = sums[i - 1] + possible[i];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int n1 = sums[i];
+                int s1 = n1 - (i + 1 - n1);
+                int n2 = sums[n - 1] - sums[i];
+
+                int s2 = n2 - (n - i - 1 - n2);
+                if (s1 > s2 && i + 1 < n)
+                {
+                    return i + 1;
+                }
+            }
+
+
+            return -1;
+        }
+
+
+    }
+}
