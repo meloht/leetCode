464de3751345572b6091692c4fdd8b155c48279b
luohongtao <luohongtao621@hotmail.com>
2024-07-20 14:02:59 +0800

    2850

---
+++ TestLeetCode/2801_2850/2850_MinimumMovesToSpreadStonesOverGridTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2801_2850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2801_2850
+{
+    [TestClass]
+    public class _2850_MinimumMovesToSpreadStonesOverGridTest
+    {
+        _2850_MinimumMovesToSpreadStonesOverGridAlg alg = new _2850_MinimumMovesToSpreadStonesOverGridAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[1, 1, 0], [1, 1, 1], [1, 2, 1]];
+            int exp = 3;
+            int res = alg.MinimumMoves(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] grid = [[1, 3, 0], [1, 0, 0], [1, 0, 3]];
+            int exp = 4;
+            int res = alg.MinimumMoves(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2801_2850/2850_MinimumMovesToSpreadStonesOverGridAlg.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2801_2850
+{
+    public class _2850_MinimumMovesToSpreadStonesOverGridAlg
+    {
+        public int MinimumMoves(int[][] grid)
+        {
+            IList<int[]> more = new List<int[]>();
+            IList<int[]> less = new List<int[]>();
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    if (grid[i][j] > 1)
+                    {
+                        for (int k = 2; k <= grid[i][j]; ++k)
+                        {
+                            more.Add(new int[] { i, j });
+                        }
+                    }
+                    else if (grid[i][j] == 0)
+                    {
+                        less.Add(new int[] { i, j });
+                    }
+                }
+            }
+
+            int ans = int.MaxValue;
+            do
+            {
+                int steps = 0;
+                for (int i = 0; i < more.Count; i++)
+                {
+                    steps += Math.Abs(less[i][0] - more[i][0]) + Math.Abs(less[i][1] - more[i][1]);
+                }
+                ans = Math.Min(ans, steps);
+            } while (NextPermutation(more));
+            return ans;
+        }
+
+        public bool NextPermutation(IList<int[]> more)
+        {
+            int p = -1;
+            for (int idx = 0; idx < more.Count - 1; idx++)
+            {
+                if (IsLess(more[idx], more[idx + 1]))
+                {
+                    p = idx;
+                }
+            }
+            if (p == -1)
+            {
+                return false;
+            }
+            int q = -1;
+            for (int idx = p + 1; idx < more.Count; idx++)
+            {
+                if (IsLess(more[p], more[idx]))
+                {
+                    q = idx;
+                }
+            }
+            Swap(more, p, q);
+            int i = p + 1, j = more.Count - 1;
+            while (i < j)
+            {
+                Swap(more, i, j);
+                i++;
+                j--;
+            }
+            return true;
+        }
+
+        public bool IsLess(int[] pair1, int[] pair2)
+        {
+            return pair1[0] < pair2[0] || (pair1[0] == pair2[0] && pair1[1] < pair2[1]);
+        }
+
+        public void Swap(IList<int[]> more, int index1, int index2)
+        {
+            int[] temp = more[index1];
+            more[index1] = more[index2];
+            more[index2] = temp;
+        }
+
+    }
+}
