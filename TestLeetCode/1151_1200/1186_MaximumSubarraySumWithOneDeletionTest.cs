a46208edbef207ef1370c474ef8e8d3b10017d9b
luohongtao <luohongtao621@hotmail.com>
2024-07-21 15:39:19 +0800

    1186

---
+++ TestLeetCode/1151_1200/1186_MaximumSubarraySumWithOneDeletionTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1151_1200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1151_1200
+{
+    [TestClass]
+    public class _1186_MaximumSubarraySumWithOneDeletionTest
+    {
+        _1186_MaximumSubarraySumWithOneDeletionAlg alg = new _1186_MaximumSubarraySumWithOneDeletionAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [1, -2, 0, 3];
+            int exp = 4;
+            int res = alg.MaximumSum(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [1, -2, -2, 3];
+            int exp = 3;
+            int res = alg.MaximumSum(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] arr = [-1, -1, -1, -1];
+            int exp = -1;
+            int res = alg.MaximumSum(arr);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1151_1200/1186_MaximumSubarraySumWithOneDeletionAlg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._1151_1200
+{
+    public class _1186_MaximumSubarraySumWithOneDeletionAlg
+    {
+        public int MaximumSum(int[] arr)
+        {
+            int n = arr.Length;
+            int ans = int.MinValue;
+            int[][] f = new int[n + 1][];
+            for (int i = 0; i < f.Length; i++)
+            {
+                f[i] = [int.MinValue / 2, int.MinValue / 2];
+            }
+            // 除 2 防止负数相加溢出
+            for (int i = 0; i < n; i++)
+            {
+                f[i + 1][0] = Math.Max(f[i][0], 0) + arr[i];
+                f[i + 1][1] = Math.Max(f[i][1] + arr[i], f[i][0]);
+                ans = Math.Max(ans, Math.Max(f[i + 1][0], f[i + 1][1]));
+            }
+            return ans;
+
+        }
+
+        public int MaximumSum1(int[] arr)
+        {
+            int ans = int.MinValue / 2;
+            int f0 = ans;
+            int f1 = ans;
+            foreach (int x in arr)
+            {
+                f1 = Math.Max(f1 + x, f0);
+                f0 = Math.Max(f0, 0) + x;
+                ans = Math.Max(ans, Math.Max(f0, f1));
+            }
+            return ans;
+        }
+
+    }
+}
