771e9e57d898abd29d0f9c66384fd7a9bed3b864
hongtao_luo <luohongtao621@hotmail.com>
2024-07-22 13:59:15 +0800

    2101

---
+++ TestLeetCode/2101_2150/2101_DetonateTheMaximumBombsTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._2101_2150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2101_2150
+{
+    [TestClass]
+    public class _2101_DetonateTheMaximumBombsTest
+    {
+        _2101_DetonateTheMaximumBombsAlg alg = new _2101_DetonateTheMaximumBombsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] bombs = [[2, 1, 3], [6, 1, 4]];
+            int exp = 2;
+            int res = alg.MaximumDetonation(bombs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] bombs = [[1, 1, 5], [10, 10, 5]];
+            int exp = 1;
+            int res = alg.MaximumDetonation(bombs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] bombs = [[1, 2, 3], [2, 3, 1], [3, 4, 2], [4, 5, 3], [5, 6, 4]];
+            int exp = 5;
+            int res = alg.MaximumDetonation(bombs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] bombs = [[54, 95, 4], [99, 46, 3], [29, 21, 3], [96, 72, 8], [49, 43, 3], [11, 20, 3], [2, 57, 1], [69, 51, 7], [97, 1, 10], [85, 45, 2], [38, 47, 1], [83, 75, 3], [65, 59, 3], [33, 4, 1], [32, 10, 2], [20, 97, 8], [35, 37, 3]];
+            int exp = 1;
+            int res = alg.MaximumDetonation(bombs);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2101_2150/2101_DetonateTheMaximumBombsAlg.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2101_2150
+{
+    public class _2101_DetonateTheMaximumBombsAlg
+    {
+        public int MaximumDetonation(int[][] bombs)
+        {
+            int n = bombs.Length;
+            Dictionary<int[], List<int[]>> dictDis = new Dictionary<int[], List<int[]>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] p1 = bombs[i];
+                long x1 = p1[0];
+                long y1 = p1[1];
+                long r1 = p1[2];
+                long rr = r1 * r1;
+                dictDis.Add(p1, new List<int[]>());
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                        continue;
+                    int[] p2 = bombs[j];
+                    long x2 = p2[0];
+                    long y2 = p2[1];
+                    long r2 = p2[2];
+                  
+                    long dis = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+                    if (dis <= rr)
+                    {
+                        dictDis[p1].Add(p2);
+                    }
+                }
+            }
+
+            int ans = 0;
+            HashSet<int[]> set = new HashSet<int[]>();
+            for (int i = 0; i < n; i++)
+            {
+                int cnt = 1;
+                Queue<int[]> queue = new Queue<int[]>();
+                set.Clear();
+                queue.Enqueue(bombs[i]);
+                set.Add(bombs[i]);
+
+                while (queue.Count > 0)
+                {
+                    int count = queue.Count;
+
+                    for (int j = 0; j < count; j++)
+                    {
+                        var p = queue.Dequeue();
+                        if (dictDis[p].Count > 0)
+                        {
+                            var ls = dictDis[p];
+                            foreach (var item in ls)
+                            {
+                                if (set.Contains(item))
+                                    continue;
+                                cnt++;
+                                set.Add(item);
+                                queue.Enqueue(item);
+                            }
+                        }
+                    }
+                }
+
+                ans = Math.Max(ans, cnt);
+
+            }
+
+            return ans;
+        }
+    }
+}
