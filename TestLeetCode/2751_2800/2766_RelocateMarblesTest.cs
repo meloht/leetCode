6aa9b2bfd7f82ee6565267f7be83941c7aed54cb
hongtao_luo <luohongtao621@hotmail.com>
2024-07-24 13:17:18 +0800

    2766

---
+++ TestLeetCode/2751_2800/2766_RelocateMarblesTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._2751_2800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2751_2800
+{
+    [TestClass]
+    public class _2766_RelocateMarblesTest
+    {
+        _2766_RelocateMarblesAlg alg = new _2766_RelocateMarblesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 6, 7, 8], moveFrom = [1, 7, 2], moveTo = [2, 9, 5];
+            int[] exp = [5, 6, 8, 9];
+            int[] res = alg.RelocateMarbles(nums, moveFrom, moveTo).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1, 3, 3], moveFrom = [1, 3], moveTo = [2, 2];
+            int[] exp = [2];
+            int[] res = alg.RelocateMarbles(nums, moveFrom, moveTo).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2751_2800/2766_RelocateMarblesAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2751_2800
+{
+    public class _2766_RelocateMarblesAlg
+    {
+        public IList<int> RelocateMarbles(int[] nums, int[] moveFrom, int[] moveTo)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+            for (int i = 0; i < moveFrom.Length; i++)
+            {
+                int n = moveFrom[i];
+                if (!set.Contains(n)) 
+                {
+                    continue;
+                }
+                int to = moveTo[i];
+                set.Remove(n);
+                set.Add(to);
+            }
+            var list = set.ToList();
+            list.Sort();
+            return list;
+        }
+    }
+}
