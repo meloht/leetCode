4aa9156839a73aa6cf3baa943e4324524be47ef2
hongtao_luo <luohongtao621@hotmail.com>
2024-07-26 13:28:56 +0800

    2740

---
+++ TestLeetCode/2701_2750/2740_FindTheValueOfThePartitionTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2740_FindTheValueOfThePartitionTest
+    {
+        _2740_FindTheValueOfThePartitionAlg alg = new _2740_FindTheValueOfThePartitionAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 2, 4];
+            int exp = 1;
+            int res = alg.FindValueOfPartition(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [100, 1, 10];
+            int exp = 9;
+            int res = alg.FindValueOfPartition(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2701_2750/2740_FindTheValueOfThePartitionAlg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2740_FindTheValueOfThePartitionAlg
+    {
+        public int FindValueOfPartition(int[] nums)
+        {
+            Array.Sort(nums);
+            int ans = int.MaxValue;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int n1 = nums[i - 1];
+                int n2 = nums[i];
+                ans = Math.Min(ans, Math.Abs(n2 - n1));
+            }
+            return ans;
+        }
+    }
+}
