6763d72d8d67925deb3122aa3ad65328098a1fea
luohongtao <luohongtao621@hotmail.com>
2024-07-27 13:58:39 +0800

    3106

---
+++ TestLeetCode/3101_3150/3106_LexicographicallySmallestStringAfterOperationsWithConstraintTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3106_LexicographicallySmallestStringAfterOperationsWithConstraintTest
+    {
+        _3106_LexicographicallySmallestStringAfterOperationsWithConstraintAlg alg = new _3106_LexicographicallySmallestStringAfterOperationsWithConstraintAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "zbbz";int k = 3;
+            string exp = "aaaz";
+            string res = alg.GetSmallestString(s, k);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "xaxcd"; int k = 4;
+            string exp = "aawcd";
+            string res = alg.GetSmallestString(s, k);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "lol"; int k = 0;
+            string exp = "lol";
+            string res = alg.GetSmallestString(s, k);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/3101_3150/3106_LexicographicallySmallestStringAfterOperationsWithConstraintAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3106_LexicographicallySmallestStringAfterOperationsWithConstraintAlg
+    {
+        public string GetSmallestString(string s, int k)
+        {
+            char[] arr=s.ToCharArray();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char ch = arr[i];
+                int dis = Math.Min(ch - 'a', 'z' - ch + 1);
+                if (dis > k)
+                {
+                    arr[i] = (char)(arr[i] - k);
+                    break;
+                }
+                arr[i] = 'a';
+                k -= dis;
+            }
+            return new string(arr);
+        }
+    }
+}
