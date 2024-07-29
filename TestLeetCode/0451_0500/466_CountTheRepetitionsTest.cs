1b5ecbead1a64b4f3ea2f22a198fb3d078a21e0d
hongtao_luo <luohongtao621@hotmail.com>
2024-07-29 14:00:12 +0800

    466

---
+++ TestLeetCode/0451_0500/466_CountTheRepetitionsTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _466_CountTheRepetitionsTest
+    {
+        _466_CountTheRepetitionsAlg alg = new _466_CountTheRepetitionsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s1 = "acb";
+            int n1 = 4;
+            string s2 = "ab";
+            int n2 = 2;
+            int exp = 2;
+            int res = alg.GetMaxRepetitions(s1, n1, s2, n2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s1 = "acb";
+            int n1 = 1;
+            string s2 = "acb";
+            int n2 = 1;
+            int exp = 1;
+            int res = alg.GetMaxRepetitions(s1, n1, s2, n2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s1 = "baba";
+            int n1 = 11;
+            string s2 = "baab";
+            int n2 = 1;
+            int exp = 7;
+            int res = alg.GetMaxRepetitions(s1, n1, s2, n2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/466_CountTheRepetitionsAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _466_CountTheRepetitionsAlg
+    {
+        public int GetMaxRepetitions(string s1, int n1, string s2, int n2)
+        {
+            int[] arr1 = GetChar(s1, n1);
+            int[] arr2 = GetChar(s2, n2);
+            int ans = int.MinValue;
+            for (int i = 0; i < 26; i++)
+            {
+                int cnt2 = arr2[i];
+                if (cnt2 == 0)
+                {
+                    continue;
+                }
+                int cnt1 = arr1[i];
+                int max = cnt1 / cnt2;
+                ans = Math.Max(ans, max);
+            }
+            return ans;
+        }
+        private int[] GetChar(string s, int n)
+        {
+            int[] arr = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                int idx = s[i] - 'a';
+                arr[idx]++;
+            }
+            for (int i = 0; i < 26; i++)
+            {
+                arr[i] = arr[i] * n;
+            }
+            return arr;
+        }
+    }
+}
