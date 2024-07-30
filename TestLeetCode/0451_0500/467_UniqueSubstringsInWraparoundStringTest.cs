26c95bd6940aa781821a88db94681004bee3385b
hongtao_luo <luohongtao621@hotmail.com>
2024-07-30 16:08:10 +0800

    467

---
+++ TestLeetCode/0451_0500/467_UniqueSubstringsInWraparoundStringTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    public class _467_UniqueSubstringsInWraparoundStringTest
+    {
+        _467_UniqueSubstringsInWraparoundStringAlg alg = new _467_UniqueSubstringsInWraparoundStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "a";
+            int exp = 1;
+            int res = alg.FindSubstringInWraproundString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "cac";
+            int exp = 2;
+            int res = alg.FindSubstringInWraproundString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "zab";
+            int exp = 6;
+            int res = alg.FindSubstringInWraproundString(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/467_UniqueSubstringsInWraparoundStringAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _467_UniqueSubstringsInWraparoundStringAlg
+    {
+        public int FindSubstringInWraproundString(string s)
+        {
+
+            return 0;
+        }
+    }
+}
