7aad5979f27643e5238e28c563fea722a3e7e61f
luohongtao <luohongtao621@hotmail.com>
2024-07-31 12:04:10 +0800

    3111

---
+++ TestLeetCode/3101_3150/3111_MinimumRectanglesToCoverPointsTest.cs
@@ -0,0 +1,54 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3111_MinimumRectanglesToCoverPointsTest
+    {
+        _3111_MinimumRectanglesToCoverPointsAlg alg = new _3111_MinimumRectanglesToCoverPointsAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] points = [[2, 1], [1, 0], [1, 4], [1, 8], [3, 5], [4, 6]]; 
+            int w = 1;
+            int exp = 2;
+            int res = alg.MinRectanglesToCoverPoints(points, w);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] points = [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]];
+            int w = 2;
+            int exp = 3;
+            int res = alg.MinRectanglesToCoverPoints(points, w);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] points = [[2, 3], [1, 2]];
+            int w = 0;
+            int exp = 2;
+            int res = alg.MinRectanglesToCoverPoints(points, w);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] points = [[1, 3], [7, 3]];
+            int w = 1;
+            int exp = 2;
+            int res = alg.MinRectanglesToCoverPoints(points, w);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3111_MinimumRectanglesToCoverPointsAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._3101_3150
+{
+    public class _3111_MinimumRectanglesToCoverPointsAlg
+    {
+        public int MinRectanglesToCoverPoints(int[][] points, int w)
+        {
+            Array.Sort(points, (x, y) => x[0].CompareTo(y[0]));
+
+            int bound = -1;
+            int ans = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                var x = points[i][0];
+                if (x > bound)
+                {
+                    bound = x + w;
+                    ans++;
+                }
+
+            }
+
+            return ans;
+        }
+    }
+}
