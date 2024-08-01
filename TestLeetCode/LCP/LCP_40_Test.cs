b6882f89aa0fc010c106c33bd406974fcc484c36
hongtao_luo <luohongtao621@hotmail.com>
2024-08-01 11:17:25 +0800

    40

---
+++ TestLeetCode/LCP/LCP_40_Test.cs
@@ -0,0 +1,37 @@
+﻿using leetCode.LCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.LCP
+{
+    [TestClass]
+    public class LCP_40_Test
+    {
+        LCP_40 alg = new LCP_40();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] cards = [1, 2, 8, 9];
+            int cnt = 3;
+            int exp = 18;
+            int res = alg.MaxmiumScore(cards, cnt);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] cards = [3, 3, 1];
+            int cnt = 1;
+            int exp = 0;
+            int res = alg.MaxmiumScore(cards, cnt);
+            Assert.AreEqual(exp, res);
+        }
+
+
+    }
+}
---
+++ leetCode/LCP/LCP_40.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode.LCP
+{
+    public class LCP_40
+    {
+        public int MaxmiumScore(int[] cards, int cnt)
+        {
+            int evenCount = cards.Count(p => p % 2 == 0);
+            int oddCount = cards.Length - evenCount;
+            if (cards.Length == cnt && oddCount % 2 == 1)
+                return 0;
+            Array.Sort(cards, (x, y) => y.CompareTo(x));
+
+            int ansEven = 0;
+            int evenCnt = 0;
+            int ansOdd = 0;
+            int oddCnt = 0;
+            int lastEvenIdx = -1;
+            int lastOddIdx = -1;
+            for (int i = 0; i < cnt; i++)
+            {
+                if (cards[i] % 2 == 0)
+                {
+                    ansEven += cards[i];
+                    evenCnt++;
+                    lastEvenIdx = i;
+                }
+                else
+                {
+                    ansOdd += cards[i];
+                    lastOddIdx = i;
+                    oddCnt++;
+                }
+            }
+
+            if (oddCnt % 2 == 0)
+            {
+                return ansEven + ansOdd;
+            }
+            List<int> res = new List<int>();
+            if (evenCount - evenCnt > 0 && lastOddIdx > -1)
+            {
+                int ans = ansOdd + ansEven - cards[lastOddIdx];
+
+                for (int i = cnt; i < cards.Length; i++)
+                {
+                    if (cards[i] % 2 == 0)
+                    {
+                        ans += cards[i];
+                        res.Add(ans);
+                        break;
+                    }
+                }
+
+            }
+            if (oddCount - oddCnt > 0 && lastEvenIdx > -1)
+            {
+                int ans = ansOdd + ansEven - cards[lastEvenIdx];
+                for (int i = cnt; i < cards.Length; i++)
+                {
+                    if (cards[i] % 2 == 1)
+                    {
+                        ans += cards[i];
+                        res.Add(ans);
+                        break;
+                    }
+                }
+            }
+
+            if (res.Count > 0)
+            {
+                return res.Max();
+            }
+
+            return 0;
+        }
+    }
+}
