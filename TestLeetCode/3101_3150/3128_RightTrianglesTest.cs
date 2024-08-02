da904b62060f2d7185de80bba90fe60d3242e5e2
hongtao_luo <luohongtao621@hotmail.com>
2024-08-02 13:09:13 +0800

    3128

---
+++ TestLeetCode/3101_3150/3128_RightTrianglesTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3128_RightTrianglesTest
+    {
+        _3128_RightTrianglesAlg alg = new _3128_RightTrianglesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[0, 1, 0], [0, 1, 1], [0, 1, 0]];
+            long exp = 2;
+            long res=alg.NumberOfRightTriangles(grid);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[1, 0, 0, 0], [0, 1, 0, 1], [1, 0, 0, 0]];
+            long exp = 0;
+            long res = alg.NumberOfRightTriangles(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] grid = [[1, 0, 1], [1, 0, 0], [1, 0, 0]];
+            long exp = 2;
+            long res = alg.NumberOfRightTriangles(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3128_RightTrianglesAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+
+    public class _3128_RightTrianglesAlg
+    {
+        public long NumberOfRightTriangles(int[][] grid)
+        {
+          
+            int m = grid[0].Length;
+            long ans = 0;
+            int[] colSum = new int[m];
+            Array.Fill(colSum, -1);
+            foreach (int[] row in grid)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    colSum[i] += row[i];
+                }
+            }
+            foreach (int[] row in grid)
+            {
+                int rowSum = -1;
+                foreach (int item in row)
+                {
+                    rowSum += item;
+                }
+                for (int i = 0; i < m; i++)
+                {
+                    if (row[i] == 1)
+                    {
+                        ans += rowSum * colSum[i];
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
