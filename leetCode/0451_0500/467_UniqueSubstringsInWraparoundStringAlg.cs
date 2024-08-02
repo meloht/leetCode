bca9947177e83ad2692d68d8f3aba63d8841e3ce
hongtao_luo <luohongtao621@hotmail.com>
2024-08-02 13:45:33 +0800

    467

--- leetCode/0451_0500/467_UniqueSubstringsInWraparoundStringAlg.cs
+++ leetCode/0451_0500/467_UniqueSubstringsInWraparoundStringAlg.cs
@@ -10,9 +10,21 @@ namespace leetCode._0451_0500
     {
         public int FindSubstringInWraproundString(string s)
         {
-            int ans = 0;
-
-            return ans;
+            int[] dp = new int[26];
+            int k = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i > 0 && (s[i] - s[i - 1] + 26) % 26 == 1)
+                {
+                    k++;
+                }
+                else
+                {
+                    k = 1;
+                }
+                dp[s[i] - 'a'] = Math.Max(k, dp[s[i] - 'a']);
+            }
+            return dp.Sum();
         }
     }
 }
