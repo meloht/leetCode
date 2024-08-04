92ad6e7c2d24c91153001dd5c7ac8eb326d18efc
luohongtao <luohongtao621@hotmail.com>
2024-08-04 21:25:18 +0800

    472

---
+++ TestLeetCode/0451_0500/472_ConcatenatedWordsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _472_ConcatenatedWordsTest
+    {
+        _472_ConcatenatedWordsAlg alg = new _472_ConcatenatedWordsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat"];
+            string[] exp = ["catsdogcats", "dogcatsdog", "ratcatdogcat"];
+            string[] res = alg.FindAllConcatenatedWordsInADict(words).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["cat", "dog", "catdog"];
+            string[] exp = ["catdog"];
+            string[] res = alg.FindAllConcatenatedWordsInADict(words).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0451_0500/472_ConcatenatedWordsAlg.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._0451_0500
+{
+    public class _472_ConcatenatedWordsAlg
+    {
+        public IList<string> FindAllConcatenatedWordsInADict(string[] words)
+        {
+            Dictionary<int, List<string>> dict = new Dictionary<int, List<string>>();
+            foreach (string word in words)
+            {
+                if (dict.ContainsKey(word.Length))
+                {
+                    dict[word.Length].Add(word);
+                }
+                else
+                {
+                    dict.Add(word.Length, [word]);
+                }
+            }
+
+            var list = dict.Keys.ToList();
+            list.Sort();
+
+
+
+
+
+            return null;
+        }
+
+        class Trie
+        {
+            private Trie[] children;
+            private bool isEnd;
+
+            public Trie()
+            {
+                children = new Trie[26];
+                isEnd = false;
+            }
+
+            public void Insert(string word)
+            {
+                Trie node = this;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    char ch = word[i];
+                    int index = ch - 'a';
+                    if (node.children[index] == null)
+                    {
+                        node.children[index] = new Trie();
+                    }
+                    node = node.children[index];
+                }
+                node.isEnd = true;
+            }
+
+            public bool Search(string word)
+            {
+                Trie node = SearchPrefix(word);
+                return node != null && node.isEnd;
+            }
+
+            public bool StartsWith(string prefix)
+            {
+                return SearchPrefix(prefix) != null;
+            }
+
+            private Trie SearchPrefix(string prefix)
+            {
+                Trie node = this;
+                for (int i = 0; i < prefix.Length; i++)
+                {
+                    char ch = prefix[i];
+                    int index = ch - 'a';
+                    if (node.children[index] == null)
+                    {
+                        return null;
+                    }
+                    node = node.children[index];
+                }
+                return node;
+            }
+        }
+
+
+    }
+}
