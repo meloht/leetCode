f7c0ab0c6baa727078dd36e0d482b8fe717d0e5d
hongtao_luo <luohongtao621@hotmail.com>
2024-08-05 09:55:23 +0800

    473

---
+++ TestLeetCode/0451_0500/473_MatchsticksToSquareTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _473_MatchsticksToSquareTest
+    {
+        _473_MatchsticksToSquareAlg alg = new _473_MatchsticksToSquareAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] matchsticks = [1, 1, 2, 2, 2];
+            bool exp = true;
+            bool res = alg.Makesquare(matchsticks);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] matchsticks = [3, 3, 3, 3, 4];
+            bool exp = false;
+            bool res = alg.Makesquare(matchsticks);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
