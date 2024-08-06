537b690b0c8d83487b5764db7e11b0ec364b7dd7
hongtao_luo <luohongtao621@hotmail.com>
2024-08-06 13:54:52 +0800

    3129

--- leetCode/3101_3150/3129_FindAllPossibleStableBinaryArraysIAlg.cs
+++ leetCode/3101_3150/3129_FindAllPossibleStableBinaryArraysIAlg.cs
@@ -9,12 +9,61 @@ namespace leetCode._3101_3150
     
     public class _3129_FindAllPossibleStableBinaryArraysIAlg
     {
+        const int MOD = 1000000007;
+        int[][][] memo;
+        int limit;
+
         public int NumberOfStableArrays(int zero, int one, int limit)
         {
-            if(Math.Abs(zero-one)>limit)
-                return 0;
-
-            return 0;
+            this.memo = new int[zero + 1][][];
+            for (int i = 0; i <= zero; i++)
+            {
+                memo[i] = new int[one + 1][];
+                for (int j = 0; j <= one; j++)
+                {
+                    memo[i][j] = new int[2];
+                    Array.Fill(memo[i][j], -1);
+                }
+            }
+            this.limit = limit;
+            return (DP(zero, one, 0) + DP(zero, one, 1)) % MOD;
         }
+
+        public int DP(int zero, int one, int lastBit)
+        {
+            if (zero == 0)
+            {
+                return (lastBit == 0 || one > limit) ? 0 : 1;
+            }
+            else if (one == 0)
+            {
+                return (lastBit == 1 || zero > limit) ? 0 : 1;
+            }
+
+            if (memo[zero][one][lastBit] == -1)
+            {
+                int res = 0;
+                if (lastBit == 0)
+                {
+                    res = (DP(zero - 1, one, 0) + DP(zero - 1, one, 1)) % MOD;
+                    if (zero > limit)
+                    {
+                        res = (res - DP(zero - limit - 1, one, 1) + MOD) % MOD;
+                    }
+                }
+                else
+                {
+                    res = (DP(zero, one - 1, 0) + DP(zero, one - 1, 1)) % MOD;
+                    if (one > limit)
+                    {
+                        res = (res - DP(zero, one - limit - 1, 0) + MOD) % MOD;
+                    }
+                }
+                memo[zero][one][lastBit] = res % MOD;
+            }
+            return memo[zero][one][lastBit];
+        }
+
+
     }
 }
