e67ea3ff32c821b38378745c6e550fefde95d8c1
hongtao_luo <luohongtao621@hotmail.com>
2024-08-08 12:19:27 +0800

    3131

---
+++ TestLeetCode/3101_3150/3131_FindTheIntegerAddedToArrayITest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3131_FindTheIntegerAddedToArrayITest
+    {
+        _3131_FindTheIntegerAddedToArrayIAlg alg = new _3131_FindTheIntegerAddedToArrayIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [2, 6, 4], nums2 = [9, 7, 5];
+            int exp = 3;
+            int res = alg.AddedInteger(nums1, nums2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [10], nums2 = [5];
+            int exp = -5;
+            int res = alg.AddedInteger(nums1, nums2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3131_FindTheIntegerAddedToArrayIAlg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+
+    public class _3131_FindTheIntegerAddedToArrayIAlg
+    {
+        public int AddedInteger(int[] nums1, int[] nums2)
+        {
+
+            int ans = nums2.Min()- nums1.Min();
+          
+            return ans;
+        }
+    }
+}
