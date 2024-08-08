282bdec4381ea3caf913a4972a8c1b0f71945173
hongtao_luo <luohongtao621@hotmail.com>
2024-08-08 14:03:39 +0800

    475

---
+++ TestLeetCode/0451_0500/475_HeatersTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _475_HeatersTest
+    {
+        _475_HeatersAlg alg = new _475_HeatersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] houses = [1, 2, 3], heaters = [2];
+            int exp = 1;
+            int res = alg.FindRadius(houses, heaters);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] houses = [1, 2, 3, 4], heaters = [1, 4];
+            int exp = 1;
+            int res = alg.FindRadius(houses, heaters);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] houses = [1, 5], heaters = [2];
+            int exp = 3;
+            int res = alg.FindRadius(houses, heaters);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/475_HeatersAlg.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _475_HeatersAlg
+    {
+        public int FindRadius(int[] houses, int[] heaters)
+        {
+            Array.Sort(houses);
+            List<Node> list = new List<Node>();
+            foreach (var item in houses)
+            {
+                list.Add(new Node() { Val = item, IsHouse = true });
+            }
+            foreach (var item in heaters)
+            {
+                list.Add(new Node() { Val = item, IsHouse = false });
+            }
+
+            list.Sort((x, y) => x.Val.CompareTo(y.Val));
+            int[] arr = new int[heaters.Length];
+
+            for (int j = 0, i = 0; j < list.Count; j++)
+            {
+                if (list[j].IsHouse == false)
+                {
+                    arr[i++] = j;
+                }
+            }
+            int ans = int.MinValue;
+
+            if (arr.Length == 1)
+            {
+                if (arr[0] == list.Count - 1)
+                {
+                    ans = list[arr[0]].Val - list[0].Val;
+                }
+                else if (arr[0] == 0)
+                {
+                    ans = list[list.Count - 1].Val - list[arr[0]].Val;
+                }
+                else
+                {
+                    ans = Math.Max(list[arr[0]].Val - list[0].Val, list[list.Count - 1].Val - list[arr[0]].Val);
+                }
+                return ans;
+            }
+            if (arr[0] > 0)
+            {
+                ans = list[arr[0]].Val - list[0].Val;
+            }
+            if (arr[0] < list.Count - 1)
+            {
+                ans = Math.Max(ans, list[list.Count - 1].Val - list[arr[0]].Val);
+            }
+
+            int idx = 1;
+
+            while (idx < arr.Length)
+            {
+                int begin = arr[idx - 1] + 1;
+                int end = arr[idx] - 1;
+                for (int i = begin, j = end; i < j; i++, j--)
+                {
+                    int d1 = list[i].Val - list[arr[idx - 1]].Val;
+                    int d2 = list[idx].Val - list[j].Val;
+                    ans = Math.Max(ans, d2);
+                    ans = Math.Max(ans, d1);
+                }
+                if (begin == end)
+                {
+                    ans = Math.Max(list[begin].Val - list[arr[idx - 1]].Val,ans);
+                    ans = Math.Max(list[idx].Val - list[begin].Val, ans);
+                }
+                idx++;
+            }
+
+
+            return ans;
+        }
+        class Node
+        {
+            public int Val { get; set; }
+            public bool IsHouse { get; set; }
+        }
+    }
+}
