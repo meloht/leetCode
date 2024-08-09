79a1cb244689563aa5628e3e0c3baebf3ac3341f
hongtao_luo <luohongtao621@hotmail.com>
2024-08-09 13:31:34 +0800

    3132

---
+++ TestLeetCode/3101_3150/3132_FindTheIntegerAddedToArrayIITest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3132_FindTheIntegerAddedToArrayIITest
+    {
+        _3132_FindTheIntegerAddedToArrayIIAlg alg = new _3132_FindTheIntegerAddedToArrayIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [4, 20, 16, 12, 8], nums2 = [14, 18, 10];
+            int exp = -2;
+            int res = alg.MinimumAddedInteger(nums1, nums2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [3, 5, 5, 3], nums2 = [7, 7];
+            int exp = 2;
+            int res = alg.MinimumAddedInteger(nums1, nums2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums1 = [3, 5, 5, 3], nums2 = [7, 7];
+            int exp = 2;
+            int res = alg.MinimumAddedInteger(nums1, nums2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums1 = [10, 2, 8, 7, 5, 6, 7, 10], nums2 = [5, 8, 5, 3, 8, 4];
+            int exp = -2;
+            int res = alg.MinimumAddedInteger(nums1, nums2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3132_FindTheIntegerAddedToArrayIIAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3132_FindTheIntegerAddedToArrayIIAlg
+    {
+        public int MinimumAddedInteger(int[] nums1, int[] nums2)
+        {
+            Array.Sort(nums1);
+            Array.Sort(nums2);
+
+            int[] idx0 = [2, 1, 0];
+            foreach (int i in idx0)
+            {
+                int idx1 = i + 1;
+                int idx2 = 1;
+                while (idx1 < nums1.Length && idx2 < nums2.Length)
+                {
+                    if (nums1[idx1] - nums2[idx2] == nums1[i] - nums2[0])
+                    {
+                        idx2++;
+                    }
+                    idx1++;
+                }
+                if (idx2 == nums2.Length)
+                {
+                    return nums2[0] - nums1[i];
+                }
+            }
+
+            return 0;
+        }
+    }
+}
