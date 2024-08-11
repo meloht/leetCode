be64ed7d5f0815a4e679f422a2012a8a120f0848
luohongtao <luohongtao621@hotmail.com>
2024-08-11 10:51:12 +0800

    1035

---
+++ TestLeetCode/1001_1050/1035_UncrossedLinesTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1035_UncrossedLinesTest
+    {
+        _1035_UncrossedLinesAlg alg = new _1035_UncrossedLinesAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [1, 4, 2], nums2 = [1, 2, 4];
+            int exp = 2;
+            int res = alg.MaxUncrossedLines(nums1, nums2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [2, 5, 1, 2, 5], nums2 = [10, 5, 2, 1, 5, 2];
+            int exp = 3;
+            int res = alg.MaxUncrossedLines(nums1, nums2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums1 = [1, 3, 7, 1, 7, 5], nums2 = [1, 9, 2, 5, 1];
+            int exp = 2;
+            int res = alg.MaxUncrossedLines(nums1, nums2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1035_UncrossedLinesAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1035_UncrossedLinesAlg
+    {
+        public int MaxUncrossedLines(int[] nums1, int[] nums2)
+        {
+            int n = nums1.Length;
+            int m = nums2.Length;
+
+            int[][] memo = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                memo[i] = new int[m];
+                Array.Fill(memo[i], -1);
+            }
+            return Dfs(n - 1, m - 1, nums1, nums2, memo);
+        }
+
+        private int Dfs(int i, int j, int[] s, int[] t, int[][] memo)
+        {
+            if (i < 0 || j < 0)
+                return 0;
+
+            if (memo[i][j] != -1)
+                return memo[i][j];
+
+            if (s[i] == t[j])
+            {
+                memo[i][j] = Dfs(i - 1, j - 1, s, t, memo) + 1;
+                return memo[i][j];
+            }
+            memo[i][j] = Math.Max(Dfs(i - 1, j, s, t, memo), Dfs(i, j - 1, s, t, memo));
+            return memo[i][j];
+        }
+    }
+}
