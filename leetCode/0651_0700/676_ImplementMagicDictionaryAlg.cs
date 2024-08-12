b0324c856e3b424ab423c3f0f5cdecf24e704689
luohongtao <luohongtao621@hotmail.com>
2024-08-12 20:20:49 +0800

    676

--- leetCode/0651_0700/676_ImplementMagicDictionaryAlg.cs
+++ leetCode/0651_0700/676_ImplementMagicDictionaryAlg.cs
@@ -10,8 +10,8 @@ namespace leetCode._0651_0700
     {
         public class MagicDictionary
         {
-            HashSet<int> set = new HashSet<int>();
-            Trie trie = new Trie();
+
+            TrieNode root = new TrieNode();
             public MagicDictionary()
             {
 
@@ -21,30 +21,15 @@ namespace leetCode._0651_0700
             {
                 foreach (var item in dictionary)
                 {
-                    this.trie.Insert(item);
-                    set.Add(item.Length);
+                    Insert(item);
+
                 }
             }
-
             public bool Search(string searchWord)
             {
-                if (!set.Contains(searchWord.Length))
-                    return false;
-                bool bl = this.trie.Search(searchWord);
-                if (bl)
-                    return false;
-
-                return false;
+                return DFS(searchWord, root, 0, false);
             }
-        }
 
-        public class Trie
-        {
-            TrieNode root;
-            public Trie()
-            {
-                root = new TrieNode();
-            }
 
             public void Insert(string word)
             {
@@ -66,60 +51,42 @@ namespace leetCode._0651_0700
                 node.Count++;
 
             }
-
-            public bool Search(string word)
+            private bool DFS(string searchWord, TrieNode node, int pos, bool modified)
             {
-                if (word.Length == 0)
-                    return false;
-                TrieNode node = root;
-                char[] ch = word.ToCharArray();
-                for (int i = 0; i < ch.Length; i++)
+                if (pos == searchWord.Length)
                 {
-                    int index = ch[i] - 'a';
-                    if (node.NextNode[index] == null)
-                        return false;
-                    node = node.NextNode[index];
+                    return modified && node.Count>0;
                 }
-
-                if (node.Count > 0)
-                    return true;
-                return false;
-            }
-
-            public bool Dfs(string word, TrieNode perent)
-            {
-                if (word.Length == 0)
-                    return false;
-                TrieNode node = perent;
-                char[] ch = word.ToCharArray();
-                for (int i = 0; i < ch.Length; i++)
+                int idx = searchWord[pos] - 'a';
+                if (node.NextNode[idx] != null)
                 {
-                    int index = ch[i] - 'a';
-                    for (int j = 0; j < node.Count; j++)
+                    if (DFS(searchWord, node.NextNode[idx], pos + 1, modified))
                     {
-                        if (j == index)
-                            continue;
-                        if (node.NextNode[index] == null)
-                            continue;
-                        if (i != ch.Length - 1 && Search(word.Substring(i + 1)))
-                        {
-                            return true;
-                        }
-                        if (i == ch.Length - 1)
-                            return true;
-
+                        return true;
                     }
-                    if (node.NextNode[index] == null)
-                        return false;
-                    node = node.NextNode[index];
                 }
-
-              
+                if (!modified)
+                {
+                    for (int i = 0; i < 26; ++i)
+                    {
+                        if (i != idx && node.NextNode[i] != null)
+                        {
+                            if (DFS(searchWord, node.NextNode[i], pos + 1, true))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
                 return false;
             }
 
-          
+
         }
+
+
+
+
 
         public class TrieNode
         {
