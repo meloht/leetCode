207b5c04a5c91ccce9acc0c879f6fe086641929b
hongtao_luo <luohongtao621@hotmail.com>
2024-08-12 13:51:31 +0800

    676

---
+++ TestLeetCode/0651_0700/676_ImplementMagicDictionaryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0651_0700._676_ImplementMagicDictionaryAlg;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _676_ImplementMagicDictionaryTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            MagicDictionary magicDictionary = new MagicDictionary();
+            magicDictionary.BuildDict(["hello", "leetcode"]);
+            bool res = magicDictionary.Search("hello"); // 返回 False
+            Assert.IsFalse(res);
+            res = magicDictionary.Search("hhllo"); // 将第二个 'h' 替换为 'e' 可以匹配 "hello" ，所以返回 True
+            Assert.IsTrue(res);
+            res = magicDictionary.Search("hell"); // 返回 False
+            Assert.IsFalse(res);
+            res = magicDictionary.Search("leetcoded"); // 返回 False
+            Assert.IsFalse(res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/676_ImplementMagicDictionaryAlg.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _676_ImplementMagicDictionaryAlg
+    {
+        public class MagicDictionary
+        {
+            HashSet<int> set = new HashSet<int>();
+            Trie trie = new Trie();
+            public MagicDictionary()
+            {
+
+            }
+
+            public void BuildDict(string[] dictionary)
+            {
+                foreach (var item in dictionary)
+                {
+                    this.trie.Insert(item);
+                    set.Add(item.Length);
+                }
+            }
+
+            public bool Search(string searchWord)
+            {
+                if(!set.Contains(searchWord.Length))
+                    return false;
+                bool bl = this.trie.Search(searchWord);
+                if (bl)
+                    return false;
+
+                return false;
+            }
+        }
+
+        public class Trie
+        {
+            TrieNode root;
+            public Trie()
+            {
+                root = new TrieNode();
+            }
+
+            public void Insert(string word)
+            {
+                TrieNode node = root;
+                if (word.Length == 0)
+                    return;
+
+                char[] ch = word.ToCharArray();
+                for (int i = 0; i < ch.Length; i++)
+                {
+                    int index = ch[i] - 'a';
+                    if (node.NextNode[index] == null)
+                    {
+                        node.NextNode[index] = new TrieNode();
+                    }
+                    node = node.NextNode[index];
+                    node.Prefix++;
+                }
+                node.Count++;
+
+            }
+
+            public bool Search(string word)
+            {
+                if (word.Length == 0)
+                    return false;
+                TrieNode node = root;
+                char[] ch = word.ToCharArray();
+                for (int i = 0; i < ch.Length; i++)
+                {
+                    int index = ch[i] - 'a';
+                    if (node.NextNode[index] == null)
+                        return false;
+                    node = node.NextNode[index];
+                }
+
+                if (node.Count > 0)
+                    return true;
+                return false;
+            }
+
+            public bool Dfs(string word, TrieNode perent)
+            {
+                if (word.Length == 0)
+                    return false;
+                TrieNode node = perent;
+                char[] ch = word.ToCharArray();
+                for (int i = 0; i < ch.Length; i++)
+                {
+                    int index = ch[i] - 'a';
+                    if (node.NextNode[index] == null)
+                        return false;
+                    node = node.NextNode[index];
+                }
+
+                if (node.Count > 0)
+                    return true;
+                return false;
+            }
+
+            public bool SearchWord(string word, TrieNode perent)
+            {
+                if (word.Length == 0)
+                    return false;
+                TrieNode node = perent;
+                char[] ch = word.ToCharArray();
+                for (int i = 0; i < ch.Length; i++)
+                {
+                    int index = ch[i] - 'a';
+                    if (node.NextNode[index] == null)
+                        return false;
+                    node = node.NextNode[index];
+                }
+
+                if (node.Count > 0)
+                    return true;
+                return false;
+            }
+        }
+
+        public class TrieNode
+        {
+            public int Count;
+            public int Prefix;
+            public TrieNode[] NextNode = new TrieNode[26];
+
+            public override string ToString()
+            {
+                return $"{Count},{Prefix}";
+            }
+        }
+    }
+}
