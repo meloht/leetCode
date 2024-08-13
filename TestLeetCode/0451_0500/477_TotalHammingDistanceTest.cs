a01200499eac8f1492c2e72a4924546f018567ea
hongtao_luo <luohongtao621@hotmail.com>
2024-08-13 13:55:35 +0800

    477

---
+++ TestLeetCode/0451_0500/477_TotalHammingDistanceTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _477_TotalHammingDistanceTest
+    {
+        _477_TotalHammingDistanceAlg alg = new _477_TotalHammingDistanceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 14, 2];
+            int exp = 6;
+            int res = alg.TotalHammingDistance(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [4, 14, 4];
+            int exp = 4;
+            int res = alg.TotalHammingDistance(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/477_TotalHammingDistanceAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _477_TotalHammingDistanceAlg
+    {
+        public int TotalHammingDistance(int[] nums)
+        {
+            int ans = 0;
+
+            for (int i = 0; i < 30; i++)
+            {
+                int c = 0;
+                foreach (var item in nums)
+                {
+                    c += item >> i & 1;
+                }
+                ans += c * (nums.Length - c);
+            }
+            return ans;
+        }
+    }
+}
