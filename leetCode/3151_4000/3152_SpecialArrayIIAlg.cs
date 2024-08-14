24766f1ea21fa09f4133c0d7799d1bd020c8b8be
luohongtao <luohongtao621@hotmail.com>
2024-08-14 20:20:55 +0800

    3152

--- leetCode/3151_4000/3152_SpecialArrayIIAlg.cs
+++ leetCode/3151_4000/3152_SpecialArrayIIAlg.cs
@@ -9,13 +9,37 @@ namespace leetCode._3151_4000
 {
     public class _3152_SpecialArrayIIAlg
     {
+
         public bool[] IsArraySpecial(int[] nums, int[][] queries)
         {
             int n = nums.Length;
-            BigInteger total = 1;
-            BigInteger t1 = 1;
+            int[] dp = new int[n];
+            Array.Fill(dp, 1);
+            for (int i = 1; i < n; i++)
+            {
+                if (((nums[i] ^ nums[i - 1]) & 1) != 0)
+                {
+                    dp[i] = dp[i - 1] + 1;
+                }
+            }
 
-            BigInteger t2 = 1;
+            bool[] res = new bool[queries.Length];
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int x = queries[i][0], y = queries[i][1];
+                res[i] = dp[y] >= y - x + 1;
+            }
+            return res;
+        }
+
+
+        public bool[] IsArraySpecial1(int[] nums, int[][] queries)
+        {
+            int n = nums.Length;
+            BigInteger total = BigInteger.One;
+            BigInteger t1 = BigInteger.One;
+
+            BigInteger t2 = BigInteger.One;
             for (int i = 0; i < n; i++)
             {
                 t1 = t1 << 1;
@@ -28,19 +52,17 @@ namespace leetCode._3151_4000
                 t2 = t2 | ((i + 1) % 2);
 
             }
-            string ss = total.ToString("B");
-            string ss1 = t2.ToString("B");
-            string ss2 = t1.ToString("B");
+
             bool[] ans = new bool[queries.Length];
             for (int i = 0; i < queries.Length; i++)
             {
                 int begin = queries[i][0];
                 int end = queries[i][1];
-                int left = n - end;
-                int right = begin; 
-                BigInteger tt = total >> right;
-                BigInteger tt1 = t1 >> begin;
-                BigInteger tt2 = t2 >> begin;
+                int left = n - end-1;
+                int right = end-begin+1;
+                BigInteger tt = GetDis(queries[i], n, total);
+                BigInteger tt1 = GetDis(queries[i], n, t1);
+                BigInteger tt2 = GetDis(queries[i], n, t2);
                 if (tt.Equals(tt1) || tt.Equals(tt2))
                 {
                     ans[i] = true;
@@ -52,6 +74,19 @@ namespace leetCode._3151_4000
             }
 
             return ans;
+        }
+
+        private BigInteger GetDis(int[] arr,int n, BigInteger t) 
+        {
+            int begin = arr[0];
+            int end = arr[1];
+            int left = n - end - 1;
+            int right = end - begin + 1;
+            BigInteger tt = t >> left;
+            BigInteger ttt = tt >> right;
+            ttt = ttt << right;
+            ttt = tt ^ ttt;
+            return ttt;
         }
     }
 }
