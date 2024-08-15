3be5c373bca528bcf751a3601116f4018e180d19
hongtao_luo <luohongtao621@hotmail.com>
2024-08-15 13:29:44 +0800

    3148

---
+++ TestLeetCode/3101_3150/3148_MaximumDifferenceScoreInAGridTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3101_3150;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3148_MaximumDifferenceScoreInAGridTest
+    {
+        _3148_MaximumDifferenceScoreInAGridAlg alg = new _3148_MaximumDifferenceScoreInAGridAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[9, 5, 7, 3], [8, 9, 6, 1], [6, 7, 14, 3], [2, 5, 3, 1]];
+            int exp = 9;
+            int res = alg.MaxScore(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[4, 3, 2], [3, 2, 1]];
+            int exp = -1;
+            int res = alg.MaxScore(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/3101_3150/3148_MaximumDifferenceScoreInAGridAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+
+    public class _3148_MaximumDifferenceScoreInAGridAlg
+    {
+        public int MaxScore(IList<IList<int>> grid)
+        {
+            return 0;
+        }
+    }
+}
