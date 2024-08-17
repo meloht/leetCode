c7abd49d40e65caef252032a36695b9dd46e35e1
luohongtao <luohongtao621@hotmail.com>
2024-08-17 19:19:36 +0800

    478

--- leetCode/0451_0500/478_GenerateRandomPointInACircleAlg.cs
+++ leetCode/0451_0500/478_GenerateRandomPointInACircleAlg.cs
@@ -8,18 +8,54 @@ namespace leetCode._0451_0500
 {
     public class _478_GenerateRandomPointInACircleAlg
     {
-        public class Solution
+        public class Solution1
         {
-
-            public Solution(double radius, double x_center, double y_center)
+            Random random;
+            double xc, yc, r;
+            public Solution1(double radius, double x_center, double y_center)
             {
+                random = new Random();
+                xc = x_center;
+                yc = y_center;
+                r = radius;
 
             }
 
             public double[] RandPoint()
             {
-                return null;
+                while (true)
+                {
+                    double x = random.NextDouble() * (2 * r) - r;
+                    double y = random.NextDouble() * (2 * r) - r;
+                    if (x * x + y * y <= r * r)
+                    {
+                        return new double[] { xc + x, yc + y };
+                    }
+                }
+
+
             }
         }
+
+        public class Solution
+        {
+            double r, x, y;
+            Random random = new Random();
+            public Solution(double _r, double _x, double _y)
+            {
+                r = _r; x = _x; y = _y;
+            }
+            public double[] RandPoint()
+            {
+                double u = random.NextDouble();
+                double theta = random.NextDouble() * 2 * Math.PI;
+                double r = Math.Sqrt(u);
+                return new double[] { x + r * Math.Cos(theta) * this.r, y + r * Math.Sin(theta) * this.r };
+
+
+            }
+        }
+
+
     }
 }
