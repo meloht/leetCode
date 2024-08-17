375107fb483eded5b1abaec18af93cca4feedb8c
luohongtao <luohongtao621@hotmail.com>
2024-08-17 21:01:00 +0800

    479

---
+++ TestLeetCode/0451_0500/479_LargestPalindromeProductTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _479_LargestPalindromeProductTest
+    {
+        _479_LargestPalindromeProductAlg alg = new _479_LargestPalindromeProductAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 2;
+            int exp = 987;
+            int res = alg.LargestPalindrome(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1;
+            int exp = 9;
+            int res = alg.LargestPalindrome(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/479_LargestPalindromeProductAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _479_LargestPalindromeProductAlg
+    {
+        public int LargestPalindrome(int n)
+        {
+            if (n == 1)
+                return 9;
+            int max = (int)Math.Pow(10, n) - 1;
+            int ans = 0;
+            for (int left = max; ans == 0; left--)
+            {
+                long p = left;
+                for (int x = left; x > 0; x /= 10)
+                {
+                    p = p * 10 + x % 10;
+                }
+                for (long i = max; i * i >= p; i--)
+                {
+                    if (p % i == 0)
+                    {
+                        ans = (int)(p % 1337);
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
