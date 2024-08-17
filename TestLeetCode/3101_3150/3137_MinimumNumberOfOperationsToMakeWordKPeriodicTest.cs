0dc4dbbf10f46962830b76e7a73a8a7cb28083e5
luohongtao <luohongtao621@hotmail.com>
2024-08-17 12:53:35 +0800

    3137

---
+++ TestLeetCode/3101_3150/3137_MinimumNumberOfOperationsToMakeWordKPeriodicTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3137_MinimumNumberOfOperationsToMakeWordKPeriodicTest
+    {
+        _3137_MinimumNumberOfOperationsToMakeWordKPeriodicAlg alg = new _3137_MinimumNumberOfOperationsToMakeWordKPeriodicAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string word = "leetcodeleet";
+            int k = 4;
+            int exp = 1;
+            int res = alg.MinimumOperationsToMakeKPeriodic(word, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string word = "leetcoleet";
+            int k = 2;
+            int exp = 3;
+            int res = alg.MinimumOperationsToMakeKPeriodic(word, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3137_MinimumNumberOfOperationsToMakeWordKPeriodicAlg.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3051_3100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3137_MinimumNumberOfOperationsToMakeWordKPeriodicAlg
+    {
+        public int MinimumOperationsToMakeKPeriodic(string word, int k)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < word.Length; i += k)
+            {
+                string s = word.Substring(i, k);
+                if (dict.ContainsKey(s))
+                {
+                    dict[s]++;
+                }
+                else
+                {
+                    dict.Add(s, 1);
+                }
+            }
+            int max = dict.Values.Max();
+            int ans = word.Length / k - max;
+            return ans;
+        }
+    }
+}
