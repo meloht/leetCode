1e01718048a703f9064994eef2f4ce84d47ea197
luohongtao <luohongtao621@hotmail.com>
2024-08-18 14:57:20 +0800

    480

---
+++ TestLeetCode/0451_0500/480_SlidingWindowMedianTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _480_SlidingWindowMedianTest
+    {
+        _480_SlidingWindowMedianAlg alg = new _480_SlidingWindowMedianAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, -1, -3, 5, 3, 6, 7];
+            int k = 3;
+            double[] exp = [1, -1, -1, 3, 5, 6];
+            var res = alg.MedianSlidingWindow(nums, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4, 2, 3, 1, 4, 2];
+            int k = 3;
+            double[] exp = [2, 3, 3, 3, 2, 3, 2];
+            var res = alg.MedianSlidingWindow(nums, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [2147483647, 2147483647];
+            int k = 2;
+            double[] exp = [2147483647.0];
+            var res = alg.MedianSlidingWindow(nums, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0451_0500/480_SlidingWindowMedianAlg.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _480_SlidingWindowMedianAlg
+    {
+        public double[] MedianSlidingWindow(int[] nums, int k)
+        {
+            double[] result = new double[nums.Length - k + 1];
+
+            SortedList<int, int> sortList = new SortedList<int, int>();
+
+            int idx = 0;
+            int left = k / 2;
+            int right = k - left;
+            for (int i = 0, j = 0; i < nums.Length; i++)
+            {
+                sortList.Add(nums[i], nums[i]);
+
+                if (sortList.Count > k)
+                {
+                    sortList.Remove(nums[j++]);
+                }
+                if (sortList.Count == k)
+                {
+                    if (left == right)
+                    {
+                        double ans = ((double)sortList[left - 1] + (double)sortList[left]) / 2;
+                        result[idx++] = ans;
+                    }
+                    else
+                    {
+                        result[idx++] = (double)sortList.GetKeyAtIndex(left);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        PriorityQueue<int, int> queueLeft = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+
+        PriorityQueue<int, int> queueRight = new PriorityQueue<int, int>();
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        private int leftCount;
+        private int rightCount;
+
+        public double[] MedianSlidingWindow2(int[] nums, int k)
+        {
+            double[] result = new double[nums.Length - k + 1];
+
+            int idx = 0;
+            for (int i = 0; i < k; i++)
+            {
+                Insert(nums[i]);
+            }
+            result[idx++] = GetMid(k);
+            for (int i = k; i < nums.Length; i++)
+            {
+                Insert(nums[i]);
+                Delete(nums[i - k]);
+                result[idx++] = GetMid(k);
+            }
+
+            return result;
+        }
+
+        private void Insert(int num)
+        {
+            if (queueLeft.Count == 0 || num < queueLeft.Peek())
+            {
+                queueLeft.Enqueue(num, num);
+                leftCount++;
+
+
+            }
+            else
+            {
+                queueRight.Enqueue(num, num);
+                rightCount++;
+               
+            }
+            Balance();
+
+        }
+        private double GetMid(int k)
+        {
+            if ((k & 1) == 1)
+            {
+                return queueLeft.Peek();
+              
+            }
+            double ans = ((double)queueLeft.Peek() + (double)queueRight.Peek()) / 2;
+            return ans;
+        }
+        private void Delete(int num)
+        {
+            if (dict.ContainsKey(num))
+            {
+                dict[num]++;
+            }
+            else
+            {
+                dict.Add(num, 1);
+            }
+            if (num <= queueLeft.Peek())
+            {
+                leftCount--;
+                if (num == queueLeft.Peek())
+                {
+                    DeleteQueue(queueLeft);
+                }
+               
+            }
+            else
+            {
+                rightCount--;
+                if (num == queueRight.Peek())
+                {
+                    DeleteQueue(queueRight);
+                }
+               
+            }
+            Balance();
+        }
+
+        private void Balance()
+        {
+            if (leftCount > rightCount + 1)
+            {
+                var item = queueLeft.Dequeue();
+            
+                queueRight.Enqueue(item, item);
+                leftCount--;
+                rightCount++;
+                DeleteQueue(queueLeft);
+            }
+            else if (leftCount < rightCount)
+            {
+                var item = queueRight.Dequeue();
+                queueLeft.Enqueue(item, item);
+                leftCount++;
+                rightCount--;
+                DeleteQueue(queueRight);
+            }
+
+        }
+
+        private void DeleteQueue(PriorityQueue<int, int> queue) 
+        {
+            while (queue.Count > 0 && dict.ContainsKey(queue.Peek()) && dict[queue.Peek()] > 0)
+            {
+               var num= queue.Dequeue();
+                dict[num]--;
+                if (dict[num] == 0)
+                {
+                    dict.Remove(num);
+                }
+            }
+        }
+
+
+    }
+}
