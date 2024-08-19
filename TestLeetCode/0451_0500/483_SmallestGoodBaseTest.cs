22e19f21ebccb3758465a732ab35df6e45405e20
luohongtao <luohongtao621@hotmail.com>
2024-08-19 21:44:59 +0800

    483

---
+++ TestLeetCode/0451_0500/483_SmallestGoodBaseTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0451_0500;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _483_SmallestGoodBaseTest
+    {
+        _483_SmallestGoodBaseAlg alg = new _483_SmallestGoodBaseAlg();
+        
+        [TestMethod]
+        public void TestCase01()
+        {
+            string n = "13";
+            string exp = "3";
+            string res = alg.SmallestGoodBase(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string n = "4681";
+            string exp = "8";
+            string res = alg.SmallestGoodBase(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/483_SmallestGoodBaseAlg.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _483_SmallestGoodBaseAlg
+    {
+        public string SmallestGoodBase1(string n)
+        {
+            long nVal = long.Parse(n);
+            int mMax = (int)Math.Floor(Math.Log(nVal) / Math.Log(2));
+            for (int m = mMax; m > 0; m--)
+            {
+                int k = (int)Math.Pow(nVal, 1.0 / m);
+                long mul = 1, sum = 1;
+                for (int i = 0; i < m; i++)
+                {
+                    mul *= k;
+                    sum += mul;
+                }
+                if (sum == nVal)
+                {
+                    return k.ToString();
+                }
+
+            }
+            return (nVal - 1).ToString();
+        }
+
+        public string SmallestGoodBase(string n)
+        {
+            long nVal = long.Parse(n);
+            int mMax = (int)(Math.Log(nVal) / Math.Log(2) + 1);
+            for (int len = mMax; len >= 3; len--)
+            {
+                long l = 2;
+                long r = nVal - 1;
+                while (l < r)
+                {
+                    long mid = (l + r) >> 1;
+                    if (Check(mid, len, nVal) >= 0)
+                    {
+                        r = mid;
+                    }
+                    else
+                    {
+                        l = mid + 1;
+                    }
+                }
+                if (Check(r, len, nVal) == 0)
+                {
+                    return r.ToString();
+                }
+
+            }
+            return (nVal - 1).ToString();
+        }
+
+        private int Check(long k, int len, long t)
+        {
+            long ans = 1;
+            for (int i = 1; i < len; i++)
+            {
+                if (ans > (t - 1) / k)
+                    return 1;
+                ans = ans * k + 1;
+            }
+            if (ans == t)
+                return 0;
+            return ans - t > 0 ? 1 : -1;
+        }
+    }
+}
