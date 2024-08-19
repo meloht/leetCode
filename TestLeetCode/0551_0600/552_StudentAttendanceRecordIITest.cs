6f85f51dfedf03701dc6db8efc386cb667005fa6
hongtao_luo <luohongtao621@hotmail.com>
2024-08-19 14:07:29 +0800

    552

---
+++ TestLeetCode/0551_0600/552_StudentAttendanceRecordIITest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _552_StudentAttendanceRecordIITest
+    {
+        _552_StudentAttendanceRecordIIAlg alg = new _552_StudentAttendanceRecordIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 2;
+            int exp = 8;
+            int res = alg.CheckRecord(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1;
+            int exp = 3;
+            int res = alg.CheckRecord(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 10101;
+            int exp = 183236316;
+            int res = alg.CheckRecord(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/552_StudentAttendanceRecordIIAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _552_StudentAttendanceRecordIIAlg
+    {
+       
+        public int CheckRecord(int n)
+        {
+            return Dfs(0, n, 0, 0);
+        }
+
+        private int Dfs(int idx, int n, int a, int l)
+        {
+            if (a > 1)
+            {
+                return 0;
+            }
+            if (l >= 3)
+            {
+                return 0;
+            }
+            if (idx >= n)
+            {
+                return 1;
+            }
+            int ans = 0;
+            if (l > 0)
+            {
+                ans += Dfs(idx + 1, n, a, l + 1);
+            }
+            else
+            {
+                ans += Dfs(idx + 1, n, a, 1);
+            }
+            ans += Dfs(idx + 1, n, a + 1, 0);
+            ans += Dfs(idx + 1, n, a, 0);
+            return ans;
+        }
+    }
+}
