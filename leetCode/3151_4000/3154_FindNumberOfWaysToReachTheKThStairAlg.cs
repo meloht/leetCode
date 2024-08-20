5e57e5c729fdde37c5f7464a6b38cbaef7132eb7
luohongtao <luohongtao621@hotmail.com>
2024-08-20 21:27:32 +0800

    3154

--- leetCode/3151_4000/3154_FindNumberOfWaysToReachTheKThStairAlg.cs
+++ leetCode/3151_4000/3154_FindNumberOfWaysToReachTheKThStairAlg.cs
@@ -10,7 +10,30 @@ namespace leetCode._3151_4000
     {
         public int WaysToReachStair(int k)
         {
-            return 0;
+            return Dfs(1, 0, 0, k, new Dictionary<long, int>());
+        }
+
+        private int Dfs(int i, int j, int preDown, int k, Dictionary<long, int> dict)
+        {
+            if (i > k + 1)
+            {
+                return 0;
+            }
+            long ii = (long)i;
+            long mask = ii << 32 | j << 1 | preDown;
+            if (dict.ContainsKey(mask))
+            {
+                return dict[mask];
+            }
+            int res = i == k ? 1 : 0;
+            res += Dfs(i + (1 << j), j + 1, 0, k, dict);
+            if (preDown == 0 && i > 0)
+            {
+                res += Dfs(i - 1, j, 1, k, dict);
+            }
+
+            dict.Add(mask, res);
+            return res;
         }
     }
 }
