8ed41873a7813a28ee94dc46d4fc96cf78ae44b3
hongtao_luo <luohongtao621@hotmail.com>
2024-08-20 12:38:29 +0800

    3154

---
+++ TestLeetCode/3151_4000/3154_FindNumberOfWaysToReachTheKThStairTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3151_4000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_4000
+{
+    [TestClass]
+    public class _3154_FindNumberOfWaysToReachTheKThStairTest
+    {
+        _3154_FindNumberOfWaysToReachTheKThStairAlg alg = new _3154_FindNumberOfWaysToReachTheKThStairAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int k = 0;
+            int exp = 2;
+            int res = alg.WaysToReachStair(k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int k = 1;
+            int exp = 4;
+            int res = alg.WaysToReachStair(k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_4000/3154_FindNumberOfWaysToReachTheKThStairAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    public class _3154_FindNumberOfWaysToReachTheKThStairAlg
+    {
+        public int WaysToReachStair(int k)
+        {
+            return 0;
+        }
+    }
+}
