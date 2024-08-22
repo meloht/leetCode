4fa3fc8a49ce17165b18889742b1120689036428
hongtao_luo <luohongtao621@hotmail.com>
2024-08-22 13:19:47 +0800

    3133

---
+++ TestLeetCode/3101_3150/3133_MinimumArrayEndTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3133_MinimumArrayEndTest
+    {
+        _3133_MinimumArrayEndAlg alg = new _3133_MinimumArrayEndAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3, x = 4;
+            long exp = 6;
+            long res = alg.MinEnd(n, x);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 2, x = 7;
+            long exp = 15;
+            long res = alg.MinEnd(n, x);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/3101_3150/3133_MinimumArrayEndAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3133_MinimumArrayEndAlg
+    {
+        public long MinEnd(int n, int x)
+        {
+            return 0;
+        }
+    }
+}
