3fbbf314b6ad0a59a0e28b0a7c651c5b2679d702
hongtao_luo <luohongtao621@hotmail.com>
2024-08-23 13:23:09 +0800

    3145

---
+++ leetCode/3101_3150/3145_FindProductsOfElementsOfBigArrayAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3145_FindProductsOfElementsOfBigArrayAlg
+    {
+        public int[] FindProductsOfElements(long[][] queries)
+        {
+            return null;
+        }
+    }
+}
