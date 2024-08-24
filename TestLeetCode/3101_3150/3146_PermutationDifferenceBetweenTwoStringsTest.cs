53d0a88d076d61ee968ed63205734eabac875b83
luohongtao <luohongtao621@hotmail.com>
2024-08-24 21:28:38 +0800

    3146

---
+++ TestLeetCode/3101_3150/3146_PermutationDifferenceBetweenTwoStringsTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3146_PermutationDifferenceBetweenTwoStringsTest
+    {
+        _3146_PermutationDifferenceBetweenTwoStringsAlg alg = new _3146_PermutationDifferenceBetweenTwoStringsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abc", t = "bac";
+            int exp = 2;
+            int res = alg.FindPermutationDifference(s, t);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "abcde", t = "edbac";
+            int exp = 12;
+            int res = alg.FindPermutationDifference(s, t);
+            Assert.AreEqual(exp, res);
+        }
+
+
+    }
+}
---
+++ leetCode/3101_3150/3146_PermutationDifferenceBetweenTwoStringsAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3146_PermutationDifferenceBetweenTwoStringsAlg
+    {
+        public int FindPermutationDifference(string s, string t)
+        {
+            int[] dict=new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                int idx = s[i] - 'a';
+                dict[idx]= i;
+            }
+            int ans = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                ans += Math.Abs(i - dict[t[i] - 'a']);
+            }
+            return ans;
+        }
+    }
+}
