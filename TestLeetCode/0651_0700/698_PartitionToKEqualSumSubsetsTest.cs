30e3251891852be29211dd0ca906bfccc007db5c
luohongtao <luohongtao621@hotmail.com>
2024-08-25 10:42:18 +0800

    698

---
+++ TestLeetCode/0651_0700/698_PartitionToKEqualSumSubsetsTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _698_PartitionToKEqualSumSubsetsTest
+    {
+        _698_PartitionToKEqualSumSubsetsAlg alg = new _698_PartitionToKEqualSumSubsetsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 3, 2, 3, 5, 2, 1];
+            int k = 4;
+            bool exp = true;
+            bool res = alg.CanPartitionKSubsets(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int k = 3;
+            bool exp = true;
+            bool res = alg.CanPartitionKSubsets(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/698_PartitionToKEqualSumSubsetsAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _698_PartitionToKEqualSumSubsetsAlg
+    {
+        public bool CanPartitionKSubsets(int[] nums, int k)
+        {
+            long sum = 0;
+            foreach (int i in nums) 
+            {
+                sum += i;
+            }
+            if (sum % k > 0) 
+            {
+                return false;
+            }
+
+
+
+
+            return false;
+        }
+    }
+}
