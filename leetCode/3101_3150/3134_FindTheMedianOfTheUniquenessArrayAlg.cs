fba693cbb86db1d9a5c58b6a0a5d0b12a45d046b
luohongtao <luohongtao621@hotmail.com>
2024-08-27 21:19:21 +0800

    3134

--- leetCode/3101_3150/3134_FindTheMedianOfTheUniquenessArrayAlg.cs
+++ leetCode/3101_3150/3134_FindTheMedianOfTheUniquenessArrayAlg.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace leetCode._3101_3150
 {
@@ -10,7 +11,59 @@ namespace leetCode._3101_3150
     {
         public int MedianOfUniquenessArray(int[] nums)
         {
-            return 0;
+            int n = nums.Length;
+            long k = ((long)n * (n + 1) / 2 + 1) / 2;
+            int left = 0;
+            int right = n;
+            while (left + 1 < right)
+            {
+                int mid = (left + right) / 2;
+                if (Check(nums, mid, k))
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid;
+                }
+            }
+            return right;
+
         }
+
+        private bool Check(int[] nums, int upper, long k)
+        {
+            long cnt = 0;
+            int l = 0;
+            Dictionary<int, int> freq = new  Dictionary<int, int>();
+            for (int r = 0; r < nums.Length; r++)
+            {
+                if (freq.ContainsKey(nums[r]))
+                {
+                    freq[nums[r]]++;
+                }
+                else
+                {
+                    freq.Add(nums[r], 1);
+                }
+                while (freq.Count > upper)
+                { // 窗口内元素过多
+                    freq[nums[l]]--;
+                    if (freq[nums[l]] == 0)
+                    { // 移出左端点
+                        freq.Remove(nums[l]);
+                    }
+                    l++;
+                }
+                cnt += r - l + 1; // 右端点固定为 r 时，有 r-l+1 个合法左端点
+                if (cnt >= k)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
     }
 }
