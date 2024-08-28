8511d6323d7ef4efb840d16925d328e042b7180d
hongtao_luo <luohongtao621@hotmail.com>
2024-08-28 14:25:30 +0800

    486

---
+++ TestLeetCode/0451_0500/486_PredictTheWinnerTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _486_PredictTheWinnerTest
+    {
+        _486_PredictTheWinnerAlg alg = new _486_PredictTheWinnerAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 5, 2];
+            bool exp = false;
+            bool res = alg.PredictTheWinner(nums);
+            Assert.AreEqual(res, exp);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 5, 233, 7];
+            bool exp = true;
+            bool res = alg.PredictTheWinner(nums);
+            Assert.AreEqual(res, exp);
+        }
+    }
+}
---
+++ leetCode/0451_0500/486_PredictTheWinnerAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _486_PredictTheWinnerAlg
+    {
+        public bool PredictTheWinner(int[] nums)
+        {
+            return false;
+        }
+    }
+}
