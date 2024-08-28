c90391bb2bc255daa454d09671277be1de0a0e42
hongtao_luo <luohongtao621@hotmail.com>
2024-08-28 14:56:59 +0800

    486

--- leetCode/0451_0500/486_PredictTheWinnerAlg.cs
+++ leetCode/0451_0500/486_PredictTheWinnerAlg.cs
@@ -10,7 +10,52 @@ namespace leetCode._0451_0500
     {
         public bool PredictTheWinner(int[] nums)
         {
-            return false;
+            int a = 0;
+            int b = 0;
+            bool flag = true;
+            for (int i = 0, j = nums.Length - 1; i < j; )
+            {
+                if (flag)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        a += nums[i];
+                        i++;
+                    }
+                    else
+                    {
+                        a += nums[j];
+                        j--;
+                    }
+                    if (i == j)
+                    {
+                        b += nums[i];
+                        break;
+                    }
+                }
+                else
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        b += nums[i];
+                        i++;
+                    }
+                    else
+                    {
+                        b += nums[j];
+                        j--;
+                    }
+                    if (i == j)
+                    {
+                        a += nums[i];
+                        break;
+                    }
+                }
+                flag = !flag;
+            }
+            
+
+            return a > b || a == b;
         }
     }
 }
