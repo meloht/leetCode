b0a9fe26aaa164be79988fdb13e3dd0bbd52f317
hongtao_luo <luohongtao621@hotmail.com>
2024-08-29 12:39:52 +0800

    3142

---
+++ TestLeetCode/3101_3150/3142_CheckIfGridSatisfiesConditionsTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3142_CheckIfGridSatisfiesConditionsTest
+    {
+        _3142_CheckIfGridSatisfiesConditionsAlg alg = new _3142_CheckIfGridSatisfiesConditionsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[1, 0, 2], [1, 0, 2]];
+            bool exp = true;
+            bool res = alg.SatisfiesConditions(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[1, 1, 1], [0, 0, 0]];
+            bool exp = false;
+            bool res = alg.SatisfiesConditions(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] grid = [[1], [2], [3]];
+            bool exp = false;
+            bool res = alg.SatisfiesConditions(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3142_CheckIfGridSatisfiesConditionsAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3142_CheckIfGridSatisfiesConditionsAlg
+    {
+        public bool SatisfiesConditions(int[][] grid)
+        {
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (i + 1 < grid.Length)
+                    {
+                        if (grid[i][j] != grid[i + 1][j])
+                            return false;
+                    }
+                    if (j + 1 < grid[0].Length)
+                    {
+                        if (grid[i][j] == grid[i][j + 1])
+                            return false;
+                    }
+                }
+            }
+
+
+            return true;
+        }
+    }
+}
