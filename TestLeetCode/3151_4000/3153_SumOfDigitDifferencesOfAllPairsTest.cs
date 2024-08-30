2921fd43e54a8adf3e6c9324d777b28b5ba71593
hongtao_luo <luohongtao621@hotmail.com>
2024-08-30 12:09:57 +0800

    3153

---
+++ TestLeetCode/3151_4000/3153_SumOfDigitDifferencesOfAllPairsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3151_4000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_4000
+{
+    [TestClass]
+    public class _3153_SumOfDigitDifferencesOfAllPairsTest
+    {
+        _3153_SumOfDigitDifferencesOfAllPairsAlg alg = new _3153_SumOfDigitDifferencesOfAllPairsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [13, 23, 12];
+            long exp = 4;
+            long res = alg.SumDigitDifferences(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [10, 10, 10, 10];
+            long exp = 0;
+            long res = alg.SumDigitDifferences(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_4000/3153_SumOfDigitDifferencesOfAllPairsAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    public class _3153_SumOfDigitDifferencesOfAllPairsAlg
+    {
+        public long SumDigitDifferences(int[] nums)
+        {
+            long res = 0;
+            int n = nums.Length;
+            while (nums[0] > 0)
+            {
+                int[] cnt = new int[10];
+                for (int i = 0; i < n; i++)
+                {
+                    cnt[nums[i] % 10]++;
+                    nums[i] /= 10;
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    res += (long)(n - cnt[i]) * cnt[i];
+                }
+            }
+            return res / 2;
+
+        }
+    }
+}
