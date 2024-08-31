89c74661cc70467fa06290312a3aef9e19bc3393
luohongtao <luohongtao621@hotmail.com>
2024-08-31 16:17:49 +0800

    3127

---
+++ TestLeetCode/3101_3150/3127_MakeASquareWithTheSameColorTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3127_MakeASquareWithTheSameColorTest
+    {
+        _3127_MakeASquareWithTheSameColorAlg alg = new _3127_MakeASquareWithTheSameColorAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[][] grid = [['B', 'W', 'B'], ['B', 'W', 'W'], ['B', 'W', 'B']];
+            bool exp = true;
+            bool res=alg.CanMakeSquare(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[][] grid = [['B', 'W', 'B'], ['W', 'B', 'W'], ['B', 'W', 'B']];
+            bool exp = false;
+            bool res = alg.CanMakeSquare(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            char[][] grid = [['B', 'W', 'B'], ['B', 'W', 'W'], ['B', 'W', 'W']];
+            bool exp = true;
+            bool res = alg.CanMakeSquare(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3127_MakeASquareWithTheSameColorAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3127_MakeASquareWithTheSameColorAlg
+    {
+        public bool CanMakeSquare(char[][] grid)
+        {
+
+            for (int i = 0; i < grid.Length - 1; i++)
+            {
+                for (int j = 0; j < grid[0].Length - 1; j++)
+                {
+                    int b = 0;
+                    int w = 0;
+                    for (int k = i; k <= i + 1; k++)
+                    {
+                        for (int m = j; m <= j + 1; m++)
+                        {
+                            if (grid[k][m] == 'B')
+                            {
+                                b++;
+                            }
+                            else
+                            {
+                                w++;
+                            }
+                        }
+                    }
+
+                    if (b >= 3 || w >= 3)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+
+    }
+}
