6cd93b929f70c081699f64272fae6d6c70e0002c
luohongtao <luohongtao621@hotmail.com>
2024-09-03 21:01:38 +0800

    2708

---
+++ TestLeetCode/2701_2750/2708_MaximumStrengthOfAGroupTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2708_MaximumStrengthOfAGroupTest
+    {
+        _2708_MaximumStrengthOfAGroupAlg alg = new _2708_MaximumStrengthOfAGroupAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, -1, -5, 2, 5, -9];
+            long exp = 1350;
+            long res = alg.MaxStrength(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [-4, -5, -4];
+            long exp = 20;
+            long res = alg.MaxStrength(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2701_2750/2708_MaximumStrengthOfAGroupAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2708_MaximumStrengthOfAGroupAlg
+    {
+        public long MaxStrength(int[] nums)
+        {
+
+            return 0;
+        }
+    }
+}
