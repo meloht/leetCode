56b3e9acf3a293cc988a347423cec3e9de42da42
hongtao_luo <luohongtao621@hotmail.com>
2024-09-05 14:20:44 +0800

    491

---
+++ TestLeetCode/0451_0500/491_NonDecreasingSubsequencesTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _491_NonDecreasingSubsequencesTest
+    {
+        _491_NonDecreasingSubsequencesAlg alg = new _491_NonDecreasingSubsequencesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 6, 7, 7];
+            IList<IList<int>> exp = [[4, 6], [4, 6, 7], [4, 6, 7, 7], [4, 7], [4, 7, 7], [6, 7], [6, 7, 7], [7, 7]];
+
+            IList<IList<int>> res = alg.FindSubsequences(nums);
+            Assert.IsTrue(Utils.IsSame(exp,res));
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [4, 4, 3, 2, 1];
+            IList<IList<int>> exp = [[4, 4]];
+
+            IList<IList<int>> res = alg.FindSubsequences(nums);
+            Assert.IsTrue(Utils.IsSame(exp, res));
+
+        }
+    }
+}
---
+++ leetCode/0451_0500/491_NonDecreasingSubsequencesAlg.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _491_NonDecreasingSubsequencesAlg
+    {
+        public IList<IList<int>> FindSubsequences1(int[] nums)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+            bool[] used = new bool[nums.Length];
+            Dfs(0, -101, nums, used, res);
+            return res;
+        }
+        private void Dfs(int index, int prev, int[] nums, bool[] used, List<IList<int>> res)
+        {
+            if (index >= nums.Length)
+            {
+                if (prev == -101)
+                    return;
+                List<int> ls = new List<int>();
+                for (int i = 0; i < used.Length; i++)
+                {
+                    if (used[i])
+                    {
+                        ls.Add(nums[i]);
+                    }
+                }
+
+                if (ls.Count > 1)
+                {
+                    res.Add(ls);
+                }
+
+                return;
+            }
+
+            if (nums[index] >= prev)
+            {
+                used[index] = true;
+                Dfs(index + 1, nums[index], nums, used, res);
+                used[index] = false;
+            }
+            if (nums[index] != prev)
+            {
+                used[index] = false;
+                Dfs(index + 1, prev, nums, used, res);
+            }
+
+        }
+
+        public IList<IList<int>> FindSubsequences(int[] nums)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+            bool[] used = new bool[nums.Length];
+            Dfs1(0, -101, nums, new List<int>(), res);
+            return res;
+        }
+        private void Dfs1(int index, int prev, int[] nums, List<int> list, List<IList<int>> res)
+        {
+            if (index >= nums.Length)
+            {
+                if (prev == -101)
+                    return;
+
+                if (list.Count > 1)
+                {
+                    res.Add(list.ToArray());
+                }
+
+                return;
+            }
+
+            if (nums[index] >= prev)
+            {
+                list.Add(nums[index]);
+                Dfs1(index + 1, nums[index], nums, list, res);
+                list.RemoveAt(list.Count - 1);
+            }
+
+            if (nums[index] != prev)
+            {
+                Dfs1(index + 1, prev, nums, list, res);
+            }
+
+        }
+    }
+}
