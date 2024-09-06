bbb980babe92b01792a92b48a5e419b569c1d2e9
hongtao_luo <luohongtao621@hotmail.com>
2024-09-06 13:46:09 +0800

    493

---
+++ TestLeetCode/0451_0500/493_ReversePairsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _493_ReversePairsTest
+    {
+        _493_ReversePairsAlg alg = new _493_ReversePairsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 2, 3, 1];
+            int exp = 2;
+            int res = alg.ReversePairs(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 4, 3, 5, 1];
+            int exp = 3;
+            int res = alg.ReversePairs(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0451_0500/493_ReversePairsAlg.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _493_ReversePairsAlg
+    {
+        public int ReversePairs(int[] nums)
+        {
+            Array.Sort(nums);
+            int ans = 0;
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int target = nums[i] * 2;
+                if (target >= nums[nums.Length - 1])
+                {
+                    return ans;
+                }
+                int idx = BinarySearch(nums, i + 1, target);
+                if (nums[idx] > nums[i] * 2)
+                {
+                    ans += (nums.Length - idx);
+                }
+            }
+
+            return ans;
+        }
+
+        private int BinarySearch(int[] nums, int left, int target)
+        {
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int mid = (right - left) / 2 + left;
+
+                if (nums[mid] <= target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+            return right;
+        }
+    }
+}
