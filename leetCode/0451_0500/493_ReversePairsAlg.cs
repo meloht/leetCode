dbfa28057b7f21e839e0ecd7ff2dcdd4a029e34a
luohongtao <luohongtao621@hotmail.com>
2024-09-06 21:39:42 +0800

    493

--- leetCode/0451_0500/493_ReversePairsAlg.cs
+++ leetCode/0451_0500/493_ReversePairsAlg.cs
@@ -10,44 +10,75 @@ namespace leetCode._0451_0500
     {
         public int ReversePairs(int[] nums)
         {
-            Array.Sort(nums);
-            int ans = 0;
-
-            for (int i = 0; i < nums.Length - 2; i++)
+            if (nums.Length == 0)
             {
-                int target = nums[i] * 2;
-                if (target >= nums[nums.Length - 1])
-                {
-                    return ans;
-                }
-                int idx = BinarySearch(nums, i + 1, target);
-                if (nums[idx] > nums[i] * 2)
-                {
-                    ans += (nums.Length - idx);
-                }
+                return 0;
             }
-
-            return ans;
+            return ReversePairsRecursive(nums, 0, nums.Length - 1);
         }
 
-        private int BinarySearch(int[] nums, int left, int target)
+
+        public int ReversePairsRecursive(int[] nums, int left, int right)
         {
-            int right = nums.Length - 1;
-
-            while (left < right)
+            if (left == right)
             {
-                int mid = (right - left) / 2 + left;
-
-                if (nums[mid] <= target)
-                {
-                    left = mid + 1;
-                }
-                else
-                {
-                    right = mid;
-                }
+                return 0;
             }
-            return right;
+            else
+            {
+                int mid = (left + right) / 2;
+                int n1 = ReversePairsRecursive(nums, left, mid);
+                int n2 = ReversePairsRecursive(nums, mid + 1, right);
+                int ret = n1 + n2;
+
+                // 首先统计下标对的数量
+                int i = left;
+                int j = mid + 1;
+                while (i <= mid)
+                {
+                    while (j <= right && (long)nums[i] > 2 * (long)nums[j])
+                    {
+                        j++;
+                    }
+                    ret += j - mid - 1;
+                    i++;
+                }
+
+                // 随后合并两个排序数组
+                int[] sorted = new int[right - left + 1];
+                int p1 = left, p2 = mid + 1;
+                int p = 0;
+                while (p1 <= mid || p2 <= right)
+                {
+                    if (p1 > mid)
+                    {
+                        sorted[p++] = nums[p2++];
+                    }
+                    else if (p2 > right)
+                    {
+                        sorted[p++] = nums[p1++];
+                    }
+                    else
+                    {
+                        if (nums[p1] < nums[p2])
+                        {
+                            sorted[p++] = nums[p1++];
+                        }
+                        else
+                        {
+                            sorted[p++] = nums[p2++];
+                        }
+                    }
+                }
+                for (int k = 0; k < sorted.Length; k++)
+                {
+                    nums[left + k] = sorted[k];
+                }
+                return ret;
+            }
         }
+
+
+
     }
 }
