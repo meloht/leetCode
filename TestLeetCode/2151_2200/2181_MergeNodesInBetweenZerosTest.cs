de61c9f8bd92610eab56f807858c3b4150d99636
hongtao_luo <luohongtao621@hotmail.com>
2024-09-09 17:53:44 +0800

    2181

---
+++ TestLeetCode/2151_2200/2181_MergeNodesInBetweenZerosTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode;
+using leetCode._2151_2200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2151_2200
+{
+    [TestClass]
+    public class _2181_MergeNodesInBetweenZerosTest
+    {
+        _2181_MergeNodesInBetweenZerosAlg alg = new _2181_MergeNodesInBetweenZerosAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [0, 3, 1, 0, 4, 5, 2, 0];
+            int[] exp = [4, 11];
+
+            ListNode listNode = ListNode.BuildListNode(head);
+            var resNode = alg.MergeNodes(listNode);
+            int[] res = ListNode.GetLinkNodeList(resNode);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [0, 1, 0, 3, 0, 2, 2, 0];
+            int[] exp = [1, 3, 4];
+
+            ListNode listNode = ListNode.BuildListNode(head);
+            var resNode = alg.MergeNodes(listNode);
+            int[] res = ListNode.GetLinkNodeList(resNode);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2151_2200/2181_MergeNodesInBetweenZerosAlg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2151_2200
+{
+    public class _2181_MergeNodesInBetweenZerosAlg
+    {
+        public ListNode MergeNodes1(ListNode head)
+        {
+            ListNode headNew = new ListNode(-1);
+            ListNode node = head.next;
+            ListNode nodeNew = headNew;
+            int sum = 0;
+            while (node != null)
+            {
+                if (node.val != 0)
+                {
+                    sum += node.val;
+                }
+                if (node.val == 0)
+                {
+                    ListNode listNode = new ListNode(sum);
+                    nodeNew.next = listNode;
+                    nodeNew = listNode;
+                    sum = 0;
+                }
+                node = node.next;
+            }
+
+            return headNew.next;
+        }
+
+        public ListNode MergeNodes(ListNode head)
+        {
+            ListNode tail = head;
+            ListNode node = head.next;
+
+            while (node.next != null)
+            {
+                if (node.val != 0)
+                {
+                    tail.val += node.val;
+                }
+                else
+                {
+                    tail = tail.next;
+                    tail.val = 0;
+                }
+
+                node = node.next;
+            }
+
+            tail.next = null;
+
+            return head;
+        }
+    }
+}
