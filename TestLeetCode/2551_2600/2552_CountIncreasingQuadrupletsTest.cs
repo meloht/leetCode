c414ed13a7d73cf7e6ecd8e693e147deb4afb78d
hongtao_luo <luohongtao621@hotmail.com>
2024-09-10 13:24:35 +0800

    2552

---
+++ TestLeetCode/2551_2600/2552_CountIncreasingQuadrupletsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2551_2600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2551_2600
+{
+    [TestClass]
+    public class _2552_CountIncreasingQuadrupletsTest
+    {
+        _2552_CountIncreasingQuadrupletsAlg alg = new _2552_CountIncreasingQuadrupletsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 2, 4, 5];
+            long exp = 2;
+            long res = alg.CountQuadruplets(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4];
+            long exp = 0;
+            long res = alg.CountQuadruplets(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2551_2600/2552_CountIncreasingQuadrupletsAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2551_2600
+{
+    public class _2552_CountIncreasingQuadrupletsAlg
+    {
+        public long CountQuadruplets(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
