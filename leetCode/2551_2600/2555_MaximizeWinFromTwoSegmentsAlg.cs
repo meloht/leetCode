2871adef1c0e70dfc9f9c783346032f1d2d6493a
luohongtao <luohongtao621@hotmail.com>
2024-09-11 21:09:24 +0800

    2555

--- leetCode/2551_2600/2555_MaximizeWinFromTwoSegmentsAlg.cs
+++ leetCode/2551_2600/2555_MaximizeWinFromTwoSegmentsAlg.cs
@@ -11,8 +11,26 @@ namespace leetCode._2551_2600
         public int MaximizeWin(int[] prizePositions, int k)
         {
 
+            int n = prizePositions.Length;
+            if (k * 2 + 1 >= prizePositions[n - 1] - prizePositions[0])
+            {
+                return n;
+            }
+            int ans = 0;
+            int left = 0;
+            int[] mx = new int[n + 1];
+            for (int right = 0; right < n; right++)
+            {
+                while (prizePositions[right] - prizePositions[left] > k)
+                {
+                    left++;
+                }
+                ans = Math.Max(ans, mx[left] + right - left + 1);
+                mx[right + 1] = Math.Max(mx[right], right - left + 1);
+            }
+            return ans;
 
-            return 0;
+
         }
     }
 }
