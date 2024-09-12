11700feba8a4eb3f7c13f249ad63f423c4c63624
hongtao_luo <luohongtao621@hotmail.com>
2024-09-12 13:39:47 +0800

    2576

---
+++ TestLeetCode/2551_2600/2576_FindTheMaximumNumberOfMarkedIndicesTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2551_2600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2551_2600
+{
+    [TestClass]
+    public class _2576_FindTheMaximumNumberOfMarkedIndicesTest
+    {
+        _2576_FindTheMaximumNumberOfMarkedIndicesAlg alg = new _2576_FindTheMaximumNumberOfMarkedIndicesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 5, 2, 4];
+            int exp = 2;
+            int res = alg.MaxNumOfMarkedIndices(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [9, 2, 5, 4];
+            int exp = 4;
+            int res = alg.MaxNumOfMarkedIndices(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [7, 6, 8];
+            int exp = 0;
+            int res = alg.MaxNumOfMarkedIndices(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2551_2600/2576_FindTheMaximumNumberOfMarkedIndicesAlg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2551_2600
+{
+    public class _2576_FindTheMaximumNumberOfMarkedIndicesAlg
+    {
+        public int MaxNumOfMarkedIndices(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int n = nums.Length;
+            int l = 0, r = n / 2;
+            while (l < r)
+            {
+                int m = l + r + 1 >> 1;
+                if (Check(nums, m))
+                {
+                    l = m;
+                }
+                else
+                {
+                    r = m - 1;
+                }
+            }
+            return l * 2;
+
+        }
+
+        public bool Check(int[] nums, int m)
+        {
+            int n = nums.Length;
+            for (int i = 0; i < m; i++)
+            {
+                if (nums[i] * 2 > nums[n - m + i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
