e7925cb1016f165b09c9ceeef5b706a03604d980
hongtao_luo <luohongtao621@hotmail.com>
2024-09-13 14:03:54 +0800

    2398

---
+++ TestLeetCode/2351_2400/2398_MaximumNumberOfRobotsWithinBudgeTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._2351_2400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2351_2400
+{
+    [TestClass]
+    public class _2398_MaximumNumberOfRobotsWithinBudgeTest
+    {
+        _2398_MaximumNumberOfRobotsWithinBudgeAlg alg = new _2398_MaximumNumberOfRobotsWithinBudgeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] chargeTimes = [3, 6, 1, 3, 4];
+            int[] runningCosts = [2, 1, 3, 4, 5];
+            int budget = 25;
+            int exp = 3;
+            int res = alg.MaximumRobots(chargeTimes, runningCosts, budget);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] chargeTimes = [11, 12, 19];
+            int[] runningCosts = [10, 8, 7];
+            int budget = 19;
+            int exp = 0;
+            int res = alg.MaximumRobots(chargeTimes, runningCosts, budget);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2351_2400/2398_MaximumNumberOfRobotsWithinBudgeAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2351_2400
+{
+    public class _2398_MaximumNumberOfRobotsWithinBudgeAlg
+    {
+        public int MaximumRobots(int[] chargeTimes, int[] runningCosts, long budget)
+        {
+            int res = 0;
+            int n = chargeTimes.Length;
+            long runningCostSum = 0;
+            LinkedList<int> q = new LinkedList<int>();
+            for (int i = 0, j = 0; i < n; i++)
+            {
+                runningCostSum += runningCosts[i];
+                while (q.Count > 0 && chargeTimes[q.Last.Value] <= chargeTimes[i])
+                {
+                    q.RemoveLast();
+                }
+                q.AddLast(i);
+                while (j <= i && (i - j + 1) * runningCostSum + chargeTimes[q.First.Value] > budget)
+                {
+                    if (q.Count > 0 && q.First.Value == j)
+                    {
+                        q.RemoveFirst();
+                    }
+                    runningCostSum -= runningCosts[j];
+                    j++;
+                }
+                res = Math.Max(res, i - j + 1);
+            }
+            return res;
+        }
+
+    }
+}
