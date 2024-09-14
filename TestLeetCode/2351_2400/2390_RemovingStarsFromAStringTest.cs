ddb47aaac7820de2b8bb9e739dc705257c3efd7b
luohongtao <luohongtao621@hotmail.com>
2024-09-14 20:58:12 +0800

    2390

---
+++ TestLeetCode/2351_2400/2390_RemovingStarsFromAStringTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2351_2400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2351_2400
+{
+    [TestClass]
+    public class _2390_RemovingStarsFromAStringTest
+    {
+        _2390_RemovingStarsFromAStringAlg alg = new _2390_RemovingStarsFromAStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "leet**cod*e";
+            string exp = "lecoe";
+            string res = alg.RemoveStars(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "erase*****";
+            string exp = "";
+            string res = alg.RemoveStars(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2351_2400/2390_RemovingStarsFromAStringAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2351_2400
+{
+    
+    public class _2390_RemovingStarsFromAStringAlg
+    {
+        public string RemoveStars(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '*')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    stack.Push(s[i]);
+                }
+            }
+            char[] arr = stack.ToArray();
+            var ls = arr.Reverse().ToArray();
+            return new string(ls);
+        }
+    }
+}
