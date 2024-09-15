daad8b3569f445ee73f58c987230a45aa7e29314
luohongtao <luohongtao621@hotmail.com>
2024-09-15 21:06:31 +0800

    2848

---
+++ TestLeetCode/2801_2850/2848_PointsThatIntersectWithCarsTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2801_2850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2801_2850
+{
+    [TestClass]
+    public class _2848_PointsThatIntersectWithCarsTest
+    {
+        _2848_PointsThatIntersectWithCarsAlg alg = new _2848_PointsThatIntersectWithCarsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] nums = [[3, 6], [1, 5], [4, 7]];
+            int exp = 7;
+            int res=alg.NumberOfPoints(nums);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] nums = [[1, 3], [5, 8]];
+            int exp = 7;
+            int res = alg.NumberOfPoints(nums);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/2801_2850/2848_PointsThatIntersectWithCarsAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2801_2850
+{
+    public class _2848_PointsThatIntersectWithCarsAlg
+    {
+        public int NumberOfPoints(IList<IList<int>> nums)
+        {
+            var numArr = nums.ToArray();
+            Array.Sort(numArr, (x, y) => x[0].CompareTo(y[0]));
+
+            int ans = 0;
+            int begin = numArr[0][0];
+            int end = numArr[0][1];
+            ans = end - begin + 1;
+            for (int i = 1; i < numArr.Length; i++)
+            {
+                if (numArr[i][1] <= end)
+                {
+                    continue;
+                }
+                if (numArr[i][0] <= end)
+                {
+                    ans += numArr[i][1] - end;
+                }
+                else
+                {
+                    ans += numArr[i][1] - numArr[i][0] + 1;
+                }
+
+                begin = Math.Min(begin, numArr[i][0]);
+                end = Math.Max(numArr[i][1], end);
+            }
+
+
+            return ans;
+        }
+    }
+}
