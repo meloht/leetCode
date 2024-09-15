9c9c74717bccce10423528d89575bfb638218021
luohongtao <luohongtao621@hotmail.com>
2024-09-15 22:16:53 +0800

    498

---
+++ TestLeetCode/0451_0500/498_DiagonalTraverseTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0451_0500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0451_0500
+{
+    [TestClass]
+    public class _498_DiagonalTraverseTest
+    {
+        _498_DiagonalTraverseAlg alg = new _498_DiagonalTraverseAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+            int[] exp = [1, 2, 4, 7, 5, 3, 6, 8, 9];
+            int[] res = alg.FindDiagonalOrder(mat);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] mat = [[1, 2], [3, 4]];
+            int[] exp = [1, 2, 3, 4];
+            int[] res = alg.FindDiagonalOrder(mat);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0451_0500/498_DiagonalTraverseAlg.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0451_0500
+{
+    public class _498_DiagonalTraverseAlg
+    {
+        public int[] FindDiagonalOrder(int[][] mat)
+        {
+            int[] nums = new int[mat.Length * mat[0].Length];
+
+            int n = 0;
+            int m = 0;
+            bool flag = true;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = mat[n][m];
+                if (flag)
+                {
+                    n--;
+                    m++;
+
+                    if (n < 0 || m > mat[0].Length - 1)
+                    {
+                        flag = false;
+                        n = Math.Max(n, 0);
+                        m = Math.Min(m, mat[0].Length - 1);
+                    }
+
+                }
+                else
+                {
+                    n++;
+                    m--;
+
+                    if (n > mat.Length - 1 || m < 0)
+                    {
+                        flag = true;
+                        n = Math.Min(n, mat.Length - 1);
+                        m = Math.Max(m, 0);
+                    }
+                    
+                }
+
+            }
+
+
+            return nums;
+        }
+    }
+}
