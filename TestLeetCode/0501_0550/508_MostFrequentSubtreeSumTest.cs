e7a89aa56d36d7740a2e26dcfee52e0d4c7f8d82
luohongtao <luohongtao621@hotmail.com>
2024-09-16 15:53:39 +0800

    508

---
+++ TestLeetCode/0501_0550/508_MostFrequentSubtreeSumTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._0501_0550;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _508_MostFrequentSubtreeSumTest
+    {
+        _508_MostFrequentSubtreeSumAlg alg = new _508_MostFrequentSubtreeSumAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [5, 2, -3];
+            int[] exp = [2, -3, 4];
+            TreeNode node = TreeNode.BuildTree(root);
+            int[] res = alg.FindFrequentTreeSum(node);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [5, 2, -5];
+            int[] exp = [2];
+            TreeNode node = TreeNode.BuildTree(root);
+            int[] res = alg.FindFrequentTreeSum(node);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+    }
+}
---
+++ leetCode/0501_0550/508_MostFrequentSubtreeSumAlg.cs
@@ -0,0 +1,47 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _508_MostFrequentSubtreeSumAlg
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        int max = 0;
+        public int[] FindFrequentTreeSum(TreeNode root)
+        {
+            Dfs(root);
+            List<int> list = new List<int>();
+
+            foreach (var item in dict)
+            {
+                if (item.Value == max)
+                {
+                    list.Add(item.Key);
+                }
+            }
+            return list.ToArray(); 
+        }
+        private int Dfs(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            
+            int sum = root.val+ Dfs(root.left) + Dfs(root.right);
+            if (dict.ContainsKey(sum))
+            {
+                dict[sum]++;
+                max=Math.Max(max, dict[sum]);
+            }
+            else
+            {
+                dict.Add(sum, 1);
+                max = Math.Max(max, 1);
+            }
+            return sum;
+        }
+    }
+}
