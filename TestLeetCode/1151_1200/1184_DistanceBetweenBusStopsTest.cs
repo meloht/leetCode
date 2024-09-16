4429806380a4172640493899bf899b56b9b21039
luohongtao <luohongtao621@hotmail.com>
2024-09-16 15:04:39 +0800

    1184

---
+++ TestLeetCode/1151_1200/1184_DistanceBetweenBusStopsTest.cs
@@ -0,0 +1,48 @@
+﻿using leetCode._1151_1200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1151_1200
+{
+    [TestClass]
+    public class _1184_DistanceBetweenBusStopsTest
+    {
+        _1184_DistanceBetweenBusStopsAlg alg = new _1184_DistanceBetweenBusStopsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] distance = [1, 2, 3, 4];
+            int start = 0, destination = 1;
+            int exp = 1;
+            int res = alg.DistanceBetweenBusStops(distance, start, destination);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] distance = [1, 2, 3, 4];
+            int start = 0, destination = 2;
+            int exp = 3;
+            int res = alg.DistanceBetweenBusStops(distance, start, destination);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] distance = [1, 2, 3, 4];
+            int start = 0, destination = 3;
+            int exp = 4;
+            int res = alg.DistanceBetweenBusStops(distance, start, destination);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/1151_1200/1184_DistanceBetweenBusStopsAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1151_1200
+{
+    public class _1184_DistanceBetweenBusStopsAlg
+    {
+        public int DistanceBetweenBusStops(int[] distance, int start, int destination)
+        {
+
+            if (start > destination)
+            {
+                int temp = start;
+                start = destination;
+                destination = temp;
+            }
+            int sum1 = 0, sum2 = 0;
+            for (int i = 0; i < distance.Length; i++)
+            {
+                if (i >= start && i < destination)
+                {
+                    sum1 += distance[i];
+                }
+                else
+                {
+                    sum2 += distance[i];
+                }
+            }
+            return Math.Min(sum1, sum2);
+
+        }
+    }
+}
