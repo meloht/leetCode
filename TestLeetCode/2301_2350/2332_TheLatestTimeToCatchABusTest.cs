4bcaafe79bb2aa7b31e8d700be8a402651b27d52
hongtao_luo <luohongtao621@hotmail.com>
2024-09-18 13:47:31 +0800

    2332

---
+++ TestLeetCode/2301_2350/2332_TheLatestTimeToCatchABusTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2301_2350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2301_2350
+{
+    [TestClass]
+    public class _2332_TheLatestTimeToCatchABusTest
+    {
+        _2332_TheLatestTimeToCatchABusAlg alg = new _2332_TheLatestTimeToCatchABusAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] buses = [10, 20], passengers = [2, 17, 18, 19];
+            int capacity = 2;
+            int exp = 16;
+            int res = alg.LatestTimeCatchTheBus(buses, passengers, capacity);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] buses = [20, 30, 10], passengers = [19, 13, 26, 4, 25, 11, 21];
+            int capacity = 2;
+            int exp = 20;
+            int res = alg.LatestTimeCatchTheBus(buses, passengers, capacity);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2301_2350/2332_TheLatestTimeToCatchABusAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2301_2350
+{
+    public class _2332_TheLatestTimeToCatchABusAlg
+    {
+        public int LatestTimeCatchTheBus(int[] buses, int[] passengers, int capacity)
+        {
+            Array.Sort(buses);
+            Array.Sort(passengers);
+
+            int max = buses[buses.Length - 1];
+
+            for (int i = passengers.Length - 1; i >= 0; i--)
+            {
+                if (passengers[i] > max)
+                {
+                    continue;
+                }
+                if (i == passengers.Length - 1 && passengers[i] < max)
+                {
+                    return passengers[i] + 1;
+                }
+
+                if (i < passengers.Length - 1 && passengers[i + 1] - passengers[i] > 1)
+                {
+                    return passengers[i] + 1;
+                }
+            }
+
+            return passengers[0] - 1;
+        }
+    }
+}
