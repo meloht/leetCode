76d56dfdc5ed6de61857b4c01d3aa6361b77bc2d
hongtao_luo <luohongtao621@hotmail.com>
2024-09-19 13:49:47 +0800

    2414

---
+++ TestLeetCode/2401_2450/2414_LengthOfTheLongestAlphabeticalContinuousSubstringTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2401_2450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2401_2450
+{
+    [TestClass]
+    public class _2414_LengthOfTheLongestAlphabeticalContinuousSubstringTest
+    {
+        _2414_LengthOfTheLongestAlphabeticalContinuousSubstringAlg alg = new _2414_LengthOfTheLongestAlphabeticalContinuousSubstringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abacaba";
+            int exp = 2;
+            int res = alg.LongestContinuousSubstring(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "abcde";
+            int exp = 5;
+            int res = alg.LongestContinuousSubstring(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2401_2450/2414_LengthOfTheLongestAlphabeticalContinuousSubstringAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2401_2450
+{
+    public class _2414_LengthOfTheLongestAlphabeticalContinuousSubstringAlg
+    {
+      
+        public int LongestContinuousSubstring(string s)
+        {
+            int ans = 1;
+            int count = 1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i - 1] + 1 == s[i])
+                {
+                    count++;
+                    ans = Math.Max(ans, count);
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            return ans;
+        }
+       
+
+    }
+}
