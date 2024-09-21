a6b9a225bdd3c48b805419fb5ef2f6d9fc2a7a9e
luohongtao <luohongtao621@hotmail.com>
2024-09-21 16:09:20 +0800

    2374

---
+++ TestLeetCode/2351_2400/2374_NodeWithHighestEdgeScoreTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2351_2400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2351_2400
+{
+    [TestClass]
+    public class _2374_NodeWithHighestEdgeScoreTest
+    {
+        _2374_NodeWithHighestEdgeScoreAlg alg = new _2374_NodeWithHighestEdgeScoreAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] edges = [1, 0, 0, 0, 0, 7, 7, 5];
+            int exp = 7;
+            int res = alg.EdgeScore(edges);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] edges = [2, 0, 0, 2];
+            int exp = 0;
+            int res = alg.EdgeScore(edges);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/2351_2400/2374_NodeWithHighestEdgeScoreAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2351_2400
+{
+    public class _2374_NodeWithHighestEdgeScoreAlg
+    {
+        public int EdgeScore(int[] edges)
+        {
+            long[] dict = new long[edges.Length];
+
+            int ans = 0;
+            for (int i = 0; i < edges.Length; i++)
+            {
+                int node = edges[i];
+                dict[node] += i;
+                if (dict[ans] < dict[node] || (dict[node] == dict[ans] && node < ans))
+                {
+                    ans = node;
+                }
+            }
+
+
+            return ans;
+        }
+    }
+}
