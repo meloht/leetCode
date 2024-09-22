9127e904d1365e1a928fa7daf749bd764ae8d461
luohongtao <luohongtao621@hotmail.com>
2024-09-22 08:56:09 +0800

    515

---
+++ TestLeetCode/0501_0550/515_FindLargestValueInEachTreeRowTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._0501_0550;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _515_FindLargestValueInEachTreeRowTest
+    {
+        _515_FindLargestValueInEachTreeRowAlg alg = new _515_FindLargestValueInEachTreeRowAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 3, 2, 5, 3, null, 9];
+            int[] exp = [1, 3, 9];
+            TreeNode node = TreeNode.BuildTree(root);
+            var res = alg.LargestValues(node);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1, 2, 3];
+            int[] exp = [1, 3];
+            TreeNode node = TreeNode.BuildTree(root);
+            var res = alg.LargestValues(node);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0501_0550/515_FindLargestValueInEachTreeRowAlg.cs
@@ -0,0 +1,43 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _515_FindLargestValueInEachTreeRowAlg
+    {
+        public IList<int> LargestValues(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            if (root == null)
+                return result;
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+                int max = int.MinValue;
+                for (int i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+                    max = Math.Max(max, node.val);
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+
+
+                }
+                result.Add(max);
+            }
+            return result;
+        }
+    }
+}
