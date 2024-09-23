22a87e630978dc9c12446c2f67610b1e65a4ad82
hongtao_luo <luohongtao621@hotmail.com>
2024-09-23 13:12:08 +0800

    1014

---
+++ TestLeetCode/1051_1100/1014_BestSightseeingPairTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1051_1100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1051_1100
+{
+    [TestClass]
+    public class _1014_BestSightseeingPairTest
+    {
+        _1014_BestSightseeingPairAlg alg = new _1014_BestSightseeingPairAlg();
+
+        [TestMethod]
+        public void TestCase01() 
+        {
+            int[] values = [8, 1, 5, 2, 6];
+            int exp = 11;
+            int res = alg.MaxScoreSightseeingPair(values);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] values = [1, 2];
+            int exp = 2;
+            int res = alg.MaxScoreSightseeingPair(values);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1051_1100/1014_BestSightseeingPairAlg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1051_1100
+{
+    public class _1014_BestSightseeingPairAlg
+    {
+        public int MaxScoreSightseeingPair(int[] values)
+        {
+            int ans = 0, mx = values[0] + 0;
+            for (int j = 1; j < values.Length; ++j)
+            {
+                ans = Math.Max(ans, mx + values[j] - j);
+                // 边遍历边维护
+                mx = Math.Max(mx, values[j] + j);
+            }
+            return ans;
+
+        }
+    }
+}
