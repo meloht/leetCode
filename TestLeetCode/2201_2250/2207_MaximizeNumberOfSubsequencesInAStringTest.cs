59f54ec027bd732b094de706e2057daa57fecfdc
hongtao_luo <luohongtao621@hotmail.com>
2024-09-24 13:18:09 +0800

    2207

---
+++ TestLeetCode/2201_2250/2207_MaximizeNumberOfSubsequencesInAStringTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._2201_2250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2201_2250
+{
+    [TestClass]
+    public class _2207_MaximizeNumberOfSubsequencesInAStringTest
+    {
+        _2207_MaximizeNumberOfSubsequencesInAStringAlg alg = new _2207_MaximizeNumberOfSubsequencesInAStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string text = "abdcdbc", pattern = "ac";
+            long exp = 4;
+            long res = alg.MaximumSubsequenceCount(text, pattern);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string text = "aabb", pattern = "ab";
+            long exp = 6;
+            long res = alg.MaximumSubsequenceCount(text, pattern);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string text = "zigfj", pattern = "ju";
+            long exp = 1;
+            long res = alg.MaximumSubsequenceCount(text, pattern);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string text = "fwymvreuftzgrcrxczjacqovduqaiig", pattern = "yy";
+            long exp = 1;
+            long res = alg.MaximumSubsequenceCount(text, pattern);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2201_2250/2207_MaximizeNumberOfSubsequencesInAStringAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2201_2250
+{
+    public class _2207_MaximizeNumberOfSubsequencesInAStringAlg
+    {
+        public long MaximumSubsequenceCount(string text, string pattern)
+        {
+            long res = 0, cnt1 = 0, cnt2 = 0;
+            foreach (char c in text)
+            {
+                if (c == pattern[1])
+                {
+                    res += cnt1;
+                    cnt2++;
+                }
+                if (c == pattern[0])
+                {
+                    cnt1++;
+                }
+            }
+            return res + Math.Max(cnt1, cnt2);
+        }
+    }
+}
