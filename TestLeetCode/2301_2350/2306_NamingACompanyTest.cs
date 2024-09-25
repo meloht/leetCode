8c71f4faf22527e5d6ae54b3ee748b31aaed3ee8
hongtao_luo <luohongtao621@hotmail.com>
2024-09-25 13:53:54 +0800

    2306

---
+++ TestLeetCode/2301_2350/2306_NamingACompanyTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2301_2350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2301_2350
+{
+    [TestClass]
+    public class _2306_NamingACompanyTest
+    {
+        _2306_NamingACompanyAlg alg = new _2306_NamingACompanyAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] ideas = ["coffee", "donuts", "time", "toffee"];
+            long exp = 6;
+            long res =alg.DistinctNames(ideas);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] ideas = ["lack", "back"];
+            long exp = 0;
+            long res = alg.DistinctNames(ideas);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2301_2350/2306_NamingACompanyAlg.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2301_2350
+{
+    public class _2306_NamingACompanyAlg
+    {
+        public long DistinctNames(string[] ideas)
+        {
+            Dictionary<char,HashSet<string>> dict=new Dictionary<char, HashSet<string>>();
+            foreach (string idea in ideas) 
+            {
+                dict.TryAdd(idea[0], new HashSet<string>());
+                dict[idea[0]].Add(idea.Substring(1));
+            }
+            long ans = 0;
+            foreach (KeyValuePair<char, HashSet<string>> pairA in dict)
+            {
+                char preA = pairA.Key;
+                ISet<string> setA = pairA.Value;
+                foreach (KeyValuePair<char, HashSet<string>> pairB in dict)
+                {
+                    char preB = pairB.Key;
+                    ISet<string> setB = pairB.Value;
+                    if (preA == preB)
+                    {
+                        continue;
+                    }
+                    int intersect = GetIntersectSize(setA, setB);
+                    ans += (long)(setA.Count - intersect) * (setB.Count - intersect);
+                }
+            }
+            return ans;
+
+
+        }
+        public int GetIntersectSize(ISet<string> a, ISet<string> b)
+        {
+            int ans = 0;
+            foreach (string s in a)
+            {
+                if (b.Contains(s))
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+
+    }
+}
