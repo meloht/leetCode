c669957e61c1de22f0a02a21cc2a8269a909524c
hongtao_luo <luohongtao621@hotmail.com>
2024-09-26 16:06:15 +0800

    2535

---
+++ TestLeetCode/2501_2550/2535_DifferenceBetweenElementSumAndDigitSumOfAnArrayTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2501_2550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2501_2550
+{
+    [TestClass]
+    public class _2535_DifferenceBetweenElementSumAndDigitSumOfAnArrayTest
+    {
+        _2535_DifferenceBetweenElementSumAndDigitSumOfAnArrayAlg alg = new _2535_DifferenceBetweenElementSumAndDigitSumOfAnArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 15, 6, 3];
+            int exp = 9;
+            int res = alg.DifferenceOfSum(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int exp = 0;
+            int res = alg.DifferenceOfSum(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2501_2550/2535_DifferenceBetweenElementSumAndDigitSumOfAnArrayAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2501_2550
+{
+    public class _2535_DifferenceBetweenElementSumAndDigitSumOfAnArrayAlg
+    {
+        public int DifferenceOfSum(int[] nums)
+        {
+            int sum = nums.Sum();
+
+            int total = 0;
+            foreach (var item in nums)
+            {
+                int n = item;
+                while (n > 0)
+                {
+                    total += (n % 10);
+                    n = n / 10;
+                }
+            }
+
+            return Math.Abs(sum - total);
+        }
+    }
+}
