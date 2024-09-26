7f8534e4326f3844c7085301d91494c43801714e
hongtao_luo <luohongtao621@hotmail.com>
2024-09-26 16:39:27 +0800

    517

---
+++ TestLeetCode/0501_0550/517_SuperWashingMachinesTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _517_SuperWashingMachinesTest
+    {
+        _517_SuperWashingMachinesAlg alg = new _517_SuperWashingMachinesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] machines = [1, 0, 5];
+            int exp = 3;
+            int res = alg.FindMinMoves(machines);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] machines = [0, 3, 0];
+            int exp = 2;
+            int res = alg.FindMinMoves(machines);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] machines = [0, 2, 0];
+            int exp = -1;
+            int res = alg.FindMinMoves(machines);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/517_SuperWashingMachinesAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _517_SuperWashingMachinesAlg
+    {
+        public int FindMinMoves(int[] machines)
+        {
+            int tot = machines.Sum();
+            int n = machines.Length;
+            if (tot % n != 0)
+            {
+                return -1;
+            }
+            int avg = tot / n;
+            int ans = 0, sum = 0;
+            foreach (int num in machines)
+            {
+                int tmp = num - avg;
+                sum += tmp;
+                ans = Math.Max(ans, Math.Max(Math.Abs(sum), tmp));
+            }
+            return ans;
+
+        }
+    }
+}
