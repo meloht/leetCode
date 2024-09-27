3a7a7347863897720337d6ca23247f31f78cd199
hongtao_luo <luohongtao621@hotmail.com>
2024-09-27 13:07:14 +0800

    2516

---
+++ TestLeetCode/2501_2550/2516_TakeKOfEachCharacterFromLeftAndRightTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2501_2550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2501_2550
+{
+    [TestClass]
+    public class _2516_TakeKOfEachCharacterFromLeftAndRightTest
+    {
+        _2516_TakeKOfEachCharacterFromLeftAndRightAlg alg=new _2516_TakeKOfEachCharacterFromLeftAndRightAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "aabaaaacaabc";
+            int k = 2;
+            int exp = 8;
+            int res = alg.TakeCharacters(s, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "a";
+            int k = 1;
+            int exp = -1;
+            int res = alg.TakeCharacters(s, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2501_2550/2516_TakeKOfEachCharacterFromLeftAndRightAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2501_2550
+{
+
+    public class _2516_TakeKOfEachCharacterFromLeftAndRightAlg
+    {
+        public int TakeCharacters(string s, int k)
+        {
+            int[] dict = new int[3];
+            foreach (var item in s)
+            {
+                dict[item - 'a']++;
+            }
+            if (dict[0] < k || dict[1] < k || dict[2] < k)
+                return -1;
+
+            int max = 0;
+            int j = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int c = s[i] - 'a';
+                dict[c]--;
+                while (dict[c] < k)
+                {
+                    dict[s[j] - 'a']++;
+                    j++;
+                }
+                max = Math.Max(max, i - j + 1);
+            }
+            return s.Length - max;
+        }
+    }
+}
