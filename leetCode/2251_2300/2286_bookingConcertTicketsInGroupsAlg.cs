63c94dde2790125ae8c7ebbf434d3e2ded2adef3
luohongtao <luohongtao621@hotmail.com>
2024-09-28 18:48:33 +0800

    2286

---
+++ leetCode/2251_2300/2286_bookingConcertTicketsInGroupsAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2286_bookingConcertTicketsInGroupsAlg
+    {
+        public class BookMyShow
+        {
+
+            public BookMyShow(int n, int m)
+            {
+
+            }
+
+            public int[] Gather(int k, int maxRow)
+            {
+
+            }
+
+            public bool Scatter(int k, int maxRow)
+            {
+
+            }
+        }
+    }
+}
