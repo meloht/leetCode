0d1e295763cf4755bfa9cc38543fe3402ebb8d65
hongtao_luo <luohongtao621@hotmail.com>
2024-09-30 13:10:20 +0800

    1845

---
+++ TestLeetCode/1801_1850/1845_SeatReservationManagerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._1801_1850._1845_SeatReservationManagerAlg;
+
+namespace Test._1801_1850
+{
+    [TestClass]
+    public class _1845_SeatReservationManagerTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            SeatManager seatManager = new SeatManager(5); // 初始化 SeatManager ，有 5 个座位。
+            Assert.AreEqual(1,seatManager.Reserve());    // 所有座位都可以预约，所以返回最小编号的座位，也就是 1 。
+            Assert.AreEqual(2, seatManager.Reserve());   // 可以预约的座位为 [2,3,4,5] ，返回最小编号的座位，也就是 2 。
+            seatManager.Unreserve(2); // 将座位 2 变为可以预约，现在可预约的座位为 [2,3,4,5] 。
+            Assert.AreEqual(2, seatManager.Reserve());    // 可以预约的座位为 [2,3,4,5] ，返回最小编号的座位，也就是 2 。
+            Assert.AreEqual(3, seatManager.Reserve());    // 可以预约的座位为 [3,4,5] ，返回最小编号的座位，也就是 3 。
+            Assert.AreEqual(4, seatManager.Reserve());  // 可以预约的座位为 [4,5] ，返回最小编号的座位，也就是 4 。
+            Assert.AreEqual(5, seatManager.Reserve());   // 唯一可以预约的是座位 5 ，所以返回 5 。
+            seatManager.Unreserve(5); // 将座位 5 变为可以预约，现在可预约的座位为 [5] 。
+        }
+    }
+}
---
+++ leetCode/1801_1850/1845_SeatReservationManagerAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1801_1850
+{
+    public class _1845_SeatReservationManagerAlg
+    {
+        public class SeatManager
+        {
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>();
+            public SeatManager(int n)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    queue.Enqueue(i, i);
+                }
+            }
+
+            public int Reserve()
+            {
+                return queue.Dequeue();
+            }
+
+            public void Unreserve(int seatNumber)
+            {
+                queue.Enqueue(seatNumber, seatNumber);
+            }
+        }
+    }
+}
