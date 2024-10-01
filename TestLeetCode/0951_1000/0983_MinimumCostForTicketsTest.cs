687211ac166b75efb3aa6533fbfda1c2a5a1b70f
luohongtao <luohongtao621@hotmail.com>
2024-10-01 11:24:18 +0800

    0983

---
+++ TestLeetCode/0951_1000/0983_MinimumCostForTicketsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0951_1000
+{
+    [TestClass]
+    public class _0983_MinimumCostForTicketsTest
+    {
+        _0983_MinimumCostForTicketsAlg alg = new _0983_MinimumCostForTicketsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] days = [1, 4, 6, 7, 8, 20], costs = [2, 7, 15];
+            int exp = 11;
+            int res = alg.MincostTickets(days, costs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31], costs = [2, 7, 15];
+            int exp = 17;
+            int res = alg.MincostTickets(days, costs);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0983_MinimumCostForTicketsAlg.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace leetCode._0951_1000
+{
+    public class _0983_MinimumCostForTicketsAlg
+    {
+        public int MincostTickets(int[] days, int[] costs)
+        {
+            int lastDay = days[days.Length - 1];
+            bool[] isTraval = new bool[lastDay + 1];
+            foreach (int d in days)
+            {
+                isTraval[d] = true;
+            }
+            int[] memo = new int[lastDay + 1];
+            return Dfs(lastDay,isTraval,costs,memo);
+        }
+        private int Dfs(int i, bool[] isTravel, int[] costs, int[] memo)
+        {
+            if (i <= 0)
+            {
+                return 0;
+            }
+            if (memo[i] > 0)
+            { // 之前计算过
+                return memo[i];
+            }
+            if (!isTravel[i])
+            {
+                return memo[i] = Dfs(i - 1, isTravel, costs, memo);
+            }
+            return memo[i] = Math.Min(Dfs(i - 1, isTravel, costs, memo) + costs[0],
+                             Math.Min(Dfs(i - 7, isTravel, costs, memo) + costs[1],
+                                      Dfs(i - 30, isTravel, costs, memo) + costs[2]));
+        }
+
+        public int MincostTickets1(int[] days, int[] costs)
+        {
+            int lastDay = days[days.Length - 1];
+            bool[] isTravel = new bool[lastDay + 1];
+            foreach (int d in days)
+            {
+                isTravel[d] = true;
+            }
+            int[] f = new int[lastDay + 1];
+            for (int i = 1; i <= lastDay; i++)
+            {
+                if (!isTravel[i])
+                {
+                    f[i] = f[i - 1];
+                }
+                else
+                {
+                    f[i] = Math.Min(f[i - 1] + costs[0],
+                           Math.Min(f[Math.Max(i - 7, 0)] + costs[1],
+                                    f[Math.Max(i - 30, 0)] + costs[2]));
+                }
+            }
+            return f[lastDay];
+        }
+
+
+
+    }
+}
