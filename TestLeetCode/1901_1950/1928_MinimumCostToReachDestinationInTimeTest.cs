82b53419233a1d968c85b3090294421b925f4f7d
luohongtao <luohongtao621@hotmail.com>
2024-10-03 10:21:59 +0800

    1928

---
+++ TestLeetCode/1901_1950/1928_MinimumCostToReachDestinationInTimeTest.cs
@@ -0,0 +1,48 @@
+﻿using leetCode._1901_1950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1901_1950
+{
+    [TestClass]
+    public class _1928_MinimumCostToReachDestinationInTimeTest
+    {
+        _1928_MinimumCostToReachDestinationInTimeAlg alg = new _1928_MinimumCostToReachDestinationInTimeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int maxTime = 30;
+            int[][] edges = [[0, 1, 10], [1, 2, 10], [2, 5, 10], [0, 3, 1], [3, 4, 10], [4, 5, 15]];
+            int[] passingFees = [5, 1, 2, 20, 20, 3];
+            int exp = 11;
+            int res = alg.MinCost(maxTime, edges, passingFees);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int maxTime = 29;
+            int[][] edges = [[0, 1, 10], [1, 2, 10], [2, 5, 10], [0, 3, 1], [3, 4, 10], [4, 5, 15]];
+            int[] passingFees = [5, 1, 2, 20, 20, 3];
+            int exp = 48;
+            int res = alg.MinCost(maxTime, edges, passingFees);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int maxTime = 25;
+            int[][] edges = [[0, 1, 10], [1, 2, 10], [2, 5, 10], [0, 3, 1], [3, 4, 10], [4, 5, 15]];
+            int[] passingFees = [5, 1, 2, 20, 20, 3];
+            int exp = -1;
+            int res = alg.MinCost(maxTime, edges, passingFees);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1901_1950/1928_MinimumCostToReachDestinationInTimeAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1901_1950
+{
+    public class _1928_MinimumCostToReachDestinationInTimeAlg
+    {
+        public int MinCost(int maxTime, int[][] edges, int[] passingFees)
+        {
+            int n = passingFees.Length;
+            int[,] f = new int[maxTime + 1, n];
+            for (int i = 0; i <= maxTime; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    f[i, j] = int.MaxValue;
+                }
+            }
+            f[0, 0] = passingFees[0];
+            for (int t = 1; t <= maxTime; t++)
+            {
+                foreach (var edge in edges)
+                {
+                    int i = edge[0], j = edge[1], cost = edge[2];
+                    if (cost <= t)
+                    {
+                        if (f[t - cost, j] != int.MaxValue)
+                        {
+                            f[t, i] = Math.Min(f[t, i], f[t - cost, j] + passingFees[i]);
+                        }
+                        if (f[t - cost, i] != int.MaxValue)
+                        {
+                            f[t, j] = Math.Min(f[t, j], f[t - cost, i] + passingFees[j]);
+                        }
+                    }
+                }
+            }
+
+            int ans = int.MaxValue;
+            for (int t = 1; t <= maxTime; t++)
+            {
+                ans = Math.Min(ans, f[t, n - 1]);
+            }
+            return ans == int.MaxValue ? -1 : ans;
+
+        }
+    }
+}
