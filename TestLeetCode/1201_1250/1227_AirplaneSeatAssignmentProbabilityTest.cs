0128cdd1fc88ce7c47ad07fa462333255dfa733a
luohongtao <luohongtao621@hotmail.com>
2024-10-04 09:48:45 +0800

    1227

---
+++ TestLeetCode/1201_1250/1227_AirplaneSeatAssignmentProbabilityTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1201_1250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1201_1250
+{
+    [TestClass]
+    public class _1227_AirplaneSeatAssignmentProbabilityTest
+    {
+        _1227_AirplaneSeatAssignmentProbabilityAlg alg = new _1227_AirplaneSeatAssignmentProbabilityAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 1;
+            double exp = 1;
+            double res = alg.NthPersonGetsNthSeat(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 2;
+            double exp = 0.5;
+            double res = alg.NthPersonGetsNthSeat(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1201_1250/1227_AirplaneSeatAssignmentProbabilityAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1201_1250
+{
+    public class _1227_AirplaneSeatAssignmentProbabilityAlg
+    {
+        public double NthPersonGetsNthSeat(int n)
+        {
+            return n == 1 ? 1 : 0.5;
+        }
+    }
+}
