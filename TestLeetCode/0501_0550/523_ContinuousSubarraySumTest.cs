46f1dc252ab6809c1267edd5a6989b9e85fe7d21
luohongtao <luohongtao621@hotmail.com>
2024-10-06 09:31:37 +0800

    523

---
+++ TestLeetCode/0501_0550/523_ContinuousSubarraySumTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _523_ContinuousSubarraySumTest
+    {
+        _523_ContinuousSubarraySumAlg alg = new _523_ContinuousSubarraySumAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [23, 2, 4, 6, 7];
+            int k = 6;
+            bool exp = true;
+            bool res = alg.CheckSubarraySum(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [23, 2, 6, 4, 7];
+            int k = 6;
+            bool exp = true;
+            bool res = alg.CheckSubarraySum(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [23, 2, 6, 4, 7];
+            int k = 13;
+            bool exp = false;
+            bool res = alg.CheckSubarraySum(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/523_ContinuousSubarraySumAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _523_ContinuousSubarraySumAlg
+    {
+        public bool CheckSubarraySum(int[] nums, int k)
+        {
+            int m = nums.Length;
+            if (m < 2)
+            {
+                return false;
+            }
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            dictionary.Add(0, -1);
+            int remainder = 0;
+            for (int i = 0; i < m; i++)
+            {
+                remainder = (remainder + nums[i]) % k;
+                if (dictionary.ContainsKey(remainder))
+                {
+                    int prevIndex = dictionary[remainder];
+                    if (i - prevIndex >= 2)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    dictionary.Add(remainder, i);
+                }
+            }
+            return false;
+        }
+
+    }
+}
