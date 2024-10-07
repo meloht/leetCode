a419d142f8c960c99e2ee02fdaa793bac19db0c2
luohongtao <luohongtao621@hotmail.com>
2024-10-07 10:28:09 +0800

    871

---
+++ TestLeetCode/0851_0900/871_MinimumNumberOfRefuelingStopsTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _871_MinimumNumberOfRefuelingStopsTest
+    {
+        _871_MinimumNumberOfRefuelingStopsAlg alg = new _871_MinimumNumberOfRefuelingStopsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int target = 1, startFuel = 1;
+            int[][] stations = [];
+            int exp = 0;
+            int res = alg.MinRefuelStops(target, startFuel, stations);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int target = 100, startFuel = 1;
+            int[][] stations = [[10, 100]];
+            int exp = -1;
+            int res = alg.MinRefuelStops(target, startFuel, stations);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int target = 100, startFuel = 10;
+            int[][] stations = [[10, 60], [20, 30], [30, 30], [60, 40]];
+            int exp = 2;
+            int res = alg.MinRefuelStops(target, startFuel, stations);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/871_MinimumNumberOfRefuelingStopsAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _871_MinimumNumberOfRefuelingStopsAlg
+    {
+        public int MinRefuelStops(int target, int startFuel, int[][] stations)
+        {
+            int n = stations.Length;
+            int ans = 0;
+            int prePosition = 0;
+            int curFuel = startFuel;
+            PriorityQueue<int,int> fuelHeap = new PriorityQueue<int,int>(Comparer<int>.Create((a,b)=>b.CompareTo(a))); // 最大堆
+            for (int i = 0; i <= n; i++)
+            {
+                int position = i < n ? stations[i][0] : target;
+                curFuel -= position - prePosition; // 每行驶 1 英里用掉 1 升汽油
+                while (fuelHeap.Count>0 && curFuel < 0)
+                { // 没油了
+                    curFuel += fuelHeap.Dequeue(); // 选油量最多的油桶
+                    ans++;
+                }
+                if (curFuel < 0)
+                { // 无法到达
+                    return -1;
+                }
+                int num = i < n ? stations[i][1] : 0;
+                fuelHeap.Enqueue(num, num); // 留着后面加油
+                prePosition = position;
+            }
+            return ans;
+
+
+        }
+    }
+}
