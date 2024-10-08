bcc91e9adc2d48b131911570c1effe55450b5e3d
luohongtao <luohongtao621@hotmail.com>
2024-10-08 20:12:33 +0800

    1436

---
+++ TestLeetCode/1401_1450/1436_DestinationCityTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._1401_1450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1401_1450
+{
+    [TestClass]
+    public class _1436_DestinationCityTest
+    {
+        _1436_DestinationCityAlg alg = new _1436_DestinationCityAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[][] paths = [["London", "New York"], ["New York", "Lima"], ["Lima", "Sao Paulo"]];
+            string exp = "Sao Paulo";
+            string res = alg.DestCity(paths);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[][] paths = [["B", "C"], ["D", "B"], ["C", "A"]];
+            string exp = "A";
+            string res = alg.DestCity(paths);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[][] paths = [["A", "Z"]];
+            string exp = "Z";
+            string res = alg.DestCity(paths);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1401_1450/1436_DestinationCityAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1401_1450
+{
+    public class _1436_DestinationCityAlg
+    {
+        public string DestCity(IList<IList<string>> paths)
+        {
+
+
+            HashSet<String> setA = new HashSet<string>(); // 预分配空间
+            foreach (var item in paths)
+            {
+                setA.Add(item[0]);
+            }
+
+            foreach (var p in paths)
+            {
+                if (!setA.Contains(p[1]))
+                {
+                    return p[1];
+                }
+            }
+            return "";
+
+
+        }
+    }
+}
