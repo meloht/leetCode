9033c631a98764d04ccccdc647b676f773c5f21b
hongtao_luo <luohongtao621@hotmail.com>
2024-10-10 12:00:27 +0800

    3171

---
+++ TestLeetCode/3151_4000/3171_FindSubarrayWithBitwiseOrClosestToKTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._3151_4000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_4000
+{
+    [TestClass]
+    public class _3171_FindSubarrayWithBitwiseOrClosestToKTest
+    {
+        _3171_FindSubarrayWithBitwiseOrClosestToKAlg alg = new _3171_FindSubarrayWithBitwiseOrClosestToKAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 4, 5];
+            int k = 3;
+            int exp = 0;
+            int res = alg.MinimumDifference(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 3, 1, 3];
+            int k = 2;
+            int exp = 1;
+            int res = alg.MinimumDifference(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1];
+            int k = 10;
+            int exp = 9;
+            int res = alg.MinimumDifference(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_4000/3171_FindSubarrayWithBitwiseOrClosestToKAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    public class _3171_FindSubarrayWithBitwiseOrClosestToKAlg
+    {
+        public int MinimumDifference(int[] nums, int k)
+        {
+            int ans = int.MaxValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int x = nums[i];
+                ans = Math.Min(ans, Math.Abs(x - k));
+                // 如果 x 是 nums[j] 的子集，就退出循环
+                for (int j = i - 1; j >= 0 && (nums[j] | x) != nums[j]; j--)
+                {
+                    nums[j] |= x;
+                    ans = Math.Min(ans, Math.Abs(nums[j] - k));
+                }
+            }
+            return ans;
+
+        }
+    }
+}
