ba072640726d6d1a7893ebf8e49c3c6e8b301364
hongtao_luo <luohongtao621@hotmail.com>
2024-10-10 12:20:53 +0800

    3162

---
+++ TestLeetCode/3151_4000/3162_FindTheNumberOfGoodPairsITest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3151_4000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_4000
+{
+    [TestClass]
+    public class _3162_FindTheNumberOfGoodPairsITest
+    {
+        _3162_FindTheNumberOfGoodPairsIAlg alg = new _3162_FindTheNumberOfGoodPairsIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [1, 3, 4], nums2 = [1, 3, 4];
+            int k = 1;
+            int exp = 5;
+            int res = alg.NumberOfPairs(nums1, nums2, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [1, 2, 4, 12], nums2 = [2, 4];
+            int k = 3;
+            int exp = 2;
+            int res = alg.NumberOfPairs(nums1, nums2, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_4000/3162_FindTheNumberOfGoodPairsIAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    public class _3162_FindTheNumberOfGoodPairsIAlg
+    {
+        public int NumberOfPairs(int[] nums1, int[] nums2, int k)
+        {
+
+            int ans = 0;
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                foreach (var item in nums2)
+                {
+                    if (nums1[i] % (item * k) == 0) 
+                    {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
