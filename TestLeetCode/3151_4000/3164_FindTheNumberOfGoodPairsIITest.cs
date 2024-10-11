d0a3ae605f90466ad522050fe37732398f87f0f8
hongtao_luo <luohongtao621@hotmail.com>
2024-10-11 13:15:01 +0800

    3164

---
+++ TestLeetCode/3151_4000/3164_FindTheNumberOfGoodPairsIITest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3151_4000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_4000
+{
+    [TestClass]
+    public class _3164_FindTheNumberOfGoodPairsIITest
+    {
+        _3164_FindTheNumberOfGoodPairsIIAlg alg = new _3164_FindTheNumberOfGoodPairsIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums1 = [1, 3, 4], nums2 = [1, 3, 4];
+            int k = 1;
+            long exp = 5;
+            long res = alg.NumberOfPairs(nums1, nums2, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums1 = [1, 2, 4, 12], nums2 = [2, 4];
+            int k = 3;
+            long exp = 2;
+            long res = alg.NumberOfPairs(nums1, nums2, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_4000/3164_FindTheNumberOfGoodPairsIIAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    public class _3164_FindTheNumberOfGoodPairsIIAlg
+    {
+        public long NumberOfPairs(int[] nums1, int[] nums2, int k)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            Dictionary<int, int> dict2 = new Dictionary<int, int>();
+            int max1 = 0;
+            foreach (var item in nums1)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+                max1 = Math.Max(max1, item);
+            }
+
+            foreach (var item in nums2)
+            {
+                if (dict2.ContainsKey(item))
+                {
+                    dict2[item]++;
+                }
+                else
+                {
+                    dict2.Add(item, 1);
+                }
+            }
+
+            long res = 0;
+            foreach (var item in dict2.Keys)
+            {
+                for (int i = item * k; i <= max1; i += item * k)
+                {
+                    if (dict.ContainsKey(i)) 
+                    {
+                        res += 1L * dict[i] * dict2[item];
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
