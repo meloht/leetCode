1707d30745ea9a0e5e712ecae794fc8130ad7b2d
hongtao_luo <luohongtao621@hotmail.com>
2024-10-12 13:23:29 +0800

    3158

---
+++ TestLeetCode/3151_4000/3158_FindTheXorOfNumbersWhichAppearTwiceTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3151_4000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_4000
+{
+    [TestClass]
+    public class _3158_FindTheXorOfNumbersWhichAppearTwiceTest
+    {
+        _3158_FindTheXorOfNumbersWhichAppearTwiceAlg alg = new _3158_FindTheXorOfNumbersWhichAppearTwiceAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 1, 3];
+            int exp = 1;
+            int res = alg.DuplicateNumbersXOR(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3];
+            int exp = 0;
+            int res = alg.DuplicateNumbersXOR(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 2, 2, 1];
+            int exp = 3;
+            int res = alg.DuplicateNumbersXOR(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_4000/3158_FindTheXorOfNumbersWhichAppearTwiceAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    public class _3158_FindTheXorOfNumbersWhichAppearTwiceAlg
+    {
+        public int DuplicateNumbersXOR(int[] nums)
+        {
+            int[] arr = new int[51];
+
+            foreach (var item in nums)
+            {
+                arr[item]++;
+            }
+            int ans = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 1)
+                {
+                    if (ans == 0)
+                    {
+                        ans = i;
+                    }
+                    else
+                    {
+                        ans = ans ^ i;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
