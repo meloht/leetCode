2c6f87ab4b9f7ed528af592a4cd28c9e55bb8321
luohongtao <luohongtao621@hotmail.com>
2024-10-13 13:15:20 +0800

    1884

---
+++ TestLeetCode/1851_1900/1884_EggDropWith2EggsAndNFloorsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1851_1900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1851_1900
+{
+    [TestClass]
+    public class _1884_EggDropWith2EggsAndNFloorsTest
+    {
+        _1884_EggDropWith2EggsAndNFloorsAlg alg = new _1884_EggDropWith2EggsAndNFloorsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 2;
+            int exp = 2;
+            int res = alg.TwoEggDrop(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 100;
+            int exp = 14;
+            int res = alg.TwoEggDrop(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1851_1900/1884_EggDropWith2EggsAndNFloorsAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1851_1900
+{
+    public class _1884_EggDropWith2EggsAndNFloorsAlg
+    {
+        public int TwoEggDrop(int n)
+        {
+            int[] f = new int[1001];
+            for (int i = 1; i < f.Length; i++)
+            {
+                f[i] = int.MaxValue;
+                for (int j = 1; j <= i; j++)
+                {
+                    f[i] = Math.Min(f[i], Math.Max(j, f[i - j] + 1));
+                }
+            }
+
+
+            return f[n];
+        }
+    }
+}
