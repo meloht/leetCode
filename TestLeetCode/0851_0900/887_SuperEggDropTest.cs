4cff15eabbd0388b9370bb445f630c6d4f50c79d
hongtao_luo <luohongtao621@hotmail.com>
2024-10-14 13:56:06 +0800

    887

---
+++ TestLeetCode/0851_0900/887_SuperEggDropTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _887_SuperEggDropTest
+    {
+        _887_SuperEggDropAlg alg = new _887_SuperEggDropAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int k = 1, n = 2;
+            int exp = 2;
+            int res = alg.SuperEggDrop(k, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int k = 2, n = 6;
+            int exp = 3;
+            int res = alg.SuperEggDrop(k, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int k = 3, n = 14;
+            int exp = 4;
+            int res = alg.SuperEggDrop(k, n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/887_SuperEggDropAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _887_SuperEggDropAlg
+    {
+        public int SuperEggDrop(int k, int n)
+        {
+            int[] f = new int[k + 1];
+            for (int i = 1; ; i++)
+            {
+                for (int j = k; j > 0; j--)
+                {
+                    f[j] += f[j - 1] + 1;
+                }
+                if (f[k] >= n)
+                {
+                    return i;
+                }
+            }
+
+        }
+    }
+}
