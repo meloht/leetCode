f1f0337e3e411ff59af5aab9b4d7db2eae0bb2da
hongtao_luo <luohongtao621@hotmail.com>
2024-10-15 13:59:40 +0800

    3200

---
+++ TestLeetCode/3151_4000/3200_MaximumHeightOfATriangleTest.cs
@@ -0,0 +1,68 @@
+﻿using leetCode._3151_4000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_4000
+{
+    [TestClass]
+    public class _3200_MaximumHeightOfATriangleTest
+    {
+        _3200_MaximumHeightOfATriangleAlg alg = new _3200_MaximumHeightOfATriangleAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int red = 2, blue = 4;
+            int exp = 3;
+            int res = alg.MaxHeightOfTriangle(red, blue);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int red = 2, blue = 1;
+            int exp = 2;
+            int res = alg.MaxHeightOfTriangle(red, blue);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int red = 1, blue = 1;
+            int exp = 1;
+            int res = alg.MaxHeightOfTriangle(red, blue);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int red = 10, blue = 1;
+            int exp = 2;
+            int res = alg.MaxHeightOfTriangle(red, blue);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int red = 9, blue = 3;
+            int exp = 3;
+            int res = alg.MaxHeightOfTriangle(red, blue);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            int red = 10, blue = 10;
+            int exp = 5;
+            int res = alg.MaxHeightOfTriangle(red, blue);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_4000/3200_MaximumHeightOfATriangleAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    public class _3200_MaximumHeightOfATriangleAlg
+    {
+        public int MaxHeightOfTriangle(int red, int blue)
+        {
+            return Math.Max(getMax(red, blue), getMax(blue, red));
+        }
+
+        private int getMax(int a, int b)
+        {
+            int index = 1;
+            for (int i = 0, j = 0; ;)
+            {
+                if (index % 2 == 1)
+                {
+                    i += index;
+                    if (i > a)
+                    {
+                        return index-1;
+                    }
+                   
+                }
+                else
+                {
+                    j += index;
+                    if (j > b)
+                    {
+                        return index-1;
+                    }
+                }
+               
+                index++;
+            }
+
+        }
+    }
+}
