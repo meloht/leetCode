02e27e877bf1d615a61be451ee3c89fd2e47fcca
hongtao_luo <luohongtao621@hotmail.com>
2024-10-16 14:17:20 +0800

    3194

---
+++ TestLeetCode/3151_4000/3194_MinimumAverageOfSmallestAndLargestElementsTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3151_4000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_4000
+{
+    [TestClass]
+    public class _3194_MinimumAverageOfSmallestAndLargestElementsTest
+    {
+        _3194_MinimumAverageOfSmallestAndLargestElementsAlg alg = new _3194_MinimumAverageOfSmallestAndLargestElementsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [7, 8, 3, 4, 15, 13, 4, 1];
+            double exp = 5.5;
+            double res = alg.MinimumAverage(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 9, 8, 3, 10, 5];
+            double exp = 5.5;
+            double res = alg.MinimumAverage(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 2, 3, 7, 8, 9];
+            double exp = 5.0;
+            double res = alg.MinimumAverage(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_4000/3194_MinimumAverageOfSmallestAndLargestElementsAlg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    public class _3194_MinimumAverageOfSmallestAndLargestElementsAlg
+    {
+        public double MinimumAverage(int[] nums)
+        {
+            Array.Sort(nums);
+            double min = int.MaxValue;
+            for (int i = 0, j = nums.Length - 1, n = nums.Length / 2; i < j && n > 0; i++, j--, n--)
+            {
+                double a = ((double)nums[i] + (double)nums[j]) / 2.0;
+                min=Math.Min(min, a);
+            }
+            return min;
+        }
+    }
+}
