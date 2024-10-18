dadf51a6f987f4a4058f88dc05592902da9b69e6
hongtao_luo <luohongtao621@hotmail.com>
2024-10-18 12:30:10 +0800

    3191

---
+++ TestLeetCode/3151_4000/3191_MinimumOperationsToMakeBinaryArrayElementsEqualToOneITest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3151_4000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_4000
+{
+    [TestClass]
+    public class _3191_MinimumOperationsToMakeBinaryArrayElementsEqualToOneITest
+    {
+        _3191_MinimumOperationsToMakeBinaryArrayElementsEqualToOneIAlg alg = new _3191_MinimumOperationsToMakeBinaryArrayElementsEqualToOneIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [0, 1, 1, 1, 0, 0];
+            int exp = 3;
+            int res = alg.MinOperations(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0, 1, 1, 1];
+            int exp = -1;
+            int res = alg.MinOperations(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_4000/3191_MinimumOperationsToMakeBinaryArrayElementsEqualToOneIAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    
+    public class _3191_MinimumOperationsToMakeBinaryArrayElementsEqualToOneIAlg
+    {
+        public int MinOperations(int[] nums)
+        {
+            int n = nums.Length;
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    if (i > n - 3)
+                    {
+                        return -1;
+                    }
+                    nums[i] ^= 1;
+                    nums[i + 1] ^= 1;
+                    nums[i + 2] ^= 1;
+                    ans++;
+                }
+            }
+            return ans;
+        }
+
+    }
+}
