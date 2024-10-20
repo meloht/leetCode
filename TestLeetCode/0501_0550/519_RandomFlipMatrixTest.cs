f8a4dd53b87d0dc80443e5f1deda5248777d17a2
luohongtao <luohongtao621@hotmail.com>
2024-10-20 09:53:25 +0800

    519

---
+++ TestLeetCode/0501_0550/519_RandomFlipMatrixTest.cs
@@ -0,0 +1,24 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _519_RandomFlipMatrixTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            _519_RandomFlipMatrixAlg.Solution solution = new _519_RandomFlipMatrixAlg.Solution(3, 1);
+            solution.Flip();  // 返回 [1, 0]，此时返回 [0,0]、[1,0] 和 [2,0] 的概率应当相同
+            solution.Flip();  // 返回 [2, 0]，因为 [1,0] 已经返回过了，此时返回 [2,0] 和 [0,0] 的概率应当相同
+            solution.Flip();  // 返回 [0, 0]，根据前面已经返回过的下标，此时只能返回 [0,0]
+            solution.Reset(); // 所有值都重置为 0 ，并可以再次选择下标返回
+            solution.Flip();  // 返回 [2, 0]，此时返回 [0,0]、[1,0] 和 [2,0] 的概率应当相同
+        }
+    }
+}
---
+++ leetCode/0501_0550/519_RandomFlipMatrixAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _519_RandomFlipMatrixAlg
+    {
+        public class Solution
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            int m, n, total;
+            Random rand = new Random();
+
+            public Solution(int m, int n)
+            {
+                this.m = m;
+                this.n = n;
+                this.total = m * n;
+            }
+
+            public int[] Flip()
+            {
+                int x = rand.Next(total);
+                total--;
+                // 查找位置 x 对应的映射
+                int idx = dictionary.ContainsKey(x) ? dictionary[x] : x;
+                // 将位置 x 对应的映射设置为位置 total 对应的映射
+                int value = dictionary.ContainsKey(total) ? dictionary[total] : total;
+                if (dictionary.ContainsKey(x))
+                {
+                    dictionary[x] = value;
+                }
+                else
+                {
+                    dictionary.Add(x, value);
+                }
+                return new int[] { idx / n, idx % n };
+            }
+
+            public void Reset()
+            {
+                total = m * n;
+                dictionary.Clear();
+            }
+
+        }
+    }
+}
