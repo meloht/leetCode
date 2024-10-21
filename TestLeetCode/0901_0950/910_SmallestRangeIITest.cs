61c52fd4cd0ee24d296e9d50c11fccd59eeb50f2
hongtao_luo <luohongtao621@hotmail.com>
2024-10-21 13:44:54 +0800

    910

---
+++ TestLeetCode/0901_0950/910_SmallestRangeIITest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0901_0950
+{
+    [TestClass]
+    public class _910_SmallestRangeIITest
+    {
+        _910_SmallestRangeIIAlg alg = new _910_SmallestRangeIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1];
+            int k = 0;
+            int exp = 0;
+            int res = alg.SmallestRangeII(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0, 10];
+            int k = 2;
+            int exp = 6;
+            int res = alg.SmallestRangeII(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 3, 6];
+            int k = 3;
+            int exp = 3;
+            int res = alg.SmallestRangeII(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/910_SmallestRangeIIAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._0901_0950
+{
+    public class _910_SmallestRangeIIAlg
+    {
+        public int SmallestRangeII(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int n = nums.Length;
+            int ans = nums[n - 1] - nums[0];
+            for (int i = 1; i < n; i++)
+            {
+                int mx = Math.Max(nums[i - 1] + k, nums[n - 1] - k);
+                int mn = Math.Min(nums[0] + k, nums[i] - k);
+                ans = Math.Min(ans, mx - mn);
+            }
+            return ans;
+
+        }
+    }
+}
