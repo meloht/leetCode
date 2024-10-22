f35d02c89c1f65fadb4c5e1626a0de742b4c1f99
hongtao_luo <luohongtao621@hotmail.com>
2024-10-22 12:13:49 +0800

    3184

---
+++ leetCode/3151_4000/3184_CountPairsThatFormACompleteDayIAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    public class _3184_CountPairsThatFormACompleteDayIAlg
+    {
+        public int CountCompleteDayPairs(int[] hours)
+        {
+            int ans = 0;
+            for (int i = 0; i < hours.Length; i++)
+            {
+                for (int j = i + 1; j < hours.Length; j++)
+                {
+                    int num = hours[i] + hours[j];
+                    if (num >= 24 && num % 24 == 0)
+                    {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
