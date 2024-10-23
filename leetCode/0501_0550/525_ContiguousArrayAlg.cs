9bb6971e9fd1a375848d81f463dd5d6f14de2c9e
hongtao_luo <luohongtao621@hotmail.com>
2024-10-23 13:58:09 +0800

    525

---
+++ leetCode/0501_0550/525_ContiguousArrayAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _525_ContiguousArrayAlg
+    {
+        public int FindMaxLength(int[] nums)
+        {
+            int maxLength = 0;
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            int counter = 0;
+            dictionary.Add(counter, -1);
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int num = nums[i];
+                if (num == 1)
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter--;
+                }
+                if (dictionary.ContainsKey(counter))
+                {
+                    int prevIndex = dictionary[counter];
+                    maxLength = Math.Max(maxLength, i - prevIndex);
+                }
+                else
+                {
+                    dictionary.Add(counter, i);
+                }
+            }
+            return maxLength;
+
+        }
+    }
+}
