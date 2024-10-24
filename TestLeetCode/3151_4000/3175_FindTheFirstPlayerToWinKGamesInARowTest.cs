71594adeba0cb899577667a90a05d963f4da777b
hongtao_luo <luohongtao621@hotmail.com>
2024-10-24 12:44:31 +0800

    3175

---
+++ TestLeetCode/3151_4000/3175_FindTheFirstPlayerToWinKGamesInARowTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._3151_4000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_4000
+{
+    [TestClass]
+    public class _3175_FindTheFirstPlayerToWinKGamesInARowTest
+    {
+        _3175_FindTheFirstPlayerToWinKGamesInARowAlg alg = new _3175_FindTheFirstPlayerToWinKGamesInARowAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] skills = [4, 2, 6, 3, 9]; 
+            int k = 2;
+            int exp = 2;
+            int res = alg.FindWinningPlayer(skills, 2);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] skills = [2, 5, 4];
+            int k = 3;
+            int exp = 1;
+            int res = alg.FindWinningPlayer(skills, 2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_4000/3175_FindTheFirstPlayerToWinKGamesInARowAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_4000
+{
+    public class _3175_FindTheFirstPlayerToWinKGamesInARowAlg
+    {
+        public int FindWinningPlayer(int[] skills, int k)
+        {
+            int maxI = 0;
+            int win = 0;
+            for (int i = 1; i < skills.Length && win < k; i++)
+            {
+                if (skills[i] > skills[maxI])
+                { // 打擂台，发现新的最大值
+                    maxI = i;
+                    win = 0;
+                }
+                win++; // 获胜回合 +1
+            }
+            // 如果 k 很大，那么 maxI 就是 skills 最大值的下标，毕竟最大值会一直赢下去
+            return maxI;
+        }
+    }
+}
