e6501de081051f5fc0e53b728b294445df239d99
luohongtao <luohongtao621@hotmail.com>
2024-10-26 14:27:52 +0800

    538

---
+++ TestLeetCode/0501_0550/538_ConvertBstToGreaterTreeTest.cs
@@ -0,0 +1,61 @@
+﻿using leetCode._0501_0550;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _538_ConvertBstToGreaterTreeTest
+    {
+        _538_ConvertBstToGreaterTreeAlg alg = new _538_ConvertBstToGreaterTreeAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] arr = [4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8];
+            TreeNode root = TreeNode.BuildTree(arr);
+            int?[] exp = [30, 36, 21, 36, 35, 26, 15, null, null, null, 33, null, null, null, 8];
+            TreeNode res = alg.ConvertBST(root);
+            int?[] resArr = TreeNode.GetTreeLevelList(res);
+            Assert.IsTrue(exp.SequenceEqual(resArr));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] arr = [0, null, 1];
+            TreeNode root = TreeNode.BuildTree(arr);
+            int?[] exp = [1, null, 1];
+            TreeNode res = alg.ConvertBST(root);
+            int?[] resArr = TreeNode.GetTreeLevelList(res);
+            Assert.IsTrue(exp.SequenceEqual(resArr));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] arr = [1, 0, 2];
+            TreeNode root = TreeNode.BuildTree(arr);
+            int?[] exp = [3, 3, 2];
+            TreeNode res = alg.ConvertBST(root);
+            int?[] resArr = TreeNode.GetTreeLevelList(res);
+            Assert.IsTrue(exp.SequenceEqual(resArr));
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int?[] arr = [3, 2, 4, 1];
+            TreeNode root = TreeNode.BuildTree(arr);
+            int?[] exp = [7, 9, 4, 10];
+            TreeNode res = alg.ConvertBST(root);
+            int?[] resArr = TreeNode.GetTreeLevelList(res);
+            Assert.IsTrue(exp.SequenceEqual(resArr));
+        }
+    }
+}
---
+++ leetCode/0501_0550/538_ConvertBstToGreaterTreeAlg.cs
@@ -0,0 +1,57 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _538_ConvertBstToGreaterTreeAlg
+    {
+        int index = 0;
+        public TreeNode ConvertBST(TreeNode root)
+        {
+            if (root == null)
+                return root;
+            List<int> list = new List<int>();
+            Dfs(root, list);
+            int[] arr = new int[list.Count];
+            arr[arr.Length - 1] = list[arr.Length - 1];
+            for (int i = arr.Length - 2; i >= 0; i--)
+            {
+                arr[i] = list[i] + arr[i + 1];
+            }
+            index = 0;
+            DfsVal(root, arr);
+            return root;
+        }
+
+        private void Dfs(TreeNode root, List<int> list)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            Dfs(root.left, list);
+            list.Add(root.val);
+            Dfs(root.right, list);
+
+        }
+
+        private void DfsVal(TreeNode root, int[] arr)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            DfsVal(root.left, arr);
+            root.val = arr[index];
+            index++;
+           
+            DfsVal(root.right, arr);
+
+        }
+    }
+}
