14c22e69884369ece7ac53e672d37a8732368f3b
luohongtao <luohongtao621@hotmail.com>
2024-10-26 13:19:47 +0800

    532

---
+++ TestLeetCode/0501_0550/532_KDiffPairsInAnArrayTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _532_KDiffPairsInAnArrayTest
+    {
+        _532_KDiffPairsInAnArrayAlg alg = new _532_KDiffPairsInAnArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 1, 4, 1, 5];
+            int k = 2;
+            int exp = 2;
+            int res = alg.FindPairs(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4, 5];
+            int k = 1;
+            int exp = 4;
+            int res = alg.FindPairs(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 3, 1, 5, 4];
+            int k = 0;
+            int exp = 1;
+            int res = alg.FindPairs(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [1, 2, 4, 4, 3, 3, 0, 9, 2, 3];
+            int k = 3;
+            int exp = 2;
+            int res = alg.FindPairs(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/532_KDiffPairsInAnArrayAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _532_KDiffPairsInAnArrayAlg
+    {
+        public int FindPairs(int[] nums, int k)
+        {
+
+            HashSet<int> visited = new HashSet<int>();
+            HashSet<int> res = new HashSet<int>();
+            foreach (int num in nums)
+            {
+                if (visited.Contains(num - k))
+                {
+                    res.Add(num - k);
+                }
+                if (visited.Contains(num + k))
+                {
+                    res.Add(num);
+                }
+                visited.Add(num);
+            }
+            return res.Count;
+
+
+        }
+    }
+}
