fad1a52b0f19ac35fd709cfa50f93ef689885b28
luohongtao <luohongtao621@hotmail.com>
2024-10-26 13:46:02 +0800

    537

---
+++ TestLeetCode/0501_0550/537_ComplexNumberMultiplicationTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _537_ComplexNumberMultiplicationTest
+    {
+        _537_ComplexNumberMultiplicationAlg alg = new _537_ComplexNumberMultiplicationAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string num1 = "1+1i", num2 = "1+1i";
+            string exp = "0+2i";
+            string res=alg.ComplexNumberMultiply(num1, num2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string num1 = "1+-1i", num2 = "1+-1i";
+            string exp = "0+-2i";
+            string res = alg.ComplexNumberMultiply(num1, num2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/537_ComplexNumberMultiplicationAlg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _537_ComplexNumberMultiplicationAlg
+    {
+        public string ComplexNumberMultiply(string num1, string num2)
+        {
+            string[] complex1 = num1.Split(new char[2] { '+', 'i' });
+            string[] complex2 = num2.Split(new char[2] { '+', 'i' });
+            int real1 = int.Parse(complex1[0]);
+            int imag1 = int.Parse(complex1[1]);
+            int real2 = int.Parse(complex2[0]);
+            int imag2 = int.Parse(complex2[1]);
+            return $"{real1 * real2 - imag1 * imag2}+{real1 * imag2 + imag1 * real2}i";
+
+
+        }
+    }
+}
