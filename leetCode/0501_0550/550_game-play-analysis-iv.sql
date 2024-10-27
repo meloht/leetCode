977159ede1d495358bda9dc679ca7ee376300c95
luohongtao <luohongtao621@hotmail.com>
2024-10-27 12:42:40 +0800

    550

---
+++ leetCode/0501_0550/550_game-play-analysis-iv.sql
@@ -0,0 +1,4 @@
+﻿
+select CONVERT((select count(aa.player_id) from Activity aa inner join 
+(select player_id, adddate(min(event_date),interval 1 day) as event_date from Activity group by player_id) tt on aa.player_id=tt.player_id and aa.event_date=tt.event_date
+)/(select count(distinct player_id) from Activity ),DECIMAL(15,2)) AS fraction
