e12b6fbb47fb2e9f97373a10105278b6a1d2899b
luohongtao <luohongtao621@hotmail.com>
2024-10-27 12:21:00 +0800

    546

---
+++ TestLeetCode/0501_0550/546_RemoveBoxesTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0501_0550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _546_RemoveBoxesTest
+    {
+        _546_RemoveBoxesAlg alg = new _546_RemoveBoxesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] boxes = [1, 3, 2, 2, 2, 3, 4, 3, 1];
+            int exp = 23;
+            int res = alg.RemoveBoxes(boxes);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] boxes = [1, 1, 1];
+            int exp = 9;
+            int res = alg.RemoveBoxes(boxes);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] boxes = [1];
+            int exp = 1;
+            int res = alg.RemoveBoxes(boxes);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0501_0550/546_RemoveBoxesAlg.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0501_0550
+{
+    public class _546_RemoveBoxesAlg
+    {
+        int[][][] dp;
+        public int RemoveBoxes(int[] boxes)
+        {
+            int length = boxes.Length;
+            dp = new int[length][][];
+            for (int i = 0; i < length; i++)
+            {
+                dp[i] = new int[length][];
+                for (int j = 0; j < length; j++)
+                {
+                    dp[i][j] = new int[length];
+                }
+
+            }
+            return calculatePoints(boxes, 0, length - 1, 0);
+
+        }
+
+        public int calculatePoints(int[] boxes, int l, int r, int k)
+        {
+            if (l > r)
+            {
+                return 0;
+            }
+            if (dp[l][r][k] == 0)
+            {
+                int r1 = r, k1 = k;
+                while (r1 > l && boxes[r1] == boxes[r1 - 1])
+                {
+                    r1--;
+                    k1++;
+                }
+                dp[l][r][k] = calculatePoints(boxes, l, r1 - 1, 0) + (k1 + 1) * (k1 + 1);
+                for (int i = l; i < r1; i++)
+                {
+                    if (boxes[i] == boxes[r1])
+                    {
+                        dp[l][r][k] = Math.Max(dp[l][r][k], calculatePoints(boxes, l, i, k1 + 1) + calculatePoints(boxes, i + 1, r1 - 1, 0));
+                    }
+                }
+            }
+            return dp[l][r][k];
+        }
+
+
+    }
+}
