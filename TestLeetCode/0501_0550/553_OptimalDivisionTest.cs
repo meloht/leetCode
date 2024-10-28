f8381f9256ddc92afe55cb02cb812ba89a1e5d7c
hongtao_luo <luohongtao621@hotmail.com>
2024-10-28 12:58:27 +0800

    553

---
+++ TestLeetCode/0501_0550/553_OptimalDivisionTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0501_0550
+{
+    [TestClass]
+    public class _553_OptimalDivisionTest
+    {
+        _553_OptimalDivisionAlg alg = new _553_OptimalDivisionAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1000, 100, 10, 2];
+            string exp = "1000/(100/10/2)";
+            string res = alg.OptimalDivision(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 3, 4];
+            string exp = "2/(3/4)";
+            string res = alg.OptimalDivision(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/553_OptimalDivisionAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _553_OptimalDivisionAlg
+    {
+        public string OptimalDivision(int[] nums)
+        {
+            int n = nums.Length;
+            if (n == 1)
+            {
+                return nums[0].ToString();
+            }
+            if (n == 2)
+            {
+                return nums[0].ToString() + "/" + nums[1].ToString();
+            }
+            StringBuilder res = new StringBuilder();
+            res.Append(nums[0]);
+            res.Append("/(");
+            res.Append(nums[1]);
+            for (int i = 2; i < n; i++)
+            {
+                res.Append("/");
+                res.Append(nums[i]);
+            }
+            res.Append(")");
+            return res.ToString();
+
+        }
+    }
+}
