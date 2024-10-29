6b0927c8d74c018e1e36228c5fe92810aff62208
hongtao_luo <luohongtao621@hotmail.com>
2024-10-29 13:41:03 +0800

    3211

---
+++ TestLeetCode/3201_3250/3211_GenerateBinaryStringsWithoutAdjacentZerosTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3211_GenerateBinaryStringsWithoutAdjacentZerosTest
+    {
+        _3211_GenerateBinaryStringsWithoutAdjacentZerosAlg alg = new _3211_GenerateBinaryStringsWithoutAdjacentZerosAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3;
+            IList<string> exp = ["010", "011", "101", "110", "111"];
+            IList<string> res = alg.ValidStrings(n);
+
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1;
+            IList<string> exp = ["0", "1"];
+            IList<string> res = alg.ValidStrings(n);
+
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/3201_3250/3211_GenerateBinaryStringsWithoutAdjacentZerosAlg.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3211_GenerateBinaryStringsWithoutAdjacentZerosAlg
+    {
+        IList<string> res = new List<string>();
+        int n;
+
+        public IList<string> ValidStrings(int n)
+        {
+            this.n = n;
+            Dfs(new StringBuilder());
+            return res;
+        }
+
+        public void Dfs(StringBuilder sb)
+        {
+            if (sb.Length == n)
+            {
+                res.Add(sb.ToString());
+            }
+            else
+            {
+                if (sb.Length == 0 || sb[sb.Length - 1] == '1')
+                {
+                    sb.Append('0');
+                    Dfs(sb);
+                    sb.Length--;
+                }
+                sb.Append('1');
+                Dfs(sb);
+                sb.Length--;
+            }
+        }
+
+        public List<string> ValidStrings2(int n)
+        {
+            List<string> res = new List<string>();
+            int mask = (1 << n) - 1;
+            for (int i = 0; i < 1 << n; i++)
+            {
+                int t = mask ^ i;
+                if (((t >> 1) & t) == 0)
+                {
+                    string str = Convert.ToString(i,2); 
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = n - str.Length; j > 0; j--)
+                    {
+                        sb.Append('0');
+                    }
+                    sb.Append(str);
+                    res.Add(sb.ToString());
+                }
+            }
+            return res;
+        }
+
+
+    }
+}
