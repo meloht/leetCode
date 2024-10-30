c42787336e028ebb1e0c4174c525422654949146
hongtao_luo <luohongtao621@hotmail.com>
2024-10-30 13:15:52 +0800

    3216

---
+++ TestLeetCode/3201_3250/3216_LexicographicallySmallestStringAfterASwapTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3216_LexicographicallySmallestStringAfterASwapTest
+    {
+        _3216_LexicographicallySmallestStringAfterASwapAlg alg = new _3216_LexicographicallySmallestStringAfterASwapAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "45320";
+            string exp = "43520";
+            string res = alg.GetSmallestString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "001";
+            string exp = "001";
+            string res = alg.GetSmallestString(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3216_LexicographicallySmallestStringAfterASwapAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3216_LexicographicallySmallestStringAfterASwapAlg
+    {
+        public string GetSmallestString(string s)
+        {
+            char[] chars = s.ToCharArray();
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+
+                if (s[i] % 2 == s[i + 1] % 2 && s[i] > s[i + 1])
+                {
+                    char temp = chars[i];
+                    chars[i] = chars[i + 1];
+                    chars[i + 1] = temp;
+                    break;
+                }
+            }
+            return new string(chars);
+        }
+    }
+}
