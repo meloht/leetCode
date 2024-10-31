a6e355a150ab3c54eef6900e4c6496a87598b8bb
hongtao_luo <luohongtao621@hotmail.com>
2024-10-31 13:35:18 +0800

    3165

---
+++ TestLeetCode/3151_3200/3165_MaximumSumOfSubsequenceWithNonAdjacentElementsTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3151_3200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_3200
+{
+    [TestClass]
+    public class _3165_MaximumSumOfSubsequenceWithNonAdjacentElementsTest
+    {
+        _3165_MaximumSumOfSubsequenceWithNonAdjacentElementsAlg alg = new _3165_MaximumSumOfSubsequenceWithNonAdjacentElementsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [3, 5, 9];
+            int[][] queries = [[1, -2], [0, -3]];
+            int exp = 21;
+            int res=alg.MaximumSumSubsequence(nums, queries);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0, -1];
+            int[][] queries = [[0, -5]];
+            int exp = 0;
+            int res = alg.MaximumSumSubsequence(nums, queries);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_3200/3165_MaximumSumOfSubsequenceWithNonAdjacentElementsAlg.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_3200
+{
+    public class _3165_MaximumSumOfSubsequenceWithNonAdjacentElementsAlg
+    {
+        public int MaximumSumSubsequence(int[] nums, int[][] queries)
+        {
+
+            int n = nums.Length;
+            // 4 个数分别保存 f00, f01, f10, f11
+            long[][] t = new long[2 << (32 - int.LeadingZeroCount(n))][];
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i] = new long[4];
+            }
+            build(t, nums, 1, 0, n - 1);
+
+            long ans = 0;
+            foreach (int[] q in queries)
+            {
+                update(t, 1, 0, n - 1, q[0], q[1]);
+                ans += t[1][3]; // 注意 f11 没有任何限制，也就是整个数组的打家劫舍
+            }
+            return (int)(ans % 1_000_000_007);
+        }
+       
+
+        // 合并左右儿子
+        private void maintain(long[][] t, int o)
+        {
+            long[] a = t[o * 2];
+            long[] b = t[o * 2 + 1];
+            t[o][0] = Math.Max(a[0] + b[2], a[1] + b[0]);
+            t[o][1] = Math.Max(a[0] + b[3], a[1] + b[1]);
+            t[o][2] = Math.Max(a[2] + b[2], a[3] + b[0]);
+            t[o][3] = Math.Max(a[2] + b[3], a[3] + b[1]);
+        }
+
+        // 用 nums 初始化线段树
+        private void build(long[][] t, int[] nums, int o, int l, int r)
+        {
+            if (l == r)
+            {
+                t[o][3] = Math.Max(nums[l], 0);
+                return;
+            }
+            int m = (l + r) / 2;
+            build(t, nums, o * 2, l, m);
+            build(t, nums, o * 2 + 1, m + 1, r);
+            maintain(t, o);
+        }
+
+        // 把 nums[i] 改成 val
+        private void update(long[][] t, int o, int l, int r, int i, int val)
+        {
+            if (l == r)
+            {
+                t[o][3] = Math.Max(val, 0);
+                return;
+            }
+            int m = (l + r) / 2;
+            if (i <= m)
+            {
+                update(t, o * 2, l, m, i, val);
+            }
+            else
+            {
+                update(t, o * 2 + 1, m + 1, r, i, val);
+            }
+            maintain(t, o);
+        }
+
+
+
+    }
+}
