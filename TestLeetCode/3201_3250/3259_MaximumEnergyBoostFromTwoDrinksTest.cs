b6ef4b27369445d3f1f8b6c7c14d2bf4b9233ba2
hongtao_luo <luohongtao621@hotmail.com>
2024-11-01 13:04:32 +0800

    3259

---
+++ TestLeetCode/3201_3250/3259_MaximumEnergyBoostFromTwoDrinksTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3259_MaximumEnergyBoostFromTwoDrinksTest
+    {
+        _3259_MaximumEnergyBoostFromTwoDrinksAlg alg = new _3259_MaximumEnergyBoostFromTwoDrinksAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] energyDrinkA = [1, 3, 1], energyDrinkB = [3, 1, 1];
+            long exp = 5;
+            long res = alg.MaxEnergyBoost(energyDrinkA,energyDrinkB);
+            Assert.AreEqual(exp,res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] energyDrinkA = [4, 1, 1], energyDrinkB = [1, 1, 3];
+            long exp = 7;
+            long res = alg.MaxEnergyBoost(energyDrinkA, energyDrinkB);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3259_MaximumEnergyBoostFromTwoDrinksAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3259_MaximumEnergyBoostFromTwoDrinksAlg
+    {
+        public long MaxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB)
+        {
+            int n = energyDrinkA.Length;
+            long[,] d = new long[n + 1, 2];
+            for (int i = 1; i <= n; i++)
+            {
+                d[i, 0] = d[i - 1, 0] + energyDrinkA[i - 1];
+                d[i, 1] = d[i - 1, 1] + energyDrinkB[i - 1];
+                if (i >= 2)
+                {
+                    d[i, 0] = Math.Max(d[i, 0], d[i - 2, 1] + energyDrinkA[i - 1]);
+                    d[i, 1] = Math.Max(d[i, 1], d[i - 2, 0] + energyDrinkB[i - 1]);
+                }
+            }
+            return Math.Max(d[n, 0], d[n, 1]);
+
+        }
+    }
+}
