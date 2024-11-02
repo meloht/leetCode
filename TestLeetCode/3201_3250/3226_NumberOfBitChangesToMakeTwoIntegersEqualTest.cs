11190360459c6b097b519f84077739ddbd05e83d
luohongtao <luohongtao621@hotmail.com>
2024-11-02 11:29:24 +0800

    3226

---
+++ TestLeetCode/3201_3250/3226_NumberOfBitChangesToMakeTwoIntegersEqualTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3226_NumberOfBitChangesToMakeTwoIntegersEqualTest
+    {
+        _3226_NumberOfBitChangesToMakeTwoIntegersEqualAlg alg = new _3226_NumberOfBitChangesToMakeTwoIntegersEqualAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 13, k = 4;
+            int exp = 2;
+            int res = alg.MinChanges(n, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 21, k = 21;
+            int exp = 0;
+            int res = alg.MinChanges(n, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 14, k = 13;
+            int exp = -1;
+            int res = alg.MinChanges(n, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3226_NumberOfBitChangesToMakeTwoIntegersEqualAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3226_NumberOfBitChangesToMakeTwoIntegersEqualAlg
+    {
+        public int MinChanges(int n, int k)
+        {
+            return (n & k) != k ? -1 : int.PopCount(n ^ k);
+        }
+    }
+}
