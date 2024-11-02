5e869660809b9587c9e11b141ae82541d0fc3764
luohongtao <luohongtao621@hotmail.com>
2024-11-02 12:56:20 +0800

    554

---
+++ TestLeetCode/0551_0600/554_BrickWallTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _554_BrickWallTest
+    {
+        _554_BrickWallAlg alg = new _554_BrickWallAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] wall = [[1, 2, 2, 1], [3, 1, 2], [1, 3, 2], [2, 4], [3, 1, 2], [1, 3, 1, 1]];
+            int exp = 2;
+            int res = alg.LeastBricks(wall);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] wall = [[1], [1], [1]];
+            int exp = 3;
+            int res = alg.LeastBricks(wall);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/554_BrickWallAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _554_BrickWallAlg
+    {
+        public int LeastBricks(IList<IList<int>> wall)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+        
+            for (int i = 0; i < wall.Count; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < wall[i].Count-1; j++)
+                {
+                    sum += wall[i][j];
+                    if (dict.ContainsKey(sum))
+                    {
+                        dict[sum]++;
+                    }
+                    else
+                    {
+                        dict.Add(sum, 1);
+                    }
+                }
+
+            }
+
+
+            int ans = wall.Count;
+            if (dict.Count > 0)
+            {
+                ans = ans - dict.Values.Max();
+            }
+
+            return ans;
+        }
+    }
+}
