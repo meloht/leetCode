cbf8df76cee6c1fdf325149db322d254d6dcb6b2
hongtao_luo <luohongtao621@hotmail.com>
2024-11-04 13:43:17 +0800

    633

---
+++ TestLeetCode/0601_0650/633_SumOfSquareNumbersTest.cs
@@ -0,0 +1,49 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _633_SumOfSquareNumbersTest
+    {
+        _633_SumOfSquareNumbersAlg alg = new _633_SumOfSquareNumbersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int c = 5;
+            bool exp = true;
+            bool res = alg.JudgeSquareSum(c);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int c = 3;
+            bool exp = false;
+            bool res = alg.JudgeSquareSum(c);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int c = 4;
+            bool exp = true;
+            bool res = alg.JudgeSquareSum(c);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int c = 2147483646;
+            bool exp = false;
+            bool res = alg.JudgeSquareSum(c);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/633_SumOfSquareNumbersAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _633_SumOfSquareNumbersAlg
+    {
+        public bool JudgeSquareSum(int c)
+        {
+
+            for (int a = 0; a * a <= c / 2; a++)
+            {
+                int b = (int)Math.Sqrt(c - a * a);
+                if (a * a + b * b == c)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
