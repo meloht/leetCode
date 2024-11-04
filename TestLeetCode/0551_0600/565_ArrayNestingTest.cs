9ff5c3195ec22eedd2d50691ff82e88b345ea23c
hongtao_luo <luohongtao621@hotmail.com>
2024-11-04 14:32:16 +0800

    565

---
+++ TestLeetCode/0551_0600/565_ArrayNestingTest.cs
@@ -0,0 +1,25 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _565_ArrayNestingTest
+    {
+        _565_ArrayNestingAlg alg = new _565_ArrayNestingAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] A = [5, 4, 0, 3, 1, 6, 2];
+            int exp = 4;
+            int res = alg.ArrayNesting(A);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0551_0600/565_ArrayNestingAlg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _565_ArrayNestingAlg
+    {
+        public int ArrayNesting(int[] nums)
+        {
+
+            return 0;
+        }
+
+        private void Dfs(int[] nums)
+        {
+            
+        }
+    }
+}
