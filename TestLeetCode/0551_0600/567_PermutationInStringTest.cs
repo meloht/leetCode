3b18c87d9955b1d37e702db1fefab4a2c9df9a9a
hongtao_luo <luohongtao621@hotmail.com>
2024-11-05 14:12:23 +0800

    567

--- TestLeetCode/0551_0600/567_PermutationInStringTest.cs
+++ TestLeetCode/0551_0600/567_PermutationInStringTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using leetCode._0551_0600;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,33 @@ namespace Test._0551_0600
     [TestClass]
     public class _567_PermutationInStringTest
     {
+        _567_PermutationInStringAlg alg = new _567_PermutationInStringAlg();
 
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s1 = "ab", s2 = "eidbaooo";
+            bool exp = true;
+            bool res=alg.CheckInclusion(s1, s2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s1 = "ab", s2 = "eidboaoo";
+            bool exp = false;
+            bool res = alg.CheckInclusion(s1, s2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s1 = "adc", s2 = "dcda";
+            bool exp = true;
+            bool res = alg.CheckInclusion(s1, s2);
+            Assert.AreEqual(exp, res);
+        }
     }
 }
--- leetCode/0551_0600/567_PermutationInStringAlg.cs
+++ leetCode/0551_0600/567_PermutationInStringAlg.cs
@@ -10,6 +10,31 @@ namespace leetCode._0551_0600
     {
         public bool CheckInclusion(string s1, string s2)
         {
+            int[] dict = new int[26];
+            foreach (var item in s1)
+            {
+                dict[item - 'a']++;
+            }
+            int[] dict2 = new int[26];
+            int j = 0;
+            for (int i = 0; i < s2.Length; i++)
+            {
+                int idx = s2[i] - 'a';
+                dict2[idx]++;
+                if (i > s1.Length - 1)
+                {
+                    dict2[s2[j] - 'a']--;
+                    j++;
+                }
+                if (i >= s1.Length - 1)
+                {
+                    if (dict2.SequenceEqual(dict))
+                    {
+                        return true;
+                    }
+                }
+            }
+
             return false;
         }
     }
