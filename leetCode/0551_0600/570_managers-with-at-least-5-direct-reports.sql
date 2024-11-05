5eb43996a8c2e66482a25b2a4403972f79e4a820
hongtao_luo <luohongtao621@hotmail.com>
2024-11-05 14:37:05 +0800

    570

---
+++ leetCode/0551_0600/570_managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,4 @@
+﻿
+
+select c.name from Employee c inner join (select a.id from Employee a inner join Employee b on a.id=b.managerid 
+where b.managerid is not null group by a.id having count(a.id)>=5) t on c.id=t.id
