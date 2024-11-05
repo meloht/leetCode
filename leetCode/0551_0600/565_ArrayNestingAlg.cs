07bb2a06b6e67476fdf0d417ca7889503ae3b231
hongtao_luo <luohongtao621@hotmail.com>
2024-11-05 10:13:41 +0800

    565

--- leetCode/0551_0600/565_ArrayNestingAlg.cs
+++ leetCode/0551_0600/565_ArrayNestingAlg.cs
@@ -8,15 +8,36 @@ namespace leetCode._0551_0600
 {
     public class _565_ArrayNestingAlg
     {
+
         public int ArrayNesting(int[] nums)
         {
+            int ans = 0;
 
-            return 0;
+            int[] memo = new int[nums.Length];
+            foreach (var item in nums)
+            {
+                ans = Math.Max(Dfs(nums, item, new HashSet<int>(), memo), ans);
+            }
+            return ans;
         }
 
-        private void Dfs(int[] nums)
+        private int Dfs(int[] nums, int index, HashSet<int> set, int[] memo)
         {
+
+            if (set.Contains(index))
+            {
+                return 0;
+            }
+            if (memo[index]!=0)
+            {
+                return memo[index];
+            }
+            set.Add(index);
+            int len = Dfs(nums, nums[index], set, memo) + 1;
             
+            memo[index] = len;
+            return len;
+
         }
     }
 }
