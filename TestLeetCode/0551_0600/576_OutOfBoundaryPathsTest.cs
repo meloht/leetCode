b935915c6b2a37e059d765d14e23202c9c583e68
hongtao_luo <luohongtao621@hotmail.com>
2024-11-06 14:52:45 +0800

    576

---
+++ TestLeetCode/0551_0600/576_OutOfBoundaryPathsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _576_OutOfBoundaryPathsTest
+    {
+        _576_OutOfBoundaryPathsAlg alg = new _576_OutOfBoundaryPathsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int m = 2, n = 2, maxMove = 2, startRow = 0, startColumn = 0;
+            int exp = 6;
+            int res = alg.FindPaths(m, n, maxMove, startRow, startColumn);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int m = 1, n = 3, maxMove = 3, startRow = 0, startColumn = 1;
+            int exp = 12;
+            int res = alg.FindPaths(m, n, maxMove, startRow, startColumn);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/576_OutOfBoundaryPathsAlg.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _576_OutOfBoundaryPathsAlg
+    {
+        public int FindPaths1(int m, int n, int maxMove, int startRow, int startColumn)
+        {
+            const int MOD = 1000000007;
+            int[][] directions = new int[][] {
+            new int[]{-1, 0},
+            new int[]{1, 0},
+            new int[]{0, -1},
+            new int[]{0, 1}
+        };
+            int outCounts = 0;
+            int[,,] dp = new int[maxMove + 1, m, n];
+            dp[0, startRow, startColumn] = 1;
+            for (int i = 0; i < maxMove; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        int count = dp[i, j, k];
+                        if (count > 0)
+                        {
+                            foreach (int[] direction in directions)
+                            {
+                                int j1 = j + direction[0], k1 = k + direction[1];
+                                if (j1 >= 0 && j1 < m && k1 >= 0 && k1 < n)
+                                {
+                                    dp[i + 1, j1, k1] = (dp[i + 1, j1, k1] + count) % MOD;
+                                }
+                                else
+                                {
+                                    outCounts = (outCounts + count) % MOD;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return outCounts;
+
+        }
+
+        public int FindPaths(int m, int n, int maxMove, int startRow, int startColumn)
+        {
+            const int MOD = 1000000007;
+            int[][] directions = new int[][] {
+            new int[]{-1, 0},
+            new int[]{1, 0},
+            new int[]{0, -1},
+            new int[]{0, 1}
+        };
+            int outCounts = 0;
+            int[,] dp = new int[m, n];
+            dp[startRow, startColumn] = 1;
+            for (int i = 0; i < maxMove; i++)
+            {
+                int[,] dpNew = new int[m, n];
+                for (int j = 0; j < m; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        int count = dp[j, k];
+                        if (count > 0)
+                        {
+                            foreach (int[] direction in directions)
+                            {
+                                int j1 = j + direction[0], k1 = k + direction[1];
+                                if (j1 >= 0 && j1 < m && k1 >= 0 && k1 < n)
+                                {
+                                    dpNew[j1, k1] = (dpNew[j1, k1] + count) % MOD;
+                                }
+                                else
+                                {
+                                    outCounts = (outCounts + count) % MOD;
+                                }
+                            }
+                        }
+                    }
+                }
+                dp = dpNew;
+            }
+            return outCounts;
+
+
+        }
+    }
+}
