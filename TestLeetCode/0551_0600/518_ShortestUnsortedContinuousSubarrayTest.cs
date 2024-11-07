456d485e41dd460561ada9143814635d5a47efb8
hongtao_luo <luohongtao621@hotmail.com>
2024-11-07 14:40:48 +0800

    518

---
+++ TestLeetCode/0551_0600/518_ShortestUnsortedContinuousSubarrayTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _518_ShortestUnsortedContinuousSubarrayTest
+    {
+        _518_ShortestUnsortedContinuousSubarrayAlg alg = new _518_ShortestUnsortedContinuousSubarrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 6, 4, 8, 10, 9, 15];
+            int exp = 5;
+            int res = alg.FindUnsortedSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int exp = 0;
+            int res = alg.FindUnsortedSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1];
+            int exp = 0;
+            int res = alg.FindUnsortedSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
