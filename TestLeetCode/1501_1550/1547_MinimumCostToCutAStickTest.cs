74c186b8d3009743c17db2e00b09dbb1e2b9ce54
hongtao_luo <luohongtao621@hotmail.com>
2024-11-11 13:48:37 +0800

    1547

---
+++ TestLeetCode/1501_1550/1547_MinimumCostToCutAStickTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1501_1550
+{
+    [TestClass]
+    public class _1547_MinimumCostToCutAStickTest
+    {
+        _1547_MinimumCostToCutAStickAlg alg = new _1547_MinimumCostToCutAStickAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 7;
+            int[] cuts = [1, 3, 4, 5];
+            int exp = 16;
+            int res = alg.MinCost(n, cuts);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 9;
+            int[] cuts = [5, 6, 1, 4, 2];
+            int exp = 22;
+            int res = alg.MinCost(n, cuts);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1501_1550/1547_MinimumCostToCutAStickAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1501_1550
+{
+    public class _1547_MinimumCostToCutAStickAlg
+    {
+        public int MinCost(int n, int[] cuts)
+        {
+            return 0;
+        }
+    }
+}
