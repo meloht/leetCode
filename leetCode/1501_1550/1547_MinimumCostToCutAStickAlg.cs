f58eb6659e50d9bc2d0209ebf5267b24f5df9d49
luohongtao <luohongtao621@hotmail.com>
2024-11-11 19:23:37 +0800

    1547

--- leetCode/1501_1550/1547_MinimumCostToCutAStickAlg.cs
+++ leetCode/1501_1550/1547_MinimumCostToCutAStickAlg.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace leetCode._1501_1550
 {
@@ -10,7 +11,29 @@ namespace leetCode._1501_1550
     {
         public int MinCost(int n, int[] cuts)
         {
-            return 0;
+            Array.Sort(cuts);
+            int m = cuts.Length + 2;
+            int[] newCuts = new int[m];
+            Array.Copy(cuts, 0, newCuts, 1, m - 2);
+            newCuts[m - 1] = n;
+
+            int[,] f = new int[m,m];
+            for (int i = m - 3; i >= 0; i--)
+            {
+                for (int j = i + 2; j < m; j++)
+                {
+                    int res = int.MaxValue;
+                    for (int k = i + 1; k < j; k++)
+                    {
+                        res = Math.Min(res, f[i,k] + f[k,j]);
+                    }
+                    f[i, j] = res + newCuts[j] - newCuts[i];
+                }
+            }
+            return f[0,m - 1];
+
+
         }
+
     }
 }
