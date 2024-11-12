c830b8153f3f9f883872a2a456af37d6f511284a
hongtao_luo <luohongtao621@hotmail.com>
2024-11-12 13:38:31 +0800

    3258

---
+++ TestLeetCode/3251_3300/3258_CountSubstringsThatSatisfyKConstraintITest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._3251_3300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3251_3300
+{
+    [TestClass]
+    public class _3258_CountSubstringsThatSatisfyKConstraintITest
+    {
+        _3258_CountSubstringsThatSatisfyKConstraintIAlg alg = new _3258_CountSubstringsThatSatisfyKConstraintIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "10101";
+            int k = 1;
+            int exp = 12;
+            int res = alg.CountKConstraintSubstrings(s, 1);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "1010101";
+            int k = 2;
+            int exp = 25;
+            int res = alg.CountKConstraintSubstrings(s, 1);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "11111";
+            int k = 1;
+            int exp = 15;
+            int res = alg.CountKConstraintSubstrings(s, 1);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3251_3300/3258_CountSubstringsThatSatisfyKConstraintIAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3251_3300
+{
+    public class _3258_CountSubstringsThatSatisfyKConstraintIAlg
+    {
+        public int CountKConstraintSubstrings(string s, int k)
+        {
+            int ans = 0;
+            int left = 0;
+            int[] cnt = new int[2];
+            for (int i = 0; i < s.Length; i++)
+            {
+                cnt[s[i] & 1]++;
+                while (cnt[0] > k && cnt[1] > k)
+                {
+                    cnt[s[left] & 1]--;
+                    left++;
+                }
+                ans += i - left + 1;
+            }
+            return ans;
+
+        }
+    }
+}
