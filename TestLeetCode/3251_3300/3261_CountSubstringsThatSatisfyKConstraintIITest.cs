ac057f506b4e3476ec48a436b559f7b88b205822
luohongtao <luohongtao621@hotmail.com>
2024-11-13 20:11:46 +0800

    3261

---
+++ TestLeetCode/3251_3300/3261_CountSubstringsThatSatisfyKConstraintIITest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._3251_3300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3251_3300
+{
+    [TestClass]
+    public class _3261_CountSubstringsThatSatisfyKConstraintIITest
+    {
+        _3261_CountSubstringsThatSatisfyKConstraintIIAlg alg = new _3261_CountSubstringsThatSatisfyKConstraintIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "0001111";
+            int k = 2;
+            int[][] queries = [[0, 6]];
+            long[] exp = [26];
+            long[] res = alg.CountKConstraintSubstrings(s, k, queries);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "010101";
+            int k = 1;
+            int[][] queries = [[0, 5], [1, 4], [2, 3]];
+            long[] exp = [15, 9, 3];
+            long[] res = alg.CountKConstraintSubstrings(s, k, queries);
+            bool bl = Utils.IsSameArr(exp, res);
+            Assert.IsTrue(bl);
+
+
+        }
+    }
+}
---
+++ leetCode/3251_3300/3261_CountSubstringsThatSatisfyKConstraintIIAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._3251_3300
+{
+    public class _3261_CountSubstringsThatSatisfyKConstraintIIAlg
+    {
+        public long[] CountKConstraintSubstrings(string s, int k, int[][] queries)
+        {
+            int n = s.Length;
+            int[] right = new int[n];
+            long[] sum = new long[n + 1];
+            int[] cnt = new int[2];
+            int l = 0;
+            for (int i = 0; i < n; i++)
+            {
+                cnt[s[i] & 1]++;
+                while (cnt[0] > k && cnt[1] > k)
+                {
+                    cnt[s[l] & 1]--;
+                    right[l++] = i;
+                }
+                sum[i + 1] = sum[i] + i - l + 1;
+            }
+            // 剩余没填的 right[l] 均为 n
+            Array.Fill(right, n, l, n - l);
+
+            long[] ans = new long[queries.Length];
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int ql = queries[i][0];
+                int qr = queries[i][1];
+                int j = Math.Min(right[ql], qr + 1);
+                ans[i] = sum[qr + 1] - sum[j] + (long)(j - ql + 1) * (j - ql) / 2;
+            }
+            return ans;
+
+
+        }
+    }
+}
