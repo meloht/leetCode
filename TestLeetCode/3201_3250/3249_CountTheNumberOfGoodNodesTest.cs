3a969f0287baeb7014f83e5f10d7af0e284f90b8
luohongtao <luohongtao621@hotmail.com>
2024-11-14 15:49:38 +0800

    3249

---
+++ TestLeetCode/3201_3250/3249_CountTheNumberOfGoodNodesTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3249_CountTheNumberOfGoodNodesTest
+    {
+        _3249_CountTheNumberOfGoodNodesAlg alg = new _3249_CountTheNumberOfGoodNodesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] edges = [[0, 1], [0, 2], [1, 3], [1, 4], [2, 5], [2, 6]];
+            int exp = 7;
+            int res = alg.CountGoodNodes(edges);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] edges = [[0, 1], [1, 2], [2, 3], [3, 4], [0, 5], [1, 6], [2, 7], [3, 8]];
+            int exp = 6;
+            int res = alg.CountGoodNodes(edges);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] edges = [[0, 1], [1, 2], [1, 3], [1, 4], [0, 5], [5, 6], [6, 7], [7, 8], [0, 9], [9, 10], [9, 12], [10, 11]];
+            int exp = 12;
+            int res = alg.CountGoodNodes(edges);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3249_CountTheNumberOfGoodNodesAlg.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._3201_3250
+{
+    
+    public class _3249_CountTheNumberOfGoodNodesAlg
+    {
+        private int ans;
+        public int CountGoodNodes(int[][] edges)
+        {
+            int n = edges.Length + 1;
+            List<int>[] g = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                g[i] = new List<int>();
+            }
+          
+            foreach (int[] e in edges)
+            {
+                int x = e[0];
+                int y = e[1];
+                g[x].Add(y);
+                g[y].Add(x);
+            }
+            dfs(0, -1, g);
+            return ans;
+
+        }
+
+        private int dfs(int x, int fa, List<int>[] g)
+        {
+            int size = 1;
+            int sz0 = 0;
+            bool ok = true;
+            foreach (int y in g[x])
+            {
+                if (y == fa)
+                {
+                    continue; // 不能递归到父节点
+                }
+                int sz = dfs(y, x, g);
+                if (sz0 == 0)
+                {
+                    sz0 = sz; // 记录第一个儿子子树的大小
+                }
+                else if (sz != sz0)
+                { // 存在大小不一样的儿子子树
+                    ok = false; // 注意不能 break，其他子树 y 仍然要递归
+                }
+                size += sz;
+            }
+            ans += ok ? 1 : 0;
+            return size;
+        }
+
+
+    }
+}
