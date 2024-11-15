380c09b0e1d3bb1db9bc188fd4c97ff0086c0f7c
hongtao_luo <luohongtao621@hotmail.com>
2024-11-15 13:29:46 +0800

    3239

---
+++ TestLeetCode/3201_3250/3239_MinimumNumberOfFlipsToMakeBinaryGridPalindromicITest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3239_MinimumNumberOfFlipsToMakeBinaryGridPalindromicITest
+    {
+        _3239_MinimumNumberOfFlipsToMakeBinaryGridPalindromicIAlg alg = new _3239_MinimumNumberOfFlipsToMakeBinaryGridPalindromicIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[1, 0, 0], [0, 0, 0], [0, 0, 1]];
+            int exp = 2;
+            int res = alg.MinFlips(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[0, 1], [0, 1], [0, 0]];
+            int exp = 1;
+            int res = alg.MinFlips(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] grid = [[1], [0]];
+            int exp = 0;
+            int res = alg.MinFlips(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3239_MinimumNumberOfFlipsToMakeBinaryGridPalindromicIAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3239_MinimumNumberOfFlipsToMakeBinaryGridPalindromicIAlg
+    {
+        public int MinFlips(int[][] grid)
+        {
+            int rows = grid.Length;
+            int cols = grid[0].Length;
+            int ans = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0, k = cols - 1; j < k; j++, k--)
+                {
+                    if (grid[i][j] != grid[i][k])
+                    {
+                        ans++;
+                    }
+                }
+            }
+            int ans2 = 0;
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0, k = rows - 1; j < k; j++, k--)
+                {
+                    if (grid[j][i] != grid[k][i])
+                    {
+                        ans2++;
+                    }
+                }
+            }
+            return Math.Min(ans, ans2);
+        }
+    }
+}
