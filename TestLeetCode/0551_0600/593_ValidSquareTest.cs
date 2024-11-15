a4a687ed83adfb01a3b4fbccde9748a019a4e665
hongtao_luo <luohongtao621@hotmail.com>
2024-11-15 14:08:42 +0800

    593

---
+++ TestLeetCode/0551_0600/593_ValidSquareTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0551_0600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0551_0600
+{
+    [TestClass]
+    public class _593_ValidSquareTest
+    {
+        _593_ValidSquareAlg alg = new _593_ValidSquareAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] p1 = [0, 0], p2 = [1, 1], p3 = [1, 0], p4 = [0, 1];
+            bool exp = true;
+            bool res = alg.ValidSquare(p1, p2, p3, p4);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] p1 = [0, 0], p2 = [1, 1], p3 = [1, 0], p4 = [0, 12];
+            bool exp = false;
+            bool res = alg.ValidSquare(p1, p2, p3, p4);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] p1 = [1, 0], p2 = [-1, 0], p3 = [0, 1], p4 = [0, -1];
+            bool exp = true;
+            bool res = alg.ValidSquare(p1, p2, p3, p4);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0551_0600/593_ValidSquareAlg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0551_0600
+{
+    public class _593_ValidSquareAlg
+    {
+        public bool ValidSquare(int[] p1, int[] p2, int[] p3, int[] p4)
+        {
+           
+            int[][] arr = new int[4][];
+            arr[0] = p1;
+            arr[1] = p2;
+            arr[2] = p3;
+            arr[3] = p4;
+            Array.Sort(arr, (x, y) => x[0].CompareTo(y[0]));
+
+            if (arr[0][0] != arr[1][0])
+                return false;
+            if (arr[2][0] != arr[3][0])
+                return false;
+
+            
+
+            Array.Sort(arr, (x, y) => x[1].CompareTo(y[1]));
+
+            if (arr[0][1] != arr[1][1])
+                return false;
+            if (arr[2][1] != arr[3][1])
+                return false;
+
+
+
+            return false;
+        }
+        private long Dis(int[] p1, int[] p2)
+        {
+            long x = p1[0] - p2[0];
+            long y = p1[1] - p2[1];
+            return x * x + y * y;
+        }
+    }
+}
