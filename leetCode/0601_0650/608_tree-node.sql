fa08be41f2831a2b3f3ad16637e059c825e183d6
hongtao_luo <luohongtao621@hotmail.com>
2024-11-18 14:08:00 +0800

    608

---
+++ leetCode/0601_0650/608_tree-node.sql
@@ -0,0 +1,5 @@
+﻿select id,'Root' as type from Tree where p_id is null
+union
+select b.p_id,'Inner'as type from Tree a inner join Tree b on a.id=b.p_id where a.p_id is not null group by b.p_id
+union
+select a.id ,'Leaf' as type from Tree a left join Tree b on a.id=b.p_id where b.id is null and a.p_id is not null
