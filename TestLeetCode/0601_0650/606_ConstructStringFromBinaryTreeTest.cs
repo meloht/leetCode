36e0f1d9a6a0efb34584fb4ecf9e9225231dff62
hongtao_luo <luohongtao621@hotmail.com>
2024-11-18 13:47:40 +0800

    606

---
+++ TestLeetCode/0601_0650/606_ConstructStringFromBinaryTreeTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0601_0650;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _606_ConstructStringFromBinaryTreeTest
+    {
+        _606_ConstructStringFromBinaryTreeAlg alg = new _606_ConstructStringFromBinaryTreeAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 2, 3, 4];
+            TreeNode node = TreeNode.BuildTree(root);
+            string exp = "1(2(4))(3)";
+            string res = alg.Tree2str(node);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1, 2, 3, null, 4];
+            TreeNode node = TreeNode.BuildTree(root);
+            string exp = "1(2()(4))(3)";
+            string res = alg.Tree2str(node);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/606_ConstructStringFromBinaryTreeAlg.cs
@@ -0,0 +1,30 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _606_ConstructStringFromBinaryTreeAlg
+    {
+
+        public string Tree2str(TreeNode root)
+        {
+            if (root == null)
+                return "";
+            if (root.left == null && root.right == null)
+            {
+                return root.val.ToString();
+            }
+            string left = Tree2str(root.left);
+            string right = Tree2str(root.right);
+            if (right == "")
+            {
+                return $"{root.val}({left})";
+            }
+            return $"{root.val}({left})({right})";
+        }
+    }
+}
