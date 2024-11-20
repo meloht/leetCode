03037208d353f3bcc4389e997108785b54664de8
hongtao_luo <luohongtao621@hotmail.com>
2024-11-20 12:23:08 +0800

    3244

---
+++ TestLeetCode/3201_3250/3244_ShortestDistanceAfterRoadAdditionQueriesIITest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3244_ShortestDistanceAfterRoadAdditionQueriesIITest
+    {
+        _3244_ShortestDistanceAfterRoadAdditionQueriesIIAlg alg = new _3244_ShortestDistanceAfterRoadAdditionQueriesIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 5;
+            int[][] queries = [[2, 4], [0, 2], [0, 4]];
+            int[] exp = [3, 2, 1];
+            int[] res = alg.ShortestDistanceAfterQueries(n, queries);
+            bool bl = Utils.IsSameArr(exp, res);
+
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 4;
+            int[][] queries = [[0, 3], [0, 2]];
+            int[] exp = [1, 1];
+            int[] res = alg.ShortestDistanceAfterQueries(n, queries);
+            bool bl = Utils.IsSameArr(exp, res);
+
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3244_ShortestDistanceAfterRoadAdditionQueriesIIAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3244_ShortestDistanceAfterRoadAdditionQueriesIIAlg
+    {
+        public int[] ShortestDistanceAfterQueries(int n, int[][] queries) 
+        {
+            int[] nxt = new int[n - 1];
+            for (int i = 0; i < n - 1; i++)
+            {
+                nxt[i] = i + 1;
+            }
+
+            int[] ans = new int[queries.Length];
+            int cnt = n - 1;
+            for (int qi = 0; qi < queries.Length; qi++)
+            {
+                int i = queries[qi][0];
+                int r = queries[qi][1];
+                while (nxt[i] < r)
+                {
+                    cnt--;
+                    int tmp = nxt[i];
+                    nxt[i] = r;
+                    i = tmp;
+                }
+                ans[qi] = cnt;
+            }
+            return ans;
+
+        }
+    }
+}
