3520631cd63390aef8e1e0a168b116a2a15a8825
hongtao_luo <luohongtao621@hotmail.com>
2024-11-20 14:06:23 +0800

    621

---
+++ TestLeetCode/0601_0650/621_TaskSchedulerTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _621_TaskSchedulerTest
+    {
+        _621_TaskSchedulerAlg alg = new _621_TaskSchedulerAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            char[] tasks = ['A', 'A', 'A', 'B', 'B', 'B'];
+            int n = 2;
+            int exp = 8;
+            int res = alg.LeastInterval(tasks,n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            char[] tasks = ['A', 'C', 'A', 'B', 'D', 'B'];
+            int n = 1;
+            int exp = 6;
+            int res = alg.LeastInterval(tasks, n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            char[] tasks = ['A', 'A', 'A', 'B', 'B', 'B'];
+            int n = 3;
+            int exp = 10;
+            int res = alg.LeastInterval(tasks, n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/621_TaskSchedulerAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _621_TaskSchedulerAlg
+    {
+        public int LeastInterval(char[] tasks, int n)
+        {
+            int[] arr = new int[26];
+
+            int maxExec = 0;
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                int idx = tasks[i] - 'A';
+                arr[idx]++;
+                maxExec = Math.Max(maxExec, arr[idx]);
+            }
+            int maxCount = 0;
+            foreach (var item in arr)
+            {
+                if (item == maxExec)
+                {
+                    maxCount++;
+                }
+            }
+
+            return Math.Max((maxExec - 1) * (n + 1) + maxCount, tasks.Length);
+        }
+    }
+}
