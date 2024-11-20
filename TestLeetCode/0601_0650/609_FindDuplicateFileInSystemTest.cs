0bdeaabb99998cbc2627771a40e1dba0875804c9
hongtao_luo <luohongtao621@hotmail.com>
2024-11-20 13:26:36 +0800

    609

---
+++ TestLeetCode/0601_0650/609_FindDuplicateFileInSystemTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _609_FindDuplicateFileInSystemTest
+    {
+        _609_FindDuplicateFileInSystemAlg alg = new _609_FindDuplicateFileInSystemAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] paths = ["root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"];
+            string[][] exp = [["root/a/2.txt", "root/c/d/4.txt", "root/4.txt"], ["root/a/1.txt", "root/c/3.txt"]];
+            IList<IList<string>> res = alg.FindDuplicate(paths);
+            bool bl = Utils.IsSame(exp, res);
+            Assert.IsTrue(bl);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] paths = ["root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)"];
+            string[][] exp = [["root/a/2.txt", "root/c/d/4.txt"], ["root/a/1.txt", "root/c/3.txt"]];
+            IList<IList<string>> res = alg.FindDuplicate(paths);
+            bool bl = Utils.IsSame(exp, res);
+            Assert.IsTrue(bl);
+        }
+    }
+}
---
+++ leetCode/0601_0650/609_FindDuplicateFileInSystemAlg.cs
@@ -0,0 +1,74 @@
+﻿using leetCode._1951_2000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _609_FindDuplicateFileInSystemAlg
+    {
+        public IList<IList<string>> FindDuplicate(string[] paths)
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            foreach (string path in paths)
+            {
+                string[] arr = path.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string pathVal = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    string name = getName(arr[i]);
+                    string val = getContent(arr[i], name.Length);
+                    if (!dict.ContainsKey(val))
+                    {
+                        dict[val] = new List<string>();
+
+                    }
+                    dict[val].Add($"{pathVal}/{name}");
+                }
+
+            }
+            IList<IList<string>> res=new List<IList<string>>();
+
+            foreach (var val in dict.Values)
+            {
+                if (val.Count > 1)
+                {
+                    res.Add(val);
+                }
+            }
+
+            return res;
+        }
+        private string getName(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != '(')
+                {
+                    sb.Append(s[i]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return sb.ToString();
+        }
+        private string getContent(string s, int skip)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = skip; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == ')')
+                {
+                    continue;
+                }
+                sb.Append(s[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
