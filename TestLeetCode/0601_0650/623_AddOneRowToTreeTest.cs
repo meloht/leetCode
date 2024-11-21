4a6a623b71a41cc9d0f5dada189db07927931e78
hongtao_luo <luohongtao621@hotmail.com>
2024-11-21 14:25:06 +0800

    623

---
+++ TestLeetCode/0601_0650/623_AddOneRowToTreeTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._0601_0650;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _623_AddOneRowToTreeTest
+    {
+        _623_AddOneRowToTreeAlg alg = new _623_AddOneRowToTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [4, 2, 6, 3, 1, 5];
+            int val = 1, depth = 2;
+            int?[] exp = [4, 1, 1, 2, null, null, 6, 3, 1, 5];
+            TreeNode node=TreeNode.BuildTree(root);
+            var res = alg.AddOneRow(node, val, depth);
+            int?[] rees=TreeNode.GetTreeLevelList(node);
+
+            Assert.IsTrue(exp.SequenceEqual(rees));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [4, 2, null, 3, 1];
+            int val = 1, depth = 3;
+            int?[] exp = [4, 2, null, 1, 1, 3, null, null, 1];
+            TreeNode node = TreeNode.BuildTree(root);
+            var res = alg.AddOneRow(node, val, depth);
+            int?[] rees = TreeNode.GetTreeLevelList(node);
+
+            Assert.IsTrue(exp.SequenceEqual(rees));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [4, 2, 6, 3, 1, 5];
+            int val = 1, depth = 1;
+            int?[] exp = [1, 4, null, 2, 6, 3, 1, 5];
+            TreeNode node = TreeNode.BuildTree(root);
+            var res = alg.AddOneRow(node, val, depth);
+            int?[] rees = TreeNode.GetTreeLevelList(node);
+
+            Assert.IsTrue(exp.SequenceEqual(rees));
+        }
+    }
+}
---
+++ leetCode/0601_0650/623_AddOneRowToTreeAlg.cs
@@ -0,0 +1,58 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _623_AddOneRowToTreeAlg
+    {
+        public TreeNode AddOneRow(TreeNode root, int val, int depth)
+        {
+            if (depth == 1)
+            {
+                TreeNode node = new TreeNode(val);
+                node.left = root;
+                return node;
+            }
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            int dep = 0;
+            while (queue.Count > 0)
+            {
+                dep++;
+
+                int cnt = queue.Count;
+                for (int i = 0; i < cnt; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                    if (dep == depth - 1)
+                    {
+                        TreeNode left = new TreeNode(val, node.left);
+                        TreeNode right = new TreeNode(val, null, node.right);
+                        node.left = left;
+                        node.right = right;
+                    }
+                }
+                if (dep == depth - 1)
+                {
+                    break;
+                }
+            }
+
+            return root;
+        }
+
+
+    }
+}
