d958af9d00151af70a2eba78d29e0f8c72c8e58d
hongtao_luo <luohongtao621@hotmail.com>
2024-11-22 13:27:14 +0800

    624

---
+++ TestLeetCode/0601_0650/624_MaximumDistanceInArraysTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _624_MaximumDistanceInArraysTest
+    {
+        _624_MaximumDistanceInArraysAlg alg = new _624_MaximumDistanceInArraysAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] arr=[[1, 2, 3], [4, 5], [1, 2, 3]];
+            int exp = 4;
+            int res = alg.MaxDistance(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] arr = [[1], [1]];
+            int exp = 0;
+            int res = alg.MaxDistance(arr);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/624_MaximumDistanceInArraysAlg.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _624_MaximumDistanceInArraysAlg
+    {
+        public int MaxDistance(IList<IList<int>> arrays)
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int maxIdx = 0;
+            int minIdx = 0;
+
+            for (int i = 0; i < arrays.Count; i++)
+            {
+                if (max < arrays[i][arrays[i].Count - 1])
+                {
+                    maxIdx = i;
+                    max = arrays[i][arrays[i].Count - 1];
+                }
+
+                if (min > arrays[i][0])
+                {
+                    minIdx = i;
+                    min = arrays[i][0];
+                }
+            }
+
+            if (maxIdx != minIdx)
+            {
+                return Math.Abs(max - min);
+            }
+            int min1 = GetMin(arrays, maxIdx);
+            int nn1 = Math.Abs(max - min1);
+
+            int max1 = GetMax(arrays, maxIdx);
+            int nn2 = Math.Abs(max1 - min);
+            return Math.Max(nn1, nn2);
+        }
+        private int GetMax(IList<IList<int>> arrays, int idx)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < arrays.Count; i++)
+            {
+                if (i == idx)
+                    continue;
+
+                if (max < arrays[i][arrays[i].Count - 1])
+                {
+
+                    max = arrays[i][arrays[i].Count - 1];
+                }
+
+
+            }
+            return max;
+        }
+
+        private int GetMin(IList<IList<int>> arrays, int idx)
+        {
+            int min = int.MaxValue;
+            for (int i = 0; i < arrays.Count; i++)
+            {
+                if (i == idx)
+                    continue;
+
+                if (min > arrays[i][0])
+                {
+                    min = arrays[i][0];
+                }
+
+
+            }
+            return min;
+        }
+    }
+}
