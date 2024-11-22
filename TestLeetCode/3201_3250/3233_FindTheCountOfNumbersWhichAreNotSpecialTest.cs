3d78f4599314a0db747bf979f499b831994b25c7
hongtao_luo <luohongtao621@hotmail.com>
2024-11-22 12:52:51 +0800

    3233

---
+++ TestLeetCode/3201_3250/3233_FindTheCountOfNumbersWhichAreNotSpecialTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3233_FindTheCountOfNumbersWhichAreNotSpecialTest
+    {
+        _3233_FindTheCountOfNumbersWhichAreNotSpecialAlg alg = new _3233_FindTheCountOfNumbersWhichAreNotSpecialAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int l = 5, r = 7;
+            int exp = 3;
+            int res = alg.NonSpecialCount(l, r);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int l = 4, r = 16;
+            int exp = 11;
+            int res = alg.NonSpecialCount(l, r);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3233_FindTheCountOfNumbersWhichAreNotSpecialAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3233_FindTheCountOfNumbersWhichAreNotSpecialAlg
+    {
+        public int NonSpecialCount(int l, int r)
+        {
+            int n = (int)Math.Sqrt(r);
+            int[] v = new int[n + 1];
+            int res = r - l + 1;
+            for (int i = 2; i <= n; i++)
+            {
+                if (v[i] == 0)
+                {
+                    if (i * i >= l && i * i <= r)
+                    {
+                        res--;
+                    }
+                    for (int j = i * 2; j <= n; j += i)
+                    {
+                        v[j] = 1;
+                    }
+                }
+            }
+            return res;
+
+        }
+    }
+}
