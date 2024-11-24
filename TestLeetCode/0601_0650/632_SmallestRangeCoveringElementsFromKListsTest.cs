82741a9a273e7d3d5dd7aebf36d33f9180b8e9ef
luohongtao <luohongtao621@hotmail.com>
2024-11-24 11:13:15 +0800

    632

---
+++ TestLeetCode/0601_0650/632_SmallestRangeCoveringElementsFromKListsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _632_SmallestRangeCoveringElementsFromKListsTest
+    {
+        _632_SmallestRangeCoveringElementsFromKListsAlg alg = new _632_SmallestRangeCoveringElementsFromKListsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] nums = [[4, 10, 15, 24, 26], [0, 9, 12, 20], [5, 18, 22, 30]];
+            int[] exp = [20, 24];
+            int[] res = alg.SmallestRange(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] nums = [[1, 2, 3], [1, 2, 3], [1, 2, 3]];
+            int[] exp = [1, 1];
+            int[] res = alg.SmallestRange(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0601_0650/632_SmallestRangeCoveringElementsFromKListsAlg.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._0601_0650
+{
+    public class _632_SmallestRangeCoveringElementsFromKListsAlg
+    {
+        public int[] SmallestRange(IList<IList<int>> nums)
+        {
+            int n = 0;
+            foreach (var item in nums)
+            {
+                n += item.Count;
+            }
+
+
+            List<int[]> ordered = new List<int[]>();
+
+            for (int kk = 0; kk < nums.Count; kk++)
+            {
+                foreach (int item in nums[kk])
+                {
+                    ordered.Add([item, kk]);
+                }
+            }
+            ordered.Sort((x, y) => x[0].CompareTo(y[0]));
+
+            int i = 0, k = 0;
+
+            int[] ans = [-1, -1];
+
+            int[] count = new int[nums.Count];
+
+            for (int j = 0; j < ordered.Count; j++)
+            {
+                if (count[ordered[j][1]]++ == 0)
+                {
+                    ++k;
+                }
+                if (k == nums.Count)
+                {
+                    while (count[ordered[i][1]] > 1)
+                    {
+                        --count[ordered[i++][1]];
+                    }
+                    if ((ans[0] == -1 && ans[1] == -1) || ans[1] - ans[0] > ordered[j][0] - ordered[i][0])
+                    {
+                        ans = [ordered[i][0], ordered[j][0]];
+                    }
+
+
+
+                }
+            }
+
+
+            return ans;
+        }
+
+
+    }
+}
