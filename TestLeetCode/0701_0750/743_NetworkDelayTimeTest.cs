993cee7cb4d2c47e8d263bd097b54e7a05786e6c
hongtao_luo <luohongtao621@hotmail.com>
2024-11-25 13:26:51 +0800

    743

---
+++ TestLeetCode/0701_0750/743_NetworkDelayTimeTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _743_NetworkDelayTimeTest
+    {
+        _743_NetworkDelayTimeAlg alg = new _743_NetworkDelayTimeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] times = [[2, 1, 1], [2, 3, 1], [3, 4, 1]];
+            int n = 4, k = 2;
+            int exp = 2;
+            int res = alg.NetworkDelayTime(times, n, k);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] times = [[1, 2, 1]];
+            int n = 2, k = 1;
+            int exp = 1;
+            int res = alg.NetworkDelayTime(times, n, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] times = [[1, 2, 1]];
+            int n = 2, k = 1;
+            int exp = 1;
+            int res = alg.NetworkDelayTime(times, n, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0701_0750/743_NetworkDelayTimeAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _743_NetworkDelayTimeAlg
+    {
+        public int NetworkDelayTime(int[][] times, int n, int k)
+        {
+            List<int[]>[] g = new List<int[]>[n];
+            for (int i = 0; i < n; i++)
+            {
+                g[i] = new List<int[]>();
+            }
+            foreach (var item in times)
+            {
+                g[item[0] - 1].Add([item[1] - 1, item[2]]);
+            }
+            int maxDis = 0;
+            int left = n;
+            int[] dis = new int[n];
+            Array.Fill(dis, int.MaxValue);
+            dis[k - 1] = 0;
+            PriorityQueue<int[], int> pq = new PriorityQueue<int[], int>(Comparer<int>.Create((x, y) => x.CompareTo(y)));
+            pq.Enqueue([0, k - 1], 0);
+
+            while (pq.Count > 0)
+            {
+                int[] p = pq.Dequeue();
+                int dx = p[0];
+                int x = p[1];
+
+                if (dx > dis[x])
+                {
+                    continue;
+                }
+                maxDis = dx;
+                left--;
+
+                foreach (int[] e in g[x])
+                {
+                    int y = e[0];
+                    int newDis = dx + e[1];
+                    if (newDis < dis[y])
+                    {
+                        dis[y] = newDis; // 更新 x 的邻居的最短路
+                        pq.Enqueue([newDis, y], newDis);
+                    }
+                }
+
+
+
+            }
+            return left == 0 ? maxDis : -1;
+        }
+    }
+}
