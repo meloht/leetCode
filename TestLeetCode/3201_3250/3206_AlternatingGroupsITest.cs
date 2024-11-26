3153f4b6f952deb072834d316c67630d8be2f982
hongtao_luo <luohongtao621@hotmail.com>
2024-11-26 13:03:01 +0800

    3206

---
+++ TestLeetCode/3201_3250/3206_AlternatingGroupsITest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3206_AlternatingGroupsITest
+    {
+        _3206_AlternatingGroupsIAlg alg = new _3206_AlternatingGroupsIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] colors = [1, 1, 1];
+            int exp = 0;
+            int res = alg.NumberOfAlternatingGroups(colors);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] colors = [0, 1, 0, 0, 1];
+            int exp = 3;
+            int res = alg.NumberOfAlternatingGroups(colors);
+            Assert.AreEqual(exp, res);
+        }
+
+      
+    }
+}
---
+++ leetCode/3201_3250/3206_AlternatingGroupsIAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3206_AlternatingGroupsIAlg
+    {
+        public int NumberOfAlternatingGroups(int[] colors)
+        {
+            int ans = 0;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if (colors[colors.Length - 1] != colors[0] && colors[0] != colors[1])
+                    {
+                        ans++;
+                    }
+                }
+                else if (i == colors.Length - 1)
+                {
+                    if (colors[i] != colors[0] && colors[i] != colors[i - 1])
+                    {
+                        ans++;
+                    }
+                }
+                else
+                {
+                    if (colors[i] != colors[i-1] && colors[i] != colors[i + 1])
+                    {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
