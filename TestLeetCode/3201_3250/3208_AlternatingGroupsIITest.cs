e2802a9d8c2365eb2ac2f51bd525d9d2ecd63d1b
hongtao_luo <luohongtao621@hotmail.com>
2024-11-27 13:41:32 +0800

    3208

---
+++ TestLeetCode/3201_3250/3208_AlternatingGroupsIITest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3208_AlternatingGroupsIITest
+    {
+        _3208_AlternatingGroupsIIAlg alg = new _3208_AlternatingGroupsIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] colors = [0, 1, 0, 1, 0];
+            int k = 3;
+            int exp = 3;
+            int res = alg.NumberOfAlternatingGroups(colors, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] colors = [0, 1, 0, 0, 1, 0, 1];
+            int k = 6;
+            int exp = 2;
+            int res = alg.NumberOfAlternatingGroups(colors, k);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] colors = [1, 1, 0, 1];
+            int k = 4;
+            int exp = 0;
+            int res = alg.NumberOfAlternatingGroups(colors, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3208_AlternatingGroupsIIAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3208_AlternatingGroupsIIAlg
+    {
+        public int NumberOfAlternatingGroups(int[] colors, int k)
+        {
+            int n = colors.Length;
+            int ans = 0;
+            int cnt = 0;
+            for (int i = 0; i < n + k - 1; i++)
+            {
+                if (i > 0 && colors[i % n] == colors[(i - 1) % n])
+                {
+                    cnt = 0;
+                }
+                cnt++;
+                if (cnt >= k)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+
+        }
+    }
+}
