b9182c77c312fe1d11c5b9fb03c7d46f39e22d06
hongtao_luo <luohongtao621@hotmail.com>
2024-11-29 13:13:12 +0800

    1050

---
+++ leetCode/1051_1100/1050_product-sales-analysis-iii.sql
@@ -0,0 +1,3 @@
+﻿select tt.product_id,tt.first_year,a.quantity,a.price from 
+(select product_id,min(year)as first_year  from Sales group by product_id) tt 
+inner join Sales a on tt.product_id=a.product_id and tt.first_year=a.year
