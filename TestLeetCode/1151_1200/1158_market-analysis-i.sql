08a3bc8c610c063254b508fb3aa1038d5b0a5fd5
luohongtao <luohongtao621@hotmail.com>
2024-11-30 10:53:14 +0800

    1158

---
+++ TestLeetCode/1151_1200/1158_market-analysis-i.sql
@@ -0,0 +1,3 @@
+﻿select b.user_id as buyer_id,b.join_date,case when a.orders_in_2019 is null then 0 else a.orders_in_2019 end as orders_in_2019  from Users b left join
+(select buyer_id,count(order_id) as orders_in_2019  from  Orders   where order_date between '2019-01-01' and  '2019-12-31' group by buyer_id) a
+on a.buyer_id=b.user_id
