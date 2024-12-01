e2f226a4a714635f3e06ef2aa2e55c0beac95a22
luohongtao <luohongtao621@hotmail.com>
2024-12-01 11:39:50 +0800

    1204

---
+++ leetCode/1201_1250/1204_last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,14 @@
+﻿
+
+select person_name from 
+(select a.turn, a.person_id,a.person_name,a.weight,(select sum(tt.weight) from Queue tt where tt.turn<=a.turn) as total from Queue a  order by a.turn asc) t where t.total<=1000 order by t.turn desc limit  1;
+
+
+select person_name
+from (
+    select *,sum(weight) over(order by turn) as sumWeight
+    from Queue
+) as t
+where sumWeight <= 1000
+order by sumWeight desc
+limit 1
