404ca09b85f3240dceaa169dd5fc55a3a5e34e22
hongtao_luo <luohongtao621@hotmail.com>
2024-12-02 13:53:47 +0800

    1321

---
+++ leetCode/1301_1350/1321_restaurant-growth.sql
@@ -0,0 +1,7 @@
+﻿
+
+	select visited_on,amount ,ROUND(amount/7, 2) AS average_amount  from (
+	
+	select visited_on, sum(amount) over(ORDER BY visited_on RANGE '6 days' PRECEDING) as amount from 
+	(select visited_on,sum(amount) as amount  from Customer group by visited_on order by visited_on) tt) ttt 
+	where visited_on>(select min(visited_on) from Customer)+interval '5 day'
