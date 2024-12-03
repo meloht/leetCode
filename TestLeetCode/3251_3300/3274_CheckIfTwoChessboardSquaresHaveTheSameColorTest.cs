cc874ddbd449eee5488ef57552b25bf19f3938a4
hongtao_luo <luohongtao621@hotmail.com>
2024-12-03 12:32:59 +0800

    3274

---
+++ TestLeetCode/3251_3300/3274_CheckIfTwoChessboardSquaresHaveTheSameColorTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._3251_3300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3251_3300
+{
+    [TestClass]
+    public class _3274_CheckIfTwoChessboardSquaresHaveTheSameColorTest
+    {
+        _3274_CheckIfTwoChessboardSquaresHaveTheSameColorAlg alg = new _3274_CheckIfTwoChessboardSquaresHaveTheSameColorAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string coordinate1 = "a1", coordinate2 = "c3";
+            bool exp = true;
+            bool res = alg.CheckTwoChessboards(coordinate1, coordinate2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string coordinate1 = "a1", coordinate2 = "h3";
+            bool exp = false;
+            bool res = alg.CheckTwoChessboards(coordinate1, coordinate2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3251_3300/3274_CheckIfTwoChessboardSquaresHaveTheSameColorAlg.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3251_3300
+{
+    public class _3274_CheckIfTwoChessboardSquaresHaveTheSameColorAlg
+    {
+        public bool CheckTwoChessboards(string coordinate1, string coordinate2)
+        {
+            int col1 = coordinate1[0] - 'a';
+            int row1 = coordinate1[1] - '1';
+
+            int col2 = coordinate2[0] - 'a';
+            int row2 = coordinate2[1] - '1';
+
+            if (IsBlack(row1, col1) == IsBlack(row2, col2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        private bool IsBlack(int row,int col) 
+        {
+            if (row % 2 == 0 && col % 2 == 0)
+            {
+                return false;
+            }
+            if (row % 2 == 1 && col % 2 == 0)
+            {
+                return true;
+            }
+            if (row % 2 == 0 && col % 2 == 1)
+            {
+                return true;
+            }
+            if (row % 2 == 1 && col % 2 == 1)
+            {
+                return false;
+            }
+
+            return false;
+
+        }
+    }
+}
