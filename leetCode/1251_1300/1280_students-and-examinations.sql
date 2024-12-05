3f3ef0323e8003d33ecac2d01936677355c3cac6
luohongtao <luohongtao621@hotmail.com>
2024-12-05 20:37:21 +0800

    1280

---
+++ leetCode/1251_1300/1280_students-and-examinations.sql
@@ -0,0 +1,5 @@
+﻿
+
+select tt.student_id,tt.student_name,tt.subject_name ,sum(case when b.subject_name is null then 0 else 1 end) as attended_exams from 
+(select a.student_id,a.student_name, b.subject_name from Students a , Subjects b ) tt left join Examinations b on tt.student_id=b.student_id and tt.subject_name=b.subject_name 
+ group by tt.student_id,tt.subject_name order by tt.student_id
---
+++ leetCode/3001_3050/3001_MinimumMovesToCaptureTheQueenAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3001_3050
+{
+    public class _3001_MinimumMovesToCaptureTheQueenAlg
+    {
+        public int MinMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f)
+        {
+            // 车与皇后处在同一行，且中间没有象
+            if (a == e && (c != a || d <= Math.Min(b, f) || d >= Math.Max(b, f)))
+            {
+                return 1;
+            }
+            // 车与皇后处在同一列，且中间没有象
+            if (b == f && (d != b || c <= Math.Min(a, e) || c >= Math.Max(a, e)))
+            {
+                return 1;
+            }
+            // 象、皇后处在同一条对角线，且中间没有车
+            if (Math.Abs(c - e) == Math.Abs(d - f) && ((c - e) * (b - f) != (a - e) * (d - f)
+                || a < Math.Min(c, e) || a > Math.Max(c, e)))
+            {
+                return 1;
+            }
+            return 2;
+        }
+
+
+    }
+}
