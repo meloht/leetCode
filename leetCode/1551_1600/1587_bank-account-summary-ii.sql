245896aea83bbde96d1e3d8eb6b9a7219f7af229
hongtao_luo <luohongtao621@hotmail.com>
2024-12-09 11:17:33 +0800

    1587

---
+++ leetCode/1551_1600/1587_bank-account-summary-ii.sql
@@ -0,0 +1,8 @@
+﻿
+
+
+select a.name,b.balance from Users a inner join (
+select account,sum(amount) as balance  
+	from Transactions 
+	group by account 
+	having(sum(amount)>10000)) b on a.account=b.account
