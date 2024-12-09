76268d79f5b02346d970bc9be9237bb041c8c7fb
hongtao_luo <luohongtao621@hotmail.com>
2024-12-09 14:00:56 +0800

    636

---
+++ TestLeetCode/0601_0650/636_ExclusiveTimeOfFunctionsTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _636_ExclusiveTimeOfFunctionsTest
+    {
+        _636_ExclusiveTimeOfFunctionsAlg alg = new _636_ExclusiveTimeOfFunctionsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 2;
+            string[] logs = ["0:start:0", "1:start:2", "1:end:5", "0:end:6"];
+            int[] exp = [3, 4];
+            int[] res = alg.ExclusiveTime(n, logs);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1;
+            string[] logs = ["0:start:0", "0:start:2", "0:end:5", "0:start:6", "0:end:6", "0:end:7"];
+            int[] exp = [8];
+            int[] res = alg.ExclusiveTime(n, logs);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 2;
+            string[] logs = ["0:start:0", "0:start:2", "0:end:5", "1:start:6", "1:end:6", "0:end:7"];
+            int[] exp = [7, 1];
+            int[] res = alg.ExclusiveTime(n, logs);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0601_0650/636_ExclusiveTimeOfFunctionsAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _636_ExclusiveTimeOfFunctionsAlg
+    {
+        public int[] ExclusiveTime(int n, IList<string> logs)
+        {
+            int[] ans = new int[n];
+
+            Stack<int[]> stack = new Stack<int[]>();
+           
+            foreach (string log in logs)
+            {
+                string[] arr = log.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                int fn = int.Parse(arr[0]);
+                int time = int.Parse(arr[2]);
+                if (arr[1] == "end")
+                {
+                    int[] item = stack.Pop();
+                    int fnIdx = item[0];
+                    ans[item[0]] += time + 1 - item[1];
+
+                    if (stack.Count > 0)
+                    {
+                        stack.Peek()[1] = time + 1;
+                    }
+                }
+                else
+                {
+                    if (stack.Count > 0)
+                    {
+                        ans[stack.Peek()[0]] += time - stack.Peek()[1];
+                        stack.Peek()[1] = time;
+                    }
+                    stack.Push([fn, time]);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
