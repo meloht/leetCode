845731cffe444c4715d3551bd1964c4dec8941fc
hongtao_luo <luohongtao621@hotmail.com>
2024-12-09 10:39:25 +0800

    1812

---
+++ TestLeetCode/1801_1850/1812_DetermineColorOfAChessboardSquareTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1801_1850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1801_1850
+{
+    [TestClass]
+    public class _1812_DetermineColorOfAChessboardSquareTest
+    {
+        _1812_DetermineColorOfAChessboardSquareAlg alg = new _1812_DetermineColorOfAChessboardSquareAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string coordinates = "a1";
+            bool exp = false;
+            bool res = alg.SquareIsWhite(coordinates);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string coordinates = "h3";
+            bool exp = true;
+            bool res = alg.SquareIsWhite(coordinates);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string coordinates = "c7";
+            bool exp = false;
+            bool res = alg.SquareIsWhite(coordinates);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1801_1850/1812_DetermineColorOfAChessboardSquareAlg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1801_1850
+{
+    public class _1812_DetermineColorOfAChessboardSquareAlg
+    {
+        public bool SquareIsWhite(string coordinates)
+        {
+            int col = coordinates[0] - 'a';
+            int row = coordinates[1] - '1';
+            if (col % 2 != row % 2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
