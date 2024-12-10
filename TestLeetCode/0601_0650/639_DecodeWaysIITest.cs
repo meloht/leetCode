b865ff01bc848f6f94ec3b1f58d48c53487c511c
hongtao_luo <luohongtao621@hotmail.com>
2024-12-10 16:24:34 +0800

    639

---
+++ TestLeetCode/0601_0650/639_DecodeWaysIITest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _639_DecodeWaysIITest
+    {
+        _639_DecodeWaysIIAlg alg=new _639_DecodeWaysIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "*";
+            int exp = 9;
+            int res = alg.NumDecodings(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "1*";
+            int exp = 18;
+            int res = alg.NumDecodings(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "2*";
+            int exp = 15;
+            int res = alg.NumDecodings(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/639_DecodeWaysIIAlg.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _639_DecodeWaysIIAlg
+    {
+        const int MOD = 1000000007;
+        public int NumDecodings(string s)
+        {
+            int n = s.Length;
+            long a = 0;
+            long b = 1;
+            long c = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                c = b * Check1Digit(s[i - 1]) % MOD;
+                if (i > 1)
+                {
+                    c = (c + a * Check2Digit(s[i - 2], s[i - 1])) % MOD;
+                }
+                a = b;
+                b = c;
+            }
+
+            return (int)c;
+        }
+
+      
+
+        private int Check1Digit(char ch)
+        {
+            if (ch == '0')
+            {
+                return 0;
+            }
+            return ch == '*' ? 9 : 1;
+        }
+
+        private int Check2Digit(char ch0, char ch1)
+        {
+            if (ch0 == '*' && ch1 == '*')
+            {
+                return 15;
+            }
+            if (ch0 == '*')
+            {
+                return ch1 <= '6' ? 2 : 1;
+            }
+            if (ch1 == '*')
+            {
+                if (ch0 == '1')
+                {
+                    return 9;
+                }
+                if (ch0 == '2')
+                {
+                    return 6;
+                }
+                return 0;
+            }
+            return (ch0 != '0' && (ch0 - '0') * 10 + (ch1 - '0') <= 26) ? 1 : 0;
+        }
+
+    }
+}
