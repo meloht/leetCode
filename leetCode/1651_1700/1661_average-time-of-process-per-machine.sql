f997840d19ddd55a969bc1068d3841504b86ef99
hongtao_luo <luohongtao621@hotmail.com>
2024-12-10 13:13:05 +0800

    1661

---
+++ leetCode/1651_1700/1661_average-time-of-process-per-machine.sql
@@ -0,0 +1,7 @@
+﻿
+
+select machine_id,
+round( sum( case activity_type 
+when 'start' then -timestamp
+else timestamp end )::numeric/count(distinct process_id),3) as processing_time 
+from Activity group by machine_id
