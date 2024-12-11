6cd005bf543c5bd5cd6b057876c642f67a0daaef
hongtao_luo <luohongtao621@hotmail.com>
2024-12-11 16:51:26 +0800

    640

---
+++ TestLeetCode/0601_0650/640_SolveTheEquationTest.cs
@@ -0,0 +1,53 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _640_SolveTheEquationTest
+    {
+        _640_SolveTheEquationAlg alg = new _640_SolveTheEquationAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string equation = "x+5-3+x=6+x-2";
+            string exp = "x=2";
+            string res = alg.SolveEquation(equation);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string equation = "x=x";
+            string exp = "Infinite solutions";
+            string res = alg.SolveEquation(equation);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string equation = "2x=x";
+            string exp = "x=0";
+            string res = alg.SolveEquation(equation);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string equation = "-x=-1";
+            string exp = "x=1";
+            string res = alg.SolveEquation(equation);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/640_SolveTheEquationAlg.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _640_SolveTheEquationAlg
+    {
+        public string SolveEquation(string equation)
+        {
+            string[] arr = equation.Split('=', StringSplitOptions.RemoveEmptyEntries);
+
+            int[] left = getXVal(arr[0]);
+            int x = left[0];
+            int val = -left[1];
+            int[] right = getXVal(arr[1]);
+            x -= right[0];
+            val += right[1];
+
+            if (x == 0 && val == 0)
+            {
+                return "Infinite solutions";
+            }
+            if (x == 0 && val != 0)
+            {
+                return "No solution";
+            }
+            if (Math.Abs(val) % Math.Abs(x) != 0)
+            {
+                return "No solution";
+            }
+
+            return $"x={val / x}";
+        }
+        private int[] getXVal(string s)
+        {
+            int x = 0;
+            int val = 0;
+            int i = 0;
+
+            while (i < s.Length)
+            {
+                if (s[i] == '+' || s[i] == '-')
+                {
+
+                    int ch = s[i] == '-' ? -1 : 1;
+                    i++;
+                    if (char.IsDigit(s[i]))
+                    {
+                       
+                        StringBuilder sb = new StringBuilder();
+                        while (i < s.Length && char.IsDigit(s[i]))
+                        {
+                            sb.Append(s[i]);
+                            i++;
+                        }
+                        if (i < s.Length && s[i] == 'x')
+                        {
+                            x += ch * int.Parse(sb.ToString());
+                        }
+                        else
+                        {
+                            val += ch * int.Parse(sb.ToString());
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        x += ch;
+                    }
+                }
+                else if (char.IsDigit(s[i]))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    while (i < s.Length && char.IsDigit(s[i]))
+                    {
+                        sb.Append(s[i]);
+                        i++;
+                    }
+                    if (i < s.Length && s[i] == 'x')
+                    {
+                        x += int.Parse(sb.ToString());
+                    }
+                    else
+                    {
+                        val += int.Parse(sb.ToString());
+                        continue;
+                    }
+
+                }
+                else if (s[i] == 'x')
+                {
+                    x++;
+                }
+                i++;
+            }
+
+            return [x, val];
+        }
+    }
+}
