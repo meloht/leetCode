d73532b0409f9725d1dca4a71c4797e99e1f7050
hongtao_luo <luohongtao621@hotmail.com>
2024-12-11 10:09:08 +0800

    2717

---
+++ TestLeetCode/2701_2750/2717_SemiOrderedPermutationTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2717_SemiOrderedPermutationTest
+    {
+        _2717_SemiOrderedPermutationAlg alg = new _2717_SemiOrderedPermutationAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 1, 4, 3];
+            int exp = 2;
+            int res = alg.SemiOrderedPermutation(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 4, 1, 3];
+            int exp = 3;
+            int res = alg.SemiOrderedPermutation(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 3, 4, 2, 5];
+            int exp = 0;
+            int res = alg.SemiOrderedPermutation(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2701_2750/2717_SemiOrderedPermutationAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2717_SemiOrderedPermutationAlg
+    {
+        public int SemiOrderedPermutation(int[] nums)
+        {
+            int minIdx = 0;
+            int maxIdx = nums.Length - 1;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < min)
+                {
+                    min = nums[i];
+                    minIdx = i;
+                }
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                    maxIdx = i;
+                }
+            }
+            int ans = minIdx + nums.Length - 1 - maxIdx;
+            if (maxIdx < minIdx)
+            {
+                ans--;
+            }
+            return ans;
+        }
+    }
+}
