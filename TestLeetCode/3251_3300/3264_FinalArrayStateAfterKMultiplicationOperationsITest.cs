e82a564edf7b8a0813d07112bf3dc00d94a38004
hongtao_luo <luohongtao621@hotmail.com>
2024-12-13 11:31:16 +0800

    3264

---
+++ TestLeetCode/3251_3300/3264_FinalArrayStateAfterKMultiplicationOperationsITest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3251_3300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3251_3300
+{
+    [TestClass]
+    public class _3264_FinalArrayStateAfterKMultiplicationOperationsITest
+    {
+        _3264_FinalArrayStateAfterKMultiplicationOperationsIAlg alg = new _3264_FinalArrayStateAfterKMultiplicationOperationsIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 1, 3, 5, 6];
+            int k = 5, multiplier = 2;
+            int[] exp = [8, 4, 6, 5, 6];
+            int[] res = alg.GetFinalState(nums, k, multiplier);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2];
+            int k = 3, multiplier = 4;
+            int[] exp = [16, 8];
+            int[] res = alg.GetFinalState(nums, k, multiplier);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/3251_3300/3264_FinalArrayStateAfterKMultiplicationOperationsIAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3251_3300
+{
+    public class _3264_FinalArrayStateAfterKMultiplicationOperationsIAlg
+    {
+        public int[] GetFinalState(int[] nums, int k, int multiplier)
+        {
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) =>
+            {
+                if (nums[x] == nums[y])
+                {
+                    return x.CompareTo(y);
+                }
+                else
+                {
+                    return nums[x].CompareTo(nums[y]);
+                }
+            }));
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                queue.Enqueue(i, i);
+            }
+
+
+            for (int i = 0; i < k; i++)
+            {
+                int idx = queue.Dequeue();
+                nums[idx] = nums[idx] * multiplier;
+                queue.Enqueue(idx, idx);
+            }
+
+            return nums;
+        }
+    }
+}
