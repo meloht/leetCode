42991240473c0e199396d72290063d8ca9280cdc
hongtao_luo <luohongtao621@hotmail.com>
2024-12-13 12:59:35 +0800

    1731

---
+++ leetCode/1701_1750/1731_the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,6 @@
+﻿
+
+select b.employee_id,b.name,count(a.employee_id) as reports_count,round(avg(a.age),0)as average_age  from Employees a 
+inner join Employees b on a.reports_to=b.employee_id
+group by b.employee_id,b.name 
+order by b.employee_id
