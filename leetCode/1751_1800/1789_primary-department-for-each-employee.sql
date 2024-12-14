d8680c716f7a2f28a673d9ef6fd891637f79f37a
luohongtao <luohongtao621@hotmail.com>
2024-12-14 12:23:52 +0800

    1789

---
+++ leetCode/1751_1800/1789_primary-department-for-each-employee.sql
@@ -0,0 +1,6 @@
+﻿
+
+select employee_id ,department_id from Employee where primary_flag='Y'
+union
+select employee_id,max(department_id)  as department_id from Employee 
+ group by employee_id having (count(department_id)=1)
