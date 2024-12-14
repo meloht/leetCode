19507958b827850157b5e00789b36a8eaff90c47
luohongtao <luohongtao621@hotmail.com>
2024-12-14 11:57:32 +0800

    3266

---
+++ TestLeetCode/3251_3300/3266_FinalArrayStateAfterKMultiplicationOperationsIITest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._3251_3300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3251_3300
+{
+    [TestClass]
+    public class _3266_FinalArrayStateAfterKMultiplicationOperationsIITest
+    {
+        _3266_FinalArrayStateAfterKMultiplicationOperationsIIAlg alg = new _3266_FinalArrayStateAfterKMultiplicationOperationsIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 1, 3, 5, 6];
+            int k = 5, multiplier = 2;
+            int[] exp = [8, 4, 6, 5, 6];
+            int[] res = alg.GetFinalState(nums, k, multiplier);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [100000, 2000];
+            int k = 2, multiplier = 1000000;
+            int[] exp = [999999307, 999999993];
+            int[] res = alg.GetFinalState(nums, k, multiplier);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [66307295, 441787703, 589039035, 322281864];
+            int k = 900900704, multiplier = 641725;
+            int[] exp = [664480092, 763599523, 886046925, 47878852];
+            int[] res = alg.GetFinalState(nums, k, multiplier);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/3251_3300/3266_FinalArrayStateAfterKMultiplicationOperationsIIAlg.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3251_3300
+{
+    public class _3266_FinalArrayStateAfterKMultiplicationOperationsIIAlg
+    {
+        int Mod = 1_000_000_007;
+        public int[] GetFinalState(int[] nums, int k, int multiplier)
+        {
+            if (multiplier == 1)
+            { // 数组不变
+                return nums;
+            }
+            int n = nums.Length;
+            int max = 0;
+            long[] ans = new long[n];
+            for (int i = 0; i < n; i++)
+            {
+                ans[i] = nums[i];
+            }
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) =>
+            {
+                if (ans[x] == ans[y])
+                {
+                    return x.CompareTo(y);
+                }
+                else
+                {
+                    return ans[x].CompareTo(ans[y]);
+                }
+            }));
+
+         
+            for (int i = 0; i < nums.Length; i++)
+            {
+                queue.Enqueue(i, i);
+                max = Math.Max(max, nums[i]);
+            }
+
+            for (; k > 0 && nums[queue.Peek()] < max; k--)
+            {
+                int idx = queue.Dequeue();
+                ans[idx] = ans[idx] * multiplier;
+                queue.Enqueue(idx, idx);
+            }
+          
+
+            for (int i = 0; i < n; i++)
+            {
+                int idx = queue.Dequeue();
+
+                nums[idx] = (int)(ans[idx] % Mod * pow(multiplier, k / n + (i < k % n ? 1 : 0)) % Mod);
+            }
+
+            return nums;
+        }
+
+        private long pow(long x, int n)
+        {
+            long res = 1;
+            for (; n > 0; n /= 2)
+            {
+                if (n % 2 > 0)
+                {
+                    res = res * x % Mod;
+                }
+                x = x * x % Mod;
+            }
+            return res;
+        }
+
+
+    }
+}
