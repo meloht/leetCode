b9c540882043e392f3e3db6715623e683ae363b2
luohongtao <luohongtao621@hotmail.com>
2024-12-15 11:02:31 +0800

    1338

---
+++ TestLeetCode/1301_1350/1338_ReduceArraySizeToTheHalfTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1301_1350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1301_1350
+{
+    [TestClass]
+    public class _1338_ReduceArraySizeToTheHalfTest
+    {
+        _1338_ReduceArraySizeToTheHalfAlg alg = new _1338_ReduceArraySizeToTheHalfAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [3, 3, 3, 3, 5, 5, 5, 2, 2, 7];
+            int exp = 2;
+            int res = alg.MinSetSize(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [7, 7, 7, 7, 7, 7];
+            int exp = 1;
+            int res = alg.MinSetSize(arr);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1301_1350/1338_ReduceArraySizeToTheHalfAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1301_1350
+{
+    public class _1338_ReduceArraySizeToTheHalfAlg
+    {
+        public int MinSetSize(int[] arr)
+        {
+            int t = arr.Length / 2;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var item in arr)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+
+            int[] numCnt = dict.Values.ToArray();
+            
+
+            Array.Sort(numCnt, (x, y) => y.CompareTo(x));
+
+            int ans = 0;
+
+            for (int i = 0; i < numCnt.Length; i++)
+            {
+                ans += numCnt[i];
+
+                if (ans >= t)
+                {
+                    return i + 1;
+                }
+            }
+
+
+            return numCnt.Length;
+        }
+    }
+}
