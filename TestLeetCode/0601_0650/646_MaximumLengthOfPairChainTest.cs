a9780257c24e27d18a454b19d97de7f0d2112c02
luohongtao <luohongtao621@hotmail.com>
2024-12-15 14:41:59 +0800

    646

---
+++ TestLeetCode/0601_0650/646_MaximumLengthOfPairChainTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _646_MaximumLengthOfPairChainTest
+    {
+        _646_MaximumLengthOfPairChainAlg alg = new _646_MaximumLengthOfPairChainAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] pairs = [[1, 2], [2, 3], [3, 4]];
+            int exp = 2;
+            int res = alg.FindLongestChain(pairs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] pairs = [[1, 2], [7, 8], [4, 5]];
+            int exp = 3;
+            int res = alg.FindLongestChain(pairs);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/646_MaximumLengthOfPairChainAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _646_MaximumLengthOfPairChainAlg
+    {
+        public int FindLongestChain(int[][] pairs)
+        {
+            Array.Sort(pairs, (x, y) => x[1].CompareTo(y[0]));
+
+
+            return 0;
+        }
+
+
+        private int Dfs(int i,int j, int[][] pairs)
+        {
+            if (i == 0)
+            {
+                
+            }
+   
+            if (pairs[i][1] < pairs[i][0])
+            {
+                Dfs(i-1,i,pairs)+1
+            }
+        }
+    }
+}
