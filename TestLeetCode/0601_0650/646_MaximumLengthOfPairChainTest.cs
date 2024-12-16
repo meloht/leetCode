fa2a31e261f682721531c61e6ef3470bbfdd91bc
hongtao_luo <luohongtao621@hotmail.com>
2024-12-16 17:01:29 +0800

    646

--- TestLeetCode/0601_0650/646_MaximumLengthOfPairChainTest.cs
+++ TestLeetCode/0601_0650/646_MaximumLengthOfPairChainTest.cs
@@ -29,5 +29,23 @@ namespace Test._0601_0650
             int res = alg.FindLongestChain(pairs);
             Assert.AreEqual(exp, res);
         }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] pairs = [[-10, -8], [8, 9], [-5, 0], [6, 10], [-6, -4], [1, 7], [9, 10], [-4, 7]];
+            int exp = 4;
+            int res = alg.FindLongestChain(pairs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] pairs = [[-6, 9], [1, 6], [8, 10], [-1, 4], [-6, -2], [-9, 8], [-5, 3], [0, 3]];
+            int exp = 3;
+            int res = alg.FindLongestChain(pairs);
+            Assert.AreEqual(exp, res);
+        }
     }
 }
--- leetCode/0601_0650/646_MaximumLengthOfPairChainAlg.cs
+++ leetCode/0601_0650/646_MaximumLengthOfPairChainAlg.cs
@@ -10,26 +10,110 @@ namespace leetCode._0601_0650
     {
         public int FindLongestChain(int[][] pairs)
         {
-            Array.Sort(pairs, (x, y) => x[1].CompareTo(y[0]));
+            Array.Sort(pairs, (x, y) => x[0].CompareTo(y[0]));
+            int n = pairs.Length;
+            int[,] memo = new int[n + 1, n + 1];
 
+            int ans = Dfs(n - 2, n - 1, pairs, memo);
 
-            return 0;
+            return ans;
         }
 
 
-        private int Dfs(int i,int j, int[][] pairs)
+        private int Dfs(int i, int j, int[][] pairs, int[,] memo)
         {
-            if (i == 0)
+            if (i < 0)
             {
-                
+                return 1;
             }
-   
-            if (pairs[i][1] < pairs[i][0])
+            if (memo[i, j] != 0)
             {
-               // Dfs(i-1,i,pairs)+1
+                return memo[i, j];
+            }
+            int res = int.MinValue;
+            if (pairs[i][1] < pairs[j][0])
+            {
+                res = Dfs(i - 1, i, pairs, memo) + 1;
             }
 
-            return 0;
+            int ans = Dfs(i - 1, j, pairs, memo);
+            memo[i, j] = Math.Max(ans, res);
+            return memo[i, j];
         }
+
+        public int FindLongestChain1(int[][] pairs)
+        {
+            int n = pairs.Length;
+            Array.Sort(pairs, (a, b) => a[0] - b[0]);
+            int[] dp = new int[n];
+            Array.Fill(dp, 1);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (pairs[i][0] > pairs[j][1])
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+            return dp[n - 1];
+        }
+
+        public int FindLongestChain2(int[][] pairs)
+        {
+            Array.Sort(pairs, (a, b) => a[0] - b[0]);
+            IList<int> arr = new List<int>();
+            foreach (int[] p in pairs)
+            {
+                int x = p[0], y = p[1];
+                if (arr.Count == 0 || x > arr[arr.Count - 1])
+                {
+                    arr.Add(y);
+                }
+                else
+                {
+                    int idx = BinarySearch(arr, x);
+                    arr[idx] = Math.Min(arr[idx], y);
+                }
+            }
+            return arr.Count;
+        }
+
+        public int BinarySearch(IList<int> arr, int x)
+        {
+            int low = 0, high = arr.Count - 1;
+            while (low < high)
+            {
+                int mid = low + (high - low) / 2;
+                if (arr[mid] >= x)
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return low;
+        }
+
+        public int FindLongestChain3(int[][] pairs)
+        {
+            int curr = int.MinValue, res = 0;
+            Array.Sort(pairs, (a, b) => a[1] - b[1]);
+            foreach (int[] p in pairs)
+            {
+                if (curr < p[0])
+                {
+                    curr = p[1];
+                    res++;
+                }
+            }
+            return res;
+        }
+
+
+
     }
 }
