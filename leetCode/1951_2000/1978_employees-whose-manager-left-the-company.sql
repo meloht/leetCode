b4080997383c9cc24d874cababafdf393fccac79
hongtao_luo <luohongtao621@hotmail.com>
2024-12-17 13:15:16 +0800

    1978

---
+++ leetCode/1951_2000/1978_employees-whose-manager-left-the-company.sql
@@ -0,0 +1,6 @@
+﻿
+
+select a.employee_id from Employees a 
+left join Employees b on a.manager_id=b.employee_id 
+where b.employee_id is null and a.salary<30000 and a.manager_id is not null 
+order by a.employee_id asc
