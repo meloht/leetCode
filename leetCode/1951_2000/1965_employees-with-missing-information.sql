1bd2887a3292241fb987395457764d43cfea02fb
hongtao_luo <luohongtao621@hotmail.com>
2024-12-17 13:00:22 +0800

    1965

---
+++ leetCode/1951_2000/1965_employees-with-missing-information.sql
@@ -0,0 +1,8 @@
+﻿
+select employee_id from (
+select a.employee_id from Employees a 
+left join Salaries b on a.employee_id=b.employee_id where b.employee_id is null
+union
+select c.employee_id from Employees a 
+right join Salaries c on a.employee_id=c.employee_id where  a.employee_id is null) tt 
+order by employee_id
