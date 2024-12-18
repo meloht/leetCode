1a1785a011ba1ba91723ab47b316a6b7e5e3eb99
hongtao_luo <luohongtao621@hotmail.com>
2024-12-18 13:18:25 +0800

    3220

---
+++ TestLeetCode/3201_3250/3220_odd-and-even-transactions.sql
@@ -0,0 +1,15 @@
+﻿
+select transaction_date,
+sum(
+case when amount is null then 0 
+when amount%2=1 then amount
+else 0	end ) as odd_sum ,
+
+sum(
+case when amount is null then 0 
+when amount%2=0 then amount
+else 0	end ) as even_sum 
+
+from transactions 
+group by transaction_date 
+order by transaction_date asc
