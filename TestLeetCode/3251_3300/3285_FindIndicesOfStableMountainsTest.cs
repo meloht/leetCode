707e0d92f6fab31f6d045a554f09d2ee6e719200
hongtao_luo <luohongtao621@hotmail.com>
2024-12-19 11:17:00 +0800

    3285

---
+++ TestLeetCode/3251_3300/3285_FindIndicesOfStableMountainsTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._3251_3300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3251_3300
+{
+    [TestClass]
+    public class _3285_FindIndicesOfStableMountainsTest
+    {
+        _3285_FindIndicesOfStableMountainsAlg alg = new _3285_FindIndicesOfStableMountainsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] height = [1, 2, 3, 4, 5];
+            int threshold = 2;
+            int[] exp = [3, 4];
+            int[] res = alg.StableMountains(height, threshold).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] height = [10, 1, 10, 1, 10];
+            int threshold = 3;
+            int[] exp = [1, 3];
+            int[] res = alg.StableMountains(height, threshold).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] height = [10, 1, 10, 1, 10];
+            int threshold = 10;
+            int[] exp = [];
+            int[] res = alg.StableMountains(height, threshold).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/3251_3300/3285_FindIndicesOfStableMountainsAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3251_3300
+{
+    public class _3285_FindIndicesOfStableMountainsAlg
+    {
+        public IList<int> StableMountains(int[] height, int threshold)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 1; i < height.Length; i++)
+            {
+                if (threshold < height[i - 1])
+                {
+                    list.Add(i);
+                }
+            }
+
+            return list;
+        }
+    }
+}
