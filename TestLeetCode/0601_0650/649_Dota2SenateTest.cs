2b02817ed1751512f2dfa103f7bf9670670b5513
hongtao_luo <luohongtao621@hotmail.com>
2024-12-19 14:28:00 +0800

    649

---
+++ TestLeetCode/0601_0650/649_Dota2SenateTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _649_Dota2SenateTest
+    {
+        _649_Dota2SenateAlg alg = new _649_Dota2SenateAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string senate = "RD";
+            string exp = "Radiant";
+            string res = alg.PredictPartyVictory(senate);
+            Assert.AreEqual(res, exp);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string senate = "RDD";
+            string exp = "Dire";
+            string res = alg.PredictPartyVictory(senate);
+            Assert.AreEqual(res, exp);
+        }
+    }
+}
---
+++ leetCode/0601_0650/649_Dota2SenateAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _649_Dota2SenateAlg
+    {
+        public string PredictPartyVictory(string senate)
+        {
+
+            return null;
+        }
+    }
+}
