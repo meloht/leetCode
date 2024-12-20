993f1f092f803d200d0cba5d02835318e72142cf
hongtao_luo <luohongtao621@hotmail.com>
2024-12-20 11:12:20 +0800

    3138

---
+++ TestLeetCode/3101_3150/3138_MinimumLengthOfAnagramConcatenationTest.cs
@@ -0,0 +1,69 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3138_MinimumLengthOfAnagramConcatenationTest
+    {
+        _3138_MinimumLengthOfAnagramConcatenationAlg alg = new _3138_MinimumLengthOfAnagramConcatenationAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abba";
+            int exp = 2;
+            int res = alg.MinAnagramLength(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "cdef";
+            int exp = 4;
+            int res = alg.MinAnagramLength(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "aabb";
+            int exp = 4;
+            int res = alg.MinAnagramLength(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "aabbbb";
+            int exp = 6;
+            int res = alg.MinAnagramLength(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string s = "abbbaa";
+            int exp = 6;
+            int res = alg.MinAnagramLength(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            string s = "jjj";
+            int exp = 1;
+            int res = alg.MinAnagramLength(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3138_MinimumLengthOfAnagramConcatenationAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3138_MinimumLengthOfAnagramConcatenationAlg
+    {
+        public int MinAnagramLength(string s)
+        {
+            int[] map = new int[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int idx = s[i] - 'a';
+
+                map[idx]++;
+            }
+            int[] nums = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                int idx = s[i] - 'a';
+                nums[idx]++;
+
+                if (s.Length % (i + 1) == 0)
+                {
+                    int j = i + 1;
+                    int end = j + i + 1;
+                    bool bl = true;
+                    while (j < end && end <= s.Length)
+                    {
+                        int[] nums2 = new int[26];
+                        for (; j < end; j++)
+                        {
+                            int idxx = s[j] - 'a';
+                            nums2[idxx]++;
+                        }
+                        if (nums.SequenceEqual(nums2))
+                        {
+                            end += (i + 1);
+                        }
+                        else
+                        {
+                            bl = false;
+                            break;
+                        }
+                    }
+                    if (bl)
+                    {
+                        return i + 1;
+                    }
+                }
+            }
+
+            return s.Length;
+        }
+    }
+}
