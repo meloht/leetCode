028410d2ac4ff688d07cbdac9abc8aaca4e5739d
hongtao_luo <luohongtao621@hotmail.com>
2024-12-20 14:18:07 +0800

    650

---
+++ TestLeetCode/0601_0650/650_2KeysKeyboardTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0601_0650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0601_0650
+{
+    [TestClass]
+    public class _650_2KeysKeyboardTest
+    {
+        _650_2KeysKeyboardAlg alg = new _650_2KeysKeyboardAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3;
+            int exp = 3;
+            int res = alg.MinSteps(n);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1;
+            int exp = 0;
+            int res = alg.MinSteps(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 111;
+            int exp = 40;
+            int res = alg.MinSteps(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0601_0650/650_2KeysKeyboardAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0601_0650
+{
+    public class _650_2KeysKeyboardAlg
+    {
+        public int MinSteps(int n)
+        {
+            int ans = 0;
+            for (int i = 2; i * i <= n; ++i)
+            {
+                while (n % i == 0)
+                {
+                    n /= i;
+                    ans += i;
+                }
+            }
+            if (n > 1)
+            {
+                ans += n;
+            }
+            return ans;
+        }
+
+
+
+       
+    }
+}
