ddbbb3c07900ac36138cd0a568f4e3888095fa5d
luohongtao <luohongtao621@hotmail.com>
2024-12-23 15:51:35 +0800

    855

---
+++ TestLeetCode/0851_0900/855_ExamRoomTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0851_0900._855_ExamRoomAlg;
+
+namespace Test._0851_0900
+{
+    [TestClass]
+    public class _855_ExamRoomTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            ExamRoom examRoom = new ExamRoom(10);
+            Assert.AreEqual(0, examRoom.Seat()); // 返回 0，房间里没有人，学生坐在 0 号座位。
+            Assert.AreEqual(9, examRoom.Seat()); // 返回 9，学生最后坐在 9 号座位。
+            Assert.AreEqual(4, examRoom.Seat()); // 返回 4，学生最后坐在 4 号座位。
+            Assert.AreEqual(2, examRoom.Seat()); // 返回 2，学生最后坐在 2 号座位。
+            examRoom.Leave(4);
+            Assert.AreEqual(5, examRoom.Seat()); // 返回 5，学生最后坐在 5 号座位。
+        }
+    }
+}
---
+++ leetCode/0851_0900/855_ExamRoomAlg.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _855_ExamRoomAlg
+    {
+        public class ExamRoom
+        {
+            PriorityQueue<int[], int[]> priorityQueue;
+            private int n;
+            private SortedSet<int> seats;
+            public ExamRoom(int n)
+            {
+                this.n = n;
+                seats = new SortedSet<int>();
+                priorityQueue = new PriorityQueue<int[], int[]>(Comparer<int[]>.Create((p1, p2) => {
+
+                    int d1 = p1[1] - p1[0], d2 = p2[1] - p2[0];
+                    return d1 / 2 == d2 / 2 ? (p1[0] > p2[0] ? 1 : -1) : (d1 / 2 < d2 / 2 ? 1 : -1);
+                }));
+            }
+
+            public int Seat()
+            {
+                if (seats.Count == 0)
+                {
+                    seats.Add(0);
+                    return 0;
+                }
+                int left = seats.Min, right = n - 1 - seats.Max;
+
+                while (seats.Count >= 2)
+                {
+                    var p = priorityQueue.Peek();
+                    if (seats.Contains(p[0]) && seats.Contains(p[1]) && seats.GetViewBetween(p[0] + 1, n - 1).Min == p[1])
+                    { // 不属于延迟删除的区间
+                        int d = p[1] - p[0];
+                        if (d / 2 < right || d / 2 <= left)
+                        { // 最左或最右的座位更优
+                            break;
+                        }
+                        priorityQueue.Dequeue();
+                        priorityQueue.Enqueue(new int[] { p[0], p[0] + d / 2 }, new int[] { p[0], p[0] + d / 2 });
+                        priorityQueue.Enqueue(new int[] { p[0] + d / 2, p[1] }, new int[] { p[0] + d / 2, p[1] });
+                        seats.Add(p[0] + d / 2);
+                        return p[0] + d / 2;
+                    }
+                    priorityQueue.Dequeue(); // leave 函数中延迟删除的区间在此时删除
+                }
+                if (right > left)
+                { // 最右的位置更优
+                    priorityQueue.Enqueue(new int[] { seats.Max, n - 1 }, new int[] { seats.Max, n - 1 });
+                    seats.Add(n - 1);
+                    return n - 1;
+                }
+                else
+                {
+                    priorityQueue.Enqueue(new int[] { 0, seats.Min }, new int[] { 0, seats.Min });
+                    seats.Add(0);
+                    return 0;
+                }
+
+
+
+            }
+
+            public void Leave(int p)
+            {
+                if (p != seats.Min && p != seats.Max)
+                {
+                    int prev = seats.GetViewBetween(0, p - 1).Max;
+                    int next = seats.GetViewBetween(p + 1, n - 1).Min;
+                    priorityQueue.Enqueue(new int[] { prev, next }, new int[] { prev, next });
+                }
+                seats.Remove(p);
+
+
+            }
+        }
+    }
+}
