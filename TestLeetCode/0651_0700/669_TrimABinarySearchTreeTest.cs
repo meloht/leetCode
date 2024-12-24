d730116dfe5ec6b47420c8f23c97561ea9802fe4
luohongtao <luohongtao621@hotmail.com>
2024-12-24 20:14:51 +0800

    669

---
+++ TestLeetCode/0651_0700/669_TrimABinarySearchTreeTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode._0651_0700;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _669_TrimABinarySearchTreeTest
+    {
+        _669_TrimABinarySearchTreeAlg alg = new _669_TrimABinarySearchTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 0, 2];
+            int low = 1, high = 2;
+            int?[] exp = [1, null, 2];
+            var tree = TreeNode.BuildTree(root);
+            var res = alg.TrimBST(tree,low,high);
+            int?[] ress = TreeNode.GetTreeLevelList(res);
+            Assert.IsTrue(exp.SequenceEqual(ress));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [3, 0, 4, null, 2, null, null, 1];
+            int low = 1, high = 3;
+            int?[] exp = [3, 2, null, 1];
+            var tree = TreeNode.BuildTree(root);
+            var res = alg.TrimBST(tree, low, high);
+            int?[] ress = TreeNode.GetTreeLevelList(res);
+            Assert.IsTrue(exp.SequenceEqual(ress));
+        }
+    }
+}
---
+++ leetCode/0651_0700/669_TrimABinarySearchTreeAlg.cs
@@ -0,0 +1,37 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _669_TrimABinarySearchTreeAlg
+    {
+        public TreeNode TrimBST(TreeNode root, int low, int high)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            if (root.val < low)
+            {
+                return TrimBST(root.right, low, high);
+            }
+            else if (root.val > high)
+            {
+                return TrimBST(root.left, low, high);
+            }
+            else
+            {
+                root.left = TrimBST(root.left, low, high);
+                root.right = TrimBST(root.right, low, high);
+                return root;
+            }
+
+
+        }
+       
+    }
+}
