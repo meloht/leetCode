73a8c6a9e7ccd881a542e8b494e47cfd0169e874
luohongtao <luohongtao621@hotmail.com>
2024-12-24 19:36:05 +0800

    662

---
+++ TestLeetCode/0651_0700/662_MaximumWidthOfBinaryTreeTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._0651_0700;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _662_MaximumWidthOfBinaryTreeTest
+    {
+        _662_MaximumWidthOfBinaryTreeAlg alg = new _662_MaximumWidthOfBinaryTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 3, 2, 5, 3, null, 9];
+            int exp = 4;
+            var tree = TreeNode.BuildTree(root);
+            int res = alg.WidthOfBinaryTree(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1, 3, 2, 5, null, null, 9, 6, null, 7];
+            int exp = 7;
+            var tree = TreeNode.BuildTree(root);
+            int res = alg.WidthOfBinaryTree(tree);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [1, 3, 2, 5];
+            int exp = 2;
+            var tree = TreeNode.BuildTree(root);
+            int res = alg.WidthOfBinaryTree(tree);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/662_MaximumWidthOfBinaryTreeAlg.cs
@@ -0,0 +1,45 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _662_MaximumWidthOfBinaryTreeAlg
+    {
+
+        public int WidthOfBinaryTree(TreeNode root)
+        {
+            int res = 1;
+            IList<Tuple<TreeNode, int>> arr = new List<Tuple<TreeNode, int>>();
+            arr.Add(new Tuple<TreeNode, int>(root, 1));
+            while (arr.Count > 0)
+            {
+                IList<Tuple<TreeNode, int>> tmp = new List<Tuple<TreeNode, int>>();
+                foreach (Tuple<TreeNode, int> pair in arr)
+                {
+                    TreeNode node = pair.Item1;
+                    int index = pair.Item2;
+                    if (node.left != null)
+                    {
+                        tmp.Add(new Tuple<TreeNode, int>(node.left, index * 2));
+                    }
+                    if (node.right != null)
+                    {
+                        tmp.Add(new Tuple<TreeNode, int>(node.right, index * 2 + 1));
+                    }
+                }
+                res = Math.Max(res, arr[arr.Count - 1].Item2 - arr[0].Item2 + 1);
+                arr = tmp;
+            }
+            return res;
+
+
+        }
+
+      
+    }
+}
