ac25c3152cb6e3f57db8178e381e8cc68be4f788
luohongtao <luohongtao621@hotmail.com>
2024-12-24 19:01:58 +0800

    1705

---
+++ TestLeetCode/1701_1750/1705_MaximumNumberOfEatenApplesTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1701_1750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1701_1750
+{
+    [TestClass]
+    public class _1705_MaximumNumberOfEatenApplesTest
+    {
+        _1705_MaximumNumberOfEatenApplesAlg alg = new _1705_MaximumNumberOfEatenApplesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] apples = [1, 2, 3, 5, 2], days = [3, 2, 1, 4, 2];
+            int exp = 7;
+            int res = alg.EatenApples(apples, days);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] apples = [3, 0, 0, 0, 0, 2], days = [3, 0, 0, 0, 0, 2];
+            int exp = 5;
+            int res = alg.EatenApples(apples, days);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1701_1750/1705_MaximumNumberOfEatenApplesAlg.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1701_1750
+{
+    public class _1705_MaximumNumberOfEatenApplesAlg
+    {
+        public int EatenApples(int[] apples, int[] days)
+        {
+            int idx = 0;
+            int ans = 0;
+            PriorityQueue<int[], int> queue = new PriorityQueue<int[], int>();
+            for (int i = 0; i < apples.Length; i++)
+            {
+                if (apples[i] > 0)
+                {
+                    ans++;
+                    if (apples[i] - 1 > 0)
+                    {
+                        idx = i;
+                        int day = i + days[i];
+
+                        queue.Enqueue([apples[i] - 1, day], day);
+                        break;
+                    }
+
+                }
+            }
+            idx++;
+
+            while (queue.Count > 0 || queue.Count == 0 && idx < apples.Length)
+            {
+                while (queue.Count > 0 && queue.Peek()[1] <= idx)
+                {
+                    queue.Dequeue();
+                }
+                if (idx < apples.Length)
+                {
+                    if (apples[idx] > 0)
+                    {
+                        int day = idx + days[idx];
+
+                        queue.Enqueue([apples[idx], day], day);
+
+                    }
+                }
+
+                while (queue.Count == 0 && idx < apples.Length)
+                {
+                    if (apples[idx] > 0)
+                    {
+                        int day = idx + days[idx];
+                        queue.Enqueue([apples[idx], day], day);
+                        break;
+                    }
+                    idx++;
+
+                }
+                if (queue.Count > 0)
+                {
+                    var item = queue.Peek();
+                    if (item[0] == 1)
+                    {
+                        ans++;
+                        queue.Dequeue();
+                    }
+                    else if (item[0] > 1)
+                    {
+                        item[0]--;
+                        ans++;
+                    }
+                }
+                idx++;
+
+            }
+
+            return ans;
+        }
+    }
+}
