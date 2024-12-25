3ea1a23435f417168ee45cc7725702d2c87c307d
luohongtao <luohongtao621@hotmail.com>
2024-12-25 19:38:14 +0800

    3218

---
+++ TestLeetCode/3201_3250/3218_MinimumCostForCuttingCakeITest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3218_MinimumCostForCuttingCakeITest
+    {
+        _3218_MinimumCostForCuttingCakeIAlg alg = new _3218_MinimumCostForCuttingCakeIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int m = 3, n = 2;
+            int[] horizontalCut = [1, 3], verticalCut = [5];
+            int exp = 13;
+            int res = alg.MinimumCost(m, n, horizontalCut, verticalCut);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int m = 2, n = 2;
+            int[] horizontalCut = [7], verticalCut = [4];
+            int exp = 15;
+            int res = alg.MinimumCost(m, n, horizontalCut, verticalCut);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3218_MinimumCostForCuttingCakeIAlg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3218_MinimumCostForCuttingCakeIAlg
+    {
+        int[][][][] memo;
+        int[] horizontalCut;
+        int[] verticalCut;
+
+        public int MinimumCost(int m, int n, int[] horizontalCut, int[] verticalCut)
+        {
+            this.memo = new int[m][][][];
+            this.horizontalCut = horizontalCut;
+            this.verticalCut = verticalCut;
+            for (int i = 0; i < m; i++)
+            {
+                memo[i] = new int[n][][];
+                for (int j = 0; j < n; j++)
+                {
+                    memo[i][j] = new int[m][];
+                    for (int k = 0; k < m; k++)
+                    {
+                        memo[i][j][k] = new int[n];
+                        Array.Fill(memo[i][j][k], -1);
+                    }
+                }
+            }
+            return DP(0, 0, m - 1, n - 1);
+        }
+
+        public int DP(int row1, int col1, int row2, int col2)
+        {
+            if (row1 == row2 && col1 == col2)
+            {
+                return 0;
+            }
+            if (memo[row1][col1][row2][col2] < 0)
+            {
+                int res = int.MaxValue;
+                for (int i = row1; i < row2; i++)
+                {
+                    res = Math.Min(res, DP(row1, col1, i, col2) + DP(i + 1, col1, row2, col2) + horizontalCut[i]);
+                }
+                for (int i = col1; i < col2; i++)
+                {
+                    res = Math.Min(res, DP(row1, col1, row2, i) + DP(row1, i + 1, row2, col2) + verticalCut[i]);
+                }
+                memo[row1][col1][row2][col2] = res;
+            }
+            return memo[row1][col1][row2][col2];
+        }
+
+    }
+}
