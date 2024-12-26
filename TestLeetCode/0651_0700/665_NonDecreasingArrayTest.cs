305c4082146628e1f993ef8205cbcc226de969a3
luohongtao <luohongtao621@hotmail.com>
2024-12-26 16:03:37 +0800

    665

---
+++ TestLeetCode/0651_0700/665_NonDecreasingArrayTest.cs
@@ -0,0 +1,68 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _665_NonDecreasingArrayTest
+    {
+        _665_NonDecreasingArrayAlg alg = new _665_NonDecreasingArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 2, 3];
+            bool exp = true;
+            bool res = alg.CheckPossibility(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [4, 2, 1];
+            bool exp = false;
+            bool res = alg.CheckPossibility(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [5, 7, 1, 8];
+            bool exp = true;
+            bool res = alg.CheckPossibility(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [3, 4, 2, 3];
+            bool exp = false;
+            bool res = alg.CheckPossibility(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] nums = [3, 7, 2, 4];
+            bool exp = false;
+            bool res = alg.CheckPossibility(nums);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase06()
+        {
+            int[] nums = [-1, 4, 2, 3];
+            bool exp = true;
+            bool res = alg.CheckPossibility(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/665_NonDecreasingArrayAlg.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._0651_0700
+{
+    public class _665_NonDecreasingArrayAlg
+    {
+        public bool CheckPossibility(int[] nums)
+        {
+            int ans = 0;
+            int idx = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i - 1] > nums[i])
+                {
+                    ans++;
+                    idx = i;
+                }
+                if (ans > 1)
+                {
+                    return false;
+                }
+                
+            }
+           
+            if (ans == 0)
+            {
+                return true;
+            }
+
+            if (idx == nums.Length - 1 || idx == 1)
+                return true;
+
+            if (nums[idx + 1] >= nums[idx - 1])
+            {
+                return true;
+            }
+
+            if (idx -2 >= 0 && nums[idx - 2] <= nums[idx])
+            {
+                return true;
+            }
+           
+
+            return false;
+        }
+
+        public bool CheckPossibility1(int[] nums)
+        {
+            int n = nums.Length, cnt = 0;
+            for (int i = 0; i < n - 1; ++i)
+            {
+                int x = nums[i], y = nums[i + 1];
+                if (x > y)
+                {
+                    cnt++;
+                    if (cnt > 1)
+                    {
+                        return false;
+                    }
+                    if (i > 0 && y < nums[i - 1])
+                    {
+                        nums[i + 1] = x;
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+}
