23950e6f03c0f929bf0857d27e4c67ba026364e4
luohongtao <luohongtao621@hotmail.com>
2024-12-26 14:19:08 +0800

    3083

---
+++ TestLeetCode/3051_3100/3083_ExistenceOfASubstringInAStringAndItsReverseTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3051_3100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3051_3100
+{
+    [TestClass]
+    public class _3083_ExistenceOfASubstringInAStringAndItsReverseTest
+    {
+        _3083_ExistenceOfASubstringInAStringAndItsReverseAlg alg = new _3083_ExistenceOfASubstringInAStringAndItsReverseAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "leetcode";
+            bool exp = true;
+            bool res = alg.IsSubstringPresent(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "abcba";
+            bool exp = true;
+            bool res = alg.IsSubstringPresent(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "abcd";
+            bool exp = false;
+            bool res = alg.IsSubstringPresent(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3051_3100/3083_ExistenceOfASubstringInAStringAndItsReverseAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3051_3100
+{
+    public class _3083_ExistenceOfASubstringInAStringAndItsReverseAlg
+    {
+        public bool IsSubstringPresent(string s)
+        {
+            if (s.Length < 2)
+                return false;
+            HashSet<string> dict = new HashSet<string>();
+            char[] arr = new char[2];
+            arr[0] = s[0];
+            arr[1] = s[1];
+            if (arr[0] == arr[1])
+                return true;
+
+            dict.Add(new string(arr));
+
+            for (int i = 2; i < s.Length; i++)
+            {
+
+                arr[1] = s[i];
+                arr[0] = s[i - 1];
+
+                if (arr[0] == arr[1])
+                    return true;
+
+                string ss = new string([arr[1], arr[0]]);
+                if (dict.Contains(ss))
+                {
+                    return true;
+                }
+                dict.Add(new string(arr));
+            }
+
+
+            return false;
+        }
+    }
+}
