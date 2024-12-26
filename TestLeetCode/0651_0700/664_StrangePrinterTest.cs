a9ebf7ba00a6cc8a51d25305fae8f052c97cf0c0
luohongtao <luohongtao621@hotmail.com>
2024-12-26 15:24:28 +0800

    664

---
+++ TestLeetCode/0651_0700/664_StrangePrinterTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _664_StrangePrinterTest
+    {
+        _664_StrangePrinterAlg alg = new _664_StrangePrinterAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "aaabbb";
+            int exp = 2;
+            int res = alg.StrangePrinter(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "aba";
+            int exp = 2;
+            int res = alg.StrangePrinter(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "tbgtgb";
+            int exp = 4;
+            int res = alg.StrangePrinter(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/664_StrangePrinterAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _664_StrangePrinterAlg
+    {
+        public int StrangePrinter(string s)
+        {
+            int n = s.Length;
+            int[,] f = new int[n, n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                f[i, i] = 1;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        f[i, j] = f[i, j - 1];
+                    }
+                    else
+                    {
+                        int minn = int.MaxValue;
+                        for (int k = i; k < j; k++)
+                        {
+                            minn = Math.Min(minn, f[i, k] + f[k + 1, j]);
+                        }
+                        f[i, j] = minn;
+                    }
+                }
+            }
+            return f[0, n - 1];
+        }
+
+    }
+}
