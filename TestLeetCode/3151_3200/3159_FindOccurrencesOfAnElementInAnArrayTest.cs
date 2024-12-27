9025a1ad35953deed736ea7e7042e3d2a04caf4e
luohongtao <luohongtao621@hotmail.com>
2024-12-27 16:17:51 +0800

    3159

---
+++ TestLeetCode/3151_3200/3159_FindOccurrencesOfAnElementInAnArrayTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._3151_3200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_3200
+{
+    [TestClass]
+    public class _3159_FindOccurrencesOfAnElementInAnArrayTest
+    {
+        _3159_FindOccurrencesOfAnElementInAnArrayAlg alg = new _3159_FindOccurrencesOfAnElementInAnArrayAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 1, 7], queries = [1, 3, 2, 4];
+            int x = 1;
+            int[] exp = [0, -1, 2, -1];
+            int[] res = alg.OccurrencesOfElement(nums, queries, x);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3], queries = [10];
+            int x = 5;
+            int[] exp = [-1];
+            int[] res = alg.OccurrencesOfElement(nums, queries, x);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/3151_3200/3159_FindOccurrencesOfAnElementInAnArrayAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_3200
+{
+    public class _3159_FindOccurrencesOfAnElementInAnArrayAlg
+    {
+        public int[] OccurrencesOfElement(int[] nums, int[] queries, int x)
+        {
+            int[] result = new int[queries.Length];
+            List<int> dict = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == x)
+                {
+                    dict.Add(i);
+                }
+            }
+            for (int i = 0; i < queries.Length; i++)
+            {
+                if (dict.Count >= queries[i])
+                {
+                    result[i] = dict[queries[i] - 1];
+                }
+                else
+                {
+                    result[i] = -1;
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
