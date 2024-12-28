2156b1ecb2a9bbf767d5b3af43cbb0f1b793808e
luohongtao <luohongtao621@hotmail.com>
2024-12-28 14:22:34 +0800

    667

--- leetCode/0651_0700/667_BeautifulArrangementIIAlg.cs
+++ leetCode/0651_0700/667_BeautifulArrangementIIAlg.cs
@@ -10,7 +10,23 @@ namespace leetCode._0651_0700
     {
         public int[] ConstructArray(int n, int k)
         {
-            return null;
+            int[] ans = new int[n];
+            int idx = 0;
+            for (int i = 1; i < n - k; i++)
+            {
+                ans[idx++] = i;
+            }
+            for (int i = n-k,j=n; i <= j; i++,j--)
+            {
+                ans[idx] = i;
+                idx++;
+                if (i != j)
+                {
+                    ans[idx] = j;
+                    idx++;
+                }
+            }
+            return ans;
         }
     }
 }
