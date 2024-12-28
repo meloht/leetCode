7f6b5e5b562f8848eeec46c969c8ad67ba84418f
luohongtao <luohongtao621@hotmail.com>
2024-12-28 14:03:51 +0800

    3046

---
+++ TestLeetCode/3001_3050/3046_SplitTheArrayTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3001_3050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3001_3050
+{
+    [TestClass]
+    public class _3046_SplitTheArrayTest
+    {
+        _3046_SplitTheArrayAlg alg = new _3046_SplitTheArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 1, 2, 2, 3, 4];
+            bool exp = true;
+            bool res = alg.IsPossibleToSplit(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1, 1, 1];
+            bool exp = false;
+            bool res = alg.IsPossibleToSplit(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3001_3050/3046_SplitTheArrayAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3001_3050
+{
+    public class _3046_SplitTheArrayAlg
+    {
+        public bool IsPossibleToSplit(int[] nums)
+        {
+            int[] arr = new int[101];
+            foreach (var item in nums)
+            {
+                arr[item]++;
+                if (arr[item] > 2)
+                {
+                    return false;
+                }
+            }
+           
+            return true;
+        }
+
+
+    }
+}
