15cacd876aaf9c762b061e3cfbffccd4101a3664
luohongtao <luohongtao621@hotmail.com>
2024-12-28 14:51:50 +0800

    668

---
+++ TestLeetCode/0651_0700/668_KthSmallestNumberInMultiplicationTableTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _668_KthSmallestNumberInMultiplicationTableTest
+    {
+        _668_KthSmallestNumberInMultiplicationTableAlg alg = new _668_KthSmallestNumberInMultiplicationTableAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int m = 3, n = 3, k = 5;
+            int exp = 3;
+            int res = alg.FindKthNumber(m, n, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int m = 2, n = 3, k = 6;
+            int exp = 6;
+            int res = alg.FindKthNumber(m, n, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/668_KthSmallestNumberInMultiplicationTableAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._0651_0700
+{
+    public class _668_KthSmallestNumberInMultiplicationTableAlg
+    {
+        public int FindKthNumber(int m, int n, int k)
+        {
+            int left = 0;
+            int right = m * n;
+
+            while (left + 1 < right)
+            {
+                int mid = (left + right) >> 1;
+                if (check(mid, m, n, k))
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid;
+                }
+            }
+
+            return right;
+        }
+        private bool check(int x, int m, int n, int k)
+        {
+            int cnt = 0;
+            for (int i = 1; i <= m; i++)
+            {
+                cnt += Math.Min(x / i, n);
+            }
+            return cnt >= k;
+        }
+    }
+}
