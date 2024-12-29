2e088da46347e45b5179f840d0f6fc256c6ed501
luohongtao <luohongtao621@hotmail.com>
2024-12-29 14:45:25 +0800

    1366

---
+++ TestLeetCode/1351_1400/1366_RankTeamsByVotesTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._1351_1400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1351_1400
+{
+    [TestClass]
+    public class _1366_RankTeamsByVotesTest
+    {
+        _1366_RankTeamsByVotesAlg alg = new _1366_RankTeamsByVotesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] votes = ["ABC", "ACB", "ABC", "ACB", "ACB"];
+            string exp = "ACB";
+            string res = alg.RankTeams(votes);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] votes = ["WXYZ", "XYZW"];
+            string exp = "XWYZ";
+            string res = alg.RankTeams(votes);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] votes = ["ZMNAGUEDSJYLBOPHRQICWFXTVK"];
+            string exp = "ZMNAGUEDSJYLBOPHRQICWFXTVK";
+            string res = alg.RankTeams(votes);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string[] votes = ["BCA", "CAB", "CBA", "ABC", "ACB", "BAC"];
+            string exp = "ABC";
+            string res = alg.RankTeams(votes);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1351_1400/1366_RankTeamsByVotesAlg.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1351_1400
+{
+    public class _1366_RankTeamsByVotesAlg
+    {
+        public string RankTeams(string[] votes)
+        {
+            int n = votes[0].Length;
+            Node[] arr = new Node[26];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Node('0', n);
+            }
+
+            foreach (var vote in votes)
+            {
+                for (int i = 0; i < vote.Length; i++)
+                {
+                    var idx = vote[i]-'A';
+                    if (arr[idx].Item == '0')
+                    {
+                        arr[idx].Item = vote[i];
+                    }
+
+                    arr[idx].Cnt[i]++;
+
+                }
+
+            }
+            Array.Sort(arr, (x, y) =>
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (y.Cnt[i] != x.Cnt[i])
+                    {
+                        return y.Cnt[i] - x.Cnt[i];
+                    }
+                }
+                return x.Item.CompareTo(y.Item);
+            });
+            StringBuilder ans = new StringBuilder();
+            foreach (var item in arr)
+            {
+                if (item.Item != '0')
+                {
+                    ans.Append(item.Item);
+                }
+              
+            }
+            return ans.ToString();
+        }
+
+
+        class Node
+        {
+            public char Item;
+            public int[] Cnt;
+
+            public Node(char item, int n)
+            {
+                Item = item;
+                Cnt = new int[n];
+            }
+
+        }
+    }
+}
