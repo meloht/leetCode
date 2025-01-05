899495d70438e9465e16dc4b28b96de20f4adc75
luohongtao <luohongtao621@hotmail.com>
2025-01-05 15:33:00 +0800

    675

---
+++ TestLeetCode/0651_0700/675_CutOffTreesForGolfEventTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._0651_0700;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _675_CutOffTreesForGolfEventTest
+    {
+        _675_CutOffTreesForGolfEventAlg alg = new _675_CutOffTreesForGolfEventAlg();
+        
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] forest = [[1, 2, 3], [0, 0, 4], [7, 6, 5]];
+            int exp = 6;
+            int res = alg.CutOffTree(forest);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] forest = [[1, 2, 3], [0, 0, 0], [7, 6, 5]];
+            int exp = -1;
+            int res = alg.CutOffTree(forest);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] forest = [[2, 3, 4], [0, 0, 5], [8, 7, 6]];
+            int exp = 6;
+            int res = alg.CutOffTree(forest);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2201_2250/2241_DesignAnAtmMachineAlg.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2201_2250
+{
+    public class _2241_DesignAnAtmMachineAlg
+    {
+        public class ATM
+        {
+            int[] res = [-1];
+            int[] cnt;
+            int[] amounts = [20, 50, 100, 200, 500];
+            public ATM()
+            {
+                cnt = new int[5];
+            }
+
+            public void Deposit(int[] banknotesCount)
+            {
+                for (int i = 0; i < banknotesCount.Length; i++)
+                {
+                    cnt[i] += banknotesCount[i];
+                }
+            }
+
+            public int[] Withdraw(int amount)
+            {
+                int[] ans = new int[5];
+                for (int i = 4; i >= 0; i--)
+                {
+                    if (amount >= amounts[i] && cnt[i] > 0)
+                    {
+                        int count = Math.Min(amount / amounts[i], cnt[i]);
+                        ans[i] = count;
+                        amount -= count * amounts[i];
+                    }
+                    if (amount == 0)
+                    {
+                        break;
+                    }
+                }
+                if (amount == 0)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        cnt[i] -= ans[i];
+                    }
+                    return ans;
+                }
+                else
+                {
+                    return res;
+                }
+               
+            }
+        }
+
+    }
+}
