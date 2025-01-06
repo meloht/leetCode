39848c6bebd33b8f96667e466d241a3fe36163d2
hongtao_luo <luohongtao621@hotmail.com>
2025-01-06 14:17:06 +0800

    2274

---
+++ TestLeetCode/2251_2300/2274_MaximumConsecutiveFloorsWithoutSpecialFloorsTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2251_2300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2251_2300
+{
+    [TestClass]
+    public class _2274_MaximumConsecutiveFloorsWithoutSpecialFloorsTest
+    {
+        _2274_MaximumConsecutiveFloorsWithoutSpecialFloorsAlg alg = new _2274_MaximumConsecutiveFloorsWithoutSpecialFloorsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int bottom = 2, top = 9;
+            int[] special = [4, 6];
+            int exp = 3;
+            int res = alg.MaxConsecutive(bottom, top, special);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int bottom = 6, top = 8;
+            int[] special = [7, 6, 8];
+            int exp = 0;
+            int res = alg.MaxConsecutive(bottom, top, special);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2251_2300/2274_MaximumConsecutiveFloorsWithoutSpecialFloorsAlg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2274_MaximumConsecutiveFloorsWithoutSpecialFloorsAlg
+    {
+        public int MaxConsecutive(int bottom, int top, int[] special)
+        {
+            Array.Sort(special);
+
+            int ans = special[0] - bottom;
+            for (int i = 1; i < special.Length; i++)
+            {
+                ans = Math.Max(ans, special[i] - special[i - 1] - 1);
+            }
+            ans = Math.Max(ans, top - special[special.Length - 1]);
+
+            return ans;
+        }
+    }
+}
