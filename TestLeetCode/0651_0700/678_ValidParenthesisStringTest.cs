deec9ed35ca2b42dd3edb353843269d85d4af0f5
hongtao_luo <luohongtao621@hotmail.com>
2025-01-07 17:48:43 +0800

    678

---
+++ TestLeetCode/0651_0700/678_ValidParenthesisStringTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _678_ValidParenthesisStringTest
+    {
+        _678_ValidParenthesisStringAlg alg = new _678_ValidParenthesisStringAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "()";
+            bool exp = true;
+            bool res = alg.CheckValidString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "(*)";
+            bool exp = true;
+            bool res = alg.CheckValidString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "(*))";
+            bool exp = true;
+            bool res = alg.CheckValidString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "(((()))())))*))())()(**(((())(()(*()((((())))*())(())*(*(()(*)))()*())**((()(()))())(*(*))*))())";
+            bool exp = false;
+            bool res = alg.CheckValidString(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/678_ValidParenthesisStringAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _678_ValidParenthesisStringAlg
+    {
+        public bool CheckValidString(string s)
+        {
+            int leftMin = 0;
+            int leftMax = 0;
+            foreach (var c in s)
+            {
+                if (c == '(')
+                {
+                    leftMin++;
+                    leftMax++;
+                }
+                else if (c == ')')
+                {
+                    leftMin--;
+                    leftMax--;
+                }
+                else
+                {
+                    leftMin--;
+                    leftMax++;
+                }
+                if (leftMax < 0)
+                {
+                    return false;
+                }
+                leftMin = Math.Max(leftMin, 0);
+            }
+            return leftMin == 0;
+        }
+    }
+}
