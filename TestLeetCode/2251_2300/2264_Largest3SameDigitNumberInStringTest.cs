265da70702cd2a67ced5304a2a3aaba1a5e494a0
hongtao_luo <luohongtao621@hotmail.com>
2025-01-08 10:56:14 +0800

    2264

---
+++ TestLeetCode/2251_2300/2264_Largest3SameDigitNumberInStringTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2251_2300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2251_2300
+{
+    [TestClass]
+    public class _2264_Largest3SameDigitNumberInStringTest
+    {
+        _2264_Largest3SameDigitNumberInStringAlg alg = new _2264_Largest3SameDigitNumberInStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string num = "6777133339";
+            string exp = "777";
+            string res = alg.LargestGoodInteger(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string num = "2300019";
+            string exp = "000";
+            string res = alg.LargestGoodInteger(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string num = "42352338";
+            string exp = "";
+            string res = alg.LargestGoodInteger(num);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2251_2300/2264_Largest3SameDigitNumberInStringAlg.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2264_Largest3SameDigitNumberInStringAlg
+    {
+        public string LargestGoodInteger(string num)
+        {
+            int cnt = 1;
+
+            char ch = num[0];
+            char numSub = ' ';
+            for (int i = 1; i < num.Length; i++)
+            {
+                if (num[i] == num[i - 1])
+                {
+                    cnt++;
+                }
+                else
+                {
+                    cnt = 1;
+                    ch = num[i];
+
+                }
+                if (cnt == 3)
+                {
+                    if (numSub == ' ')
+                    {
+                        numSub = ch;
+                    }
+                    else
+                    {
+                        if (ch > numSub)
+                        {
+                            numSub = ch;
+                        }
+                    }
+                }
+            }
+
+            if (numSub != ' ')
+            {
+                return "".PadLeft(3, numSub);
+            }
+            return "";
+        }
+    }
+}
