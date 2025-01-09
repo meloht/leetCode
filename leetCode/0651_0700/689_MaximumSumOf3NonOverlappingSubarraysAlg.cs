192eae51994a10767fa39eb2c3827bbba39d510f
hongtao_luo <luohongtao621@hotmail.com>
2025-01-09 17:26:56 +0800

    689

--- leetCode/0651_0700/689_MaximumSumOf3NonOverlappingSubarraysAlg.cs
+++ leetCode/0651_0700/689_MaximumSumOf3NonOverlappingSubarraysAlg.cs
@@ -8,7 +8,7 @@ namespace leetCode._0651_0700
 {
     public class _689_MaximumSumOf3NonOverlappingSubarraysAlg
     {
-        public int[] MaxSumOfThreeSubarrays(int[] nums, int k)
+        public int[] MaxSumOfThreeSubarrays1(int[] nums, int k)
         {
             int[] ans = new int[3];
             int sum1 = 0, maxSum1 = 0, maxSum1Idx = 0;
@@ -44,6 +44,77 @@ namespace leetCode._0651_0700
                     sum3 -= nums[i - k + 1];
                 }
             }
+            return ans;
+        }
+
+        public int[] MaxSumOfThreeSubarrays(int[] nums, int k)
+        {
+            int n = nums.Length;
+            int[] ans = new int[3];
+
+            int[] left = new int[n];
+            int[] right = new int[n];
+
+            long[] leftNum = new long[n];
+
+            long sum = nums.Take(k).Sum();
+            leftNum[0] = sum;
+            for (int i = k, j = 0; i < n; i++)
+            {
+                sum += nums[i];
+                sum -= nums[j++];
+                leftNum[i - k + 1] = sum;
+            }
+
+            long max = 0;
+            int idx = 0;
+            for (int i = 0; i <= n - k; i++)
+            {
+                if (leftNum[i] > max)
+                {
+                    idx = i;
+                    max = leftNum[i];
+                }
+                left[i] = idx;
+            }
+
+            long[] rightNum = new long[n];
+            sum = nums.Skip(n - k).Take(k).Sum();
+            rightNum[n - k] = sum;
+            for (int i = n - k - 1, j = n - 1; i >= 0; i--)
+            {
+                sum += nums[i];
+                sum -= nums[j--];
+                rightNum[i] = sum;
+            }
+
+            max = 0;
+            idx = 0;
+            for (int i = n - k; i >= 0; i--)
+            {
+                if (rightNum[i] >= max)
+                {
+                    idx = i;
+                    max = rightNum[i];
+                }
+                right[i] = idx;
+            }
+
+            max = 0;
+            int lenn = n - 2 * k;
+            sum = 0;
+            for (int i = k; i <= lenn; i++)
+            {
+                sum = leftNum[left[i - k]] + leftNum[i] + rightNum[right[i + k]];
+                if (sum > max)
+                {
+                    max = sum;
+                    ans[0] = left[i - k];
+                    ans[1] = i;
+                    ans[2] = right[i + k];
+                }
+            }
+
             return ans;
         }
 
