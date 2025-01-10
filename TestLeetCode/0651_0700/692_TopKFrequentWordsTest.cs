eadd87574be412b584f14ed80af7dc1bb6c555b8
hongtao_luo <luohongtao621@hotmail.com>
2025-01-10 14:12:56 +0800

    692

---
+++ TestLeetCode/0651_0700/692_TopKFrequentWordsTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _692_TopKFrequentWordsTest
+    {
+        _692_TopKFrequentWordsAlg alg = new _692_TopKFrequentWordsAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["i", "love", "leetcode", "i", "love", "coding"];
+            int k = 2;
+            string[] exp = ["i", "love"];
+            string[] res = alg.TopKFrequent(words, k).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"];
+            int k = 4;
+            string[] exp = ["the", "is", "sunny", "day"];
+            string[] res = alg.TopKFrequent(words, k).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0651_0700/692_TopKFrequentWordsAlg.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _692_TopKFrequentWordsAlg
+    {
+        public IList<string> TopKFrequent(string[] words, int k)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (var item in words)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+            List<(string, int)> list = new List<(string, int)>();
+            foreach (var item in dict)
+            {
+                list.Add((item.Key, item.Value));
+            }
+            list.Sort((x, y) =>
+            {
+                if (x.Item2 == y.Item2)
+                {
+                    return x.Item1.CompareTo(y.Item1);
+                }
+                return y.Item2 - x.Item2;
+
+            });
+
+            List<string> ls = new List<string>();
+            for (int i = 0; i < k; i++)
+            {
+                ls.Add(list[i].Item1);
+            }
+            return ls;
+        }
+    }
+}
