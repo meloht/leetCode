2f484c1bbac65b0aa97bd9f36d8b3eaf11b4be8e
hongtao_luo <luohongtao621@hotmail.com>
2025-01-10 10:11:21 +0800

    691

---
+++ TestLeetCode/0651_0700/691_StickersToSpellWordTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0651_0700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0651_0700
+{
+    [TestClass]
+    public class _691_StickersToSpellWordTest
+    {
+        _691_StickersToSpellWordAlg alg = new _691_StickersToSpellWordAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] stickers = ["with", "example", "science"];
+            string target = "thehat";
+            int exp = 3;
+            int res = alg.MinStickers(stickers, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] stickers = ["notice", "possible"];
+            string target = "basicbasic";
+            int exp = -1;
+            int res = alg.MinStickers(stickers, target);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0651_0700/691_StickersToSpellWordAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0651_0700
+{
+    public class _691_StickersToSpellWordAlg
+    {
+        public int MinStickers(string[] stickers, string target)
+        {
+
+            return 0;
+        }
+    }
+}
