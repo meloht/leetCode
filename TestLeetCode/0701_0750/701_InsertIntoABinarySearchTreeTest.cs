b98700166359e59d0d56187061100ffe6b86a598
hongtao_luo <luohongtao621@hotmail.com>
2025-01-10 15:37:24 +0800

    701

---
+++ TestLeetCode/0701_0750/701_InsertIntoABinarySearchTreeTest.cs
@@ -0,0 +1,56 @@
+﻿using leetCode._0701_0750;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _701_InsertIntoABinarySearchTreeTest
+    {
+        _701_InsertIntoABinarySearchTreeAlg alg = new _701_InsertIntoABinarySearchTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [4, 2, 7, 1, 3];
+            int val = 5;
+            int?[] exp = [4, 2, 7, 1, 3, 5];
+
+            TreeNode node = TreeNode.BuildTree(root);
+            TreeNode res = alg.InsertIntoBST(node, val);
+            int?[] ress = TreeNode.GetTreeLevelList(node);
+            Assert.IsTrue(exp.SequenceEqual(ress));
+        }
+
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [40, 20, 60, 10, 30, 50, 70];
+            int val = 25;
+            int?[] exp = [40, 20, 60, 10, 30, 50, 70, null, null, 25];
+
+            TreeNode node = TreeNode.BuildTree(root);
+            TreeNode res = alg.InsertIntoBST(node, val);
+            int?[] ress = TreeNode.GetTreeLevelList(node);
+            Assert.IsTrue(exp.SequenceEqual(ress));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [4, 2, 7, 1, 3, null, null, null, null, null, null];
+            int val = 5;
+            int?[] exp = [4, 2, 7, 1, 3, 5];
+
+            TreeNode node = TreeNode.BuildTree(root);
+            TreeNode res = alg.InsertIntoBST(node, val);
+            int?[] ress = TreeNode.GetTreeLevelList(node);
+            Assert.IsTrue(exp.SequenceEqual(ress));
+        }
+    }
+}
---
+++ leetCode/0701_0750/701_InsertIntoABinarySearchTreeAlg.cs
@@ -0,0 +1,48 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _701_InsertIntoABinarySearchTreeAlg
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                root = new TreeNode(val);
+                return root;
+            }
+            Dfs(root, val);
+            return root;
+        }
+
+        public void Dfs(TreeNode root, int val)
+        {
+            if (root == null)
+                return;
+
+            if (val > root.val)
+            {
+                if (root.right == null)
+                {
+                    root.right = new TreeNode(val);
+                    return;
+                }
+                Dfs(root.right, val);
+            }
+            else
+            {
+                if (root.left == null)
+                {
+                    root.left = new TreeNode(val);
+                    return;
+                }
+                Dfs(root.left, val);
+            }
+        }
+    }
+}
