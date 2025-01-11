063945cd439545737a83671e30b6f7afd209201e
luohongtao <luohongtao621@hotmail.com>
2025-01-11 13:58:47 +0800

    3270

---
+++ TestLeetCode/3251_3300/3270_FindTheKeyOfTheNumbersTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._3251_3300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3251_3300
+{
+    [TestClass]
+    public class _3270_FindTheKeyOfTheNumbersTest
+    {
+        _3270_FindTheKeyOfTheNumbersAlg alg = new _3270_FindTheKeyOfTheNumbersAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int num1 = 1, num2 = 10, num3 = 1000;
+            int exp = 0;
+            int res = alg.GenerateKey(num1, num2, num3);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int num1 = 987, num2 = 879, num3 = 798;
+            int exp = 777;
+            int res = alg.GenerateKey(num1, num2, num3);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int num1 = 1, num2 = 2, num3 = 3;
+            int exp = 1;
+            int res = alg.GenerateKey(num1, num2, num3);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3251_3300/3270_FindTheKeyOfTheNumbersAlg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3251_3300
+{
+    public class _3270_FindTheKeyOfTheNumbersAlg
+    {
+        public int GenerateKey(int num1, int num2, int num3)
+        {
+            string n1 = num1.ToString().PadLeft(4, '0');
+            string n2 = num2.ToString().PadLeft(4, '0');
+            string n3 = num3.ToString().PadLeft(4, '0');
+
+            List<char> list = new List<char>();
+            for (int i = 0; i <4; i++)
+            {
+                var nn = Math.Min(n1[i], n2[i]) ;
+                var nnn = Math.Min(nn, n3[i]) ;
+                if (list.Count == 0&& nnn == '0')
+                {
+                    continue;
+                }
+                list.Add((char)nnn);
+            }
+
+            if(list.Count==0)
+                return 0;
+
+            StringBuilder sb=new StringBuilder();
+            foreach (var item in list)
+            {
+                sb.Append(item);
+            }
+            return int.Parse(sb.ToString());
+        }
+
+        public int GenerateKey1(int num1, int num2, int num3)
+        {
+            int key = 0;
+            for (int p = 1; num1 > 0 && num2 > 0 && num3 > 0; p *= 10)
+            {
+                key += Math.Min(Math.Min(num1 % 10, num2 % 10), num3 % 10) * p;
+                num1 /= 10;
+                num2 /= 10;
+                num3 /= 10;
+            }
+            return key;
+        }
+
+
+    }
+}
