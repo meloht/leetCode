8b5f2e54f7204a3f39900a60ffc2801e63684e20
luohongtao <luohongtao621@hotmail.com>
2025-01-12 15:47:23 +0800

    725

---
+++ TestLeetCode/0701_0750/725_SplitLinkedListInPartsTest.cs
@@ -0,0 +1,53 @@
+﻿using leetCode;
+using leetCode._0701_0750;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _725_SplitLinkedListInPartsTest
+    {
+        _725_SplitLinkedListInPartsAlg alg = new _725_SplitLinkedListInPartsAlg();
+
+        private int[][] GetList(ListNode[] nodes)
+        {
+            int[][] res = new int[nodes.Length][];
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                res[i] = ListNode.GetLinkNodeList(nodes[i]);
+            }
+            return res;
+
+        }
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [1, 2, 3];
+            int k = 5;
+            int[][] exp = [[1], [2], [3], [], []];
+            ListNode node = ListNode.BuildListNode(head);
+            var resNode = alg.SplitListToParts(node, k);
+            int[][] res = GetList(resNode);
+
+            Assert.IsTrue(Utils.IsSameList(exp, res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            int k = 3;
+            int[][] exp = [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]];
+            ListNode node = ListNode.BuildListNode(head);
+            var resNode = alg.SplitListToParts(node, k);
+            int[][] res = GetList(resNode);
+
+            Assert.IsTrue(Utils.IsSameList(exp, res));
+        }
+    }
+}
---
+++ leetCode/0701_0750/725_SplitLinkedListInPartsAlg.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _725_SplitLinkedListInPartsAlg
+    {
+        public ListNode[] SplitListToParts(ListNode head, int k)
+        {
+            ListNode[] result = new ListNode[k];
+            int len = 0;
+            var node = head;
+            while (node != null)
+            {
+                len++;
+                node = node.next;
+            }
+            int res = len % k;
+            int avg = len / k;
+
+            node = head;
+            int i = 0;
+            int count = 0;
+
+            ListNode curr = null;
+            int nodeCount = 0;
+            while (node != null)
+            {
+                if (count == 0)
+                {
+                    nodeCount = res > 0 ? avg + 1 : avg;
+                    if (nodeCount > 0)
+                    {
+                        curr = new ListNode(node.val);
+                        result[i] = curr;
+
+                        count++;
+                        i++;
+                        if (res > 0)
+                        {
+                            res--;
+                        }
+                    }
+                }
+                else
+                {
+                    curr.next = new ListNode(node.val);
+                    curr = curr.next;
+                    count++;
+                }
+                if (nodeCount == count)
+                {
+                    count = 0;
+                }
+                node = node.next;
+            }
+
+            return result;
+        }
+    }
+}
