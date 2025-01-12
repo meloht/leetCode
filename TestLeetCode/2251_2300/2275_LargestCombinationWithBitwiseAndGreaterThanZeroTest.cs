d665ddaeeafde5f98dbb5871d2be9688f2aea898
luohongtao <luohongtao621@hotmail.com>
2025-01-12 13:41:56 +0800

    2275

---
+++ TestLeetCode/2251_2300/2275_LargestCombinationWithBitwiseAndGreaterThanZeroTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._2251_2300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2251_2300
+{
+    [TestClass]
+    public class _2275_LargestCombinationWithBitwiseAndGreaterThanZeroTest
+    {
+        _2275_LargestCombinationWithBitwiseAndGreaterThanZeroAlg alg = new _2275_LargestCombinationWithBitwiseAndGreaterThanZeroAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+
+            int[] candidates = [16, 17, 71, 62, 12, 24, 14];
+            int exp = 4;
+            int res = alg.LargestCombination(candidates);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] candidates = [8, 8];
+            int exp = 2;
+            int res = alg.LargestCombination(candidates);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2251_2300/2275_LargestCombinationWithBitwiseAndGreaterThanZeroAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2275_LargestCombinationWithBitwiseAndGreaterThanZeroAlg
+    {
+        public int LargestCombination(int[] candidates)
+        {
+            int[] dict = new int[32];
+            foreach (int item in candidates)
+            {
+                int x = item;
+                int i = 0;
+                while (x != 0)
+                {
+                    dict[i] += (x & 1);
+
+                    x >>= 1;
+                    i++;
+                }
+            }
+            Array.Sort(dict);
+            int max = dict[31];
+          
+            return max;
+        }
+
+
+    }
+}
