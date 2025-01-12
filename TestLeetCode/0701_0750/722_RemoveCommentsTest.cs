0ded9999af9e8f1157efa68b2035f1aad1552211
luohongtao <luohongtao621@hotmail.com>
2025-01-12 14:31:05 +0800

    722

---
+++ TestLeetCode/0701_0750/722_RemoveCommentsTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._0701_0750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0701_0750
+{
+    [TestClass]
+    public class _722_RemoveCommentsTest
+    {
+        _722_RemoveCommentsAlg alg = new _722_RemoveCommentsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] source = ["/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"];
+            string[] exp = ["int main()", "{ ", "  ", "int a, b, c;", "a = b + c;", "}"];
+
+            string[] res = alg.RemoveComments(source).ToArray();
+
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] source = ["a/*comment", "line", "more_comment*/b"];
+            string[] exp = ["ab"];
+
+            string[] res = alg.RemoveComments(source).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+    }
+}
---
+++ leetCode/0701_0750/722_RemoveCommentsAlg.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0701_0750
+{
+    public class _722_RemoveCommentsAlg
+    {
+        public IList<string> RemoveComments(string[] source)
+        {
+            List<string> list = new List<string>();
+
+            bool bl = false;
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in source)
+            {
+                bool bl2 = false;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (!bl && !bl2)
+                    {
+                        if (i + 1 < s.Length && s[i] == '/' && s[i + 1] == '*')
+                        {
+                            bl = true;
+                            i++;
+                        }
+                        else if (i + 1 < s.Length && s[i] == '/' && s[i + 1] == '/')
+                        {
+                            bl2 = true;
+                            i++;
+                        }
+                        else
+                        {
+
+                            sb.Append(s[i]);
+                        }
+                    }
+                    else if (bl)
+                    {
+                        if (i + 1 < s.Length && s[i] == '*' && s[i + 1] == '/')
+                        {
+                            bl = false;
+                            i++;
+                        }
+                    }
+                }
+
+                if (!bl && sb.Length > 0)
+                {
+                    list.Add(sb.ToString());
+                    sb.Clear();
+                }
+
+
+            }
+
+            return list;
+        }
+    }
+}
