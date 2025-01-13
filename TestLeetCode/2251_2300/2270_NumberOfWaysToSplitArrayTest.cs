b3c695db050be6c90c1c0c5544d5bac91131bd9b
hongtao_luo <luohongtao621@hotmail.com>
2025-01-13 12:12:26 +0800

    2270

---
+++ TestLeetCode/2251_2300/2270_NumberOfWaysToSplitArrayTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2251_2300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2251_2300
+{
+    [TestClass]
+    public class _2270_NumberOfWaysToSplitArrayTest
+    {
+        _2270_NumberOfWaysToSplitArrayAlg alg = new _2270_NumberOfWaysToSplitArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [10, 4, -8, 7];
+            int exp = 2;
+            int res = alg.WaysToSplitArray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 3, 1, 0];
+            int exp = 2;
+            int res = alg.WaysToSplitArray(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2251_2300/2270_NumberOfWaysToSplitArrayAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2270_NumberOfWaysToSplitArrayAlg
+    {
+        public int WaysToSplitArray(int[] nums)
+        {
+            long total =0 ;
+           
+            for (int i = 0; i < nums.Length; i++)
+            {
+                total += nums[i];
+            }
+            int ans = 0;
+            long sum = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                sum += nums[i];
+                long right = total - sum;
+                if (sum >= right)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
