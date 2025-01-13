34a037f75d061ae33e2a8732ba2f24ae01fb8938
hongtao_luo <luohongtao621@hotmail.com>
2025-01-13 13:44:23 +0800

    735

--- leetCode/0701_0750/735_AsteroidCollisionAlg.cs
+++ leetCode/0701_0750/735_AsteroidCollisionAlg.cs
@@ -65,5 +65,34 @@ namespace leetCode._0701_0750
 
             return ans;
         }
+
+        public int[] AsteroidCollision1(int[] asteroids)
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (int aster in asteroids)
+            {
+                bool alive = true;
+                while (alive && aster < 0 && stack.Count > 0 && stack.Peek() > 0)
+                {
+                    alive = stack.Peek() < -aster; // aster 是否存在
+                    if (stack.Peek() <= -aster)
+                    {  // 栈顶行星爆炸
+                        stack.Pop();
+                    }
+                }
+                if (alive)
+                {
+                    stack.Push(aster);
+                }
+            }
+            int count = stack.Count;
+            int[] ans = new int[count];
+            for (int i = count - 1; i >= 0; i--)
+            {
+                ans[i] = stack.Pop();
+            }
+            return ans;
+        }
+
     }
 }
