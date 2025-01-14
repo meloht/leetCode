4275dd08a135a9541d8f4f0f1ecbbd37130ba6af
hongtao_luo <luohongtao621@hotmail.com>
2025-01-14 14:22:52 +0800

    754

---
+++ TestLeetCode/0751_0800/754_ReachANumberTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _754_ReachANumberTest
+    {
+        _754_ReachANumberAlg alg = new _754_ReachANumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int target = 2;
+            int exp = 3;
+            int res = alg.ReachNumber(target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int target = 3;
+            int exp = 2;
+            int res = alg.ReachNumber(target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int target = 4;
+            int exp = 3;
+            int res = alg.ReachNumber(target);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/754_ReachANumberAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _754_ReachANumberAlg
+    {
+        public int ReachNumber(int target)
+        {
+            int t = Math.Abs(target);
+            int sum = 0;
+            int s = 0;
+            while (sum < t)
+            {
+                s++;
+                sum += s;
+            }
+            int dis = sum - t;
+            if (dis % 2 == 0)
+                return s;
+            if (s % 2 == 0)
+            {
+                return s + 1;
+            }
+
+            return s + 2;
+        }
+    }
+}
