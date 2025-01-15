fa5de4d7f373e5ce681c7d5d077c327175981666
hongtao_luo <luohongtao621@hotmail.com>
2025-01-15 11:01:27 +0800

    3066

---
+++ TestLeetCode/3051_3100/3066_MinimumOperationsToExceedThresholdValueIITest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._3051_3100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3051_3100
+{
+    [TestClass]
+    public class _3066_MinimumOperationsToExceedThresholdValueIITest
+    {
+        _3066_MinimumOperationsToExceedThresholdValueIIAlg alg = new _3066_MinimumOperationsToExceedThresholdValueIIAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 11, 10, 1, 3];
+            int k = 10;
+            int exp = 2;
+            int res = alg.MinOperations(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 1, 2, 4, 9];
+            int k = 20;
+            int exp = 4;
+            int res = alg.MinOperations(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3051_3100/3066_MinimumOperationsToExceedThresholdValueIIAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3051_3100
+{
+    public class _3066_MinimumOperationsToExceedThresholdValueIIAlg
+    {
+        public int MinOperations(int[] nums, int k)
+        {
+            PriorityQueue<long, long> queue = new PriorityQueue<long, long>();
+
+            foreach (var item in nums)
+            {
+                queue.Enqueue(item, item);
+            }
+            int ans = 0;
+            while (queue.Count > 1 && queue.Peek() < k)
+            {
+                var num1 = queue.Dequeue();
+                var num2 = queue.Dequeue();
+                long n = Math.Min(num1, num2) * 2 + Math.Max(num1, num2);
+                queue.Enqueue(n, n);
+                ans++;
+            }
+
+            return ans;
+        }
+    }
+}
