5c783af3d05fd8b800d11477adc069ef98cdf2fd
hongtao_luo <luohongtao621@hotmail.com>
2025-01-15 11:16:18 +0800

    764

---
+++ TestLeetCode/0751_0800/764_LargestPlusSignTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _764_LargestPlusSignTest
+    {
+        _764_LargestPlusSignAlg alg = new _764_LargestPlusSignAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 5;
+            int[][] mines = [[4, 2]];
+            int exp = 2;
+            int res = alg.OrderOfLargestPlusSign(n, mines);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1;
+            int[][] mines = [[0, 0]];
+            int exp = 0;
+            int res = alg.OrderOfLargestPlusSign(n, mines);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/764_LargestPlusSignAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _764_LargestPlusSignAlg
+    {
+        public int OrderOfLargestPlusSign(int n, int[][] mines)
+        {
+            return 0;
+        }
+    }
+}
