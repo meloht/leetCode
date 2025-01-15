dac02d43645e2984658db1b43f39a2caf0ff2573
hongtao_luo <luohongtao621@hotmail.com>
2025-01-15 15:31:25 +0800

    767

---
+++ TestLeetCode/0751_0800/767_ReorganizeStringTest.cs
@@ -0,0 +1,70 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _767_ReorganizeStringTest
+    {
+        _767_ReorganizeStringAlg alg = new _767_ReorganizeStringAlg();
+
+        private bool CheckString(string s)
+        {
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i - 1] == s[i])
+                    return false;
+
+            }
+            return true;
+        }
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "aab";
+            string exp = "aba";
+            string res = alg.ReorganizeString(s);
+            Assert.IsTrue(CheckString(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "aaab";
+            string exp = "";
+            string res = alg.ReorganizeString(s);
+            Assert.IsTrue(CheckString(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "ogccckcwmbmxtsbmozli";
+            string exp = "cmcmcmcbobotsxzlkigw";
+            string res = alg.ReorganizeString(s);
+            Assert.IsTrue(CheckString(res));
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "aabbcc";
+            string exp = "abacbc";
+            string res = alg.ReorganizeString(s);
+            Assert.IsTrue(CheckString(res));
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string s = "aaaaaaccccbbb";
+            string exp = "abacbc";
+            string res = alg.ReorganizeString(s);
+            Assert.IsTrue(CheckString(res));
+        }
+    }
+}
---
+++ leetCode/0751_0800/767_ReorganizeStringAlg.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _767_ReorganizeStringAlg
+    {
+        public string ReorganizeString(string s)
+        {
+            if (s.Length == 1)
+                return s;
+
+            int[] arr = new int[26];
+            int max = 0;
+            foreach (var ch in s)
+            {
+                arr[ch - 'a']++;
+                max = Math.Max(max, arr[ch - 'a']);
+            }
+
+            if (max >= s.Length - max + 2)
+            {
+                return "";
+            }
+            PriorityQueue<char, int> queue = new PriorityQueue<char, int>(Comparer<int>.Create((x, y) => y - x));
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    continue;
+                }
+                queue.Enqueue((char)(i + 'a'), arr[i]);
+            }
+            StringBuilder sb = new StringBuilder();
+            char prevChar = queue.Dequeue();
+            while (queue.Count > 0)
+            {
+                char currChar = queue.Dequeue();
+                int prevCount = arr[prevChar - 'a'];
+                int currCount = arr[currChar - 'a'];
+                int cnt = Math.Min(prevCount, currCount);
+                for (int i = 0; i < cnt; i++)
+                {
+                    if (sb.Length > 0 && sb[sb.Length - 1] == prevChar)
+                    {
+                        sb.Append(currChar);
+                        sb.Append(prevChar);
+                    }
+                    else
+                    {
+                        sb.Append(prevChar);
+                        sb.Append(currChar);
+                    }
+
+                }
+                prevCount -= cnt;
+                currCount -= cnt;
+                arr[prevChar - 'a'] = prevCount;
+                arr[currChar - 'a'] = currCount;
+
+                if (prevCount == 0 && currCount == 0)
+                {
+                    prevChar = '-';
+                    if (queue.Count > 0)
+                    {
+                        prevChar = queue.Dequeue();
+                    }
+                }
+                else if (currCount > 0)
+                {
+                    prevChar = currChar;
+                }
+            }
+
+            if (prevChar != '-')
+            {
+                if (arr[prevChar - 'a'] == 1 && sb[sb.Length - 1] != prevChar)
+                {
+                    sb.Append(prevChar);
+                    return sb.ToString();
+                }
+
+                if (arr[prevChar - 'a'] > 0)
+                {
+                    StringBuilder sbb = new StringBuilder();
+                    int count = arr[prevChar - 'a'];
+                    for (int i = 0, j = 0; i < sb.Length; i++, j++)
+                    {
+
+                        if (j < count)
+                        {
+                            sbb.Append(prevChar);
+                        }
+                        sbb.Append(sb[i]);
+                    }
+
+                    return sbb.ToString();
+                }
+            }
+            return sb.ToString();
+
+        }
+    }
+}
