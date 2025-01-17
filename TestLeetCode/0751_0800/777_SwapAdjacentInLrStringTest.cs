4b932712dc7280811c7040922f4e3bfdcfc7a5e2
hongtao_luo <luohongtao621@hotmail.com>
2025-01-17 16:24:33 +0800

    777

---
+++ TestLeetCode/0751_0800/777_SwapAdjacentInLrStringTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _777_SwapAdjacentInLrStringTest
+    {
+        _777_SwapAdjacentInLrStringAlg alg = new _777_SwapAdjacentInLrStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string start = "RXXLRXRXL";
+            string result = "XRLXXRRLX";
+            bool exp = true;
+            bool res = alg.CanTransform(start, result);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string start = "X";
+            string result = "L";
+            bool exp = false;
+            bool res = alg.CanTransform(start, result);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/777_SwapAdjacentInLrStringAlg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _777_SwapAdjacentInLrStringAlg
+    {
+        public bool CanTransform1(string start, string end)
+        {
+            if (start.Length != end.Length)
+            {
+                return false;
+            }
+
+            int n = start.Length;
+            int i = 0, j = 0;
+
+            while (i < n || j < n)
+            {
+                while (i < n && start[i] == 'X')
+                {
+                    i++;
+                }
+                while (j < n && end[j] == 'X')
+                {
+                    j++;
+                }
+
+                if (i < n && j < n)
+                {
+                    if (start[i] != end[j])
+                    {
+                        return false;
+                    }
+
+                    if (start[i] == 'L' && i < j)
+                    {
+                        return false;
+                    }
+
+                    if (start[i] == 'R' && i > j)
+                    {
+                        return false;
+                    }
+
+                    i++;
+                    j++;
+                }
+                else if (i < n || j < n)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
