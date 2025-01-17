4d4d117fddd1bb07517eeab2cd6d84eedaa7f987
hongtao_luo <luohongtao621@hotmail.com>
2025-01-17 15:31:42 +0800

    775

---
+++ TestLeetCode/0751_0800/775_GlobalAndLocalInversionsTest.cs
@@ -0,0 +1,49 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _775_GlobalAndLocalInversionsTest
+    {
+        _775_GlobalAndLocalInversionsAlg alg = new _775_GlobalAndLocalInversionsAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 0, 2];
+            bool exp = true;
+            bool res = alg.IsIdealPermutation(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 0];
+            bool exp = false;
+            bool res = alg.IsIdealPermutation(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [2, 0, 1];
+            bool exp = false;
+            bool res = alg.IsIdealPermutation(nums);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] nums = [2, 1, 0];
+            bool exp = false;
+            bool res = alg.IsIdealPermutation(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/775_GlobalAndLocalInversionsAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _775_GlobalAndLocalInversionsAlg
+    {
+        public bool IsIdealPermutation1(int[] nums)
+        {
+            int global = 0;
+            int local = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i + 1 < nums.Length && nums[i] > nums[i + 1])
+                {
+                    local++;
+                }
+                if (i < nums[i])
+                {
+                    global += nums[i] - i;
+                }
+                else if (i - nums[i] > 1)
+                {
+                    global += i - nums[i] - 1;
+                }
+            }
+
+            return global == local;
+        }
+
+        public bool IsIdealPermutation(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (Math.Abs(nums[i] - i) > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+    }
+}
