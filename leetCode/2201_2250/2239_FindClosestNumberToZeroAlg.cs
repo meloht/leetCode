b12a3129e81c6c005634c27f45c3d7b84e783c8c
hongtao_luo <luohongtao621@hotmail.com>
2025-01-20 17:48:26 +0800

    2239

---
+++ leetCode/2201_2250/2239_FindClosestNumberToZeroAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2201_2250
+{
+    public class _2239_FindClosestNumberToZeroAlg
+    {
+        public int FindClosestNumber(int[] nums)
+        {
+            int ans = 0;
+            int min = int.MaxValue;
+            foreach (var item in nums)
+            {
+                int val = Math.Abs(item);
+                if (val < min)
+                {
+                    min = val;
+                    ans = item;
+                }
+                else if (val == min)
+                {
+                    ans = Math.Max(ans, item);
+                }
+
+            }
+            return ans;
+        }
+    }
+}
