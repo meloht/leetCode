4882b767a2179dba4ed42fd34867b92b917d6b9d
hongtao_luo <luohongtao621@hotmail.com>
2025-01-22 13:00:49 +0800

    1561

---
+++ TestLeetCode/1551_1600/1561_MaximumNumberOfCoinsYouCanGetTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1551_1600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1551_1600
+{
+    [TestClass]
+    public class _1561_MaximumNumberOfCoinsYouCanGetTest
+    {
+        _1561_MaximumNumberOfCoinsYouCanGetAlg alg = new _1561_MaximumNumberOfCoinsYouCanGetAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] piles = [2, 4, 1, 2, 7, 8];
+            int exp = 9;
+            int res = alg.MaxCoins(piles);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] piles = [2, 4, 5];
+            int exp = 4;
+            int res = alg.MaxCoins(piles);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] piles = [9, 8, 7, 6, 5, 1, 2, 3, 4];
+            int exp = 18;
+            int res = alg.MaxCoins(piles);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1551_1600/1561_MaximumNumberOfCoinsYouCanGetAlg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1551_1600
+{
+    public class _1561_MaximumNumberOfCoinsYouCanGetAlg
+    {
+        public int MaxCoins(int[] piles)
+        {
+            Array.Sort(piles);
+            int n = piles.Length / 3;
+            int res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                res += piles[piles.Length - 2 * (i + 1)];
+            }
+            return res;
+        }
+    }
+}
