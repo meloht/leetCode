38debef2f3cc6becbe5a1bfead10ca6168f8335d
hongtao_luo <luohongtao621@hotmail.com>
2025-01-23 14:46:46 +0800

    2920

---
+++ TestLeetCode/2901_2950/2920_MaximumPointsAfterCollectingCoinsFromAllNodesTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._2901_2950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2901_2950
+{
+    [TestClass]
+    public class _2920_MaximumPointsAfterCollectingCoinsFromAllNodesTest
+    {
+        _2920_MaximumPointsAfterCollectingCoinsFromAllNodesAlg alg = new _2920_MaximumPointsAfterCollectingCoinsFromAllNodesAlg();
+        
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] edges = [[0, 1], [1, 2], [2, 3]];
+            int[] coins = [10, 10, 3, 3];
+            int k = 5;
+            int exp = 11;
+            int res = alg.MaximumPoints(edges, coins, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] edges = [[0, 1], [0, 2]];
+            int[] coins = [8, 4, 4];
+            int k = 0;
+            int exp = 16;
+            int res = alg.MaximumPoints(edges, coins, k);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/2901_2950/2920_MaximumPointsAfterCollectingCoinsFromAllNodesAlg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2901_2950
+{
+    public class _2920_MaximumPointsAfterCollectingCoinsFromAllNodesAlg
+    {
+        public int MaximumPoints(int[][] edges, int[] coins, int k)
+        {
+
+
+            return 0;
+        }
+    }
+}
