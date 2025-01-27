dd5647071626b6c8618519b2f71abe3214479d89
hongtao_luo <luohongtao621@hotmail.com>
2025-01-27 09:46:06 +0800

    781

---
+++ TestLeetCode/0751_0800/781_RabbitsInForestTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _781_RabbitsInForestTest
+    {
+        _781_RabbitsInForestAlg alg=new _781_RabbitsInForestAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] answers = [1, 1, 2];
+            int exp = 5;
+            int res = alg.NumRabbits(answers);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] answers = [10, 10, 10];
+            int exp = 11;
+            int res = alg.NumRabbits(answers);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] answers = [1, 0, 1, 0, 0];
+            int exp = 5;
+            int res = alg.NumRabbits(answers);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/781_RabbitsInForestAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _781_RabbitsInForestAlg
+    {
+        public int NumRabbits(int[] answers)
+        {
+
+            Dictionary<int,int> dict = new Dictionary<int, int>();
+            int ans = 0;
+            foreach (var item in answers)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict[item] = 1;
+                }
+            }
+
+            foreach (var item in dict)
+            {
+                if (item.Key == item.Value - 1)
+                {
+                    ans += item.Key + 1;
+                }
+                else
+                {
+                    ans += (item.Value + item.Key) / (item.Key + 1) * (item.Key + 1);
+                }
+            }
+            return ans;
+        }
+    }
+}
