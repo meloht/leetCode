d1f43f0a7bc9152513d71616b01664d32e361631
hongtao_luo <luohongtao621@hotmail.com>
2025-02-05 13:50:24 +0800

    786

---
+++ TestLeetCode/0751_0800/786_KThSmallestPrimeFractionTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _786_KThSmallestPrimeFractionTest
+    {
+        _786_KThSmallestPrimeFractionAlg alg = new _786_KThSmallestPrimeFractionAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [1, 2, 3, 5];
+            int k = 3;
+            int[] exp = [2, 5];
+            int[] res = alg.KthSmallestPrimeFraction(arr, k);
+            CollectionAssert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [1, 7];
+            int k = 1;
+            int[] exp = [1, 7];
+            int[] res = alg.KthSmallestPrimeFraction(arr, k);
+            CollectionAssert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/786_KThSmallestPrimeFractionAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _786_KThSmallestPrimeFractionAlg
+    {
+        public int[] KthSmallestPrimeFraction(int[] arr, int k)
+        {
+            PriorityQueue<int[], double> queue = new PriorityQueue<int[], double>(Comparer<double>.Create((x, y) => y.CompareTo(x)));
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    double d = (double)arr[j] / arr[i];
+                    queue.Enqueue([arr[j], arr[i]], d);
+                    while (queue.Count > k)
+                    {
+                        queue.Dequeue();
+                    }
+                }
+            }
+
+           
+            return queue.Peek();
+        }
+    }
+}
