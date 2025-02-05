653abe91fcdbf331d9cb394496341b62384240af
hongtao_luo <luohongtao621@hotmail.com>
2025-02-05 12:25:27 +0800

    784

---
+++ TestLeetCode/0751_0800/784_LetterCasePermutationTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _784_LetterCasePermutationTest
+    {
+        _784_LetterCasePermutationAlg alg=new _784_LetterCasePermutationAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "a1b2";
+            List<string> exp = new List<string> { "a1b2", "A1b2", "a1B2", "A1B2" };
+            IList<string> res = alg.LetterCasePermutation(s);
+            CollectionAssert.AreEqual(exp, res.ToList());
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "3z4";
+            List<string> exp = ["3z4", "3Z4"];
+            IList<string> res = alg.LetterCasePermutation(s);
+            CollectionAssert.AreEqual(exp, res.ToList());
+        }
+    }
+}
---
+++ leetCode/0751_0800/784_LetterCasePermutationAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _784_LetterCasePermutationAlg
+    {
+        public IList<string> LetterCasePermutation(string s)
+        {
+            List<string> result = new List<string>();
+            result.Add(s);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    int count = result.Count;
+                    for (int j = 0; j < count; j++)
+                    {
+                        char[] temp = result[j].ToCharArray();
+                        temp[i] = char.IsUpper(s[i]) ? char.ToLower(s[i]) : char.ToUpper(s[i]);
+                        result.Add(new string(temp));
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
