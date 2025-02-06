913882aee2f4d2af634970c0a99578fff932f61a
hongtao_luo <luohongtao621@hotmail.com>
2025-02-06 14:25:26 +0800

    787

---
+++ TestLeetCode/0751_0800/787_CheapestFlightsWithinKStopsTest.cs
@@ -0,0 +1,57 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _787_CheapestFlightsWithinKStopsTest
+    {
+        _787_CheapestFlightsWithinKStopsAlg alg = new _787_CheapestFlightsWithinKStopsAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 4;
+            int[][] flights = [
+                [0, 1, 100],
+                [1, 2, 100],
+                [2, 0, 100],
+                [1, 3, 600],
+                [2, 3, 200]];
+            int src = 0;
+            int dst = 3;
+            int k = 1;
+            int exp = 700;
+            int res = alg.FindCheapestPrice(n, flights, src, dst, k);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 3;
+            int[][] flights = [[0, 1, 100], [1, 2, 100], [0, 2, 500]];
+            int src = 0;
+            int dst = 2;
+            int k = 1;
+            int exp = 200;
+            int res = alg.FindCheapestPrice(n, flights, src, dst, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 3;
+            int[][] flights = [[0, 1, 100], [1, 2, 100], [0, 2, 500]];
+            int src = 0;
+            int dst = 2;
+            int k = 0;
+            int exp = 500;
+            int res = alg.FindCheapestPrice(n, flights, src, dst, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/787_CheapestFlightsWithinKStopsAlg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _787_CheapestFlightsWithinKStopsAlg
+    {
+        public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k)
+        {
+            int[][] dp = new int[n][];
+            List<int[]>[] vist = new List<int[]>[n];
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = new int[k + 2];
+                vist[i] = new List<int[]>();
+            }
+
+            foreach (var item in flights)
+            {
+                vist[item[0]].Add(item);
+            }
+
+
+            return Dfs(vist, src, dst, k + 1, dp);
+        }
+
+        private int Dfs(List<int[]>[] flights, int src, int dst, int k, int[][] dp)
+        {
+            if (k < 0)
+            {
+                return -1;
+            }
+            if (src == dst)
+            {
+                return 0;
+            }
+            if (dp[src][k] != 0)
+            {
+                return dp[src][k];
+            }
+            int min = int.MaxValue;
+            var list = flights[src];
+            foreach (var flight in list)
+            {
+                int price = Dfs(flights, flight[1], dst, k - 1, dp);
+                if (price != -1)
+                {
+                    min = Math.Min(min, price + flight[2]);
+                }
+            }
+            dp[src][k] = min == int.MaxValue ? -1 : min;
+            return dp[src][k];
+        }
+    }
+}
