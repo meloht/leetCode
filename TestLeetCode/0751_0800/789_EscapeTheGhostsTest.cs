a011029c8046881c07ece2747e3e9499c24648eb
hongtao_luo <luohongtao621@hotmail.com>
2025-02-06 15:05:37 +0800

    789

---
+++ TestLeetCode/0751_0800/789_EscapeTheGhostsTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _789_EscapeTheGhostsTest
+    {
+        _789_EscapeTheGhostsAlg alg = new _789_EscapeTheGhostsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] ghosts = [[1, 0], [0, 3]];
+            int[] target = [0, 1];
+            bool exp = true;
+            bool res=alg.EscapeGhosts(ghosts, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] ghosts = [[1, 0]];
+            int[] target = [2, 0];
+            bool exp = false;
+            bool res = alg.EscapeGhosts(ghosts, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] ghosts = [[2, 0]];
+            int[] target = [1, 0];
+            bool exp = false;
+            bool res = alg.EscapeGhosts(ghosts, target);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0751_0800/789_EscapeTheGhostsAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _789_EscapeTheGhostsAlg
+    {
+        public bool EscapeGhosts(int[][] ghosts, int[] target)
+        {
+
+            int min = int.MaxValue;
+            foreach (var ghost in ghosts)
+            {
+                min = Math.Min(min, Math.Abs(ghost[0] - target[0]) + Math.Abs(ghost[1] - target[1]));
+            }
+
+            int dis = Math.Abs(target[0]) + Math.Abs(target[1]);
+
+
+            return dis < min;
+        }
+    }
+}
