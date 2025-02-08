fe2cfc943d0f3e103e74a1d85c2d17f239c15db7
hongtao_luo <luohongtao621@hotmail.com>
2025-02-08 13:48:19 +0800

    791

---
+++ TestLeetCode/0751_0800/791_CustomSortStringTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _791_CustomSortStringTest
+    {
+        _791_CustomSortStringAlg alg = new _791_CustomSortStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string S = "cba";
+            string T = "abcd";
+            string exp = "cbad";
+            string res = alg.CustomSortString(S, T);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string S = "cbafg";
+            string T = "abcd";
+            string exp = "cbad";
+            string res = alg.CustomSortString(S, T);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/791_CustomSortStringAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _791_CustomSortStringAlg
+    {
+        public string CustomSortString(string S, string T)
+        {
+            int[] count = new int[26];
+            foreach (char c in T)
+            {
+                count[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in S)
+            {
+                if (count[c - 'a']-- > 0)
+                {
+                    sb.Append(c);
+                }
+            }
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                while (count[c - 'a']-- > 0)
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
