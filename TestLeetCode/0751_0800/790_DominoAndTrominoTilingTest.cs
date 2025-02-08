76e6d3ebfd4955e058ca4d2ff7453432d7e8854d
hongtao_luo <luohongtao621@hotmail.com>
2025-02-08 10:55:15 +0800

    790

---
+++ TestLeetCode/0751_0800/790_DominoAndTrominoTilingTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _790_DominoAndTrominoTilingTest
+    {
+        _790_DominoAndTrominoTilingAlg alg = new _790_DominoAndTrominoTilingAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int N = 3;
+            int exp = 5;
+            int res = alg.NumTilings(N);
+            Console.WriteLine(res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int N = 1;
+            int exp = 1;
+            int res = alg.NumTilings(N);
+            Console.WriteLine(res);
+        }
+       
+    }
+}
---
+++ leetCode/0751_0800/790_DominoAndTrominoTilingAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _790_DominoAndTrominoTilingAlg
+    {
+        public int NumTilings(int N)
+        {
+            if (N == 1) return 1;
+            if (N == 2) return 2;
+            if (N == 3) return 5;
+            int mod = 1000000007;
+            int[] dp = new int[N + 1];
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 5;
+            for (int i = 4; i <= N; i++)
+            {
+                dp[i] = ((2 * dp[i - 1]) % mod + dp[i - 3]) % mod;
+            }
+            return dp[N];
+        }
+    }
+}
