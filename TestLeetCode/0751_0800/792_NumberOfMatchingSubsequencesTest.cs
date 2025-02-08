61bc797c26476badbc47a66d328a89f2498d88df
hongtao_luo <luohongtao621@hotmail.com>
2025-02-08 15:24:10 +0800

    792

---
+++ TestLeetCode/0751_0800/792_NumberOfMatchingSubsequencesTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _792_NumberOfMatchingSubsequencesTest
+    {
+        _792_NumberOfMatchingSubsequencesAlg alg=new _792_NumberOfMatchingSubsequencesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abcde";
+            string[] words = new string[] { "a", "bb", "acd", "ace" };
+            int exp = 3;
+            int res = alg.NumMatchingSubseq(s, words);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "dsahjpjauf";
+            string[] words = ["ahjpjau", "ja", "ahbwzgqnuk", "tnmlanowax"];
+            int exp = 2;
+            int res = alg.NumMatchingSubseq(s, words);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0751_0800/792_NumberOfMatchingSubsequencesAlg.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _792_NumberOfMatchingSubsequencesAlg
+    {
+        public int NumMatchingSubseq(string s, string[] words)
+        {
+            IList<int>[] pos = new IList<int>[26];
+            for (int i = 0; i < 26; ++i)
+            {
+                pos[i] = new List<int>();
+            }
+            for (int i = 0; i < s.Length; ++i)
+            {
+                pos[s[i] - 'a'].Add(i);
+            }
+            int res = words.Length;
+            foreach (string w in words)
+            {
+                if (w.Length > s.Length)
+                {
+                    --res;
+                    continue;
+                }
+                int p = -1;
+                for (int i = 0; i < w.Length; ++i)
+                {
+                    char c = w[i];
+                    int idx = pos[c - 'a'].Count - 1;
+                    if (pos[c - 'a'].Count == 0 || pos[c - 'a'][idx] <= p)
+                    {
+                        --res;
+                        break;
+                    }
+                    p = BinarySearch(pos[c - 'a'], p);
+                }
+            }
+            return res;
+
+
+        }
+
+        public int BinarySearch(IList<int> list, int target)
+        {
+            int left = 0, right = list.Count - 1;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (list[mid] > target)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return list[left];
+        }
+
+    }
+}
