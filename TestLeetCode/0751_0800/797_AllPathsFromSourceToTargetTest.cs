f129a0dd8fbff945e401aa1faced6c3a3469fee8
hongtao_luo <luohongtao621@hotmail.com>
2025-02-13 15:13:09 +0800

    797

---
+++ TestLeetCode/0751_0800/797_AllPathsFromSourceToTargetTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _797_AllPathsFromSourceToTargetTest
+    {
+        _797_AllPathsFromSourceToTargetAlg alg = new _797_AllPathsFromSourceToTargetAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] graph = [[1, 2], [3], [3], []];
+            IList<IList<int>> exp = [[0, 1, 3], [0, 2, 3]];
+            var res=alg.AllPathsSourceTarget(graph);
+           Assert.IsTrue(Utils.IsSame(exp,res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] graph = [[4, 3, 1], [3, 2, 4], [3], [4], []];
+            IList<IList<int>> exp = [[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]];
+            var res = alg.AllPathsSourceTarget(graph);
+            Assert.IsTrue(Utils.IsSame(exp, res));
+        }
+    }
+}
---
+++ leetCode/0751_0800/797_AllPathsFromSourceToTargetAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _797_AllPathsFromSourceToTargetAlg
+    {
+        public IList<IList<int>> AllPathsSourceTarget(int[][] graph)
+        {
+            IList<IList<int>> res = new List<IList<int>>();
+            IList<int> path = new List<int>();
+            path.Add(0);
+            DFS(graph, 0, path, res);
+            return res;
+        }
+
+        private void DFS(int[][] graph, int node, IList<int> path, IList<IList<int>> res)
+        {
+            if (node == graph.Length - 1)
+            {
+                res.Add(new List<int>(path));
+                return;
+            }
+            foreach (int next in graph[node])
+            {
+                path.Add(next);
+                DFS(graph, next, path, res);
+                path.RemoveAt(path.Count - 1);
+            }
+        }
+    }
+}
