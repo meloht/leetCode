c88200d171da2589758c4e3c36d3227aa92184ea
hongtao_luo <luohongtao621@hotmail.com>
2025-02-13 14:53:38 +0800

    795

---
+++ TestLeetCode/0751_0800/795_NumberOfSubarraysWithBoundedMaximumTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._0751_0800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0751_0800
+{
+    [TestClass]
+    public class _795_NumberOfSubarraysWithBoundedMaximumTest
+    {
+        _795_NumberOfSubarraysWithBoundedMaximumAlg alg = new _795_NumberOfSubarraysWithBoundedMaximumAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 1, 4, 3];
+            int left = 2;
+            int right = 3;
+            int exp = 3;
+            int res = alg.NumSubarrayBoundedMax(nums, left, right);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 9, 2, 5, 6];
+            int left = 2;
+            int right = 8;
+            int exp = 7;
+            int res = alg.NumSubarrayBoundedMax(nums, left, right);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0751_0800/795_NumberOfSubarraysWithBoundedMaximumAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0751_0800
+{
+    public class _795_NumberOfSubarraysWithBoundedMaximumAlg
+    {
+        public int NumSubarrayBoundedMax(int[] nums, int left, int right)
+        {
+            int ans = 0;
+            int n = nums.Length, i0 = -1, i1 = -1;
+            for (int i = 0; i < n; ++i)
+            {
+                if (nums[i] > right)
+                    i0 = i;
+                if (nums[i] >= left) 
+                    i1 = i;
+                ans += i1 - i0;
+            }
+            return ans;
+
+        }
+    }
+}
