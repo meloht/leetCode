d4d6f1085c262b2702bc10d3a632f55c0af65467
hongtao_luo <luohongtao621@hotmail.com>
2025-02-14 11:52:30 +0800

    1552

---
+++ TestLeetCode/1551_1600/1552_MagneticForceBetweenTwoBallsTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._1551_1600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1551_1600
+{
+    [TestClass]
+    public class _1552_MagneticForceBetweenTwoBallsTest
+    {
+        _1552_MagneticForceBetweenTwoBallsAlg alg = new _1552_MagneticForceBetweenTwoBallsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] position = [1, 2, 3, 4, 7];
+            int m = 3;
+            int exp = 3;
+            var res = alg.MaxDistance(position, m);
+            Assert.IsTrue(exp == res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] position = [5, 4, 3, 2, 1, 1000000000];
+            int m = 2;
+            int exp = 999999999;
+            var res = alg.MaxDistance(position, m);
+            Assert.IsTrue(exp == res);
+        }
+    }
+}
---
+++ leetCode/1551_1600/1552_MagneticForceBetweenTwoBallsAlg.cs
@@ -0,0 +1,52 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1551_1600
+{
+    public class _1552_MagneticForceBetweenTwoBallsAlg
+    {
+        public int MaxDistance(int[] position, int m)
+        {
+            Array.Sort(position);
+            int ans = 0;
+            int left = 1;
+            int right = position[position.Length - 1] - position[0];
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (Check(position, mid, m))
+                {
+                    ans = mid;
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return ans;
+        }
+        private bool Check(int[] position, int mid, int m)
+        {
+            int count = 1;
+            int prev = position[0];
+            for (int i = 1; i < position.Length; i++)
+            {
+                if (position[i] - prev >= mid)
+                {
+                    count++;
+                    prev = position[i];
+                    if (count >= m)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return count >= m;
+        }
+    }
+}
