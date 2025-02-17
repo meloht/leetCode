e0e3b89dcef65a86d9687490a15fa60cebb6e210
hongtao_luo <luohongtao621@hotmail.com>
2025-02-17 14:03:08 +0800

    799

--- leetCode/0751_0800/799_ChampagneTowerAlg.cs
+++ leetCode/0751_0800/799_ChampagneTowerAlg.cs
@@ -10,7 +10,24 @@ namespace leetCode._0751_0800
     {
         public double ChampagneTower(int poured, int query_row, int query_glass)
         {
-            return 0;
+            double[] row = [poured];
+
+            for (int i = 1; i <= query_row; i++)
+            {
+                double[] nextRow = new double[i+1];
+                for (int j = 0; j < i; j++)
+                {
+                    double val = row[j];
+                    if (val > 1)
+                    {
+                        nextRow[j] += (val - 1) / 2;
+                        nextRow[j + 1] += (val - 1) / 2;
+                    }
+                }
+                row = nextRow;
+            }
+
+            return Math.Min(1, row[query_glass]);
         }
     }
 }
