34ca50bd31bc8c9cbc3ad4a231b27e58672d1ff0
hongtao_luo <luohongtao621@hotmail.com>
2025-02-17 10:43:05 +0800

    1287

---
+++ TestLeetCode/1251_1300/1287_ElementAppearingMoreThan25InSortedArrayTest.cs
@@ -0,0 +1,25 @@
+﻿using leetCode._1251_1300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1251_1300
+{
+    [TestClass]
+    public class _1287_ElementAppearingMoreThan25InSortedArrayTest
+    {
+        _1287_ElementAppearingMoreThan25InSortedArrayAlg alg = new _1287_ElementAppearingMoreThan25InSortedArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [1, 2, 2, 6, 6, 6, 6, 7, 10];
+            int exp = 6;
+            var res = alg.FindSpecialInteger(arr);
+            Assert.IsTrue(exp == res);
+        }
+
+    }
+}
---
+++ leetCode/1251_1300/1287_ElementAppearingMoreThan25InSortedArrayAlg.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1251_1300
+{
+    public class _1287_ElementAppearingMoreThan25InSortedArrayAlg
+    {
+        public int FindSpecialInteger(int[] arr)
+        {
+            int nn = arr.Length / 4;
+            int ans = 1;
+            int len = arr.Length - 1;
+            for (int i = 0; i < len; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    ans++;
+                    if (ans > nn)
+                    {
+                        return arr[i];
+                    }
+                }
+                else
+                {
+                    if (ans > nn)
+                    {
+                        return arr[i];
+                    }
+                    ans = 1;
+                   
+                }
+            }
+            if (ans > nn)
+            {
+                return arr[len];
+            }
+            return 0;
+        }
+
+        public int FindSpecialInteger1(int[] arr)
+        {
+            int n = arr.Length;
+            int cur = arr[0], cnt = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                if (arr[i] == cur)
+                {
+                    ++cnt;
+                    if (cnt * 4 > n)
+                    {
+                        return cur;
+                    }
+                }
+                else
+                {
+                    cur = arr[i];
+                    cnt = 1;
+                }
+            }
+            return -1;
+        }
+
+
+    }
+}
