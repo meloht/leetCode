bfdc29d5b29568e6076b4a0bdc069db9fd77dd32
hongtao_luo <luohongtao621@hotmail.com>
2025-02-19 09:38:57 +0800

    802

---
+++ TestLeetCode/0801_0850/802_FindEventualSafeStateTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _802_FindEventualSafeStateTest
+    {
+        _802_FindEventualSafeStateAlg alg = new _802_FindEventualSafeStateAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] graph = [[1, 2], [2, 3], [5], [0], [5], [], []];
+            int[] exp = [2, 4, 5, 6];
+            int[] res = alg.EventualSafeNodes(graph).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] graph = [[1, 2, 3, 4], [1, 2], [3, 4], [0, 4], []];
+            int[] exp = [4];
+            int[] res = alg.EventualSafeNodes(graph).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/0801_0850/802_FindEventualSafeStateAlg.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _802_FindEventualSafeStateAlg
+    {
+        public IList<int> EventualSafeNodes(int[][] graph)
+        {
+            int n = graph.Length;
+            int[] color = new int[n];
+            List<int> res = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (DFS(graph, color, i))
+                {
+                    res.Add(i);
+                }
+            }
+            return res;
+        }
+        private bool DFS(int[][] graph, int[] color, int node)
+        {
+            if (color[node] > 0)
+            {
+                return color[node] == 2;
+            }
+            color[node] = 1;
+            foreach (var next in graph[node])
+            {
+                if (color[next] == 2)
+                {
+                    continue;
+                }
+                if (color[next] == 1 || !DFS(graph, color, next))
+                {
+                    return false;
+                }
+            }
+            color[node] = 2;
+            return true;
+        }
+
+        /// <summary>
+        /// 拓扑排序
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <returns></returns>
+        public IList<int> EventualSafeNodes1(int[][] graph)
+        {
+            int n = graph.Length;
+            IList<IList<int>> rg = new List<IList<int>>();
+            for (int i = 0; i < n; ++i)
+            {
+                rg.Add(new List<int>());
+            }
+            int[] inDeg = new int[n];
+            for (int x = 0; x < n; ++x)
+            {
+                foreach (int y in graph[x])
+                {
+                    rg[y].Add(x);
+                }
+                inDeg[x] = graph[x].Length;
+            }
+
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < n; ++i)
+            {
+                if (inDeg[i] == 0)
+                {
+                    queue.Enqueue(i);
+                }
+            }
+            while (queue.Count > 0)
+            {
+                int y = queue.Dequeue();
+                foreach (int x in rg[y])
+                {
+                    if (--inDeg[x] == 0)
+                    {
+                        queue.Enqueue(x);
+                    }
+                }
+            }
+
+            IList<int> ans = new List<int>();
+            for (int i = 0; i < n; ++i)
+            {
+                if (inDeg[i] == 0)
+                {
+                    ans.Add(i);
+                }
+            }
+            return ans;
+        }
+
+    }
+}
