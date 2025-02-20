26141ff1e5949e80e816fcda592d28eb5dbc40f1
hongtao_luo <luohongtao621@hotmail.com>
2025-02-20 11:34:35 +0800

    813

---
+++ TestLeetCode/0801_0850/813_LargestSumOfAveragesTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _813_LargestSumOfAveragesTest
+    {
+        _813_LargestSumOfAveragesAlg alg = new _813_LargestSumOfAveragesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [9, 1, 2, 3, 9];
+            int k = 3;
+            double exp = 20.00000;
+            double res = alg.LargestSumOfAverages(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 2, 3, 4, 5, 6, 7];
+            int k = 4;
+            double exp = 20.50000;
+            double res = alg.LargestSumOfAverages(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/813_LargestSumOfAveragesAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _813_LargestSumOfAveragesAlg
+    {
+        public double LargestSumOfAverages(int[] nums, int k)
+        {
+            return 0;
+        }
+    }
+}
