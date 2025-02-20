c7f24f51c690bdea0f4bdb0eab1b504b4a6e8f74
hongtao_luo <luohongtao621@hotmail.com>
2025-02-20 15:18:10 +0800

    813

--- leetCode/0801_0850/813_LargestSumOfAveragesAlg.cs
+++ leetCode/0801_0850/813_LargestSumOfAveragesAlg.cs
@@ -10,7 +10,33 @@ namespace leetCode._0801_0850
     {
         public double LargestSumOfAverages(int[] nums, int k)
         {
-            return 0;
+            int n = nums.Length;
+            double[] prefix = new double[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                prefix[i + 1] = prefix[i] + nums[i];
+            }
+            double[][] dp = new double[n + 1][];
+            for (int i = 0; i <= n; i++)
+            {
+                dp[i] = new double[k + 1];
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                dp[i][1] = prefix[i] / i;
+            }
+            for (int j = 2; j <= k; j++)
+            {
+                for (int i = j; i <= n; i++)
+                {
+                    for (int x = j - 1; x < i; x++)
+                    {
+                        dp[i][j] = Math.Max(dp[i][j], dp[x][j - 1] + (prefix[i] - prefix[x]) / (i - x));
+                    }
+                }
+            }
+            return dp[n][k];
         }
+
     }
 }
