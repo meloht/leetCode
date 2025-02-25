d50f3d51d2edbf43dbc6de1de835dd1f3edbe2f0
hongtao_luo <luohongtao621@hotmail.com>
2025-02-25 14:05:32 +0800

    2502

---
+++ TestLeetCode/2501_2550/2502_DesignMemoryAllocatorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._2501_2550._2502_DesignMemoryAllocatorAlg;
+
+namespace Test._2501_2550
+{
+    [TestClass]
+    public class _2502_DesignMemoryAllocatorTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            Allocator loc = new Allocator(10); // 初始化一个大小为 10 的内存数组，所有内存单元都是空闲的。
+            Assert.AreEqual(0, loc.Allocate(1, 1)); // 最左侧的块的第一个下标是 0 。内存数组变为 [1, , , , , , , , , ]。返回 0 。
+            Assert.AreEqual(1, loc.Allocate(1, 2)); // 最左侧的块的第一个下标是 1 。内存数组变为 [1,2, , , , , , , , ]。返回 1 。
+            Assert.AreEqual(2, loc.Allocate(1, 3)); // 最左侧的块的第一个下标是 2 。内存数组变为 [1,2,3, , , , , , , ]。返回 2 。
+            Assert.AreEqual(1, loc.FreeMemory(2)); // 释放 mID 为 2 的所有内存单元。内存数组变为 [1, ,3, , , , , , , ] 。返回 1 ，因为只有 1 个 mID 为 2 的内存单元。
+            Assert.AreEqual(3, loc.Allocate(3, 4)); // 最左侧的块的第一个下标是 3 。内存数组变为 [1, ,3,4,4,4, , , , ]。返回 3 。
+            Assert.AreEqual(1, loc.Allocate(1, 1)); // 最左侧的块的第一个下标是 1 。内存数组变为 [1,1,3,4,4,4, , , , ]。返回 1 。
+            Assert.AreEqual(6, loc.Allocate(1, 1)); // 最左侧的块的第一个下标是 6 。内存数组变为 [1,1,3,4,4,4,1, , , ]。返回 6 。
+            Assert.AreEqual(3, loc.FreeMemory(1)); // 释放 mID 为 1 的所有内存单元。内存数组变为 [ , ,3,4,4,4, , , , ] 。返回 3 ，因为有 3 个 mID 为 1 的内存单元。
+            Assert.AreEqual(-1, loc.Allocate(10, 2)); // 无法找出长度为 10 个连续空闲内存单元的空闲块，所有返回 -1 。
+            Assert.AreEqual(0, loc.FreeMemory(7)); // 释放 mID 为 7 的所有内存单元。内存数组保持原状，因为不存在 mID 为 7 的内存单元。返回 0 。
+        }
+    }
+}
---
+++ leetCode/2501_2550/2502_DesignMemoryAllocatorAlg.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2501_2550
+{
+    public class _2502_DesignMemoryAllocatorAlg
+    {
+        public class Allocator
+        {
+            private int n;
+            private int[] memory;
+
+            public Allocator(int n)
+            {
+                this.n = n;
+                this.memory = new int[n];
+            }
+
+            public int Allocate(int size, int mID)
+            {
+                int count = 0;
+                for (int i = 0; i < n; ++i)
+                {
+                    if (memory[i] != 0)
+                    {
+                        count = 0;
+                    }
+                    else
+                    {
+                        ++count;
+                        if (count == size)
+                        {
+                            for (int j = i - count + 1; j <= i; ++j)
+                            {
+                                memory[j] = mID;
+                            }
+                            return i - count + 1;
+                        }
+                    }
+                }
+                return -1;
+            }
+
+            public int FreeMemory(int mID)
+            {
+                int count = 0;
+                for (int i = 0; i < n; ++i)
+                {
+                    if (memory[i] == mID)
+                    {
+                        ++count;
+                        memory[i] = 0;
+                    }
+                }
+                return count;
+            }
+
+
+        }
+    }
+
+}
