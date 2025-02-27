004dfc381658c28860faf2b0085f1bd6734ef773
hongtao_luo <luohongtao621@hotmail.com>
2025-02-27 13:22:32 +0800

    2296

---
+++ TestLeetCode/2251_2300/2296_DesignATextEditorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._2251_2300._2296_DesignATextEditorAlg;
+
+namespace Test._2251_2300
+{
+    [TestClass]
+    public class _2296_DesignATextEditorTest
+    {
+        [TestMethod]
+        public void TestCase01()
+        {
+            TextEditor textEditor = new TextEditor(); // 当前 text 为 "|" 。（'|' 字符表示光标）
+            textEditor.AddText("leetcode"); // 当前文本为 "leetcode|" 。
+            Assert.AreEqual(4, textEditor.DeleteText(4)); // 返回 4
+                                                          // 当前文本为 "leet|" 。
+                                                          // 删除了 4 个字符。
+            textEditor.AddText("practice"); // 当前文本为 "leetpractice|" 。
+            Assert.AreEqual("etpractice", textEditor.CursorRight(3)); // 返回 "etpractice"
+                                                                      // 当前文本为 "leetpractice|". 
+                                                                      // 光标无法移动到文本以外，所以无法移动。
+                                                                      // "etpractice" 是光标左边的 10 个字符。
+            Assert.AreEqual("leet", textEditor.CursorLeft(8)); // 返回 "leet"
+                                                               // 当前文本为 "leet|practice" 。
+                                                               // "leet" 是光标左边的 min(10, 4) = 4 个字符。
+            Assert.AreEqual(4, textEditor.DeleteText(10)); // 返回 4
+                                                           // 当前文本为 "|practice" 。
+                                                           // 只有 4 个字符被删除了。
+            Assert.AreEqual("", textEditor.CursorLeft(2)); // 返回 ""
+                                                           // 当前文本为 "|practice" 。
+                                                           // 光标无法移动到文本以外，所以无法移动。
+                                                           // "" 是光标左边的 min(10, 0) = 0 个字符。
+            Assert.AreEqual("practi", textEditor.CursorRight(6)); // 返回 "practi"
+                                                                  // 当前文本为 "practi|ce" 。
+                                                                  // "practi" 是光标左边的 min(10, 6) = 6 个字符。
+        }
+
+    }
+}
---
+++ leetCode/2251_2300/2296_DesignATextEditorAlg.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2296_DesignATextEditorAlg
+    {
+        public class TextEditor
+        {
+            private LinkedList<char> left;
+            private LinkedList<char> right;
+
+            public TextEditor()
+            {
+                left = new LinkedList<char>();
+                right = new LinkedList<char>();
+            }
+
+            public void AddText(string text)
+            {
+                foreach (char c in text)
+                {
+                    left.AddLast(c);
+                }
+            }
+
+            public int DeleteText(int k)
+            {
+                int count = 0;
+                while (k > 0 && left.Count > 0)
+                {
+                    left.RemoveLast();
+                    count++;
+                    k--;
+                }
+                return count;
+            }
+
+            public string CursorLeft(int k)
+            {
+                int move = Math.Min(k, left.Count);
+                for (int i = 0; i < move; i++)
+                {
+                    right.AddLast(left.Last.Value);
+                    left.RemoveLast();
+                }
+                return GetLeftText();
+            }
+
+            public string CursorRight(int k)
+            {
+                int move = Math.Min(k, right.Count);
+                for (int i = 0; i < move; i++)
+                {
+                    left.AddLast(right.Last.Value);
+                    right.RemoveLast();
+                }
+                return GetLeftText();
+            }
+
+            private string GetLeftText()
+            {
+                var text = new char[Math.Min(10, left.Count)];
+                var current = left.Last;
+                int index = text.Length - 1;
+                while (current != null && index >= 0)
+                {
+                    text[index--] = current.Value;
+                    current = current.Previous;
+                }
+                return new string(text);
+            }
+
+        }
+
+        public class TextEditor1
+        {
+
+            private Stack<char> left;
+            private Stack<char> right;
+
+            public TextEditor1()
+            {
+                left = new Stack<char>();
+                right = new Stack<char>();
+            }
+
+            public void AddText(string text)
+            {
+                foreach (char c in text)
+                {
+                    left.Push(c);
+                }
+            }
+
+            public int DeleteText(int k)
+            {
+                int k0 = k;
+                while (k > 0 && left.Count > 0)
+                {
+                    left.Pop();
+                    k--;
+                }
+                return k0 - k;
+            }
+
+            public string Text()
+            {
+                // Get the last 10 characters from the left stack
+                Stack<char> temp = new Stack<char>();
+                int count = Math.Min(10, left.Count);
+                for (int i = 0; i < count; i++)
+                {
+                    temp.Push(left.Pop());
+                }
+
+                string result = new string(temp.ToArray());
+
+                // Restore the characters back to the left stack
+                foreach (char c in temp)
+                {
+                    left.Push(c);
+                }
+
+                return result;
+            }
+
+            public string CursorLeft(int k)
+            {
+                while (k > 0 && left.Count > 0)
+                {
+                    right.Push(left.Pop());
+                    k--;
+                }
+                return Text();
+            }
+
+            public string CursorRight(int k)
+            {
+                while (k > 0 && right.Count > 0)
+                {
+                    left.Push(right.Pop());
+                    k--;
+                }
+                return Text();
+            }
+
+        }
+    }
+}
