eec4fc0198151312f70ee99c1d5eccd4b17f3945
hongtao_luo <luohongtao621@hotmail.com>
2025-03-05 13:58:14 +0800

    1328

---
+++ TestLeetCode/1301_1350/1328_BreakAPalindromeTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1301_1350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1301_1350
+{
+    [TestClass]
+    public class _1328_BreakAPalindromeTest
+    {
+        _1328_BreakAPalindromeAlg alg = new _1328_BreakAPalindromeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string palindrome = "abccba";
+            string exp = "aaccba";
+            string res = alg.BreakPalindrome(palindrome);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string palindrome = "a";
+            string exp = "";
+            string res = alg.BreakPalindrome(palindrome);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1301_1350/1328_BreakAPalindromeAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1301_1350
+{
+    public class _1328_BreakAPalindromeAlg
+    {
+        public string BreakPalindrome(string palindrome)
+        {
+            int n = palindrome.Length;
+            if (n == 1)
+                return "";
+            int len = n / 2;
+            char[] s=palindrome.ToCharArray();
+            for (int i = 0; i < len; i++)
+            {
+                if (palindrome[i] != 'a')
+                {
+                    s[i] = 'a';
+                    return new string(s);
+                }
+            }
+            s[n - 1] = 'b';
+            return new string(s);
+        }
+    }
+}
