b33c409c4b0fdd397a4c030c1522cc1599940231
hongtao_luo <luohongtao621@hotmail.com>
2025-03-05 16:33:29 +0800

    814

---
+++ TestLeetCode/0801_0850/814_BinaryTreePruningTest.cs
@@ -0,0 +1,52 @@
+﻿using leetCode._0801_0850;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _814_BinaryTreePruningTest
+    {
+        _814_BinaryTreePruningAlg alg = new _814_BinaryTreePruningAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, null, 0, 0, 1];
+            int?[] exp = [1, null, 0, null, 1];
+            TreeNode node = TreeNode.BuildTree(root);
+            var resNode = alg.PruneTree(node);
+            int?[] res = TreeNode.GetTreeLevelList(resNode);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1, 0, 1, 0, 0, 0, 1];
+            int?[] exp = [1, null, 1, null, 1];
+            TreeNode node = TreeNode.BuildTree(root);
+            var resNode = alg.PruneTree(node);
+            int?[] res = TreeNode.GetTreeLevelList(resNode);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [1, 1, 0, 1, 1, 0, 1, 0];
+            int?[] exp = [1, 1, 0, 1, 1, null, 1];
+            TreeNode node = TreeNode.BuildTree(root);
+            var resNode = alg.PruneTree(node);
+            int?[] res = TreeNode.GetTreeLevelList(resNode);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+    }
+}
---
+++ leetCode/0801_0850/814_BinaryTreePruningAlg.cs
@@ -0,0 +1,46 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _814_BinaryTreePruningAlg
+    {
+        public TreeNode PruneTree(TreeNode root)
+        {
+            if (Dfs(root)) 
+            {
+                return null;
+            }
+            return root;
+        }
+
+
+        private bool Dfs(TreeNode node) 
+        {
+            if (node == null)
+                return true;
+            bool left = Dfs(node.left);
+            bool right = Dfs(node.right);
+            if (left)
+            {
+                node.left = null;
+            }
+            if (right)
+            {
+                node.right = null;
+            }
+            if (left && right)
+            {
+                if (node.val == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
