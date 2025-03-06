216143c911a4972d8a7f77109223f11232461df4
hongtao_luo <luohongtao621@hotmail.com>
2025-03-06 14:30:27 +0800

    2588

---
+++ TestLeetCode/2551_2600/2588_CountTheNumberOfBeautifulSubarraysTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2551_2600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2551_2600
+{
+    [TestClass]
+    public class _2588_CountTheNumberOfBeautifulSubarraysTest
+    {
+        _2588_CountTheNumberOfBeautifulSubarraysAlg alg = new _2588_CountTheNumberOfBeautifulSubarraysAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 3, 1, 2, 4];
+            long exp = 2;
+            long res = alg.BeautifulSubarrays(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 10, 4];
+            long exp = 0;
+            long res = alg.BeautifulSubarrays(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2551_2600/2588_CountTheNumberOfBeautifulSubarraysAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2551_2600
+{
+    public class _2588_CountTheNumberOfBeautifulSubarraysAlg
+    {
+        public long BeautifulSubarrays(int[] nums)
+        {
+            long ans = 0;
+            int s = 0;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            dict.Add(0, 1);
+            foreach (var x in nums)
+            {
+                s ^= x;
+                int c = dict.GetValueOrDefault(s, 0);
+                ans += c;
+                if (dict.ContainsKey(s))
+                {
+                    dict[s] = c + 1;
+                }
+                else
+                {
+                    dict.Add(s, c + 1);
+                }
+
+            }
+            return ans;
+        }
+    }
+}
