e6cebbf60a9445ed4a96da305e93f331b19a5cdb
hongtao_luo <luohongtao621@hotmail.com>
2025-03-07 11:17:41 +0800

    2597

---
+++ TestLeetCode/2551_2600/2597_TheNumberOfBeautifulSubsetsTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._2551_2600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2551_2600
+{
+    [TestClass]
+    public class _2597_TheNumberOfBeautifulSubsetsTest
+    {
+        _2597_TheNumberOfBeautifulSubsetsAlg alg = new _2597_TheNumberOfBeautifulSubsetsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 4, 6];
+            int k = 2;
+            int exp = 4;
+            int res = alg.BeautifulSubsets(nums, k);
+            Assert.AreEqual(exp, res);
+
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1];
+            int k = 1;
+            int exp = 1;
+            int res = alg.BeautifulSubsets(nums, k);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/2551_2600/2597_TheNumberOfBeautifulSubsetsAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2551_2600
+{
+    public class _2597_TheNumberOfBeautifulSubsetsAlg
+    {
+        private int ans = -1;
+        public int BeautifulSubsets(int[] nums, int k)
+        {
+            Dictionary<int, int> cnt = new Dictionary<int, int>();
+            Dfs(0, nums, k, cnt);
+            return ans;
+        }
+
+        private void Dfs(int i, int[] nums, int k, Dictionary<int, int> cnt)
+        {
+            if (nums.Length == i)
+            {
+                ans++;
+                return;
+            }
+            Dfs(i + 1, nums, k, cnt);
+            int x = nums[i];
+            if (cnt.GetValueOrDefault(x - k, 0) == 0 && cnt.GetValueOrDefault(x + k, 0) == 0)
+            {
+                if (cnt.ContainsKey(x))
+                {
+                    cnt[x]++;
+                }
+                else
+                {
+                    cnt.Add(x, 1);
+                }
+               
+                Dfs(i + 1, nums, k, cnt);
+                cnt[x]--;
+            }
+        }
+    }
+}
