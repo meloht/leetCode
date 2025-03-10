15ae520beedf2d7bda7190947b9bfe2ae2b8f800
hongtao_luo <luohongtao621@hotmail.com>
2025-03-10 13:42:16 +0800

    2269

---
+++ TestLeetCode/2251_2300/2269_FindTheKBeautyOfANumberTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2251_2300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2251_2300
+{
+    [TestClass]
+    public class _2269_FindTheKBeautyOfANumberTest
+    {
+        _2269_FindTheKBeautyOfANumberAlg alg = new _2269_FindTheKBeautyOfANumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int num = 240, k = 2;
+            int exp = 2;
+            int res = alg.DivisorSubstrings(num, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int num = 430043, k = 2;
+            int exp = 2;
+            int res = alg.DivisorSubstrings(num, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2251_2300/2269_FindTheKBeautyOfANumberAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2269_FindTheKBeautyOfANumberAlg
+    {
+        public int DivisorSubstrings(int num, int k)
+        {
+            int ans = 0;
+            string s = num.ToString();
+            StringBuilder sb = new StringBuilder();
+            sb.Append(s.Substring(0, k));
+            int val = int.Parse(sb.ToString());
+            if (num % val == 0)
+            {
+                ans++;
+            }
+            for (int i = k; i < s.Length; i++)
+            {
+                sb.Remove(0, 1);
+
+                sb.Append(s[i]);
+                val = int.Parse(sb.ToString());
+                if (val > 0 && num % val == 0)
+                {
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
