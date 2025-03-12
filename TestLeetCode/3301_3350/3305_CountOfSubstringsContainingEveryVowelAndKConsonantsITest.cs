eb8cb4924964da41203e5f047f7464bd11ad5871
hongtao_luo <luohongtao621@hotmail.com>
2025-03-12 14:33:12 +0800

    3305

---
+++ TestLeetCode/3301_3350/3305_CountOfSubstringsContainingEveryVowelAndKConsonantsITest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._3301_3350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3301_3350
+{
+    [TestClass]
+    public class _3305_CountOfSubstringsContainingEveryVowelAndKConsonantsITest
+    {
+        _3305_CountOfSubstringsContainingEveryVowelAndKConsonantsIAlg alg = new _3305_CountOfSubstringsContainingEveryVowelAndKConsonantsIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string word = "aeioqq";
+            int k = 1;
+            int exp = 0;
+            int res = alg.CountOfSubstrings(word, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string word = "aeiou";
+            int k = 0;
+            int exp = 1;
+            int res = alg.CountOfSubstrings(word, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string word = "ieaouqqieaouqq";
+            int k = 1;
+            int exp = 3;
+            int res = alg.CountOfSubstrings(word, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3301_3350/3305_CountOfSubstringsContainingEveryVowelAndKConsonantsIAlg.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3301_3350
+{
+    public class _3305_CountOfSubstringsContainingEveryVowelAndKConsonantsIAlg
+    {
+
+        public long CountOfSubstrings1(string word, int k)
+        {
+            HashSet<char> vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+            long Count(int m)
+            {
+                int n = word.Length, consonants = 0;
+                long res = 0;
+                Dictionary<char, int> occur = new Dictionary<char, int>();
+                for (int i = 0, j = 0; i < n; i++)
+                {
+                    while (j < n && (consonants < m || occur.Count < 5))
+                    {
+                        char ch = word[j];
+                        if (vowels.Contains(ch))
+                        {
+                            if (!occur.ContainsKey(ch)) occur[ch] = 0;
+                            occur[ch]++;
+                        }
+                        else
+                        {
+                            consonants++;
+                        }
+                        j++;
+                    }
+                    if (consonants >= m && occur.Count == 5)
+                    {
+                        res += n - j + 1;
+                    }
+                    char left = word[i];
+                    if (vowels.Contains(left))
+                    {
+                        occur[left]--;
+                        if (occur[left] == 0)
+                        {
+                            occur.Remove(left);
+                        }
+                    }
+                    else
+                    {
+                        consonants--;
+                    }
+                }
+                return res;
+            }
+            return Count(k) - Count(k + 1);
+        }
+
+
+        public int CountOfSubstrings(string word, int k)
+        {
+            var n1 = GetCount(word, k);
+            var n2 = GetCount(word, k + 1);
+            return (int)(n1 - n2);
+        }
+        private long GetCount(string word, int k)
+        {
+            long ans = 0;
+            int[] map = new int[26];
+
+
+            for (int i = 0, j = 0;  i < word.Length; i++)
+            {
+                if (i > 0)
+                {
+                    map[word[i - 1] - 'a']--;
+                }
+
+                if (j < word.Length)
+                {
+                    for (; j < word.Length; j++)
+                    {
+                        map[word[j] - 'a']++;
+
+                        if (GetVowelCount(map, k))
+                        {
+                            ans += word.Length - j;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (GetVowelCount(map, k))
+                    {
+                        ans++;
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+              
+            }
+            return ans;
+        }
+
+
+        private bool GetVowelCount(int[] map, int k)
+        {
+            int cnt = 0;
+            int num = 0;
+            for (int i = 0; i < map.Length; i++)
+            {
+                if (i == 0 || i == 'e' - 'a' || i == 'i' - 'a' || i == 'o' - 'a' || i == 'u' - 'a')
+                {
+                    if (map[i] > 0)
+                    {
+                        num++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    cnt += map[i];
+                }
+            }
+            if (num == 5 && cnt >= k)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
