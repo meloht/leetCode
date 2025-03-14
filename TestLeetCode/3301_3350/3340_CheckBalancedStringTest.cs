580824e2cfcff035d180d5fde9af4eb3a28f85a0
hongtao_luo <luohongtao621@hotmail.com>
2025-03-14 11:32:17 +0800

    3340

---
+++ TestLeetCode/3301_3350/3340_CheckBalancedStringTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3301_3350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3301_3350
+{
+    [TestClass]
+    public class _3340_CheckBalancedStringTest
+    {
+        _3340_CheckBalancedStringAlg alg = new _3340_CheckBalancedStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string num = "1234";
+            bool exp = false;
+            bool res = alg.IsBalanced(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string num = "24123";
+            bool exp = true;
+            bool res = alg.IsBalanced(num);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3301_3350/3340_CheckBalancedStringAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3301_3350
+{
+    public class _3340_CheckBalancedStringAlg
+    {
+        public bool IsBalanced(string num)
+        {
+            int sum = 0;
+            int sum2 = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += num[i] - '0';
+                }
+                else
+                {
+                    sum2 += num[i] - '0';
+                }
+
+            }
+            return sum == sum2;
+        }
+    }
+}
