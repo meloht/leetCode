e8c353dbc2948b20bfd245e70f30de0b25293b6f
luohongtao <luohongtao621@hotmail.com>
2025-03-15 12:41:16 +0800

    3110

---
+++ TestLeetCode/3101_3150/3110_ScoreOfAStringTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3110_ScoreOfAStringTest
+    {
+        _3110_ScoreOfAStringAlg alg = new _3110_ScoreOfAStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "hello";
+            int exp = 13;
+            int res = alg.ScoreOfString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "zaz";
+            int exp = 50;
+            int res = alg.ScoreOfString(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3110_ScoreOfAStringAlg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3110_ScoreOfAStringAlg
+    {
+        public int ScoreOfString(string s)
+        {
+            int ans = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                ans += Math.Abs(s[i - 1] - s[i]);
+            }
+            return ans;
+        }
+    }
+}
