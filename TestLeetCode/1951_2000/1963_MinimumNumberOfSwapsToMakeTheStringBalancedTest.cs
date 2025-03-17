cb9c54eadc1a76841e710c77460cbfab4face356
hongtao_luo <luohongtao621@hotmail.com>
2025-03-17 14:36:05 +0800

    1963

---
+++ TestLeetCode/1951_2000/1963_MinimumNumberOfSwapsToMakeTheStringBalancedTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._1951_2000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1951_2000
+{
+    [TestClass]
+    public class _1963_MinimumNumberOfSwapsToMakeTheStringBalancedTest
+    {
+        _1963_MinimumNumberOfSwapsToMakeTheStringBalancedAlg alg = new _1963_MinimumNumberOfSwapsToMakeTheStringBalancedAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "][][";
+            int exp = 1;
+            int res = alg.MinSwaps(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "]]][[[";
+            int exp = 2;
+            int res = alg.MinSwaps(s);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "[]";
+            int exp = 0;
+            int res = alg.MinSwaps(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1951_2000/1963_MinimumNumberOfSwapsToMakeTheStringBalancedAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1951_2000
+{
+    public class _1963_MinimumNumberOfSwapsToMakeTheStringBalancedAlg
+    {
+        public int MinSwaps(string s)
+        {
+            int c = 0;
+            foreach (var item in s)
+            {
+                if (item == '[' || c == 0)
+                {
+                    c++;
+                }
+                else
+                {
+                    c--;
+                }
+            }
+
+            return c/2;
+        }
+    }
+}
