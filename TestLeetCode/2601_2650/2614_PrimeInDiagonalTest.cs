0e24354e97b500eda3fa8b49d537dd9895037c31
hongtao_luo <luohongtao621@hotmail.com>
2025-03-18 14:30:06 +0800

    2614

---
+++ TestLeetCode/2601_2650/2614_PrimeInDiagonalTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2601_2650;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2601_2650
+{
+
+    [TestClass]
+    public class _2614_PrimeInDiagonalTest
+    {
+        _2614_PrimeInDiagonalAlg alg = new _2614_PrimeInDiagonalAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] nums = [[1, 2, 3], [5, 6, 7], [9, 10, 11]];
+            int exp = 11;
+            int res = alg.DiagonalPrime(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] nums = [[1, 2, 3], [5, 17, 7], [9, 11, 10]];
+            int exp = 17;
+            int res = alg.DiagonalPrime(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/2601_2650/2614_PrimeInDiagonalAlg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2601_2650
+{
+    public class _2614_PrimeInDiagonalAlg
+    {
+        public int DiagonalPrime(int[][] nums)
+        {
+            int ans = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (IsPrime(nums[i][i]))
+                {
+                    ans=Math.Max(ans, nums[i][i]);
+                }
+                if (IsPrime(nums[i][nums.Length - i - 1]))
+                {
+                    ans = Math.Max(ans, nums[i][nums.Length - i - 1]);
+                }
+            }
+            return ans;
+        }
+
+        public bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            if (n <= 3)
+                return true;
+
+            if (n % 2 == 0 || n % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
