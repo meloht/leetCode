4fe6f72c7ae4457aa7caa2162276420d95ca2fe7
hongtao_luo <luohongtao621@hotmail.com>
2025-03-18 17:05:36 +0800

    820

---
+++ TestLeetCode/0801_0850/820_ShortEncodingOfWordsTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _820_ShortEncodingOfWordsTest
+    {
+        _820_ShortEncodingOfWordsAlg alg = new _820_ShortEncodingOfWordsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["time", "me", "bell"];
+            int exp = 10;
+            int res = alg.MinimumLengthEncoding(words);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["t"];
+            int exp = 2;
+            int res = alg.MinimumLengthEncoding(words);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] words = ["me", "time"];
+            int exp = 5;
+            int res = alg.MinimumLengthEncoding(words);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/820_ShortEncodingOfWordsAlg.cs
@@ -0,0 +1,60 @@
+﻿using leetCode.Model.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _820_ShortEncodingOfWordsAlg
+    {
+        public int MinimumLengthEncoding(string[] words)
+        {
+            Dictionary<TrieNode, int> dict = new Dictionary<TrieNode, int>();
+            TrieNode root = new TrieNode();
+            for (int i = 0; i < words.Length; i++)
+            {
+                TrieNode cur = root;
+                for (int j = words[i].Length - 1; j >= 0; j--)
+                {
+                    cur = cur.GetNode(words[i][j]);
+                }
+                if (!dict.ContainsKey(cur))
+                {
+                    dict.Add(cur, i);
+                }
+               
+            }
+
+
+            int ans = 0;
+            foreach (var item in dict)
+            {
+                if (item.Key.Count == 0)
+                {
+                    ans += words[item.Value].Length + 1;
+                }
+            }
+           
+            return ans;
+        }
+        public class TrieNode
+        {
+
+            public int Count;
+            public TrieNode[] NextNode = new TrieNode[26];
+
+            public TrieNode GetNode(char c)
+            {
+                if (NextNode[c - 'a'] == null)
+                {
+                    NextNode[c - 'a'] = new TrieNode();
+                    Count++;
+                }
+                return NextNode[c - 'a'];
+            }
+
+        }
+    }
+}
