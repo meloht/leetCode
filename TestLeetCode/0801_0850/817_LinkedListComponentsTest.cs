31be44ad8c7ee6d30d29c21ef89bb3cfb3f8e99d
hongtao_luo <luohongtao621@hotmail.com>
2025-03-18 14:53:44 +0800

    817

---
+++ TestLeetCode/0801_0850/817_LinkedListComponentsTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode;
+using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _817_LinkedListComponentsTest
+    {
+        _817_LinkedListComponentsAlg alg = new _817_LinkedListComponentsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [0, 1, 2, 3], nums = [0, 1, 3];
+            int exp = 2;
+            ListNode node = ListNode.BuildListNode(head);
+            int res = alg.NumComponents(node, nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [0, 1, 2, 3, 4], nums = [0, 3, 1, 4];
+            int exp = 2;
+            ListNode node = ListNode.BuildListNode(head);
+            int res = alg.NumComponents(node, nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/817_LinkedListComponentsAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _817_LinkedListComponentsAlg
+    {
+        public int NumComponents(ListNode head, int[] nums)
+        {
+            int ans = 0;
+            HashSet<int> set = new HashSet<int>(nums);
+            ListNode node = head;
+            bool bl = false;
+            while (node != null)
+            {
+                if (set.Contains(node.val))
+                {
+                    bl = true;
+                }
+                else
+                {
+                    if (bl)
+                    {
+                        ans++;
+                    }
+                    bl = false;
+                }
+                node = node.next;
+            }
+            if (bl)
+            {
+                ans++;
+            }
+
+            return ans;
+        }
+    }
+}
