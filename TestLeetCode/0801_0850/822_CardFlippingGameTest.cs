a7b6f758b6f64c75f17b39e68887f97d58bea85a
hongtao_luo <luohongtao621@hotmail.com>
2025-03-19 16:12:21 +0800

    822

---
+++ TestLeetCode/0801_0850/822_CardFlippingGameTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _822_CardFlippingGameTest
+    {
+        _822_CardFlippingGameAlg alg = new _822_CardFlippingGameAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] fronts = [1, 2, 4, 4, 7], backs = [1, 3, 4, 1, 3];
+            int exp = 2;
+            int res = alg.Flipgame(fronts, backs);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] fronts = [1], backs = [1];
+            int exp = 0;
+            int res = alg.Flipgame(fronts, backs);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/822_CardFlippingGameAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _822_CardFlippingGameAlg
+    {
+        public int Flipgame(int[] fronts, int[] backs)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            for (int i = 0; i < fronts.Length; i++)
+            {
+                if (fronts[i] == backs[i])
+                {
+                    set.Add(fronts[i]);
+                }
+            }
+            int ans = int.MaxValue;
+
+            foreach (var item in fronts)
+            {
+                if (!set.Contains(item))
+                {
+                    ans = Math.Min(ans, item);
+                }
+            }
+            foreach (var item in backs)
+            {
+                if (!set.Contains(item))
+                {
+                    ans = Math.Min(ans, item);
+                }
+            }
+
+            return ans == int.MaxValue ? 0 : ans;
+        }
+    }
+}
