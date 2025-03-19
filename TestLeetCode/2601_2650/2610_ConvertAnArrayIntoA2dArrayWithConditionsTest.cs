52acb724b1775dc85426f699c4dff91df10ef3dd
hongtao_luo <luohongtao621@hotmail.com>
2025-03-19 11:23:24 +0800

    2610

---
+++ TestLeetCode/2601_2650/2610_ConvertAnArrayIntoA2dArrayWithConditionsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2601_2650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2601_2650
+{
+    [TestClass]
+    public class _2610_ConvertAnArrayIntoA2dArrayWithConditionsTest
+    {
+        _2610_ConvertAnArrayIntoA2dArrayWithConditionsAlg alg = new _2610_ConvertAnArrayIntoA2dArrayWithConditionsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 4, 1, 2, 3, 1];
+            IList<IList<int>> exp = [[1, 3, 4, 2], [1, 3], [1]];
+            var res = alg.FindMatrix(nums);
+            Assert.IsTrue(Utils.IsSame(exp,res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4];
+            IList<IList<int>> exp = [[4, 3, 2, 1]];
+            var res = alg.FindMatrix(nums);
+            Assert.IsTrue(Utils.IsSame(exp, res));
+        }
+    }
+}
---
+++ leetCode/2601_2650/2610_ConvertAnArrayIntoA2dArrayWithConditionsAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2601_2650
+{
+    public class _2610_ConvertAnArrayIntoA2dArrayWithConditionsAlg
+    {
+        public IList<IList<int>> FindMatrix(int[] nums)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+
+            HashSet<int> set = new HashSet<int>();
+            List<int> list = new List<int>(nums);
+            while (list.Count > 0)
+            {
+                set.Clear();
+                List<int> rows = new List<int>();
+                List<int> list2 = new List<int>();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (set.Add(list[i]))
+                    {
+                        rows.Add(list[i]);
+                    }
+                    else
+                    {
+                        list2.Add(list[i]);
+                    }
+                }
+                res.Add(rows);
+                list = list2;
+            }
+            return res;
+        }
+    }
+}
