58ba431264dde804fa212ebeaa08f5dc6bb03cb2
hongtao_luo <luohongtao621@hotmail.com>
2025-03-20 12:24:46 +0800

    2612

---
+++ TestLeetCode/2601_2650/2612_MinimumReverseOperationsTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._2601_2650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2601_2650
+{
+    [TestClass]
+    public class _2612_MinimumReverseOperationsTest
+    {
+        _2612_MinimumReverseOperationsAlg alg = new _2612_MinimumReverseOperationsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 4, p = 0;
+            int[] banned = [1, 2];
+            int k = 4;
+            int[] exp = [0, -1, -1, 1];
+            int[] res = alg.MinReverseOperations(n, p, banned, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 5, p = 0;
+            int[] banned = [2, 4];
+            int k = 3;
+            int[] exp = [0, -1, -1, -1, -1];
+            int[] res = alg.MinReverseOperations(n, p, banned, k);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2601_2650/2612_MinimumReverseOperationsAlg.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2601_2650
+{
+    public class _2612_MinimumReverseOperationsAlg
+    {
+        public int[] MinReverseOperations(int n, int p, int[] banned, int k)
+        {
+            HashSet<int> ban = new HashSet<int>(banned);
+            SortedSet<int>[] sets = new SortedSet<int>[2];
+            sets[0] = new SortedSet<int>();
+            sets[1] = new SortedSet<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (i != p && !ban.Contains(i))
+                {
+                    sets[i % 2].Add(i);
+                }
+            }
+            int[] ans = new int[n];
+            Array.Fill(ans, -1);
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(p);
+            ans[p] = 0;
+            while (queue.Count > 0)
+            {
+                int i = queue.Dequeue();
+                int mn = Math.Max(i - k + 1, k - i - 1);
+                int mx = Math.Min(i + k - 1, n * 2 - k - i - 1);
+                SortedSet<int> set = sets[mx % 2];
+
+                var toRemove=new List<int>();
+                var it = set.GetViewBetween(mn, mx).GetEnumerator();
+                while (it.MoveNext())
+                {
+                    int current = it.Current;
+                    ans[current] = ans[i] + 1;
+                    queue.Enqueue(current);
+                    toRemove.Add(current);
+                }
+                foreach (var item in toRemove)
+                {
+                    set.Remove(item);
+                }
+            }
+            return ans;
+        }
+    }
+}
