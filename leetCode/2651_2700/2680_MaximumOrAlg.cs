6a271e1f6cfee37f99788699d8f9a6d379c74ecf
hongtao_luo <luohongtao621@hotmail.com>
2025-03-21 11:35:45 +0800

    2680

--- leetCode/2651_2700/2680_MaximumOrAlg.cs
+++ leetCode/2651_2700/2680_MaximumOrAlg.cs
@@ -8,7 +8,7 @@ namespace leetCode._2651_2700
 {
     public class _2680_MaximumOrAlg
     {
-        public long MaximumOr(int[] nums, int k)
+        public long MaximumOr1(int[] nums, int k)
         {
             int n = nums.Length;
             int[] suf = new int[n];
@@ -28,5 +28,26 @@ namespace leetCode._2651_2700
 
             return ans;
         }
+
+        public long MaximumOr(int[] nums, int k)
+        {
+            int allOr = 0;
+            int fixedVal = 0;
+            foreach (int x in nums)
+            {
+                // 如果在计算 allOr |= x 之前，allOr 和 x 有公共的 1
+                // 那就意味着有多个 nums[i] 在这些比特位上都是 1
+                fixedVal |= allOr & x; // 把公共的 1 记录到 fixed 中
+                allOr |= x; // 所有数的 OR
+            }
+
+            long ans = 0;
+            foreach (int x in nums)
+            {
+                ans = Math.Max(ans, (allOr ^ x) | fixedVal | ((long)x << k));
+            }
+            return ans;
+        }
+
     }
 }
