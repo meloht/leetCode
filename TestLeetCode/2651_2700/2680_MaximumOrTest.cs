1bdb0d80941f4cc00a6d2f1fd380d83e519bb9f0
hongtao_luo <luohongtao621@hotmail.com>
2025-03-21 11:24:40 +0800

    2680

---
+++ TestLeetCode/2651_2700/2680_MaximumOrTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._2651_2700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2651_2700
+{
+    [TestClass]
+    public class _2680_MaximumOrTest
+    {
+        _2680_MaximumOrAlg alg = new _2680_MaximumOrAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [12, 9];
+            int k = 1;
+            long exp = 30;
+            long res = alg.MaximumOr(nums, k);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [8, 1, 2];
+            int k = 2;
+            long exp = 35;
+            long res = alg.MaximumOr(nums, k);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/2651_2700/2680_MaximumOrAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2651_2700
+{
+    public class _2680_MaximumOrAlg
+    {
+        public long MaximumOr(int[] nums, int k)
+        {
+            int n = nums.Length;
+            int[] suf = new int[n];
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                suf[i] = suf[i + 1] | nums[i + 1];
+            }
+
+            long ans = 0;
+            int pre = 0;
+            for (int i = 0; i < n; i++)
+            {
+                ans = Math.Max(ans, pre | ((long)nums[i] << k) | suf[i]);
+                pre |= nums[i];
+            }
+
+            return ans;
+        }
+    }
+}
