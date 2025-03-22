836ffb80b93bf9c64a896b5824574037cda72e58
luohongtao <luohongtao621@hotmail.com>
2025-03-22 11:11:55 +0800

    2643

---
+++ TestLeetCode/2601_2650/2643_RowWithMaximumOnesTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2601_2650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2601_2650
+{
+    [TestClass]
+    public class _2643_RowWithMaximumOnesTest
+    {
+        _2643_RowWithMaximumOnesAlg alg = new _2643_RowWithMaximumOnesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] mat = [[0, 1], [1, 0]];
+            int[] exp = [0, 1];
+            int[] res = alg.RowAndMaximumOnes(mat);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] mat = [[0, 0, 0], [0, 1, 1]];
+            int[] exp = [1, 2];
+            int[] res = alg.RowAndMaximumOnes(mat);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] mat = [[0, 0], [1, 1], [0, 0]];
+            int[] exp = [1, 2];
+            int[] res = alg.RowAndMaximumOnes(mat);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/2601_2650/2643_RowWithMaximumOnesAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2601_2650
+{
+    public class _2643_RowWithMaximumOnesAlg
+    {
+        public int[] RowAndMaximumOnes(int[][] mat)
+        {
+            int m = mat.Length;
+            int n = mat[0].Length;
+            int ans = 0;
+            int max = 0;
+            for (int i = 0; i < m; i++)
+            {
+                int cnt = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i][j] == 1) cnt++;
+                }
+                if (cnt > max)
+                {
+                    max = cnt;
+                    ans = i;
+                }
+            }
+            return [ans, max];
+        }
+    }
+}
