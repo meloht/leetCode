c28bb55596f08c1ac05aa83e7b6c2fb54d3ae3c7
luohongtao <luohongtao621@hotmail.com>
2025-03-23 14:15:52 +0800

    2116

---
+++ TestLeetCode/2101_2150/2116_CheckIfAParenthesesStringCanBeValidTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._2101_2150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2101_2150
+{
+    [TestClass]
+    public class _2116_CheckIfAParenthesesStringCanBeValidTest
+    {
+        _2116_CheckIfAParenthesesStringCanBeValidAlg alg = new _2116_CheckIfAParenthesesStringCanBeValidAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "))()))", locked = "010100";
+            bool exp = true;
+            bool res = alg.CanBeValid(s, locked);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "()()", locked = "0000";
+            bool exp = true;
+            bool res = alg.CanBeValid(s, locked);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = ")", locked = "0";
+            bool exp = false;
+            bool res = alg.CanBeValid(s, locked);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string s = "(((())(((())", locked = "111111010111";
+            bool exp = true;
+            bool res = alg.CanBeValid(s, locked);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2101_2150/2116_CheckIfAParenthesesStringCanBeValidAlg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2101_2150
+{
+    public class _2116_CheckIfAParenthesesStringCanBeValidAlg
+    {
+        public bool CanBeValid(string s, string locked)
+        {
+            if (s.Length % 2 > 0)
+            {
+                return false;
+            }
+            int mn = 0;
+            int mx = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (locked[i] == '1')
+                {
+                    int d = s[i] == '(' ? 1 : -1;
+                    mx += d;
+                    if (mx < 0)
+                    {
+                        return false;
+                    }
+                    mn += d;
+                }
+                else
+                {
+                    mx++;
+                    mn--;
+                }
+                if (mn < 0)
+                {
+                    mn = 1;
+                }
+            }
+
+            return mn == 0; 
+        }
+    }
+}
