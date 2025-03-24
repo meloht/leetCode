49127a62f7c0be20535ca93151d8934713a5b950
hongtao_luo <luohongtao621@hotmail.com>
2025-03-24 16:01:35 +0800

    823

--- leetCode/0801_0850/823_BinaryTreesWithFactorsAlg.cs
+++ leetCode/0801_0850/823_BinaryTreesWithFactorsAlg.cs
@@ -10,9 +10,46 @@ namespace leetCode._0801_0850
     {
         public int NumFactoredBinaryTrees(int[] arr)
         {
+            long Mod = 1_000_000_007;
+            Array.Sort(arr);
+            int n = arr.Length;
 
+            Dictionary<int, int> dict = new Dictionary<int, int>();
 
-            return 0;
+            for (int i = 0; i < n; i++)
+            {
+                dict.Add(arr[i], i);
+            }
+
+            long ans = 0;
+            long[] f = new long[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int val = arr[i];
+                f[i] = 1;
+
+                for (int j = 0; j < i; j++)
+                {
+                    int x = arr[j];
+                    if ((long)x * x > val)
+                    { // 防止乘法溢出
+                        break;
+                    }
+                    if (x * x == val)
+                    {
+                        f[i] += f[j] * f[j];
+                        break;
+                    }
+                    if (val % x == 0 && dict.ContainsKey(val / x))
+                    {
+                        f[i] += f[j] * f[dict[val / x]] * 2;
+                    }
+                }
+                ans += f[i];
+            }
+
+            return (int)(ans % Mod);
         }
     }
 }
