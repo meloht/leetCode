0406744887af5c76b14323073a2c7759873ec205
hongtao_luo <luohongtao621@hotmail.com>
2025-03-24 14:29:13 +0800

    2255

---
+++ TestLeetCode/2251_2300/2255_CountPrefixesOfAGivenStringTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._2251_2300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2251_2300
+{
+    [TestClass]
+    public class _2255_CountPrefixesOfAGivenStringTest
+    {
+        _2255_CountPrefixesOfAGivenStringAlg alg = new _2255_CountPrefixesOfAGivenStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["a", "b", "c", "ab", "bc", "abc"];
+            string s = "abc";
+            int exp = 3;
+            int res = alg.CountPrefixes(words,s);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["a", "a"];
+            string s = "aa";
+            int exp = 2;
+            int res = alg.CountPrefixes(words, s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2251_2300/2255_CountPrefixesOfAGivenStringAlg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2255_CountPrefixesOfAGivenStringAlg
+    {
+        public int CountPrefixes(string[] words, string s)
+        {
+            int ans = 0;
+            foreach (var item in words)
+            {
+                if (s.StartsWith(item))
+                {
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
