4645a62a600aa73c886d974cf889ade49b6d4373
hongtao_luo <luohongtao621@hotmail.com>
2025-03-24 16:57:16 +0800

    831

---
+++ TestLeetCode/0801_0850/831_MaskingPersonalInformationTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _831_MaskingPersonalInformationTest
+    {
+        _831_MaskingPersonalInformationAlg alg = new _831_MaskingPersonalInformationAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "LeetCode@LeetCode.com";
+            string exp = "l*****e@leetcode.com";
+            string res = alg.MaskPII(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "AB@qq.com";
+            string exp = "a*****b@qq.com";
+            string res = alg.MaskPII(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "1(234)567-890";
+            string exp = "***-***-7890";
+            string res = alg.MaskPII(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/831_MaskingPersonalInformationAlg.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _831_MaskingPersonalInformationAlg
+    {
+        public string MaskPII(string s)
+        {
+            if (s.Contains("@"))
+            {
+                return GetMail(s);
+            }
+            return GetTel(s);
+        }
+
+        private string GetMail(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] ss = s.ToLower().Split('@', StringSplitOptions.RemoveEmptyEntries);
+
+            sb.Append(ss[0][0]);
+            sb.Append("*****");
+            sb.Append(ss[0][ss[0].Length - 1]);
+            sb.Append('@');
+            sb.Append(ss[1]);
+
+
+            return sb.ToString();
+        }
+
+        private string GetTel(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            List<char> ls = new List<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsNumber(s[i]))
+                {
+                    ls.Add(s[i]);
+                }
+            }
+            int n = ls.Count - 10;
+            if (n > 0)
+            {
+                sb.Append('+');
+                for (int i = 0; i < n; i++)
+                {
+                    sb.Append('*');
+                }
+                sb.Append('-');
+               
+            }
+            sb.Append("***-***-");
+
+
+            for (int i = ls.Count - 4; i < ls.Count; i++)
+            {
+                sb.Append(ls[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
