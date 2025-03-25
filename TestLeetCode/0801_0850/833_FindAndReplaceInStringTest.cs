5c2c64ff199a83c9ab17007db43ec189a82a5b65
hongtao_luo <luohongtao621@hotmail.com>
2025-03-25 17:40:05 +0800

    833

---
+++ TestLeetCode/0801_0850/833_FindAndReplaceInStringTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _833_FindAndReplaceInStringTest
+    {
+        _833_FindAndReplaceInStringAlg alg = new _833_FindAndReplaceInStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abcd";
+            int[] indices = [0, 2];
+            string[] sources = ["a", "cd"], targets = ["eee", "ffff"];
+
+            string exp = "eeebffff";
+            string res = alg.FindReplaceString(s, indices, sources, targets);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "abcd";
+            int[] indices = [0, 2];
+            string[] sources = ["ab", "ec"], targets = ["eee", "ffff"];
+
+            string exp = "eeecd";
+            string res = alg.FindReplaceString(s, indices, sources, targets);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/833_FindAndReplaceInStringAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _833_FindAndReplaceInStringAlg
+    {
+        public string FindReplaceString(string s, int[] indices, string[] sources, string[] targets)
+        {
+            return null;
+        }
+    }
+}
