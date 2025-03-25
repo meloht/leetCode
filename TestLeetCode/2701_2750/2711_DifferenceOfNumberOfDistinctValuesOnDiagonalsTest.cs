d41e63b85764e457a61a5fe147a2e006f3196902
hongtao_luo <luohongtao621@hotmail.com>
2025-03-25 12:23:08 +0800

    2711

---
+++ TestLeetCode/2701_2750/2711_DifferenceOfNumberOfDistinctValuesOnDiagonalsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2711_DifferenceOfNumberOfDistinctValuesOnDiagonalsTest
+    {
+        _2711_DifferenceOfNumberOfDistinctValuesOnDiagonalsAlg alg = new _2711_DifferenceOfNumberOfDistinctValuesOnDiagonalsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[1, 2, 3], [3, 1, 5], [3, 2, 1]];
+            int[][] exp = [[1, 1, 0], [1, 0, 1], [0, 1, 1]];
+            int[][] res = alg.DifferenceOfDistinctValues(grid);
+            Assert.IsTrue(Utils.IsSameArr(exp, res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[1]];
+            int[][] exp = [[0]];
+            int[][] res = alg.DifferenceOfDistinctValues(grid);
+            Assert.IsTrue(Utils.IsSameArr(exp, res));
+        }
+    }
+}
---
+++ leetCode/2701_2750/2711_DifferenceOfNumberOfDistinctValuesOnDiagonalsAlg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2711_DifferenceOfNumberOfDistinctValuesOnDiagonalsAlg
+    {
+        public int[][] DifferenceOfDistinctValues(int[][] grid)
+        {
+            int[][] ans = new int[grid.Length][];
+            for (int i = 0; i < grid.Length; i++)
+            {
+                ans[i] = new int[grid[i].Length];
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    ans[i][j] = Math.Abs(GetTop(grid, i, j) - GetBottom(grid, i, j));
+                }
+            }
+
+            return ans;
+        }
+
+        private int GetTop(int[][] grid, int n, int m)
+        {
+            HashSet<int> result = new HashSet<int>();
+            for (int i = n - 1, j = m - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                result.Add(grid[i][j]);
+            }
+
+            return result.Count;
+        }
+        private int GetBottom(int[][] grid, int n, int m)
+        {
+            HashSet<int> result = new HashSet<int>();
+            for (int i = n + 1, j = m + 1; i < grid.Length && j < grid[0].Length; i++, j++)
+            {
+                result.Add(grid[i][j]);
+            }
+
+            return result.Count;
+        }
+    }
+}
