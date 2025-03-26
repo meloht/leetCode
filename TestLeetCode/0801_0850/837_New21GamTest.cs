b80370bf8192939a6f4b43e86aef99f0c4175bdb
hongtao_luo <luohongtao621@hotmail.com>
2025-03-26 14:53:35 +0800

    837

---
+++ TestLeetCode/0801_0850/837_New21GamTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _837_New21GamTest
+    {
+        _837_New21GamAlg alg = new _837_New21GamAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 10, k = 1, maxPts = 10;
+            double exp = 1.00000;
+            double res = alg.New21Game(n, k, maxPts);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 6, k = 1, maxPts = 10;
+            double exp = 0.60000;
+            double res = alg.New21Game(n, k, maxPts);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 21, k = 17, maxPts = 10;
+            double exp = 0.73278;
+            double res = alg.New21Game(n, k, maxPts);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/837_New21GamAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _837_New21GamAlg
+    {
+        public double New21Game(int n, int k, int maxPts)
+        {
+            if (k == 0 || n >= k + maxPts - 1)
+            {
+                return 1.0;
+            }
+
+            double[] dp = new double[k + maxPts];
+            for (int i = k; i <= Math.Min(n, k + maxPts - 1); i++)
+            {
+                dp[i] = 1.0;
+            }
+
+            double sum = 0;
+            for (int i = k; i < k + maxPts; i++)
+            {
+                sum += dp[i];
+            }
+
+            for (int i = k - 1; i >= 0; i--)
+            {
+                dp[i] = sum / maxPts;
+                sum = sum - dp[i + maxPts] + dp[i];
+            }
+
+            return dp[0];
+        }
+    }
+}
