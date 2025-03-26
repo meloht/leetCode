94d8032f508435cf4ffbd8a5686142f458ee46af
hongtao_luo <luohongtao621@hotmail.com>
2025-03-26 13:25:51 +0800

    2829

---
+++ TestLeetCode/2801_2850/2829_DetermineTheMinimumSumOfAKAvoidingArrayTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2801_2850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2801_2850
+{
+    [TestClass]
+    public class _2829_DetermineTheMinimumSumOfAKAvoidingArrayTest
+    {
+        _2829_DetermineTheMinimumSumOfAKAvoidingArrayAlg alg = new _2829_DetermineTheMinimumSumOfAKAvoidingArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 5, k = 4;
+            int exp = 18;
+            int res = alg.MinimumSum(n, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 2, k = 6;
+            int exp = 3;
+            int res = alg.MinimumSum(n, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2801_2850/2829_DetermineTheMinimumSumOfAKAvoidingArrayAlg.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2801_2850
+{
+    public class _2829_DetermineTheMinimumSumOfAKAvoidingArrayAlg
+    {
+        public int MinimumSum1(int n, int k)
+        {
+            int sum = 0;
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0, j = 1; i < n; i++)
+            {
+                if (j < k)
+                {
+                    while (set.Contains(k - j))
+                    {
+                        j++;
+                    }
+
+                }
+
+                sum += j;
+                set.Add(j);
+                j++;
+            }
+
+            return sum;
+        }
+
+        public int MinimumSum(int n, int k)
+        {
+            if (n <= k / 2)
+            {
+                return ArithmeticSeriesSum(1, 1, n);
+            }
+            else
+            {
+                return ArithmeticSeriesSum(1, 1, k / 2) + ArithmeticSeriesSum(k, 1, n - k / 2);
+            }
+        }
+
+        private int ArithmeticSeriesSum(int a1, int d, int n)
+        {
+            return (a1 + a1 + (n - 1) * d) * n / 2;
+        }
+
+
+    }
+}
