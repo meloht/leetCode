228b1a9d641632dd0883fc5d571cf9e62450aa8f
hongtao_luo <luohongtao621@hotmail.com>
2025-03-26 14:37:05 +0800

    835

---
+++ TestLeetCode/0801_0850/835_ImageOverlapTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _835_ImageOverlapTest
+    {
+        _835_ImageOverlapAlg alg = new _835_ImageOverlapAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] img1 = [[1, 1, 0], [0, 1, 0], [0, 1, 0]], img2 = [[0, 0, 0], [0, 1, 1], [0, 0, 1]];
+            int exp = 3;
+            int res = alg.LargestOverlap(img1, img2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] img1 = [[1]], img2 = [[1]];
+            int exp = 1;
+            int res = alg.LargestOverlap(img1, img2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] img1 = [[0]], img2 = [[0]];
+            int exp = 0;
+            int res = alg.LargestOverlap(img1, img2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/835_ImageOverlapAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _835_ImageOverlapAlg
+    {
+        public int LargestOverlap(int[][] img1, int[][] img2)
+        {
+            int N = img1.Length;
+            int[,] count = new int[2 * N + 1, 2 * N + 1];
+            for (int i = 0; i < N; ++i)
+                for (int j = 0; j < N; ++j)
+                    if (img1[i][j] == 1)
+                        for (int i2 = 0; i2 < N; ++i2)
+                            for (int j2 = 0; j2 < N; ++j2)
+                                if (img2[i2][j2] == 1)
+                                    count[i - i2 + N, j - j2 + N] += 1;
+
+            int ans = 0;
+            int nn = 2 * N + 1;
+            for (int i = 0; i < nn; i++)
+            {
+                for (int j = 0; j < nn; j++)
+                {
+                    ans = Math.Max(ans, count[i, j]);
+                }
+            }
+
+            return ans;
+
+        }
+    }
+}
