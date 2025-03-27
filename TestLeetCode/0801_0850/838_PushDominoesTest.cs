c1c7230a350b552f5269f55abe8d0cc64904f780
hongtao_luo <luohongtao621@hotmail.com>
2025-03-27 17:14:20 +0800

    838

---
+++ TestLeetCode/0801_0850/838_PushDominoesTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _838_PushDominoesTest
+    {
+        _838_PushDominoesAlg alg = new _838_PushDominoesAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string dominoes = "RR.L";
+            string exp = "RR.L";
+            string res = alg.PushDominoes(dominoes);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string dominoes = ".L.R...LR..L..";
+            string exp = "LL.RR.LLRRLL..";
+            string res = alg.PushDominoes(dominoes);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/838_PushDominoesAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _838_PushDominoesAlg
+    {
+        public string PushDominoes(string dominoes)
+        {
+            char[] s = dominoes.ToCharArray();
+
+            int n = dominoes.Length;
+            int i = 0;
+            char left = 'L';
+            while (i < n)
+            {
+                int j = i;
+                while (j < n && s[j] == '.')
+                {
+                    j++;
+                }
+                char right = j < n ? s[j] : 'R';
+                if (left == right)
+                {
+                    while (i < j)
+                    {
+                        s[i++] = right;
+                    }
+                }
+                else if (left == 'R' && right == 'L')
+                {
+                    int k = j - 1;
+                    while (i < k)
+                    {
+                        s[i++] = 'R';
+                        s[k--] = 'L';
+                    }
+                }
+                left = right;
+                i = j + 1;
+            }
+
+            return new string(s);
+
+
+        }
+    }
+}
