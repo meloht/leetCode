a12a970bf1aa241a04b4e35804dfd05a91f018b2
hongtao_luo <luohongtao621@hotmail.com>
2025-03-27 13:19:56 +0800

    2712

---
+++ TestLeetCode/2701_2750/2712_MinimumCostToMakeAllCharactersEqualTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2712_MinimumCostToMakeAllCharactersEqualTest
+    {
+        _2712_MinimumCostToMakeAllCharactersEqualAlg alg = new _2712_MinimumCostToMakeAllCharactersEqualAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "0011";
+            long exp = 2;
+            long res = alg.MinimumCost(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "010101";
+            long exp = 9;
+            long res = alg.MinimumCost(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2701_2750/2712_MinimumCostToMakeAllCharactersEqualAlg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2712_MinimumCostToMakeAllCharactersEqualAlg
+    {
+        public long MinimumCost(string s)
+        {
+            long ans = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i - 1] != s[i])
+                {
+                    ans += Math.Min(i, s.Length - i);
+                }
+            }
+            return ans;
+        }
+    }
+}
