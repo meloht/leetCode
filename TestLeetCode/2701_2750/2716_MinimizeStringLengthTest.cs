eafbc66d7038a32ddd99239d5b9552a32ed8d941
hongtao_luo <luohongtao621@hotmail.com>
2025-03-28 09:41:58 +0800

    2716

---
+++ TestLeetCode/2701_2750/2716_MinimizeStringLengthTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2701_2750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2701_2750
+{
+    [TestClass]
+    public class _2716_MinimizeStringLengthTest
+    {
+        _2716_MinimizeStringLengthAlg alg = new _2716_MinimizeStringLengthAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "aaabc";
+            int exp = 3;
+            int res = alg.MinimizedStringLength(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "cbbd";
+            int exp = 3;
+            int res = alg.MinimizedStringLength(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "dddaaa";
+            int exp = 3;
+            int res = alg.MinimizedStringLength(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2701_2750/2716_MinimizeStringLengthAlg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2701_2750
+{
+    public class _2716_MinimizeStringLengthAlg
+    {
+        public int MinimizedStringLength(string s)
+        {
+            HashSet<char> set = new HashSet<char>();
+            foreach (var item in s)
+            {
+                set.Add(item);
+            }
+            return set.Count;
+        }
+
+    }
+}
