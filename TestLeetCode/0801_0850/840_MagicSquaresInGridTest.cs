6c8defb6f849108e1d88773373fcbd6e637b49b2
hongtao_luo <luohongtao621@hotmail.com>
2025-03-28 11:31:09 +0800

    840

---
+++ TestLeetCode/0801_0850/840_MagicSquaresInGridTest.cs
@@ -0,0 +1,59 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _840_MagicSquaresInGridTest
+    {
+        _840_MagicSquaresInGridAlg alg = new _840_MagicSquaresInGridAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [
+                [4, 3, 8, 4], 
+                [9, 5, 1, 9], 
+                [2, 7, 6, 2]];
+            int exp = 1;
+            int res = alg.NumMagicSquaresInside(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[8]];
+            int exp = 0;
+            int res = alg.NumMagicSquaresInside(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] grid = [[5, 5, 5], [5, 5, 5], [5, 5, 5]];
+            int exp = 0;
+            int res = alg.NumMagicSquaresInside(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[][] grid = [
+                [3, 10, 2, 3, 4], 
+                [4, 5, 6, 8, 1], 
+                [8, 8, 1, 6, 8], 
+                [1, 3, 5, 7, 1], 
+                [9, 4, 9, 2, 9]];
+            int exp = 1;
+            int res = alg.NumMagicSquaresInside(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/840_MagicSquaresInGridAlg.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _840_MagicSquaresInGridAlg
+    {
+        public int NumMagicSquaresInside(int[][] grid)
+        {
+            if (grid.Length < 3 || grid[0].Length < 3)
+                return 0;
+
+            int ans = 0;
+            for (int i = 0; i + 2 < grid.Length; i++)
+            {
+                for (int j = 0; j + 2 < grid[0].Length; j++)
+                {
+                    if (IsGrid(i, i + 2, j, j + 2, grid))
+                    {
+                        ans++;
+                    }
+                }
+            }
+
+            return ans;
+        }
+
+        private bool IsGrid(int r1, int r2, int c1, int c2, int[][] grid)
+        {
+            int sum = 0;
+            int[] arr = new int[10];
+
+            for (int i = r1, j = c1; i <= r2; i++, j++)
+            {
+                sum += grid[i][j];
+            }
+
+            int num = 0;
+            for (int i = r1; i <= r2; i++)
+            {
+                num = 0;
+                for (int j = c1; j <= c2; j++)
+                {
+                    num += grid[i][j];
+                    if (grid[i][j] > 9 || grid[i][j] < 1)
+                        return false;
+                    arr[grid[i][j]] = 1;
+
+
+                }
+                if (sum == -1)
+                {
+                    sum = num;
+                }
+                if (sum != num)
+                {
+                    return false;
+                }
+            }
+            if (arr.Count(p => p == 1) != 9)
+                return false;
+
+            for (int i = c1; i <= c2; i++)
+            {
+                num = 0;
+                for (int j = r1; j <= r2; j++)
+                {
+                    num += grid[j][i];
+                }
+                if (sum != num)
+                {
+                    return false;
+                }
+            }
+            num = 0;
+
+            for (int i = r1, j = c2; i <= r2; i++, j--)
+            {
+                num += grid[i][j];
+            }
+            if (sum != num)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
