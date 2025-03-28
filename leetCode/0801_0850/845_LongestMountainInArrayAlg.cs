f58e3db6b9f95b4b1c46122cf77a6b969a048bc5
hongtao_luo <luohongtao621@hotmail.com>
2025-03-28 16:50:13 +0800

    845

---
+++ leetCode/0801_0850/845_LongestMountainInArrayAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _845_LongestMountainInArrayAlg
+    {
+        public int LongestMountain(int[] arr)
+        {
+            return 0;
+        }
+    }
+}
