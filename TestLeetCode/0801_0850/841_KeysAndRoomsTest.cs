7de77be8e696001bd7ee5359923a4df93a536f1a
hongtao_luo <luohongtao621@hotmail.com>
2025-03-28 11:51:53 +0800

    841

---
+++ TestLeetCode/0801_0850/841_KeysAndRoomsTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _841_KeysAndRoomsTest
+    {
+        _841_KeysAndRoomsAlg alg = new _841_KeysAndRoomsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            IList<IList<int>> rooms = [[1], [2], [3], []];
+            bool exp = true;
+            bool res = alg.CanVisitAllRooms(rooms);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            IList<IList<int>> rooms = [[1, 3], [3, 0, 1], [2], [0]];
+            bool exp = false;
+            bool res = alg.CanVisitAllRooms(rooms);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/841_KeysAndRoomsAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _841_KeysAndRoomsAlg
+    {
+
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            HashSet<int> visited = new HashSet<int>();
+            Dfs(rooms, 0, visited);
+            return visited.Count == rooms.Count;
+        }
+
+        private void Dfs(IList<IList<int>> rooms, int i, HashSet<int> visited)
+        {
+            if (visited.Contains(i))
+                return;
+
+            visited.Add(i);
+
+            var list = rooms[i];
+            foreach (var item in list)
+            {
+                Dfs(rooms, item, visited);
+            }
+        }
+    }
+}
