91a40383e9f1f78e127714fbb2051b45bc639bff
luohongtao <luohongtao621@hotmail.com>
2025-03-29 14:11:35 +0800

    2360

---
+++ TestLeetCode/2351_2400/2360_LongestCycleInAGraphTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2351_2400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2351_2400
+{
+    [TestClass]
+    public class _2360_LongestCycleInAGraphTest
+    {
+        _2360_LongestCycleInAGraphAlg alg = new _2360_LongestCycleInAGraphAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] edges = [3, 3, 4, 2, 3];
+            int exp = 3;
+            int res = alg.LongestCycle(edges);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] edges = [2, -1, 3, 1];
+            int exp = -1;
+            int res = alg.LongestCycle(edges);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2351_2400/2360_LongestCycleInAGraphAlg.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2351_2400
+{
+    public class _2360_LongestCycleInAGraphAlg
+    {
+        public int LongestCycle1(int[] edges)
+        {
+            int n = edges.Length;
+            int maxCycle = -1;
+            bool[] visited = new bool[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!visited[i])
+                {
+                    Dictionary<int, int> stepMap = new Dictionary<int, int>();
+                    int current = i;
+                    int step = 0;
+                    while (current != -1)
+                    {
+                        if (stepMap.ContainsKey(current))
+                        {
+                            int cycleLength = step - stepMap[current];
+                            if (cycleLength > maxCycle)
+                            {
+                                maxCycle = cycleLength;
+                            }
+                            break;
+                        }
+                        if (visited[current])
+                        {
+                            break;
+                        }
+                        visited[current] = true;
+                        stepMap[current] = step;
+                        step++;
+                        current = edges[current];
+                    }
+                }
+            }
+            return maxCycle;
+        }
+
+        public int LongestCycle(int[] edges)
+        {
+            int n = edges.Length;
+            int[] label = new int[n];
+            int current_label = 0, ans = -1;
+            for (int i = 0; i < n; ++i)
+            {
+                if (label[i] != 0)
+                {
+                    continue;
+                }
+                int pos = i, start_label = current_label;
+                while (pos != -1)
+                {
+                    ++current_label;
+                    // 如果遇到了已经遍历过的节点
+                    if (label[pos] != 0)
+                    {
+                        // 如果该节点是这一次 i 循环中遍历的，说明找到了新的环，更新答案
+                        if (label[pos] > start_label)
+                        {
+                            ans = Math.Max(ans, current_label - label[pos]);
+                        }
+                        break;
+                    }
+                    label[pos] = current_label;
+                    pos = edges[pos];
+                }
+            }
+            return ans;
+        }
+
+
+    }
+}
