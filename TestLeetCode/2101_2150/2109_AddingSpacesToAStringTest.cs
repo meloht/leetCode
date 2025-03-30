8acaeac3ff5024bcc5e5c0a7d19a0e9ba11eba5f
luohongtao <luohongtao621@hotmail.com>
2025-03-30 11:34:10 +0800

    2109

---
+++ TestLeetCode/2101_2150/2109_AddingSpacesToAStringTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._2101_2150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2101_2150
+{
+    [TestClass]
+    public class _2109_AddingSpacesToAStringTest
+    {
+        _2109_AddingSpacesToAStringAlg alg = new _2109_AddingSpacesToAStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "LeetcodeHelpsMeLearn";
+            int[] spaces = [8, 13, 15];
+            string exp = "Leetcode Helps Me Learn";
+            string res = alg.AddSpaces(s, spaces);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "icodeinpython";
+            int[] spaces = [1, 5, 7, 9];
+            string exp = "i code in py thon";
+            string res = alg.AddSpaces(s, spaces);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "spacing";
+            int[] spaces = [0, 1, 2, 3, 4, 5, 6];
+            string exp = " s p a c i n g";
+            string res = alg.AddSpaces(s, spaces);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2101_2150/2109_AddingSpacesToAStringAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2101_2150
+{
+    public class _2109_AddingSpacesToAStringAlg
+    {
+        public string AddSpaces(string s, int[] spaces)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool[] arr = new bool[s.Length];
+         
+            for (int i = 0; i < spaces.Length; i++)
+            {
+                arr[spaces[i]] = true;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (arr[i])
+                {
+                    sb.Append(' ');
+                }
+                sb.Append(s[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
