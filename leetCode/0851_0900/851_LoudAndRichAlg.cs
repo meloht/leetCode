f4d0f2d0b247ad83a195d7df06e83c5b06e308dd
hongtao_luo <luohongtao621@hotmail.com>
2025-03-31 16:58:36 +0800

    851

--- leetCode/0851_0900/851_LoudAndRichAlg.cs
+++ leetCode/0851_0900/851_LoudAndRichAlg.cs
@@ -11,7 +11,7 @@ namespace leetCode._0851_0900
     public class _851_LoudAndRichAlg
     {
 
-        public int[] LoudAndRich(int[][] richer, int[] quiet)
+        public int[] LoudAndRich1(int[][] richer, int[] quiet)
         {
             int n = quiet.Length;
             List<int>[] adjList = new List<int>[n];
@@ -46,13 +46,66 @@ namespace leetCode._0851_0900
             foreach (var item in ls)
             {
 
-                Dfs(adjList, item, quiet,ans);
+                Dfs(adjList, item, quiet, ans);
                 if (quiet[ans[item]] < quiet[ans[val]])
                 {
                     ans[val] = ans[item];
                 }
             }
 
+        }
+
+        public int[] LoudAndRich(int[][] richer, int[] quiet)
+        {
+            int n = quiet.Length;
+
+            int[] ans = new int[n];
+
+            List<int>[] g = new List<int>[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                g[i] = new List<int>();
+            }
+
+            int[] inDeg = new int[n];
+
+            foreach (var item in richer)
+            {
+                g[item[0]].Add(item[1]);
+                inDeg[item[1]]++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                ans[i] = i;
+            }
+
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (inDeg[i] == 0)
+                {
+                    queue.Enqueue(i);
+                }
+            }
+
+            while (queue.Count > 0)
+            {
+                int x = queue.Dequeue();
+                foreach (int y in g[x])
+                {
+                    if (quiet[ans[x]] < quiet[ans[y]])
+                    {
+                        ans[y] = ans[x];
+                    }
+                    inDeg[y]--;
+                    if (inDeg[y] == 0)
+                    {
+                        queue.Enqueue(y);
+                    }
+                }
+            }
+            return ans;
         }
 
 
