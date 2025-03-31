53dd64c31ca0361a02809baad52c893f9f72cdb9
hongtao_luo <luohongtao621@hotmail.com>
2025-03-31 16:48:36 +0800

    851

---
+++ Testx/0851_0900/851_LoudAndRichTest.cs
@@ -0,0 +1,58 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    public class _851_LoudAndRichTest
+    {
+        _851_LoudAndRichAlg alg = new _851_LoudAndRichAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[][] richer = [[1, 0], [2, 1], [3, 1], [3, 7], [4, 3], [5, 3], [6, 3]];
+            int[] quiet = [3, 2, 5, 4, 6, 1, 7, 0];
+            int[] exp = [5, 5, 2, 5, 4, 5, 6, 7];
+            int[] res = alg.LoudAndRich(richer, quiet);
+            Assert.Equal(exp, res);
+
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[][] richer = [];
+            int[] quiet = [0];
+            int[] exp = [0];
+            int[] res = alg.LoudAndRich(richer, quiet);
+            Assert.Equal(exp, res);
+
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[][] richer = [[0, 1], [1, 2]];
+            int[] quiet = [1, 0, 2];
+            int[] exp = [0, 1, 1];
+            int[] res = alg.LoudAndRich(richer, quiet);
+            Assert.Equal(exp, res);
+
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int[][] richer = [[0, 1], [1, 2]];
+            int[] quiet = [0, 1, 2];
+            int[] exp = [0, 0, 0];
+            int[] res = alg.LoudAndRich(richer, quiet);
+            Assert.Equal(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/0851_0900/851_LoudAndRichAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _851_LoudAndRichAlg
+    {
+
+        public int[] LoudAndRich(int[][] richer, int[] quiet)
+        {
+            int n = quiet.Length;
+            List<int>[] adjList = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                adjList[i] = new List<int>();
+            }
+            for (int i = 0; i < richer.Length; i++)
+            {
+                adjList[richer[i][1]].Add(richer[i][0]);
+            }
+
+            int[] ans = new int[n];
+            Array.Fill(ans, -1);
+
+            for (int i = 0; i < n; i++)
+            {
+                Dfs(adjList, i, quiet, ans);
+            }
+
+            return ans;
+        }
+
+        private void Dfs(List<int>[] adjList, int val, int[] quiet, int[] ans)
+        {
+            if (ans[val] != -1)
+                return;
+
+            ans[val] = val;
+            var ls = adjList[val];
+
+            foreach (var item in ls)
+            {
+
+                Dfs(adjList, item, quiet,ans);
+                if (quiet[ans[item]] < quiet[ans[val]])
+                {
+                    ans[val] = ans[item];
+                }
+            }
+
+        }
+
+
+    }
+}
