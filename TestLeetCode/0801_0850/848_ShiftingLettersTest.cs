4ba0f4cdb369d2dcf73fa67afb021fc11a89bcfb
hongtao_luo <luohongtao621@hotmail.com>
2025-03-31 13:55:37 +0800

    848

---
+++ TestLeetCode/0801_0850/848_ShiftingLettersTest.cs
@@ -0,0 +1,48 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _848_ShiftingLettersTest
+    {
+        _848_ShiftingLettersAlg alg = new _848_ShiftingLettersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abc";
+            int[] shifts = [3, 5, 9];
+            string exp = "rpl";
+            string res = alg.ShiftingLetters(s, shifts);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "aaa";
+            int[] shifts = [1, 2, 3];
+            string exp = "gfd";
+            string res = alg.ShiftingLetters(s, shifts);
+            Assert.AreEqual(exp, res);
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "bad";
+            int[] shifts = [10, 20, 30];
+            string exp = "jyh";
+            string res = alg.ShiftingLetters(s, shifts);
+            Assert.AreEqual(exp, res);
+
+        }
+    }
+}
---
+++ leetCode/0801_0850/848_ShiftingLettersAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _848_ShiftingLettersAlg
+    {
+        public string ShiftingLetters(string s, int[] shifts)
+        {
+            char[] chars = s.ToCharArray();
+
+            int[] arr = new int[chars.Length];
+            arr[shifts.Length - 1] = shifts[shifts.Length - 1];
+            for (int i = shifts.Length - 2; i >= 0; i--)
+            {
+                arr[i] = (shifts[i] + arr[i + 1]) % 26;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                chars[i] = (char)((chars[i] - 'a' + arr[i]) % 26 + 'a');
+            }
+
+            return new string(chars);
+        }
+    }
+}
