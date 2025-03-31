9f3ceed7690d9679149be4949093aba27548f416
hongtao_luo <luohongtao621@hotmail.com>
2025-03-31 13:22:57 +0800

    846

---
+++ TestLeetCode/0801_0850/846_HandOfStraightsTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._0801_0850;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._0801_0850
+{
+    [TestClass]
+    public class _846_HandOfStraightsTest
+    {
+        _846_HandOfStraightsAlg alg = new _846_HandOfStraightsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] hand = [1, 2, 3, 6, 2, 3, 4, 7, 8];
+            int groupSize = 3;
+            bool exp = true;
+            bool res = alg.IsNStraightHand(hand, groupSize);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] hand = [1, 2, 3, 4, 5];
+            int groupSize = 4;
+            bool exp = false;
+            bool res = alg.IsNStraightHand(hand, groupSize);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/0801_0850/846_HandOfStraightsAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0801_0850
+{
+    public class _846_HandOfStraightsAlg
+    {
+        public bool IsNStraightHand(int[] hand, int groupSize)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < hand.Length; i++)
+            {
+                if (dict.ContainsKey(hand[i]))
+                {
+                    dict[hand[i]]++;
+                }
+                else
+                {
+                    dict.Add(hand[i], 1);
+                }
+
+            }
+
+            Array.Sort(hand);
+
+            for (int i = 0; i < hand.Length; i++)
+            {
+                if (dict.ContainsKey(hand[i]) && dict[hand[i]] > 0)
+                {
+                    int end = hand[i] + groupSize;
+                    for (int j = hand[i]; j < end; j++)
+                    {
+                        if (!dict.ContainsKey(j))
+                            return false;
+
+                        dict[j]--;
+                        if (dict[j] == 0)
+                        {
+                            dict.Remove(j);
+                        }
+                    }
+                }
+            }
+
+            if (dict.Count > 0)
+                return false;
+
+
+            return true;
+        }
+    }
+}
