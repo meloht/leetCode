526153f90f90cd63b1dce2e112682259e2668f50
hongtao_luo <luohongtao621@hotmail.com>
2025-04-01 15:30:40 +0800

    858

---
+++ Testx/0851_0900/_858_MirrorReflectionTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    public class _858_MirrorReflectionTest
+    {
+        _858_MirrorReflectionAlg alg = new _858_MirrorReflectionAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int p = 2, q = 1;
+            int exp = 2;
+            int res = alg.MirrorReflection(p, q);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int p = 3, q = 1;
+            int exp = 1;
+            int res = alg.MirrorReflection(p, q);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/858_MirrorReflectionAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _858_MirrorReflectionAlg
+    {
+        public int MirrorReflection(int p, int q)
+        {
+            int g = gcd(p, q);
+            p /= g; p %= 2;
+            q /= g; q %= 2;
+
+            if (p == 1 && q == 1) return 1;
+            return p == 1 ? 0 : 2;
+
+        }
+
+        public int gcd(int a, int b)
+        {
+            if (a == 0) return b;
+            return gcd(b % a, a);
+        }
+
+
+    }
+}
