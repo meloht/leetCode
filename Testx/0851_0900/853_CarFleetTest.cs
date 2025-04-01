00895e76fb765e223aca8a4732059b51d6561852
hongtao_luo <luohongtao621@hotmail.com>
2025-04-01 13:17:41 +0800

    853

---
+++ Testx/0851_0900/853_CarFleetTest.cs
@@ -0,0 +1,65 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    public class _853_CarFleetTest
+    {
+        _853_CarFleetAlg alg = new _853_CarFleetAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int target = 12;
+            int[] position = [10, 8, 0, 5, 3];
+            int[] speed = [2, 4, 1, 1, 3];
+            int exp = 3;
+            int res = alg.CarFleet(target, position, speed);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int target = 10;
+            int[] position = [3], speed = [3];
+            int exp = 1;
+            int res = alg.CarFleet(target, position, speed);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int target = 100;
+            int[] position = [0, 2, 4], speed = [4, 2, 1];
+            int exp = 1;
+            int res = alg.CarFleet(target, position, speed);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int target = 10;
+            int[] position = [6, 8], speed = [3, 2];
+            int exp = 2;
+            int res = alg.CarFleet(target, position, speed);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test05()
+        {
+            int target = 10;
+            int[] position = [0, 4, 2], speed = [2, 1, 3];
+            int exp = 1;
+            int res = alg.CarFleet(target, position, speed);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/853_CarFleetAlg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _853_CarFleetAlg
+    {
+        public int CarFleet(int target, int[] position, int[] speed)
+        {
+            int n = position.Length;
+
+            PoSp[] arr = new PoSp[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = new PoSp(position[i], (double)(target - position[i]) / speed[i]);
+            }
+
+            Array.Sort(arr, (x, y) => x.Position - y.Position);
+
+            Stack<PoSp> stack = new Stack<PoSp>();
+            for (int i = 0; i < n; i++)
+            {
+                var item = arr[i];
+                if (stack.Count == 0)
+                {
+                    stack.Push(item);
+                    continue;
+                }
+                if (stack.Peek().Time > item.Time)
+                {
+                    stack.Push(item);
+                }
+                else
+                {
+                    while (stack.Count > 0 && stack.Peek().Time <= item.Time)
+                    {
+                        stack.Pop();
+                    }
+                    stack.Push(item);
+                }
+            }
+
+            return stack.Count;
+        }
+
+        public struct PoSp
+        {
+            public int Position;
+            public double Time;
+
+            public PoSp(int p, double time)
+            {
+                Position = p;
+                Time = time;
+            }
+        }
+    }
+}
