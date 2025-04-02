8c0001355563dee670131dd40f7b7955eddee46e
hongtao_luo <luohongtao621@hotmail.com>
2025-04-02 16:46:24 +0800

    869

---
+++ Testx/0851_0900/869_ReorderedPowerOf2Test.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    public class _869_ReorderedPowerOf2Test
+    {
+        _869_ReorderedPowerOf2Alg alg = new _869_ReorderedPowerOf2Alg();
+
+        [Fact]
+        public void Test01()
+        {
+            int n = 1;
+            bool exp = true;
+            bool res = alg.ReorderedPowerOf2(n);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int n = 10;
+            bool exp = false;
+            bool res = alg.ReorderedPowerOf2(n);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/869_ReorderedPowerOf2Alg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _869_ReorderedPowerOf2Alg
+    {
+        public bool ReorderedPowerOf2(int n)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            int max = 1000_000_000;
+            string ss = GetNum(n);
+            int num = 1;
+            while (num <= max)
+            {
+                string s = GetNum(num);
+                if (s == ss)
+                    return true;
+                num = num * 2;
+            }
+
+
+
+            return false;
+        }
+
+        private string GetNum(int n)
+        {
+            List<char> list = new List<char>();
+            while (n > 0)
+            {
+                int num = n % 10;
+                list.Add((char)(num + '0'));
+                n = n / 10;
+            }
+            list.Sort();
+            return new string(list.ToArray());
+        }
+    }
+}
