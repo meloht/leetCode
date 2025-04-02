8f5588ae064432a3c2202f4f90bf6ef12372c136
hongtao_luo <luohongtao621@hotmail.com>
2025-04-02 14:51:02 +0800

    866

---
+++ Testx/0851_0900/866_PrimePalindromeTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    
+    public class _866_PrimePalindromeTest
+    {
+        _866_PrimePalindromeAlg alg = new _866_PrimePalindromeAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int n = 6;
+            int exp = 7;
+            int res = alg.PrimePalindrome(n);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int n = 8;
+            int exp = 11;
+            int res = alg.PrimePalindrome(n);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int n = 13;
+            int exp = 101;
+            int res = alg.PrimePalindrome(n);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/866_PrimePalindromeAlg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _866_PrimePalindromeAlg
+    {
+        public int PrimePalindrome(int n)
+        {
+            while (true)
+            {
+                if (n == Reverse(n) && IsPrime(n))
+                    return n;
+                n++;
+                if (10_000_000 < n && n < 100_000_000)
+                    n = 100_000_000;
+
+            }
+
+        }
+
+        private int Reverse(int n)
+        {
+            int ans = 0;
+            while (n > 0)
+            {
+                ans = 10 * ans + (n % 10);
+                n /= 10;
+            }
+            return ans;
+        }
+        public bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            if (n <= 3)
+                return true;
+
+            if (n % 2 == 0 || n % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+    }
+}
