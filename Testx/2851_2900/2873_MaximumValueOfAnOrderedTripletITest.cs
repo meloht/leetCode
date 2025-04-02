b99aead7a593507e2c0e4ec257b14c08951b3828
hongtao_luo <luohongtao621@hotmail.com>
2025-04-02 09:40:48 +0800

    2873

---
+++ Testx/2851_2900/2873_MaximumValueOfAnOrderedTripletITest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._2851_2900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2851_2900
+{
+    public class _2873_MaximumValueOfAnOrderedTripletITest
+    {
+        _2873_MaximumValueOfAnOrderedTripletIAlg alg = new _2873_MaximumValueOfAnOrderedTripletIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [12, 6, 1, 2, 7];
+            long exp = 77;
+            long res = alg.MaximumTripletValue(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [1, 10, 3, 4, 19];
+            long exp = 133;
+            long res = alg.MaximumTripletValue(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] nums = [1, 2, 3];
+            long exp = 0;
+            long res = alg.MaximumTripletValue(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2851_2900/2873_MaximumValueOfAnOrderedTripletIAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2851_2900
+{
+    public class _2873_MaximumValueOfAnOrderedTripletIAlg
+    {
+        public long MaximumTripletValue(int[] nums)
+        {
+            long ans = 0;
+            long maxDiff = 0;
+            long max = 0;
+            foreach (int item in nums)
+            {
+                ans = Math.Max(ans, maxDiff * item);
+                maxDiff = Math.Max(max - item, maxDiff);
+                max = Math.Max(max, item);
+            }
+
+            return ans;
+
+        }
+    }
+}
