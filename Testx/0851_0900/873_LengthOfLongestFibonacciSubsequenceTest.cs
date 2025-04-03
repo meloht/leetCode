9248d5c5781d3601ea0cb48d1789017669a0d204
hongtao_luo <luohongtao621@hotmail.com>
2025-04-03 13:56:58 +0800

    873

---
+++ Testx/0851_0900/873_LengthOfLongestFibonacciSubsequenceTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._0851_0900;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    public class _873_LengthOfLongestFibonacciSubsequenceTest
+    {
+        _873_LengthOfLongestFibonacciSubsequenceAlg alg = new _873_LengthOfLongestFibonacciSubsequenceAlg();
+        [Fact]
+        public void Test01()
+        {
+            int[] arr = [1, 2, 3, 4, 5, 6, 7, 8];
+            int exp = 5;
+            int res = alg.LenLongestFibSubseq(arr);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] arr = [1, 3, 7, 11, 12, 14, 18];
+            int exp = 3;
+            int res = alg.LenLongestFibSubseq(arr);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] arr = [2, 4, 5, 6, 7, 8, 11, 13, 14, 15, 21, 22, 34];
+            int exp = 5;
+            int res = alg.LenLongestFibSubseq(arr);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int[] arr = [1, 2, 3, 6, 7, 8, 9, 10, 12, 14, 21, 33];
+            int exp = 5;
+            int res = alg.LenLongestFibSubseq(arr);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/873_LengthOfLongestFibonacciSubsequenceAlg.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _873_LengthOfLongestFibonacciSubsequenceAlg
+    {
+        int ans = 0;
+        public int LenLongestFibSubseq1(int[] arr)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                dict.Add(arr[i], i);
+            }
+            bool[,] dp = new bool[arr.Length, arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int nn = arr[i] + arr[j];
+                    if (dict.ContainsKey(nn))
+                    {
+                        ans = Math.Max(ans, 3);
+                        int idx = dict[nn];
+                        Dfs(arr, j, idx, 3, dict, dp);
+                    }
+
+                }
+
+            }
+
+            return ans;
+        }
+
+        private void Dfs(int[] arr, int prev, int idx, int len, Dictionary<int, int> dict, bool[,] dp)
+        {
+            if (idx == arr.Length - 1)
+            {
+                return;
+            }
+            if (dp[prev, idx])
+            {
+                return;
+            }
+            dp[prev, idx] = true;
+            int nn = arr[prev] + arr[idx];
+            if (dict.ContainsKey(nn))
+            {
+                ans = Math.Max(ans, len + 1);
+                Dfs(arr, idx, dict[nn], len + 1, dict, dp);
+            }
+        }
+
+        public int LenLongestFibSubseq(int[] arr)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                dict.Add(arr[i], i);
+            }
+            int n = arr.Length;
+            int[,] dp = new int[n, n];
+
+
+            int res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num1 = arr[i];
+                for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--)
+                {
+                    int nn = arr[i] - arr[j];
+
+                    if (dict.ContainsKey(nn))
+                    {
+                        int k = dict[nn];
+                        dp[j, i] = Math.Max(dp[k, j] + 1, 3);
+                        res = Math.Max(res, dp[j, i]);
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
