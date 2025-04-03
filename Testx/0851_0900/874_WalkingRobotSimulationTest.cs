7066627871455180b2027188ceeb46d4326ca6f5
hongtao_luo <luohongtao621@hotmail.com>
2025-04-03 15:43:25 +0800

    874

---
+++ Testx/0851_0900/874_WalkingRobotSimulationTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    public class _874_WalkingRobotSimulationTest
+    {
+        _874_WalkingRobotSimulationAlg alg = new _874_WalkingRobotSimulationAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] commands = [4, -1, 3];
+            int[][] obstacles = [];
+            int exp = 25;
+            int res = alg.RobotSim(commands, obstacles);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] commands = [4, -1, 4, -2, 4];
+            int[][] obstacles = [[2, 4]];
+            int exp = 65;
+            int res = alg.RobotSim(commands, obstacles);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] commands = [6, -1, -1, 6];
+            int[][] obstacles = [[0, 0]];
+            int exp = 36;
+            int res = alg.RobotSim(commands, obstacles);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/874_WalkingRobotSimulationAlg.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0701_0750._705_DesignHashsetAlg;
+
+namespace leetCode._0851_0900
+{
+    public class _874_WalkingRobotSimulationAlg
+    {
+        public int RobotSim(int[] commands, int[][] obstacles)
+        {
+            Dictionary<int, HashSet<int>> dict = new Dictionary<int, HashSet<int>>();
+            foreach (int[] item in obstacles)
+            {
+                if (dict.ContainsKey(item[0]))
+                {
+                    dict[item[0]].Add(item[1]);
+                }
+                else
+                {
+                    HashSet<int> set = new HashSet<int>();
+                    set.Add(item[1]);
+                    dict.Add(item[0], set);
+                }
+
+            }
+
+            int x = 0, y = 0;
+            char direction = 'N';//E S W N
+            int ans = 0;
+            foreach (int cmd in commands)
+            {
+                if (cmd == -2)
+                {
+                    switch (direction)
+                    {
+                        case 'N':
+                            direction = 'W';
+                            break;
+                        case 'E':
+                            direction = 'N';
+                            break;
+                        case 'W':
+                            direction = 'S';
+                            break;
+                        case 'S':
+                            direction = 'E';
+                            break;
+                    }
+                }
+                else if (cmd == -1)
+                {
+                    switch (direction)
+                    {
+                        case 'N':
+                            direction = 'E';
+                            break;
+                        case 'E':
+                            direction = 'S';
+                            break;
+                        case 'W':
+                            direction = 'N';
+                            break;
+                        case 'S':
+                            direction = 'W';
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (direction)
+                    {
+                        case 'N':
+                            int nn = y + cmd;
+                            for (int i = y + 1; i <= nn; i++)
+                            {
+                                if (dict.ContainsKey(x) && dict[x].Contains(i))
+                                {
+                                    break;
+                                }
+                                y = i;
+                            }
+
+                            break;
+                        case 'E':
+                            int nn1 = x + cmd;
+                            for (int i = x + 1; i <= nn1; i++)
+                            {
+                                if (dict.ContainsKey(i) && dict[i].Contains(y))
+                                {
+                                    break;
+                                }
+                                x = i;
+                            }
+                            break;
+                        case 'W':
+                            int nn2 = x - cmd;
+                            for (int i = x - 1; i >= nn2; i--)
+                            {
+                                if (dict.ContainsKey(i) && dict[i].Contains(y))
+                                {
+                                    break;
+                                }
+                                x = i;
+                            }
+                            break;
+                        case 'S':
+                            int nn3 = y - cmd;
+                            for (int i = y - 1; i >= nn3; i--)
+                            {
+                                if (dict.ContainsKey(x) && dict[x].Contains(i))
+                                {
+                                    break;
+                                }
+                                y = i;
+                            }
+                            break;
+                    }
+
+                    ans = Math.Max(x * x + y * y, ans);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
