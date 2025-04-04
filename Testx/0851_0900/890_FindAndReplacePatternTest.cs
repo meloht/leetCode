b06efad45fd2a7620dab0ae365c9573698ac47cb
luohongtao <luohongtao621@hotmail.com>
2025-04-04 13:50:34 +0800

    890

---
+++ Testx/0851_0900/890_FindAndReplacePatternTest.cs
@@ -0,0 +1,26 @@
+﻿using leetCode._0851_0900;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    public class _890_FindAndReplacePatternTest
+    {
+        _890_FindAndReplacePatternAlg alg = new _890_FindAndReplacePatternAlg();
+        [Fact]
+        public void Test01()
+        {
+            string[] words = ["abc", "deq", "mee", "aqq", "dkd", "ccc"];
+            string pattern = "abb";
+            string[] exp = ["mee", "aqq"];
+            string[] res = alg.FindAndReplacePattern(words, pattern).ToArray();
+            Assert.Equal(exp, res);
+        }
+
+
+    }
+}
---
+++ leetCode/0851_0900/890_FindAndReplacePatternAlg.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _890_FindAndReplacePatternAlg
+    {
+        public IList<string> FindAndReplacePattern(string[] words, string pattern)
+        {
+            List<string> result = new List<string>();
+            Dictionary<char, char> dict = new Dictionary<char, char>();
+            int cnt = pattern.ToCharArray().Distinct().Count();
+            foreach (var item in words)
+            {
+                dict.Clear();
+                bool bl = true;
+                for (int i = 0; i < pattern.Length; i++)
+                {
+                    if (dict.ContainsKey(item[i]))
+                    {
+                        if (dict[item[i]] != pattern[i])
+                        {
+                            bl = false;
+                            break;
+                        }
+
+                    }
+                    else
+                    {
+                        dict.Add(item[i], pattern[i]);
+                    }
+                }
+
+                if (bl && dict.Count == cnt)
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+
+        public IList<string> FindAndReplacePattern1(string[] words, string pattern)
+        {
+            List<string> ans = new List<string>();
+            bool Match(string s, string t)
+            {
+                int[] m1 = new int[26];
+                int[] m2 = new int[26];
+                for (int i = 0; i < s.Length; ++i)
+                {
+                    if (m1[s[i] - 'a'] != m2[t[i] - 'a']) return false;
+                    m1[s[i] - 'a'] = i + 1;
+                    m2[t[i] - 'a'] = i + 1;
+                }
+                return true;
+            }
+            foreach (var word in words)
+            {
+                if (Match(word, pattern)) ans.Add(word);
+            }
+            return ans;
+        }
+    }
+}
