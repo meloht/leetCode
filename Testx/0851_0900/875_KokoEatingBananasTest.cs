10c8ed6b4d1abaedd42a74838d55be3008257aca
luohongtao <luohongtao621@hotmail.com>
2025-04-04 11:40:52 +0800

    875

---
+++ Testx/0851_0900/875_KokoEatingBananasTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    public class _875_KokoEatingBananasTest
+    {
+        _875_KokoEatingBananasAlg alg = new _875_KokoEatingBananasAlg();
+        
+        [Fact]
+        public void Test01()
+        {
+            int[] piles = [3, 6, 7, 11];
+            int h = 8;
+            int exp = 4;
+            int res = alg.MinEatingSpeed(piles, h);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] piles = [30, 11, 23, 4, 20];
+            int h = 5;
+            int exp = 30;
+            int res = alg.MinEatingSpeed(piles, h);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] piles = [30, 11, 23, 4, 20];
+            int h = 6;
+            int exp = 23;
+            int res = alg.MinEatingSpeed(piles, h);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/875_KokoEatingBananasAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _875_KokoEatingBananasAlg
+    {
+        public int MinEatingSpeed(int[] piles, int h)
+        {
+            int max = piles.Max();
+
+            int left = 1;
+            int right = max;
+
+            int k = right;
+            while (left <= right)
+            {
+                int speed = (right - left) / 2 + left;
+                long time = GetTime(piles, speed);
+                if (time <= h)
+                {
+                    k = speed;
+                    right = speed - 1;
+                }
+                else
+                {
+                    left = speed + 1;
+                }
+            }
+            return k;
+        }
+
+        private long GetTime(int[] piles, int speed)
+        {
+            long time = 0;
+            foreach (var item in piles)
+            {
+                int curTime = item / speed;
+                if (item % speed > 0)
+                {
+                    curTime++;
+                }
+                time += curTime;
+            }
+            return time;
+        }
+    }
+}
