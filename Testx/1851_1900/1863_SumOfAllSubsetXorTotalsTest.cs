9769a88e055400a4fefb5f229077bf85278a7c61
luohongtao <luohongtao621@hotmail.com>
2025-04-05 10:14:12 +0800

    1863

---
+++ Testx/1851_1900/1863_SumOfAllSubsetXorTotalsTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._1851_1900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1851_1900
+{
+    public class _1863_SumOfAllSubsetXorTotalsTest
+    {
+        _1863_SumOfAllSubsetXorTotalsAlg alg = new _1863_SumOfAllSubsetXorTotalsAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [1, 3];
+            int exp = 6;
+            int res = alg.SubsetXORSum(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [5, 1, 6];
+            int exp = 28;
+            int res = alg.SubsetXORSum(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] nums = [3, 4, 5, 6, 7, 8];
+            int exp = 480;
+            int res = alg.SubsetXORSum(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1851_1900/1863_SumOfAllSubsetXorTotalsAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1851_1900
+{
+    public class _1863_SumOfAllSubsetXorTotalsAlg
+    {
+        int ans = 0;
+        public int SubsetXORSum(int[] nums)
+        {
+            Dfs(nums, 0, 0);
+            return ans;
+        }
+
+        private void Dfs(int[] nums, int val, int idx)
+        {
+            if (idx == nums.Length)
+            {
+                ans += val;
+                return;
+            }
+
+            Dfs(nums, val ^ nums[idx], idx + 1);
+            Dfs(nums, val, idx + 1);
+        }
+    }
+}
