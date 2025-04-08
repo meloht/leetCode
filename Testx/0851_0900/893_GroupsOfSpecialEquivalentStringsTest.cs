7e6fb4e1b9144cac7e0aeb07167ef4ead2085fcc
hongtao_luo <luohongtao621@hotmail.com>
2025-04-08 14:03:11 +0800

    893

---
+++ Testx/0851_0900/893_GroupsOfSpecialEquivalentStringsTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    public class _893_GroupsOfSpecialEquivalentStringsTest
+    {
+        _893_GroupsOfSpecialEquivalentStringsAlg alg = new _893_GroupsOfSpecialEquivalentStringsAlg();
+        
+        [Fact]
+        public void Test01()
+        {
+            string[] words = ["abcd", "cdab", "cbad", "xyzz", "zzxy", "zzyx"];
+            int exp = 3;
+            int res = alg.NumSpecialEquivGroups(words);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            string[] words = ["abc", "acb", "bac", "bca", "cab", "cba"];
+            int exp = 3;
+            int res = alg.NumSpecialEquivGroups(words);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0851_0900/893_GroupsOfSpecialEquivalentStringsAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _893_GroupsOfSpecialEquivalentStringsAlg
+    {
+        public int NumSpecialEquivGroups(string[] words)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            foreach (string word in words)
+            {
+                char[] chars = new char[word.Length];
+                int n1 = 0;
+                int n2 = (word.Length + 1) / 2;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if ((i & 1) == 0)
+                    {
+                        chars[n1++] = word[i];
+                    }
+                    else
+                    {
+                        chars[n2++] = word[i];
+                    }
+                }
+                Array.Sort(chars, 0, word.Length / 2);
+                Array.Sort(chars, word.Length / 2, (word.Length + 1) / 2);
+                set.Add(new string(chars));
+            }
+
+            return set.Count;
+        }
+    }
+}
