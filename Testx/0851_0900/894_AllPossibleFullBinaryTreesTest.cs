10f65214c94a0bfc78998d86f26d0c6010341f44
hongtao_luo <luohongtao621@hotmail.com>
2025-04-08 15:30:14 +0800

    894

---
+++ Testx/0851_0900/894_AllPossibleFullBinaryTreesTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._0851_0900;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0851_0900
+{
+    public class _894_AllPossibleFullBinaryTreesTest
+    {
+        _894_AllPossibleFullBinaryTreesAlg alg = new _894_AllPossibleFullBinaryTreesAlg();
+        [Fact]
+        public void Test01()
+        {
+            int n = 7;
+            int?[][] exp = [
+                [0, 0, 0, null, null, 0, 0, null, null, 0, 0],
+            [0, 0, 0, null, null, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, null, null, null, null, 0, 0],
+            [0, 0, 0, 0, 0, null, null, 0, 0]];
+
+            var res = alg.AllPossibleFBT(n);
+            var ress = TreeNode.GetTreeLevelLists(res).ToArray();
+
+            Assert.True(Utils.IsSameList(exp,ress));
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int n = 3;
+            int?[][] exp = [[0, 0, 0]];
+
+            var res = alg.AllPossibleFBT(n);
+            var ress = TreeNode.GetTreeLevelLists(res).ToArray();
+
+            Assert.True(Utils.IsSameList(exp, ress));
+        }
+    }
+}
---
+++ leetCode/0851_0900/894_AllPossibleFullBinaryTreesAlg.cs
@@ -0,0 +1,43 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _894_AllPossibleFullBinaryTreesAlg
+    {
+        public IList<TreeNode> AllPossibleFBT(int n)
+        {
+            List<TreeNode> list = new List<TreeNode>();
+            if (n % 2 == 0)
+                return list;
+            if (n == 1)
+            {
+                list.Add(new TreeNode(0));
+                return list;
+            }
+
+            for (int i = 1; i < n; i += 2)
+            {
+                IList<TreeNode> left = AllPossibleFBT(i);
+                IList<TreeNode> right = AllPossibleFBT(n - 1 - i);
+                foreach (TreeNode leftSubtree in left)
+                {
+                    foreach (TreeNode rightSubtree in right)
+                    {
+                        TreeNode root = new TreeNode(0, leftSubtree, rightSubtree);
+                        list.Add(root);
+                    }
+                }
+            }
+
+            return list;
+        }
+
+      
+       
+    }
+}
