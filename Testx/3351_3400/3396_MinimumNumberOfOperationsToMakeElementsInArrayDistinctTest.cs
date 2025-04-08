f5ac3c9c3fa19cc61d0e885ff2dbf521731131ed
hongtao_luo <luohongtao621@hotmail.com>
2025-04-08 12:18:08 +0800

    3396

---
+++ Testx/3351_3400/3396_MinimumNumberOfOperationsToMakeElementsInArrayDistinctTest.cs
@@ -0,0 +1,40 @@
+﻿using leetCode._3351_3400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3351_3400
+{
+    public class _3396_MinimumNumberOfOperationsToMakeElementsInArrayDistinctTest
+    {
+        _3396_MinimumNumberOfOperationsToMakeElementsInArrayDistinctAlg alg = new _3396_MinimumNumberOfOperationsToMakeElementsInArrayDistinctAlg();
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [1, 2, 3, 4, 2, 3, 3, 5, 7];
+            int exp = 2;
+            int res = alg.MinimumOperations(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [4, 5, 6, 4, 4];
+            int exp = 2;
+            int res = alg.MinimumOperations(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] nums = [6, 7, 8, 9];
+            int exp = 0;
+            int res = alg.MinimumOperations(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3351_3400/3396_MinimumNumberOfOperationsToMakeElementsInArrayDistinctAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3351_3400
+{
+    public class _3396_MinimumNumberOfOperationsToMakeElementsInArrayDistinctAlg
+    {
+        public int MinimumOperations(int[] nums)
+        {
+            int[] map = new int[101];
+
+            int ans = 0;
+            int idx = -1;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (map[nums[i]] > 0)
+                {
+                    idx = i;
+                    break;
+                }
+                map[nums[i]]++;
+            }
+            int n = idx + 1;
+            ans = n / 3;
+            if (n % 3 > 0)
+            {
+                ans += 1;
+            }
+
+            return ans;
+        }
+    }
+}
