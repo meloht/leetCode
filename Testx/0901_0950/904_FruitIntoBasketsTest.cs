304967f80fb5b3305da8962bc9fa13fc723e2abd
hongtao_luo <luohongtao621@hotmail.com>
2025-04-09 16:39:15 +0800

    904

---
+++ Testx/0901_0950/904_FruitIntoBasketsTest.cs
@@ -0,0 +1,88 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _904_FruitIntoBasketsTest
+    {
+        _904_FruitIntoBasketsAlg alg = new _904_FruitIntoBasketsAlg();
+
+
+        [Fact]
+        public void Test01()
+        {
+            int[] fruits = [1, 2, 1];
+            int exp = 3;
+            int res = alg.TotalFruit(fruits);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] fruits = [0, 1, 2, 2];
+            int exp = 3;
+            int res = alg.TotalFruit(fruits);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] fruits = [1, 2, 3, 2, 2];
+            int exp = 4;
+            int res = alg.TotalFruit(fruits);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int[] fruits = [3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4];
+            int exp = 5;
+            int res = alg.TotalFruit(fruits);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test05()
+        {
+            int[] fruits = [0, 0, 1, 1];
+            int exp = 4;
+            int res = alg.TotalFruit(fruits);
+            Assert.Equal(exp, res);
+        }
+
+
+        [Fact]
+        public void Test06()
+        {
+            int[] fruits = [1, 0, 1, 4, 1, 4, 1, 2, 3];
+            int exp = 5;
+            int res = alg.TotalFruit(fruits);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test07()
+        {
+            int[] fruits = [1, 0, 3, 4, 3];
+            int exp = 3;
+            int res = alg.TotalFruit(fruits);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test08()
+        {
+            int[] fruits = [1, 9, 1, 8, 22, 0, 22, 19, 19, 2, 19, 6, 6, 19, 2, 20, 2, 9, 9, 9, 9, 16, 19, 16, 19, 11, 19, 0, 19, 19];
+            int exp = 5;
+            int res = alg.TotalFruit(fruits);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/904_FruitIntoBasketsAlg.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _904_FruitIntoBasketsAlg
+    {
+        public int TotalFruit2(int[] fruits)
+        {
+            int total = 0;
+            Dictionary<int, int> dictNum = new Dictionary<int, int>();
+
+            int max = int.MinValue;
+            int minIndex = 0;
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                total++;
+                if (dictNum.ContainsKey(fruits[i]))
+                {
+                    dictNum[fruits[i]] = i;
+                }
+                else
+                {
+                    if (dictNum.Count < 2)
+                    {
+                        dictNum.Add(fruits[i], i);
+                    }
+                    else
+                    {
+                        int prev = fruits[i - 1];
+                        int delIndex = GetDeleteNum(dictNum, prev);
+                        dictNum.Remove(fruits[delIndex]);
+                        dictNum.Add(fruits[i], i);
+
+                        total = total - (delIndex - minIndex + 1);
+                        minIndex = delIndex + 1;
+                    }
+                }
+                max = Math.Max(max, total);
+            }
+
+            return max;
+        }
+
+        private int GetDeleteNum(Dictionary<int, int> dict, int num)
+        {
+            foreach (var i in dict)
+            {
+                if (i.Key != num)
+                    return i.Value;
+            }
+            return 0;
+        }
+
+        public int TotalFruit(int[] fruits)
+        {
+            int total = 0;
+
+            int num1 = -1;
+            int num1MaxIndex = -1;
+            int num2 = -1;
+            int num2MaxIndex = -1;
+
+            int max = int.MinValue;
+            int minIndex = 0;
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                total++;
+                if (num1 == fruits[i] || num2 == fruits[i])
+                {
+                    if (num2 == fruits[i])
+                    {
+                        num2MaxIndex = i;
+                    }
+                    else
+                    {
+                        num1MaxIndex = i;
+                    }
+                }
+                else
+                {
+                    if (num1MaxIndex == -1 || num2MaxIndex == -1)
+                    {
+                        if (num1MaxIndex == -1)
+                        {
+                            num1MaxIndex = i;
+                            num1 = fruits[i];
+                        }
+                        else
+                        {
+                            num2MaxIndex = i;
+                            num2 = fruits[i];
+                        }
+                    }
+                    else
+                    {
+                        int curr = fruits[i];
+                        int prev = fruits[i - 1];
+
+                        int delIndex = num1MaxIndex;
+                        if (prev == num1)
+                        {
+                            delIndex = num2MaxIndex;
+                            num2MaxIndex = i;
+                            num2=curr;
+                        }
+                        else
+                        {
+                            num1MaxIndex = i;
+                            num1 = curr;
+                        }
+                        total = total - (delIndex - minIndex + 1);
+                        minIndex = delIndex + 1;
+                    }
+                }
+                max = Math.Max(max, total);
+            }
+
+            return max;
+        }
+
+     
+    }
+}
