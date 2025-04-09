713089111126a77b5ea7c5aac1d0537ed2aec640
hongtao_luo <luohongtao621@hotmail.com>
2025-04-09 11:39:26 +0800

    900

---
+++ Testx/0851_0900/900_RleIteratorTest.cs
@@ -0,0 +1,25 @@
+﻿using leetCode._0851_0900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._0851_0900._900_RleIteratorAlg;
+
+namespace Testx._0851_0900
+{
+    public class _900_RleIteratorTest
+    {
+        _900_RleIteratorAlg alg = new _900_RleIteratorAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            RLEIterator rLEIterator = new RLEIterator([3, 8, 0, 9, 2, 5]); // 这映射到序列 [8,8,8,5,5]。
+            Assert.Equal(8, rLEIterator.Next(2)); // 耗去序列的 2 个项，返回 8。现在剩下的序列是 [8, 5, 5]。
+            Assert.Equal(8, rLEIterator.Next(1)); // 耗去序列的 1 个项，返回 8。现在剩下的序列是 [5, 5]。
+            Assert.Equal(5, rLEIterator.Next(1)); // 耗去序列的 1 个项，返回 5。现在剩下的序列是 [5]。
+            Assert.Equal(-1, rLEIterator.Next(2)); // 耗去序列的 2 个项，返回 -1。 这是由于第一个被耗去的项是 5，
+        }
+    }
+}
---
+++ leetCode/0851_0900/900_RleIteratorAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0851_0900
+{
+    public class _900_RleIteratorAlg
+    {
+        public class RLEIterator
+        {
+            int[] nums;
+            int idx = 0;
+            public RLEIterator(int[] encoding)
+            {
+                nums = encoding;
+            }
+
+            public int Next(int n)
+            {
+                for (int i = idx; i < nums.Length; i += 2)
+                {
+                    int count = nums[i];
+
+                    if (count >= n)
+                    {
+                        nums[i] = nums[i] - n;
+                        if (count == n)
+                        {
+                            idx = i + 2;
+                        }
+                        return nums[i + 1];
+                    }
+                    else
+                    {
+                        n -= count;
+                        nums[i] = 0;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
