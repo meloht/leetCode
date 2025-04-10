1c3667ef8085b05fa2e6d1cbb84bb9a719c2e74d
hongtao_luo <luohongtao621@hotmail.com>
2025-04-10 17:21:29 +0800

    907

---
+++ Testx/0901_0950/907_SumOfSubarrayMinimumsTest.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _907_SumOfSubarrayMinimumsTest
+    {
+        _907_SumOfSubarrayMinimumsAlg alg = new _907_SumOfSubarrayMinimumsAlg();
+        [Fact]
+        public void Test01()
+        {
+            int[] arr = [3, 1, 2, 4];
+            int exp = 17;
+            int res = alg.SumSubarrayMins(arr);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] arr = [11, 81, 94, 43, 3];
+            int exp = 444;
+            int res = alg.SumSubarrayMins(arr);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/907_SumOfSubarrayMinimumsAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _907_SumOfSubarrayMinimumsAlg
+    {
+        public int SumSubarrayMins(int[] arr)
+        {
+            int Mod = 1_000_000_007;
+            long ans = 0;
+            Stack<int> stack = new Stack<int>();
+            stack.Push(-1);
+            for (int r = 0; r <= arr.Length; r++)
+            {
+                int x = r < arr.Length ? arr[r] : -1;
+                while (stack.Count > 1 && arr[stack.Peek()] >= x)
+                {
+                    int i = stack.Pop();
+                    ans += (long)arr[i] * (i - stack.Peek()) * (r - i);
+                }
+                stack.Push(r);
+            }
+
+            return (int)(ans % Mod);
+        }
+
+    }
+}
