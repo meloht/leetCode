0ae1693679c6a10d0f05b9dcf2d88f7cac276f51
luohongtao <luohongtao621@hotmail.com>
2025-04-12 21:23:13 +0800

    915

---
+++ Testx/0901_0950/915_PartitionArrayIntoDisjointIntervalsTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _915_PartitionArrayIntoDisjointIntervalsTest
+    {
+        _915_PartitionArrayIntoDisjointIntervalsAlg alg = new _915_PartitionArrayIntoDisjointIntervalsAlg();
+        
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [5, 0, 3, 8, 6];
+            int exp = 3;
+            int res = alg.PartitionDisjoint(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [1, 1, 1, 0, 6, 12];
+            int exp = 4;
+            int res = alg.PartitionDisjoint(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] nums = [1, 1];
+            int exp = 1;
+            int res = alg.PartitionDisjoint(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/915_PartitionArrayIntoDisjointIntervalsAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _915_PartitionArrayIntoDisjointIntervalsAlg
+    {
+        public int PartitionDisjoint(int[] nums)
+        {
+            int[] right = new int[nums.Length];
+            right[nums.Length - 1] = nums[nums.Length - 1];
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                right[i] = Math.Min(right[i + 1], nums[i]);
+            }
+            int max = nums[0];
+            int ans = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                max = Math.Max(nums[i], max);
+                if (max <= right[i + 1])
+                {
+                    return i + 1;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
