4fbd7b4a4a761da70b1cc298e3aa1ddba6348c27
luohongtao <luohongtao621@hotmail.com>
2025-04-12 21:44:26 +0800

    916

---
+++ Testx/0901_0950/916_WordSubsetsTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _916_WordSubsetsTest
+    {
+        _916_WordSubsetsAlg alg = new _916_WordSubsetsAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            string[] words1 = ["amazon", "apple", "facebook", "google", "leetcode"], words2 = ["e", "o"];
+            string[] exp = ["facebook", "google", "leetcode"];
+            string[] res = alg.WordSubsets(words1, words2).ToArray();
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            string[] words1 = ["amazon", "apple", "facebook", "google", "leetcode"], words2 = ["lc", "eo"];
+            string[] exp = ["leetcode"];
+            string[] res = alg.WordSubsets(words1, words2).ToArray();
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            string[] words1 = ["acaac", "cccbb", "aacbb", "caacc", "bcbbb"], words2 = ["c", "cc", "b"];
+            string[] exp = ["cccbb"];
+            string[] res = alg.WordSubsets(words1, words2).ToArray();
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            string[] words1 = ["amazon", "apple", "facebook", "google", "leetcode"], words2 = ["lo", "eo"];
+            string[] exp = ["google", "leetcode"];
+            string[] res = alg.WordSubsets(words1, words2).ToArray();
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/916_WordSubsetsAlg.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _916_WordSubsetsAlg
+    {
+        public IList<string> WordSubsets(string[] words1, string[] words2)
+        {
+
+            int[] arr = new int[26];
+            foreach (var word in words2)
+            {
+                int[] map = new int[26];
+                foreach (var item in word)
+                {
+                    map[item - 'a']++;
+                }
+                for (int i = 0; i < 26; i++)
+                {
+                    arr[i] = Math.Max(map[i], arr[i]);
+                }
+            }
+            List<string> ans = new List<string>();
+            foreach (var word in words1)
+            {
+                int[] map = new int[26];
+                foreach (var item in word)
+                {
+                    map[item - 'a']++;
+                }
+                bool bl = true;
+                for (int i = 0; i < 26; i++)
+                {
+                    if (arr[i] > map[i])
+                    {
+                        bl = false;
+                        break;
+                    }
+                }
+                if (bl)
+                {
+                    ans.Add(word);
+                }
+            }
+            return ans;
+        }
+    }
+}
