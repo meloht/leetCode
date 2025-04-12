982c60e3acf66adf8c1b58419faec8e9ecccc912
luohongtao <luohongtao621@hotmail.com>
2025-04-12 20:49:13 +0800

    3272

---
+++ TestLeetCode/3251_3300/3272_FindTheCountOfGoodIntegersTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3251_3300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3251_3300
+{
+    [TestClass]
+    public class _3272_FindTheCountOfGoodIntegersTest
+    {
+        _3272_FindTheCountOfGoodIntegersAlg alg = new _3272_FindTheCountOfGoodIntegersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3, k = 5;
+            int exp = 27;
+            long res = alg.CountGoodIntegers(n, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1, k = 4;
+            int exp = 2;
+            long res = alg.CountGoodIntegers(n, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 5, k = 6;
+            int exp = 2468;
+            long res = alg.CountGoodIntegers(n, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3251_3300/3272_FindTheCountOfGoodIntegersAlg.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._3251_3300
+{
+    public class _3272_FindTheCountOfGoodIntegersAlg
+    {
+        public long CountGoodIntegers(int n, int k)
+        {
+            int[] fac = new int[n + 1];
+            fac[0] = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                fac[i] = fac[i - 1] * i;
+            }
+
+            long ans = 0;
+            int skip = n & 1;
+            HashSet<string> vis = new HashSet<string>();
+            int baseVal = (int)Math.Pow(10, (n - 1) / 2);
+            for (int i = baseVal; i < baseVal * 10; i++)
+            { // 枚举回文数左半边
+                string s = i.ToString(); ;
+                var chs = s.ToCharArray();
+                Array.Reverse(chs);
+                s += new string(chs).Substring(skip);
+                if (long.Parse(s) % k > 0)
+                { // 回文数不能被 k 整除
+                    continue;
+                }
+
+                char[] sortedS = s.ToCharArray();
+                Array.Sort(sortedS);
+                if (!vis.Add(new string(sortedS)))
+                { // 不能重复统计
+                    continue;
+                }
+
+                int[] cnt = new int[10];
+                foreach (char c in sortedS)
+                {
+                    cnt[c - '0']++;
+                }
+                int res = (n - cnt[0]) * fac[n - 1];
+                foreach (int c in cnt)
+                {
+                    res /= fac[c];
+                }
+                ans += res;
+            }
+            return ans;
+
+          
+        }
+    }
+}
