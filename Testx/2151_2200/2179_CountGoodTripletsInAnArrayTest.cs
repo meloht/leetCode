6fea0ff3f630fde6fa7d46400dc35159199612f9
hongtao_luo <luohongtao621@hotmail.com>
2025-04-15 15:50:26 +0800

    2179

---
+++ Testx/2151_2200/2179_CountGoodTripletsInAnArrayTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._2151_2200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2151_2200
+{
+    public class _2179_CountGoodTripletsInAnArrayTest
+    {
+        _2179_CountGoodTripletsInAnArrayAlg alg = new _2179_CountGoodTripletsInAnArrayAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums1 = [2, 0, 1, 3], nums2 = [0, 1, 2, 3];
+            long exp = 1;
+            long res = alg.GoodTriplets(nums1, nums2);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums1 = [4, 0, 1, 3, 2], nums2 = [4, 1, 0, 2, 3];
+            long exp = 4;
+            long res = alg.GoodTriplets(nums1, nums2);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2151_2200/2179_CountGoodTripletsInAnArrayAlg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2151_2200
+{
+    public class _2179_CountGoodTripletsInAnArrayAlg
+    {
+        public long GoodTriplets(int[] nums1, int[] nums2)
+        {
+            int n = nums1.Length;
+            int[] p = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                p[nums1[i]] = i;
+            }
+            long ans = 0;
+            FenwickTree t = new FenwickTree(n);
+            for (int i = 0; i < n - 1; i++)
+            {
+                int y = p[nums2[i]];
+                long less = t.Pre(y);
+                ans += less * (n - 1 - y - (i - less));
+                t.Update(y + 1, 1);
+            }
+            return ans;
+        }
+
+        class FenwickTree
+        {
+            private long[] tree;
+            public FenwickTree(int n)
+            {
+                tree = new long[n + 1];
+            }
+
+            public void Update(int i, long val)
+            {
+                for (; i < tree.Length; i += i & -i)
+                {
+                    tree[i] += val;
+                }
+            }
+
+            public long Pre(int i)
+            {
+                long res = 0;
+                for (; i > 0; i &= i - 1)
+                {
+                    res += tree[i];
+                }
+                return res;
+            }
+        }
+    }
+}
