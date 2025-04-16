b6b4f4ad7e1351211ec90ae95ea18fa98b4df654
hongtao_luo <luohongtao621@hotmail.com>
2025-04-16 14:09:31 +0800

    2537

---
+++ Testx/2501_2550/2537_CountTheNumberOfGoodSubarraysTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._2501_2550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2501_2550
+{
+    public class _2537_CountTheNumberOfGoodSubarraysTest
+    {
+        _2537_CountTheNumberOfGoodSubarraysAlg alg = new _2537_CountTheNumberOfGoodSubarraysAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [1, 1, 1, 1, 1];
+            int k = 10;
+            long exp = 1;
+            long res = alg.CountGood(nums, k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [3, 1, 4, 3, 2, 2, 4];
+            int k = 2;
+            long exp = 4;
+            long res = alg.CountGood(nums, k);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2501_2550/2537_CountTheNumberOfGoodSubarraysAlg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2501_2550
+{
+    public class _2537_CountTheNumberOfGoodSubarraysAlg
+    {
+        public long CountGood(int[] nums, int k)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            long ans = 0;
+            long cnt = 0;
+            for (int i = 0, j = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    cnt += dict[nums[i]];
+                    dict[nums[i]]++;
+                }
+                else
+                {
+                    dict.Add(nums[i], 1);
+                }
+                if (cnt >= k)
+                {
+                    ans += (nums.Length - i);
+                }
+
+                while (cnt >= k)
+                {
+                    if (dict[nums[j]] > 1)
+                    {
+                        cnt = cnt - (dict[nums[j]] - 1);
+                    }
+                    dict[nums[j]]--;
+                    if (dict[nums[j]] == 0)
+                    {
+                        dict.Remove(nums[j]);
+                    }
+                    j++;
+                    if (cnt >= k)
+                    {
+                        ans += (nums.Length - i);
+                    }
+                }
+
+            }
+
+            return ans;
+        }
+    }
+}
