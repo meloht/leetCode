97a6f907eec29ee7a95bcb694315fab31086da44
hongtao_luo <luohongtao621@hotmail.com>
2025-04-17 09:15:01 +0800

    2176

---
+++ Testx/2151_2200/2176_CountEqualAndDivisiblePairsInAnArrayTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._2101_2150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2151_2200
+{
+    public class _2176_CountEqualAndDivisiblePairsInAnArrayTest
+    {
+        _2176_CountEqualAndDivisiblePairsInAnArrayAlg alg = new _2176_CountEqualAndDivisiblePairsInAnArrayAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [3, 1, 2, 2, 2, 1, 3];
+            int k = 2;
+            int exp = 4;
+            int res= alg.CountPairs(nums, k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int k = 1;
+            int exp = 0;
+            int res = alg.CountPairs(nums, k);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2101_2150/2176_CountEqualAndDivisiblePairsInAnArrayAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2101_2150
+{
+    public class _2176_CountEqualAndDivisiblePairsInAnArrayAlg
+    {
+        public int CountPairs(int[] nums, int k)
+        {
+            int ans = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] == nums[j] && (i * j) % k == 0)
+                    {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
