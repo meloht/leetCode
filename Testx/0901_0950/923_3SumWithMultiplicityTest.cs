2cd64820fa8ccdb6cb621c23849581e155ae6ac5
hongtao_luo <luohongtao621@hotmail.com>
2025-04-18 14:15:35 +0800

    923

---
+++ Testx/0901_0950/923_3SumWithMultiplicityTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _923_3SumWithMultiplicityTest
+    {
+        _923_3SumWithMultiplicityAlg alg = new _923_3SumWithMultiplicityAlg();
+        [Fact]
+        public void Test01()
+        {
+            int[] arr = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
+            int target = 8;
+            int exp = 20;
+            int res = alg.ThreeSumMulti(arr, target);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] arr = [1, 1, 2, 2, 2, 2];
+            int target = 5;
+            int exp = 12;
+            int res = alg.ThreeSumMulti(arr, target);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/923_3SumWithMultiplicityAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _923_3SumWithMultiplicityAlg
+    {
+        public int ThreeSumMulti(int[] arr, int target)
+        {
+            int Mod = 1_000_000_007;
+            int[] dict = new int[101];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                dict[arr[i]]++;
+            }
+            int ans = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int n2 = arr[i];
+                dict[n2]--;
+                for (int j = 0; j < i; j++)
+                {
+                    int n1 = arr[j];
+                    int n3 = target - (n1 + n2);
+                    if (n3 <= 100 && n3 >= 0 && dict[n3] > 0)
+                    {
+                        ans = (ans + dict[n3]) % Mod;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
