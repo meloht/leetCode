f9cf04a3a618d20c729fdce81330e587fd4de191
hongtao_luo <luohongtao621@hotmail.com>
2025-04-18 09:33:58 +0800

    2364

---
+++ Testx/2351_2400/2364_CountNumberOfBadPairsTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._2351_2400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2351_2400
+{
+    public class _2364_CountNumberOfBadPairsTest
+    {
+        _2364_CountNumberOfBadPairsAlg alg = new _2364_CountNumberOfBadPairsAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [4, 1, 3, 3];
+            long exp = 5;
+            long res = alg.CountBadPairs(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [1, 2, 3, 4, 5];
+            long exp = 0;
+            long res = alg.CountBadPairs(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2351_2400/2364_CountNumberOfBadPairsAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2351_2400
+{
+    public class _2364_CountNumberOfBadPairsAlg
+    {
+        public long CountBadPairs(int[] nums)
+        {
+            int n = nums.Length;
+            long ans = (long)n * (n - 1) / 2;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int val=nums[i]-i;
+                if (dict.ContainsKey(val))
+                {
+                    ans -= dict[val];
+                    dict[val]++;
+                }
+                else
+                {
+                    dict.Add(val, 1);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
