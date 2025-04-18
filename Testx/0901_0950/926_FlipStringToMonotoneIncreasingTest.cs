b8e6adb556f456a2b99927f23a5e8d20f45c23cc
hongtao_luo <luohongtao621@hotmail.com>
2025-04-18 17:31:21 +0800

    926

---
+++ Testx/0901_0950/926_FlipStringToMonotoneIncreasingTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0901_0950;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _926_FlipStringToMonotoneIncreasingTest
+    {
+        _926_FlipStringToMonotoneIncreasingAlg alg = new _926_FlipStringToMonotoneIncreasingAlg();
+        [Fact]
+        public void Test01()
+        {
+            string s = "00110";
+            int exp = 1;
+            int res = alg.MinFlipsMonoIncr(s);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            string s = "010110";
+            int exp = 2;
+            int res = alg.MinFlipsMonoIncr(s);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            string s = "00011000";
+            int exp = 2;
+            int res = alg.MinFlipsMonoIncr(s);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/926_FlipStringToMonotoneIncreasingAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _926_FlipStringToMonotoneIncreasingAlg
+    {
+        public int MinFlipsMonoIncr(string s)
+        {
+            int n = s.Length;
+            int dp0 = 0, dp1 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char c = s[i];
+                int dp0New = dp0, dp1New = Math.Min(dp0, dp1);
+                if (c == '1')
+                {
+                    dp0New++;
+                }
+                else
+                {
+                    dp1New++;
+                }
+                dp0 = dp0New;
+                dp1 = dp1New;
+            }
+            return Math.Min(dp0, dp1);
+
+        }
+    }
+}
