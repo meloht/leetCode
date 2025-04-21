1fea88ce2772f01dfb14d0914e8f3486cf391081
hongtao_luo <luohongtao621@hotmail.com>
2025-04-21 12:46:05 +0800

    2145

---
+++ Testx/2151_2200/2145_CountTheHiddenSequencesTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._2151_2200;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2151_2200
+{
+    public class _2145_CountTheHiddenSequencesTest
+    {
+        _2145_CountTheHiddenSequencesAlg alg = new _2145_CountTheHiddenSequencesAlg();
+        [Fact]
+        public void Test01()
+        {
+            int[] differences = [1, -3, 4];
+            int lower = 1, upper = 6;
+            int exp = 2;
+            int res = alg.NumberOfArrays(differences, lower, upper);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] differences = [3, -4, 5, 1, -2];
+            int lower = -4, upper = 5;
+            int exp = 4;
+            int res = alg.NumberOfArrays(differences, lower, upper);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] differences = [4, -7, 2];
+            int lower = 3, upper = 6;
+            int exp = 0;
+            int res = alg.NumberOfArrays(differences, lower, upper);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2151_2200/2145_CountTheHiddenSequencesAlg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2151_2200
+{
+    public class _2145_CountTheHiddenSequencesAlg
+    {
+        public int NumberOfArrays(int[] differences, int lower, int upper)
+        {
+            long s = 0;
+            long mins = 0;
+            long maxs = 0;
+            foreach (var item in differences)
+            {
+                s += item;
+                mins = Math.Min(s, mins);
+                maxs = Math.Max(s, maxs);
+            }
+            return (int)Math.Max(upper - lower - maxs + mins + 1, 0);
+        }
+    }
+}
