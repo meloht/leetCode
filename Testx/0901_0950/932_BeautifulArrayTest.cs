3d46f7d99a04d9edcfac2a0db486e8866e3b1920
hongtao_luo <luohongtao621@hotmail.com>
2025-04-22 17:46:26 +0800

    932

---
+++ Testx/0901_0950/932_BeautifulArrayTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _932_BeautifulArrayTest
+    {
+        _932_BeautifulArrayAlg alg = new _932_BeautifulArrayAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int n = 4;
+            int[] exp = [2, 1, 4, 3];
+            int[] res = alg.BeautifulArray(n);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int n = 5;
+            int[] exp = [3, 1, 2, 5, 4];
+            int[] res = alg.BeautifulArray(n);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/932_BeautifulArrayAlg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _932_BeautifulArrayAlg
+    {
+        public int[] BeautifulArray(int n)
+        {
+
+            
+
+            return null;
+        }
+    }
+}
