32d1292bef2c2ab2561372743ba5c18450560782
hongtao_luo <luohongtao621@hotmail.com>
2025-04-23 09:27:19 +0800

    1399

---
+++ Testx/1351_1400/1399_CountLargestGroupTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._1351_1400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1351_1400
+{
+    public class _1399_CountLargestGroupTest
+    {
+        _1399_CountLargestGroupAlg alg = new _1399_CountLargestGroupAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int n = 13;
+            int exp = 4;
+            int res = alg.CountLargestGroup(n);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int n = 2;
+            int exp = 2;
+            int res = alg.CountLargestGroup(n);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int n = 15;
+            int exp = 6;
+            int res = alg.CountLargestGroup(n);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int n = 24;
+            int exp = 5;
+            int res = alg.CountLargestGroup(n);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1351_1400/1399_CountLargestGroupAlg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1351_1400
+{
+    public class _1399_CountLargestGroupAlg
+    {
+        public int CountLargestGroup(int n)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int max = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int num = i;
+                int sum = 0;
+
+                while (num > 0)
+                {
+                    int d = num % 10;
+                    sum += d;
+                    num /= 10;
+                }
+                if (dict.ContainsKey(sum))
+                {
+                    dict[sum]++;
+                }
+                else
+                {
+                    dict.Add(sum, 1);
+                }
+                max=Math.Max(max, dict[sum]);
+            }
+
+            int ans = 0;
+
+            foreach (var item in dict)
+            {
+                if (item.Value == max)
+                {
+                    ans++;
+                }
+            }
+
+            return ans; 
+        }
+    }
+}
