883506487b7099496862b919296d61a67a069585
hongtao_luo <luohongtao621@hotmail.com>
2025-04-24 16:21:40 +0800

    937

---
+++ Testx/0901_0950/937_ReorderDataInLogFilesTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _937_ReorderDataInLogFilesTest
+    {
+        _937_ReorderDataInLogFilesAlg alg = new _937_ReorderDataInLogFilesAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            string[] logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"];
+            string[] exp = ["let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6"];
+            string[] res = alg.ReorderLogFiles(logs);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            string[] logs = ["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"];
+            string[] exp = ["g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"];
+            string[] res = alg.ReorderLogFiles(logs);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            string[] logs = ["1 n u", "r 527", "j 893", "6 14", "6 82"];
+            string[] exp = ["1 n u", "r 527", "j 893", "6 14", "6 82"];
+            string[] res = alg.ReorderLogFiles(logs);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/937_ReorderDataInLogFilesAlg.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _937_ReorderDataInLogFilesAlg
+    {
+        public string[] ReorderLogFiles(string[] logs)
+        {
+            List<string> listNum = new List<string>();
+            List<string> listStr = new List<string>();
+
+            foreach (var item in logs)
+            {
+                if (char.IsDigit(item[item.Length - 1]))
+                {
+                    listNum.Add(item);
+                }
+                else
+                {
+                    listStr.Add(item);
+                }
+            }
+
+            listStr.Sort(CompareFun);
+
+            listStr.AddRange(listNum);
+
+            return listStr.ToArray();
+        }
+
+        public int CompareFun(string s1, string s2)
+        {
+
+            StringBuilder sb1 = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+
+            int i = 0, j = 0;
+          
+            for (; i < s1.Length; i++)
+            {
+                if (s1[i] == ' ')
+                {
+                    break;
+                }
+
+                sb1.Append(s1[i]);
+            }
+            for (; j < s2.Length; j++)
+            {
+                if (s2[j] == ' ')
+                {
+                    break;
+                }
+                sb2.Append(s2[j]);
+            }
+           
+            for (; i < s1.Length || j < s2.Length; i++, j++)
+            {
+                if (i < s1.Length && j < s2.Length)
+                {
+                    if (s1[i] != s2[j])
+                    {
+                        return s1[i].CompareTo(s2[j]);
+                    }
+                }
+                else if (i < s1.Length)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            string ss1 = sb1.ToString();
+            string ss2 = sb2.ToString();
+            return ss1.CompareTo(ss2);
+        }
+
+
+
+
+
+
+    }
+}
