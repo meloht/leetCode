58b82bb02dfd1a7a4af66fdc9677636f11865dd8
hongtao_luo <luohongtao621@hotmail.com>
2025-04-24 11:43:55 +0800

    2799

---
+++ Testx/2751_2800/2799_CountCompleteSubarraysInAnArrayTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._2751_2800;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2751_2800
+{
+    public class _2799_CountCompleteSubarraysInAnArrayTest
+    {
+        _2799_CountCompleteSubarraysInAnArrayAlg alg = new _2799_CountCompleteSubarraysInAnArrayAlg();
+        [Fact]
+        public void Test1()
+        {
+            int[] nums = [1, 3, 1, 2, 2];
+            int exp = 4;
+            int res = alg.CountCompleteSubarrays(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] nums = [5, 5, 5, 5];
+            int exp = 10;
+            int res = alg.CountCompleteSubarrays(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] nums = [1898, 370, 822, 1659, 1360, 128, 370, 360, 261, 1898];
+            int exp = 4;
+            int res = alg.CountCompleteSubarrays(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2751_2800/2799_CountCompleteSubarraysInAnArrayAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2751_2800
+{
+    public class _2799_CountCompleteSubarraysInAnArrayAlg
+    {
+        public int CountCompleteSubarrays(int[] nums)
+        {
+            int cnt = nums.Distinct().Count();
+            int ans = 0;
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0, j = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    dict[nums[i]]++;
+                }
+                else
+                {
+                    dict.Add(nums[i], 1);
+                }
+                while (dict.Count == cnt)
+                {
+                    ans += (nums.Length - i);
+                    dict[nums[j]]--;
+                    if (dict[nums[j]] <= 0)
+                    {
+                        dict.Remove(nums[j]);
+                    }
+                    j++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
