c333f6238f72f9cf4f4b9575567d7c0ee40c43cd
luohongtao <luohongtao621@hotmail.com>
2025-04-26 12:24:06 +0800

    2563

---
+++ Testx/2550_2600/2563_CountTheNumberOfFairPairsTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._2551_2600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2550_2600
+{
+    public class _2563_CountTheNumberOfFairPairsTest
+    {
+        _2563_CountTheNumberOfFairPairsAlg alg = new _2563_CountTheNumberOfFairPairsAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [0, 1, 7, 4, 4, 5];
+            int lower = 3, upper = 6;
+            long exp = 6;
+            long res = alg.CountFairPairs(nums, lower, upper);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [1, 7, 9, 2, 5];
+            int lower = 11, upper = 11;
+            long exp = 1;
+            long res = alg.CountFairPairs(nums, lower, upper);
+            Assert.Equal(exp, res);
+        }
+    }
+}
