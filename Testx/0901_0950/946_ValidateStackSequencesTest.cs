cec481284637a2ae71aafe0097544708c18d5907
hongtao_luo <luohongtao621@hotmail.com>
2025-04-28 14:46:58 +0800

    946

---
+++ Testx/0901_0950/946_ValidateStackSequencesTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _946_ValidateStackSequencesTest
+    {
+        _946_ValidateStackSequencesAlg alg = new _946_ValidateStackSequencesAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] pushed = [1, 2, 3, 4, 5], popped = [4, 5, 3, 2, 1];
+            bool exp = true;
+            bool res = alg.ValidateStackSequences(pushed, popped);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] pushed = [1, 2, 3, 4, 5], popped = [4, 3, 5, 1, 2];
+            bool exp = false;
+            bool res = alg.ValidateStackSequences(pushed, popped);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/946_ValidateStackSequencesAlg.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _946_ValidateStackSequencesAlg
+    {
+        public bool ValidateStackSequences1(int[] pushed, int[] popped)
+        {
+            Stack<int> st = [];
+
+            for (int i = 0, j = 0; i < pushed.Length || j < popped.Length;)
+            {
+                if (i < pushed.Length && j < popped.Length)
+                {
+                    if (pushed[i] == popped[j])
+                    {
+                        i++;
+                        j++;
+                    }
+                    else if (st.Count > 0 && st.Peek() == popped[j])
+                    {
+                        st.Pop();
+                        j++;
+                    }
+                    else
+                    {
+                        st.Push(pushed[i]);
+                        i++;
+                    }
+                }
+                else if (j < popped.Length)
+                {
+                    if (st.Count > 0 && st.Peek() == popped[j])
+                    {
+                        st.Pop();
+                        j++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+            return st.Count == 0;
+        }
+
+        public bool ValidateStackSequences(int[] pushed, int[] popped)
+        {
+            Stack<int> st = [];
+
+            for (int i = 0, j = 0; i < pushed.Length; i++)
+            {
+                st.Push(pushed[i]);
+                while (st.Count > 0 && st.Peek() == popped[j])
+                {
+                    st.Pop();
+                    j++;
+                }
+
+            }
+            return st.Count == 0;
+        }
+    }
+}
