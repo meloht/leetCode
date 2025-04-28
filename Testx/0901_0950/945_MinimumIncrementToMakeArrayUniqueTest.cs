bd2267f53536689a08551b27284a0f3794d3153c
hongtao_luo <luohongtao621@hotmail.com>
2025-04-28 10:49:07 +0800

    945

---
+++ Testx/0901_0950/945_MinimumIncrementToMakeArrayUniqueTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _945_MinimumIncrementToMakeArrayUniqueTest
+    {
+        _945_MinimumIncrementToMakeArrayUniqueAlg alg = new _945_MinimumIncrementToMakeArrayUniqueAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [1, 2, 2];
+            int exp = 1;
+            int res = alg.MinIncrementForUnique(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [3, 2, 1, 2, 1, 7];
+            int exp = 6;
+            int res = alg.MinIncrementForUnique(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/945_MinimumIncrementToMakeArrayUniqueAlg.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _945_MinimumIncrementToMakeArrayUniqueAlg
+    {
+        public int MinIncrementForUnique1(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int ans = 0;
+            int last = int.MinValue;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (last >= nums[i])
+                {
+                    int diff = last - nums[i] + 1;
+                    nums[i] += diff;
+                    last = nums[i];
+                    ans += diff;
+
+                }
+                if (nums[i - 1] == nums[i])
+                {
+                    nums[i]++;
+                    last = nums[i];
+                    ans++;
+                }
+            }
+            return ans;
+        }
+
+        public int MinIncrementForUnique(int[] nums)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            foreach (int item in nums)
+            {
+                min = Math.Min(min, item);
+                max = Math.Max(max, item);
+            }
+            int[] dict = new int[max + 1];
+            foreach (int item in nums)
+            {
+                dict[item]++; ;
+            }
+            int ans = 0;
+            int last = -1;
+            for (int i = min; i <= max; i++)
+            {
+                if (dict[i] == 0)
+                {
+                    continue;
+                }
+
+                if (last >= i)
+                {
+                    int diff = last - i + 1;
+                    ans += ((diff + diff + dict[i] - 1) * dict[i] / 2);
+                    last = i + diff + dict[i] - 1;
+                }
+                else
+                {
+                    ans += (dict[i] - 1) * dict[i] / 2;
+                    last = i + dict[i] - 1;
+                }
+            }
+            return ans;
+        }
+    }
+}
