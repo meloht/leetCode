f624637dd637b813c5fba7ac79f679b1f1153013
hongtao_luo <luohongtao621@hotmail.com>
2025-04-28 15:55:49 +0800

    948

---
+++ Testx/0901_0950/948_BagOfTokensTest.cs
@@ -0,0 +1,54 @@
+﻿using leetCode._0901_0950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0901_0950
+{
+    public class _948_BagOfTokensTest
+    {
+        _948_BagOfTokensAlg alg = new _948_BagOfTokensAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] tokens = [100];
+            int power = 50;
+            int exp = 0;
+            int res = alg.BagOfTokensScore(tokens, power);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] tokens = [200, 100];
+            int power = 150;
+            int exp = 1;
+            int res = alg.BagOfTokensScore(tokens, power);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] tokens = [100, 200, 300, 400];
+            int power = 200;
+            int exp = 2;
+            int res = alg.BagOfTokensScore(tokens, power);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int[] tokens = [71, 55, 82];
+            int power = 54;
+            int exp = 0;
+            int res = alg.BagOfTokensScore(tokens, power);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0901_0950/948_BagOfTokensAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0901_0950
+{
+    public class _948_BagOfTokensAlg
+    {
+        public int BagOfTokensScore(int[] tokens, int power)
+        {
+            int ans = 0;
+            Array.Sort(tokens);
+            int num = 0;
+            for (int i = 0, j = tokens.Length - 1; i <= j;)
+            {
+                if (power >= tokens[i])
+                {
+                    num++;
+                    power -= tokens[i];
+                    ans = Math.Max(ans, num);
+                    i++;
+                }
+                else if (num > 0)
+                {
+                    power += tokens[j];
+                    j--;
+                    num--;
+
+                }
+                else
+                {
+                    return ans;
+                }
+            }
+
+            return ans;
+        }
+
+
+
+
+    }
+}
