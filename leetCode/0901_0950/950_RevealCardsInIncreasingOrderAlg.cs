40a3cbf30e0c885a9305c916e8b756b13a6bfa66
hongtao_luo <luohongtao621@hotmail.com>
2025-04-29 18:04:25 +0800

    950

--- leetCode/0901_0950/950_RevealCardsInIncreasingOrderAlg.cs
+++ leetCode/0901_0950/950_RevealCardsInIncreasingOrderAlg.cs
@@ -10,10 +10,37 @@ namespace leetCode._0901_0950
     {
         public int[] DeckRevealedIncreasing(int[] deck)
         {
+            int[] arr = new int[deck.Length];
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < deck.Length; i++)
+            {
+                arr[i] = i;
+                queue.Enqueue(i);
+            }
 
+            Array.Sort(arr, (x, y) => deck[x] - deck[y]);
 
+            int[][] order = new int[deck.Length][];
+            int idx = 0;
+            while (queue.Count > 0)
+            {
+                int index = queue.Dequeue();
+                order[idx] = [index, idx];
+                idx++;
+                if (queue.Count > 0)
+                {
+                    var item = queue.Dequeue();
+                    queue.Enqueue(item);
+                }
+            }
+            Array.Sort(order, (x, y) => arr[x[1]] - arr[y[1]]);
+            int[] ans = new int[deck.Length];
+            for (int i = 0; i < deck.Length; i++)
+            {
+                ans[i] = deck[order[i][0]];
+            }
 
-            return null;
+            return ans;
         }
     }
 }
