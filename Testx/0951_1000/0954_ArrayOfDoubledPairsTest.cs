dd6cf758c11ab87866c7846b21146a17150759f2
hongtao_luo <luohongtao621@hotmail.com>
2025-04-30 14:05:42 +0800

    954

---
+++ Testx/0951_1000/0954_ArrayOfDoubledPairsTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0951_1000
+{
+    public class _0954_ArrayOfDoubledPairsTest
+    {
+        _0954_ArrayOfDoubledPairsAlg alg = new _0954_ArrayOfDoubledPairsAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] arr = [3, 1, 3, 6];
+            bool exp = false;
+            bool res = alg.CanReorderDoubled(arr);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] arr = [2, 1, 2, 6];
+            bool exp = false;
+            bool res = alg.CanReorderDoubled(arr);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] arr = [4, -2, 2, -4];
+            bool exp = true;
+            bool res = alg.CanReorderDoubled(arr);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int[] arr = [2, 4, 0, 0, 8, 1];
+            bool exp = true;
+            bool res = alg.CanReorderDoubled(arr);
+            Assert.Equal(exp, res);
+        }
+
+
+        [Fact]
+        public void Test05()
+        {
+            int[] arr = [1, 2, 4, 16, 8, 4];
+            bool exp = false;
+            bool res = alg.CanReorderDoubled(arr);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0954_ArrayOfDoubledPairsAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0954_ArrayOfDoubledPairsAlg
+    {
+        public bool CanReorderDoubled(int[] arr)
+        {
+            Dictionary<int, int> dict = [];
+            foreach (int i in arr)
+            {
+                if (dict.ContainsKey(i))
+                {
+                    dict[i]++;
+                }
+                else
+                {
+                    dict.Add(i, 1);
+                }
+            }
+            int cnt = 0;
+            Array.Sort(arr);
+            foreach (var item in arr)
+            {
+                int nn = item * 2;
+               
+                if (dict.ContainsKey(nn))
+                {
+                    cnt++;
+
+                    if (dict.ContainsKey(item))
+                    {
+                        dict[item]--;
+                        if (dict[item] <= 0)
+                        {
+                            dict.Remove(item);
+                        }
+                    }
+                    if (dict.ContainsKey(nn))
+                    {
+                        dict[nn]--;
+                        if (dict[nn] <= 0)
+                        {
+                            dict.Remove(nn);
+                        }
+                    }
+
+                }
+            }
+            if (cnt >= arr.Length / 2)
+                return true;
+
+            return false;
+        }
+    }
+}
