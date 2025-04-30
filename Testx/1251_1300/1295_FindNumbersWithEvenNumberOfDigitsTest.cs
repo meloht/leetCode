711bee6038fe9a57c9a3b80e28013cfd1945c076
hongtao_luo <luohongtao621@hotmail.com>
2025-04-30 11:36:57 +0800

    1295

---
+++ Testx/1251_1300/1295_FindNumbersWithEvenNumberOfDigitsTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._1251_1300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1251_1300
+{
+    public class _1295_FindNumbersWithEvenNumberOfDigitsTest
+    {
+        _1295_FindNumbersWithEvenNumberOfDigitsAlg alg = new _1295_FindNumbersWithEvenNumberOfDigitsAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [12, 345, 2, 6, 7896];
+            int exp = 2;
+            int res = alg.FindNumbers(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [555, 901, 482, 1771];
+            int exp = 1;
+            int res = alg.FindNumbers(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1251_1300/1295_FindNumbersWithEvenNumberOfDigitsAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1251_1300
+{
+    public class _1295_FindNumbersWithEvenNumberOfDigitsAlg
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int ans = 0;
+            foreach (var item in nums)
+            {
+                int n = item;
+                int cnt = 0;
+                while (n > 0)
+                {
+                    n /= 10;
+                    cnt++;
+                }
+                if ((cnt & 1) == 0)
+                {
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
