37fd1fafc25bc622ef0475968d8d06b4cf066bca
luohongtao <luohongtao621@hotmail.com>
2025-05-01 12:23:46 +0800

    2071

---
+++ Testx/2051_2100/2071_MaximumNumberOfTasksYouCanAssignTest.cs
@@ -0,0 +1,56 @@
+﻿using leetCode._2051_2100;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2051_2100
+{
+    public class _2071_MaximumNumberOfTasksYouCanAssignTest
+    {
+        _2071_MaximumNumberOfTasksYouCanAssignAlg alg = new _2071_MaximumNumberOfTasksYouCanAssignAlg();
+        [Fact]
+        public void Test01()
+        {
+            int[] tasks = [3, 2, 1], workers = [0, 3, 3];
+            int pills = 1, strength = 1;
+            int exp = 3;
+            int res = alg.MaxTaskAssign(tasks, workers, pills, strength);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] tasks = [5, 4], workers = [0, 0, 0];
+            int pills = 1, strength = 5;
+            int exp = 1;
+            int res = alg.MaxTaskAssign(tasks, workers, pills, strength);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] tasks = [10, 15, 30], workers = [0, 10, 10, 10, 10];
+            int pills = 3, strength = 10;
+            int exp = 2;
+            int res = alg.MaxTaskAssign(tasks, workers, pills, strength);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int[] tasks = [5, 9, 8, 5, 9], workers = [1, 6, 4, 2, 6];
+            int pills = 1, strength = 5;
+            int exp = 3;
+            int res = alg.MaxTaskAssign(tasks, workers, pills, strength);
+            Assert.Equal(exp, res);
+        }
+
+      
+    }
+}
---
+++ leetCode/2051_2100/2071_MaximumNumberOfTasksYouCanAssignAlg.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2051_2100
+{
+    public class _2071_MaximumNumberOfTasksYouCanAssignAlg
+    {
+        public int MaxTaskAssign1(int[] tasks, int[] workers, int pills, int strength)
+        {
+            Array.Sort(tasks);
+            Array.Sort(workers);
+
+            int left = 0;
+            int mn = Math.Min(tasks.Length, workers.Length);
+
+            int[] validTasks = new int[mn];
+            int right = mn + 1;
+
+            while (left + 1 < right)
+            {
+                int mid = (left + right) >> 1;
+                if (CheckArr(tasks, workers, pills, strength, mid, validTasks))
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            return left;
+        }
+
+        public int MaxTaskAssign(int[] tasks, int[] workers, int pills, int strength)
+        {
+            Array.Sort(tasks);
+            Array.Sort(workers);
+
+            int left = 1;
+            int mn = Math.Min(tasks.Length, workers.Length);
+
+            int[] validTasks = new int[mn];
+            int right = mn;
+            int ans = 0;
+            while (left <= right)
+            {
+                int mid = (left + right) >> 1;
+                if (CheckArr(tasks, workers, pills, strength, mid, validTasks))
+                {
+                    ans = mid;
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return ans;
+        }
+
+
+
+
+        public bool CheckArr(int[] task, int[] workers, int pills, int strength, int k, int[] validTasks)
+        {
+            int head = 0;
+            int tail = 0;
+            int i = 0;
+            for (int j = workers.Length - k; j < workers.Length; j++)
+            {
+                int w = workers[j];
+                while (i < k && task[i] <= w + strength)
+                {
+                    validTasks[tail++] = task[i];
+                    i++;
+                }
+                if (head == tail)
+                {
+                    return false;
+                }
+                if (w >= validTasks[head])
+                {
+                    head++;
+                    continue;
+                }
+                if (pills == 0)
+                    return false;
+
+                pills--;
+                tail--;
+            }
+
+            return true;
+        }
+
+    }
+}
