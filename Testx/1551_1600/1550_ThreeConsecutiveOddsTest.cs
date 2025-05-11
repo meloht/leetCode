86d7373bbef8a675dd3d4dc692688b563dcac42f
luohongtao <luohongtao621@hotmail.com>
2025-05-11 11:39:38 +0800

    1550

---
+++ Testx/1551_1600/1550_ThreeConsecutiveOddsTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._1501_1550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1551_1600
+{
+    public class _1550_ThreeConsecutiveOddsTest
+    {
+        _1550_ThreeConsecutiveOddsAlg alg = new _1550_ThreeConsecutiveOddsAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] arr = [2, 6, 4, 1];
+            bool exp = false;
+            bool res = alg.ThreeConsecutiveOdds(arr);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] arr = [1, 2, 34, 3, 4, 5, 7, 23, 12];
+            bool exp = true;
+            bool res = alg.ThreeConsecutiveOdds(arr);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1501_1550/1550_ThreeConsecutiveOddsAlg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1501_1550
+{
+    public class _1550_ThreeConsecutiveOddsAlg
+    {
+        public bool ThreeConsecutiveOdds(int[] arr)
+        {
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                if (arr[i - 1] % 2 == 1 && arr[i] % 2 == 1 && arr[i + 1] % 2 == 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
