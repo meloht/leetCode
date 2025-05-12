edb058aac757502ad968ee9f11333f5b09a03620
hongtao_luo <luohongtao621@hotmail.com>
2025-05-12 17:33:37 +0800

    958

---
+++ Testx/0951_1000/0958_CheckCompletenessOfABinaryTreeTest.cs
@@ -0,0 +1,64 @@
+﻿using leetCode._0951_1000;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0951_1000
+{
+    public class _0958_CheckCompletenessOfABinaryTreeTest
+    {
+        _0958_CheckCompletenessOfABinaryTreeAlg alg = new _0958_CheckCompletenessOfABinaryTreeAlg();
+        [Fact]
+        public void Test01()
+        {
+            int?[] root = [1, 2, 3, 4, 5, 6];
+            bool exp = true;
+            TreeNode node = TreeNode.BuildTree(root);
+            bool res = alg.IsCompleteTree(node);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int?[] root = [1, 2, 3, 4, 5, null, 7];
+            bool exp = false;
+            TreeNode node = TreeNode.BuildTree(root);
+            bool res = alg.IsCompleteTree(node);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int?[] root = [1, 2, 3, 5];
+            bool exp = true;
+            TreeNode node = TreeNode.BuildTree(root);
+            bool res = alg.IsCompleteTree(node);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int?[] root = [1, 2, 3, null, null, 7, 8];
+            bool exp = false;
+            TreeNode node = TreeNode.BuildTree(root);
+            bool res = alg.IsCompleteTree(node);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test05()
+        {
+            int?[] root = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, null, null, 15];
+            bool exp = false;
+            TreeNode node = TreeNode.BuildTree(root);
+            bool res = alg.IsCompleteTree(node);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0958_CheckCompletenessOfABinaryTreeAlg.cs
@@ -0,0 +1,55 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0958_CheckCompletenessOfABinaryTreeAlg
+    {
+        public bool IsCompleteTree(TreeNode root)
+        {
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            bool bl = false;
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+               
+                for (int i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+                    var left = node.left;
+                    var right = node.right;
+                    if (bl && (left != null || right != null))
+                    {
+                        return false;
+                    }
+                    if (left != null && right != null)
+                    {
+                        queue.Enqueue(left);
+                        queue.Enqueue(right);
+                    }
+                    else if (left != null && right == null)
+                    {
+                        queue.Enqueue(left);
+                        bl = true;
+
+                    }
+                    else if (left == null && right != null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        bl = true;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
