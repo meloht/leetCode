7ad19c1753d129bf900a050bd4eadbaf2de2db41
hongtao_luo <luohongtao621@hotmail.com>
2025-05-15 16:56:21 +0800

    962

---
+++ Testx/0951_1000/0962_MaximumWidthRampTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0951_1000
+{
+    public class _0962_MaximumWidthRampTest
+    {
+        _0962_MaximumWidthRampAlg alg = new _0962_MaximumWidthRampAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [6, 0, 8, 2, 1, 5];
+            int exp = 4;
+            int res = alg.MaxWidthRamp(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [9, 8, 1, 0, 1, 9, 4, 0, 4, 1];
+            int exp = 7;
+            int res = alg.MaxWidthRamp(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] nums = [2, 2, 1];
+            int exp = 1;
+            int res = alg.MaxWidthRamp(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0962_MaximumWidthRampAlg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0962_MaximumWidthRampAlg
+    {
+        public int MaxWidthRamp1(int[] nums)
+        {
+            int n = nums.Length;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = i;
+            }
+            Array.Sort(arr, (x, y) => { if (nums[x] != nums[y]) return nums[x] - nums[y]; else return x - y; });
+            int ans = 0;
+            int min = n;
+
+            foreach (var item in arr)
+            {
+                ans = Math.Max(ans, item - min);
+                min = Math.Min(min, item);
+            }
+            return ans;
+        }
+
+        public int MaxWidthRamp(int[] nums)
+        {
+            int n = nums.Length;
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(i);
+                }
+                else if (nums[i] < nums[stack.Peek()])
+                {
+                    stack.Push(i);
+                }
+            }
+
+            int ans = 0;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (stack.Count == 0)
+                    break;
+                while (stack.Count > 0 && nums[i] >= nums[stack.Peek()])
+                {
+                    ans = Math.Max(ans, i - stack.Pop());
+                }
+            }
+            return ans;
+        }
+    }
+}
