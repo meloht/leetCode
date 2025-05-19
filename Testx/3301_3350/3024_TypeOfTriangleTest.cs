1f97d06f57309b41a74c3abed05e0350e886aa15
hongtao_luo <luohongtao621@hotmail.com>
2025-05-19 11:48:53 +0800

    3024

---
+++ Testx/3301_3350/3024_TypeOfTriangleTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._3001_3050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3301_3350
+{
+    public class _3024_TypeOfTriangleTest
+    {
+        _3024_TypeOfTriangleAlg alg = new _3024_TypeOfTriangleAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [3, 3, 3];
+            string exp = "equilateral";
+            string res = alg.TriangleType(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [3, 4, 5];
+            string exp = "scalene";
+            string res = alg.TriangleType(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3001_3050/3024_TypeOfTriangleAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3001_3050
+{
+    public class _3024_TypeOfTriangleAlg
+    {
+        public string TriangleType(int[] nums)
+        {
+            Array.Sort(nums);
+            if (nums[0] + nums[1] <= nums[2])
+            {
+                return "none";
+            }
+            int cnt = nums.Distinct().Count();
+            if (cnt == 1)
+                return "equilateral";
+            else if (cnt == 2)
+                return "isosceles";
+            return "scalene";
+        }
+    }
+}
