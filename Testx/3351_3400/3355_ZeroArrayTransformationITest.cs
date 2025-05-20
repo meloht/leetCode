a6f52a400c38e60da8c1b96685b68c34b7c7255c
luohongtao <luohongtao621@hotmail.com>
2025-05-20 19:30:33 +0800

    3355

---
+++ Testx/3351_3400/3355_ZeroArrayTransformationITest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._3351_3400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3351_3400
+{
+    public class _3355_ZeroArrayTransformationITest
+    {
+        _3355_ZeroArrayTransformationIAlg alg = new _3355_ZeroArrayTransformationIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [1, 0, 1];
+            int[][] queries = [[0, 2]];
+            bool exp = true;
+            bool res = alg.IsZeroArray(nums, queries);
+            Assert.Equal(exp,res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [4, 3, 2, 1];
+            int[][] queries = [[1, 3], [0, 2]];
+            bool exp = false;
+            bool res = alg.IsZeroArray(nums, queries);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3351_3400/3355_ZeroArrayTransformationIAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3351_3400
+{
+    public class _3355_ZeroArrayTransformationIAlg
+    {
+        public bool IsZeroArray(int[] nums, int[][] queries)
+        {
+            int n = nums.Length;
+            int[] diff = new int[n + 1];
+
+            foreach (var item in queries)
+            {
+                diff[item[0]]++;
+                diff[item[1] + 1]--;
+            }
+            int[] counts = new int[n + 1];
+            int current = 0;
+            for (int i = 0; i < counts.Length; i++)
+            {
+                current += diff[i];
+                counts[i] = current;
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                if (counts[i] < nums[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
