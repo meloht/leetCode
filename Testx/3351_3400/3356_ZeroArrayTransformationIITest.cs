71e35eb690b3c7ef7dc41babaa1a0374132cf8de
luohongtao <luohongtao621@hotmail.com>
2025-05-21 19:18:26 +0800

    3356

---
+++ Testx/3351_3400/3356_ZeroArrayTransformationIITest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3351_3400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3351_3400
+{
+    public class _3356_ZeroArrayTransformationIITest
+    {
+        _3356_ZeroArrayTransformationIIAlg alg = new _3356_ZeroArrayTransformationIIAlg();
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [2, 0, 2];
+            int[][] queries = [[0, 2, 1], [0, 2, 1], [1, 1, 3]];
+            int exp = 2;
+            int res = alg.MinZeroArray(nums, queries);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [4, 3, 2, 1];
+            int[][] queries = [[1, 3, 2], [0, 2, 1]];
+            int exp = -1;
+            int res = alg.MinZeroArray(nums, queries);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3351_3400/3356_ZeroArrayTransformationIIAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3351_3400
+{
+    public class _3356_ZeroArrayTransformationIIAlg
+    {
+        public int MinZeroArray(int[] nums, int[][] queries)
+        {
+            int n = nums.Length;
+            int[] deltaArray = new int[n + 1];
+            int operations = 0;
+            int k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = nums[i];
+                operations += deltaArray[i];
+                while (k < queries.Length && operations < num)
+                {
+                    int left = queries[k][0];
+                    int right = queries[k][1];
+                    int value = queries[k][2];
+                    deltaArray[left] += value;
+                    deltaArray[right + 1] -= value;
+                    if (left <= i && i <= right)
+                    {
+                        operations += value;
+                    }
+                    k++;
+                }
+                if (operations < num)
+                {
+                    return -1;
+                }
+            }
+            return k;
+        }
+
+
+    }
+}
