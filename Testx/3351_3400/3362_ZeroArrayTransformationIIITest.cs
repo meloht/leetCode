b2614cc3f2e6960b7c07d926afe75327dd4d512f
hongtao_luo <luohongtao621@hotmail.com>
2025-05-22 15:21:19 +0800

    3362

---
+++ Testx/3351_3400/3362_ZeroArrayTransformationIIITest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._3351_3400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3351_3400
+{
+    public class _3362_ZeroArrayTransformationIIITest
+    {
+        _3362_ZeroArrayTransformationIIIAlg alg = new _3362_ZeroArrayTransformationIIIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [2, 0, 2];
+            int[][] queries = [[0, 2], [0, 2], [1, 1]];
+            int exp = 1;
+            int res = alg.MaxRemoval(nums, queries);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [1, 1, 1, 1];
+            int[][] queries = [[1, 3], [0, 2], [1, 3], [1, 2]];
+            int exp = 2;
+            int res = alg.MaxRemoval(nums, queries);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int[][] queries = [[0, 3]];
+            int exp = -1;
+            int res = alg.MaxRemoval(nums, queries);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3351_3400/3362_ZeroArrayTransformationIIIAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._3351_3400
+{
+    public class _3362_ZeroArrayTransformationIIIAlg
+    {
+        public int MaxRemoval(int[] nums, int[][] queries)
+        {
+            Array.Sort(queries, (a, b)=>a[0] - b[0]);
+            PriorityQueue<int,int> pq = new PriorityQueue<int,int>(Comparer<int>.Create((x,y)=>y-x));
+            int n = nums.Length;
+            int[] diff = new int[n + 1];
+            int sumD = 0;
+            int j = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sumD += diff[i];
+                // 维护左端点 <= i 的区间
+                while (j < queries.Length && queries[j][0] <= i)
+                {
+                    pq.Enqueue(queries[j][1], queries[j][1]);
+                    j++;
+                }
+                // 选择右端点最大的区间
+                while (sumD < nums[i] && pq.Count>0 && pq.Peek() >= i)
+                {
+                    sumD++;
+                    diff[pq.Dequeue() + 1]--;
+                }
+                if (sumD < nums[i])
+                {
+                    return -1;
+                }
+            }
+            return pq.Count;
+
+        }
+    }
+}
