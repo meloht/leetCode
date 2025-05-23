cebbddb0183f46d2b101402628064ef2f904f48c
hongtao_luo <luohongtao621@hotmail.com>
2025-05-23 13:12:54 +0800

    3068

---
+++ Testx/3051_3100/3068_FindTheMaximumSumOfNodeValuesTest.cs
@@ -0,0 +1,47 @@
+﻿using leetCode._3051_3100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3051_3100
+{
+    public class _3068_FindTheMaximumSumOfNodeValuesTest
+    {
+        _3068_FindTheMaximumSumOfNodeValuesAlg alg = new _3068_FindTheMaximumSumOfNodeValuesAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [1, 2, 1];
+            int k = 3;
+            int[][] edges = [[0, 1], [0, 2]];
+            long exp = 6;
+            long res = alg.MaximumValueSum(nums, k, edges);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [2, 3];
+            int k = 7;
+            int[][] edges = [[0, 1]];
+            long exp = 9;
+            long res = alg.MaximumValueSum(nums, k, edges);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] nums = [7, 7, 7, 7, 7, 7];
+            int k = 3;
+            int[][] edges = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5]];
+            long exp = 42;
+            long res = alg.MaximumValueSum(nums, k, edges);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3051_3100/3068_FindTheMaximumSumOfNodeValuesAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3051_3100
+{
+    public class _3068_FindTheMaximumSumOfNodeValuesAlg
+    {
+        public long MaximumValueSum(int[] nums, int k, int[][] edges)
+        {
+            long f0 = 0;
+            long f1 = long.MinValue;
+            foreach (int x in nums)
+            {
+                long t = Math.Max(f1 + x, f0 + (x ^ k));
+                f0 = Math.Max(f0 + x, f1 + (x ^ k));
+                f1 = t;
+            }
+            return f0;
+
+        }
+    }
+}
