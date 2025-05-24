2d05368d229cc39c53e0431fbd3be2d05d0038e1
luohongtao <luohongtao621@hotmail.com>
2025-05-24 13:32:59 +0800

    2042

---
+++ Testx/2901_2950/2942_FindWordsContainingCharacterTest.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._2901_2950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2901_2950
+{
+    public class _2942_FindWordsContainingCharacterTest
+    {
+        _2942_FindWordsContainingCharacterAlg alg = new _2942_FindWordsContainingCharacterAlg();
+        [Fact]
+        public void Test01()
+        {
+            string[] words = ["leet", "code"];
+            char x = 'e';
+            int[] exp = [0, 1];
+            int[] res = alg.FindWordsContaining(words, x).ToArray();
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            string[] words = ["abc", "bcd", "aaaa", "cbc"];
+            char x = 'z';
+            int[] exp = [];
+            int[] res = alg.FindWordsContaining(words, x).ToArray();
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            string[] words = ["abc", "bcd", "aaaa", "cbc"];
+            char x = 'a';
+            int[] exp = [0, 2];
+            int[] res = alg.FindWordsContaining(words, x).ToArray();
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2901_2950/2942_FindWordsContainingCharacterAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2901_2950
+{
+    public class _2942_FindWordsContainingCharacterAlg
+    {
+        public IList<int> FindWordsContaining(string[] words, char x)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].IndexOf(x) >= 0)
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
