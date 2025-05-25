f1484e753d2280333fbd1428a7057a94e119718a
luohongtao <luohongtao621@hotmail.com>
2025-05-25 14:44:30 +0800

    0969

---
+++ Testx/0951_1000/0969_PancakeSortingTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0951_1000
+{
+    public class _0969_PancakeSortingTest
+    {
+        _0969_PancakeSortingAlg alg = new _0969_PancakeSortingAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [3, 2, 4, 1];
+            int[] exp = [4, 2, 4, 3];
+          
+            int[] res = alg.PancakeSort(nums).ToArray();
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [1, 2, 3];
+            int[] exp = [];
+            int[] res = alg.PancakeSort(nums).ToArray();
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0969_PancakeSortingAlg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0969_PancakeSortingAlg
+    {
+        public IList<int> PancakeSort(int[] arr)
+        {
+            List<int> list = new List<int>();
+
+            int last = arr.Length - 1;
+            int max = arr.Length;
+            while (last > 0)
+            {
+                if (arr[last] != max)
+                {
+                    int idx = Array.IndexOf(arr, max);
+
+                    if (idx > 0)
+                    {
+                        list.Add(idx + 1);
+                        Array.Reverse(arr, 0, idx + 1);
+                    }
+
+                    Array.Reverse(arr, 0, last + 1);
+                    list.Add(last + 1);
+                }
+
+                last--;
+                max--;
+
+            }
+
+            return list;
+        }
+
+
+
+    }
+}
