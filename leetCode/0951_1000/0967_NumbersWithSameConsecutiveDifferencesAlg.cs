8785a5e85023ca35a84b89a3d34e40e04bb95780
luohongtao <luohongtao621@hotmail.com>
2025-05-25 14:02:57 +0800

    967

--- leetCode/0951_1000/0967_NumbersWithSameConsecutiveDifferencesAlg.cs
+++ leetCode/0951_1000/0967_NumbersWithSameConsecutiveDifferencesAlg.cs
@@ -10,9 +10,33 @@ namespace leetCode._0951_1000
     {
         public int[] NumsSameConsecDiff(int n, int k)
         {
-         
+          List<int> list = new List<int>();
 
-            return null;
+            for (int i = 1; i <= 9; i++)
+            {
+                Dfs(i, 1, n, k, list);
+            }
+            return list.ToArray();
+        }
+
+        private void Dfs(int num,int length,int n,int k,List<int> list)
+        {
+            if (length == n)
+            {
+                list.Add(num);
+                return;
+            }
+            int last = num % 10;
+            int num1 = last - k;
+            int num2 = last + k;
+            if (num1 >= 0)
+            {
+                Dfs(num * 10 + num1, length + 1, n, k, list);
+            }
+            if (num2 <= 9 && num1 != num2)
+            {
+                Dfs(num * 10 + num2, length + 1, n, k, list);
+            }
         }
     }
 }
