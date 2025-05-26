ac6135eea0c8e6b9c8f04fb39099cc646d19a959
hongtao_luo <luohongtao621@hotmail.com>
2025-05-26 16:56:28 +0800

    973

---
+++ Testx/0951_1000/0973_KClosestPointsToOriginTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0951_1000
+{
+    public class _0973_KClosestPointsToOriginTest
+    {
+        _0973_KClosestPointsToOriginAlg alg = new _0973_KClosestPointsToOriginAlg();
+        [Fact]
+        public void Test01()
+        {
+            int[][] points = [[1, 3], [-2, 2]];
+            int k = 1;
+            int[][] exp = [[-2, 2]];
+            int[][] res = alg.KClosest(points, k);
+            Assert.True(Utils.IsSameArr(exp, res));
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[][] points = [[3, 3], [5, -1], [-2, 4]];
+            int k = 2;
+            int[][] exp = [[3, 3], [-2, 4]];
+            int[][] res = alg.KClosest(points, k);
+            Assert.True(Utils.IsSame(exp, res));
+        }
+    }
+}
---
+++ leetCode/0951_1000/0973_KClosestPointsToOriginAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0973_KClosestPointsToOriginAlg
+    {
+        public int[][] KClosest(int[][] points, int k)
+        {
+            if(points.Length==k)
+                return points;
+
+            Array.Sort(points, Dis);
+            int[][] ans = new int[k][];
+            for (int i = 0; i < k; i++)
+            {
+                ans[i] = points[i];
+            }
+
+            return ans;
+        }
+
+        private int Dis(int[] x, int[] y)
+        {
+            long d1 = (long)x[0] * (long)x[0] + (long)x[1] * (long)x[1];
+            long d2 = (long)y[0] * (long)y[0] + (long)y[1] * (long)y[1];
+            return d1.CompareTo(d2);
+        }
+    }
+}
