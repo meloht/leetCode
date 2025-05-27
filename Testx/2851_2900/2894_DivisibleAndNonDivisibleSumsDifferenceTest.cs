9b5ffd4cb3a50a1ef9892a68083bfeae2683d9c6
hongtao_luo <luohongtao621@hotmail.com>
2025-05-27 12:58:47 +0800

    2894

---
+++ Testx/2851_2900/2894_DivisibleAndNonDivisibleSumsDifferenceTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2851_2900
+{
+    public class _2894_DivisibleAndNonDivisibleSumsDifferenceTest
+    {
+        _2894_DivisibleAndNonDivisibleSumsDifferenceAlg alg = new _2894_DivisibleAndNonDivisibleSumsDifferenceAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int n = 10, m = 3;
+            int exp = 19;
+            int res = alg.DifferenceOfSums(n, m);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int n = 5, m = 6;
+            int exp = 15;
+            int res = alg.DifferenceOfSums(n, m);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int n = 5, m = 1;
+            int exp = -15;
+            int res = alg.DifferenceOfSums(n, m);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/2894_DivisibleAndNonDivisibleSumsDifferenceAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _2894_DivisibleAndNonDivisibleSumsDifferenceAlg
+    {
+        public int DifferenceOfSums(int n, int m)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % m == 0)
+                {
+                    num2 += i;
+                }
+                else
+                {
+                    num1 += i;
+                }
+            }
+
+            return num1-num2;
+        }
+
+        public int DifferenceOfSums1(int n, int m)
+        {
+            int k = n / m;
+            return n * (n + 1) / 2 - k * (k + 1) * m;
+        }
+
+
+    }
+}
