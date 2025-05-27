35bef92ef518d65c3295b1d82c4d632c5314635e
hongtao_luo <luohongtao621@hotmail.com>
2025-05-27 16:38:51 +0800

    978

---
+++ Testx/0951_1000/0978_LongestTurbulentSubarrayTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0951_1000
+{
+    public class _0978_LongestTurbulentSubarrayTest
+    {
+        _0978_LongestTurbulentSubarrayAlg alg = new _0978_LongestTurbulentSubarrayAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] arr = [9, 4, 2, 10, 7, 8, 8, 1, 9];
+            int exp = 5;
+            int res = alg.MaxTurbulenceSize(arr);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] arr = [4, 8, 12, 16];
+            int exp = 2;
+            int res = alg.MaxTurbulenceSize(arr);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0978_LongestTurbulentSubarrayAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0978_LongestTurbulentSubarrayAlg
+    {
+        public int MaxTurbulenceSize(int[] arr)
+        {
+            int n = arr.Length;
+            int ret = 1;
+            int left = 0, right = 0;
+
+            while (right < n - 1)
+            {
+                if (left == right)
+                {
+                    if (arr[left] == arr[left + 1])
+                    {
+                        left++;
+                    }
+                    right++;
+                }
+                else
+                {
+                    if (arr[right - 1] < arr[right] && arr[right] > arr[right + 1])
+                    {
+                        right++;
+                    }
+                    else if (arr[right - 1] > arr[right] && arr[right] < arr[right + 1])
+                    {
+                        right++;
+                    }
+                    else
+                    {
+                        left = right;
+                    }
+                }
+                ret = Math.Max(ret, right - left + 1);
+            }
+            return ret;
+
+        }
+    }
+}
