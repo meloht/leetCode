40186fb835da31ecacd085fd138ddfc358cdcd34
hongtao_luo <luohongtao621@hotmail.com>
2025-05-28 15:23:12 +0800

    3372

---
+++ Testx/3351_3400/3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesITest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._3351_3400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3351_3400
+{
+    public class _3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesITest
+    {
+        _3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesIAlg alg = new _3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[][] edges1 = [[0, 1], [0, 2], [2, 3], [2, 4]], edges2 = [[0, 1], [0, 2], [0, 3], [2, 7], [1, 4], [4, 5], [4, 6]];
+            int k = 2;
+            int[] exp = [9, 7, 9, 8, 8];
+            int[] res = alg.MaxTargetNodes(edges1, edges2, k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[][] edges1 = [[0, 1], [0, 2], [0, 3], [0, 4]], edges2 = [[0, 1], [1, 2], [2, 3]];
+            int k = 1;
+            int[] exp = [6, 3, 3, 3, 3];
+            int[] res = alg.MaxTargetNodes(edges1, edges2, k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[][] edges1 = [[0, 1]], edges2 = [[0, 1]];
+            int k = 0;
+            int[] exp = [1, 1];
+            int[] res = alg.MaxTargetNodes(edges1, edges2, k);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3351_3400/3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesIAlg.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3351_3400
+{
+    public class _3372_MaximizeTheNumberOfTargetNodesAfterConnectingTreesIAlg
+    {
+        public int[] MaxTargetNodes(int[][] edges1, int[][] edges2, int k)
+        {
+            int n = edges1.Length + 1;
+            int[] ans = new int[n];
+            List<int>[] arr1 = GetAdjArr(edges1);
+            List<int>[] arr2 = GetAdjArr(edges2);
+            int count = GetMax(arr2, k);
+            for (int i = 0; i < n; i++)
+            {
+                ans[i] = GetMax(arr1, i, k) + count;
+            }
+
+            return ans;
+        }
+
+        private int GetMax(List<int>[] arr, int k)
+        {
+            int ans = 0;
+            if (k == 0)
+                return ans;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                ans = Math.Max(ans, GetMax(arr, i, k - 1));
+            }
+            return ans;
+        }
+
+        private List<int>[] GetAdjArr(int[][] edges)
+        {
+            List<int>[] arr = new List<int>[edges.Length + 1];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new List<int>();
+            }
+
+            foreach (var item in edges)
+            {
+                arr[item[0]].Add(item[1]);
+                arr[item[1]].Add(item[0]);
+            }
+            return arr;
+        }
+
+        private int GetMax(List<int>[] arr, int idx, int k)
+        {
+            int num = 1;
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(idx);
+            bool[] visited = new bool[arr.Length];
+            visited[idx] = true;
+            while (queue.Count > 0 && k > 0)
+            {
+                int count = queue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    int p = queue.Dequeue();
+
+                    var next = arr[p];
+                    foreach (var item in next)
+                    {
+                        if (!visited[item])
+                        {
+                            queue.Enqueue(item);
+                            visited[item] = true;
+                            num++;
+                        }
+                    }
+
+                }
+                k--;
+
+            }
+            return num;
+        }
+
+
+    }
+}
