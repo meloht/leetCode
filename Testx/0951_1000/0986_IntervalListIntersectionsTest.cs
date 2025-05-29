dacd9d14894a8137a873cad139c9ebdb51132144
hongtao_luo <luohongtao621@hotmail.com>
2025-05-29 13:52:36 +0800

    986

---
+++ Testx/0951_1000/0986_IntervalListIntersectionsTest.cs
@@ -0,0 +1,50 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0951_1000
+{
+    public class _0986_IntervalListIntersectionsTest
+    {
+        _0986_IntervalListIntersectionsAlg alg = new _0986_IntervalListIntersectionsAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[][] firstList = [[0, 2], [5, 10], [13, 23], [24, 25]], secondList = [[1, 5], [8, 12], [15, 24], [25, 26]];
+            int[][] exp = [[1, 2], [5, 5], [8, 10], [15, 23], [24, 24], [25, 25]];
+            int[][] res = alg.IntervalIntersection(firstList, secondList);
+            Assert.True(Utils.IsSameArr(exp, res));
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[][] firstList = [[1, 3], [5, 9]], secondList = [];
+            int[][] exp = [];
+            int[][] res = alg.IntervalIntersection(firstList, secondList);
+            Assert.True(Utils.IsSameArr(exp, res));
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[][] firstList = [], secondList = [[4, 8], [10, 12]];
+            int[][] exp = [];
+            int[][] res = alg.IntervalIntersection(firstList, secondList);
+            Assert.True(Utils.IsSameArr(exp, res));
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int[][] firstList = [[1, 7]], secondList = [[3, 10]];
+            int[][] exp = [[3, 7]];
+            int[][] res = alg.IntervalIntersection(firstList, secondList);
+            Assert.True(Utils.IsSameArr(exp, res));
+        }
+    }
+}
---
+++ leetCode/0951_1000/0986_IntervalListIntersectionsAlg.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0986_IntervalListIntersectionsAlg
+    {
+        public int[][] IntervalIntersection(int[][] firstList, int[][] secondList)
+        {
+            List<int[]> ans = new List<int[]>();
+
+            for (int i = 0, j = 0; i < firstList.Length && j < secondList.Length;)
+            {
+                int[] f1 = firstList[i];
+                int[] f2 = secondList[j];
+                if (f1[1] >= f2[0] && f1[0] <= f2[1])
+                {
+                    ans.Add([Math.Max(f1[0], f2[0]), Math.Min(f1[1], f2[1])]);
+                    if (f1[1] > f2[1])
+                    {
+                        j++;
+                    }
+                    else if (f2[1] > f1[1])
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        i++;
+                        j++;
+                    }
+                }
+                else if (f1[0] > f2[1])
+                {
+                    j++;
+                }
+                else if (f2[0] > f1[1])
+                {
+                    i++;
+                }
+            }
+
+            return ans.ToArray();
+        }
+    }
+}
