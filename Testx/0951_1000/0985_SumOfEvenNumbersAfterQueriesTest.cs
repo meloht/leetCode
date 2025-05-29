293b6f9f11d57b0e6f0989edda2962fc7b5b61ab
hongtao_luo <luohongtao621@hotmail.com>
2025-05-29 13:30:52 +0800

    985

---
+++ Testx/0951_1000/0985_SumOfEvenNumbersAfterQueriesTest.cs
@@ -0,0 +1,25 @@
+﻿using leetCode._0951_1000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0951_1000
+{
+    public class _0985_SumOfEvenNumbersAfterQueriesTest
+    {
+        _0985_SumOfEvenNumbersAfterQueriesAlg alg = new _0985_SumOfEvenNumbersAfterQueriesAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] A = [1, 2, 3, 4];
+            int[][] queries = [[1, 0], [-3, 1], [-4, 0], [2, 3]];
+            int[] exp = [8, 6, 2, 4];
+            int[] res = alg.SumEvenAfterQueries(A, queries);
+            Assert.Equal(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/0951_1000/0985_SumOfEvenNumbersAfterQueriesAlg.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0985_SumOfEvenNumbersAfterQueriesAlg
+    {
+        public int[] SumEvenAfterQueries(int[] nums, int[][] queries)
+        {
+            int sum = 0;
+            foreach (var item in nums)
+            {
+                if (item % 2 == 0)
+                {
+                    sum += item;
+                }
+            }
+            int[] ans= new int[nums.Length];
+            int i = 0;
+            foreach (var item in queries)
+            {
+                int idx = item[1];
+                int val = item[0]+ nums[idx];
+                bool bl = val % 2 == 0;
+                if (nums[idx] % 2 == 0)
+                {
+                    if (bl)
+                    {
+                        sum += item[0];
+                    }
+                    else
+                    {
+                        sum -= nums[idx];
+                    }
+                }
+                else
+                {
+                    if (bl)
+                    {
+                        sum += val;
+                    }
+                   
+                }
+                nums[idx] = val;
+                ans[i]= sum;
+                i++;
+            }
+            return ans;
+        }
+    }
+}
