16716e326cd3ede87d856fc79e2186511154374d
hongtao_luo <luohongtao621@hotmail.com>
2025-05-30 13:58:14 +0800

    988

---
+++ Testx/0951_1000/0988_SmallestStringStartingFromLeafTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._0951_1000;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._0951_1000
+{
+    public class _0988_SmallestStringStartingFromLeafTest
+    {
+        _0988_SmallestStringStartingFromLeafAlg alg = new _0988_SmallestStringStartingFromLeafAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int?[] root = [0, 1, 2, 3, 4, 3, 4];
+            string exp = "dba";
+            string res = alg.SmallestFromLeaf(TreeNode.BuildTree(root));
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int?[] root = [25, 1, 3, 1, 3, 0, 2];
+            string exp = "adz";
+            string res = alg.SmallestFromLeaf(TreeNode.BuildTree(root));
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int?[] root = [2, 2, 1, null, 1, 0, null, 0];
+            string exp = "abc";
+            string res = alg.SmallestFromLeaf(TreeNode.BuildTree(root));
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int?[] root = [18, 23, 17, 12, 2, 11, 15];
+            string exp = "cxs";
+            string res = alg.SmallestFromLeaf(TreeNode.BuildTree(root));
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/0951_1000/0988_SmallestStringStartingFromLeafAlg.cs
@@ -0,0 +1,70 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._0951_1000
+{
+    public class _0988_SmallestStringStartingFromLeafAlg
+    {
+        string ans = null;
+        public string SmallestFromLeaf(TreeNode root)
+        {
+            Dfs(root, "");
+            var ch = ans.ToCharArray();
+            Array.Reverse(ch);
+            return new string(ch);
+        }
+        private void Dfs(TreeNode root, string s)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            string ch = s+ char.ToString((char)(root.val + 'a'));
+            if (root.left == null && root.right == null)
+            {
+                if (ans == null)
+                {
+                    ans = ch;
+                    return;
+                }
+                int bl = 0;
+                for (int i = ch.Length - 1, j = ans.Length - 1; i >= 0 && j >= 0; i--, j--)
+                {
+                    if (ch[i] < ans[j])
+                    {
+                        bl = -1;
+                        break;
+                    }
+                    else if (ch[i] > ans[j])
+                    {
+                        bl = 1;
+                        break;
+                    }
+
+                }
+                if (bl == 0 && ch.Length < ans.Length)
+                {
+                    bl = -1;
+                }
+                if (bl == -1)
+                {
+                    ans = ch;
+                }
+                return;
+            }
+
+            Dfs(root.left, ch);
+            Dfs(root.right, ch);
+
+           
+        }
+
+
+
+    }
+}
