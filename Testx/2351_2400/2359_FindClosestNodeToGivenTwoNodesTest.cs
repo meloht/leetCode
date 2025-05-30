70e161f06e038de84bdbd8a663eec8be3b92990e
hongtao_luo <luohongtao621@hotmail.com>
2025-05-30 12:43:08 +0800

    2359

---
+++ Testx/2351_2400/2359_FindClosestNodeToGivenTwoNodesTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._2351_2400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2351_2400
+{
+    public class _2359_FindClosestNodeToGivenTwoNodesTest
+    {
+        _2359_FindClosestNodeToGivenTwoNodesAlg alg = new _2359_FindClosestNodeToGivenTwoNodesAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] edges = [2, 2, 3, -1];
+            int node1 = 0, node2 = 1;
+            int exp = 2;
+            int res = alg.ClosestMeetingNode(edges, node1, node2);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] edges = [1, 2, -1];
+            int node1 = 0, node2 = 2;
+            int exp = 2;
+            int res = alg.ClosestMeetingNode(edges, node1, node2);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] edges = [5, 3, 1, 0, 2, 4, 5];
+            int node1 = 3, node2 = 2;
+            int exp = 3;
+            int res = alg.ClosestMeetingNode(edges, node1, node2);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2351_2400/2359_FindClosestNodeToGivenTwoNodesAlg.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2351_2400
+{
+    public class _2359_FindClosestNodeToGivenTwoNodesAlg
+    {
+        public int ClosestMeetingNode(int[] edges, int node1, int node2)
+        {
+            var dict1 = GetList(edges, node1);
+            bool[] visited = new bool[edges.Length];
+
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(node2);
+            visited[node2] = true;
+
+            int dis = 0;
+            int disMin = int.MaxValue;
+            int ans = int.MaxValue;
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    int p = queue.Dequeue();
+
+                    if (dict1[p] > -1)
+                    {
+                        int max = Math.Max(dis, dict1[p]);
+                        if (max <= disMin)
+                        {
+                            if (max == disMin)
+                            {
+                                ans = Math.Min(ans, p);
+                            }
+                            else
+                            {
+                                ans = p;
+                            }
+
+                            disMin = max;
+                        }
+                    }
+
+                    int next = edges[p];
+                    if (next > -1 && !visited[next])
+                    {
+                        visited[next] = true;
+                        queue.Enqueue(next);
+
+                    }
+                }
+                dis++;
+            }
+            if (ans != int.MaxValue)
+                return ans;
+
+            return -1;
+        }
+
+        private int[] GetList(int[] edges, int node)
+        {
+            bool[] visited = new bool[edges.Length];
+
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(node);
+            visited[node] = true;
+
+            int[] dict = new int[edges.Length];
+            Array.Fill(dict, -1);
+            dict[node] = 0;
+            int dis = 1;
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    int p = queue.Dequeue();
+                    int next = edges[p];
+
+                    if (next > -1 && !visited[next])
+                    {
+                        visited[next] = true;
+                        queue.Enqueue(next);
+
+                        dict[next] = dis;
+                    }
+                }
+                dis++;
+            }
+            return dict;
+        }
+
+    }
+}
