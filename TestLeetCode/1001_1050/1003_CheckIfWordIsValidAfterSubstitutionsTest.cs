0368624ee59f2f33f0c2fc534a32c1072d68b13d
luohongtao <luohongtao621@hotmail.com>
2025-05-31 19:26:40 +0800

    1003

---
+++ TestLeetCode/1001_1050/1003_CheckIfWordIsValidAfterSubstitutionsTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1003_CheckIfWordIsValidAfterSubstitutionsTest
+    {
+        _1003_CheckIfWordIsValidAfterSubstitutionsAlg alg = new _1003_CheckIfWordIsValidAfterSubstitutionsAlg();
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "aabcbc";
+            bool exp = true;
+            bool res= alg.IsValid(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "abcabcababcc";
+            bool exp = true;
+            bool res = alg.IsValid(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "abccba";
+            bool exp = false;
+            bool res = alg.IsValid(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1003_CheckIfWordIsValidAfterSubstitutionsAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1003_CheckIfWordIsValidAfterSubstitutionsAlg
+    {
+        public bool IsValid(string s)
+        {
+           var ss=s.ToCharArray();
+
+            int i = 0;
+            foreach (char c in s) 
+            {
+                if (c > 'a' && (i == 0 || c - ss[--i] != 1))
+                {
+                    return false;
+                }
+                if (c < 'c')
+                {
+                    ss[i++] = c;
+                }
+            }
+
+            return i==0;
+        }
+    }
+}
