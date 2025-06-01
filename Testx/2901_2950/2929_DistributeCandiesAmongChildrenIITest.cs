2c76141b50d997cf7dd2a81a509c6fce2ecf3bea
luohongtao <luohongtao621@hotmail.com>
2025-06-01 16:55:14 +0800

    2929

---
+++ Testx/2901_2950/2929_DistributeCandiesAmongChildrenIITest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._2901_2950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2901_2950
+{
+    public class _2929_DistributeCandiesAmongChildrenIITest
+    {
+        _2929_DistributeCandiesAmongChildrenIIAlg alg = new _2929_DistributeCandiesAmongChildrenIIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int n = 5, limit = 2;
+            long exp = 3;
+            long res = alg.DistributeCandies(n, limit);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int n = 3, limit = 3;
+            long exp = 10;
+            long res = alg.DistributeCandies(n, limit);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2901_2950/2929_DistributeCandiesAmongChildrenIIAlg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2901_2950
+{
+    public class _2929_DistributeCandiesAmongChildrenIIAlg
+    {
+        public long DistributeCandies(int n, int limit)
+        {
+            return C2(n + 2) - 3 * C2(n - limit + 1) + 3 * C2(n - 2 * limit) - C2(n - 3 * limit - 1);
+
+        }
+        private long C2(int n)
+        {
+            return n > 1 ? (long)n * (n - 1) / 2 : 0;
+        }
+
+
+    }
+}
