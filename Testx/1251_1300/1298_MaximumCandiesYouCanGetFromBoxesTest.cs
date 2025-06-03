d5aabd07cff6ed9d40b3bf671bc80412500dc091
hongtao_luo <luohongtao621@hotmail.com>
2025-06-03 13:38:25 +0800

    1298

---
+++ Testx/1251_1300/1298_MaximumCandiesYouCanGetFromBoxesTest.cs
@@ -0,0 +1,69 @@
+﻿using leetCode._1251_1300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1251_1300
+{
+    public class _1298_MaximumCandiesYouCanGetFromBoxesTest
+    {
+        _1298_MaximumCandiesYouCanGetFromBoxesAlg alg = new _1298_MaximumCandiesYouCanGetFromBoxesAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] status = [1, 0, 1, 0], candies = [7, 5, 4, 100];
+            int[][] keys = [[], [], [1], []], containedBoxes = [[1, 2], [3], [], []];
+            int[] initialBoxes = [0];
+            int exp = 16;
+            int res = alg.MaxCandies(status, candies, keys, containedBoxes, initialBoxes);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] status = [1, 0, 0, 0, 0, 0], candies = [1, 1, 1, 1, 1, 1];
+            int[][] keys = [[1, 2, 3, 4, 5], [], [], [], [], []], containedBoxes = [[1, 2, 3, 4, 5], [], [], [], [], []];
+            int[] initialBoxes = [0];
+            int exp = 6;
+            int res = alg.MaxCandies(status, candies, keys, containedBoxes, initialBoxes);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] status = [1, 1, 1], candies = [100, 1, 100];
+            int[][] keys = [[], [0, 2], []], containedBoxes = [[], [], []];
+            int[] initialBoxes = [1];
+            int exp = 1;
+            int res = alg.MaxCandies(status, candies, keys, containedBoxes, initialBoxes);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            int[] status = [1], candies = [100];
+            int[][] keys = [[]], containedBoxes = [[]];
+            int[] initialBoxes = [];
+            int exp = 0;
+            int res = alg.MaxCandies(status, candies, keys, containedBoxes, initialBoxes);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test05()
+        {
+            int[] status = [1, 1, 1], candies = [2, 3, 2];
+            int[][] keys = [[], [], []], containedBoxes = [[], [], []];
+            int[] initialBoxes = [2, 1, 0];
+            int exp = 7;
+            int res = alg.MaxCandies(status, candies, keys, containedBoxes, initialBoxes);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1251_1300/1298_MaximumCandiesYouCanGetFromBoxesAlg.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1251_1300
+{
+    public class _1298_MaximumCandiesYouCanGetFromBoxesAlg
+    {
+        public int MaxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes)
+        {
+            int n = status.Length;
+            bool[] visited = new bool[n];
+            int[] keySey = new int[n];
+            bool[] noUser = new bool[n];
+            Queue<int> queue = new Queue<int>();
+            foreach (var item in initialBoxes)
+            {
+                queue.Enqueue(item);
+                visited[item] = true;
+
+            }
+            int ans = 0;
+
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    int idx = queue.Dequeue();
+
+                    var arr = keys[idx];
+                    foreach (var item in arr)
+                    {
+                        if (idx == item)
+                            continue;
+                        keySey[item]++;
+                        if (noUser[item])
+                        {
+                            ans += candies[item];
+                            noUser[item] = false;
+                        }
+                    }
+
+                    if (status[idx] == 1)
+                    {
+                        ans += candies[idx];
+
+                    }
+                    else
+                    {
+                        if (keySey[idx] > 0)
+                        {
+                            ans += candies[idx];
+                        }
+                        else
+                        {
+                            noUser[idx] = true;
+                        }
+
+                    }
+                    var boxs = containedBoxes[idx];
+                    foreach (var item in boxs)
+                    {
+                        if (visited[item] == false)
+                        {
+                            queue.Enqueue(item);
+                            visited[item] = true;
+                        }
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
