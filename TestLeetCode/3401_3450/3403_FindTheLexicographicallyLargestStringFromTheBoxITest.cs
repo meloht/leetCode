156810a479a41c1b93d459e08cb8f7e3c0f2bf7a
hongtao_luo <luohongtao621@hotmail.com>
2025-06-04 14:16:14 +0800

    3403

---
+++ TestLeetCode/3401_3450/3403_FindTheLexicographicallyLargestStringFromTheBoxITest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._3401_3450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3401_3450
+{
+    [TestClass]
+    public class _3403_FindTheLexicographicallyLargestStringFromTheBoxITest
+    {
+        _3403_FindTheLexicographicallyLargestStringFromTheBoxI alg = new _3403_FindTheLexicographicallyLargestStringFromTheBoxI();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string word = "dbca";
+            int numFriends = 2;
+            string exp = "dbc";
+            string res = alg.AnswerString(word, numFriends);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string word = "gggg";
+            int numFriends = 4;
+            string exp = "g";
+            string res = alg.AnswerString(word, numFriends);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3401_3450/3403_FindTheLexicographicallyLargestStringFromTheBoxI.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3401_3450
+{
+    public class _3403_FindTheLexicographicallyLargestStringFromTheBoxI
+    {
+        public string AnswerString1(string word, int numFriends)
+        {
+            if (numFriends == 1)
+            {
+                return word;
+            }
+            int n = word.Length;
+            int i = 0;
+            int j = 1;
+            while (j < n)
+            {
+                int k = 0;
+                while (j + k < n && word[i + k] == word[j + k])
+                {
+                    k++;
+                }
+                if (j + k < n && word[i + k] < word[j + k])
+                {
+                    int t = i;
+                    i = j;
+                    j = Math.Max(j + 1, t + k + 1);
+                }
+                else
+                {
+                    j += k + 1;
+                }
+            }
+            return word.Substring(i, Math.Min(n - numFriends + 1, n - i));
+
+
+        }
+
+        public string AnswerString(string word, int numFriends)
+        {
+            if (numFriends == 1)
+            {
+                return word;
+            }
+            int n = word.Length;
+            string ans = "";
+            for (int i = 0; i < n; i++)
+            {
+                string sub = word.Substring(i, Math.Min(n - numFriends + 1, n - i));
+                if (sub.CompareTo(ans) > 0)
+                {
+                    ans = sub;
+                }
+            }
+            return ans;
+        }
+
+    }
+}
