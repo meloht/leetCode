e3e62d102671644ba9d8597f2c63608a4f4f7fca
hongtao_luo <luohongtao621@hotmail.com>
2025-06-05 13:52:15 +0800

    1006

---
+++ TestLeetCode/1001_1050/1006_ClumsyFactorialTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1006_ClumsyFactorialTest
+    {
+        _1006_ClumsyFactorialAlg alg = new _1006_ClumsyFactorialAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 4;
+            int exp = 7;
+            int res = alg.Clumsy(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 10;
+            int exp = 12;
+            int res = alg.Clumsy(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1006_ClumsyFactorialAlg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1006_ClumsyFactorialAlg
+    {
+        public int Clumsy(int n)
+        {
+            int prev = 0;
+            int curr = n;
+            bool bl = false;
+            char[] arr = ['*', '/', '+', '-'];
+
+            for (int i = n - 1, j = 0; i > 0; i--)
+            {
+                char op = arr[j];
+                if (op == '*')
+                {
+                    curr = curr * i;
+                }
+                else if (op == '/')
+                {
+                    curr = curr / i;
+                }
+                else if (op == '+')
+                {
+                    if (bl)
+                    {
+                        curr = prev - curr;
+                    }
+                    curr += i;
+                    bl = false;
+                }
+                else if (op == '-')
+                {
+                    prev = curr;
+                    curr = i;
+                    bl = true;
+                }
+
+                j++;
+                j = j % 4;
+            }
+            if (bl)
+            {
+                return prev - curr;
+            }
+            else
+            {
+               return curr;
+            }
+        }
+    }
+}
