014fca2ffbe69fc3e2c1566c134ddcc9c85659b8
hongtao_luo <luohongtao621@hotmail.com>
2025-06-05 12:39:02 +0800

    1004

---
+++ TestLeetCode/1001_1050/1004_MaxConsecutiveOnesIIITest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1004_MaxConsecutiveOnesIIITest
+    {
+        _1004_MaxConsecutiveOnesIIIAlg alg = new _1004_MaxConsecutiveOnesIIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0];
+            int K = 2;
+            int exp = 6;
+            int res = alg.LongestOnes(nums, K);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1];
+            int K = 3;
+            int exp = 10;
+            int res = alg.LongestOnes(nums, K);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1004_MaxConsecutiveOnesIIIAlg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1004_MaxConsecutiveOnesIIIAlg
+    {
+        public int LongestOnes(int[] nums, int k)
+        {
+            int cnt1 = 0;
+            int cnt0 = 0;
+            int ans = 0;
+            for (int i = 0, j = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    cnt0++;
+                }
+                else
+                {
+                    cnt1++;
+                }
+                if (cnt0 > k)
+                {
+                    while (cnt0 > k && j < nums.Length)
+                    {
+                        if (nums[j] == 0)
+                        {
+                            cnt0--;
+                        }
+                        else
+                        {
+                            cnt1--;
+                        }
+                        j++;
+                    }
+                }
+                ans = Math.Max(ans, cnt0 + cnt1);
+            }
+            return ans;
+        }
+    }
+}
