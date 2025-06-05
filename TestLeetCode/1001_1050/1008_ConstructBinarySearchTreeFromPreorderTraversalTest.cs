bdf721373467f96d3a87314b1bb56efb343c8e36
hongtao_luo <luohongtao621@hotmail.com>
2025-06-05 14:49:36 +0800

    1008

---
+++ TestLeetCode/1001_1050/1008_ConstructBinarySearchTreeFromPreorderTraversalTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._1001_1050;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1008_ConstructBinarySearchTreeFromPreorderTraversalTest
+    {
+        _1008_ConstructBinarySearchTreeFromPreorderTraversalAlg alg = new _1008_ConstructBinarySearchTreeFromPreorderTraversalAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] preorder = [8, 5, 1, 7, 10, 12];
+            int?[] exp = [8, 5, 10, 1, 7, null, 12];
+            var res = alg.BstFromPreorder(preorder);
+            int?[] ress = TreeNode.GetTreeLevelList(res);
+
+            Assert.IsTrue(exp.SequenceEqual(ress));
+        }
+
+         [TestMethod]
+        public void TestCase02()
+        {
+            int[] preorder = [1, 3];
+            int?[] exp = [1, null, 3];
+            var res = alg.BstFromPreorder(preorder);
+            int?[] ress = TreeNode.GetTreeLevelList(res);
+
+            Assert.IsTrue(exp.SequenceEqual(ress));
+        }
+    }
+}
---
+++ leetCode/1001_1050/1008_ConstructBinarySearchTreeFromPreorderTraversalAlg.cs
@@ -0,0 +1,57 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1008_ConstructBinarySearchTreeFromPreorderTraversalAlg
+    {
+        public TreeNode BstFromPreorder(int[] preorder)
+        {
+            int[] order = new int[preorder.Length];
+            Array.Copy(preorder, order, order.Length);
+            Array.Sort(order);
+
+            return BuildTree(preorder, order);
+        }
+        private Dictionary<int, int> dict;
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            int n = preorder.Length;
+            dict = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                dict.Add(inorder[i], i);
+            }
+            return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+
+        }
+        public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right)
+        {
+            if (preorder_left > preorder_right)
+            {
+                return null;
+            }
+
+            // 前序遍历中的第一个节点就是根节点
+            int preorder_root = preorder_left;
+            // 在中序遍历中定位根节点
+            int inorder_root = dict[preorder[preorder_root]];
+
+            // 先把根节点建立出来
+            TreeNode root = new TreeNode(preorder[preorder_root]);
+            // 得到左子树中的节点数目
+            int size_left_subtree = inorder_root - inorder_left;
+            // 递归地构造左子树，并连接到根节点
+            // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+            root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+            // 递归地构造右子树，并连接到根节点
+            // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+            root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+            return root;
+        }
+    }
+}
