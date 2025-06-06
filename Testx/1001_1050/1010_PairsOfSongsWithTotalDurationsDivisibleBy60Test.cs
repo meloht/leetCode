885a4f82801c7827f4001b99b9384008a7a7a99a
hongtao_luo <luohongtao621@hotmail.com>
2025-06-06 13:48:15 +0800

    1010

---
+++ Testx/1001_1050/1010_PairsOfSongsWithTotalDurationsDivisibleBy60Test.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1001_1050
+{
+    public class _1010_PairsOfSongsWithTotalDurationsDivisibleBy60Test
+    {
+
+        _1010_PairsOfSongsWithTotalDurationsDivisibleBy60Alg alg = new _1010_PairsOfSongsWithTotalDurationsDivisibleBy60Alg();
+        [Fact]
+        public void Test01()
+        {
+            int[] time = [30, 20, 150, 100, 40];
+            int exp = 3;
+            int res = alg.NumPairsDivisibleBy60(time);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] time = [60, 60, 60];
+            int exp = 3;
+            int res = alg.NumPairsDivisibleBy60(time);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] time = [15, 63, 451, 213, 37, 209, 343, 319];
+            int exp = 1;
+            int res = alg.NumPairsDivisibleBy60(time);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1010_PairsOfSongsWithTotalDurationsDivisibleBy60Alg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1010_PairsOfSongsWithTotalDurationsDivisibleBy60Alg
+    {
+        public int NumPairsDivisibleBy60(int[] time)
+        {
+            int[] arr = new int[60];
+            int ans = 0;
+            foreach (var item in time)
+            {
+                int num = item % 60;
+                ans += arr[(60 - num) % 60];
+                arr[num]++;
+
+            }
+            return ans;
+        }
+    }
+}
