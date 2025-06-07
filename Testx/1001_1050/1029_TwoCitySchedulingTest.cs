f9ff07bf61966c354989e71e7b0c08c375c61a01
luohongtao <luohongtao621@hotmail.com>
2025-06-07 18:57:44 +0800

    1029

---
+++ Testx/1001_1050/1029_TwoCitySchedulingTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1001_1050
+{
+    public class _1029_TwoCitySchedulingTest
+    {
+        _1029_TwoCitySchedulingAlg alg = new _1029_TwoCitySchedulingAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[][] costs = [[10, 20], [30, 200], [400, 50], [30, 20]];
+            int exp = 110;
+            int res = alg.TwoCitySchedCost(costs);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[][] costs = [[259, 770], [448, 54], [926, 667], [184, 139], [840, 118], [577, 469]];
+            int exp = 1859;
+            int res = alg.TwoCitySchedCost(costs);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1029_TwoCitySchedulingAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1029_TwoCitySchedulingAlg
+    {
+        public int TwoCitySchedCost(int[][] costs)
+        {
+
+            Array.Sort(costs, (x, y) => x[0] - y[0] - (x[1] - y[1]));
+
+            int ans = 0;
+            int n = costs.Length / 2;
+            for (int i = 0, j = n; i < n ;i++,j++)
+            {
+                ans += costs[i][0];
+                ans += costs[j][1];
+            }
+
+            return ans;
+        }
+    }
+}
