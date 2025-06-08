3a35af890abeab4a59726fd7628708f074bfa633
luohongtao <luohongtao621@hotmail.com>
2025-06-08 18:11:08 +0800

    1019

---
+++ Testx/1001_1050/1019_NextGreaterNodeInLinkedListTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode;
+using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1001_1050
+{
+    public class _1019_NextGreaterNodeInLinkedListTest
+    {
+        _1019_NextGreaterNodeInLinkedListAlg alg = new _1019_NextGreaterNodeInLinkedListAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] head = [2, 1, 5];
+            int[] exp = [5, 5, 0];
+            ListNode node = ListNode.BuildListNode(head);
+            int[] res = alg.NextLargerNodes(node);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] head = [2, 7, 4, 3, 5];
+            int[] exp = [7, 0, 5, 5, 0];
+            ListNode node = ListNode.BuildListNode(head);
+            int[] res = alg.NextLargerNodes(node);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] head = [2, 5, 4, 1, 3, 7];
+            int[] exp = [5, 7, 7, 3, 7, 0];
+            ListNode node = ListNode.BuildListNode(head);
+            int[] res = alg.NextLargerNodes(node);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1019_NextGreaterNodeInLinkedListAlg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1019_NextGreaterNodeInLinkedListAlg
+    {
+        public int[] NextLargerNodes(ListNode head)
+        {
+            int n = GetLen(head);
+            int[] ans = new int[n];
+
+            ListNode node = head;
+            int idx = 0;
+            Stack<ItemData> stack = new Stack<ItemData>();
+            while (node != null)
+            {
+               
+                while (stack.Count > 0 && stack.Peek().Val < node.val)
+                {
+                    var item = stack.Pop();
+                    ans[item.Idx] = node.val;
+                }
+                stack.Push(new ItemData(node.val, idx));
+
+                node = node.next;
+                idx++;
+            }
+            return ans.ToArray();
+        }
+
+        private int GetLen(ListNode head)
+        {
+            ListNode node = head;
+            int cnt = 0;
+            while (node != null)
+            {
+                cnt++;
+                node = node.next;
+            }
+            return cnt;
+        }
+    }
+
+    public struct ItemData
+    {
+        public int Val;
+        public int Idx;
+        public ItemData(int val, int idx) 
+        {
+            this.Idx = idx;
+            this.Val = val;
+        }
+    }
+}
