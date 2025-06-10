4dd24385c1c4da68599ce518ac75ddc59470259a
hongtao_luo <luohongtao621@hotmail.com>
2025-06-10 12:45:32 +0800

    3442

---
+++ Testx/3301_3350/3442_MaximumDifferenceBetweenEvenAndOddFrequencyITest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._3301_3350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3301_3350
+{
+    public class _3442_MaximumDifferenceBetweenEvenAndOddFrequencyITest
+    {
+        _3442_MaximumDifferenceBetweenEvenAndOddFrequencyIAlg alg = new _3442_MaximumDifferenceBetweenEvenAndOddFrequencyIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            string s = "aaaaabbc";
+            int exp = 3;
+            int res = alg.MaxDifference(s);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            string s = "abcabcab";
+            int exp = 1;
+            int res = alg.MaxDifference(s);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3301_3350/3442_MaximumDifferenceBetweenEvenAndOddFrequencyIAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3301_3350
+{
+    public class _3442_MaximumDifferenceBetweenEvenAndOddFrequencyIAlg
+    {
+        public int MaxDifference(string s)
+        {
+            int[] arr = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                arr[s[i] - 'a']++;
+            }
+            int a1 = 0;
+            int a2 = s.Length;
+            for (int i = 0; i < 26; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    if (arr[i] % 2 == 0)
+                    {
+                        a2 = Math.Min(arr[i], a2);
+                    }
+                    else
+                    {
+                        a1 = Math.Max(arr[i], a1);
+                    }
+                }
+            }
+            return a1 - a2;
+        }
+    }
+}
