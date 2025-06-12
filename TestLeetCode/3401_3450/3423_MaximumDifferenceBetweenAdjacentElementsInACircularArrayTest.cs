5a44aae406dd04e59ac93c554c6061ebfaa869f0
hongtao_luo <luohongtao621@hotmail.com>
2025-06-12 13:17:41 +0800

    3423

---
+++ TestLeetCode/3401_3450/3423_MaximumDifferenceBetweenAdjacentElementsInACircularArrayTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3401_3450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3401_3450
+{
+    [TestClass]
+    public class _3423_MaximumDifferenceBetweenAdjacentElementsInACircularArrayTest
+    {
+        _3423_MaximumDifferenceBetweenAdjacentElementsInACircularArrayAlg alg = new _3423_MaximumDifferenceBetweenAdjacentElementsInACircularArrayAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] nums = [1, 2, 4];
+            int exp = 3;
+            int res = alg.MaxAdjacentDistance(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] nums = [-5, -10, -5];
+            int exp = 5;
+            int res = alg.MaxAdjacentDistance(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3401_3450/3423_MaximumDifferenceBetweenAdjacentElementsInACircularArrayAlg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3401_3450
+{
+    public class _3423_MaximumDifferenceBetweenAdjacentElementsInACircularArrayAlg
+    {
+        public int MaxAdjacentDistance(int[] nums)
+        {
+            int ans = Math.Abs(nums[0] - nums[nums.Length - 1]);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                ans = Math.Max(Math.Abs(nums[i - 1] - nums[i]), ans);
+
+            }
+            return ans;
+        }
+    }
+}
