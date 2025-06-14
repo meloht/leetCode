1d025bafc03996ff6db2d7c4fc7bfd2e980ab149
luohongtao <luohongtao621@hotmail.com>
2025-06-14 16:48:45 +0800

    2566

---
+++ Testx/2550_2600/2566_MaximumDifferenceByRemappingADigitTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._2551_2600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2550_2600
+{
+    public class _2566_MaximumDifferenceByRemappingADigitTest
+    {
+        _2566_MaximumDifferenceByRemappingADigitAlg alg = new _2566_MaximumDifferenceByRemappingADigitAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int num = 11891;
+            int exp = 99009;
+            int res = alg.MinMaxDifference(num);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int num = 90;
+            int exp = 99;
+            int res = alg.MinMaxDifference(num);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2551_2600/2566_MaximumDifferenceByRemappingADigitAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2551_2600
+{
+    public class _2566_MaximumDifferenceByRemappingADigitAlg
+    {
+        public int MinMaxDifference(int num)
+        {
+            string s = num.ToString();
+            int mx = num;
+            var chs = s.ToCharArray();
+            foreach (char c in chs)
+            {
+                if (c != '9')
+                { // 第一个不等于 9 的字符
+                    mx = int.Parse(s.Replace(c, '9')); // 替换成 9
+                    break;
+                }
+            }
+            // 第一个不等于 0 的字符，替换成 0
+            int mn = int.Parse(s.Replace(s[0], '0'));
+            return mx - mn;
+
+
+        }
+    }
+}
