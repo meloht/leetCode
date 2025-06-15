fe791ab40cdd1eec627c9479373f94da71d6103b
luohongtao <luohongtao621@hotmail.com>
2025-06-15 14:20:34 +0800

    1432

---
+++ TestLeetCode/1401_1450/1432_MaxDifferenceYouCanGetFromChangingAnIntegerTest.cs
@@ -0,0 +1,68 @@
+﻿using leetCode._1401_1450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1401_1450
+{
+    [TestClass]
+    public class _1432_MaxDifferenceYouCanGetFromChangingAnIntegerTest
+    {
+        _1432_MaxDifferenceYouCanGetFromChangingAnIntegerAlg alg = new _1432_MaxDifferenceYouCanGetFromChangingAnIntegerAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int num = 555;
+            int exp = 888;
+            int res = alg.MaxDiff(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int num = 9;
+            int exp = 8;
+            int res = alg.MaxDiff(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int num = 123456;
+            int exp = 820000;
+            int res = alg.MaxDiff(num);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase04()
+        {
+            int num = 10000;
+            int exp = 80000;
+            int res = alg.MaxDiff(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int num = 9288;
+            int exp = 8700;
+            int res = alg.MaxDiff(num);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase06()
+        {
+            int num = 1101057;
+            int exp = 8808050;
+            int res = alg.MaxDiff(num);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1401_1450/1432_MaxDifferenceYouCanGetFromChangingAnIntegerAlg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1401_1450
+{
+    public class _1432_MaxDifferenceYouCanGetFromChangingAnIntegerAlg
+    {
+        public int MaxDiff(int num)
+        {
+            string s = num.ToString();
+            string num1 = s;
+            for (int i = 0; i < num1.Length; i++)
+            {
+                if (num1[i] != '9')
+                {
+                    num1 = num1.Replace(num1[i], '9');
+                    break;
+                }
+            }
+            string num2 = s;
+            for (int i = 0; i < num2.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if (num2[i] != '1')
+                    {
+                        num2 = num2.Replace(num2[i], '1');
+                        break;
+                    }
+                }
+                if (i > 0)
+                {
+
+                    if (num2[i] == num2[0])
+                    {
+                        if (num2[i] != '1')
+                        {
+                            num2 = num2.Replace(num2[i], '1');
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (num2[i] != '0')
+                        {
+                            num2 = num2.Replace(num2[i], '0');
+                            break;
+                        }
+                    }
+                }
+            }
+            return int.Parse(num1) - int.Parse(num2);
+        }
+    }
+}
