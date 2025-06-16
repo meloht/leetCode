9e0e7b44f998e0f461a94225767db7478a331a46
hongtao_luo <luohongtao621@hotmail.com>
2025-06-16 12:34:50 +0800

    2016

---
+++ TestLeetCode/2001_2050/2016_MaximumDifferenceBetweenIncreasingElementsTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2001_2050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2001_2050
+{
+    [TestClass]
+    public class _2016_MaximumDifferenceBetweenIncreasingElementsTest
+    {
+        _2016_MaximumDifferenceBetweenIncreasingElementsAlg alg = new _2016_MaximumDifferenceBetweenIncreasingElementsAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] nums = [7, 1, 5, 4];
+            int exp = 4;
+            int res = alg.MaximumDifference(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] nums = [9, 4, 3, 2];
+            int exp = -1;
+            int res = alg.MaximumDifference(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[] nums = [1, 5, 2, 10];
+            int exp = 9;
+            int res = alg.MaximumDifference(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2001_2050/2016_MaximumDifferenceBetweenIncreasingElementsAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2001_2050
+{
+    public class _2016_MaximumDifferenceBetweenIncreasingElementsAlg
+    {
+        public int MaximumDifference1(int[] nums)
+        {
+            int left = int.MaxValue;
+            int right = 0;
+            int[] arr = new int[nums.Length];
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                arr[i] = Math.Max(nums[i], right);
+                right = arr[i];
+            }
+            int ans = -1;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                left = Math.Min(left, nums[i]);
+                if (left < arr[i + 1])
+                {
+                    ans=Math.Max(ans, arr[i + 1]-left);
+                }
+
+            }
+
+            return ans;
+        }
+
+        public int MaximumDifference(int[] nums)
+        {
+            int n = nums.Length;
+            int ans = -1, premin = nums[0];
+            for (int i = 1; i < n; ++i)
+            {
+                if (nums[i] > premin)
+                {
+                    ans = Math.Max(ans, nums[i] - premin);
+                }
+                else
+                {
+                    premin = nums[i];
+                }
+            }
+            return ans;
+        }
+
+
+    }
+}
