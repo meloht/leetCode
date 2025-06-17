5e3851a1d7f42ef3666fca8413198ab843120543
hongtao_luo <luohongtao621@hotmail.com>
2025-06-17 16:58:40 +0800

    3405

---
+++ TestLeetCode/3401_3450/3405_CountTheNumberOfArraysWithKMatchingAdjacentElementsTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3401_3450;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3401_3450
+{
+    [TestClass]
+    public class _3405_CountTheNumberOfArraysWithKMatchingAdjacentElementsTest
+    {
+        _3405_CountTheNumberOfArraysWithKMatchingAdjacentElementsAlg alg = new _3405_CountTheNumberOfArraysWithKMatchingAdjacentElementsAlg(); 
+        [TestMethod]
+        public void Test01()
+        {
+            int n = 3, m = 2, k = 1;
+            int exp = 4;
+            int res = alg.CountGoodArrays(n, m, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int n = 4, m = 2, k = 2;
+            int exp = 6;
+            int res = alg.CountGoodArrays(n, m, k);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void Test03()
+        {
+            int n = 5, m = 2, k = 0;
+            int exp = 2;
+            int res = alg.CountGoodArrays(n, m, k);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/3401_3450/3405_CountTheNumberOfArraysWithKMatchingAdjacentElementsAlg.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._3401_3450
+{
+    public class _3405_CountTheNumberOfArraysWithKMatchingAdjacentElementsAlg
+    {
+        const int MOD = 1_000_000_007;
+        const int MX = 100000;
+        static long[] fac = new long[MX];
+        static long[] invF = new long[MX];
+
+        private static bool initialized = false;
+
+        private void init()
+        {
+            if (initialized)
+            {
+                return;
+            }
+            initialized = true;
+
+            fac[0] = 1;
+            for (int i = 1; i < MX; i++)
+            {
+                fac[i] = fac[i - 1] * i % MOD;
+            }
+
+            invF[MX - 1] = pow(fac[MX - 1], MOD - 2);
+            for (int i = MX - 1; i > 0; i--)
+            {
+                invF[i - 1] = invF[i] * i % MOD;
+            }
+        }
+        private long pow(long x, int n)
+        {
+            long res = 1;
+            for (; n > 0; n /= 2)
+            {
+                if (n % 2 > 0)
+                {
+                    res = res * x % MOD;
+                }
+                x = x * x % MOD;
+            }
+            return res;
+        }
+        private long comb(int n, int m)
+        {
+            return fac[n] * invF[m] % MOD * invF[n - m] % MOD;
+        }
+
+
+
+        public int CountGoodArrays(int n, int m, int k)
+        {
+            init();
+            return (int)(comb(n - 1, k) * m % MOD * pow(m - 1, n - k - 1) % MOD);
+
+        }
+    }
+}
