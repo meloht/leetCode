5b94ff1a1e292721e9a6972bd99667e401916eb5
hongtao_luo <luohongtao621@hotmail.com>
2025-06-19 12:48:43 +0800

    2294

---
+++ TestLeetCode/2251_2300/2294_PartitionArraySuchThatMaximumDifferenceIsKTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._2251_2300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2251_2300
+{
+    [TestClass]
+    public class _2294_PartitionArraySuchThatMaximumDifferenceIsKTest
+    {
+        _2294_PartitionArraySuchThatMaximumDifferenceIsKAlg alg = new _2294_PartitionArraySuchThatMaximumDifferenceIsKAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] nums = [3, 6, 1, 2, 5];
+            int k = 2;
+            int exp = 2;
+            int res=alg.PartitionArray(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] nums = [1, 2, 3];
+            int k = 1;
+            int exp = 2;
+            int res = alg.PartitionArray(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[] nums = [2, 2, 4, 5];
+            int k = 0;
+            int exp = 3;
+            int res = alg.PartitionArray(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test04()
+        {
+            int[] nums = [0];
+            int k = 0;
+            int exp = 1;
+            int res = alg.PartitionArray(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2251_2300/2294_PartitionArraySuchThatMaximumDifferenceIsKAlg.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2294_PartitionArraySuchThatMaximumDifferenceIsKAlg
+    {
+        public int PartitionArray1(int[] nums, int k)
+        {
+            if (nums.Length == 1)
+                return 1;
+            Array.Sort(nums);
+            int ans = 0;
+            int pre = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] - pre > k)
+                {
+                    ans++;
+                    pre = nums[i];
+                }
+                if (i == nums.Length - 1)
+                {
+                    ans++;
+                }
+            }
+
+            return ans;
+        }
+
+        public int PartitionArray(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int ans = 0;
+            int pre = int.MinValue / 2;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] - pre > k)
+                {
+                    ans++;
+                    pre = nums[i];
+                }
+
+            }
+
+            return ans;
+        }
+    }
+}
