c5fac6a8693f804f4b79c511ba1667a2ab74f8d5
luohongtao <luohongtao621@hotmail.com>
2025-06-20 17:53:22 +0800

    3443

---
+++ TestLeetCode/3401_3450/3443_MaximumManhattanDistanceAfterKChangesTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3401_3450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3401_3450
+{
+    [TestClass]
+    public class _3443_MaximumManhattanDistanceAfterKChangesTest
+    {
+        _3443_MaximumManhattanDistanceAfterKChangesAlg alg = new _3443_MaximumManhattanDistanceAfterKChangesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "NWSE";
+            int k = 1;
+            int exp = 3;
+            int res = alg.MaxDistance(s,k);
+            Assert.AreEqual(exp,res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "NSWWEW";
+            int k = 3;
+            int exp = 6;
+            int res = alg.MaxDistance(s, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
