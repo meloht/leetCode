a9f7447e757c8bd3ec5d042db2ee084cd0b18dfb
luohongtao <luohongtao621@hotmail.com>
2025-06-22 12:39:52 +0800

    2138

---
+++ leetCode/2101_2150/2138_DivideAStringIntoGroupsOfSizeKAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2101_2150
+{
+    public class _2138_DivideAStringIntoGroupsOfSizeKAlg
+    {
+        public string[] DivideString(string s, int k, char fill)
+        {
+            int n = s.Length;
+            string[] ans = new string[(n + k - 1) / k];
+            for (int i = 0; i < n; i += k)
+            {
+                if (i + k <= n)
+                {
+                    ans[i / k] = s.Substring(i, k);
+                }
+                else
+                {
+                    ans[i / k] = s.Substring(i).PadRight(k, fill);
+                }
+            }
+            return ans;
+
+        }
+    }
+}
