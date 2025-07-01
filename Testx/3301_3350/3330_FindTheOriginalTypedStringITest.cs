67a13a326f9c1a96ef7fc11e67080620607664f3
hongtao_luo <luohongtao621@hotmail.com>
2025-07-01 13:20:46 +0800

    3330

---
+++ Testx/3301_3350/3330_FindTheOriginalTypedStringITest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._3301_3350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3301_3350
+{
+    public class _3330_FindTheOriginalTypedStringITest
+    {
+        _3330_FindTheOriginalTypedStringIAlg alg = new _3330_FindTheOriginalTypedStringIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            string word = "abbcccc";
+            int exp = 5;
+            int res = alg.PossibleStringCount(word);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            string word = "abcd";
+            int exp = 1;
+            int res = alg.PossibleStringCount(word);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            string word = "aaaa";
+            int exp = 4;
+            int res = alg.PossibleStringCount(word);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3301_3350/3330_FindTheOriginalTypedStringIAlg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3301_3350
+{
+    public class _3330_FindTheOriginalTypedStringIAlg
+    {
+        public int PossibleStringCount(string word)
+        {
+            int n = word.Length, ans = 1;
+            for (int i = 1; i < n; ++i)
+            {
+                if (word[i - 1] == word[i])
+                {
+                    ++ans;
+                }
+            }
+            return ans;
+
+        }
+    }
+}
