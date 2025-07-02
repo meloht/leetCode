204d5ebb0573e721460c58b30fca0210a064eaf3
hongtao_luo <luohongtao621@hotmail.com>
2025-07-02 14:03:10 +0800

    3333

---
+++ Testx/3301_3350/3333_FindTheOriginalTypedStringIITest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._3301_3350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3301_3350
+{
+    public class _3333_FindTheOriginalTypedStringIITest
+    {
+        _3333_FindTheOriginalTypedStringIIAlg alg = new _3333_FindTheOriginalTypedStringIIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            string word = "aabbccdd";
+            int k = 7;
+            int exp = 5;
+            int res = alg.PossibleStringCount(word, k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            string word = "aabbccdd";
+            int k = 8;
+            int exp = 1;
+            int res = alg.PossibleStringCount(word, k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            string word = "aaabbb";
+            int k = 3;
+            int exp = 8;
+            int res = alg.PossibleStringCount(word, k);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3301_3350/3333_FindTheOriginalTypedStringIIAlg.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3301_3350
+{
+    public class _3333_FindTheOriginalTypedStringIIAlg
+    {
+        public int PossibleStringCount(string word, int k)
+        {
+            int n = word.Length;
+            if (n < k)
+                return 0;
+            int mod = 1_000_000_007;
+            List<int> cnts = new List<int>();
+            long ans = 1;
+            int cnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                cnt++;
+                if (i == n - 1 || word[i] != word[i + 1])
+                {
+                    if (cnt > 1)
+                    {
+                        if (k > 0)
+                        {
+                            cnts.Add(cnt - 1);
+                        }
+                        ans = ans * cnt % mod;
+                    }
+                    k--;
+                    cnt = 0;
+                }
+            }
+            if (k <= 0)
+            {
+                return (int)ans;
+            }
+            int m = cnts.Count;
+            int[][] f = new int[m + 1][];
+            for (int i = 0; i < f.Length; i++)
+            {
+                f[i] = new int[k];
+            }
+            Array.Fill(f[0], 1);
+            int[] s = new int[k + 1];
+            for (int i = 0; i < m; i++)
+            {
+                // 计算 f[i] 的前缀和数组 s
+                for (int j = 0; j < k; j++)
+                {
+                    s[j + 1] = (s[j] + f[i][j]) % mod;
+                }
+                int c = cnts[i];
+                // 计算子数组和
+                for (int j = 0; j < k; j++)
+                {
+                    f[i + 1][j] = (s[j + 1] - s[Math.Max(j - c, 0)]) % mod;
+                }
+            }
+
+            return (int)((ans - f[m][k - 1] + mod) % mod); // 保证结果非负
+
+        }
+    }
+}
