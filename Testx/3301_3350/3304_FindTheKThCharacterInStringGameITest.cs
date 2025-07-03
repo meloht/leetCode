9c95fee4c5ab5a9133871c35711a4cdfd6f3657b
hongtao_luo <luohongtao621@hotmail.com>
2025-07-03 12:28:00 +0800

    3304

---
+++ Testx/3301_3350/3304_FindTheKThCharacterInStringGameITest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._3301_3350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3301_3350
+{
+    public class _3304_FindTheKThCharacterInStringGameITest
+    {
+        _3304_FindTheKThCharacterInStringGameIAlg alg = new _3304_FindTheKThCharacterInStringGameIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int k = 5;
+            char exp = 'b';
+            char res = alg.KthCharacter(k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int k = 10;
+            char exp = 'c';
+            char res = alg.KthCharacter(k);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3301_3350/3304_FindTheKThCharacterInStringGameIAlg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3301_3350
+{
+    public class _3304_FindTheKThCharacterInStringGameIAlg
+    {
+        public char KthCharacter1(int k)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append('a');
+            while (sb.Length < k)
+            {
+                int count = sb.Length;
+                for (int i = 0; i < count; i++)
+                {
+                    if (sb[i] == 'z')
+                    {
+                        sb.Append('a');
+                    }
+                    else
+                    {
+                        sb.Append((char)(sb[i] + 1));
+                    }
+                }
+            }
+            return sb[k - 1];
+        }
+        public char KthCharacter2(int k)
+        {
+            int ans = 0;
+            int t;
+            while (k != 1)
+            {
+                t = (int)Math.Log(k, 2);
+                if ((1 << t) == k)
+                {
+                    t--;
+                }
+                k = k - (1 << t);
+                ans++;
+            }
+            return (char)('a' + ans);
+        }
+
+        public char KthCharacter(int k)
+        {
+            return (char)('a' + int.PopCount(k - 1));
+        }
+
+
+
+    }
+}
