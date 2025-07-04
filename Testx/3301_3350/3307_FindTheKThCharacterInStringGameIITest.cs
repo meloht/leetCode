d730b71576a21881b5e4ba6c962b46497b4cf4d6
hongtao_luo <luohongtao621@hotmail.com>
2025-07-04 12:58:17 +0800

    3307

---
+++ Testx/3301_3350/3307_FindTheKThCharacterInStringGameIITest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._3301_3350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3301_3350
+{
+    public class _3307_FindTheKThCharacterInStringGameIITest
+    {
+        _3307_FindTheKThCharacterInStringGameIIAlg alg = new _3307_FindTheKThCharacterInStringGameIIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            long k = 5;
+            int[] operations = [0, 0, 0];
+            char exp = 'a';
+            char res = alg.KthCharacter(k, operations);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            long k = 10;
+            int[] operations = [0, 1, 0, 1];
+            char exp = 'b';
+            char res = alg.KthCharacter(k, operations);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            long k = 12145134613;
+            int[] operations = [0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1];
+            char exp = 'i';
+            char res = alg.KthCharacter(k, operations);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3301_3350/3307_FindTheKThCharacterInStringGameIIAlg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3301_3350
+{
+    public class _3307_FindTheKThCharacterInStringGameIIAlg
+    {
+        public char KthCharacter(long k, int[] operations)
+        {
+            k--;
+            long inc = 0;
+            for (int i = 63 - (int)long.LeadingZeroCount(k); i >= 0; i--)
+            {
+                inc += k >> i & operations[i];
+            }
+            return (char)('a' + inc % 26);
+        }
+    }
+}
