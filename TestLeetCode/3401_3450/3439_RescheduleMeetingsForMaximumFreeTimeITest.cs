56d6916648118ca8139188d6f312f83cbfa757dd
hongtao_luo <luohongtao621@hotmail.com>
2025-07-09 13:09:31 +0800

    3439

---
+++ TestLeetCode/3401_3450/3439_RescheduleMeetingsForMaximumFreeTimeITest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._3401_3450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3401_3450
+{
+    [TestClass]
+    public class _3439_RescheduleMeetingsForMaximumFreeTimeITest
+    {
+        _3439_RescheduleMeetingsForMaximumFreeTimeIAlg alg = new _3439_RescheduleMeetingsForMaximumFreeTimeIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int eventTime = 5, k = 1;
+            int[] startTime = [1, 3], endTime = [2, 5];
+            int exp = 2;
+            int res = alg.MaxFreeTime(eventTime, k, startTime, endTime);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int eventTime = 10, k = 1;
+            int[] startTime = [0, 2, 9], endTime = [1, 4, 10];
+            int exp = 6;
+            int res = alg.MaxFreeTime(eventTime, k, startTime, endTime);
+            Assert.AreEqual(exp, res);
+        }
+
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int eventTime = 5, k = 2;
+            int[] startTime = [0, 1, 2, 3, 4], endTime = [1, 2, 3, 4, 5];
+            int exp = 0;
+            int res = alg.MaxFreeTime(eventTime, k, startTime, endTime);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3401_3450/3439_RescheduleMeetingsForMaximumFreeTimeIAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3401_3450
+{
+    public class _3439_RescheduleMeetingsForMaximumFreeTimeIAlg
+    {
+        public int MaxFreeTime(int eventTime, int k, int[] startTime, int[] endTime)
+        {
+            int ans = 0;
+            int s = 0;
+            for (int i = 0; i <= startTime.Length; i++)
+            {
+                s += get(i, eventTime, startTime, endTime);
+                if (i < k)
+                {
+                    continue;
+                }
+                ans = Math.Max(ans, s);
+                s -= get(i - k, eventTime, startTime, endTime);
+            }
+            return ans;
+
+
+        }
+        private int get(int i, int eventTime, int[] startTime, int[] endTime)
+        {
+            if (i == 0)
+            {
+                return startTime[0]; // 最左边的空余时间段
+            }
+            int n = startTime.Length;
+            if (i == n)
+            {
+                return eventTime - endTime[n - 1]; // 最右边的空余时间段
+            }
+            return startTime[i] - endTime[i - 1]; // 中间的空余时间段
+        }
+
+
+    }
+}
