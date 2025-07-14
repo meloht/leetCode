2a3c69d097bc567ffc1a4ed2fb4291f820c0890d
hongtao_luo <luohongtao621@hotmail.com>
2025-07-14 13:53:31 +0800

    1290

---
+++ TestLeetCode/1251_1300/1290_ConvertBinaryNumberInALinkedListToIntegerTest.cs
@@ -0,0 +1,66 @@
+﻿using leetCode;
+using leetCode._1251_1300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1251_1300
+{
+    [TestClass]
+    public class _1290_ConvertBinaryNumberInALinkedListToIntegerTest
+    {
+        _1290_ConvertBinaryNumberInALinkedListToIntegerAlg alg = new _1290_ConvertBinaryNumberInALinkedListToIntegerAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [1, 0, 1];
+            var node = ListNode.BuildListNode(head);
+            int exp = 5;
+            int res = alg.GetDecimalValue(node);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [0];
+            var node = ListNode.BuildListNode(head);
+            int exp = 0;
+            int res = alg.GetDecimalValue(node);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] head = [1];
+            var node = ListNode.BuildListNode(head);
+            int exp = 1;
+            int res = alg.GetDecimalValue(node);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] head = [1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0];
+            var node = ListNode.BuildListNode(head);
+            int exp = 18880;
+            int res = alg.GetDecimalValue(node);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            int[] head = [0, 0];
+            var node = ListNode.BuildListNode(head);
+            int exp = 0;
+            int res = alg.GetDecimalValue(node);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1251_1300/1290_ConvertBinaryNumberInALinkedListToIntegerAlg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1251_1300
+{
+    public class _1290_ConvertBinaryNumberInALinkedListToIntegerAlg
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            int ans = 0;
+            var node = head;
+            while (node != null)
+            {
+                ans <<= 1;
+                ans |= node.val;
+                node = node.next;
+            }
+            return ans;
+        }
+    }
+}
