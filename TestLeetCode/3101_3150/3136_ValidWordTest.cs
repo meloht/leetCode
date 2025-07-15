6aa699dd239793da7a36b00c4d51d3683857f26d
hongtao_luo <luohongtao621@hotmail.com>
2025-07-15 13:20:30 +0800

    3136

---
+++ TestLeetCode/3101_3150/3136_ValidWordTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3136_ValidWordTest
+    {
+        _3136_ValidWordAlg alg = new _3136_ValidWordAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string word = "234Adas";
+            bool exp = true;
+            bool res = alg.IsValid(word);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string word = "b3";
+            bool exp = false;
+            bool res = alg.IsValid(word);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string word = "a3$e";
+            bool exp = false;
+            bool res = alg.IsValid(word);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3136_ValidWordAlg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3136_ValidWordAlg
+    {
+        public bool IsValid(string word)
+        {
+            if (word.Length < 3)
+                return false;
+
+            bool ch1 = false;
+            bool ch2 = false;
+
+            foreach (var item in word)
+            {
+
+                if (char.IsLetter(item))
+                {
+                    char aa = char.ToLower(item);
+                    if (aa == 'a' || aa == 'e' || aa == 'i' || aa == 'o' || aa == 'u')
+                    {
+                        ch1 = true;
+                    }
+                    else
+                    {
+                        ch2 = true;
+                    }
+                }
+                else if (!char.IsNumber(item))
+                {
+                    return false;
+                }
+
+            }
+            return ch1 && ch2;
+        }
+    }
+}
