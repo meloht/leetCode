ac70f20608e1a3819556f038e17b58a5de065672
hongtao_luo <luohongtao621@hotmail.com>
2025-07-16 14:00:43 +0800

    3201

---
+++ TestLeetCode/3201_3250/3201_FindTheMaximumLengthOfValidSubsequenceITest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3201_FindTheMaximumLengthOfValidSubsequenceITest
+    {
+        _3201_FindTheMaximumLengthOfValidSubsequenceIAlg alg = new _3201_FindTheMaximumLengthOfValidSubsequenceIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int exp = 4;
+            int res = alg.MaximumLength(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 1, 1, 2, 1, 2];
+            int exp = 6;
+            int res = alg.MaximumLength(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [1, 3];
+            int exp = 2;
+            int res = alg.MaximumLength(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3201_FindTheMaximumLengthOfValidSubsequenceIAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3201_FindTheMaximumLengthOfValidSubsequenceIAlg
+    {
+        public int MaximumLength(int[] nums)
+        {
+            int ans = 0;
+            for (int m = 0; m < 2; m++)
+            {
+                int[] f = new int[2];
+
+                foreach (int item in nums)
+                {
+                    int x = item;
+                    x %= 2;
+                    f[x] = f[(m - x + 2) % 2] + 1;
+                    ans = Math.Max(ans, f[x]);
+                }
+            }
+            return ans;
+
+        }
+    }
+}
