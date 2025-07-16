ef2703769c0f0964460686db30201e6ef4f42de2
hongtao_luo <luohongtao621@hotmail.com>
2025-07-16 14:45:30 +0800

    1027

---
+++ Testx/1001_1050/1027_LongestArithmeticSubsequenceTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1001_1050
+{
+    public class _1027_LongestArithmeticSubsequenceTest
+    {
+        _1027_LongestArithmeticSubsequenceAlg alg = new _1027_LongestArithmeticSubsequenceAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [3, 6, 9, 12];
+            int exp = 4;
+            int res = alg.LongestArithSeqLength(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [9, 4, 7, 2, 10];
+            int exp = 3;
+            int res = alg.LongestArithSeqLength(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] nums = [20, 1, 15, 3, 10, 5, 8];
+            int exp = 4;
+            int res = alg.LongestArithSeqLength(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1027_LongestArithmeticSubsequenceAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1027_LongestArithmeticSubsequenceAlg
+    {
+        public int LongestArithSeqLength(int[] nums)
+        {
+            int ans = 0;
+            int m = nums.Max();
+           
+            for (int d = -m; d <= m; d++)
+            {
+                int[] f = new int[m + 1];
+                foreach (int x in nums)
+                {
+                    f[x] = 0 <= x - d && x - d <= m ? f[x - d] + 1 : 1;
+                    ans = Math.Max(ans, f[x]);
+                }
+            }
+            return ans;
+
+        }
+    }
+}
