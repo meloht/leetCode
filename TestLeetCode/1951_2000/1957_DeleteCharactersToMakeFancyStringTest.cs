ea525acc6f6975fd9d12164939f6bb6d55d9355c
hongtao_luo <luohongtao621@hotmail.com>
2025-07-21 17:08:41 +0800

    1957

---
+++ TestLeetCode/1951_2000/1957_DeleteCharactersToMakeFancyStringTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1951_2000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1951_2000
+{
+    [TestClass]
+    public class _1957_DeleteCharactersToMakeFancyStringTest
+    {
+        _1957_DeleteCharactersToMakeFancyStringAlg alg = new _1957_DeleteCharactersToMakeFancyStringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "leeetcode";
+            string exp = "leetcode";
+            string res = alg.MakeFancyString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "aaabaaaa";
+            string exp = "aabaa";
+            string res = alg.MakeFancyString(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "aab";
+            string exp = "aab";
+            string res = alg.MakeFancyString(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1951_2000/1957_DeleteCharactersToMakeFancyStringAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1951_2000
+{
+    public class _1957_DeleteCharactersToMakeFancyStringAlg
+    {
+        public string MakeFancyString(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (sb.Length < 2)
+                {
+                    sb.Append(s[i]);
+                }
+                else
+                {
+                    if (sb[sb.Length - 2] == s[i] && sb[sb.Length - 1] == s[i])
+                    {
+                        continue;
+                    }
+                    sb.Append(s[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
