cc7c54beff8ea2e6753e2cac1bac9d23ec9a1c14
hongtao_luo <luohongtao621@hotmail.com>
2025-07-22 12:52:52 +0800

    1695

---
+++ TestLeetCode/1651_1700/1695_MaximumErasureValueTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1651_1700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1651_1700
+{
+    [TestClass]
+    public class _1695_MaximumErasureValueTest
+    {
+        _1695_MaximumErasureValueAlg alg = new _1695_MaximumErasureValueAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [4, 2, 4, 5, 6];
+            int exp = 17;
+            int res = alg.MaximumUniqueSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [5, 2, 1, 2, 5, 2, 1, 2, 5];
+            int exp = 8;
+            int res = alg.MaximumUniqueSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1651_1700/1695_MaximumErasureValueAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1651_1700
+{
+    public class _1695_MaximumErasureValueAlg
+    {
+        public int MaximumUniqueSubarray(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+            int ans = 0;
+            int total = 0;
+            for (int i = 0, j = 0; i < nums.Length; i++)
+            {
+                while (!set.Add(nums[i]))
+                {
+                    set.Remove(nums[j]);
+                    total -= nums[j];
+                    j++;
+                }
+                total += nums[i];
+
+                ans = Math.Max(total, ans);
+            }
+
+            return ans;
+        }
+    }
+}
