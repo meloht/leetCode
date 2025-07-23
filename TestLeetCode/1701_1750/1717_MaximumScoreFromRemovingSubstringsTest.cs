58060640232ffef424e583a172a3357224d503ab
hongtao_luo <luohongtao621@hotmail.com>
2025-07-23 13:48:25 +0800

    1717

---
+++ TestLeetCode/1701_1750/1717_MaximumScoreFromRemovingSubstringsTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._1701_1750;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1701_1750
+{
+    [TestClass]
+    public class _1717_MaximumScoreFromRemovingSubstringsTest
+    {
+        _1717_MaximumScoreFromRemovingSubstringsAlg alg = new _1717_MaximumScoreFromRemovingSubstringsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "cdbcbbaaabab";
+            int x = 4, y = 5;
+            int exp = 19;
+            int res = alg.MaximumGain(s, x, y);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "aabbaaxybbaabb";
+            int x = 5, y = 4;
+            int exp = 20;
+            int res = alg.MaximumGain(s, x, y);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "aabbabkbbbfvybssbtaobaaaabataaadabbbmakgabbaoapbbbbobaabvqhbbzbbkapabaavbbeghacabamdpaaqbqabbjbababmbakbaabajabasaabbwabrbbaabbafubayaazbbbaababbaaha";
+            int x = 1926, y = 4320;
+            int exp = 112374;
+            int res = alg.MaximumGain(s, x, y);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1701_1750/1717_MaximumScoreFromRemovingSubstringsAlg.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1701_1750
+{
+    public class _1717_MaximumScoreFromRemovingSubstringsAlg
+    {
+        public int MaximumGain(string s, int x, int y)
+        {
+            Stack<char> stack = new Stack<char>();
+            int ans = 0;
+            foreach (var item in s)
+            {
+                if (x > y)
+                {
+                    if (stack.Count > 0 && stack.Peek() == 'a' && item == 'b')
+                    {
+                        stack.Pop();
+                        ans += x;
+                        continue;
+                    }
+
+                }
+                else
+                {
+                    if (stack.Count > 0 && stack.Peek() == 'b' && item == 'a')
+                    {
+                        stack.Pop();
+                        ans += y;
+                        continue;
+                    }
+                }
+                stack.Push(item);
+
+            }
+            Stack<char> stack2 = new Stack<char>();
+            while (stack.Count > 0)
+            {
+                var item = stack.Pop();
+                if (stack2.Count > 0 && stack2.Peek() == 'a' && item == 'b')
+                {
+                    stack2.Pop();
+                    ans += y;
+                }
+                else if (stack2.Count > 0 && stack2.Peek() == 'b' && item == 'a')
+                {
+                    stack2.Pop();
+                    ans += x;
+                }
+                else
+                {
+                    stack2.Push(item);
+                }
+               
+            }
+            return ans;
+        }
+    }
+}
