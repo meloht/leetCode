5a78c60a51d8154d50333b28167552e098ede404
hongtao_luo <luohongtao621@hotmail.com>
2025-07-25 11:59:48 +0800

    3487

---
+++ Testx/3451_3500/3487_MaximumUniqueSubarraySumAfterDeletionTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._3451_3500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3451_3500
+{
+    public class _3487_MaximumUniqueSubarraySumAfterDeletionTest
+    {
+        _3487_MaximumUniqueSubarraySumAfterDeletionAlg alg = new _3487_MaximumUniqueSubarraySumAfterDeletionAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [1, 2, 3, 4, 5];
+            int exp = 15;
+            int res = alg.MaxSum(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [1, 1, 0, 1, 1];
+            int exp = 1;
+            int res = alg.MaxSum(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] nums = [1, 2, -1, -2, 1, 0, -1];
+            int exp = 3;
+            int res = alg.MaxSum(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3451-3500/3487_MaximumUniqueSubarraySumAfterDeletionAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3451_3500
+{
+    public class _3487_MaximumUniqueSubarraySumAfterDeletionAlg
+    {
+        public int MaxSum(int[] nums)
+        {
+            int ans = 0;
+            HashSet<int> set = new HashSet<int>();
+            int max = int.MinValue;
+            foreach (var item in nums)
+            {
+                if (item > 0 && set.Add(item))
+                {
+                    ans += item;
+                }
+                max = Math.Max(max, item);
+            }
+            if (max > 0)
+            {
+                return ans;
+            }
+            return max;
+        }
+    }
+}
