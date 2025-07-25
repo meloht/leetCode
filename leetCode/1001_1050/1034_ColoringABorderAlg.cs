cac474a7d38402081f122c0d37b2362720ff1928
hongtao_luo <luohongtao621@hotmail.com>
2025-07-25 12:09:58 +0800

    1050

---
+++ leetCode/1001_1050/1034_ColoringABorderAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1034_ColoringABorderAlg
+    {
+        public int[][] ColorBorder(int[][] grid, int row, int col, int color)
+        {
+            return null;
+        }
+    }
+}
