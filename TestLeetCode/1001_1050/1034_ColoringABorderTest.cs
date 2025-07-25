156d723bdcf62b0a2b609f04b5a1ded90e1d17ca
hongtao_luo <luohongtao621@hotmail.com>
2025-07-25 13:46:10 +0800

    1034

---
+++ TestLeetCode/1001_1050/1034_ColoringABorderTest.cs
@@ -0,0 +1,58 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1001_1050
+{
+    [TestClass]
+    public class _1034_ColoringABorderTest
+    {
+        _1034_ColoringABorderAlg alg = new _1034_ColoringABorderAlg();
+        [TestMethod]
+        public void Test01()
+        {
+            int[][] grid = [
+                [1, 1], 
+                [1, 2]];
+            int row = 0, col = 0, color = 3;
+            int[][] exp = [
+                [3, 3], 
+                [3, 2]];
+            int[][] res = alg.ColorBorder(grid, row, col, color);
+            Assert.IsTrue(Utils.IsSameArr(exp, res));
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[][] grid = [
+                [1, 2, 2], 
+                [2, 3, 2]];
+            int row = 0, col = 1, color = 3;
+            int[][] exp = [
+                [1, 3, 3], 
+                [2, 3, 3]];
+            int[][] res = alg.ColorBorder(grid, row, col, color);
+            Assert.IsTrue(Utils.IsSameArr(exp, res));
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[][] grid = [
+                [1, 1, 1], 
+                [1, 1, 1], 
+                [1, 1, 1]];
+            int row = 1, col = 1, color = 2;
+            int[][] exp = [
+                [2, 2, 2], 
+                [2, 1, 2], 
+                [2, 2, 2]];
+            int[][] res = alg.ColorBorder(grid, row, col, color);
+            Assert.IsTrue(Utils.IsSameArr(exp, res));
+        }
+    }
+}
--- leetCode/1001_1050/1034_ColoringABorderAlg.cs
+++ leetCode/1001_1050/1034_ColoringABorderAlg.cs
@@ -10,7 +10,58 @@ namespace leetCode._1001_1050
     {
         public int[][] ColorBorder(int[][] grid, int row, int col, int color)
         {
-            return null;
+            int m = grid.Length;
+            int n = grid[0].Length;
+            bool[,] used = new bool[m, n];
+            int val = grid[row][col];
+
+            Queue<int[]> queue = new Queue<int[]>();
+            queue.Enqueue([row, col]);
+            used[row, col] = true;
+            int[][] dir = [[0, 1], [1, 0], [-1, 0], [0, -1]];
+            List<int[]> list = new List<int[]>();
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var item = queue.Dequeue();
+                    int r = item[0];
+                    int c = item[1];
+
+                    foreach (var ds in dir)
+                    {
+                        int x = r + ds[0];
+                        int y = c + ds[1];
+                        if (x >= 0 && x < m && y >= 0 && y < n && !used[x, y])
+                        {
+                            if (grid[x][y] != val)
+                            {
+                                list.Add([r, c]);
+                            }
+                            else
+                            {
+                                used[x, y] = true;
+                                queue.Enqueue([x, y]);
+                            }
+                        }
+
+                    }
+                    if (r == 0 || r == m - 1 || c == 0 || c == n - 1)
+                    {
+                        list.Add([r, c]);
+                    }
+                }
+            }
+
+            foreach (var item in list)
+            {
+                grid[item[0]][item[1]] = color;
+            }
+           
+
+
+            return grid;
         }
     }
 }
