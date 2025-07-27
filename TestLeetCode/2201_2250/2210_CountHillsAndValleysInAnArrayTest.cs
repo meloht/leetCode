d30f46f7e3940728b5eaa9901952a20f740815fe
luohongtao <luohongtao621@hotmail.com>
2025-07-27 11:50:20 +0800

    2210

---
+++ TestLeetCode/2201_2250/2210_CountHillsAndValleysInAnArrayTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2201_2250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2201_2250
+{
+    [TestClass]
+    public class _2210_CountHillsAndValleysInAnArrayTest
+    {
+        _2210_CountHillsAndValleysInAnArrayAlg alg = new _2210_CountHillsAndValleysInAnArrayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 4, 1, 1, 6, 5];
+            int exp = 3;
+            int res = alg.CountHillValley(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [6, 6, 5, 5, 4, 1];
+            int exp = 0;
+            int res = alg.CountHillValley(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+      
+    }
+}
---
+++ leetCode/2201_2250/2210_CountHillsAndValleysInAnArrayAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2201_2250
+{
+    public class _2210_CountHillsAndValleysInAnArrayAlg
+    {
+        public int CountHillValley(int[] nums)
+        {
+            int ans = 0;
+            int pre = nums[0];
+            for (int i = 1; i < nums.Length - 1; i++)
+            {
+                int cur = nums[i];
+                int nxt = nums[i + 1];
+                if (cur == nxt)
+                {
+                    continue;
+                }
+                if (pre != cur && (pre < cur == cur > nxt))
+                {
+                    ans++;
+                }
+                pre = cur;
+            }
+            return ans;
+        }
+    }
+}
