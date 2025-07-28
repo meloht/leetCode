33249a738a145ad0d00645c82636c04ac9207230
hongtao_luo <luohongtao621@hotmail.com>
2025-07-28 14:24:47 +0800

    2044

---
+++ TestLeetCode/2001_2050/2044_CountNumberOfMaximumBitwiseOrSubsetsTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2001_2050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2001_2050
+{
+    [TestClass]
+    public class _2044_CountNumberOfMaximumBitwiseOrSubsetsTest
+    {
+        _2044_CountNumberOfMaximumBitwiseOrSubsetsAlg alg = new _2044_CountNumberOfMaximumBitwiseOrSubsetsAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] nums = [3, 1];
+            int exp = 2;
+            int res = alg.CountMaxOrSubsets(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] nums = [2, 2, 2];
+            int exp = 7;
+            int res = alg.CountMaxOrSubsets(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[] nums = [3, 2, 1, 5];
+            int exp = 6;
+            int res = alg.CountMaxOrSubsets(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2001_2050/2044_CountNumberOfMaximumBitwiseOrSubsetsAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2001_2050
+{
+    public class _2044_CountNumberOfMaximumBitwiseOrSubsetsAlg
+    {
+        private int ans = 0;
+        public int CountMaxOrSubsets(int[] nums)
+        {
+            int totalOr = 0;
+            foreach (int num in nums) 
+            {
+                totalOr |= num;
+            }
+            dfs(0, 0, nums, totalOr);
+            return ans;
+        }
+        private void dfs(int i, int subsetOr, int[] nums, int totalOr)
+        {
+            if (subsetOr == totalOr)
+            {
+                ans += 1 << (nums.Length - i);
+                return;
+            }
+            if (i == nums.Length)
+            {
+                return;
+            }
+            dfs(i + 1, subsetOr, nums, totalOr); // 不选 nums[i]
+            dfs(i + 1, subsetOr | nums[i], nums, totalOr); // 选 nums[i]
+        }
+
+
+    }
+}
