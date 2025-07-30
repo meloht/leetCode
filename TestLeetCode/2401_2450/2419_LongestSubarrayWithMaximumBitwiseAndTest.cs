e6e2420c3f9f62723e120be9342e516ba16c65e7
hongtao_luo <luohongtao621@hotmail.com>
2025-07-30 12:12:38 +0800

    2419

---
+++ TestLeetCode/2401_2450/2419_LongestSubarrayWithMaximumBitwiseAndTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2401_2450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2401_2450
+{
+    [TestClass]
+    public class _2419_LongestSubarrayWithMaximumBitwiseAndTest
+    {
+        _2419_LongestSubarrayWithMaximumBitwiseAndAlg alg = new _2419_LongestSubarrayWithMaximumBitwiseAndAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3, 3, 2, 2];
+            int exp = 2;
+            int res = alg.LongestSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4];
+            int exp = 1;
+            int res = alg.LongestSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2401_2450/2419_LongestSubarrayWithMaximumBitwiseAndAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2401_2450
+{
+    public class _2419_LongestSubarrayWithMaximumBitwiseAndAlg
+    {
+        public int LongestSubarray(int[] nums)
+        {
+            int ans = 0;
+            int mx = 0;
+            int cnt = 0;
+            foreach (int x in nums)
+            {
+                if (x > mx)
+                {
+                    // 发现新的 mx，重新统计所有内容
+                    mx = x;
+                    cnt = 1;
+                    ans = 1;
+                }
+                else if (x == mx)
+                {
+                    cnt++;
+                    ans = Math.Max(ans, cnt);
+                }
+                else
+                {
+                    cnt = 0; // 连续 mx 断开了，重新统计
+                }
+            }
+            return ans;
+
+
+        }
+    }
+}
