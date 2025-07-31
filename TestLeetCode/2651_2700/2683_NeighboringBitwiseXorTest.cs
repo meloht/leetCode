e592d2fc65527c7a98b7df4bcdee51617bfc3b15
hongtao_luo <luohongtao621@hotmail.com>
2025-07-31 14:15:23 +0800

    2683

---
+++ TestLeetCode/2651_2700/2683_NeighboringBitwiseXorTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2651_2700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2651_2700
+{
+    [TestClass]
+    public class _2683_NeighboringBitwiseXorTest
+    {
+        _2683_NeighboringBitwiseXorAlg alg = new _2683_NeighboringBitwiseXorAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] derived = [1, 1, 0];
+            bool exp = true;
+            bool res = alg.DoesValidArrayExist(derived);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] derived = [1, 1];
+            bool exp = true;
+            bool res = alg.DoesValidArrayExist(derived);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[] derived = [1, 0];
+            bool exp = false;
+            bool res = alg.DoesValidArrayExist(derived);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2651_2700/2683_NeighboringBitwiseXorAlg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2651_2700
+{
+    public class _2683_NeighboringBitwiseXorAlg
+    {
+        public bool DoesValidArrayExist(int[] derived)
+        {
+            int xor = 0;
+            foreach (int x in derived)
+            {
+                xor ^= x;
+            }
+            return xor == 0;
+
+        }
+    }
+}
