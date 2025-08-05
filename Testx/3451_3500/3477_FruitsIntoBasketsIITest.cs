e704c8a519531eb08cf603bfc8b8fc50dd6cd7d2
hongtao_luo <luohongtao621@hotmail.com>
2025-08-05 13:01:05 +0800

    3477

---
+++ Testx/3451_3500/3477_FruitsIntoBasketsIITest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._3451_3500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3451_3500
+{
+    public class _3477_FruitsIntoBasketsIITest
+    {
+        _3477_FruitsIntoBasketsIIAlg alg = new _3477_FruitsIntoBasketsIIAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] fruits = [4, 2, 5], baskets = [3, 5, 4];
+            int exp = 1;
+            int res = alg.NumOfUnplacedFruits(fruits, baskets);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] fruits = [3, 6, 1], baskets = [6, 4, 7];
+            int exp = 0;
+            int res = alg.NumOfUnplacedFruits(fruits, baskets);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3451-3500/3477_FruitsIntoBasketsIIAlg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3451_3500
+{
+    public class _3477_FruitsIntoBasketsIIAlg
+    {
+        public int NumOfUnplacedFruits(int[] fruits, int[] baskets)
+        {
+
+            int ans = 0;
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                bool bl = false;
+                for (int j = 0; j < fruits.Length; j++)
+                {
+                    if ( fruits[i]<= baskets[j])
+                    {
+                        baskets[j] = 0;
+                        bl = true;
+                        break;
+                    }
+                }
+                if (!bl)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
