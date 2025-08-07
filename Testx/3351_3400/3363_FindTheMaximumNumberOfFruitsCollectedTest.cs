ec55179c81f7ae6487ae163014042000d4b72129
hongtao_luo <luohongtao621@hotmail.com>
2025-08-07 13:32:17 +0800

    3363

---
+++ Testx/3351_3400/3363_FindTheMaximumNumberOfFruitsCollectedTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._3351_3400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3351_3400
+{
+    public class _3363_FindTheMaximumNumberOfFruitsCollectedTest
+    {
+        _3363_FindTheMaximumNumberOfFruitsCollectedAlg alg = new _3363_FindTheMaximumNumberOfFruitsCollectedAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            int[][] fruits = [[1, 2, 3, 4], [5, 6, 8, 7], [9, 10, 11, 12], [13, 14, 15, 16]];
+            int exp = 100;
+            int res = alg.MaxCollectedFruits(fruits);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[][] fruits = [[1, 1], [1, 1]];
+            int exp = 4;
+            int res = alg.MaxCollectedFruits(fruits);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3351_3400/3363_FindTheMaximumNumberOfFruitsCollectedAlg.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3351_3400
+{
+    public class _3363_FindTheMaximumNumberOfFruitsCollectedAlg
+    {
+
+        public int MaxCollectedFruits(int[][] fruits)
+        {
+            int n = fruits.Length;
+            int ans = 0;
+            // 从 (0, 0) 出发的小朋友
+            for (int i = 0; i < n; i++)
+            {
+                ans += fruits[i][i];
+            }
+
+            // 从 (0, n - 1) 出发的小朋友
+            ans += dp(fruits);
+
+            // 从 (n - 1, 0) 出发的小朋友（按照主对角线翻转）
+            // 把下三角形中的数据填到上三角形中
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    fruits[j][i] = fruits[i][j];
+                }
+            }
+            return ans + dp(fruits);
+        }
+
+        int dp(int[][] fruits)
+        {
+            int n = fruits.Length;
+            int[,] f = new int[n - 1,n + 1];
+            f[0,n - 1] = fruits[0][n - 1];
+            for (int i = 1; i < n - 1; i++)
+            {
+                for (int j = Math.Max(n - 1 - i, i + 1); j < n; j++)
+                {
+                    f[i,j] = Math.Max(Math.Max(f[i - 1, j - 1], f[i - 1, j]), f[i - 1, j + 1]) + fruits[i][j];
+                }
+            }
+            return f[n - 2, n - 1];
+        }
+
+    }
+}
