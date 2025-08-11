956d719e841e8ea0ded1bbe8dee851120a52ded8
hongtao_luo <luohongtao621@hotmail.com>
2025-08-11 12:51:15 +0800

    2438

---
+++ Testx/2401_2450/2438_RangeProductQueriesOfPowersTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._2401_2450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._2401_2450
+{
+    public class _2438_RangeProductQueriesOfPowersTest
+    {
+        _2438_RangeProductQueriesOfPowersAlg alg = new _2438_RangeProductQueriesOfPowersAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            int n = 15;
+            int[][] queries = [[0, 1], [2, 2], [0, 3]];
+            int[] exp = [2, 4, 64];
+            int[] res = alg.ProductQueries(n, queries);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int n = 2;
+            int[][] queries = [[0, 0]];
+            int[] exp = [2];
+            int[] res = alg.ProductQueries(n, queries);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/2401_2450/2438_RangeProductQueriesOfPowersAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2401_2450
+{
+    public class _2438_RangeProductQueriesOfPowersAlg
+    {
+        public int[] ProductQueries(int n, int[][] queries)
+        {
+            int MOD = 1_000_000_007;
+            // 例如二进制 1100 分解为 100 + 1000
+            // 第一轮循环 lowbit(1100) = 100，然后 1100 ^ 100 = 1000
+            // 第二轮循环 lowbit(1000) = 1000，然后 1000 ^ 1000 = 0，循环结束
+            List<int> powers = new List<int>();
+            while (n > 0)
+            {
+                int lowbit = n & -n;
+                powers.Add(lowbit);
+                n ^= lowbit;
+            }
+
+            int[] ans = new int[queries.Length];
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int[] q = queries[i];
+                long mul = 1;
+                for (int j = q[0]; j <= q[1]; j++)
+                {
+                    mul = mul * powers[j] % MOD;
+                }
+                ans[i] = (int)mul;
+            }
+            return ans;
+        }
+
+        
+    }
+}
