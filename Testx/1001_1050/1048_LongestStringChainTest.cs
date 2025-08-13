f474f6ebba707ba293c5d1bbd17d9e0a603644ce
hongtao_luo <luohongtao621@hotmail.com>
2025-08-13 13:37:21 +0800

    1048

---
+++ Testx/1001_1050/1048_LongestStringChainTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1001_1050
+{
+    public class _1048_LongestStringChainTest
+    {
+        _1048_LongestStringChainAlg alg = new _1048_LongestStringChainAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            string[] words = ["a", "b", "ba", "bca", "bda", "bdca"];
+            int exp = 4;
+            int res = alg.LongestStrChain(words);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            string[] words = ["xbc", "pcxbcf", "xb", "cxbc", "pcxbc"];
+            int exp = 5;
+            int res = alg.LongestStrChain(words);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            string[] words = ["abcd", "dbqca"];
+            int exp = 1;
+            int res = alg.LongestStrChain(words);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1048_LongestStringChainAlg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1048_LongestStringChainAlg
+    {
+        private Dictionary<string, int> dict = new Dictionary<string, int>();
+        public int LongestStrChain(string[] words)
+        {
+            foreach (string word in words)
+            {
+               
+                if (!dict.ContainsKey(word))
+                {
+                    dict.Add(word, 0);
+                }
+            }
+
+            int ans = 0;
+            foreach (var item in dict.Keys)
+            {
+                ans = Math.Max(ans, Dfs(item));
+            }
+
+            return ans;
+        }
+        private int Dfs(string s)
+        {
+            int res = dict[s];
+            if (res > 0)
+            {
+                return res;
+            }
+            if (s.Length == 1)
+            {
+                dict[s] = res + 1;
+                return res + 1;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                string t = s.Substring(0, i) + s.Substring(i + 1);
+
+                if (dict.ContainsKey(t))
+                {
+                    res = Math.Max(res, Dfs(t));
+                }
+            }
+            dict[s] = res + 1;
+            return res + 1;
+        }
+    }
+}
