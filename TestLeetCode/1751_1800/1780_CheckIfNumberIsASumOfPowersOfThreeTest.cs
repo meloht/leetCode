d13a741f2d4ef5d5291109e390b9ee5ed14ea301
hongtao_luo <luohongtao621@hotmail.com>
2025-08-14 12:19:53 +0800

    1780

---
+++ TestLeetCode/1751_1800/1780_CheckIfNumberIsASumOfPowersOfThreeTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1751_1800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1751_1800
+{
+    [TestClass]
+    public class _1780_CheckIfNumberIsASumOfPowersOfThreeTest
+    {
+        _1780_CheckIfNumberIsASumOfPowersOfThreeAlg alg = new _1780_CheckIfNumberIsASumOfPowersOfThreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 12;
+            bool exp = true;
+            bool res = alg.CheckPowersOfThree(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 91;
+            bool exp = true;
+            bool res = alg.CheckPowersOfThree(n);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 21;
+            bool exp = false;
+            bool res = alg.CheckPowersOfThree(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1751_1800/1780_CheckIfNumberIsASumOfPowersOfThreeAlg.cs
@@ -0,0 +1,29 @@
+﻿using leetCode._1_50;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1751_1800
+{
+    public class _1780_CheckIfNumberIsASumOfPowersOfThreeAlg
+    {
+        public bool CheckPowersOfThree(int n)
+        {
+            while (n > 0)
+            {
+                int num = n % 3;
+                if (num == 0 || num == 1)
+                {
+                    n /= 3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
