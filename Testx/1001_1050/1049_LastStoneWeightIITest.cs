19fc4889bdd4b50ddd01207b80288921498fa2b9
hongtao_luo <luohongtao621@hotmail.com>
2025-08-15 13:02:02 +0800

    1049

---
+++ Testx/1001_1050/1049_LastStoneWeightIITest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._1001_1050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._1001_1050
+{
+    public class _1049_LastStoneWeightIITest
+    {
+        _1049_LastStoneWeightIIAlg alg = new _1049_LastStoneWeightIIAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            int[] stones = [2, 7, 4, 1, 8, 1];
+            int exp = 1;
+            int res= alg.LastStoneWeightII(stones);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] stones = [31, 26, 33, 21, 40];
+            int exp = 5;
+            int res = alg.LastStoneWeightII(stones);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/1001_1050/1049_LastStoneWeightIIAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1001_1050
+{
+    public class _1049_LastStoneWeightIIAlg
+    {
+        public int LastStoneWeightII(int[] stones)
+        {
+            int n = stones.Length;
+            int sum = 0;
+            foreach (int i in stones) sum += i;
+            int t = sum / 2;
+            int[] f = new int[t + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                int x = stones[i - 1];
+                for (int j = t; j >= x; j--)
+                {
+                    f[j] = Math.Max(f[j], f[j - x] + x);
+                }
+            }
+            return Math.Abs(sum - f[t] - f[t]);
+
+
+        }
+    }
+}
