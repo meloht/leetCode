92b7611de97d1296d7782f8498ee1030f3441cfd
hongtao_luo <luohongtao621@hotmail.com>
2025-08-19 12:50:28 +0800

    2348

---
+++ TestLeetCode/2301_2350/2348_NumberOfZeroFilledSubarraysTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._2301_2350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2301_2350
+{
+    [TestClass]
+    public class _2348_NumberOfZeroFilledSubarraysTest
+    {
+        _2348_NumberOfZeroFilledSubarraysAlg alg = new _2348_NumberOfZeroFilledSubarraysAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 3, 0, 0, 2, 0, 0, 4];
+
+            long exp = 6;
+            long res = alg.ZeroFilledSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [0, 0, 0, 2, 0, 0];
+
+            long exp = 9;
+            long res = alg.ZeroFilledSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [2, 10, 2019];
+
+            long exp = 0;
+            long res = alg.ZeroFilledSubarray(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2301_2350/2348_NumberOfZeroFilledSubarraysAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2301_2350
+{
+    public class _2348_NumberOfZeroFilledSubarraysAlg
+    {
+        public long ZeroFilledSubarray(int[] nums)
+        {
+            long ans = 0;
+            int len = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    len++;
+                    ans += len;
+                }
+                else
+                {
+                    len = 0;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
