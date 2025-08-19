9960d65b8a094332c549608d17ce3147508a488b
hongtao_luo <luohongtao621@hotmail.com>
2025-08-19 13:45:57 +0800

    1053

--- TestLeetCode/1051_1100/1053_PreviousPermutationWithOneSwapTest.cs
+++ TestLeetCode/1051_1100/1053_PreviousPermutationWithOneSwapTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using leetCode._1051_1100;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,36 @@ using System.Threading.Tasks;
 
 namespace Test._1051_1100
 {
+    [TestClass]
     public class _1053_PreviousPermutationWithOneSwapTest
     {
+        _1053_PreviousPermutationWithOneSwapAlg alg = new _1053_PreviousPermutationWithOneSwapAlg();
 
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr = [3, 2, 1];
+            int[] exp = [3, 1, 2];
+            int[] res = alg.PrevPermOpt1(arr);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr = [1, 1, 5];
+            int[] exp = [1, 1, 5];
+            int[] res = alg.PrevPermOpt1(arr);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] arr = [1, 9, 4, 6, 7];
+            int[] exp = [1, 7, 4, 6, 9];
+            int[] res = alg.PrevPermOpt1(arr);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
     }
 }
--- leetCode/1051_1100/1053_PreviousPermutationWithOneSwapAlg.cs
+++ leetCode/1051_1100/1053_PreviousPermutationWithOneSwapAlg.cs
@@ -10,7 +10,25 @@ namespace leetCode._1051_1100
     {
         public int[] PrevPermOpt1(int[] arr)
         {
-            return null;
+
+            int n = arr.Length;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    int j = n - 1;
+                    while (arr[j] >= arr[i] || arr[j] == arr[j - 1])
+                    {
+                        j--;
+                    }
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    break;
+                }
+            }
+            return arr;
+
         }
     }
 }
