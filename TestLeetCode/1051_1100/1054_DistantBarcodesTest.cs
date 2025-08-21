4a5434e85560d7885c33e82e1adbbb6d9a36b9a7
hongtao_luo <luohongtao621@hotmail.com>
2025-08-21 15:52:28 +0800

    1054

---
+++ TestLeetCode/1051_1100/1054_DistantBarcodesTest.cs
@@ -0,0 +1,51 @@
+﻿using leetCode._1051_1100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1051_1100
+{
+    [TestClass]
+    public class _1054_DistantBarcodesTest
+    {
+        _1054_DistantBarcodesAlg alg = new _1054_DistantBarcodesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] barcodes = [1, 1, 1, 2, 2, 2];
+            int[] exp = [2, 1, 2, 1, 2, 1];
+            int[] res = alg.RearrangeBarcodes(barcodes);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] barcodes = [1, 1, 1, 1, 2, 2, 3, 3];
+            int[] exp = [2, 1, 2, 1, 3, 1, 3, 1];
+            int[] res = alg.RearrangeBarcodes(barcodes);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] barcodes = [1, 1, 2];
+            int[] exp = [1, 2, 1];
+            int[] res = alg.RearrangeBarcodes(barcodes);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            int[] barcodes = [2, 2, 1, 3];
+            int[] exp = [2, 1, 2, 3];
+            int[] res = alg.RearrangeBarcodes(barcodes);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/1051_1100/1054_DistantBarcodesAlg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1051_1100
+{
+    public class _1054_DistantBarcodesAlg
+    {
+        public int[] RearrangeBarcodes(int[] barcodes)
+        {
+            int n = barcodes.Length;
+            if (n < 2)
+            {
+                return barcodes;
+            }
+
+
+            int[] ans = new int[n];
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+        
+            foreach (int item in barcodes)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+              
+            }
+
+            int evenIdx = 0;
+            int oddIdx = 1;
+            int half = n / 2;
+            foreach (var item in dict)
+            {
+                int x = item.Key;
+                int count = item.Value;
+                while (count > 0 && count <= half && oddIdx < n)
+                {
+                    ans[oddIdx] = x;
+                    count--;
+                    oddIdx += 2;
+                }
+                while (count > 0) 
+                {
+                    ans[evenIdx] = x;
+                    count--;
+                    evenIdx += 2;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
