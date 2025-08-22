7db7bd8b56563ac4c6e81e6af6ad4408926b0719
hongtao_luo <luohongtao621@hotmail.com>
2025-08-22 09:29:11 +0800

    3195

---
+++ TestLeetCode/3151_3200/3195_FindTheMinimumAreaToCoverAllOnesITest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3151_3200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_3200
+{
+    [TestClass]
+    public class _3195_FindTheMinimumAreaToCoverAllOnesITest
+    {
+        _3195_FindTheMinimumAreaToCoverAllOnesIAlg alg = new _3195_FindTheMinimumAreaToCoverAllOnesIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] grid = [[0, 1, 0], [1, 0, 1]];
+            int exp = 6;
+            int res = alg.MinimumArea(grid);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] grid = [[0, 0], [1, 0]];
+            int exp = 1;
+            int res = alg.MinimumArea(grid);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_3200/3195_FindTheMinimumAreaToCoverAllOnesIAlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_3200
+{
+    public class _3195_FindTheMinimumAreaToCoverAllOnesIAlg
+    {
+        public int MinimumArea(int[][] grid)
+        {
+            int m = grid.Length;
+            int n = grid[0].Length;
+            int top = m;
+            int bottom = 0;
+            int left = n;
+            int right = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        top = Math.Min(top, i);
+                        bottom = Math.Max(bottom, i);
+                        left = Math.Min(left, j);
+                        right = Math.Max(right, j);
+                    }
+                }
+            }
+
+
+            return (bottom - top + 1) * (right - left + 1);
+        }
+    }
+}
