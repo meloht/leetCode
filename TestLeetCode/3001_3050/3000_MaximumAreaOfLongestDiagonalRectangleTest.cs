c36e28d9553f554aa7a3392098fdb10a6494b8a6
hongtao_luo <luohongtao621@hotmail.com>
2025-08-26 12:06:11 +0800

    3000

---
+++ TestLeetCode/3001_3050/3000_MaximumAreaOfLongestDiagonalRectangleTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3001_3050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3001_3050
+{
+    [TestClass]
+    public class _3000_MaximumAreaOfLongestDiagonalRectangleTest
+    {
+        _3000_MaximumAreaOfLongestDiagonalRectangleAlg alg = new _3000_MaximumAreaOfLongestDiagonalRectangleAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] dimensions = [[9, 3], [8, 6]];
+            int exp = 48;
+            int res = alg.AreaOfMaxDiagonal(dimensions);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] dimensions = [[3, 4], [4, 3]];
+            int exp = 12;
+            int res = alg.AreaOfMaxDiagonal(dimensions);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[][] dimensions = [[6, 5], [8, 6], [2, 10], [8, 1], [9, 2], [3, 5], [3, 5]];
+            int exp = 20;
+            int res = alg.AreaOfMaxDiagonal(dimensions);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3001_3050/3000_MaximumAreaOfLongestDiagonalRectangleAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3001_3050
+{
+    public class _3000_MaximumAreaOfLongestDiagonalRectangleAlg
+    {
+        public int AreaOfMaxDiagonal(int[][] dimensions)
+        {
+
+            int ans = 0;
+            int max = 0;
+            foreach (var item in dimensions)
+            {
+                int num = item[0] * item[0] + item[1] * item[1];
+                if (num > max)
+                {
+                    max = num;
+                    ans = item[0] * item[1];
+                }
+                else if (num == max)
+                {
+                    int a = item[0] * item[1];
+                    if (a > ans)
+                    {
+                        ans = a;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
