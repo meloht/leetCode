3fcd26fe69b1206fa97c721a44c05d8bac188b2f
hongtao_luo <luohongtao621@hotmail.com>
2025-08-28 12:12:03 +0800

    3446

---
+++ TestLeetCode/3401_3450/3446_SortMatrixByDiagonalsTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._3401_3450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3401_3450
+{
+    [TestClass]
+    public class _3446_SortMatrixByDiagonalsTest
+    {
+        _3446_SortMatrixByDiagonalsAlg alg = new _3446_SortMatrixByDiagonalsAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[][] grid = [[1, 7, 3], [9, 8, 2], [4, 5, 6]];
+            int[][] exp = [[8, 2, 3], [9, 6, 7], [4, 5, 1]];
+            int[][] res = alg.SortMatrix(grid);
+
+            Assert.IsTrue(Utils.IsSameArr(exp, res));
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[][] grid = [[0, 1], [1, 2]];
+            int[][] exp = [[2, 1], [1, 0]];
+            int[][] res = alg.SortMatrix(grid);
+
+            Assert.IsTrue(Utils.IsSameArr(exp, res));
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[][] grid = [[1]];
+            int[][] exp = [[1]];
+            int[][] res = alg.SortMatrix(grid);
+
+            Assert.IsTrue(Utils.IsSameArr(exp, res));
+        }
+    }
+}
---
+++ leetCode/3401_3450/3446_SortMatrixByDiagonalsAlg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3401_3450
+{
+    public class _3446_SortMatrixByDiagonalsAlg
+    {
+        public int[][] SortMatrix(int[][] grid)
+        {
+            int m = grid.Length;
+            int n = grid[0].Length;
+
+            for (int i = 0; i < m; i++)
+            {
+                List<int> ls = new List<int>();
+                for (int r = i, c = 0; r < m && c < n; r++, c++)
+                {
+                    ls.Add(grid[r][c]);
+                }
+                ls.Sort((x, y) => y - x);
+                for (int r = i, c = 0, k = 0; r < m && c < n; r++, c++)
+                {
+                    grid[r][c] = ls[k++];
+                }
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                List<int> ls = new List<int>();
+                for (int r = 0, c = i; r < m && c < n; r++, c++)
+                {
+                    ls.Add(grid[r][c]);
+                }
+                ls.Sort();
+                for (int r = 0, c = i, k = 0; r < m && c < n; r++, c++)
+                {
+                    grid[r][c] = ls[k++];
+                }
+            }
+            return grid;
+        }
+    }
+}
