9d7d158917c94942cb6def95de5ef98c78f6dfd2
hongtao_luo <luohongtao621@hotmail.com>
2025-08-29 13:38:54 +0800

    1080

---
+++ TestLeetCode/1051_1100/1080_InsufficientNodesInRootToLeafPathsTest.cs
@@ -0,0 +1,63 @@
+﻿using leetCode._1051_1100;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1051_1100
+{
+    [TestClass]
+    public class _1080_InsufficientNodesInRootToLeafPathsTest
+    {
+        _1080_InsufficientNodesInRootToLeafPathsAlg alg = new _1080_InsufficientNodesInRootToLeafPathsAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int?[] root = [1, 2, 3, 4, -99, -99, 7, 8, 9, -99, -99, 12, 13, -99, 14];
+            int limit = 1;
+            int?[] exp = [1, 2, 3, 4, null, null, 7, 8, 9, null, 14];
+            TreeNode tree = TreeNode.BuildTree(root);
+            TreeNode ress = alg.SufficientSubset(tree, limit);
+            int?[] res = TreeNode.GetTreeLevelList(tree);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int?[] root = [5, 4, 8, 11, null, 17, 4, 7, 1, null, null, 5, 3];
+            int limit = 22;
+            int?[] exp = [5, 4, 8, 11, null, 17, 4, 7, null, null, null, 5];
+            TreeNode tree = TreeNode.BuildTree(root);
+            TreeNode ress = alg.SufficientSubset(tree, limit);
+            int?[] res = TreeNode.GetTreeLevelList(tree);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int?[] root = [1, 2, -3, -5, null, 4, null];
+            int limit = -1;
+            int?[] exp = [1, null, -3, 4];
+            TreeNode tree = TreeNode.BuildTree(root);
+            TreeNode ress = alg.SufficientSubset(tree, limit);
+            int?[] res = TreeNode.GetTreeLevelList(tree);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+        [TestMethod]
+        public void Test04()
+        {
+            int?[] root = [2, 7, 2, null, 8, null, null, null, 4];
+            int limit = 15;
+            int?[] exp = [2, 7, null, null, 8, null, 4];
+            TreeNode tree = TreeNode.BuildTree(root);
+            TreeNode ress = alg.SufficientSubset(tree, limit);
+            int?[] res = TreeNode.GetTreeLevelList(tree);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/1051_1100/1080_InsufficientNodesInRootToLeafPathsAlg.cs
@@ -0,0 +1,48 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1051_1100
+{
+    public class _1080_InsufficientNodesInRootToLeafPathsAlg
+    {
+        public TreeNode SufficientSubset(TreeNode root, int limit)
+        {
+            bool ans = Dfs(root, limit, 0);
+            if (!ans) 
+            {
+                return null;
+            }
+            return root;
+        }
+
+        private bool Dfs(TreeNode root, int limit, int val)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            if (root.left == null && root.right == null)
+            {
+                if (val + root.val >= limit)
+                    return true;
+                return false;
+            }
+
+            bool left = Dfs(root.left, limit, val + root.val);
+            if (!left)
+            {
+                root.left = null;
+            }
+            bool right = Dfs(root.right, limit, val + root.val);
+            if (!right)
+            {
+                root.right = null;
+            }
+            return right || left;
+        }
+    }
+}
