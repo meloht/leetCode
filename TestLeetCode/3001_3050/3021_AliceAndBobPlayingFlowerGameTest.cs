51ca1ee22a6babe5bf05416d43354a1087207d35
hongtao_luo <luohongtao621@hotmail.com>
2025-08-29 12:10:10 +0800

    3021

---
+++ TestLeetCode/3001_3050/3021_AliceAndBobPlayingFlowerGameTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3001_3050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3001_3050
+{
+    [TestClass]
+    public class _3021_AliceAndBobPlayingFlowerGameTest
+    {
+        _3021_AliceAndBobPlayingFlowerGameAlg alg = new _3021_AliceAndBobPlayingFlowerGameAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int n = 3, m = 2;
+            long exp = 3;
+            long res = alg.FlowerGame(n, m);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int n = 1, m = 1;
+            long exp = 0;
+            long res = alg.FlowerGame(n, m);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3001_3050/3021_AliceAndBobPlayingFlowerGameAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3001_3050
+{
+    public class _3021_AliceAndBobPlayingFlowerGameAlg
+    {
+        public long FlowerGame(int n, int m)
+        {
+            return ((long)n*m)/2;
+        }
+    }
+}
