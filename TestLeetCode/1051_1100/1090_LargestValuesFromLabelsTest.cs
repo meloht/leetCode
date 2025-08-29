a766501283cb80c4eac534a54a7a20a2b5c54bb0
hongtao_luo <luohongtao621@hotmail.com>
2025-08-29 14:24:32 +0800

    1090

---
+++ TestLeetCode/1051_1100/1090_LargestValuesFromLabelsTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._1051_1100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1051_1100
+{
+    [TestClass]
+    public class _1090_LargestValuesFromLabelsTest
+    {
+        _1090_LargestValuesFromLabelsAlg alg = new _1090_LargestValuesFromLabelsAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] values = [5, 4, 3, 2, 1], labels = [1, 1, 2, 2, 3];
+            int numWanted = 3, useLimit = 1;
+            int exp = 9;
+            int res = alg.LargestValsFromLabels(values, labels, numWanted, useLimit);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] values = [5, 4, 3, 2, 1], labels = [1, 3, 3, 3, 2];
+            int numWanted = 3, useLimit = 2;
+            int exp = 12;
+            int res = alg.LargestValsFromLabels(values, labels, numWanted, useLimit);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[] values = [9, 8, 8, 7, 6], labels = [0, 0, 0, 1, 1];
+            int numWanted = 3, useLimit = 1;
+            int exp = 16;
+            int res = alg.LargestValsFromLabels(values, labels, numWanted, useLimit);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/1051_1100/1090_LargestValuesFromLabelsAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1051_1100
+{
+    public class _1090_LargestValuesFromLabelsAlg
+    {
+        public int LargestValsFromLabels(int[] values, int[] labels, int numWanted, int useLimit)
+        {
+            int[][] map = new int[values.Length][];
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < values.Length; i++)
+            {
+                map[i] = [values[i], i];
+                if (dict.ContainsKey(labels[i]))
+                {
+                    if (dict[labels[i]] < useLimit)
+                    {
+                        dict[labels[i]]++;
+                    }
+
+                }
+                else
+                {
+                    dict.Add(labels[i], 1);
+                }
+            }
+
+            Array.Sort(map, (x, y) => y[0] - x[0]);
+            int ans = 0;
+            for (int i = 0, j = numWanted; i < values.Length; i++)
+            {
+                if (j == 0)
+                    break;
+                int lbl = labels[map[i][1]];
+                if (dict[lbl] > 0)
+                {
+                    ans += map[i][0];
+                    dict[lbl]--;
+                    j--;
+                }
+            }
+
+
+            return ans;
+        }
+    }
+}
