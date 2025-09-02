e0d00f3c39fa2b23e8a346ac2d4b85aa11770b1f
hongtao_luo <luohongtao621@hotmail.com>
2025-09-02 13:01:11 +0800

    3025

---
+++ TestLeetCode/3001_3050/3025_FindTheNumberOfWaysToPlacePeopleITest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3001_3050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3001_3050
+{
+    [TestClass]
+    public class _3025_FindTheNumberOfWaysToPlacePeopleITest
+    {
+        _3025_FindTheNumberOfWaysToPlacePeopleIAlg alg = new _3025_FindTheNumberOfWaysToPlacePeopleIAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[][] points = [[1, 1], [2, 2], [3, 3]];
+            int exp = 0;
+            int res = alg.NumberOfPairs(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[][] points = [[6, 2], [4, 4], [2, 6]];
+            int exp = 2;
+            int res = alg.NumberOfPairs(points);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[][] points = [[3, 1], [1, 3], [1, 1]];
+            int exp = 2;
+            int res = alg.NumberOfPairs(points);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3001_3050/3025_FindTheNumberOfWaysToPlacePeopleIAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3001_3050
+{
+    public class _3025_FindTheNumberOfWaysToPlacePeopleIAlg
+    {
+        public int NumberOfPairs(int[][] points)
+        {
+
+            Array.Sort(points, (x, y) =>
+            {
+                if (x[0] == y[0])
+                    return y[1] - x[1];
+                return x[0] - y[0];
+            });
+            int ans = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                for (int j = i + 1; j < points.Length; j++)
+                {
+                    if (points[i][1] < points[j][1])
+                    {
+                        continue;
+                    }
+                    bool bl = true;
+                    for (int k = i + 1; k < j; k++)
+                    {
+                        if (points[k][1] <= points[i][1] && points[k][1] >= points[j][1])
+                        {
+                            bl = false;
+                            break;
+                        }
+                    }
+                    if (bl)
+                    {
+                        ans++;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
