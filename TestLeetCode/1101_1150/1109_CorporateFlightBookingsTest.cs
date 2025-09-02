1f7452f84610c4407af7d86f00a6e5dcbebdafa4
hongtao_luo <luohongtao621@hotmail.com>
2025-09-02 13:25:00 +0800

    1109

---
+++ TestLeetCode/1101_1150/1109_CorporateFlightBookingsTest.cs
@@ -0,0 +1,37 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1109_CorporateFlightBookingsTest
+    {
+        _1109_CorporateFlightBookingsAlg alg = new _1109_CorporateFlightBookingsAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[][] bookings = [[1, 2, 10], [2, 3, 20], [2, 5, 25]];
+            int n = 5;
+            int[] exp = [10, 55, 45, 25, 25];
+            int[] res = alg.CorpFlightBookings(bookings, n);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[][] bookings = [[1, 2, 10], [2, 2, 15]];
+            int n = 2;
+            int[] exp = [10, 25];
+            int[] res = alg.CorpFlightBookings(bookings, n);
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+    }
+}
---
+++ leetCode/1101_1150/1109_CorporateFlightBookingsAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1109_CorporateFlightBookingsAlg
+    {
+        public int[] CorpFlightBookings(int[][] bookings, int n)
+        {
+            int[] nums = new int[n];
+            foreach (int[] booking in bookings)
+            {
+                nums[booking[0] - 1] += booking[2];
+                if (booking[1] < n)
+                {
+                    nums[booking[1]] -= booking[2];
+                }
+            }
+            for (int i = 1; i < n; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+            return nums;
+        }
+    }
+}
