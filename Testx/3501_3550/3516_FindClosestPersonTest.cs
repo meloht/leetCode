c942263191e816b0d3cedf58ff8bc7f36dcf2b6c
hongtao_luo <luohongtao621@hotmail.com>
2025-09-04 14:32:41 +0800

    3516

---
+++ Testx/3501_3550/3516_FindClosestPersonTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._3501_3550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3501_3550
+{
+    public class _3516_FindClosestPersonTest
+    {
+        _3516_FindClosestPersonAlg alg = new _3516_FindClosestPersonAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            int x = 2, y = 7, z = 4;
+            int exp = 1;
+            int res = alg.FindClosest(x, y, z);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int x = 2, y = 5, z = 6;
+            int exp = 2;
+            int res = alg.FindClosest(x, y, z);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int x = 1, y = 5, z = 3;
+            int exp = 0;
+            int res = alg.FindClosest(x, y, z);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3501_3550/3516_FindClosestPersonAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3501_3550
+{
+    public class _3516_FindClosestPersonAlg
+    {
+        public int FindClosest(int x, int y, int z)
+        {
+            if (Math.Abs(z - x) < Math.Abs(z - y))
+            {
+                return 1;
+            }
+            else if (Math.Abs(z - x) == Math.Abs(z - y))
+            {
+                return 0;
+            }
+            else
+            {
+                return 2;
+            }
+
+        }
+    }
+}
