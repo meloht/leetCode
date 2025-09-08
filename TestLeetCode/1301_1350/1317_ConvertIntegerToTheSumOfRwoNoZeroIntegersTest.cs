00d592341ddf20febfd3433fe0b0dff37d346074
hongtao_luo <luohongtao621@hotmail.com>
2025-09-08 12:37:59 +0800

    1317

---
+++ TestLeetCode/1301_1350/1317_ConvertIntegerToTheSumOfRwoNoZeroIntegersTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._1301_1350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1301_1350
+{
+    [TestClass]
+    public class _1317_ConvertIntegerToTheSumOfRwoNoZeroIntegersTest
+    {
+        _1317_ConvertIntegerToTheSumOfRwoNoZeroIntegersAlg alg = new _1317_ConvertIntegerToTheSumOfRwoNoZeroIntegersAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int n = 2;
+            int[] exp = [1, 1];
+            int[] res = alg.GetNoZeroIntegers(n);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int n = 11;
+            int[] exp = [2, 9];
+            int[] res = alg.GetNoZeroIntegers(n);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int n = 10000;
+            int[] exp = [1, 9999];
+            int[] res = alg.GetNoZeroIntegers(n);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void Test04()
+        {
+            int n = 69;
+            int[] exp = [1, 68];
+            int[] res = alg.GetNoZeroIntegers(n);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void Test05()
+        {
+            int n = 1010;
+            int[] exp = [11, 999];
+            int[] res = alg.GetNoZeroIntegers(n);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/1301_1350/1317_ConvertIntegerToTheSumOfRwoNoZeroIntegersAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1301_1350
+{
+    public class _1317_ConvertIntegerToTheSumOfRwoNoZeroIntegersAlg
+    {
+        public int[] GetNoZeroIntegers(int n)
+        {
+            int a = 0;
+            int baseVal = 1; // 10^k
+            for (int x = n; x > 1; x /= 10)
+            {
+                int d = x % 10;
+                if (d <= 1)
+                {
+                    d += 10;
+                    x -= 10; // 借位
+                }
+                // a 这一位填 d/2，比如百位数就是 d/2 * 100
+                a += d / 2 * baseVal;
+                baseVal *= 10;
+            }
+            return new int[] { a, n - a };
+
+
+        }
+    }
+}
