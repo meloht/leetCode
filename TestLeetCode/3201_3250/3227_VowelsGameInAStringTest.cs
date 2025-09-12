fb5bdc47693d9a88c8f3169c23fb5747b7d5947a
hongtao_luo <luohongtao621@hotmail.com>
2025-09-12 12:25:44 +0800

    3227

---
+++ TestLeetCode/3201_3250/3227_VowelsGameInAStringTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3201_3250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3227_VowelsGameInAStringTest
+    {
+        _3227_VowelsGameInAStringAlg alg = new _3227_VowelsGameInAStringAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            string s = "leetcoder";
+            bool exp = true;
+            bool res = alg.DoesAliceWin(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            string s = "bbcd";
+            bool exp = false;
+            bool res = alg.DoesAliceWin(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3201_3250/3227_VowelsGameInAStringAlg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3201_3250
+{
+    public class _3227_VowelsGameInAStringAlg
+    {
+        public bool DoesAliceWin(string s)
+        {
+
+            foreach (var item in s)
+            {
+                if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u')
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
