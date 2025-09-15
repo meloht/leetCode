210bed111e1aa85e22a34e60aa3e045fe66f8301
hongtao_luo <luohongtao621@hotmail.com>
2025-09-15 12:40:41 +0800

    1935

---
+++ TestLeetCode/1901_1950/1935_MaximumNumberOfWordsYouCanTypeTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._1901_1950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Test._1901_1950
+{
+    [TestClass]
+    public class _1935_MaximumNumberOfWordsYouCanTypeTest
+    {
+        _1935_MaximumNumberOfWordsYouCanTypeAlg alg = new _1935_MaximumNumberOfWordsYouCanTypeAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            string text = "hello world", brokenLetters = "ad";
+            int exp = 1;
+            int res = alg.CanBeTypedWords(text, brokenLetters);
+
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void Test02()
+        {
+            string text = "leet code", brokenLetters = "lt";
+            int exp = 1;
+            int res = alg.CanBeTypedWords(text, brokenLetters);
+
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            string text = "leet code", brokenLetters = "e";
+            int exp = 0;
+            int res = alg.CanBeTypedWords(text, brokenLetters);
+
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1901_1950/1935_MaximumNumberOfWordsYouCanTypeAlg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1901_1950
+{
+    public class _1935_MaximumNumberOfWordsYouCanTypeAlg
+    {
+        public int CanBeTypedWords(string text, string brokenLetters)
+        {
+            bool[] chData = new bool[26];
+            foreach (var item in brokenLetters)
+            {
+                chData[item - 'a'] = true;
+            }
+            int ans = 0;
+            bool flag = true;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ' )
+                {
+                    if (flag)
+                    {
+                        ans++;
+                    }
+                    flag = true;
+                }
+                else
+                {
+                    if (chData[text[i] - 'a'])
+                    {
+                        flag = false;
+                    }
+                }
+                if (i == text.Length - 1&&flag)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
