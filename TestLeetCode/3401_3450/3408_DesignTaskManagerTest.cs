7f9d8c0d08f556fdd5bbb73eebd842bae0229c8d
hongtao_luo <luohongtao621@hotmail.com>
2025-09-18 14:17:05 +0800

    3408

---
+++ TestLeetCode/3401_3450/3408_DesignTaskManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._3401_3450._3408_DesignTaskManagerAlg;
+
+namespace Test._3401_3450
+{
+    [TestClass]
+    public class _3408_DesignTaskManagerTest
+    {
+        [TestMethod]
+        public void Test01()
+        {
+            TaskManager taskManager = new TaskManager([[1, 101, 10], [2, 102, 20], [3, 103, 15]]); // 分别给用户 1 ，2 和 3 初始化一个任务。
+            taskManager.Add(4, 104, 5); // 给用户 4 添加优先级为 5 的任务 104 。
+            taskManager.Edit(102, 8); // 更新任务 102 的优先级为 8 。
+            Assert.AreEqual(3, taskManager.ExecTop()); // 返回 3 。执行用户 3 的任务 103 。
+            taskManager.Rmv(101); // 将系统中的任务 101 删除。
+            taskManager.Add(5, 105, 15); // 给用户 5 添加优先级为 15 的任务 105 。
+            Assert.AreEqual(5, taskManager.ExecTop()); // 返回 5 。执行用户 5 的任务 105 。
+        }
+    }
+}
---
+++ leetCode/3401_3450/3408_DesignTaskManagerAlg.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3401_3450
+{
+    public class _3408_DesignTaskManagerAlg
+    {
+        public class TaskManager
+        {
+            private Dictionary<int, int[]> taskInfo;
+            private PriorityQueue<int[], int[]> heap;
+
+
+            public TaskManager(IList<IList<int>> tasks)
+            {
+                taskInfo = new Dictionary<int, int[]>();
+                heap = new PriorityQueue<int[], int[]>(Comparer<int[]>.Create((a, b) =>
+                {
+                    if (a[0] == b[0])
+                    {
+                        return b[1].CompareTo(a[1]);
+                    }
+                    return b[0].CompareTo(a[0]);
+                }));
+                foreach (var task in tasks)
+                {
+                    int userId = task[0], taskId = task[1], priority = task[2];
+                    taskInfo[taskId] = [priority, userId];
+                    heap.Enqueue([priority, taskId], [priority, taskId]);
+                }
+
+
+
+            }
+
+            public void Add(int userId, int taskId, int priority)
+            {
+                taskInfo[taskId] = [priority, userId];
+                heap.Enqueue([priority, taskId], [priority, taskId]);
+            }
+
+            public void Edit(int taskId, int newPriority)
+            {
+                if (taskInfo.ContainsKey(taskId))
+                {
+                    taskInfo[taskId][0] = newPriority;
+                    heap.Enqueue(new int[] { newPriority, taskId }, new int[] { newPriority, taskId });
+                }
+
+
+            }
+
+            public void Rmv(int taskId)
+            {
+                taskInfo.Remove(taskId);
+            }
+
+            public int ExecTop()
+            {
+                while (heap.Count > 0)
+                {
+                    var task = heap.Dequeue();
+                    int priority = task[0], taskId = task[1];
+                    if (taskInfo.TryGetValue(taskId, out var info) && info[0] == priority)
+                    {
+                        int userId = info[1];
+                        taskInfo.Remove(taskId);
+                        return userId;
+                    }
+                }
+
+                return -1;
+
+            }
+
+
+        }
+
+
+
+    }
+}
