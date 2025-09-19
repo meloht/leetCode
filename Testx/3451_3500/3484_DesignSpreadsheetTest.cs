bb2ba8b0515b63c4987bd70b950ce514076bcfdc
hongtao_luo <luohongtao621@hotmail.com>
2025-09-19 12:31:47 +0800

    3484

---
+++ Testx/3451_3500/3484_DesignSpreadsheetTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._3451_3500._3484_DesignSpreadsheetAlg;
+
+namespace Testx._3451_3500
+{
+
+    public class _3484_DesignSpreadsheetTest
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            Spreadsheet spreadsheet = new Spreadsheet(3); // 初始化一个具有 3 行和 26 列的电子表格
+            Assert.Equal(12, spreadsheet.GetValue("=5+7")); // 返回 12 (5+7)
+            spreadsheet.SetCell("A1", 10); // 设置 A1 为 10
+            Assert.Equal(16, spreadsheet.GetValue("=A1+6")); // 返回 16 (10+6)
+            spreadsheet.SetCell("B2", 15); // 设置 B2 为 15
+            Assert.Equal(25, spreadsheet.GetValue("=A1+B2")); // 返回 25 (10+15)
+            spreadsheet.ResetCell("A1"); // 重置 A1 为 0
+            Assert.Equal(15, spreadsheet.GetValue("=A1+B2")); // 返回 15 (0+15)
+        }
+    }
+}
---
+++ leetCode/3451-3500/3484_DesignSpreadsheetAlg.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3451_3500
+{
+    public class _3484_DesignSpreadsheetAlg
+    {
+        public class Spreadsheet
+        {
+            int[][] dict = new int[26][];
+            public Spreadsheet(int rows)
+            {
+                for (int i = 0; i < 26; i++)
+                {
+                    dict[i] = new int[rows];
+                }
+            }
+
+            public void SetCell(string cell, int value)
+            {
+                int col = cell[0] - 'A';
+                int row = int.Parse(cell.Substring(1))-1;
+                dict[col][row] = value;
+            }
+
+            public void ResetCell(string cell)
+            {
+                int col = cell[0] - 'A';
+                int row = int.Parse(cell.Substring(1))-1;
+                dict[col][row] = 0;
+            }
+
+            public int GetValue(string formula)
+            {
+                string[] arr = formula.Substring(1).Split('+');
+
+                return GetNum(arr[0]) + GetNum(arr[1]);
+            }
+
+            private int GetNum(string cell)
+            {
+                if (char.IsLetter(cell[0]))
+                {
+                    int col = cell[0] - 'A';
+                    int row = int.Parse(cell.Substring(1)) - 1;
+                    return dict[col][row];
+                }
+
+                return int.Parse(cell);
+            }
+        }
+
+        public class Spreadsheet1
+        {
+            private Dictionary<string, int> cellValues = new Dictionary<string, int>();
+
+            public Spreadsheet1(int size) { }
+
+            public void SetCell(string cell, int value)
+            {
+                cellValues[cell] = value;
+            }
+
+            public void ResetCell(string cell)
+            {
+                cellValues.Remove(cell);
+            }
+
+            public int GetValue(string formula)
+            {
+                int i = formula.IndexOf('+');
+                string cell1 = formula.Substring(1, i - 1);
+                string cell2 = formula.Substring(i + 1);
+                int val1 = char.IsLetter(cell1[0]) ? cellValues.GetValueOrDefault(cell1) : int.Parse(cell1);
+                int val2 = char.IsLetter(cell2[0]) ? cellValues.GetValueOrDefault(cell2) : int.Parse(cell2);
+                return val1 + val2;
+            }
+        }
+
+    }
+}
