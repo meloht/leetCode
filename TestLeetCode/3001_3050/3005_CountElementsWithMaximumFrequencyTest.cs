ee0e4ba79e5dd1a5d0e6f1117a7b67573875fd3a
hongtao_luo <luohongtao621@hotmail.com>
2025-09-22 13:29:54 +0800

    3005

---
+++ TestLeetCode/3001_3050/3005_CountElementsWithMaximumFrequencyTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3001_3050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3001_3050
+{
+    [TestClass]
+    public class _3005_CountElementsWithMaximumFrequencyTest
+    {
+        _3005_CountElementsWithMaximumFrequencyAlg alg = new _3005_CountElementsWithMaximumFrequencyAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] nums = [1, 2, 2, 3, 1, 4];
+            int exp = 4;
+            int res = alg.MaxFrequencyElements(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] nums = [1, 2, 3, 4, 5];
+            int exp = 5;
+            int res = alg.MaxFrequencyElements(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3001_3050/3005_CountElementsWithMaximumFrequencyAlg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3001_3050
+{
+    public class _3005_CountElementsWithMaximumFrequencyAlg
+    {
+        public int MaxFrequencyElements(int[] nums)
+        {
+            int[] map=new int[101];
+            int max = 0;
+            foreach (var item in nums)
+            {
+                map[item]++;
+                max=Math.Max(max, map[item]);
+            }
+            int ans = map.Where(p => p == max).Sum();
+         
+            return ans;
+        }
+    }
+}
