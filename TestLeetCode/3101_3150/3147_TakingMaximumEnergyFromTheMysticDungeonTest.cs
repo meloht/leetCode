c0c98b0111f042712c92760c13475842a8cbdf08
hongtao_luo <luohongtao621@hotmail.com>
2025-10-10 13:06:17 +0800

    3147

---
+++ TestLeetCode/3101_3150/3147_TakingMaximumEnergyFromTheMysticDungeonTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3101_3150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3101_3150
+{
+    [TestClass]
+    public class _3147_TakingMaximumEnergyFromTheMysticDungeonTest
+    {
+        _3147_TakingMaximumEnergyFromTheMysticDungeonAlg alg = new _3147_TakingMaximumEnergyFromTheMysticDungeonAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] energy = [5, 2, -10, -5, 1];
+            int k = 3;
+            int exp = 3;
+            int res = alg.MaximumEnergy(energy, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] energy = [-2, -3, -1];
+            int k = 2;
+            int exp = -1;
+            int res = alg.MaximumEnergy(energy, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3101_3150/3147_TakingMaximumEnergyFromTheMysticDungeonAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3101_3150
+{
+    public class _3147_TakingMaximumEnergyFromTheMysticDungeonAlg
+    {
+        public int MaximumEnergy(int[] energy, int k)
+        {
+            int n = energy.Length;
+            int ans = int.MinValue;
+            for (int i = n - k; i < n; i++)
+            { // 枚举终点 i
+                int sufSum = 0;
+                for (int j = i; j >= 0; j -= k)
+                {
+                    sufSum += energy[j]; // 计算后缀和
+                    ans = Math.Max(ans, sufSum);
+                }
+            }
+            return ans;
+
+
+        }
+    }
+}
