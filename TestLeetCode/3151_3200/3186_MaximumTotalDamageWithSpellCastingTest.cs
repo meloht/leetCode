619d501f43dc1c1e2e0c7b2d7ad2b5268e69bb1d
hongtao_luo <luohongtao621@hotmail.com>
2025-10-11 12:27:23 +0800

    3186

---
+++ TestLeetCode/3151_3200/3186_MaximumTotalDamageWithSpellCastingTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3151_3200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3151_3200
+{
+    [TestClass]
+    public class _3186_MaximumTotalDamageWithSpellCastingTest
+    {
+        _3186_MaximumTotalDamageWithSpellCastingAlg alg = new _3186_MaximumTotalDamageWithSpellCastingAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] power = [1, 1, 3, 4];
+            long exp = 6;
+            long res = alg.MaximumTotalDamage(power);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] power = [7, 1, 6, 6];
+            long exp = 13;
+            long res = alg.MaximumTotalDamage(power);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3151_3200/3186_MaximumTotalDamageWithSpellCastingAlg.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3151_3200
+{
+    public class _3186_MaximumTotalDamageWithSpellCastingAlg
+    {
+        public long MaximumTotalDamage(int[] power)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var item in power)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+
+            int n = dict.Count;
+
+            int[] a = new int[n];
+
+            int k = 0;
+            foreach (var item in dict.Keys)
+            {
+                a[k++] = item;
+            }
+
+            Array.Sort(a);
+
+            long[] f = new long[n + 1];
+            int j = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int x = a[i];
+                while (a[j] < x - 2)
+                {
+                    j++;
+                }
+                f[i + 1] = Math.Max(f[i], f[j] + (long)x * dict[x]);
+            }
+            return f[n];
+
+
+        }
+    }
+}
