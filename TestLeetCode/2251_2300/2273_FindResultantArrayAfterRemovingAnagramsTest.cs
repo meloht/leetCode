94aeaee736107f83f6b7d7e7aa1bbe660ed36621
hongtao_luo <luohongtao621@hotmail.com>
2025-10-13 12:42:57 +0800

    2273

---
+++ TestLeetCode/2251_2300/2273_FindResultantArrayAfterRemovingAnagramsTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._2251_2300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2251_2300
+{
+    [TestClass]
+    public class _2273_FindResultantArrayAfterRemovingAnagramsTest
+    {
+        _2273_FindResultantArrayAfterRemovingAnagramsAlg alg = new _2273_FindResultantArrayAfterRemovingAnagramsAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["abba", "baba", "bbaa", "cd", "cd"];
+            string[] exp = ["abba", "cd"];
+            string[] res = alg.RemoveAnagrams(words).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["a", "b", "c", "d", "e"];
+            string[] exp = ["a", "b", "c", "d", "e"];
+            string[] res = alg.RemoveAnagrams(words).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+    }
+}
---
+++ leetCode/2251_2300/2273_FindResultantArrayAfterRemovingAnagramsAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2251_2300
+{
+    public class _2273_FindResultantArrayAfterRemovingAnagramsAlg
+    {
+        public IList<string> RemoveAnagrams(string[] words)
+        {
+
+            List<string> res = new List<string>();
+            res.Add(words[0]);   // 结果数组
+            int n = words.Length;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (!Compare(words[i], words[i - 1]))
+                {
+                    res.Add(words[i]);
+                }
+            }
+            return res;
+
+
+        }
+        private bool Compare(string word1, string word2)
+        {
+            int[] freq = new int[26];
+            foreach (char ch in word1)
+            {
+                freq[ch - 'a']++;
+            }
+            foreach (char ch in word2)
+            {
+                freq[ch - 'a']--;
+            }
+            foreach (int x in freq)
+            {
+                if (x != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
