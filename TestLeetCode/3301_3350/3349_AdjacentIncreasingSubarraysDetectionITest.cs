88f4482e5809c043ba46deefd90332f2d873e745
hongtao_luo <luohongtao621@hotmail.com>
2025-10-14 14:16:00 +0800

    3349

---
+++ TestLeetCode/3301_3350/3349_AdjacentIncreasingSubarraysDetectionITest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3301_3350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3301_3350
+{
+    [TestClass]
+    public class _3349_AdjacentIncreasingSubarraysDetectionITest
+    {
+        _3349_AdjacentIncreasingSubarraysDetectionIAlg alg = new _3349_AdjacentIncreasingSubarraysDetectionIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [2, 5, 7, 8, 9, 2, 3, 4, 3, 1];
+            int k = 3;
+            bool exp = true;
+            bool res=alg.HasIncreasingSubarrays(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 2, 3, 4, 4, 4, 4, 5, 6, 7];
+            int k = 5;
+            bool exp = false;
+            bool res = alg.HasIncreasingSubarrays(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3301_3350/3349_AdjacentIncreasingSubarraysDetectionIAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3301_3350
+{
+    public class _3349_AdjacentIncreasingSubarraysDetectionIAlg
+    {
+        public bool HasIncreasingSubarrays(IList<int> nums, int k)
+        {
+            int n = nums.Count;
+            int cnt = 1, precnt = 0, ans = 0;
+            for (int i = 1; i < n; ++i)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    ++cnt;
+                }
+                else
+                {
+                    precnt = cnt;
+                    cnt = 1;
+                }
+                ans = Math.Max(ans, Math.Min(precnt, cnt));
+                ans = Math.Max(ans, cnt / 2);
+            }
+            return ans >= k;
+        }
+    }
+}
