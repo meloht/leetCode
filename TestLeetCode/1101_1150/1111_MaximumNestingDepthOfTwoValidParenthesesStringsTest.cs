19efa15c72a10ca5f81859b57b94a49c3c43fcc5
hongtao_luo <luohongtao621@hotmail.com>
2025-10-17 16:21:57 +0800

    1111

---
+++ TestLeetCode/1101_1150/1111_MaximumNestingDepthOfTwoValidParenthesesStringsTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1111_MaximumNestingDepthOfTwoValidParenthesesStringsTest
+    {
+        _1111_MaximumNestingDepthOfTwoValidParenthesesStringsAlg alg = new _1111_MaximumNestingDepthOfTwoValidParenthesesStringsAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string seq = "(()())";
+            int[] exp = [0, 1, 1, 1, 1, 0];
+            int[] res = alg.MaxDepthAfterSplit(seq);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string seq = "()(())()";
+            int[] exp = [0, 0, 0, 1, 1, 0, 1, 1];
+            int[] res = alg.MaxDepthAfterSplit(seq);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/1101_1150/1111_MaximumNestingDepthOfTwoValidParenthesesStringsAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1111_MaximumNestingDepthOfTwoValidParenthesesStringsAlg
+    {
+        public int[] MaxDepthAfterSplit(string seq)
+        {
+            return null;
+        }
+    }
+}
