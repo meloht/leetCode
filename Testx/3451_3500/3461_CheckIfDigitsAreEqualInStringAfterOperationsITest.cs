c8580db2b26f8981768f3522a10d1923993f973a
hongtao_luo <luohongtao621@hotmail.com>
2025-10-23 12:37:10 +0800

    3461

---
+++ Testx/3451_3500/3461_CheckIfDigitsAreEqualInStringAfterOperationsITest.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._3451_3500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3451_3500
+{
+    public class _3461_CheckIfDigitsAreEqualInStringAfterOperationsITest
+    {
+        _3461_CheckIfDigitsAreEqualInStringAfterOperationsIAlg alg = new _3461_CheckIfDigitsAreEqualInStringAfterOperationsIAlg();
+
+        [Fact]
+        public void TestCase01()
+        {
+            string s = "3902";
+            bool res = alg.HasSameDigits(s);
+            bool exp = true;
+            Assert.Equal(exp, res);
+        }
+        [Fact]
+        public void TestCase02()
+        {
+            string s = "34789";
+            bool res = alg.HasSameDigits(s);
+            bool exp = false;
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3451-3500/3461_CheckIfDigitsAreEqualInStringAfterOperationsIAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3451_3500
+{
+    public class _3461_CheckIfDigitsAreEqualInStringAfterOperationsIAlg
+    {
+        public bool HasSameDigits(string s)
+        {
+            int n = s.Length;
+            char[] arr = s.ToCharArray();
+            for (int i = 1; i <= n - 2; i++)
+            {
+                for (int j = 0; j <= n - 1 - i; j++)
+                {
+                    arr[j] = (char)(((arr[j] - '0') + (arr[j + 1] - '0')) % 10 + '0');
+                }
+            }
+            return arr[0] == arr[1];
+
+
+        }
+    }
+}
