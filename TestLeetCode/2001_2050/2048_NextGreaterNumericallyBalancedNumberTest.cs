edbb4129f954ea10a9735d629f88709144ae4537
hongtao_luo <luohongtao621@hotmail.com>
2025-10-24 13:25:54 +0800

    2048

---
+++ TestLeetCode/2001_2050/2048_NextGreaterNumericallyBalancedNumberTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2001_2050;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2001_2050
+{
+    [TestClass]
+    public class _2048_NextGreaterNumericallyBalancedNumberTest
+    {
+        _2048_NextGreaterNumericallyBalancedNumberAlg alg = new _2048_NextGreaterNumericallyBalancedNumberAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 1;
+            int res = alg.NextBeautifulNumber(n);
+            int exp = 22;
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 1000;
+            int res = alg.NextBeautifulNumber(n);
+            int exp = 1333;
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 3000;
+            int res = alg.NextBeautifulNumber(n);
+            int exp = 3133;
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2001_2050/2048_NextGreaterNumericallyBalancedNumberAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2001_2050
+{
+    public class _2048_NextGreaterNumericallyBalancedNumberAlg
+    {
+        public int NextBeautifulNumber(int n)
+        {
+
+            while (true)
+            {
+                n++;
+
+                int[] cnt = new int[10];
+                for (int x = n; x > 0; x /= 10)
+                {
+                    cnt[x % 10]++;
+                }
+
+                bool ok = true;
+                for (int x = n; x > 0; x /= 10)
+                {
+                    if (cnt[x % 10] != x % 10)
+                    {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok)
+                {
+                    return n;
+                }
+            }
+
+        }
+    }
+}
