09221d6aa7316c8d86ded951812edc5169e367e7
hongtao_luo <luohongtao621@hotmail.com>
2025-10-31 13:09:22 +0800

    3289

---
+++ TestLeetCode/3251_3300/3289_TheTwoSneakyNumbersOfDigitvilleTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._3251_3300;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3251_3300
+{
+    [TestClass]
+    public class _3289_TheTwoSneakyNumbersOfDigitvilleTest
+    {
+        _3289_TheTwoSneakyNumbersOfDigitvilleAlg alg = new _3289_TheTwoSneakyNumbersOfDigitvilleAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] nums = [0, 1, 1, 0];
+            int[] exp = [1, 0];
+            int[] res = alg.GetSneakyNumbers(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] nums = [0, 3, 2, 1, 3, 2];
+            int[] exp = [3, 2];
+            int[] res = alg.GetSneakyNumbers(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[] nums = [7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2];
+            int[] exp = [4, 5];
+            int[] res = alg.GetSneakyNumbers(nums);
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/3251_3300/3289_TheTwoSneakyNumbersOfDigitvilleAlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3251_3300
+{
+    public class _3289_TheTwoSneakyNumbersOfDigitvilleAlg
+    {
+        public int[] GetSneakyNumbers(int[] nums)
+        {
+            HashSet<int> result = new HashSet<int>();
+            int[] ans=new int[2];
+            int idx = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!result.Add(nums[i]))
+                {
+                    ans[idx++]=nums[i];
+                }
+            }
+            return ans;
+        }
+    }
+}
