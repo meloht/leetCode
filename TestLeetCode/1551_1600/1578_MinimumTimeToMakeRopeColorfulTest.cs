c6aebd8a3c70ea9dc34cacc95ba76d288661de3d
hongtao_luo <luohongtao621@hotmail.com>
2025-11-03 12:21:54 +0800

    1578

---
+++ TestLeetCode/1551_1600/1578_MinimumTimeToMakeRopeColorfulTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._1551_1600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1551_1600
+{
+    [TestClass]
+    public class _1578_MinimumTimeToMakeRopeColorfulTest
+    {
+        _1578_MinimumTimeToMakeRopeColorfulAlg alg = new _1578_MinimumTimeToMakeRopeColorfulAlg();
+
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string colors = "abaac";
+            int[] neededTime = [1, 2, 3, 4, 5];
+            int exp = 3;
+            int res = alg.MinCost(colors, neededTime);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string colors = "abc";
+            int[] neededTime = [1, 2, 3];
+            int exp = 0;
+            int res = alg.MinCost(colors, neededTime);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string colors = "aabaa";
+            int[] neededTime = [1, 2, 3, 4, 1];
+            int exp = 2;
+            int res = alg.MinCost(colors, neededTime);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1551_1600/1578_MinimumTimeToMakeRopeColorfulAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1551_1600
+{
+    public class _1578_MinimumTimeToMakeRopeColorfulAlg
+    {
+        public int MinCost(string colors, int[] neededTime)
+        {
+            int n = neededTime.Length;
+            int ans = 0;
+            int maxtime = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int t = neededTime[i];
+                ans+= t;
+                maxtime = Math.Max(maxtime, t);
+                if (i == n - 1 || colors[i] != colors[i + 1])
+                {
+                    // 遍历到了连续同色段的末尾
+                    ans -= maxtime; // 保留耗时最大的气球
+                    maxtime = 0; // 准备计算下一段的最大耗时
+                }
+            }
+            return ans;
+        }
+    }
+}
