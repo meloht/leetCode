0ddae37edbfb38f72474d3687428ff155926c078
hongtao_luo <luohongtao621@hotmail.com>
2025-11-04 12:05:11 +0800

    3318

---
+++ TestLeetCode/3201_3250/3318_FindXSumOfAllKLongSubarraysITest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._3301_3350;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3201_3250
+{
+    [TestClass]
+    public class _3318_FindXSumOfAllKLongSubarraysITest
+    {
+        _3318_FindXSumOfAllKLongSubarraysIAlg alg = new _3318_FindXSumOfAllKLongSubarraysIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 1, 2, 2, 3, 4, 2, 3];
+            int k = 6, x = 2;
+            long[] res = alg.FindXSum(nums, k, x);
+            long[] exp = [6, 10, 12];
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [3, 8, 7, 8, 7, 5];
+            int k = 2, x = 2;
+            long[] res = alg.FindXSum(nums, k, x);
+            long[] exp = [11, 15, 15, 15, 12];
+            Assert.IsTrue(exp.SequenceEqual(res));
+        }
+    }
+}
---
+++ leetCode/3301_3350/3318_FindXSumOfAllKLongSubarraysIAlg.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3301_3350
+{
+    public class _3318_FindXSumOfAllKLongSubarraysIAlg
+    {
+        public long[] FindXSum(int[] nums, int k, int x)
+        {
+            Helper helper = new Helper(x);
+            List<long> ans = new List<long>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                helper.Insert(nums[i]);
+                if (i >= k)
+                {
+                    helper.Remove(nums[i - k]);
+                }
+                if (i >= k - 1)
+                {
+                    ans.Add(helper.Get());
+                }
+            }
+
+            return ans.ToArray();
+        }
+
+        public class Helper
+        {
+            private int x;
+            private long result;
+            private SortedSet<(int, int)> large, small;
+            private Dictionary<int, int> occ;
+
+            public Helper(int x)
+            {
+                this.x = x;
+                this.result = 0;
+                this.large = new SortedSet<(int, int)>();
+                this.small = new SortedSet<(int, int)>();
+                this.occ = new Dictionary<int, int>();
+            }
+
+            public void Insert(int num)
+            {
+                if (occ.ContainsKey(num) && occ[num] > 0)
+                {
+                    InternalRemove((occ[num], num));
+                }
+                occ[num] = occ.GetValueOrDefault(num, 0) + 1;
+                InternalInsert((occ[num], num));
+            }
+
+            public void Remove(int num)
+            {
+                InternalRemove((occ[num], num));
+                occ[num]--;
+                if (occ[num] > 0)
+                {
+                    InternalInsert((occ[num], num));
+                }
+            }
+
+            public long Get()
+            {
+                return result;
+            }
+
+            private void InternalInsert((int, int) p)
+            {
+                if (large.Count < x || Compare(p, large.Min) > 0)
+                {
+                    result += (long)p.Item1 * p.Item2;
+                    large.Add(p);
+                    if (large.Count > x)
+                    {
+                        var toRemove = large.Min;
+                        result -= (long)toRemove.Item1 * toRemove.Item2;
+                        large.Remove(toRemove);
+                        small.Add(toRemove);
+                    }
+                }
+                else
+                {
+                    small.Add(p);
+                }
+            }
+
+            private void InternalRemove((int, int) p)
+            {
+                if (Compare(p, large.Min) >= 0)
+                {
+                    result -= (long)p.Item1 * p.Item2;
+                    large.Remove(p);
+                    if (small.Count > 0)
+                    {
+                        var toAdd = small.Max;
+                        result += (long)toAdd.Item1 * toAdd.Item2;
+                        small.Remove(toAdd);
+                        large.Add(toAdd);
+                    }
+                }
+                else
+                {
+                    small.Remove(p);
+                }
+            }
+
+            private int Compare((int, int) a, (int, int) b)
+            {
+                if (a.Item1 != b.Item1) return a.Item1.CompareTo(b.Item1);
+                return a.Item2.CompareTo(b.Item2);
+            }
+        }
+
+
+    }
+}
