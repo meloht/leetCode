3eb15faf6a0362be7968afa8ac266ca33a0c67bb
hongtao_luo <luohongtao621@hotmail.com>
2025-11-07 12:07:53 +0800

    1115

---
+++ TestLeetCode/1101_1150/1115_PrintFoobarAlternatelyTest.cs
@@ -0,0 +1,25 @@
+﻿using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static leetCode._1101_1150._1115_PrintFoobarAlternatelyAlg;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1115_PrintFoobarAlternatelyTest
+    {
+        
+        [TestMethod]
+        public void TestCase01()
+        {
+           
+            int n = 1;
+            FooBar alg = new FooBar(n);
+            string exp = "foobar";
+        
+        }
+    }
+}
---
+++ leetCode/1101_1150/1115_PrintFoobarAlternatelyAlg.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace leetCode._1101_1150
+{
+    public class _1115_PrintFoobarAlternatelyAlg
+    {
+        public class FooBar1
+        {
+            private int n;
+            bool foo;
+            bool bar;
+            object lockObj = new object();
+
+            public FooBar1(int n)
+            {
+                this.n = n;
+                bar = true;
+                foo = false;
+            }
+
+            public void Foo(Action printFoo)
+            {
+
+                for (int i = 0; i < n;)
+                {
+                    lock (lockObj)
+                    {
+                        if (bar)
+                        {
+                            bar = false;
+                            // printFoo() outputs "foo". Do not change or remove this line.
+                            printFoo();
+                            i++;
+                            foo = true;
+
+                        }
+                    }
+
+                    System.Threading.Thread.Sleep(1);
+
+                }
+            }
+
+            public void Bar(Action printBar)
+            {
+
+                for (int i = 0; i < n;)
+                {
+                    lock (lockObj)
+                    {
+                        if (foo)
+                        {
+                            foo = false;
+                            printBar();
+                            i++;
+                            bar = true;
+                        }
+
+                    }
+                    System.Threading.Thread.Sleep(1);
+
+                }
+            }
+        }
+
+        public class FooBar
+        {
+            private int n;
+
+            private static System.Threading.AutoResetEvent fooAr=new System.Threading.AutoResetEvent(true);
+            private static System.Threading.AutoResetEvent barAr = new System.Threading.AutoResetEvent(false);
+
+            public FooBar(int n)
+            {
+                this.n = n;
+            }
+
+            public void Foo(Action printFoo)
+            {
+
+                for (int i = 0; i < n;i++)
+                {
+                    fooAr.WaitOne();
+                    printFoo();
+                    barAr.Set();
+                }
+            }
+
+            public void Bar(Action printBar)
+            {
+
+                for (int i = 0; i < n;)
+                {
+                    barAr.WaitOne();
+                    printBar();
+                    fooAr.Set();
+
+                }
+            }
+        }
+    }
+}
