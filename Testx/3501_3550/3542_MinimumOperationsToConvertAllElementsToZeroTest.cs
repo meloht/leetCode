02905f91600ddf3500942dccb70645d420db0d79
hongtao_luo <luohongtao621@hotmail.com>
2025-11-10 09:46:36 +0800

    3542

---
+++ Testx/3501_3550/3542_MinimumOperationsToConvertAllElementsToZeroTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._3501_3550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3501_3550
+{
+    public class _3542_MinimumOperationsToConvertAllElementsToZeroTest
+    {
+        _3542_MinimumOperationsToConvertAllElementsToZeroAlg alg = new _3542_MinimumOperationsToConvertAllElementsToZeroAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            int[] nums = [0, 2];
+            int exp = 1;
+            int res = alg.MinOperations(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] nums = [3, 1, 2, 1];
+            int exp = 3;
+            int res = alg.MinOperations(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] nums = [1, 2, 1, 2, 1, 2];
+            int exp = 4;
+            int res = alg.MinOperations(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3501_3550/3542_MinimumOperationsToConvertAllElementsToZeroAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3501_3550
+{
+    public class _3542_MinimumOperationsToConvertAllElementsToZeroAlg
+    {
+        public int MinOperations(int[] nums)
+        {
+            int ans = 0;
+            int top = -1; // 栈顶下标（把 nums 当作栈）
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int x = nums[i];
+                while (top >= 0 && x < nums[top])
+                {
+                    top--; // 出栈
+                    ans++;
+                }
+                // 如果 x 与栈顶相同，那么 x 与栈顶可以在同一次操作中都变成 0，x 无需入栈
+                if (top < 0 || x != nums[top])
+                {
+                    nums[++top] = x; // 入栈
+                }
+            }
+
+            return ans + top + (nums[0] > 0 ? 1 : 0);
+
+        }
+    }
+}
