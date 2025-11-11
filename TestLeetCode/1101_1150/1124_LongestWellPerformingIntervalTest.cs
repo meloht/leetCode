43f675673ba6de8500a78ad07a7ff0a5658e9a63
hongtao_luo <luohongtao621@hotmail.com>
2025-11-11 10:20:27 +0800

    1124

---
+++ TestLeetCode/1101_1150/1124_LongestWellPerformingIntervalTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1124_LongestWellPerformingIntervalTest
+    {
+        _1124_LongestWellPerformingIntervalAlg alg = new _1124_LongestWellPerformingIntervalAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] hours = [9, 9, 6, 0, 6, 6, 9];
+            int exp = 3;
+            int res = alg.LongestWPI(hours);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] hours = [6, 6, 6];
+            int exp = 0;
+            int res = alg.LongestWPI(hours);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1101_1150/1124_LongestWellPerformingIntervalAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1124_LongestWellPerformingIntervalAlg
+    {
+
+        public int LongestWPI(int[] hours)
+        {
+            int n = hours.Length;
+            int ans = 0;
+            int[] s = new int[n + 1];
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+            for (int i = 1; i <= n; i++)
+            {
+                s[i] = s[i - 1] + (hours[i - 1] > 8 ? 1 : -1);
+                if (s[i] < s[stack.Peek()])
+                {
+                    stack.Push(i);
+                }
+            }
+            for (int i = n; i > 0; --i)
+                while (stack.Count>0 && s[i] > s[stack.Peek()])
+                    ans = Math.Max(ans, i - stack.Pop()); // [栈顶,i) 可能是最长子数组
+            return ans;
+
+        }
+    }
+}
