352617bbabf4501e1b78802a93a2c49e33402268
hongtao_luo <luohongtao621@hotmail.com>
2025-11-12 15:50:07 +0800

    1155

---
+++ TestLeetCode/1151_1200/1155_NumberOfDiceRollsWithTargetSumTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1151_1200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1151_1200
+{
+    [TestClass]
+    public class _1155_NumberOfDiceRollsWithTargetSumTest
+    {
+        _1155_NumberOfDiceRollsWithTargetSumAlg alg = new _1155_NumberOfDiceRollsWithTargetSumAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 1, k = 6, target = 3;
+            int exp = 1;
+            int res = alg.NumRollsToTarget(n, k, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 2, k = 6, target = 7;
+            int exp = 6;
+            int res = alg.NumRollsToTarget(n, k, target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int n = 30, k = 30, target = 500;
+            int exp = 222616187;
+            int res = alg.NumRollsToTarget(n, k, target);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1151_1200/1155_NumberOfDiceRollsWithTargetSumAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1151_1200
+{
+    public class _1155_NumberOfDiceRollsWithTargetSumAlg
+    {
+        public int NumRollsToTarget(int n, int k, int target)
+        {
+            if (target < n || target > n * k)
+            {
+                return 0; // 无法组成 target
+            }
+            int MOD = 1_000_000_007;
+            long[] f = new long[target - n + 1];
+            f[0] = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                int maxJ = Math.Min(i * (k - 1), target - n); // i 个骰子至多掷出 i*(k-1)
+                for (int j = 1; j <= maxJ; j++)
+                {
+                    f[j] += f[j - 1]; // 原地计算 f 的前缀和
+                }
+                for (int j = maxJ; j >= k; j--)
+                {
+                    f[j] = (f[j] - f[j - k]) % MOD; // f[j] 是两个前缀和的差
+                }
+            }
+            return (int)f[target - n];
+
+        }
+    }
+}
