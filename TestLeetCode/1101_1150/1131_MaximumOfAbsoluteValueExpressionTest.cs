63abb7cbbebfb15d41e607adbda19d8bf1b8643b
hongtao_luo <luohongtao621@hotmail.com>
2025-11-12 15:43:09 +0800

    1131

---
+++ TestLeetCode/1101_1150/1131_MaximumOfAbsoluteValueExpressionTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1131_MaximumOfAbsoluteValueExpressionTest
+    {
+        _1131_MaximumOfAbsoluteValueExpressionAlg alg = new _1131_MaximumOfAbsoluteValueExpressionAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] arr1 = [1, 2, 3, 4], arr2 = [-1, 4, 5, 6];
+            int exp = 13;
+            int res = alg.MaxAbsValExpr(arr1, arr2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] arr1 = [1, -2, -5, 0, 10], arr2 = [0, -2, -1, -7, -4];
+            int exp = 20;
+            int res = alg.MaxAbsValExpr(arr1, arr2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+
+
+}
---
+++ leetCode/1101_1150/1131_MaximumOfAbsoluteValueExpressionAlg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1131_MaximumOfAbsoluteValueExpressionAlg
+    {
+        public int MaxAbsValExpr(int[] arr1, int[] arr2)
+        {
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            List<int> list3 = new List<int>();
+            List<int> list4 = new List<int>();
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                int x = arr1[i];
+                int y = arr2[i];
+                list1.Add(x + y + i);
+                list2.Add(x + y - i);
+                list3.Add(x - y + i);
+                list4.Add(x - y - i);
+            }
+            int a = list1.Max() - list1.Min();
+            int b = list2.Max() - list2.Min();
+            int c = list3.Max() - list3.Min();
+            int d = list4.Max() - list4.Min();
+            return Math.Max(a, Math.Max(b, Math.Max(c, d)));
+        }
+    }
+}
