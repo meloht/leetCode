52cdb10d3c6f7e49af185747d830f88a323e0602
hongtao_luo <luohongtao621@hotmail.com>
2025-11-13 09:41:20 +0800

    1138

---
+++ TestLeetCode/1101_1150/1138_AlphabetBoardPathTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1138_AlphabetBoardPathTest
+    {
+        _1138_AlphabetBoardPathAlg alg = new _1138_AlphabetBoardPathAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string target = "leet";
+         
+            string exp = "DDR!UURRR!!DDD!";
+            string res = alg.AlphabetBoardPath(target);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string target = "code";
+            string exp = "RR!DDRR!UUL!R!";
+            string res = alg.AlphabetBoardPath(target);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1101_1150/1138_AlphabetBoardPathAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1138_AlphabetBoardPathAlg
+    {
+        public string AlphabetBoardPath(string target)
+        {
+            var ans = new StringBuilder();
+            int x = 0, y = 0;
+            foreach (var c in target)
+            {
+                int nx = (c - 'a') / 5, ny = (c - 'a') % 5; // 目标位置
+                var v = nx < x ? RepeatChar('U', x - nx) : RepeatChar('D', nx - x); // 竖直
+                var h = ny < y ? RepeatChar('L', y - ny) : RepeatChar('R', ny - y); // 水平
+                ans.Append(c != 'z' ? v + h : h + v).Append('!');
+                x = nx;
+                y = ny;
+            }
+
+            return ans.ToString();
+
+        }
+        private static string RepeatChar(char c, int count)
+        {
+            return count <= 0 ? "" : new string(c, count);
+        }
+    }
+}
