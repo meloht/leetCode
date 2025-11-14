50592bb9c6fef0c09618f6d6a11ce4bb1a244990
hongtao_luo <luohongtao621@hotmail.com>
2025-11-14 10:54:36 +0800

    1144

---
+++ TestLeetCode/1101_1150/1144_DecreaseElementsToMakeArrayZigzagTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1144_DecreaseElementsToMakeArrayZigzagTest
+    {
+        _1144_DecreaseElementsToMakeArrayZigzagAlg alg = new _1144_DecreaseElementsToMakeArrayZigzagAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 2, 3];
+            int exp = 2;
+            int res = alg.MovesToMakeZigzag(nums);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [9, 6, 1, 6, 2];
+            int exp = 4;
+            int res = alg.MovesToMakeZigzag(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1101_1150/1144_DecreaseElementsToMakeArrayZigzagAlg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1144_DecreaseElementsToMakeArrayZigzagAlg
+    {
+        public int MovesToMakeZigzag(int[] nums)
+        {
+            int[] s = new int[2];
+            for (int i = 0, n = nums.Length; i < n; ++i)
+            {
+                int left = i > 0 ? nums[i - 1] : int.MaxValue;
+                int right = i < n - 1 ? nums[i + 1] : int.MaxValue;
+                s[i % 2] += Math.Max(nums[i] - Math.Min(left, right) + 1, 0);
+            }
+            return Math.Min(s[0], s[1]);
+
+        }
+    }
+}
