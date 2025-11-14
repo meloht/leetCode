a2ea7a1e1c052bd59e826f5c62b539e5d6d812cd
hongtao_luo <luohongtao621@hotmail.com>
2025-11-14 10:12:52 +0800

    2536

---
+++ TestLeetCode/2501_2550/2536_IncrementSubmatricesByOneTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2501_2550;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2501_2550
+{
+    [TestClass]
+    public class _2536_IncrementSubmatricesByOneTest
+    {
+        _2536_IncrementSubmatricesByOneAlg alg = new _2536_IncrementSubmatricesByOneAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 3;
+            int[][] queries = [[1, 1, 2, 2], [0, 0, 1, 1]];
+            int[][] exp = [[1, 1, 0], [1, 2, 1], [0, 1, 1]];
+            int[][] res = alg.RangeAddQueries(n, queries);
+            Assert.IsTrue(Utils.IsSameArr(exp, res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 2;
+            int[][] queries = [[0, 0, 1, 1]];
+            int[][] exp = [[1, 1], [1, 1]];
+            int[][] res = alg.RangeAddQueries(n, queries);
+            Assert.IsTrue(Utils.IsSameArr(exp, res));
+        }
+    }
+}
---
+++ leetCode/2501_2550/2536_IncrementSubmatricesByOneAlg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2501_2550
+{
+    public class _2536_IncrementSubmatricesByOneAlg
+    {
+        public int[][] RangeAddQueries(int n, int[][] queries)
+        {
+            int[][] diff = new int[n + 1][];
+            for (int i = 0; i <= n; i++)
+            {
+                diff[i] = new int[n + 1];
+            }
+
+            foreach (var q in queries)
+            {
+                int row1 = q[0], col1 = q[1], row2 = q[2], col2 = q[3];
+                diff[row1][col1] += 1;
+                diff[row2 + 1][col1] -= 1;
+                diff[row1][col2 + 1] -= 1;
+                diff[row2 + 1][col2 + 1] += 1;
+            }
+
+            int[][] mat = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                mat[i] = new int[n];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int x1 = (i == 0) ? 0 : mat[i - 1][j];
+                    int x2 = (j == 0) ? 0 : mat[i][j - 1];
+                    int x3 = (i == 0 || j == 0) ? 0 : mat[i - 1][j - 1];
+                    mat[i][j] = diff[i][j] + x1 + x2 - x3;
+                }
+            }
+            return mat;
+        }
+
+    }
+}
