619d44bfbf6fa7f9458b73fe1f25fde019ed6ace
hongtao_luo <luohongtao621@hotmail.com>
2025-11-14 10:40:12 +0800

    1140

---
+++ TestLeetCode/1101_1150/1140_StoneGameIITest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1101_1150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1140_StoneGameIITest
+    {
+        _1140_StoneGameIIAlg alg = new _1140_StoneGameIIAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] piles = [2, 7, 9, 4, 4];
+            int exp = 10;
+            int res = alg.StoneGameII(piles);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] piles = [1, 2, 3, 4, 5, 100];
+            int exp = 104;
+            int res = alg.StoneGameII(piles);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1101_1150/1140_StoneGameIIAlg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1140_StoneGameIIAlg
+    {
+        public int StoneGameII(int[] piles)
+        {
+            int s = 0, n = piles.Length;
+            int[,] f = new int[n, n + 1];
+            for (int i = n - 1; i >= 0; --i)
+            {
+                s += piles[i];
+                for (int m = 1; m <= i / 2 + 1; ++m)
+                {
+                    if (i + m * 2 >= n)
+                        f[i, m] = s; // 全拿
+                    else
+                    {
+                        int mn = int.MaxValue;
+                        for (int x = 1; x <= m * 2; ++x)
+                            mn = Math.Min(mn, f[i + x, Math.Max(m, x)]);
+                        f[i, m] = s - mn;
+                    }
+                }
+            }
+            return f[0, 1];
+
+        }
+    }
+}
