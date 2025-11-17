a276ebd766667bd4f809841b9f9f0658d33b65b3
hongtao_luo <luohongtao621@hotmail.com>
2025-11-17 09:59:03 +0800

    1437

---
+++ TestLeetCode/1401_1450/1437_CheckIfAll1sAreAtLeastLengthkPlacesAwayTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._1401_1450;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1401_1450
+{
+    [TestClass]
+    public class _1437_CheckIfAll1sAreAtLeastLengthkPlacesAwayTest
+    {
+        _1437_CheckIfAll1sAreAtLeastLengthkPlacesAwayAlg alg = new _1437_CheckIfAll1sAreAtLeastLengthkPlacesAwayAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [1, 0, 0, 0, 1, 0, 0, 1];
+            int k = 2;
+            bool exp = true;
+            bool res = alg.KLengthApart(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [1, 0, 0, 1, 0, 1];
+            int k = 2;
+            bool exp = false;
+            bool res = alg.KLengthApart(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] nums = [0, 1, 0, 0, 1, 0, 0, 1];
+            int k = 2;
+            bool exp = true;
+            bool res = alg.KLengthApart(nums, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1401_1450/1437_CheckIfAll1sAreAtLeastLengthkPlacesAwayAlg.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1401_1450
+{
+    public class _1437_CheckIfAll1sAreAtLeastLengthkPlacesAwayAlg
+    {
+        public bool KLengthApart(int[] nums, int k)
+        {
+            int cnt = 0;
+            bool bl = true;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (bl && nums[i] == 1)
+                {
+                    bl = false;
+                }
+                else
+                {
+                    if (!bl)
+                    {
+                        if (nums[i] == 1)
+                        {
+                            if (cnt < k )
+                                return false;
+                            cnt = 0;
+                        }
+                        else
+                        {
+                            cnt++;
+                        }
+                    }
+                  
+                }
+               
+            }
+            return true;
+        }
+
+        public bool KLengthApart1(int[] nums, int k)
+        {
+            int n = nums.Length;
+            int prev = -1;
+            for (int i = 0; i < n; ++i)
+            {
+                if (nums[i] == 1)
+                {
+                    if (prev != -1 && i - prev - 1 < k)
+                    {
+                        return false;
+                    }
+                    prev = i;
+                }
+            }
+            return true;
+        }
+
+    }
+}
