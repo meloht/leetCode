e261a17f1ff2bd9f230ed50cd0bc1cbf11f88b5c
hongtao_luo <luohongtao621@hotmail.com>
2025-11-17 10:46:17 +0800

    1156

---
+++ TestLeetCode/1151_1200/1156_SwapForLongestRepeatedCharacterSubstringTest.cs
@@ -0,0 +1,60 @@
+﻿using leetCode._1151_1200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1151_1200
+{
+    [TestClass]
+    public class _1156_SwapForLongestRepeatedCharacterSubstringTest
+    {
+        _1156_SwapForLongestRepeatedCharacterSubstringAlg alg = new _1156_SwapForLongestRepeatedCharacterSubstringAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string text = "ababa";
+            int exp = 3;
+            int res = alg.MaxRepOpt1(text);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string text = "aaabaaa";
+            int exp = 6;
+            int res = alg.MaxRepOpt1(text);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string text = "aaabbaaa";
+            int exp = 4;
+            int res = alg.MaxRepOpt1(text);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string text = "aaaaa";
+            int exp = 5;
+            int res = alg.MaxRepOpt1(text);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase05()
+        {
+            string text = "abcdef";
+            int exp = 1;
+            int res = alg.MaxRepOpt1(text);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1151_1200/1156_SwapForLongestRepeatedCharacterSubstringAlg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1151_1200
+{
+    public class _1156_SwapForLongestRepeatedCharacterSubstringAlg
+    {
+        public int MaxRepOpt1(string text)
+        {
+            Dictionary<char, int> count = new Dictionary<char, int>();
+            foreach (char c in text)
+            {
+                count.TryAdd(c, 0);
+                count[c]++;
+            }
+
+            int res = 0;
+            for (int i = 0; i < text.Length;)
+            {
+                // step1: 找出当前连续的一段 [i, j)
+                int j = i;
+                while (j < text.Length && text[j] == text[i])
+                {
+                    j++;
+                }
+                int curCnt = j - i;
+
+                // step2: 如果这一段长度小于该字符出现的总数，并且前面或后面有空位，则使用 curCnt + 1 更新答案
+                if (curCnt < (count.ContainsKey(text[i]) ? count[text[i]] : 0) && (j < text.Length || i > 0))
+                {
+                    res = Math.Max(res, curCnt + 1);
+                }
+
+                // step3: 找到这一段后面与之相隔一个不同字符的另一段 [j + 1, k)，如果不存在则 k = j + 1 
+                int k = j + 1;
+                while (k < text.Length && text[k] == text[i])
+                {
+                    k++;
+                }
+                res = Math.Max(res, Math.Min(k - i, count.ContainsKey(text[i]) ? count[text[i]] : 0));
+                i = j;
+            }
+            return res;
+
+        }
+    }
+}
