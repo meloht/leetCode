de389fd10da8d627daf1531593e4a630e95ea0f4
hongtao_luo <luohongtao621@hotmail.com>
2025-11-17 11:20:44 +0800

    1145

---
+++ TestLeetCode/1101_1150/1145_BinaryTreeColoringGameTest.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._1101_1150;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1101_1150
+{
+    [TestClass]
+    public class _1145_BinaryTreeColoringGameTest
+    {
+        _1145_BinaryTreeColoringGameAlg alg = new _1145_BinaryTreeColoringGameAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+            int n = 11, x = 3;
+            TreeNode tree = TreeNode.BuildTree(root);
+            bool exp = true;
+            bool res = alg.BtreeGameWinningMove(tree, n, x);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [1, 2, 3];
+            int n = 3, x = 1;
+            TreeNode tree = TreeNode.BuildTree(root);
+            bool exp = false;
+            bool res = alg.BtreeGameWinningMove(tree, n, x);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1101_1150/1145_BinaryTreeColoringGameAlg.cs
@@ -0,0 +1,38 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1101_1150
+{
+    public class _1145_BinaryTreeColoringGameAlg
+    {
+        private int x, lsz, rsz;
+        public bool BtreeGameWinningMove(TreeNode root, int n, int x)
+        {
+            this.x = x;
+            dfs(root);
+            return Math.Max(Math.Max(lsz, rsz), n - 1 - lsz - rsz) * 2 > n;
+
+        }
+
+        private int dfs(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+            int ls = dfs(node.left);
+            int rs = dfs(node.right);
+            if (node.val == x)
+            {
+                lsz = ls;
+                rsz = rs;
+            }
+            return ls + rs + 1;
+        }
+
+
+    }
+}
