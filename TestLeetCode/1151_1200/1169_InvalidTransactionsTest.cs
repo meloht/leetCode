7c6491e348895a6c68245ed9faeda7a9dfc15482
hongtao_luo <luohongtao621@hotmail.com>
2025-11-18 15:53:27 +0800

    1169

---
+++ TestLeetCode/1151_1200/1169_InvalidTransactionsTest.cs
@@ -0,0 +1,55 @@
+﻿using leetCode._1151_1200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1151_1200
+{
+    [TestClass]
+    public class _1169_InvalidTransactionsTest
+    {
+        _1169_InvalidTransactionsAlg alg = new _1169_InvalidTransactionsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] transactions = ["alice,20,800,mtv", "alice,50,100,beijing"];
+            string[] exp = ["alice,20,800,mtv", "alice,50,100,beijing"];
+            string[] res = alg.InvalidTransactions(transactions).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] transactions = ["alice,20,800,mtv", "alice,50,1200,mtv"];
+            string[] exp = ["alice,50,1200,mtv"];
+            string[] res = alg.InvalidTransactions(transactions).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] transactions = ["alice,20,800,mtv", "bob,50,1200,mtv"];
+            string[] exp = ["bob,50,1200,mtv"];
+            string[] res = alg.InvalidTransactions(transactions).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+
+        [TestMethod]
+        public void TestCase04()
+        {
+            string[] transactions = ["bob,689,1910,barcelona", "alex,696,122,bangkok", "bob,832,1726,barcelona", "bob,820,596,bangkok", "chalicefy,217,669,barcelona", "bob,175,221,amsterdam"];
+            string[] exp = ["bob,689,1910,barcelona", "bob,832,1726,barcelona", "bob,820,596,bangkok"];
+            string[] res = alg.InvalidTransactions(transactions).ToArray();
+            Assert.IsTrue(exp.SequenceEqual(res));
+
+        }
+    }
+}
---
+++ leetCode/1151_1200/1169_InvalidTransactionsAlg.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1151_1200
+{
+    public class _1169_InvalidTransactionsAlg
+    {
+        public IList<string> InvalidTransactions(string[] transactions)
+        {
+            string[][] trans = new string[transactions.Length][];
+            Dictionary<string, List<int>> dict = new Dictionary<string, List<int>>();
+            HashSet<int> list = new HashSet<int>();
+            for (int i = 0; i < transactions.Length; i++)
+            {
+                string[] items = transactions[i].Split(',');
+
+                trans[i] = items;
+
+
+                if (int.Parse(items[2]) > 1000)
+                {
+                    list.Add(i);
+
+                }
+                if (dict.ContainsKey(items[0]))
+                {
+                    var ls = dict[items[0]];
+                    bool bl = false;
+                    foreach (var idx in ls)
+                    {
+                        string[] arr = trans[idx];
+                        if (Math.Abs(int.Parse(arr[1]) - int.Parse(items[1])) <= 60 && arr[3] != items[3])
+                        {
+                            list.Add(idx);
+                            bl = true;
+                        }
+                    }
+                    if (bl)
+                    {
+                        list.Add(i);
+                    }
+                }
+
+                if (dict.ContainsKey(items[0]))
+                {
+                    dict[items[0]].Add(i);
+                }
+                else
+                {
+                    dict.Add(items[0], [i]);
+                }
+            }
+
+            List<string> ans=new List<string>();
+            foreach (var item in list)
+            {
+                ans.Add(transactions[item]);
+            }
+
+
+            return ans;
+        }
+    }
+}
