76158204ee4114d433aa2e3497385b20454515f5
hongtao_luo <luohongtao621@hotmail.com>
2025-11-18 14:43:56 +0800

    1161

---
+++ TestLeetCode/1151_1200/1161_MaximumLevelSumOfABinaryTreeTest.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._1151_1200;
+using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1151_1200
+{
+    [TestClass]
+    public class _1161_MaximumLevelSumOfABinaryTreeTest
+    {
+        _1161_MaximumLevelSumOfABinaryTreeAlg alg = new _1161_MaximumLevelSumOfABinaryTreeAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int?[] root = [1, 7, 0, 7, -8, null, null];
+            TreeNode node = TreeNode.BuildTree(root);
+            int exp = 2;
+            int res = alg.MaxLevelSum(node);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int?[] root = [989, null, 10250, 98693, -89388, null, null, null, -32127];
+            TreeNode node = TreeNode.BuildTree(root);
+            int exp = 2;
+            int res = alg.MaxLevelSum(node);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int?[] root = [-100, -200, -300, -20, -5, -10, null];
+            TreeNode node = TreeNode.BuildTree(root);
+            int exp = 3;
+            int res = alg.MaxLevelSum(node);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1151_1200/1161_MaximumLevelSumOfABinaryTreeAlg.cs
@@ -0,0 +1,47 @@
+﻿using leetCode.Model.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1151_1200
+{
+    public class _1161_MaximumLevelSumOfABinaryTreeAlg
+    {
+        public int MaxLevelSum(TreeNode root)
+        {
+            int max = int.MinValue;
+            int level = 1;
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            int idx = 1;
+            while (queue.Count > 0)
+            {
+                int cnt = queue.Count;
+                int sum = 0;
+                for (int i = 0; i < cnt; i++)
+                {
+                    var node = queue.Dequeue();
+                    sum += node.val;
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                }
+                if (sum > max)
+                {
+                    max = sum;
+                    level = idx;
+                }
+                idx++;
+            }
+
+            return level;
+        }
+    }
+}
