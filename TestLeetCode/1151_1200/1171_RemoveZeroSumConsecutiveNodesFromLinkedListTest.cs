09020c8987a73f2379f42cc82efb3c55a33ee249
hongtao_luo <luohongtao621@hotmail.com>
2025-11-19 10:51:21 +0800

    1171

---
+++ TestLeetCode/1151_1200/1171_RemoveZeroSumConsecutiveNodesFromLinkedListTest.cs
@@ -0,0 +1,52 @@
+﻿using leetCode;
+using leetCode._1151_1200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1151_1200
+{
+    [TestClass]
+    public class _1171_RemoveZeroSumConsecutiveNodesFromLinkedListTest
+    {
+        _1171_RemoveZeroSumConsecutiveNodesFromLinkedListAlg alg = new _1171_RemoveZeroSumConsecutiveNodesFromLinkedListAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] head = [1, 2, -3, 3, 1];
+            int[] exp = [3, 1];
+            ListNode node = ListNode.BuildListNode(head);
+            var res = alg.RemoveZeroSumSublists(node);
+            int[] ress = ListNode.GetLinkNodeList(res);
+            Assert.IsTrue(exp.SequenceEqual(ress));
+
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] head = [1, 2, 3, -3, 4];
+            int[] exp = [1, 2, 4];
+            ListNode node = ListNode.BuildListNode(head);
+            var res = alg.RemoveZeroSumSublists(node);
+            int[] ress = ListNode.GetLinkNodeList(res);
+            Assert.IsTrue(exp.SequenceEqual(ress));
+
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            int[] head = [1, 2, 3, -3, -2];
+            int[] exp = [1];
+            ListNode node = ListNode.BuildListNode(head);
+            var res = alg.RemoveZeroSumSublists(node);
+            int[] ress = ListNode.GetLinkNodeList(res);
+            Assert.IsTrue(exp.SequenceEqual(ress));
+
+        }
+    }
+}
---
+++ leetCode/1151_1200/1171_RemoveZeroSumConsecutiveNodesFromLinkedListAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1151_1200
+{
+    public class _1171_RemoveZeroSumConsecutiveNodesFromLinkedListAlg
+    {
+        public ListNode RemoveZeroSumSublists(ListNode head)
+        {
+            int prefix = 0;
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            Dictionary<int, ListNode> seen = new Dictionary<int, ListNode>();
+            seen[0] = dummy;
+            for (ListNode node = dummy; node != null; node = node.next)
+            {
+                prefix += node.val;
+                seen[prefix] = node;
+            }
+            prefix = 0;
+            for (ListNode node = dummy; node != null; node = node.next)
+            {
+                prefix += node.val;
+                node.next = seen[prefix].next;
+            }
+            return dummy.next;
+
+        }
+    }
+}
