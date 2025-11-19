43ecff70cea3ae21bae8fd53c653d656b2ba6cbe
hongtao_luo <luohongtao621@hotmail.com>
2025-11-19 09:41:14 +0800

    2154

---
+++ TestLeetCode/2151_2200/2154_KeepMultiplyingFoundValuesByTwoTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._2151_2200;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2151_2200
+{
+    [TestClass]
+    public class _2154_KeepMultiplyingFoundValuesByTwoTest
+    {
+        _2154_KeepMultiplyingFoundValuesByTwoAlg alg = new _2154_KeepMultiplyingFoundValuesByTwoAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[] nums = [5, 3, 6, 1, 12];
+            int original = 3;
+            int exp = 24;
+            int res = alg.FindFinalValue(nums, original);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[] nums = [2, 7, 9];
+            int original = 4;
+            int exp = 4;
+            int res = alg.FindFinalValue(nums, original);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2151_2200/2154_KeepMultiplyingFoundValuesByTwoAlg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2151_2200
+{
+    public class _2154_KeepMultiplyingFoundValuesByTwoAlg
+    {
+        public int FindFinalValue(int[] nums, int original)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+
+            while (set.Contains(original))
+            {
+                original = original * 2;
+            }
+
+            return original;
+        }
+    }
+}
