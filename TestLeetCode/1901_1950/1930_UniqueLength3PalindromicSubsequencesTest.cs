a90ca78f1c78d5ccd81db39d6b3ad25b76b345a1
hongtao_luo <luohongtao621@hotmail.com>
2025-11-21 09:54:11 +0800

    1930

---
+++ TestLeetCode/1901_1950/1930_UniqueLength3PalindromicSubsequencesTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1901_1950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1901_1950
+{
+    [TestClass]
+    public class _1930_UniqueLength3PalindromicSubsequencesTest
+    {
+        _1930_UniqueLength3PalindromicSubsequencesAlg alg = new _1930_UniqueLength3PalindromicSubsequencesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "aabca";
+            int exp = 3;
+            int res = alg.CountPalindromicSubsequence(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "adc";
+            int exp = 0;
+            int res = alg.CountPalindromicSubsequence(s);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "bbcbaba";
+            int exp = 4;
+            int res = alg.CountPalindromicSubsequence(s);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1901_1950/1930_UniqueLength3PalindromicSubsequencesAlg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._1901_1950
+{
+    public class _1930_UniqueLength3PalindromicSubsequencesAlg
+    {
+        public int CountPalindromicSubsequence(string s)
+        {
+            int ans = 0;
+            for (char alpha = 'a'; alpha <= 'z'; alpha++)
+            { // 枚举两侧字母 alpha
+                int i = s.IndexOf(alpha); // 最左边的 alpha 的下标
+                if (i < 0)
+                { // s 中没有 alpha
+                    continue;
+                }
+                int j = s.LastIndexOf(alpha); // 最右边的 alpha 的下标
+
+                bool[] has = new bool[26];
+                for (int k = i + 1; k < j; k++)
+                { // 枚举中间字母 mid
+                    int mid = s[k] - 'a';
+                    if (!has[mid])
+                    {
+                        has[mid] = true; // 避免重复统计
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+
+        }
+    }
+}
