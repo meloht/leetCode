92a5cc46cd45dcd1e0bfd04c099e7fcde1f45cb0
hongtao_luo <luohongtao621@hotmail.com>
2025-11-25 09:38:37 +0800

    1208

---
+++ TestLeetCode/1201_1250/1208_GetEqualSubstringsWithinBudgetTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._1201_1250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1201_1250
+{
+    [TestClass]
+    public class _1208_GetEqualSubstringsWithinBudgetTest
+    {
+        _1208_GetEqualSubstringsWithinBudgetAlg alg = new _1208_GetEqualSubstringsWithinBudgetAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s = "abcd", t = "bcdf";
+            int maxCost = 3;
+            int exp = 3;
+            int res = alg.EqualSubstring(s, t, maxCost);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s = "abcd", t = "cdef";
+            int maxCost = 3;
+            int exp = 1;
+            int res = alg.EqualSubstring(s, t, maxCost);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s = "abcd", t = "acde";
+            int maxCost = 0;
+            int exp = 1;
+            int res = alg.EqualSubstring(s, t, maxCost);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1201_1250/1208_GetEqualSubstringsWithinBudgetAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1201_1250
+{
+    public class _1208_GetEqualSubstringsWithinBudgetAlg
+    {
+        public int EqualSubstring(string s, string t, int maxCost)
+        {
+            int n = s.Length;
+            int[] diff = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                diff[i] = Math.Abs(s[i] - t[i]);
+            }
+            int maxLength = 0;
+            int start = 0, end = 0;
+            int sum = 0;
+            while (end < n)
+            {
+                sum += diff[end];
+                while (sum > maxCost)
+                {
+                    sum -= diff[start];
+                    start++;
+                }
+                maxLength = Math.Max(maxLength, end - start + 1);
+                end++;
+            }
+            return maxLength;
+
+        }
+    }
+}
