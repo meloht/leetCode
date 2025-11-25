b3ef4258336a5dfad5f894dd45752a0c41e0fb33
hongtao_luo <luohongtao621@hotmail.com>
2025-11-25 10:58:26 +0800

    1222

---
+++ TestLeetCode/1201_1250/1222_QueensThatCanAttackTheKingTest.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._1201_1250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1201_1250
+{
+    [TestClass]
+    public class _1222_QueensThatCanAttackTheKingTest
+    {
+        _1222_QueensThatCanAttackTheKingAlg alg = new _1222_QueensThatCanAttackTheKingAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int[][] queens = [[0, 1], [1, 0], [4, 0], [0, 4], [3, 3], [2, 4]];
+            int[] king = [0, 0];
+            IList<IList<int>> exp = [[0, 1], [1, 0], [3, 3]];
+            IList<IList<int>> res = alg.QueensAttacktheKing(queens,king);
+            Assert.IsTrue(Utils.IsSame(exp,res));
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int[][] queens = [[0, 0], [1, 1], [2, 2], [3, 4], [3, 5], [4, 4], [4, 5]];
+            int[] king = [3, 3];
+            IList<IList<int>> exp = [[2, 2], [3, 4], [4, 4]];
+            IList<IList<int>> res = alg.QueensAttacktheKing(queens, king);
+            Assert.IsTrue(Utils.IsSame(exp, res));
+        }
+    }
+}
---
+++ leetCode/1201_1250/1222_QueensThatCanAttackTheKingAlg.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1201_1250
+{
+    public class _1222_QueensThatCanAttackTheKingAlg
+    {
+        public IList<IList<int>> QueensAttacktheKing(int[][] queens, int[] king)
+        {
+            IDictionary<int, int[]> candidates = new Dictionary<int, int[]>();
+            int kx = king[0], ky = king[1];
+            foreach (int[] queen in queens)
+            {
+                int qx = queen[0], qy = queen[1];
+                int x = qx - kx, y = qy - ky;
+                if (x == 0 || y == 0 || Math.Abs(x) == Math.Abs(y))
+                {
+                    int dx = Sgn(x), dy = Sgn(y);
+                    int key = dx * 10 + dy;
+                    if (!candidates.ContainsKey(key))
+                    {
+                        candidates.Add(key, new int[] { queen[0], queen[1], Math.Abs(x) + Math.Abs(y) });
+                    }
+                    else if (candidates[key][2] > Math.Abs(x) + Math.Abs(y))
+                    {
+                        candidates[key] = new int[] { queen[0], queen[1], Math.Abs(x) + Math.Abs(y) };
+                    }
+                }
+            }
+
+            IList<IList<int>> ans = new List<IList<int>>();
+            foreach (KeyValuePair<int, int[]> pair in candidates)
+            {
+                int[] value = pair.Value;
+                IList<int> posList = new List<int>();
+                posList.Add(value[0]);
+                posList.Add(value[1]);
+                ans.Add(posList);
+            }
+            return ans;
+
+
+        }
+        public int Sgn(int x)
+        {
+            return x > 0 ? 1 : (x == 0 ? 0 : -1);
+        }
+    }
+}
