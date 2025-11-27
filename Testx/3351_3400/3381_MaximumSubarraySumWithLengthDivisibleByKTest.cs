14688de3705ec78e9924c16e25ed0d1048a311dc
hongtao_luo <luohongtao621@hotmail.com>
2025-11-27 10:17:58 +0800

    3381

---
+++ Testx/3351_3400/3381_MaximumSubarraySumWithLengthDivisibleByKTest.cs
@@ -0,0 +1,45 @@
+﻿using leetCode._3351_3400;
+using leetCode.WeeklyContest._0401;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3351_3400
+{
+    public class _3381_MaximumSubarraySumWithLengthDivisibleByKTest
+    {
+        _3381_MaximumSubarraySumWithLengthDivisibleByKAlg alg = new _3381_MaximumSubarraySumWithLengthDivisibleByKAlg();
+
+        [Fact]
+        public void Test01()
+        {
+            int[] nums = [1, 2];
+            int k = 1;
+            long exp = 3;
+            long res = alg.MaxSubarraySum(nums, k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            int[] nums = [-1, -2, -3, -4, -5];
+            int k = 4;
+            long exp = -10;
+            long res = alg.MaxSubarraySum(nums, k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            int[] nums = [-5, 1, 2, -3, 4];
+            int k = 2;
+            long exp = 4;
+            long res = alg.MaxSubarraySum(nums, k);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3351_3400/3381_MaximumSubarraySumWithLengthDivisibleByKAlg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._3351_3400
+{
+    public class _3381_MaximumSubarraySumWithLengthDivisibleByKAlg
+    {
+        public long MaxSubarraySum(int[] nums, int k)
+        {
+            int n = nums.Length;
+            long[] sum = new long[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                sum[i + 1] = sum[i] + nums[i];
+            }
+
+            long[] minS = new long[k];
+            Array.Fill(minS, long.MaxValue / 2); // 防止下面减法溢出
+
+            long ans = long.MinValue;
+            for (int j = 0; j < sum.Length; j++)
+            {
+                int i = j % k;
+                ans = Math.Max(ans, sum[j] - minS[i]);
+                minS[i] = Math.Min(minS[i], sum[j]);
+            }
+            return ans;
+
+
+        }
+
+        public long MaxSubarraySum1(int[] nums, int k)
+        {
+            long[] minS = new long[k];
+            Array.Fill(minS, long.MaxValue / 2); // 防止下面减法溢出
+            minS[k - 1] = 0;
+            long ans = long.MinValue;
+            long s = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                s += nums[j];
+                int i = j % k;
+                ans = Math.Max(ans, s - minS[i]);
+                minS[i] = Math.Min(minS[i], s);
+            }
+            return ans;
+        }
+
+    }
+}
