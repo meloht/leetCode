7c14fe2afc541ea1e770c90de2277360c8dde537
hongtao_luo <luohongtao621@hotmail.com>
2025-11-28 11:23:51 +0800

    1239

---
+++ TestLeetCode/1201_1250/1239_MaximumLengthOfAConcatenatedStringWithUniqueCharactersTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1201_1250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1201_1250
+{
+    [TestClass]
+    public class _1239_MaximumLengthOfAConcatenatedStringWithUniqueCharactersTest
+    {
+        _1239_MaximumLengthOfAConcatenatedStringWithUniqueCharactersAlg alg = new _1239_MaximumLengthOfAConcatenatedStringWithUniqueCharactersAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] arr = ["un", "iq", "ue"];
+            int exp = 4;
+            int res = alg.MaxLength(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] arr = ["cha", "r", "act", "ers"];
+            int exp = 6;
+            int res = alg.MaxLength(arr);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string[] arr = ["abcdefghijklmnopqrstuvwxyz"];
+            int exp = 26;
+            int res = alg.MaxLength(arr);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1201_1250/1239_MaximumLengthOfAConcatenatedStringWithUniqueCharactersAlg.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1201_1250
+{
+    public class _1239_MaximumLengthOfAConcatenatedStringWithUniqueCharactersAlg
+    {
+        int ans = 0;
+
+        public int MaxLength(IList<string> arr)
+        {
+            IList<int> masks = new List<int>();
+            foreach (string s in arr)
+            {
+                int mask = 0;
+                foreach (char c in s)
+                {
+                    int ch = c - 'a';
+                    if (((mask >> ch) & 1) != 0)
+                    { // 若 mask 已有 ch，则说明 s 含有重复字母，无法构成可行解
+                        mask = 0;
+                        break;
+                    }
+                    mask |= 1 << ch; // 将 ch 加入 mask 中
+                }
+                if (mask > 0)
+                {
+                    masks.Add(mask);
+                }
+            }
+
+            Backtrack(masks, 0, 0);
+            return ans;
+        }
+
+        public void Backtrack(IList<int> masks, int pos, int mask)
+        {
+            if (pos == masks.Count)
+            {
+                ans = Math.Max(ans, BitCount(mask));
+                return;
+            }
+            if ((mask & masks[pos]) == 0)
+            { // mask 和 masks[pos] 无公共元素
+                Backtrack(masks, pos + 1, mask | masks[pos]);
+            }
+            Backtrack(masks, pos + 1, mask);
+        }
+
+        private static int BitCount(int i)
+        {
+            i = i - ((i >> 1) & 0x55555555);
+            i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+            i = (i + (i >> 4)) & 0x0f0f0f0f;
+            i = i + (i >> 8);
+            i = i + (i >> 16);
+            return i & 0x3f;
+        }
+
+    }
+}
