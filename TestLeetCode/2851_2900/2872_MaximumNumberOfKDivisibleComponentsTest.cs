daab3b2b7123c9de463e2b0fde50ab69dde4d017
hongtao_luo <luohongtao621@hotmail.com>
2025-11-28 11:03:10 +0800

    2872

---
+++ TestLeetCode/2851_2900/2872_MaximumNumberOfKDivisibleComponentsTest.cs
@@ -0,0 +1,39 @@
+﻿using leetCode._2851_2900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2851_2900
+{
+    [TestClass]
+    public class _2872_MaximumNumberOfKDivisibleComponentsTest
+    {
+        _2872_MaximumNumberOfKDivisibleComponentsAlg alg = new _2872_MaximumNumberOfKDivisibleComponentsAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 5;
+            int[][] edges = [[0, 2], [1, 2], [1, 3], [2, 4]];
+            int[] values = [1, 8, 1, 4, 4];
+            int k = 6;
+            int exp = 2;
+            int res = alg.MaxKDivisibleComponents(n, edges, values, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 7;
+            int[][] edges = [[0, 1], [0, 2], [1, 3], [1, 4], [2, 5], [2, 6]];
+            int[] values = [3, 0, 6, 1, 5, 2, 1];
+            int k = 3;
+            int exp = 3;
+            int res = alg.MaxKDivisibleComponents(n, edges, values, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2851_2900/2872_MaximumNumberOfKDivisibleComponentsAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._2851_2900
+{
+    public class _2872_MaximumNumberOfKDivisibleComponentsAlg
+    {
+        private int ans;
+        public int MaxKDivisibleComponents(int n, int[][] edges, int[] values, int k)
+        {
+            List<int>[] g =new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                g[i] = new List<int>();
+            }
+            foreach (int[] e in edges)
+            {
+                int x = e[0];
+                int y = e[1];
+                g[x].Add(y);
+                g[y].Add(x);
+            }
+
+            dfs(0, -1, g, values, k);
+            return ans;
+
+        }
+
+        // 返回子树 x 的点权和
+        private long dfs(int x, int fa, List<int>[] g, int[] values, int k)
+        {
+            long sum = values[x];
+            foreach (int y in g[x])
+            {
+                if (y != fa)
+                { // 避免访问父节点
+                  // 加上子树 y 的点权和，得到子树 x 的点权和
+                    sum += dfs(y, x, g, values, k);
+                }
+            }
+            ans += sum % k == 0 ? 1 : 0;
+            return sum;
+        }
+
+    }
+}
