339c2845559e0aadb0c47e37ff570060456ee0a3
hongtao_luo <luohongtao621@hotmail.com>
2025-11-28 11:33:50 +0800

    1247

---
+++ TestLeetCode/1201_1250/1247_MinimumSwapsToMakeStringsEqualTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1201_1250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1201_1250
+{
+    [TestClass]
+    public class _1247_MinimumSwapsToMakeStringsEqualTest
+    {
+        _1247_MinimumSwapsToMakeStringsEqualAlg alg = new _1247_MinimumSwapsToMakeStringsEqualAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string s1 = "xx", s2 = "yy";
+            int exp = 1;
+            int res = alg.MinimumSwap(s1, s2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string s1 = "xy", s2 = "yx";
+            int exp = 2;
+            int res = alg.MinimumSwap(s1, s2);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase03()
+        {
+            string s1 = "xx", s2 = "xy";
+            int exp = -1;
+            int res = alg.MinimumSwap(s1, s2);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1201_1250/1247_MinimumSwapsToMakeStringsEqualAlg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1201_1250
+{
+    public class _1247_MinimumSwapsToMakeStringsEqualAlg
+    {
+        public int MinimumSwap(string s1, string s2)
+        {
+            int[] cnt = new int[2];
+            for (int i = 0, n = s1.Length; i < n; i++)
+                if (s1[i] != s2[i])
+                    cnt[s1[i] % 2]++; // x 和 y ASCII 值的二进制最低位不同
+            int d = cnt[0] + cnt[1];
+            return d % 2 != 0 ? -1 : d / 2 + cnt[0] % 2;
+
+        }
+    }
+}
