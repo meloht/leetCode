82de9001a78507305a6c33af010f672a8807a818
hongtao_luo <luohongtao621@hotmail.com>
2025-12-02 09:54:53 +0800

    3623

---
+++ Testx/3601_3650/3623_CountNumberOfTrapezoidsITest.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._3601_3650;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3601_3650
+{
+    public class _3623_CountNumberOfTrapezoidsITest
+    {
+        _3623_CountNumberOfTrapezoidsIAlg alg = new _3623_CountNumberOfTrapezoidsIAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            int[][] points = [[1, 0], [2, 0], [3, 0], [2, 2], [3, 2]];
+            int exp = 3;
+            int res = alg.CountTrapezoids(points);
+            Assert.Equal(exp, res);
+        }
+        [Fact]
+        public void Test2()
+        {
+            int[][] points = [[0, 0], [1, 0], [0, 1], [2, 1]];
+            int exp = 1;
+            int res = alg.CountTrapezoids(points);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3601_3650/3623_CountNumberOfTrapezoidsIAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3601_3650
+{
+    public class _3623_CountNumberOfTrapezoidsIAlg
+    {
+        public int CountTrapezoids(int[][] points)
+        {
+            int MOD = 1_000_000_007;
+            Dictionary<int, int> cnt = []; // 预分配空间
+            foreach (int[] p in points)
+            {
+               
+                if (cnt.ContainsKey(p[1]))
+                {
+                    cnt[p[1]]++;
+                }
+                else
+                {
+                    cnt.Add(p[1], 1);
+                }
+                
+            }
+
+            long ans = 0, s = 0;
+            foreach (int c in cnt.Values)
+            {
+                long k = (long)c * (c - 1) / 2;
+                ans += s * k;
+                s += k;
+            }
+            return (int)(ans % MOD);
+
+
+        }
+    }
+}
