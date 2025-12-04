7366d45083611c3ead97021eff77bfb36cb19b26
hongtao_luo <luohongtao621@hotmail.com>
2025-12-04 09:39:55 +0800

    2211

---
+++ TestLeetCode/2201_2250/2211_CountCollisionsOnARoadTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._2201_2250;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2201_2250
+{
+    [TestClass]
+    public class _2211_CountCollisionsOnARoadTest
+    {
+        _2211_CountCollisionsOnARoadAlg alg = new _2211_CountCollisionsOnARoadAlg();
+       
+        [TestMethod]
+        public void TestCase01()
+        {
+            string directions = "RLRSLL";
+            int exp = 5;
+            int res = alg.CountCollisions(directions);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string directions = "LLRR";
+            int exp = 0;
+            int res = alg.CountCollisions(directions);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2201_2250/2211_CountCollisionsOnARoadAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2201_2250
+{
+    public class _2211_CountCollisionsOnARoadAlg
+    {
+        public int CountCollisions1(string directions)
+        {
+            string s = directions.TrimStart('L').TrimEnd('R');
+
+            return s.Length - s.Count(p => p == 'S');
+        }
+
+        public int CountCollisions(string directions)
+        {
+
+            int left = -1;
+            int right = directions.Length;
+            int s = 0;
+            int num = directions.Length;
+            for (int i = 0; i < directions.Length; i++)
+            {
+                if (directions[i] == 'L')
+                {
+                    left = i;
+                    num--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            for (int i = directions.Length - 1; i >= 0; i--)
+            {
+                if (directions[i] == 'R')
+                {
+                    right = i;
+                    num--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int i = left + 1; i < right; i++)
+            {
+                if (directions[i] == 'S')
+                {
+                    s++;
+                }
+            }
+            return num - s;
+        }
+    }
+}
