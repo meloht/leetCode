254ee87bf82c62972fcbfeddaea8a8bb5af5d407
hongtao_luo <luohongtao621@hotmail.com>
2025-12-05 09:16:58 +0800

    3432

---
+++ leetCode/3401_3450/3432_CountPartitionsWithEvenSumDifferenceAlg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3401_3450
+{
+    public class _3432_CountPartitionsWithEvenSumDifferenceAlg
+    {
+        public int CountPartitions(int[] nums)
+        {
+            int ans = 0;
+            int left = nums[0]; int right = 0;
+            int total = nums.Sum();
+            for (int i = 1; i < nums.Length; i++)
+            {
+                left += nums[i];
+                right = total - left;
+                if (Math.Abs(left - right) % 2 == 0)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
