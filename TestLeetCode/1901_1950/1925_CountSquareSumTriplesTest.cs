369a7adf09e34b5ed685f1b7a070f839ff463628
hongtao_luo <luohongtao621@hotmail.com>
2025-12-08 09:43:15 +0800

    1925

---
+++ TestLeetCode/1901_1950/1925_CountSquareSumTriplesTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._1901_1950;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1901_1950
+{
+    [TestClass]
+    public class _1925_CountSquareSumTriplesTest
+    {
+        _1925_CountSquareSumTriplesAlg alg = new _1925_CountSquareSumTriplesAlg();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            int n = 5;
+            int exp = 2;
+            int res = alg.CountTriples(n);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            int n = 10;
+            int exp = 4;
+            int res = alg.CountTriples(n);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1901_1950/1925_CountSquareSumTriplesAlg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1901_1950
+{
+    public class _1925_CountSquareSumTriplesAlg
+    {
+        public int CountTriples(int n)
+        {
+            int res = 0;
+
+            for (int a = 1; a <= n; ++a)
+            {
+                for (int b = 1; b <= n; ++b)
+                {
+               
+                    int c = (int)Math.Sqrt(a * a + b * b + 1.0);
+                    if (c <= n && c * c == a * a + b * b)
+                    {
+                        ++res;
+                    }
+                }
+            }
+            return res;
+
+        }
+    }
+}
