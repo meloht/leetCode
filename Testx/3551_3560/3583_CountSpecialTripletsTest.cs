fc32c7b274bed0502345f8314108f05bae384159
hongtao_luo <luohongtao621@hotmail.com>
2025-12-09 09:55:18 +0800

    3583

---
+++ Testx/3551_3560/3583_CountSpecialTripletsTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._3551_3560;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3551_3560
+{
+    public class _3583_CountSpecialTripletsTest
+    {
+        _3583_CountSpecialTripletsAlg alg = new _3583_CountSpecialTripletsAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            int[] nums = [6, 3, 6];
+            int exp = 1;
+            int res = alg.SpecialTriplets(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] nums = [0, 1, 0, 0];
+            int exp = 1;
+            int res = alg.SpecialTriplets(nums);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] nums = [8, 4, 2, 8, 4];
+            int exp = 2;
+            int res = alg.SpecialTriplets(nums);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3551_3560/3583_CountSpecialTripletsAlg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3551_3560
+{
+    public class _3583_CountSpecialTripletsAlg
+    {
+        public int SpecialTriplets(int[] nums)
+        {
+            int mod = 1_000_000_007;
+            Dictionary<int, int> dictSuf = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (dictSuf.ContainsKey(item))
+                {
+                    dictSuf[item]++;
+                }
+                else
+                {
+                    dictSuf.Add(item, 1);
+                }
+            }
+            long ans = 0;
+            Dictionary<int, int> dictPre = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (dictSuf.ContainsKey(item))
+                {
+                    dictSuf[item]--;
+                }
+                if (dictPre.ContainsKey(item * 2) && dictSuf.ContainsKey(item * 2))
+                {
+                    ans += (long)dictPre[item * 2] * (long)dictSuf[item * 2];
+                }
+
+                if (dictPre.ContainsKey(item))
+                {
+                    dictPre[item]++;
+                }
+                else
+                {
+                    dictPre.Add(item, 1);
+                }
+            }
+            return (int)(ans % mod);
+        }
+    }
+}
