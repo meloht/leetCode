56dac43be06f6f1db05485b72d122fb380375dfe
hongtao_luo <luohongtao621@hotmail.com>
2025-12-10 09:38:06 +0800

    3577

---
+++ Testx/3551_3600/3577_CountTheNumberOfComputerUnlockingPermutationsTest.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._3551_3600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3551_3600
+{
+    public class _3577_CountTheNumberOfComputerUnlockingPermutationsTest
+    {
+        _3577_CountTheNumberOfComputerUnlockingPermutationsAlg alg = new _3577_CountTheNumberOfComputerUnlockingPermutationsAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            int[] complexity = [1, 2, 3];
+            int exp = 2;
+            int res = alg.CountPermutations(complexity);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] complexity = [3, 3, 3, 4, 4, 4];
+            int exp = 0;
+            int res = alg.CountPermutations(complexity);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3551_3600/3577_CountTheNumberOfComputerUnlockingPermutationsAlg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3551_3600
+{
+    public class _3577_CountTheNumberOfComputerUnlockingPermutationsAlg
+    {
+        public int CountPermutations(int[] complexity)
+        {
+            int MOD = 1_000_000_007;
+            long ans = 1;
+            for (int i = 1; i < complexity.Length; i++)
+            {
+                if (complexity[i] <= complexity[0])
+                {
+                    return 0;
+                }
+                ans = ans * i % MOD;
+            }
+            return (int)ans;
+
+        }
+    }
+}
