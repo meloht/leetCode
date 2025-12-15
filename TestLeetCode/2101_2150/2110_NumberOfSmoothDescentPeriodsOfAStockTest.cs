47a5fb769edd0a0b50ea1c7013c4f973b250695a
hongtao_luo <luohongtao621@hotmail.com>
2025-12-15 12:22:21 +0800

    2110

---
+++ TestLeetCode/2101_2150/2110_NumberOfSmoothDescentPeriodsOfAStockTest.cs
@@ -0,0 +1,41 @@
+﻿using leetCode._2101_2150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2101_2150
+{
+    [TestClass]
+    public class _2110_NumberOfSmoothDescentPeriodsOfAStockTest
+    {
+        _2110_NumberOfSmoothDescentPeriodsOfAStockAlg alg = new _2110_NumberOfSmoothDescentPeriodsOfAStockAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] prices = [3, 2, 1, 4];
+            long exp = 7;
+            long res = alg.GetDescentPeriods(prices);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] prices = [8, 6, 7, 7];
+            long exp = 4;
+            long res = alg.GetDescentPeriods(prices);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void Test03()
+        {
+            int[] prices = [1];
+            long exp = 1;
+            long res = alg.GetDescentPeriods(prices);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2101_2150/2110_NumberOfSmoothDescentPeriodsOfAStockAlg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2101_2150
+{
+    public class _2110_NumberOfSmoothDescentPeriodsOfAStockAlg
+    {
+        public long GetDescentPeriods(int[] prices)
+        {
+
+            long ans = 0;
+            int dec = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (i > 0 && prices[i] == prices[i - 1] - 1)
+                {
+                    dec++; // 连续递减
+                }
+                else
+                {
+                    dec = 1; // 连续递减中断，重新统计
+                }
+                ans += dec; // dec 是右端点为 i 的连续递减子数组个数
+            }
+            return ans;
+
+        }
+    }
+}
