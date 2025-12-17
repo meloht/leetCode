33a474fb19187e5b8a434407159f019bd1d0f6c1
hongtao_luo <luohongtao621@hotmail.com>
2025-12-17 10:07:17 +0800

    3573

---
+++ Testx/3551_3600/3573_BestTimeToBuyAndSellStockVTest.cs
@@ -0,0 +1,34 @@
+﻿using leetCode._3551_3600;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3551_3600
+{
+    public class _3573_BestTimeToBuyAndSellStockVTest
+    {
+        _3573_BestTimeToBuyAndSellStockVAlg alg = new _3573_BestTimeToBuyAndSellStockVAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            int[] prices = [1, 7, 9, 8, 2];
+            int k = 2;
+            long exp = 14;
+            long res = alg.MaximumProfit(prices, k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] prices = [12, 16, 19, 19, 8, 1, 19, 13, 9];
+            int k = 3;
+            long exp = 36;
+            long res = alg.MaximumProfit(prices, k);
+            Assert.Equal(exp, res);
+        }
+    }
+}
---
+++ leetCode/3551_3600/3573_BestTimeToBuyAndSellStockVAlg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3551_3600
+{
+    public class _3573_BestTimeToBuyAndSellStockVAlg
+    {
+        public long MaximumProfit(int[] prices, int k)
+        {
+            long[][] f = new long[k + 2][];
+            for (int i = 0; i < k + 2; i++)
+            {
+                f[i] = new long[3];
+            }
+            for (int j = 1; j <= k + 1; j++)
+            {
+                f[j][1] = long.MinValue / 2; // 防止溢出
+            }
+            f[0][0] = long.MinValue / 2;
+            foreach (int p in prices)
+            {
+                for (int j = k + 1; j > 0; j--)
+                {
+                    f[j][0] = Math.Max(f[j][0], Math.Max(f[j][1] + p, f[j][2] - p));
+                    f[j][1] = Math.Max(f[j][1], f[j - 1][0] - p);
+                    f[j][2] = Math.Max(f[j][2], f[j - 1][0] + p);
+                }
+            }
+            return f[k + 1][0];
+
+        }
+    }
+}
