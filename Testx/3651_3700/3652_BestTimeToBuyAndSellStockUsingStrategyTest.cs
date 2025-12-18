7673b2328968f7242e2bbb0c142f2b01f4828241
hongtao_luo <luohongtao621@hotmail.com>
2025-12-18 10:00:46 +0800

    3652

---
+++ Testx/3651_3700/3652_BestTimeToBuyAndSellStockUsingStrategyTest.cs
@@ -0,0 +1,36 @@
+﻿using leetCode._3651_3700;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testx._3651_3700
+{
+    public class _3652_BestTimeToBuyAndSellStockUsingStrategyTest
+    {
+        _3652_BestTimeToBuyAndSellStockUsingStrategyAlg alg = new _3652_BestTimeToBuyAndSellStockUsingStrategyAlg();
+
+        [Fact]
+        public void Test1()
+        {
+            int[] prices = [4, 2, 8], strategy = [-1, 0, 1];
+            int k = 2;
+            long exp = 10;
+            long res = alg.MaxProfit(prices, strategy, k);
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] prices = [5, 4, 3], strategy = [1, 1, 0];
+            int k = 2;
+            long exp = 9;
+            long res = alg.MaxProfit(prices, strategy, k);
+            Assert.Equal(exp, res);
+        }
+
+
+    }
+}
---
+++ leetCode/3651_3700/3652_BestTimeToBuyAndSellStockUsingStrategyAlg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3651_3700
+{
+    public class _3652_BestTimeToBuyAndSellStockUsingStrategyAlg
+    {
+        public long MaxProfit(int[] prices, int[] strategy, int k)
+        {
+            int n = prices.Length;
+            long[] sum = new long[n + 1];
+            long[] sumSell = new long[n + 1];
+
+            for (int i = 0; i < n; i++)
+            {
+                sum[i + 1] = sum[i] + prices[i] * strategy[i];
+                sumSell[i + 1] = sumSell[i] + prices[i];
+            }
+            long ans = sum[n];
+
+            for (int i = k; i <= n; i++)
+            {
+                long res = sum[i - k] + sum[n] - sum[i] + sumSell[i] - sumSell[i - k / 2];
+                ans = Math.Max(ans, res);
+            }
+
+            return ans;
+        }
+    }
+}
