1eca2b6d6f7c65ff1db64e166396098c4b22922d
hongtao_luo <luohongtao621@hotmail.com>
2025-12-23 14:01:41 +0800

    2054

---
+++ TestLeetCode/2051_2100/2054_TwoBestNonOverlappingEventsTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2051_2100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2051_2100
+{
+    [TestClass]
+    public class _2054_TwoBestNonOverlappingEventsTest
+    {
+        _2054_TwoBestNonOverlappingEventsAlg alg = new _2054_TwoBestNonOverlappingEventsAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[][] events = [[1, 3, 2], [4, 5, 2], [2, 4, 3]];
+            int exp = 4;
+            int res = alg.MaxTwoEvents(events);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[][] events = [[1, 3, 2], [4, 5, 2], [1, 5, 5]];
+            int exp = 5;
+            int res = alg.MaxTwoEvents(events);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[][] events = [[1, 5, 3], [1, 5, 1], [6, 6, 5]];
+            int exp = 8;
+            int res = alg.MaxTwoEvents(events);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2051_2100/2054_TwoBestNonOverlappingEventsAlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._2051_2100
+{
+    public class _2054_TwoBestNonOverlappingEventsAlg
+    {
+        public int MaxTwoEvents(int[][] events)
+        {
+            Array.Sort(events, (a, b)=>a[1] - b[1]);
+
+            int ans = 0;
+            int size = 0; // 把 events 当作栈
+            foreach (int[] e in events)
+            {
+                int i = search(events, size, e[0]);
+                int value = e[2];
+                if (i >= 0)
+                {
+                    ans = Math.Max(ans, value + events[i][2]);
+                }
+                else
+                {
+                    ans = Math.Max(ans, value);
+                }
+
+                if (size == 0 || value > events[size - 1][2])
+                {
+                    events[size++] = e;
+                }
+            }
+            return ans;
+
+        }
+
+        // 返回最后一个满足 st[i][1] < target 的 i
+        private int search(int[][] st, int right, int target)
+        {
+            int left = -1;
+            while (left + 1 < right)
+            { // 开区间二分
+                int mid = left + (right - left) / 2;
+                if (st[mid][1] < target)
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+
+    }
+}
