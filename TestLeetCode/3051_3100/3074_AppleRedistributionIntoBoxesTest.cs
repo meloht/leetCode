1a24591cd168c55b42565d5dda45ee99743f400d
hongtao_luo <luohongtao621@hotmail.com>
2025-12-24 09:38:43 +0800

    2054

---
+++ TestLeetCode/3051_3100/3074_AppleRedistributionIntoBoxesTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._3051_3100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3051_3100
+{
+    [TestClass]
+    public class _3074_AppleRedistributionIntoBoxesTest
+    {
+        _3074_AppleRedistributionIntoBoxesAlg alg = new _3074_AppleRedistributionIntoBoxesAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] apple = [1, 3, 2], capacity = [4, 3, 1, 5, 2];
+            int exp = 2;
+            int res = alg.MinimumBoxes(apple, capacity);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] apple = [5, 5, 5], capacity = [2, 4, 2, 7];
+            int exp = 4;
+            int res = alg.MinimumBoxes(apple, capacity);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3051_3100/3074_AppleRedistributionIntoBoxesAlg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._3051_3100
+{
+    public class _3074_AppleRedistributionIntoBoxesAlg
+    {
+        public int MinimumBoxes(int[] apple, int[] capacity)
+        {
+            Array.Sort(capacity, (a, b) => b - a);
+            int sum = apple.Sum();
+            int num = 0;
+
+            for (int i = 0; i < capacity.Length; i++)
+            {
+                num += capacity[i];
+                if (num >= sum)
+                {
+                    return i + 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
