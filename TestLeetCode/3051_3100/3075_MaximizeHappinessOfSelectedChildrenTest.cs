e8e5d94524f686e8aa75a07664b4d9f7d93685d3
hongtao_luo <luohongtao621@hotmail.com>
2025-12-25 09:32:14 +0800

    3075

---
+++ TestLeetCode/3051_3100/3075_MaximizeHappinessOfSelectedChildrenTest.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._3051_3100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._3051_3100
+{
+    [TestClass]
+    public class _3075_MaximizeHappinessOfSelectedChildrenTest
+    {
+        _3075_MaximizeHappinessOfSelectedChildrenAlg alg = new _3075_MaximizeHappinessOfSelectedChildrenAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] happiness = [1, 2, 3];
+            int k = 2;
+            long exp = 4;
+            long res = alg.MaximumHappinessSum(happiness, k);
+            Assert.AreEqual(exp, res);
+        }
+        [TestMethod]
+        public void Test02()
+        {
+            int[] happiness = [1, 1, 1, 1];
+            int k = 2;
+            long exp = 1;
+            long res = alg.MaximumHappinessSum(happiness, k);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[] happiness = [2, 3, 4, 5];
+            int k = 1;
+            long exp = 5;
+            long res = alg.MaximumHappinessSum(happiness, k);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/3051_3100/3075_MaximizeHappinessOfSelectedChildrenAlg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace leetCode._3051_3100
+{
+    public class _3075_MaximizeHappinessOfSelectedChildrenAlg
+    {
+        public long MaximumHappinessSum(int[] happiness, int k)
+        {
+            Array.Sort(happiness);
+            int n = happiness.Length;
+            long ans = 0;
+            for (int i = n - 1; i >= n - k && happiness[i] > n - 1 - i; i--)
+            {
+                ans += happiness[i] - (n - 1 - i);
+            }
+            return ans;
+        }
+    }
+}
