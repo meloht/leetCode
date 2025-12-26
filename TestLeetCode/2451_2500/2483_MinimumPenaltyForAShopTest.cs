6e96c78b39b76eabc0e1d365280fee460d5f19c7
hongtao_luo <luohongtao621@hotmail.com>
2025-12-26 09:53:04 +0800

    2483

---
+++ TestLeetCode/2451_2500/2483_MinimumPenaltyForAShopTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._2451_2500;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2451_2500
+{
+    [TestClass]
+    public class _2483_MinimumPenaltyForAShopTest
+    {
+        _2483_MinimumPenaltyForAShopAlg alg = new _2483_MinimumPenaltyForAShopAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            string customers = "YYNY";
+            int exp = 2;
+            int res = alg.BestClosingTime(customers);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            string customers = "NNNNN";
+            int exp = 0;
+            int res = alg.BestClosingTime(customers);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            string customers = "YYYY";
+            int exp = 4;
+            int res = alg.BestClosingTime(customers);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/2451_2500/2483_MinimumPenaltyForAShopAlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._2451_2500
+{
+    public class _2483_MinimumPenaltyForAShopAlg
+    {
+        public int BestClosingTime(string customers)
+        {
+            int penalty = 0;
+            int minPenalty = 0;
+            int ans = 0;
+            for (int i = 0; i < customers.Length; i++)
+            {
+                penalty += customers[i] == 'N' ? 1 : -1;
+                if (penalty < minPenalty)
+                {
+                    minPenalty = penalty;
+                    ans = i + 1;
+                }
+            }
+            return ans;
+
+
+        }
+    }
+}
