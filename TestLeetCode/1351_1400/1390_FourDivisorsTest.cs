5917419b752b94732ed11d90fe8912dda9de27a7
hongtao_luo <luohongtao621@hotmail.com>
2026-01-04 09:43:30 +0800

    1390

---
+++ TestLeetCode/1351_1400/1390_FourDivisorsTest.cs
@@ -0,0 +1,42 @@
+﻿using leetCode._1351_1400;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1351_1400
+{
+    [TestClass]
+    public class _1390_FourDivisorsTest
+    {
+        _1390_FourDivisorsAlg alg = new _1390_FourDivisorsAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[] nums = [21, 4, 7];
+            int exp = 32;
+            int res= alg.SumFourDivisors(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[] nums = [21, 21];
+            int exp = 64;
+            int res = alg.SumFourDivisors(nums);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            int[] nums = [1, 2, 3, 4, 5];
+            int exp = 0;
+            int res = alg.SumFourDivisors(nums);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1351_1400/1390_FourDivisorsAlg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1351_1400
+{
+    public class _1390_FourDivisorsAlg
+    {
+        public int SumFourDivisors(int[] nums)
+        {
+            int ans = 0;
+            foreach (int num in nums)
+            {
+                // factor_cnt: 因数的个数
+                // factor_sum: 因数的和
+                int factor_cnt = 0, factor_sum = 0;
+                for (int i = 1; i * i <= num; ++i)
+                {
+                    if (num % i == 0)
+                    {
+                        ++factor_cnt;
+                        factor_sum += i;
+                        if (i * i != num)
+                        {   // 判断 i 和 num/i 是否相等，若不相等才能将 num/i 看成新的因数
+                            ++factor_cnt;
+                            factor_sum += num / i;
+                        }
+                    }
+                }
+                if (factor_cnt == 4)
+                {
+                    ans += factor_sum;
+                }
+            }
+            return ans;
+
+
+        }
+    }
+}
