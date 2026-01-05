fc2ad140b6cb4054ca021db87cfd7b06d81459cd
hongtao_luo <luohongtao621@hotmail.com>
2026-01-05 09:29:42 +0800

    1975

---
+++ TestLeetCode/1951_2000/1975_MaximumMatrixSumTest.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1951_2000;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1951_2000
+{
+    [TestClass]
+    public class _1975_MaximumMatrixSumTest
+    {
+        _1975_MaximumMatrixSumAlg alg = new _1975_MaximumMatrixSumAlg();
+
+        [TestMethod]
+        public void Test01()
+        {
+            int[][] matrix = [[1, -1], [-1, 1]];
+            long exp = 4;
+            long res = alg.MaxMatrixSum(matrix);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            int[][] matrix = [[1, 2, 3], [-1, -2, -3], [1, 2, 3]];
+            long exp = 16;
+            long res = alg.MaxMatrixSum(matrix);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1951_2000/1975_MaximumMatrixSumAlg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode._1951_2000
+{
+    public class _1975_MaximumMatrixSumAlg
+    {
+        public long MaxMatrixSum(int[][] matrix)
+        {
+            long total = 0;
+            int negCnt = 0;
+            int mn = int.MaxValue;
+            foreach (int[] row in matrix)
+            {
+                foreach (int xx in row)
+                {
+                    int x = xx;
+                    if (x < 0)
+                    {
+                        negCnt++;
+                        x = -x; // 先把负数都变成正数
+                    }
+                    mn = Math.Min(mn, x);
+                    total += x;
+                }
+            }
+
+            if (negCnt % 2 > 0)
+            {
+                total -= mn * 2; // 给绝对值最小的数添加负号
+            }
+            return total;
+
+
+        }
+    }
+}
