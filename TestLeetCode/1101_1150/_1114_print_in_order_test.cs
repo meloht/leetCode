dcdab01c2f2bce9b9987cd2cae0bc95fd8a2756c
hongtao luo <luohongtao621@163.com>
2026-09-25 15:38:22 +0800

    1114

---
+++ TestLeetCode/1101_1150/_1114_print_in_order_test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test._1101_1150
+{
+    public class _1114_print_in_order_test
+    {
+
+    }
+}
---
+++ leetCode/1101_1150/1114_print-in-order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetCode._1101_1150
+{
+    public class _1114_print_in_order
+    {
+        public class Foo
+        {
+            // 信号量，控制second能否执行，初始0（不可执行）
+            private readonly System.Threading.SemaphoreSlim _semSecond = new System.Threading.SemaphoreSlim(0);
+            // 信号量，控制third能否执行，初始0（不可执行）
+            private readonly System.Threading.SemaphoreSlim _semThird = new System.Threading.SemaphoreSlim(0);
+
+            public Foo()
+            {
+
+            }
+
+            public void First(Action printFirst)
+            {
+           
+                // printFirst() outputs "first". Do not change or remove this line.
+                printFirst();
+                _semSecond.Release();
+            }
+
+            public void Second(Action printSecond)
+            {
+                _semSecond.Wait();
+                // printSecond() outputs "second". Do not change or remove this line.
+                printSecond();
+                _semThird.Release();
+            }
+
+            public void Third(Action printThird)
+            {
+                _semThird.Wait();
+                // printThird() outputs "third". Do not change or remove this line.
+                printThird();
+            }
+        }
+    }
+}
