490a6681fe2ecba6ec4820364cef0a8f5446557c
hongtao luo <luohongtao621@163.com>
2026-09-25 15:09:16 +0800

    1078

---
+++ TestLeetCode/1051_1100/1078_occurrences_after_bigram_test.cs
@@ -0,0 +1,31 @@
+﻿using leetCode._1051_1100;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test._1051_1100
+{
+    [TestClass]
+    public class _1078_occurrences_after_bigram_test
+    {
+        _1078_occurrences_after_bigram alg = new _1078_occurrences_after_bigram();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string text = "alice is a good girl she is a good student", first = "a", second = "good";
+            string[] exp = ["girl", "student"];
+            string[] res = alg.FindOcurrences(text,first,second);
+            Assert.AreSequenceEqual(exp, res);
+        }
+        [TestMethod]
+        public void TestCase02()
+        {
+            string text = text = "we will we will rock you", first = "we", second = "will";
+            string[] exp = ["we", "rock"];
+            string[] res = alg.FindOcurrences(text, first, second);
+            Assert.AreSequenceEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/1051_1100/1078_occurrences-after-bigram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetCode._1051_1100
+{
+    public class _1078_occurrences_after_bigram
+    {
+        public string[] FindOcurrences(string text, string first, string second)
+        {
+            List<string> ans = new List<string>();
+
+
+            string[] arr = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < arr.Length-2; i++)
+            {
+                string s1=arr[i];
+                string s2 = arr[i + 1];
+                string s3 = arr[i + 2];
+                if (s1 == first && s2 == second)
+                {
+                    ans.Add(s3);
+                }
+            }
+
+            return ans.ToArray();
+        }
+    }
+}
