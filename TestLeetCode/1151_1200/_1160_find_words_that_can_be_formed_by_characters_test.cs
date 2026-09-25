ca4e511e7b831276710eade76104c1bb325f16fd
hongtao luo <luohongtao621@163.com>
2026-09-25 16:10:42 +0800

    1160

---
+++ TestLeetCode/1151_1200/_1160_find_words_that_can_be_formed_by_characters_test.cs
@@ -0,0 +1,33 @@
+﻿using leetCode._1151_1200;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test._1151_1200
+{
+    [TestClass]
+    public class _1160_find_words_that_can_be_formed_by_characters_test
+    {
+        _1160_find_words_that_can_be_formed_by_characters alg = new _1160_find_words_that_can_be_formed_by_characters();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string[] words = ["cat", "bt", "hat", "tree"];
+            string chars = "atach";
+            int exp = 6;
+            int res = alg.CountCharacters(words, chars);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string[] words = ["hello", "world", "leetcode"];
+            string chars = "welldonehoneyr";
+            int exp = 10;
+            int res = alg.CountCharacters(words, chars);
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
---
+++ leetCode/1151_1200/1160_find-words-that-can-be-formed-by-characters.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetCode._1151_1200
+{
+    public class _1160_find_words_that_can_be_formed_by_characters
+    {
+        public int CountCharacters(string[] words, string chars)
+        {
+            int[] map = new int[26];
+            foreach (var item in chars)
+            {
+                map[item - 'a']++;
+            }
+            int ans = 0;
+            foreach (var item in words)
+            {
+
+                int[] map1 = new int[26];
+                foreach (var s in item)
+                {
+                    map1[s - 'a']++;
+                }
+                bool bl = true;
+                for (int i = 0; i < 26; i++)
+                {
+                    if (map1[i] > map[i] && map1[i] > 0)
+                    {
+                        bl = false;
+                        break;
+                    }
+                }
+                if (bl)
+                {
+                    ans += item.Length;
+                }
+            }
+            return ans;
+        }
+    }
+}
