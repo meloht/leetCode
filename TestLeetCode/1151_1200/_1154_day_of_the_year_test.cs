b4930bd4c380a0f0267b6a73c2b7f9a2be942a02
hongtao luo <luohongtao621@163.com>
2026-09-25 15:52:23 +0800

    1154

---
+++ TestLeetCode/1151_1200/_1154_day_of_the_year_test.cs
@@ -0,0 +1,32 @@
+﻿using leetCode._1151_1200;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test._1151_1200
+{
+    [TestClass]
+    public class _1154_day_of_the_year_test
+    {
+        _1154_day_of_the_year alg = new _1154_day_of_the_year();
+
+        [TestMethod]
+        public void TestCase01()
+        {
+            string date = "2019-01-09";
+            int exp = 9;
+            int res = alg.DayOfYear(date);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestMethod]
+        public void TestCase02()
+        {
+            string date = "2019-02-10";
+            int exp = 41;
+            int res = alg.DayOfYear(date);
+            Assert.AreEqual(exp, res);
+        }
+
+    }
+}
---
+++ leetCode/1151_1200/1154_day-of-the-year.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetCode._1151_1200
+{
+    public class _1154_day_of_the_year
+    {
+        public int DayOfYear(string date)
+        {
+            string[] arr = date.Split('-');
+            int year = int.Parse(arr[0]);
+            int month = int.Parse(arr[1]);
+            int day = int.Parse(arr[2]);
+            int[] days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+            int num = day;
+            for (int i = 0; i < month - 1; i++)
+            {
+                num += days[i];
+                if (i == 1 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
+                {
+                    num++;
+                }
+
+            }
+
+            return num;
+        }
+    }
+}
